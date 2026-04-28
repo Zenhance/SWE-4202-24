851839eb07d4c81f3ed7364d34a7089acf679458
mahibrahaman175-stack <mahibrahaman175@gmail.com>
2026-04-28 13:05:27 +0600

    innitial commit

---
+++ Lab-02/task1/src/Design.java
@@ -0,0 +1,53 @@
+public class Design {
+
+    /* Answer to the question number a of part A
+     The following information should be stored inside an account:
+     1. Account Holder's name
+        Type-string
+     2. Deposited Money.
+        Type-integer
+     3. Withdrawn money
+        Type-integer
+     4. Balance of the account.
+        Type-integer
+
+
+
+        Answer to the question number b of part A
+        The following functions can be performed by a customer:
+        1. Deposit money
+        2. Withdraw money
+        3. Check balance
+
+
+        Inputs:
+        -Account holders name;
+        -Deposited money
+        -Withdrawn money
+
+
+        Outputs:
+        -Balance of the account
+
+
+
+
+        Answer to the question number c of part A
+        Creating an array for owner names in an array would create much of an hassle. because, it will
+        have lots of codes of lines and in a very unorganized way. again, it will be very difficult
+        to track the particular owner name in the array. also, creating an array instead of a certain class
+        would disorganize the data and its highly discouraged to do so while dealing with huge number like
+        200 or more. thats why, array would be a bad decision.
+
+
+
+
+        Answer to the question number d of part A
+        i would add it into a different class if the bank wanted to add a function of 'transactions per day'.
+        This is because, in the main function, if i added this extra function, i wouldnt be able to
+        modify it according to my own need, and also, i wouldnt be able to use it into a different
+        other function/methods if necessary. so, for later usability and for less hassle, i would create the
+        'transactions per day' into a different class and use it accordingly.
+     */
+
+}
