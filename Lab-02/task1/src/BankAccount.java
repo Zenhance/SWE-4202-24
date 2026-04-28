05982ef9d71b9e745e88031a73397c87f9367e2d
Sadman Raiyan <raiyansadman121@gmail.com>
2026-04-28 22:24:26 +0600

    Taking constructor

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -2,5 +2,8 @@ public class BankAccount {
         private String owner;
         private double balance;
 
-
+        BankAccount(String owner,double balance){
+            this.owner = owner;
+            this.balance = 0;
+        }
 }
