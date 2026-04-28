baa6af57c36403e4091c181120d1572b589ac6ef
Samin <mhsamin10@gmail.com>
2026-04-28 10:22:34 +0600

    init commit

--- Lab-02/task-1/src/Main.java
+++ Lab-02/task-1/src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        BankAccount alice = new BankAccount("Alice");
+        BankAccount charlie = new BankAccount("Charlie");
+        BankAccount bob = new BankAccount("Bob");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        alice.deposit(500);
+        alice.withdraw(100);
+
+        bob.deposit(400);
+        bob.withdraw(300);
+
+        charlie.deposit(1000);
+        charlie.withdraw(600);
+
+
+        alice.printStatement();
+        bob.printStatement();
+        charlie.printStatement();
     }
 }
