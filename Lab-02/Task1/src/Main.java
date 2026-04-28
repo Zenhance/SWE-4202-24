ecb9a7310ffee4cacd895863ed858399c7875848
noobrogrammer1123 <mubtasimsmail@gmail.com>
2026-04-28 09:23:32 +0600

    init commit

--- Lab-02/Task1/src/Main.java
+++ Lab-02/Task1/src/Main.java
@@ -1,13 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+// (a) Every account should store two info. The name of the account owner
+//and the current balance of the account.
+//name -> String
+//balance -> double
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
-}
+//(b)depositMoney, withdrawMoney and checkBalance.
+//for depositMoney need to know the deposit amount
