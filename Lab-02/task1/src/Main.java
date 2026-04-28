7522b1a3f78bd807a4f5f4aac8e4d1c6bdca68d0
Sadman Raiyan <raiyansadman121@gmail.com>
2026-04-28 10:44:52 +0600

    Hello

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
+    //a.the owner variable is a String and balance double type.
+    /*b.Customer can
+    Deposit:
+        input:balance
+        output:balance increase
+     Withdraw:
+        input:amount
+        output:balance decrease
+      Check balance:
+        input:none
+        output:balance
+
+      c.
+     */
+
     public static void main(String[] args) {
 
     }
