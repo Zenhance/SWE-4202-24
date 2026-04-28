31cbf681d581a8f3ce0a77108a311eae20ab62a2
Tasnita Raida <tasnitar@gmail.com>
2026-04-28 12:53:48 +0600

    initial commit

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -1,11 +1,22 @@
 /*
 a. owner name - String
-    current balance - float
+    current balance - double
 b. 1. Deposit money - input:deposited amount; output:
     2. Withdraw money - input: withdraw amount
     3.
 c.
 */
 public class BankAccount {
+    String name;
+    double balance=0;
 
+    BankAccount(String name,
+                double balance) {
+        this.name = name;
+        this.balance = balance;
+
+        void deposit() {
+
+        }
+    }
 }
