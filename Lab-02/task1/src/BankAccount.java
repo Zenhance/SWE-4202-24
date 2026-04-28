63907ba513caee9474705bf98713a9c6add910e8
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-04-28 12:56:35 +0600

    task 1

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -13,27 +13,30 @@ public class BankAccount {
     }
 
     void deposit(double amount) {
+        balance += amount;
         if (amount <= 0) {
             System.out.println("error");
         }
-        balance += amount;
+        return;
     }
 
     void withdraw(double amount){
-        balance-=amount;
-        if(amount<0) {
+        balance -= amount;
+        if(amount<0 || amount>balance) {
             System.out.println("Insufficient funds");
         }
+        return;
     }
 
     void printStatement(){
-        System.out.println("Name:"+name+"Balance:"+balance);
+
+        System.out.println("Name:"+name+" "+"Balance:"+balance);
     }
 }
 
-
 /*
 a. BankAccount is a class and alice and bob are objects as in instances of the class.
-b. When alice.deposit(500) is called, balance of alice changes. The program knows to update alice's balance through the dot operator which assigns values and it is also known as method invocation.
-c.
+b. When alice.deposit(500) is called, balance of alice changes from 500 to 1000 since 500 was deposited(added) to the balance. The program knows to update alice's balance through the dot operator which assigns values and it is also known as method invocation.
+c. In memory the instances of classes called alice and bob is created and gets assigned the string "Alice" and "Bob" respectively in place of its name attribute.
+d. For writing alice=bob, all the attributes of alice would get assigned to bob and after calling alice.deposit(100), only the balance of the object alice is changed and no changes occur in the object called bob since each object is a distinct entity and if it was not called for making changes, it will not change.
  */
--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,13 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main{
+    public static void main(String[] args){
+        BankAccount alice = new BankAccount("Alice");
+        BankAccount bob = new BankAccount("Bob");
+        BankAccount jenny = new BankAccount("Jenny");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+
+        alice.deposit(500);
+        alice.withdraw(75);
+        alice.printStatement();
+
+        bob.deposit(100);
+        bob.withdraw(75);
+        bob.printStatement();
+
+        jenny.deposit(200);
+        jenny.withdraw(75);
+        jenny.printStatement();
     }
 }
