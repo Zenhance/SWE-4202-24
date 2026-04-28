bf69c0f820939f19b46cdc1f51b2a282d202f38e
Amina Rahman <aminarahmanshalu@gmail.com>
2026-04-28 13:06:11 +0600

    initial commit

---
+++ Lab-02/task1/src/BankAcc.java
@@ -0,0 +1,34 @@
+public class BankAcc {
+    /* PART A)
+    a)info-
+    Their bank account name -type string
+    current balance information - type double
+    last deposit information - type double
+    last withdrawal money information -type double
+    their recent bank statement - type string
+
+    PART A)
+    b)Three actions a customer can perform -
+    1) deposit money
+    input - money amount to add in bank account
+    changes- bank balance increases
+    2)withdrawal money
+     input- the amount of money need to get out of bank account
+     output- decreases amount in account
+
+     3)check their account statement
+     input- their log in information
+     output-their bank statement
+      part A)
+     c)difficulties in matching owners name array with balance array
+
+     part A)
+     d)
+     I would add that in BankAcc class because part of the account balance should be handled by class
+
+
+
+
+
+     */
+}
