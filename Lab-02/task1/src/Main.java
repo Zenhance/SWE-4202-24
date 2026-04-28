c3cf0dddf29e710377ebd752ec8459ccdef1265f
mohaiminur24-ux <mohaiminur24@iut-dhaka.edu>
2026-04-28 09:43:32 +0600

    init commit

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -4,7 +4,24 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        /*(a)account ID, current balance, deposited money, withdrawn money.
+
+        (b) add money(deposit), deduct money(withdraw), check balance
+
+        first take input of customers UID & current balance...then
+        deposit: current balance & the ammount to be added
+        withdraw: current balance & the ammount to be cut
+        check balance: current balance.
+
+        (c)
+
+
+
+
+
+        */
         System.out.printf("Hello and welcome!");
+
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
