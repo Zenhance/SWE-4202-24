75bd9f46aa05b591f1c5b12e085411c61233d13c
Mahdia Hossain <hossainmahdia22@gmail.com>
2026-04-28 12:42:47 +0600

    innit commit

---
+++ Lab-02/Task1/src/BankAccount.java
@@ -0,0 +1,18 @@
+public class BankAccount {
+    /* a. Info to store: 1) ownerName - String 2) balance - double
+    b. 3 sections:
+    1)deposit(amount)
+    *input: double amount
+    *output: increases balance
+    **amount>0
+    2)withdraw(amount)
+    *input: double amount
+    *output: decreases balance
+    **balance !< 0
+    3)printStatement()
+    *input: none
+    *output: owner name and current balance
+    c. using separate arrays for name and balance-> can't match ownerName[i] to balance[i] with too many customers
+    d. in BankAccount class. part of the account behaviour, should be handled by the class
+     */
+}
