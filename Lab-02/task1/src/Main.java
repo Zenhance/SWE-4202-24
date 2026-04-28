7288d5aca4fd2782a625c6d651e59d4b52c6746d
md-musaddik <musaddikibna1@gmail.com>
2026-04-28 10:20:28 +0600

    Question 1 answered

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,9 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+/*
+* 1) the information I need to colledt
+*       User Name
+*       Account ID
+*       Account Balance
+* */
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("");
 
 
     }
