5b65200587479eb593d296c3faa5b759b8fb2d7d
Zunied-57 <zunied0411@gmail.com>
2026-04-28 10:25:03 +0600

    class created

---
+++ Lab-02/TASK_1/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class BankAccount
+{
+    String name;
+    double balance;
+    BankAccount(String name)
+    {
+        balance = 0;
+    }
+
+}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        }
+    }
+}
