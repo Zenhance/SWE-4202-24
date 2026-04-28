9f455556d2a98e5bb4045a04ed46eee27790d987
ummulkhaer <ummulkhaer@iut-dhaka.edu>
2026-04-28 13:07:56 +0600

    initial commit

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {System.out.println("Hello world!");
-/* a.Every account needs to store-
+/* a. Every account needs to store-
    1. Current Balance
    2. Deposit money
    3. Withdrawn money
@@ -15,6 +15,11 @@ public class Main {
    3. Check balance:
       Input: Current balance
       Output: New balance
+
+   c. It would need to create separate variables and functions.
+      So, when we work with a lot of data, it becomes difficult
+      handle so much data.
+   d. Will set a limit for money that is requested to deposit and withdraw.
  */
     }
 }
