0b7d67e733ce003537f88ad90aef2c6f2f06cebd
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-04-28 10:43:20 +0600

    3 questions are answered

---
+++ Lab-02/task1/src/Main.java
@@ -0,0 +1,20 @@
+/* Question Answer-
+a) Account number, Account balance, Username , password
+b) Actions can perform -
+    Check Balance
+        => Users can easily check balance when he log in to the account.
+    Withdraw Money
+        => To withdraw money one should have to know the current amount of money.
+    Add Money
+        => Users can add money with various types of method.
+
+
+c) It will be hard to change anything as it will be error prone.
+
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Code is working");
+    }
+}
