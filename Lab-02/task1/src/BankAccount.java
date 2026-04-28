ba3f314002fc0ab1d1962aa68a6d30a13e87b883
wasi <waseesomrat8@gmail.com>
2026-04-28 13:13:13 +0600

    bank acc. questions

---
+++ Lab-02/task1/src/BankAccount.java
@@ -0,0 +1,33 @@
+/*
+  a.
+  for every account the following information are needed to store:
+
+    account holder's name -- String
+    account number -- int
+    account balance -- double
+    account PIN -- int
+
+    b. A customer can perform the following three options:
+    1) Withdraw :
+        input: PIN,Account number
+        output: current balance
+    2) Deposit :
+        input : Account number , Date ,
+        output: current balance
+    3) Transfer:
+        input : Receivers account number
+        output:
+
+    c. To handle 200 customers account balances in big array may occur a big problem .
+            if somehow the account holder's name and account number mismatches than it would
+            be hard for the coder to go through all the names and accounts number one by one in array.
+
+    d. I have to add a method in bank account class. because I have to work the transaction limit
+    limit for every account holders .
+    */
+
+public class BankAccount {
+
+
+
+}
