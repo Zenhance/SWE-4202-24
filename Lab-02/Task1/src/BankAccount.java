5e66289f62d07246ad406bf638cc0fbea464d6d1
Mahdia Hossain <hossainmahdia22@gmail.com>
2026-04-28 13:22:33 +0600

    innit commit

--- Lab-02/Task1/src/BankAccount.java
+++ Lab-02/Task1/src/BankAccount.java
@@ -1,5 +1,6 @@
 public class BankAccount {
-    /* a. Info to store: 1) ownerName - String 2) balance - double
+    /* PART A
+    a. Info to store: 1) ownerName - String 2) balance - double
     b. 3 sections:
     1)deposit(amount)
     *input: double amount
@@ -7,7 +8,6 @@ public class BankAccount {
     **amount>0
     2)withdraw(amount)
     *input: double amount
-
     *output: decreases balance
     **balance !< 0
     3)printStatement()
@@ -16,4 +16,109 @@ public class BankAccount {
     c. using separate arrays for name and balance-> can't match ownerName[i] to balance[i] with too many customers
     d. in BankAccount class. part of the account behaviour, should be handled by the class
      */
+    //Part B
+    private String ownerName;
+    private double balance;
+
+    public BankAccount(String ownerName){
+        this.ownerName = ownerName;
+        this.balance = 0;
+    }
+
+    public void deposit(double amount){
+        if (amount <= 0){
+            System.out.println("Invalid Deposit Amount!!!");
+            return;
+        }
+        balance += amount;
+    }
+
+    public void withdraw(double amount){
+        if (amount > balance){
+            System.out.println("Insufficient funds");
+            return;
+        }
+        balance -= amount;
+    }
+
+    public void printStatement(){
+        System.out.println(ownerName + "Account Balance: " + balance);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
