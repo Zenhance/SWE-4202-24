970f0bfc5641e926ae96e65e971a9ecd4f3d9252
noobrogrammer1123 <mubtasimsmail@gmail.com>
2026-04-28 09:28:09 +0600

    init commit

--- Lab-02/Task1/src/Main.java
+++ Lab-02/Task1/src/Main.java
@@ -1,8 +1,12 @@
-// (a) Every account should store two info. The name of the account owner
+//(a) Every account should store two info. The name of the account owner
 //and the current balance of the account.
 //name -> String
 //balance -> double
 
 //(b)depositMoney, withdrawMoney and checkBalance.
 //depositMoney needs to know the deposit amount and it produces the new balance
-//
+//withdrawMoney need to know the withdraw amount and it produces the new balance
+//gives error if the withdraw amount is more than the balance
+//checkBalance just returns the account balance
+
+//(c)
