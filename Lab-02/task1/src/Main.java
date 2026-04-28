a9f1eac46873070a0f5372b144c6f8c35a8b589a
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-04-28 22:36:12 +0600

    part a all questions answered

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,15 +1,22 @@
-/* Question Answer-
-a) Account number, Account balance, Username , password
-b) Actions can perform -
-    Check Balance
-        => Users can easily check balance when he log in to the account.
-    Withdraw Money
-        => To withdraw money one should have to know the current amount of money.
-    Add Money
-        => Users can add money with various types of method.
+/* Question Answer
+(a) Every account need to store two information. The name of the account owner
+and the current balance of the account.
+Name => String
+Balance => float
+
+(b)Three actions that a customer can perform on their account are- DepositMoney, WithdrawMoney and CheckBalance.
+DepositMoney => It needs to know the deposit amount and it generates the new balance.
+WithdrawMoney => It needs to know the withdraw amount and it generates the new balance also it will
+give error if the Withdraw amount is more than the balance.
+CheckBalance => It returns the main account balance.
+
+(c)The moment when I need to handle 200 customers is going to a very difficult task as I have to manage all of their
+balance or add new customers of remove existing customer. If I can use two different arrays to
+store the name and the balance.
 
 
-c) It will be hard to change anything as it will be error prone.
+(d) I would add the transaction limit in the BankAccount class so that the transaction
+limit to each customer would be unique to them and not affect other people's transaction limits at all.
 
  */
 public class Main {
