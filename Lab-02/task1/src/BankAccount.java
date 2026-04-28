c83be551e9e9d5bf423e24fee13a8089c00555b0
Tasauf Abdullah <tasaufalter@gmail.com>
2026-04-28 12:34:42 +0600

    problemset A solutions

---
+++ Lab-02/task1/src/BankAccount.java
@@ -0,0 +1,33 @@
+/*
+Problemset A solutions
+a)
+every account needs to store a name (String), current balance (float)
+every account need to be aware of change of current balances(deposit, withdraw)
+
+b)
+Customer can:
+1) check current balance : it needs to know that it has to output the value of
+balance variable, and output that.
+2) deposit money: it needs to know the amount to be deposited(inputted by the user)
+and the amount to be added to previous balance and newer balance to be outputted on
+the console
+3) withdraw money: it needs to know the amount to be withdrawed(inputted by the user)
+and the amount to be reduced from previous balance and newer balance to be outputted on
+the console
+
+c)
+Memory Overflow
+
+d)
+the transaction limit will be added at the start of the main function, where a variable
+will keep track of daily changes until it hits the ceiling.
+
+ */
+
+
+
+
+
+public class BankAccount {
+
+}
