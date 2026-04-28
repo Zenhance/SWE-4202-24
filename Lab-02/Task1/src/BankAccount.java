eab85c5ba3fe76b1599274cf878ce46e046bba60
ahnaf-code <ahnafcode@gmail.com>
2026-04-28 13:04:36 +0600

    Answer of Part A

---
+++ Lab-02/Task1/src/BankAccount.java
@@ -0,0 +1,13 @@
+// Part A
+// a. Every account needs to have two things/ attributes.
+// 1. Name (string), 2. Account Number (int) 3.Balance (float)
+
+// b. Customers can perform 3 things. deposit, withdraw & check.
+// deposit --> input of amount, Adds amount to balance
+// withdraw --> input of amount, reduces amount from balance
+// check --> output of balance
+
+// c. If we use array in the main then we need to use 200 different arrays.
+// d. We will do it in a different class which will check the amount and returns a flag value.
+
+
