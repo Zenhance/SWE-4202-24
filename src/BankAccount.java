7c3335eaf28dc458a9cd5d3b547380ea406213b1
farnazrahman <farnaz28.rahman@gmail.com>
2026-04-28 13:19:27 +0600

    init commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -27,6 +27,7 @@ public class BankAccount {
     }
     void print(){
         System.out.println(name);
+        System.out.println(balance);
     }
     public static void main(String[] args){
         BankAccount Alice =new BankAccount("Alice",500.0F);
@@ -40,6 +41,13 @@ public class BankAccount {
 
         Bob.deposit(100);
         Bob.withdraw(20);
+        Bob.print();
+
+        abc.deposit(100);
+        abc.withdraw(20);
+        abc.print();
+
+
 
 
     }
