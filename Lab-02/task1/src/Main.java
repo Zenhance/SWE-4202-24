6a2c84c58857879b43d05956058785df39157305
ayiham-1012 <ayiham1012@gmail.com>
2026-04-28 13:07:40 +0600

    initial commit

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -17,4 +17,14 @@ public class Main {
         c.printStatement();
     }
 }
-//
+/*
+a. BankAccount is CLASS
+   Alice and Bob are OBJECTS or INSTANCES of the class BankAccount
+
+b. When we call alice.deposit(500), only Alice's balance changes. The program understands because
+   we have instantiated Alice differently from Bob
+
+
+d. Bob’s balance will remain unchanged, because we are putting Bob's attributes to Alice's
+   not vice versa. So his balance is still 0
+ */
