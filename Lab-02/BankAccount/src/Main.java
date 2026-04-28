3bb67263bd93469f0241838b2cc44155e3314b04
sadeedanwar-hub <sadeedanwar@iut-dhaka.edu>
2026-04-29 01:08:47 +0600

    init commit

--- Lab-02/BankAccount/src/Main.java
+++ Lab-02/BankAccount/src/Main.java
@@ -1,12 +1,28 @@
-/* a)Account name (string), (int) account balance , (string) credit history
-   b)deposit(amount) == updates balance , withdraw(amount) == updates balance , check(balance) 
-   c) THe main problem arises when we start handling 200 customers , it would be really hard to manage the users and their
-   expected requests like manage the account , withdraw , checking balance.
-   d)the most logical thing I would do is add a restriction like a transaction limit to the BankAccount class , which will only affect that single customer
-   and no one else
-*/
 public class Main {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+
+        BankAccount sadeed = new BankAccount("Sadeed");
+        BankAccount anwar = new BankAccount("Anwar");
+        BankAccount saad = new BankAccount("Saad");
+
+        sadeed.deposit(1000);
+        sadeed.withdraw(500);
+        sadeed.printBalance();
+
+        anwar.withdraw(6500);
+        anwar.printBalance();
+
+
+        saad.printBalance();
+
+        // Checking the provided problem statement.
+        sadeed = saad;
+
+        saad.printBalance();
+
+        saad.withdraw(140);
+
+        saad.printBalance();
+
     }
 }
