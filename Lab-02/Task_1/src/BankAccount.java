7485b22b6fb9471bf28872679338a1343b9317f1
Mustakim <mustakimmahfuz@gmail.com>
2026-04-28 12:40:50 +0600

    Answer to the Question No.1 Answer to the Question No.2

--- Lab-02/Task_1/src/BankAccount.java
+++ Lab-02/Task_1/src/BankAccount.java
@@ -4,6 +4,18 @@
 //                   3. Account ID in String;
 
 
+
+//Question No (b) ==> Three actions a customer can perform
+//                    1. Deposit
+//                        input: amount (double)
+//                        output: balance increase
+//                    2. Withdraw
+//                        input: amount (double)
+//                        output: Balance decrease
+//                    3. check balance
+//                        input: None
+//                        output: current balance
+
 public class BankAccount {
 
 
