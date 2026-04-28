584c49035d29ab8f223e30b76f3bb8b111c7733c
Mustakim <mustakimmahfuz@gmail.com>
2026-04-28 12:30:48 +0600

    Answer to the Question No.1

---
+++ Lab-02/Task_1/src/BankAccount.java
@@ -0,0 +1,12 @@
+//Question No.1 ==> Every account should store
+//                   1. Owner name- String;
+//                   2. Balance in double;
+//                   3. Account ID in String;
+
+
+public class BankAccount {
+
+
+
+
+}
