b8a7924e469017c057ff2618b3b0b527f156a4ed
Rakib <rakibulhasan4266@gmail.com>
2026-04-28 12:23:07 +0600

    ans question c

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -9,6 +9,14 @@
                (2)withdraw-how much amount takeout and return new amount,if takeout amount more than
                            amount it creats error
 
+       c.
+        It is very difficult to 200 customers:
+                (1)all time need to keep the index perfectly
+                (2)if one array will delete ,it creates bug
+                (3)two separate arrays force to search
+                   both manually and keep indexes, one mistake
+                   the wrong name to the wrong balance.
+
 
 
  */
