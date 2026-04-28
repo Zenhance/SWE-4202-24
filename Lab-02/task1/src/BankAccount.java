58075529d4851858dd8e7756fe9b90d5b860c6cf
ayiham-1012 <ayiham1012@gmail.com>
2026-04-28 12:41:22 +0600

    initial commit

---
+++ Lab-02/task1/src/BankAccount.java
@@ -0,0 +1,20 @@
+public class BankAccount {
+    private String name;
+    private double balance;
+
+    BankAccount(String n){
+        name = n;
+        balance = 0;
+    }
+    public void deposit(double amount){
+        balance += amount;
+    }
+    public  void withdraw(double amount){
+        balance-=amount;
+    }
+    public void printStatement(){
+        System.out.println("Owner's name: "+name);
+        System.out.println("Balance: "+balance+" Taka");
+
+    }
+}
--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount a = new BankAccount("ayiham");
+        BankAccount b = new BankAccount("mahiya");
+        BankAccount c = new BankAccount("mahi");
+
+        a.deposit(10000);
+        a.withdraw(4000);
+        a.printStatement();
+
+        b.deposit(1000);
+        b.withdraw(7000);
+        b.printStatement();
+
+        c.deposit(12300);
+        c.withdraw(50.55);
+        c.printStatement();
+
     }
 }
