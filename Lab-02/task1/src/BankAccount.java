9927e602fb0e0f29b888b02ce14a7b2f4663dea3
hasnat-farhan <hasnatfarhan@iut-dhaka.edu>
2026-04-28 20:36:05 +0600

    none

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -9,7 +9,7 @@
 *   2.withdraw money-input account number, account holder name,amount.Changes current balance, outputs current balance after being updated,error if insufficient balance
 *   3.check balance-input account number,account holder name.outputs current balance.
 *
-* c.in this implementation, we have to keep track which customer is which element of the array. if we have to edit any details(owner names,balance), we then have to find that
+* c.in this implementation, we have to keep track which customer is which element of the array. if we have to edit any details(owner name,balance), we then have to find that
 *   element and change, which is annoying. moreover, anyone, intentionally or not, can change any values of the array and cause problems.
 *
 * d.for a transaction limit, I'd put in bank account class.If I put it in main(), it won't track transaction of each user separately.
