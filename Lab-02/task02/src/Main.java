b75481586a7441c790982653507f2acc3e9a05c2
arnob <arnobarafat@iut-dhaka.edu>
2026-04-28 22:50:40 +0600

    Created and implemented push() method

--- Lab-02/task02/src/Main.java
+++ Lab-02/task02/src/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Main runs");
+
+        StudentStack list = new StudentStack();
+
+        list.push("Arnob", 99);
+        list.push("Moin", 89);
+        list.push("Sakep", 90);
+
     }
 }
--- Lab-02/task02/src/StudentStack.java
+++ Lab-02/task02/src/StudentStack.java
@@ -1,4 +1,17 @@
 public class StudentStack {
     private String[] name = new String[50];
     private int[] marks = new int[50];
+    private int count = 0;
+
+    public void push(String name, int marks) {
+        if (count + 1 > 50){
+            System.out.println("Stack is full");
+            return;
+        }
+        this.name[count] = name;
+        this.marks[count] = marks;
+        count++;
+    }
+
+
 }
