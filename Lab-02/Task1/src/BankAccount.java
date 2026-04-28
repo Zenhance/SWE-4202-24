631cd439f1517a1dad1d4f8f4b477bed870ac5d8
Sabbir <eftesabbir2725@gmail.com>
2026-04-28 12:52:30 +0600

    Answer to the question no A

---
+++ Lab-02/Task1/src/BankAccount.java
@@ -0,0 +1,13 @@
+//answer a=>
+// account id name=string
+// balance=double
+// answer b=>
+// deposit money=input amount and output the money after add amount
+// withdraw money= input the amount to withdraw and output the balance after withdraw money
+// printstatement= output the owners name and balance
+
+
+
+public class BankAccount {
+
+}
