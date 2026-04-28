ef9e2fbe3967e3874009782cf5b55e2b751cbeaa
Rakib <rakibulhasan4266@gmail.com>
2026-04-28 13:26:34 +0600

    Subtracts amount

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -13,3 +13,16 @@ public double deposite(double amount)
  balance += amount;
  return balance;
 }
+public float withdraw(double amount)
+{
+ if(amount <= balance)
+ {
+  balance -= amount;
+  return balance;
+ }
+ else
+ {
+  System.out.println(" Insufficient funds ");
+  return balance;
+ }
+}
