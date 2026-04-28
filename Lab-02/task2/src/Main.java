86870a36b0ec7c19e7ef2473a18f1699dd302af7
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-04-29 02:43:50 +0600

    implemented main method

--- Lab-02/task2/src/Main.java
+++ Lab-02/task2/src/Main.java
@@ -1,13 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main{
+    public static void main(String[] args){
+        String[] names = new String[100];
+        int[] marks = new int[100];
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        StudentStack stack = new StudentStack(names, marks);
+
+        stack.push("Alice",100);
+        stack.push("Bob",90);
+        stack.push("Jenny",80);
+        stack.push("Clinton",70);
+        stack.push("Fred",85);
+
+        stack.peek();
+
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+        stack.display();
+
     }
 }
