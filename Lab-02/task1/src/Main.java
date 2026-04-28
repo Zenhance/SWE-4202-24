13c1b2a1880b200f08c45201fcdd250961b74b83
wasifjafar <wasifjafar71@gmail.com>
2026-04-28 10:43:44 +0600

    init commit

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -3,12 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        BankAccount account1 = new BankAccount("Wasif");
 
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
-        }
     }
 }
