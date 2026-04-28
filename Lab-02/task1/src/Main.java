1d7c79442ee0dcce10bbaf73589c2d9a1aeb010d
Sadman Raiyan <raiyansadman121@gmail.com>
2026-04-28 21:59:47 +0600

    Answer of d of Part A

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -16,6 +16,10 @@ public class Main {
          it can be very hard to maintain all the data and can face
          difficulties to update or add or remove a customer.
 
+        d.I would not add the transaction limit in the main,but
+        rather create another BankBalance class. Because by doing
+        this every customer can have their unique transaction limit,
+        and it will not affect other customer's account.
      */
 
     public static void main(String[] args) {
