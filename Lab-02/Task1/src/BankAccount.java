67a85637f5213adced3a65b989f6c071682cdada
Sabbir <eftesabbir2725@gmail.com>
2026-04-28 13:03:19 +0600

    Answer to the question no A

--- Lab-02/Task1/src/BankAccount.java
+++ Lab-02/Task1/src/BankAccount.java
@@ -5,7 +5,10 @@
 // deposit money=input amount and output the money after add amount
 // withdraw money= input the amount to withdraw and output the balance after withdraw money
 // printstatement= output the owners name and balance
-
+// answer c=>
+//hard to manage,data inconsistency,difficult to extend
+// answer d=>
+// add that transaction in the BankAccount class for easier to maintain
 
 
 public class BankAccount {
