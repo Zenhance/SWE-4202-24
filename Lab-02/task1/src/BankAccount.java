4d21afc18a497d699b25133423485b65c1a2ff92
Sameer <sameerabdullah1117@gmail.com>
2026-04-28 13:03:36 +0600

    initial commit

---
+++ Lab-02/task1/src/BankAccount.java
@@ -0,0 +1,23 @@
+//a)
+//account name-string
+//account balance-double
+//b)
+//1)can deposit money
+//input-the amount to be added
+//output-the increased balance
+//2)can withdraw money
+//input-the amount to be decreased
+//output-the decreased amount
+//can check balance
+//input-account name
+//output-current balance
+//c)
+//hard to manage ,data inconsistency, difficult to extend
+//d)
+//transaction limit per day has to be added in the BankAccount Class
+//because it is easier to maintain
+
+
+public class BankAccount {
+
+}
