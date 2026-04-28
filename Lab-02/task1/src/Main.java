d21d3e05c1f410481331d8fbee226259d91d5c94
inan <inanjaki184@gmail.com>
2026-04-28 13:03:20 +0600

    partA

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,3 +1,11 @@
+/*
+a)name and current balance
+b)deposit money: input is deposit amount(int), output is new balance(int),success or error message(char)
+  withdraw money: input is withdraw amount(int), output is new balance(int),success or error message(char)
+  check balance: no value input, output is new balance(int)
+c)it would be difficult to add and/or delete new entries. it would also be difficult to search specific individual.
+d)for a new function transaction limit per day, I would add to BankAccount class
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
