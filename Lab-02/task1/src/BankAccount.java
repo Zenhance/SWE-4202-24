ef5dccd6f64537d9b73821a821603857439a7c86
Nishi <nusratnishi1103@gmail.com>
2026-04-28 12:38:06 +0600

    initial commit

---
+++ Lab-02/task1/src/BankAccount.java
@@ -0,0 +1,25 @@
+/*information to store
+  1.String owner name
+  2.float account balance
+ */
+
+/*actions perform
+  1.Deposit
+    input - account balance
+    output - balance increase
+    change-
+  2. Withdraw
+     input - account balance
+     output - balance decrease
+     change -
+  3. Check balance
+ */
+
+/* the code will be difficult to understand and maintain. it will become messy.
+Handling 200 customers and their information using array will make it hard to find and enter data.
+ */
+
+/* I will add the transaction limit in the main.
+ */
+public class BankAccount {
+}
