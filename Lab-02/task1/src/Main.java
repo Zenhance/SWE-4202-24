5b0f737e2986723991f16bd068dc1b3498210bed
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-04-28 12:55:16 +0600

    commit 1

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,5 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+/*
+a.informations may be-account name(string),current balance(int),account number(int),
+deposited money,withdrawn money(int) etc.
+
+b. deposit money, withdraw money and check their balance.
+for deposited money it will take input the amount of money user wants to deposit,and will change the balance (add to it)
+for withdraw it will take input the amount of money user wants to withdraw,and will change the balance (deduct from it)
+check the balance will take certain input such as name and account password and check the balance of user
+
+c.overflow,management difficulty
+
+d.at the start of the main,there a variable will track of the transaction and check if it hits limit or not
+*/
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
