5c05d6ea2a256a76a7ae68dc22f94a81201a0d23
Muhtasim Moin <muhtasim24@iut-dhaka.edu>
2026-04-28 09:40:52 +0600

    init commit

--- Lab-02/Task1/src/Main.java
+++ Lab-02/Task1/src/Main.java
@@ -3,12 +3,16 @@
 public class Main {
     public static void main(String[] args) {
         /*
-          A.Every account needs to store: Current Balance
+         A.Every account needs to store: Current Balance
 
          B.Three actions one can perform from their account:
-         1.Deposit:It will take an integer and deposit
-         2.Withdraw
-         3.Check Balance
+            1.Deposit:It will take a float value and deposit
+            2.Withdraw
+            3.Check Balance
+         C.Storing all account balances in one big array in the main with a seperate array of
+           owner names will cause problems because editing these informations will be difficult and inputting the
+           balances in each index of the array will also be painful.
+
          */
         }
     }
