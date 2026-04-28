310577ad84ac2d00d8b58062e05e7d01fc6c0b1b
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-04-28 19:51:55 +0600

    init commit

---
+++ Lab-02/task2/src/StudentStack.java
@@ -0,0 +1,10 @@
+public class StudentStack {
+    int[] array;
+    int count;
+//constructor-initialize
+    StudentStack(int []array, int count){
+        this.array = array;
+        this.count = count;
+    }
+
+}
