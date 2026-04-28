c36d52087a84b5b0f59a1508b03f040be4b564d3
elfie1502 <mumtahina24@iut-dhaka.edu>
2026-04-28 10:40:24 +0600

    init commit

---
+++ Lab-02/task1/src/BankAccount.java
@@ -0,0 +1,12 @@
+public class BankAccount {
+    /* Part A
+        a.
+         1.Owner's Name using String
+         2.Owner's Balance using double
+
+        b.
+         1.Deposit (input) and add balance (change)
+         2.Withdraw ( input) and deduct balance from main balance ( change)
+         3. Check Balance (output) and print the current balance
+     */
+}
