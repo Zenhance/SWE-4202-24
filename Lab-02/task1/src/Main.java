98a9e49c05e4f486d9866a9eaf2d9418a621886d
ummulkhaer <ummulkhaer@iut-dhaka.edu>
2026-04-28 12:33:55 +0600

    initial commit

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
     public static void main(String[] args) {System.out.println("Hello world!");
+/* a.Every account needs to store-
+   1. Current Balance
+   2. Deposit money
+   3. Withdrawn money
+ */
     }
 }
