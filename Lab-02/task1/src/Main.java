9ae63866e1079eb2f8efd54d841f892c56d3a1dd
Rakib <rakibulhasan4266@gmail.com>
2026-04-28 12:04:29 +0600

    ans question b

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -3,6 +3,11 @@
         every account needs to store:
               (1)ownername-string
               (2)balance-double
+       b.
+        customer can:
+               (1)deposite-how much amount add and return new amount
+               (2)withdraw-how much amount takeout and return new amount,if takeout amount more than
+                           amount it creats error
 
 
 
