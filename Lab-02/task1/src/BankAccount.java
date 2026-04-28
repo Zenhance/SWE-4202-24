039e47d7cbd1d225fa0ef94e58c9abe09ada84c0
inan <inanjaki184@gmail.com>
2026-04-28 19:55:33 +0600

    partB

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -1,3 +1,25 @@
-public class BankAccount {
+public class BankAccount
+{
+    private String name;
+    private int balance;
+    balance=0;
+    public BankAccount(String name)
+    {
+        this.name=name;
+    }
+    public void deposit(int amount)
+    {
+        if(amount<=0) {
+            System.out.println("Insufficient amount to deposit!");
+        }
+        else {
+            balance+=amount;
+        }
+    }
+    public void withdraw(int amount)
+    {
+        balance-=amount;
+    }
+
 
 }
