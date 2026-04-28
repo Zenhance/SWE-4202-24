606f41e9893f73043c0b4ee63ac6d0049c610e1d
Zadid Bin Azad <43434510+zenhance@users.noreply.github.com>
2026-04-28 09:32:44 +0600

    init commit

---
+++ Lab-02/Task1/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*
+          A.Every account needs to store: Current Balance
+
+         B.Three actions one can perform from their account:
+         1.Deposit:It will take an integer and deposit
+         2.Withdraw
+         3.Check Balance
+         */
+        }
+    }
+
