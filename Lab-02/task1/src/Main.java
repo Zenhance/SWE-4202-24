a2ec6c7c7068df86ae670acf678975ff80bd8191
iam-tahmid <tahmidim99@gmail.com>
2026-04-29 21:43:08 +0600

    Part A answers added

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+    /*
+    a. Every account need to store Account holder's name (String) and balance (double).
+    b. A customer can perform on their account:
+       1. money deposit
+       -input: amount
+       -output: adds it to balance or it can show error as well.
+
+       2. Withdrawing money
+          -input: amount
+          -output: substrats from balance or shows "insufficient funds if balance is less than withdrawing amount.
+
+        3. printStatement()
+           -input: it won't take any.
+           -output: prints owner name and balance
+
+     c. Using separate arrays to handle big number of customers can cause mismatch between names and balances. every time both of the array must be updated, else wrong amount will be stored to any owner's account.
+
+     d. transaction limit should be implemented inseide the BankAccount class. Because it is part of the account's behaviour, not the main program.
+
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
