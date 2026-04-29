600cb25963715782f448061cb04b04a891572755
tasnovatahia <tasnovatahia2014@gmail.com>
2026-04-30 03:32:31 +0600

    StudentQueuw class created

---
+++ Lab-02/task2/StudentQueue.java
@@ -0,0 +1,11 @@
+public StudentQueue{
+    String[] names;
+    double[] marks;
+    int count=0;
+
+    StudentQueue(String[] names.double[] marks){
+        this.names=names;
+        this.marks=marks;
+    }
+
+}
