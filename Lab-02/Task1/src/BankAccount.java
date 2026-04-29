158137ce2c1a885f1bf632cb65b5a209565683dc
ahnaf-code <ahnafcode@gmail.com>
2026-04-29 21:56:42 +0600

    Part B - Write The class --> 2

--- Lab-02/Task1/src/BankAccount.java
+++ Lab-02/Task1/src/BankAccount.java
@@ -13,4 +13,9 @@ public class BankAccount {
     private String name;
     private double balance;
 
+    public BankAccount(String name) {
+        this.name = name;
+        this.balance = 0;
+    }
+
 }
