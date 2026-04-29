9f48a84c43583c706fb803e372d37e93f7e65c10
tasnovatahia <tasnovatahia2014@gmail.com>
2026-04-30 03:22:13 +0600

    push and pop method commit

--- Lab-02/task2/StudentStack.java
+++ Lab-02/task2/StudentStack.java
@@ -7,4 +7,21 @@ public class StudentStack{
         this.names=names;
         this.marks=marks;
     }
+    public void push(String names,double marks){
+        if(count==500){
+            System.out.println("Stack is full");
+            return;
+        }
+        names[count]=names;
+        marks[count]=count;
+        count++;
+    }
+    public void pop(){
+       if(count==0){
+           System.out.println("Stack is empty");
+           return;
+       }
+       count--;
+        System.out.println("Name:"+names[count]+" Marks:"+marks[count]);
+    }
 }
