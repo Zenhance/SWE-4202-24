87f7b69c3191a5f91cd5b54a133c2485eea763d8
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-04-29 21:49:40 +0600

    answering part b 2

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -12,10 +12,13 @@ public class BankAccount {
     }
 
 public void deposit(double amount){
-
-
-
+if(amount<=0) {
+    System.out.println("Error!Invalid amount to deposit");}
+else {
+balance+=amount;
 }
+
+
 
 
 
--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -24,6 +24,9 @@ problematic and hard to maintain.
 public class Main {
     public static void main(String[] args) {
 
+
+
+
         System.out.println("Hello and welcome!");
 
 
