df23c08abb19da1aa220c6ed2ece7ebb07e0dd19
tasnovatahia <tasnovatahia2014@gmail.com>
2026-04-30 03:49:47 +0600

    main commit

---
+++ Lab-02/task2/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(){
+        String[] names=new String[500];
+        double[] marks=new double[500];
+
+        System.out.println("Student Stack:");
+        StudentStack stack=new StudentStack(name,marks);
+
+        stack.push("A",80);
+        stack.push("B",90);
+        stack.push("C",80);
+        stack.push("D",90);
+        stack.push("E",80);
+
+
+    }
+}
