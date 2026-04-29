80f53e92b7f37992efda9cf576dcd25a4d75f831
mahibrahaman175-stack <mahibrahaman175@gmail.com>
2026-04-29 08:55:18 +0600

    bank account with main function done

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -2,6 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        BankAccount acc1= new BankAccount("Alif");
+        BankAccount acc2= new BankAccount("Kabir");
+        BankAccount acc3= new BankAccount("Rahim");
+
+        acc1.DepositAmount(500.0);
+        acc1.WithdrawAmount(200.0);
+
+        acc2.DepositAmount(1000.0);
+        acc2.WithdrawAmount(800.0);
+
+        acc3.DepositAmount(1500.0);
+        acc3.WithdrawAmount(2000.0);
+
+        acc1.PrintStatement();
+        acc2.PrintStatement();
+        acc3.PrintStatement();
     }
 }
