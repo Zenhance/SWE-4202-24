d8aec599ec166cb1109855a0a1b371d62a45a561
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-04-29 14:25:36 +0600

    used constructor for the class

--- Lab-02/task2/src/StudentQueue.java
+++ Lab-02/task2/src/StudentQueue.java
@@ -1,2 +1,12 @@
 public class StudentQueue {
+    String[] names;
+    int[] marks;
+    int count;
+    //constructor-initialize
+    StudentQueue(String[] name, int[] mark){
+        this.names = name;
+        this.marks = mark;
+        this.count = 0;
+    }
+
 }
