6456e6d71b7d58f3a56a3a0d79959cdede59482a
Tasauf Abdullah <tasaufalter@gmail.com>
2026-04-29 21:20:19 +0600

    solution to part c of task 1, also tried to remove gitignore hoping it works.

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,3 +1,35 @@
+/*
+Problemset A solutions
+a)
+every account needs to store a name (String), current balance (float)
+every account need to be aware of change of current balances(deposit, withdraw)
+
+b)
+Customer can:
+1) check current balance : it needs to know that it has to output the value of
+balance variable, and output that.
+2) deposit money: it needs to know the amount to be deposited(inputted by the user)
+and the amount to be added to previous balance and newer balance to be outputted on
+the console
+3) withdraw money: it needs to know the amount to be withdrawed(inputted by the user)
+and the amount to be reduced from previous balance and newer balance to be outputted on
+the console
+
+c)
+Memory Overflow
+
+d)
+the transaction limit will be added in the BankAccount class, where a variable
+will keep track of daily changes until it hits the ceiling.Main should not know about
+transaction limits, that's the account's own rule.
+
+ */
+
+
+
+
+
+
 public class Main {
     public static void main(String[] args) {
         BankAccount Tasauf = new BankAccount("Tasauf");
@@ -24,3 +56,20 @@ public class Main {
         Sifat.printStatement();
     }
 }
+
+/*
+Problemset C solutions:
+
+a)BankAccount is a class, Alice and Bob are objects.
+b)only Alice's balance changes. as we are accesing as alice.deposit(), not bob.deposit().
+c)
+      alice:
+                name : "Alice"
+                balance: 0
+         bob:
+                name : "Bob"
+                balance : 0
+
+d)it would update bob's balance. Bob's balance will be incremented by 100.
+Because bob's pointer has been copied  into alice.
+ */
