52ce7d5c72325b30da38da71c7d95c8275a2e1cd
md-musaddik <musaddikibna1@gmail.com>
2026-04-30 00:18:28 +0600

    printStatement Method declared

--- Lab-02/task1/src/Account.java
+++ Lab-02/task1/src/Account.java
@@ -5,4 +5,8 @@ public class Account {
         this.name=name;
         this.balance=0.0;
     }
+    public void printStatement() {
+        System.out.println("The owner name " + name);
+        System.out.println("The Balance is " + balance);
+    }
 }
