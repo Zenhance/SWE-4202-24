2ab216fd0a72352b55543f35b7a49a3534873fd5
tasnovatahia <tasnovatahia2014@gmail.com>
2026-04-30 03:40:15 +0600

    enqueue and dequeue method are committed

--- Lab-02/task2/StudentQueue.java
+++ Lab-02/task2/StudentQueue.java
@@ -1,11 +1,31 @@
-public StudentQueue{
+public StudentQueue {
     String[] names;
     double[] marks;
-    int count=0;
+    int count = 0;
 
-    StudentQueue(String[] names.double[] marks){
-        this.names=names;
-        this.marks=marks;
+    StudentQueue(String[]names. double[] marks){
+        this.names = names;
+        this.marks = marks;
+    }
+    public void enqueue (String names,double marks){
+        if (count == 500) {
+            System.out.println("Stack is full");
+            return;
+        }
+        names[count] = names;
+        marks[count] = marks;
+        count++;
+    }
+    public void dequeue () {
+        if (count == 0) {
+            System.out.println("Queue is empty");
+            return;
+        }
+        System.out.println("Name:" + names[0] + " Marks: " + marks[0]);
+        for (int i = 0; i < count - 1; i++) {
+            names[i] = names[i + 1];
+        }
+        count--;
     }
 
 }
