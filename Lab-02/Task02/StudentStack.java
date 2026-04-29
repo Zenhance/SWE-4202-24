745b66afbdb8ae89a5c617c10785768645042819
xenonono36-ctrl <xenonono36@gmail.com>
2026-04-29 23:15:45 +0600

    Task2

---
+++ Lab-02/Task02/StudentStack.java
@@ -0,0 +1,26 @@
+/*a. push and pop never shift any elements. What does this mean for speed when the
+stack has 50 students versus 5 students?
+
+--Ans-- As push and pop never shifts elements,
+the speed of these operations will be constant (O(1))
+regardless of the number of students in the stack. 
+Whether there are 50 students or 5 students,
+the time taken to push or pop an element will be the same as the top element is only accessed.
+
+
+b. Your StudentStack and StudentQueue both have a private field called count (or
+similar). If someone writes code that uses both classes at the same time, can one
+object’s count interfere with the other’s? Why not?
+
+
+--Ans-- No, one object's count cannot interfere with the other's because
+each instance of the StudentStack and StudentQueue 
+classes will have its own separate count variable.
+When you create an object of either class, 
+it will have its own count variable that is independent
+
+*/
+
+public class StudentStack {
+
+}
