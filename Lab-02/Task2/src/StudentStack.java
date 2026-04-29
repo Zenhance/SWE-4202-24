29b01d4795fcb1f99e47e00b582d0537e56acdf9
Muhtasim Moin <muhtasi13@gmail.com>
2026-04-29 19:05:01 +0600

    init commit

--- Lab-02/Task2/src/StudentStack.java
+++ Lab-02/Task2/src/StudentStack.java
@@ -2,5 +2,10 @@ public class StudentStack {
     private String[] name = new String[50];
     private int[] mark = new int[50];
     private int count=0;
+    public void push(String name,int mark){
+        if (count+1>50){
+            System.out.println("Stack is full");
+        }
+    }
 
 }
