0115b610c41483977b7aa07c0fe1d4926125a870
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-04-29 22:00:36 +0600

    answering part b 4

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -3,7 +3,7 @@ public class BankAccount {
     private String name;
     private double balance;
 
-    public BankAccount(string name) {
+    public BankAccount(String name) {
 
         this.name = name;
         this.balance = 0;
@@ -12,22 +12,25 @@ public class BankAccount {
     }
 
 public void deposit(double amount){
-if(amount<=0) {
+    if(amount<=0) {
     System.out.println("Error!Invalid amount to deposit");}
-else {
-balance+=amount;
-}
+      else {
+     balance+=amount;
+} }
 
 public void withdraw(double amount){
         if(amount>balance) {
-            System.out.println("Error!Invalid amount to deposit");}
+            System.out.println("Error!Insufficient Funds");}
         else {
             balance-=amount;
+        } }
+
+
+public void printStatement(){
+
+            System.out.println("Owner's Name= "+ name);
+            System.out.println("Current balance= "+ balance);
         }
-
-
-
-
 
 
 }
