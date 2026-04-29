a014a95233f6beb4194c954a3e489ac39fc06edb
xenonono36-ctrl <xenonono36@gmail.com>
2026-04-29 23:45:36 +0600

    m1

--- Lab-02/Task02/Main.java
+++ Lab-02/Task02/Main.java
@@ -1,3 +1,21 @@
 public class Main {
 
+    public static void main(String[] args) {
+
+        // Stack Func
+        StudentStack stack = new StudentStack();
+
+        // Implementing push() , pop() , peek(), display()
+        for (int i = 0; i <= 5; i++) {
+            stack.push("Student" + i, i + 50);
+        }
+
+        stack.peek();
+
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+        stack.display();
+    }
 }
