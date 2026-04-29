2cc683c280ca2b833bf82a0d49eb377f3c6b5983
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-04-29 23:15:19 +0600

    Update commitB(actual).java

--- Lab-02/task1/src/commitB(actual).java
+++ Lab-02/task1/src/commitB(actual).java
@@ -1,1 +1,55 @@
+class BankAccount 
+  {
+private String customername;
+    private String balance;
 
+    public BankAccount (String customername)
+    {
+this.ownername= ownername;
+      this.balance=0;
+    }
+
+    public void deposit (doubole amount)
+    { if (amount<=0)
+    {
+      System.out.println("Invalid deposited amount");
+      return;}
+
+     balance+=amount;
+      
+    }
+
+    public void withdraw (double amount)
+    {
+     if (amount>balance)
+     {
+
+       System.out.println("Fund is not sufficient");
+        return;}
+      balance-=amount;
+    }
+    public void printStatement()
+    {
+     System.out.println("Customer:" + customername + "Balance: " + balance);
+      
+    }
+    public static void main (String[] args)
+    {  
+      BankAccount alice = new ("Alice");
+      BankAccount bob = new ("Bob");
+      BankAccount abdul = new ("Abdul");
+      alice.deposit(500);
+      alice.withdraw(200);
+      bob.deposit(800);
+      bob.withdraw(300);
+      abdul.deposit(1000);
+      abdul.withdraw(200);
+
+      alice.printStatement();
+      bob.printStatement();
+      abdul.printStatement();
+      
+
+      
+    }
+  }
