81f59298ad921a1924364ef8e9750a5970630f42
mahibrahaman175-stack <mahibrahaman175@gmail.com>
2026-04-29 08:23:27 +0600

    bankaccountdone

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -1,2 +1,37 @@
 public class BankAccount {
+    private String ownerName;
+    private Double Balance;
+
+    public BankAccount(String ownerName){
+        this.ownerName=ownerName;
+        this.Balance=0.0;
+    }
+
+    public void DepositAmount(double amount){
+           if(amount<=0){
+               System.out.println("Invalid Deposit Amount");
+               return;
+           }
+           Balance+=amount;
+
+    }
+
+
+    public void WithdrawAmount(double amount){
+        if(amount<=0){
+            System.out.println("Invalid withdraw amount");
+        }else if(amount>Balance){
+            System.out.println("Insufficient fund");
+        }else{
+            Balance-=amount;
+        }
+    }
+
+
+    public void PrintStatement(){
+        System.out.println("Owner: "+ownerName );
+        System.out.println("Balance: "+Balance);
+    }
+
+
 }
