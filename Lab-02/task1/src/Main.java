1379716d48942b1816a76fa7c146e6909b04f695
hasnat-farhan <hasnatfarhan@iut-dhaka.edu>
2026-04-29 18:52:28 +0600

    Part C

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -10,13 +10,11 @@ public class Main {
         alice.withdraw(300);
         alice.deposit(0);
         alice.printStatement();
-
         baba.deposit(5000);
         baba.withdraw(300);
         baba.withdraw(3000);
         baba.deposit(10);
         baba.printStatement();
-
         cee.deposit(50);
         cee.withdraw(30);
         cee.withdraw(0);
@@ -24,3 +22,19 @@ public class Main {
         cee.printStatement();
     }
 }
+/*
+    a.the word for  BankAccount is class and alice and bob are two "object" created of that class.
+
+    b.for alice.deposit(500), only alice's balance changes. the program knows before each object has separate attributes and methods.
+    When I call an object(alice), all methods used only change alice's attributes(balance).
+    c.
+   +--------------------------+     +--------------------------+
+   |   BankAccount (alice)    |     |   BankAccount (bob)      |
+   +--------------------------+     +--------------------------+
+   | name: "Alice"            |     | name: "Bob"              |
+   | balance: 0.0             |     | balance: 0.0             |
+   +--------------------------+     +--------------------------+
+
+    d.If I write alice=bob, then the objects will become same or refer to one object. and then calling alice.deposit(100), bob's balance will also increase,
+     since alice and bob now refer to the same object.
+*/
