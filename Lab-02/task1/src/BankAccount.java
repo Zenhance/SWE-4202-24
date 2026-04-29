7c8635df4f1c8e433d09509b6b94c23bc8adb5af
lamisa <lamisaibnat5d@gmail.com>
2026-04-29 23:47:21 +0600

    Second commit

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -12,3 +12,49 @@ c) If two arrays are created , it'll become messy and it'll be difficult to
 handle the data. The arrays will become lengthy.
 d) We have to add the transaction limit per day in class.
  */
+
+/* part-b*/
+class Account {
+    String name;
+    double balance = 0;
+
+    public class Account(String name) {
+        this.name=name;
+    }
+
+    public class deposit(double amount) {
+        if(balance<=0)
+                System.out.print("Error!");
+        else
+        balance+=amount;
+    }
+
+    public class withdraw(double amount) {
+        if(balance<0)
+                System.out.print("Insufficient funds!");
+        else
+        balance-=amount;
+    }
+
+    public class printStatement(String name, double balance) {
+        System.out.println("Owner's name"+name);
+        System.out.println("Current balance:"+balance);
+    }
+}
+public class BankAccount{
+    public static void main(String args[]){
+        Account acc1= new Account("Kallol");
+        Account acc2= new Account("Sumi");
+        Account acc3= new Account("Sabit");
+        acc1.deposit(1000);
+        acc1.withdraw(500);
+        acc2.deposit(2000);
+        acc2.withdraw(1000);
+        acc3.deposit(500);
+        acc3.withdraw(200);
+        acc1.printStatement();
+        acc2.printStatement();
+        acc3.printStatement();
+
+    }
+}
