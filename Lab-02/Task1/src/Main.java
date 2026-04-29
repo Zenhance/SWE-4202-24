0dd76644f6b38cd1e94f419205c824d63112562f
ahnaf-code <ahnafcode@gmail.com>
2026-04-29 22:48:36 +0600

    Part B - Write The class --> 4

--- Lab-02/Task1/src/Main.java
+++ Lab-02/Task1/src/Main.java
@@ -1,8 +1,23 @@
-
-
 public class Main {
-    public static void main(String[] args) {
-        String Na
-BankAccount account1= new BankAccount("Azmain");
+    public static void main (String[] args) {
+        BankAccount bankAccount1 = new BankAccount("John");
+        BankAccount bankAccount2 = new BankAccount("Don");
+        BankAccount bankAccount3 = new BankAccount("Rose");
 
+        bankAccount1.deposit(100000);
+        bankAccount1.withdraw(40000);
+        bankAccount1.deposit(5000);
+        bankAccount1.printStatement();
+
+        bankAccount2.deposit(1000);
+        bankAccount2.withdraw(400);
+        bankAccount2.deposit(5000);
+        bankAccount2.printStatement();
+
+        bankAccount3.deposit(5000000);
+        bankAccount3.withdraw(400500);
+        bankAccount3.deposit(5000);
+        bankAccount3.printStatement();
+
+    }
 }
