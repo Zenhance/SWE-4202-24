7ec900cf1231a1e552b734b1b7afbcbeda0e4260
tasnitaraida <tasnitar@gmail.com>
2026-04-29 23:42:12 +0600

    create objects

--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        BankAccount alice = new BankAccount("Alice");
+        BankAccount bob = new BankAccount("Bob");
+        BankAccount raida = new BAnkAccount("RAIDA");
     }
 }
