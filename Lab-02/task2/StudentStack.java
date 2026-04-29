a25517b8facef432ccb9385344b5a6e20803a771
tasnovatahia <tasnovatahia2014@gmail.com>
2026-04-30 03:15:15 +0600

    StudentStack commit

---
+++ Lab-02/task2/StudentStack.java
@@ -0,0 +1,10 @@
+public class StudentStack{
+    String[] names;
+    double[] marks;
+    int count=0;
+
+    StudentStack(String[] names,double[] marks){
+        this.names=names;
+        this.marks=marks;
+    }
+}
