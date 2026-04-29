038382b9257f3882fdf4ce8c250c69f849f3d6ab
waseef_44 <waseef05@gmail.com>
2026-04-29 19:19:17 +0600

    Answer of Part A, B

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -18,6 +18,51 @@ c. If all account balances are stored in a big array, then we need to carefully 
 d. If the bank later wants to add a transaction limit per day, then I would add that inside the BankAccount class, not inside 'main'. Because the code would be more cleaner and easier to manage if I write code in this way.
 */
 public class BankAccount{
+    private String owner_name;
+    private double balance;
 
+    public BankAccount(String owner_name){
+        this.owner_name = owner_name;
+        this.balance = 0;
+    }
+    public void deposit(double amount){
+        if (balance <= 0) { // checking if the deposit amount is entered correctly
+            System.out.println("Error. Please enter a positive number.");
+        }
+        else {
+            balance += amount;
+        }
+    }
+    public void withdraw(double amount){
+        if (balance - amount < 0) // checking if the amount is sufficient for withdrawal
+        {
+            System.out.println("Insufficient funds");
+        }
+        else {
+            balance -= amount;
+        }
+    }
+    public void printStatement(){
+        System.out.println("Owner: " + owner_name);
+        System.out.println("Balance: " + balance);
+    }
+    static void main(String[] args){
+        BankAccount alice = new BankAccount("Alice");
+        BankAccount bob = new BankAccount("Bob");
+        BankAccount john = new BankAccount("John");
+
+        alice.deposit(1000);
+        alice.withdraw(300);
+
+        bob.deposit(1500);
+        bob.withdraw(500);
+
+        john.deposit(1250);
+        john.withdraw(250);
+
+        alice.printStatement();
+        bob.printStatement();
+        john.printStatement();
+    }
 }
 
