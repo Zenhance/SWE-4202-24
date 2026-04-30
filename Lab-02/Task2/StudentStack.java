1bf71f59f91bb1fd7d1c348c0e1ce8a83b7147ab
Mahdia <hossainmahdia22@gmail.com>
2026-04-30 21:15:06 +0600

    innit commit

---
+++ Lab-02/Task2/StudentStack.java
@@ -0,0 +1,31 @@
+/*
+PartA
+a) Speed stays constant regardless of size- push and pop take same time (0(1)).
+
+b) No, one object's count can't interefere with another. Each object has it's own separate copy of the fields in memory.
+*/
+
+public class StudentStack{
+
+}
+    private String[] names;
+    private int[] marks;
+    private int count;
+    private int cap = 10;
+
+    public StudentStack() {
+        names = new String[cap];
+        marks = new int[cap];
+        count = 0;
+    }
+
+    public void push(String name, int mark) {
+        if (count == cap) {
+            System.out.println("Stack is full");
+            return;
+        }
+        names[count] = name;
+        marks[count] = mark;
+        count++;
+    }
+}
