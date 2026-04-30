fd4d18d33c6e0fb6df6d6263759d10cd4ba00250
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-05-01 04:53:26 +0600

    main file

---
+++ Lab-02/task2/src/Main.java
@@ -0,0 +1,40 @@
+public class Main{
+
+public static void main(String[] args) {
+
+    StudentStack stack = new StudentStack();
+
+
+    for (int i = 1; i <= 5; i++) {
+
+        stack.push("Student" + i, i + 50);
+    }
+
+    stack.peek();
+
+    stack.pop();
+    stack.pop();
+    stack.pop();
+
+    stack.display();
+
+
+    StudentQueue queue = new StudentQueue();
+
+
+    for (int i = 1; i <= 5; i++) {
+
+        queue.enqueue("Student" + i, i + 50);
+    }
+
+    queue.front();
+
+    queue.dequeue();
+    queue.dequeue();
+    queue.dequeue();
+
+    queue.display();
+
+}
+
+}
