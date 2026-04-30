2322f0e1d3d96fe9f7a9d1826585155a1a5c7bf8
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-04-30 23:51:12 +0600

    init commit

--- Lab-02/task01/src/Main.java
+++ Lab-02/task01/src/Main.java
@@ -1,3 +1,22 @@
+/*
+a. What information does every account need to store? List each piece and the type
+you would use.
+ans:string name, double balance
+
+b. What are the three actions a customer can perform on their account? For each one,
+write down what it needs to know (inputs) and what it produces (output or change).
+ans:deposit(amount) == updates balance, withdraw(amount) == updates balance, printstatement() == prints user information
+
+c. A colleague suggests storing all account balances in one big array in main, with a
+separate array for owner names, just like Task 1 of Lab 1. What specific problem
+would appear the moment you need to handle 200 customers?
+ans:it would be really hard to manage all the info and poor scalability and readability.
+
+d. Suppose the bank later wants to add a transaction limit per day. Where exactly
+would you add that — in main, or somewhere else? Why?
+ans:i would put that in BankAccount class
+
+*/
 public class Main{
 public static void main(String[] args) {
     BankAccount ayesha = new BankAccount("ayesha");
