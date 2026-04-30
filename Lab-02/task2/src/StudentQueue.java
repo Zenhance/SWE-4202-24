6cd53d83dc0eadf73f607a2109687a3f5cd7511e
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-05-01 04:12:30 +0600

    student queue completion

--- Lab-02/task2/src/StudentQueue.java
+++ Lab-02/task2/src/StudentQueue.java
@@ -39,6 +39,25 @@ public void dequeue() {
     }
 
 
+public void front(){
+
+
+        if(count == 0){
+
+            System.out.println("Queue is empty!");
+            return;
+        }
+
+        System.out.println("Front: " + names[0] +  " (" + marks[0] + ")");
+    }
+
+public void display(){
+        System.out.println("The queue:");
+        for(int i=0;i<count;i++){
+
+            System.out.println(i + " " + names[i] + " " + marks[i]);
+        }
+    }
 
 
 
