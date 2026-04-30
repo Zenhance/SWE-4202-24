9b50d9ed6441c8a9da0d89ddaaecc1fa6d2cb288
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-04-30 23:33:52 +0600

    init commit

--- Lab-02/task01/src/BankAccount.java
+++ Lab-02/task01/src/BankAccount.java
@@ -1,2 +1,8 @@
 public class BankAccount {
-}
+    private String name;
+    private double balance;
+
+    public BankAccount(String name) {
+        this.balance = 0;
+        this.name= name;
+    }
