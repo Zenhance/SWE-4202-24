321a587c5d3d43ab8fbf13a11a8ab7653e0a74eb
md-musaddik <musaddikibna1@gmail.com>
2026-05-01 22:55:11 +0600

    pop method written

--- Lab-02/task2/src/StudentStack.java
+++ Lab-02/task2/src/StudentStack.java
@@ -6,9 +6,18 @@ public class StudentStack {
         if(cout==100){
             Suystem.out.println("Stack is full");
             return;
+            }
             this.name[count]=name;
             this.marks[count]=marks;
             cout++;
+
+    }
+    public void pop(){
+        if (count == 0) {
+            System.out.println("Stack is empty");
+            return;
         }
+        count--;
+        System.out.println("Name:"+this.name[count] + " Marks: " + this.marks[count]);
     }
 }
