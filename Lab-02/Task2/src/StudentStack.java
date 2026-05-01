0733fcf3376e8aff5fbd426c0f11409d0eabf169
Maomao-starry <aminarahmanshalu@gmail.com>
2026-05-01 22:22:48 +0600

    Task2 Part B

--- Lab-02/Task2/src/StudentStack.java
+++ Lab-02/Task2/src/StudentStack.java
@@ -1,3 +1,10 @@
 public class StudentStack {
+/*
+a)The speed of push and pop does not depend on how many students are there
+the speed is same on both 5 students or 50 students.
+b) No , one count can not interfere another count ,
+because i have given an intance variable and every object gets its own separate copy
+ */
+
 
 }
