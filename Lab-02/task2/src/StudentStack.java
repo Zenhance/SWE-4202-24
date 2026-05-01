9bed66eaaf88dc66baff900d5271033d3533e34f
md-musaddik <musaddikibna1@gmail.com>
2026-05-01 22:49:15 +0600

    push method written

--- Lab-02/task2/src/StudentStack.java
+++ Lab-02/task2/src/StudentStack.java
@@ -2,5 +2,13 @@ public class StudentStack {
     private String[] name = new String[100];
     private int[] marks = new int[100];
     private int count = 0;
-
+    public void push(String name, int marks){
+        if(cout==100){
+            Suystem.out.println("Stack is full");
+            return;
+            this.name[count]=name;
+            this.marks[count]=marks;
+            cout++;
+        }
+    }
 }
