569895ced7523a0cc9794d0f2674a0e5a041903c
tahsin1r <naylakagi@gmail.com>
2026-05-01 20:05:09 +0600

    bank account class created

--- Lab-02/task01/src/BankAccount.java
+++ Lab-02/task01/src/BankAccount.java
@@ -1,2 +1,33 @@
 public class BankAccount {
+    private String name;
+    private int balance;
+
+    public BankAccount(String name){
+        this.balance=0;
+        this.name=name;
+
+    }
+
+    public void withdraw(int amount){
+        if(this.balance-amount<=0){
+            System.out.println("Insufficient");
+        }
+        else{
+            balance=balance-amount;
+        }
+
+    }
+    public void deposit(int amount){
+        if(amount<=0){
+            System.out.println("Error");
+        }
+        else{
+            balance=balance+amount;
+        }
+    }
+    public void printing(){
+        System.out.println(this.name+" "+this.balance);
+    }
 }
+
+   
--- Lab-02/task01/src/Main.java
+++ Lab-02/task01/src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Main Runs");
+        BankAccount tahsin = new BankAccount"Tahsin")
+
     }
 }
