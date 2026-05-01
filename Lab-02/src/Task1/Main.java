b4edafdb52e1b7576acae754d39510a749822e4e
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-05-01 23:12:32 +0600

    Created three objects of the class BankAccount

---
+++ Lab-02/src/Task1/Main.java
@@ -0,0 +1,9 @@
+package Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount alice = new BankAccount("Alice");
+        BankAccount bob = new BankAccount("Bob");
+        BankAccount mahdi = new BankAccount("Mahdi");
+    }
+}
