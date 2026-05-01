d34f13a136a308ca5ddc199cf306d0a1e3bf4a11
Maomao-starry <aminarahmanshalu@gmail.com>
2026-05-01 23:06:45 +0600

    Task2 Part A+C

--- Lab-02/Task2/src/StudentStack.java
+++ Lab-02/Task2/src/StudentStack.java
@@ -6,5 +6,21 @@ b) No , one count can not interfere another count ,
 because i have given an intance variable and every object gets its own separate copy
  */
 
+private static final int MAX=50;
+private String[] n;
+private int []m;
+private int cnt;
+
+public StudentStack(){
+    n=new String[MAX];
+    cnt=0;
+    m=new int[MAX];
+}
+public void push(String name, int mar){
+    n[cnt]=name;
+    m[cnt]=mar;
+    cnt++;
+}
+
 
 }
