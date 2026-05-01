89eeaa4b6d55a6361f5f8e5ff67c4c9d37cf246c
Maomao-starry <aminarahmanshalu@gmail.com>
2026-05-01 23:45:01 +0600

    Task2 Part B+C

--- Lab-02/Task2/src/StudentQueue.java
+++ Lab-02/Task2/src/StudentQueue.java
@@ -1,2 +1,8 @@
 public class StudentQueue {
+    /*
+a)each dequeue shifts all element , if we have 50 elements , in first case , we shift 49 elements , then 48 . It goes on like this , the total shifting will be more than 1000
+b)because the array is private  , the outside code can not access it, each class control how the array is used
+ */
+    public static final int p=50;
+    
 }
