eb20de133a22ddd0a206136a8befbfed7f5ed746
04-Labiba <sabihalabiba639@gmail.com>
2026-05-01 13:15:17 +0600

    created push method

--- Lab-02/StudentQueue.java
+++ Lab-02/StudentQueue.java
@@ -1,4 +1,12 @@
 public class StudentQueue{
     private Student[] students=new Student[50];
     private int count=0;
+    public void enqueue(String name,int mark){
+        if(count==students.length){
+            System.out.println("Queue is full");
+            return;
+        }
+        students[count]=new Student(name,mark);
+        count++;
+    }
 }
