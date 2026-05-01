19d9ec16b4f4a4bddfbfe43c54dc9322cbfa2df4
Sadman Raiyan <raiyansadman121@gmail.com>
2026-05-01 21:31:14 +0600

    Adding push method

--- Lab-02/task2/src/StudentStack.java
+++ Lab-02/task2/src/StudentStack.java
@@ -3,4 +3,15 @@ public class StudentStack {
         private int[] marks=new int[50];
         private int cnt=0;
 
+        public void push(String name,int marks){
+
+            if(cnt>50) {
+                System.out.println("Stack is full");
+                return;
+            }
+            this.name[cnt]=name;
+            this.marks[cnt]=marks;
+            cnt++;
+        }
+
 }
