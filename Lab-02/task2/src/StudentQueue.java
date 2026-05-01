a61497acf9bd178096f5e5c572f068360c26f85b
Fahad <ifahad2k@gmail.com>
2026-05-01 21:58:27 +0600

    StudentQueue Question Answer a+b

--- Lab-02/task2/src/StudentQueue.java
+++ Lab-02/task2/src/StudentQueue.java
@@ -1,3 +1,24 @@
+/*
+(a)
+Ans:
+Each dequeue shifts all remaining elements one step to the left.
+So first time: 49 moves
+Second time: 48 moves
+Then 47, 46, and so on...
+
+So the total number of moves is:
+49 + 48 + 47 + ... + 5
+
+This follows the formula n(n - 1) / 2, so a lot of movements happen compared to stack.
+
+(b)
+Ans:
+Making the array private means outside code cannot directly change it.
+All changes must go through methods like push, pop, enqueue, or dequeue.
+This allows each class (Stack, Queue, List) to control how the data is used and keeps everything safe.
+*/
+
+
 public class StudentQueue {
     private String[] names;
     private double[] marks;
