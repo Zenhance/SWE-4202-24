e2532d693b235b2f2124e9f9247c80082f32fedb
md-musaddik <musaddikibna1@gmail.com>
2026-05-01 23:35:23 +0600

    enqueue method declared

--- Lab-02/task2/src/StudentQueue.java
+++ Lab-02/task2/src/StudentQueue.java
@@ -2,4 +2,12 @@ public class StudentQueue {
     private String[] name = new String[100];
     private int[] marks = new int[100];
     private int count = 0;
+    public void enqueue(String name,int marks){
+        if(count==100){
+            System.out.println("Stack is full");
+        }
+        this.name[count]=name;
+        this.marks[count]=marks;
+        count++;
+    }
 }
