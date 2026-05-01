958505c77b5bfc0efcf6da83ce4373a711ea5a55
sabbir240042147 <eftesabbir2725@gmail.com>
2026-05-01 22:13:29 +0600

    Studentqueue done studentstack done

--- Lab-02/Task2/src/Main.java
+++ Lab-02/Task2/src/Main.java
@@ -2,7 +2,27 @@ public class Main {
     public static void main(String[] args){
         StudentStack s1=new StudentStack();
         s1.push("Sabbir",80);
+        s1.push("Sameer",89);
+        s1.push("Tanvir",70);
+        s1.push("Lamisa",99);
+        s1.push("janina",10);
+        s1.peek();
+        s1.pop();
+        s1.pop();
+        s1.pop();
+        s1.display();
 
+        StudentQueue s2=new StudentQueue();
+        s2.enqueue("Sabbir",80);
+        s2.enqueue("Sameer",89);
+        s2.enqueue("Tanvir",70);
+        s2.enqueue("Lamisa",99);
+        s2.enqueue("janina",10);
+        s2.front();
+        s2.dequeue();
+        s2.dequeue();
+        s2.dequeue();
+        s2.display();
 
 
     }
