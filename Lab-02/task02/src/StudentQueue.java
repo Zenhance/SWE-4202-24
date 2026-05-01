faaf81369666319ee50b879e3eead1858fac2d09
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-01 12:57:36 +0600

    init commit

---
+++ Lab-02/task02/src/StudentQueue.java
@@ -0,0 +1,12 @@
+public class StudentQueue {
+    String[] names;
+    int[] marks;
+    int count;
+    //intialize constructor
+
+    StudentQueue(String[] name, int[] mark) {
+        this.names = name;
+        this.marks = mark;
+        this.count = 0;
+    }
+}
