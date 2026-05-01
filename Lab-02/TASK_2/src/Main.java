7dd2b820f3c799fd2803b403cf63ab4197e9f7c0
Zunied-57 <zunied0411@gmail.com>
2026-05-01 21:12:37 +0600

    class stack

--- Lab-02/TASK_2/src/Main.java
+++ Lab-02/TASK_2/src/Main.java
@@ -1,3 +1,11 @@
+class StudentStack
+{
+    private String[] name;
+    private int[] mark;
+    private int count;
+    private int total=5;
+
+}
 public class Main
 {
     public static void main(String[] args)
