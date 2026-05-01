4caf0f7f5530521049ebd288aa2ecee87034bbda
Zunied-57 <zunied0411@gmail.com>
2026-05-01 21:17:59 +0600

    class again

--- Lab-02/TASK_2/src/Main.java
+++ Lab-02/TASK_2/src/Main.java
@@ -5,6 +5,12 @@ class StudentStack
     private int count;
     private int total=5;
 
+    public StudentStack()
+    {
+        this.name=new String[total];
+        this.mark=new int[total];
+        this.count=0;
+    }
 }
 class StudentQueue
 {
@@ -13,6 +19,12 @@ class StudentQueue
     private int count;
     private int total=5;
 
+    public StudentQueue()
+    {
+        this.name=new String[total];
+        this.mark=new int[total];
+        this.count=0;
+    }
 }
 public class Main
 {
