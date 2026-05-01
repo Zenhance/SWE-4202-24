44dc9ea3a25f6d69672c1981e7b74dafc7044b0c
58-jahin <jahinhossain@iut-dhaka.edu>
2026-05-01 20:56:45 +0600

    2

--- Lab-02/task2/src/StudentStack.java
+++ Lab-02/task2/src/StudentStack.java
@@ -9,3 +9,66 @@ of another object.
 
 */
 
+
+class StudentStack {
+
+    private String[] names;
+    private int[] marks;
+    private int count;
+
+    public StudentStack(int size) {
+        names = new String[size];
+        marks = new int[size];
+        count = 0;
+    }
+
+    public void push(String name, int mark) {
+        if (count == names.length) {
+            System.out.println("List has become full");
+            return;
+        }
+
+        names[count] = name;
+        marks[count] = mark;
+        count++;
+
+        System.out.println(name + " included already");
+    }
+
+    public void pop() {
+        if (count == 0) {
+            System.out.println("List is now empty");
+            return;
+        }
+
+        count--;
+
+        System.out.println("Removed: " + names[count] +
+                " (" + marks[count] + ")");
+    }
+
+    public void peek() {
+        if (count == 0) {
+            System.out.println("List is now empty");
+            return;
+        }
+
+        System.out.println("Top Student is: " + names[count - 1] +
+                " (" + marks[count - 1] + ")");
+    }
+
+    public void display() {
+        if (count == 0) {
+            System.out.println("List is now empty");
+            return;
+        }
+
+        System.out.println("Students in List are:");
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(names[i] + " - " + marks[i]);
+        }
+    }
+
+}
+
