a6283c7a441a4ded4a6eb49b7aecfacfa9fef8c8
Rakib <rakibulhasan4266@gmail.com>
2026-05-01 21:43:42 +0600

    Making StudentStack object

--- Lab-02/task2/src/Main.java
+++ Lab-02/task2/src/Main.java
@@ -5,5 +5,8 @@ public class Main
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+
+        //Stack
+        StudentStack Stack = new StudentStack();
     }
 }
