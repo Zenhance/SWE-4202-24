cc574a9b7a8a03f4f9edb45b09e09954e4c48dc8
lamisa <lamisaibnat5d@gmail.com>
2026-05-02 00:02:06 +0600

    Second commit

--- task2/src/Students.java
+++ task2/src/Students.java
@@ -50,7 +50,66 @@
          stack.pop();
          stack.display();
 
-     
+
      }
 
 }
+/* part--b*/
+public class StudentQueue {
+    Students[] studentlist;
+    int count = 0;
+
+    public StudentQueue() {
+        this.studentlist=new Students[5];}
+
+    public void enqueue(String name,double mark){
+        if(count<5){
+            studentlist[count]=new Students(name,mark);
+            count++;
+        }
+        else{
+            System.out.println("Error!");
+        }}
+    public void dequeue(){
+        if(count>0){
+            count--;
+            System.out.println("Removed "+ studentlist[0]);
+            for(int i=0;i<count-1;i++){
+                studentlist[i]=studentlist[i+1];
+            }studentlist[count-1]=null;count--;}
+            else{
+                System.out.println("Queue is empty!");
+            }
+        }
+
+    public void front(){
+        if(count>0) {
+            System.out.println("Frontstudent at" + studentlist[0]);
+        }
+    }
+    public void display() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(studentlist[i]);
+        }
+
+    }
+    public static void main(String args[]) {
+        StudentQueue queue = new StudentQueue();
+        queue.enqueue("Nishi",98);
+        queue.enqueue("Tanbir",87);
+        queue.enqueue("Amina",89);
+        queue.enqueue("Mahdia",79);
+        queue.enqueue("Lamisa",59);
+        queue.front();
+
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+
+        queue.display();
+
+
+    }
+
+}
+
