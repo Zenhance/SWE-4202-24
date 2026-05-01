1542b3bcb9b604e7a8110dc0523199ffec504f23
Muhtasim Moin <muhtasi13@gmail.com>
2026-05-01 10:47:55 +0600

    init commit

--- Lab-02/Task2/src/StudentQueue.java
+++ Lab-02/Task2/src/StudentQueue.java
@@ -1,5 +1,10 @@
 public class StudentQueue {
     private String[] name = new String[50];
     private int[] mark = new int[50];
-    private int count=0;
+    private int count = 0;
 
+    public void enqueue(){
+
+    }
+
+}
