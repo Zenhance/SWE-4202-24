e3de28373d8a936dee7459d792390dcc8af06dc8
Muhtasim Moin <muhtasi13@gmail.com>
2026-05-01 11:39:59 +0600

    init commit

--- Lab-02/Task2/src/Main.java
+++ Lab-02/Task2/src/Main.java
@@ -4,25 +4,29 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("For Student Stack");
         StudentStack s1=new StudentStack();
-        for (int i=1;i<9;i++){
+        for (int i=1;i<5;i++){
             s1.push("Student"+i,50+i);
         }
+        System.out.println("Peek");
         s1.peek();
+        System.out.println("Pop");
         s1.pop();
         s1.pop();
+        s1.pop();
+        System.out.println("Display");
         s1.display();
         System.out.println("For Student Queue");
      StudentQueue q1=new StudentQueue();
-     for (int i=1;i<9;i++){
+     for (int i=1;i<5;i++){
          q1.enqueue("Student"+i,50+i);
      }
-        System.out.println("Displaying the queue");
-     q1.display();
-        System.out.println("The first queue");
+        System.out.println("Front");
      q1.front();
-        System.out.println("Removing");
+        System.out.println("Dequeue");
      q1.dequeue();
-        System.out.println("After Removing");
+     q1.dequeue();
+     q1.dequeue();
+        System.out.println("Display");
         q1.display();
 
 
