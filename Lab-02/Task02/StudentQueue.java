8f81db8e4df7a5eaf4fc5da47997ee1b5020aa4a
xenonono36-ctrl <xenonono36@gmail.com>
2026-05-01 14:19:18 +0600

    Q3

--- Lab-02/Task02/StudentQueue.java
+++ Lab-02/Task02/StudentQueue.java
@@ -21,19 +21,32 @@ private makes this possible.
 the public methods can be used by outside code , allowing different classes to use the same 
 array structure behaving differently.
 
-*/
+ */
 public class StudentQueue {
 
     private String[] name = new String[50];
     private int[] marks = new int[50];
     private int cnt = 0;
 
-    public void enqueue(String name, int marks){
+    public void enqueue(String name, int marks) {
         if (cnt + 1 > 50) {
             System.out.println("Full Queue");
         }
         this.name[cnt] = name;
         this.marks[cnt] = marks;
         cnt++;
+    }
+
+    public void dequeue() {
+        if (cnt - 1 < 0) {
+            System.out.println("Empty Queue");
+        }
+        System.out.println(this.name[0] + " " + this.marks[0]);
+
+        for (int i = 0; i < cnt-1; i++) {
+            this.name[i] = this.name[i+1];
+            this.marks[i] = this.marks[i+1];
+        }
+        cnt--;
     }
 }
