a7b2aabc0016b010ba696a3c49a3d07641b9e72a
tahsin1r <naylakagi@gmail.com>
2026-05-01 20:10:12 +0600

    Main file first check

--- Lab-02/task01/src/Main.java
+++ Lab-02/task01/src/Main.java
@@ -2,7 +2,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        BankAccount tahsin = new BankAccount"Tahsin")
+        BankAccount tahsin = new BankAccount("Tahsin");
+        BankAccount moin = new BankAccount("Moin");
+        BankAccount arnob= new BankAccount("Arnob");
+
+        tahsin.withdraw(690);
+        moin.withdraw(650);
+        arnob.withdraw(666);
+
+        tahsin.deposit(500);
+        moin.deposit(250);
+        arnob.deposit(333);
+
+        arnob.printing();
+
 
     }
 }
