f223225e5de123a9a1c4f1f415c3c223227d1a14
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-01 14:48:42 +0600

    init commit

--- Lab-02/task02/src/StudentQueue.java
+++ Lab-02/task02/src/StudentQueue.java
@@ -1,3 +1,27 @@
+/*
+a. dequeue always shifts the entire remaining array one step to the left. If the queue
+holds 50 students and you dequeue 45 times, roughly how many individual element
+moves does that require in total?
+ans: Total individual moves = n*(n+1)/2 - (1 + 2 + 3 + 4)
+                                = 49*50/2 - 10
+                                = 1215
+        In every dequeue we need to move count-1 elements. so the number of moves per
+        dequeue 49, 48, 47, 46,....
+        As we dequeued 45 elements, we do not need to count for last
+        4 element's dequeue's move count. Hence, 49*50/2 - 10.
+
+b. You have now written three classes — StudentList, StudentStack, and StudentQueue
+
+— all using the same private array and count. The only real difference is which meth-
+ods are available to outside code. In one sentence, explain why hiding the array as
+private makes this possible.
+ans:Because the array is private, outside code cannot directly modify it.
+   This allows each class to enforce different rules (stack or queue)
+   while using the same underlying structure.
+
+*/
+
+
 public class StudentQueue {
     String[] names;
     int[] marks;
