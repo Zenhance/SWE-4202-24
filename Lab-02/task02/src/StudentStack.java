42c9a9e74d50b9303ea63098c97e23b5c1e37a1b
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-01 12:44:53 +0600

    init commit

--- Lab-02/task02/src/StudentStack.java
+++ Lab-02/task02/src/StudentStack.java
@@ -3,19 +3,21 @@ public class StudentStack {
     int[] marks;
     int count;
 
-    StudentStack(String[] name,int[] mark){
-        this.names=name;
-        this.marks=mark;
-        this.count=0;
+    StudentStack(String[] name, int[] mark) {
+        this.names = name;
+        this.marks = mark;
+        this.count = 0;
     }
 
+    public void push(String name, int mark) {
+        if (count == 100) {
+            System.out.println("ERROR:STACK IS FULL");
+            return;
+        }
+        names[count] = name;
+        marks[count] = mark;
+        count++;
 
-
-
-
-
-
-
+    }
 }
-
 
