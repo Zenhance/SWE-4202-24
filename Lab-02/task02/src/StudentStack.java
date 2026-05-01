8e167e05e9c54000cef74569ff90c89b45aa774a
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-01 12:32:15 +0600

    init commit

---
+++ Lab-02/task02/src/StudentStack.java
@@ -0,0 +1,21 @@
+public class StudentStack {
+    String[] names;
+    int[] marks;
+    int count;
+
+    StudentStack(String[] name,int[] mark){
+        this.names=name;
+        this.marks=mark;
+        this.count=0;
+    }
+
+
+
+
+
+
+
+
+}
+
+
