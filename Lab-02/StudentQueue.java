4156b41658288e0ba62c4321fc55eb18c66b9c43
04-Labiba <sabihalabiba639@gmail.com>
2026-05-01 13:18:30 +0600

    created push method

--- Lab-02/StudentQueue.java
+++ Lab-02/StudentQueue.java
@@ -8,6 +8,17 @@ public class StudentQueue{
         }
         students[count]=new Student(name,mark);
         count++;
-
+    }
+    public void dequeue(){
+        if(count==0){
+            System.out.println("Queue is empty");
+            return;
+        }
+        System.out.println("dequeue: ");
+        students[0].display();
+        for(int i=0;i<count-1;i++){
+            students[i]=students[i+1];
+        }
+        count--;
     }
 }
