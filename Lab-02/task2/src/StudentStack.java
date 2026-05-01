ab035a643b7df5b5ee928cbd13ce2933d1e1febd
nusra <nusratnishi1103@gmail.com>
2026-05-01 23:24:32 +0600

    new

--- Lab-02/task2/src/StudentStack.java
+++ Lab-02/task2/src/StudentStack.java
@@ -1,3 +1,16 @@
+//a
+  /* push and pop never shifts elements.they only touch the top elements of the stack.
+     so the size of the stack has no effect on the speed of push and pop as both operations
+     take the same constant time(0(1)).
+   */
+
+//b
+   /* no one objects count cannot interfere with the other's. because both has its own separtate
+      copy of the private file named "count" stored in the memory. as private means the field belongs
+      to that object only. it is not accessible to all the other classes. so it cannot interfere.
+    */
+
+
 public class StudentStack {
     private String[] names = new String[50];
     private int[] marks = new int[50];
