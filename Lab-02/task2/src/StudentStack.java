2519580f73f486a6fd972d6a14ad2afcdcefeaba
Rakib <rakibulhasan4266@gmail.com>
2026-05-01 22:46:28 +0600

    peek() implementation

--- Lab-02/task2/src/StudentStack.java
+++ Lab-02/task2/src/StudentStack.java
@@ -4,4 +4,26 @@ public class StudentStack
     private String[] names = new String[99];
     private int[] marks = new int[99];
     private int count = 0;
+
+    public void push(String name, int mark)
+    {
+        {
+            if(count >= 99)
+            {
+                System.out.println("Error: Stack is full.");
+            }
+            else
+            {
+                names[count] = name;
+                marks[count] = mark;
+                count++;
+            }
+        }
+    }
+
+
+    public void peek()
+    {
+        System.out.println("Top: " + names[count - 1]  + " " + marks[count - 1] );
+    }
 }
