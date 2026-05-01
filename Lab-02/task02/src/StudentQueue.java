6f13e5568674f84d9b4a896385b24282d58a4c8f
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-01 13:05:53 +0600

    init commit

--- Lab-02/task02/src/StudentQueue.java
+++ Lab-02/task02/src/StudentQueue.java
@@ -19,10 +19,16 @@ public class StudentQueue {
         count++;
 
     }
+public void dequeue(){
+        if (count == 0) {
+            System.out.println("ERROR:QUEUE IS EMPTY");
+        return;
+        }
 
-
-
-
-
-
+        for (int i = 0; i < count - 1; i++) {
+            names[i] = names[i + 1];
+            marks[i] = marks[i + 1];
+        }
+        count--;
+    }
 }
