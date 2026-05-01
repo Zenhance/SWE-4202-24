eab21ab211ac593bff12246224a8a6eb450b8748
Muhtasim Moin <muhtasi13@gmail.com>
2026-05-01 11:08:53 +0600

    init commit

--- Lab-02/Task2/src/Main.java
+++ Lab-02/Task2/src/Main.java
@@ -3,7 +3,11 @@
 public class Main {
     public static void main(String[] args) {
         StudentStack s1=new StudentStack();
+        StudentStack s2=new StudentStack();
+        StudentStack s3=new StudentStack();
+         s1.push("Arnob",90);
+         
 
 
-
+}
 }
