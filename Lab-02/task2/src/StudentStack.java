b328c958fb70c41464d1a79b86ec786a04cba08d
mubashirulhasan-netizen <mubashirulhasan@iut-dhaka.edu>
2026-05-01 16:10:14 +0600

    test

--- Lab-02/task2/src/StudentStack.java
+++ Lab-02/task2/src/StudentStack.java
@@ -3,6 +3,7 @@
     a)push and pop never shift any elements. This means the speed stays almost the
     same whether the stack has 5 students or 50 students because only the top position
     is accessed.
+    
 
     b)The count field belongs to each object separately because it is not static.
     One StudentStack or StudentQueue object cannot interfere with another object's
