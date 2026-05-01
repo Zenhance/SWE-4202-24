d57be97ad9622d842793cd620071ee27911f6b62
sabbir240042147 <eftesabbir2725@gmail.com>
2026-05-01 17:11:44 +0600

    all the method

--- Lab-02/Task2/src/StudentStack.java
+++ Lab-02/Task2/src/StudentStack.java
@@ -8,5 +8,43 @@ public class StudentStack {
         this.mark=mark;
         this.count =0;
     }
+    public void push(String name,int mark){
+        if(count>=50){
+            System.out.println("The stack is full");
+            return;
+
+        }
+        else{
+           this.name[count]=name;
+            this.mark[count]=mark;
+            count ++;
+        }
+    }
+    public void pop(){
+        if(count ==0){
+            System.out.println("The stack is empty!");
+            return;
+        }
+        else{
+            System.out.println("Removed:"+this.name[count-1]+","+this.mark[count-1]);
+            count --;
+        }
+    }
+    public void peek(){
+        if(count==0){
+            System.out.println("the stack is empty!");
+            return;
+        }
+        else{
+            System.out.println("Top:"+this.name[count-1]+","+this.mark[count-1]);
+        }
+
+    }
+    public void display(){
+        for(int i=0;i<count;i++){
+            System.out.println(i+"."+"Name:"+this.name[i]+" "+"Mark:"+this.mark[i]);
+        }
+    }
+
 
 }
