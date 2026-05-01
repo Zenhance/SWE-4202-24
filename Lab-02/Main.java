b5e79a70a77338d0bc88eadc01e74f6e1f982418
04-Labiba <sabihalabiba639@gmail.com>
2026-05-01 13:46:48 +0600

    created push method

--- Lab-02/Main.java
+++ Lab-02/Main.java
@@ -1,5 +1,17 @@
-public class Main{
-    public static void main(String[] args){
-     System.out.println("For Student Stack");
-    }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("For Student Stack");
+        StudentStack s1 = new StudentStack();
+        for (int i = 1; i <= 5; i++) {
+            s1.push("Student" + i, 50 + i);
+        }
+        System.out.println("PEEK:");
+        s1.peek();
+        System.out.println("POP:");
+        s1.pop();
+        s1.pop();
+        s1.pop();
+        System.out.println("DISPLAY:");
+        s1.display();
+
 }
