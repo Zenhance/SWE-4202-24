80a2836d7c57072cfb99c0bf502caca465a8c360
Rakib <rakibulhasan4266@gmail.com>
2026-05-01 22:00:23 +0600

    Call push with direct input

--- Lab-02/task2/src/Main.java
+++ Lab-02/task2/src/Main.java
@@ -1,12 +1,20 @@
 package src;
 import java.util.Scanner;
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         //Stack
         StudentStack Stack = new StudentStack();
+
+        System.out.println("Enter 5 students for Stack: ");
+        for(int i = 1; i <= 5; i++)
+        {
+            System.out.print("Name and Mark " + i + ": ");
+            String name = sc.next();
+            int mark = sc.nextInt();
+            Stack.push(name, mark);
+        }
+
     }
 }
