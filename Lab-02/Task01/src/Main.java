e21ea38e7dcb9879ee95edd4c4faf4c6764b9709
Tnvr09 <rahmantanvir64@gmail.com>
2026-05-01 23:57:24 +0600

    initial comments added commit

---
+++ Lab-02/Task01/src/Main.java
@@ -0,0 +1,14 @@
+/* A. Every account needs to store Current Balance
+
+B. One can perform three actions from their account:
+1. Deposit
+2. Withdraw
+3. Check Balance
+
+C. Storing all balance in a one big array will cause several problems.
+Inputting and sorting it will be difficult. Maintaining will be another issue.
+Editing data is also a hassle here.
+
+D. I will add that in the class as an individual method.
+ */
+
