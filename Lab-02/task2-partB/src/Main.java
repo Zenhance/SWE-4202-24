4975825a172b33f40b250d02ff620ed3e3b4e02d
mahibrahaman175-stack <mahibrahaman175@gmail.com>
2026-05-01 23:50:03 +0600

    only the queue part done, main done gn

--- Lab-02/task2-partB/src/Main.java
+++ Lab-02/task2-partB/src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+    public static void main(String[] args) {
+
+        StudentQueue q = new StudentQueue(5)
+            ;
+
+
+        q.enqueue(new Student("Alice", 80));
+        q.enqueue(new Student("Bob", 70));
+        q.enqueue(new Student("Charlie", 60));
+        q.enqueue(new Student("David", 90));
+        q.enqueue(new Student("Eve", 100));
+
+
+        q.dequeue();
+        q.dequeue();
+        q.dequeue();
+
+        q.Display();
+
+    }
 }
