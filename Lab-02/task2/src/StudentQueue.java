6b14d2e09d71e8fd8dc74e431aa978bcf2565559
md-musaddik <musaddikibna1@gmail.com>
2026-05-01 23:39:47 +0600

    dequeue method declared

--- Lab-02/task2/src/StudentQueue.java
+++ Lab-02/task2/src/StudentQueue.java
@@ -5,9 +5,22 @@ public class StudentQueue {
     public void enqueue(String name,int marks){
         if(count==100){
             System.out.println("Stack is full");
+            return;
         }
         this.name[count]=name;
         this.marks[count]=marks;
         count++;
+    }
+    public void dequeue(){
+        if(count==0){
+            System.out.println("The queue is empty.");
+            return;
+        }
+        System.out.println("Name: "+this.name[0]+" Mark: "+this.marks[0]);
+        for (int i = 0; i < count-1; i++){
+            this.name[i] = this.name[i+1];
+            this.marks[i] = this.marks[i+1];
+        }
+        count--;
     }
 }
