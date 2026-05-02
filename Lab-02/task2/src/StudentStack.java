3578985eb73ef980ce104a054b161c12e76ef72c
farnazrahman <farnaz28.rahman@gmail.com>
2026-05-02 20:33:33 +0600

    constructor

--- Lab-02/task2/src/StudentStack.java
+++ Lab-02/task2/src/StudentStack.java
@@ -1,6 +1,11 @@
 public class StudentStack {
-    private String[] name;
-    private int[] marks;
+     String[] name;
+     int[] marks;
+     int count;
     final int MAX_SIZE=100;
-
+   StudentStack(String[] name,int[] marks){
+       this.name=name;
+       this.marks=marks;
+       this.count=0;
+   }
 }
