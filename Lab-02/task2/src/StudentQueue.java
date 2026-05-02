2df4f32b051e99c4cb3fb4e84a4a1f064b4634ab
farnazrahman <farnaz28.rahman@gmail.com>
2026-05-03 00:01:03 +0600

    Enqueue

--- Lab-02/task2/src/StudentQueue.java
+++ Lab-02/task2/src/StudentQueue.java
@@ -1,2 +1,14 @@
 public class StudentQueue {
+    String[] name=new String[100];
+    int[] marks =new int[100];
+    int count=0;
+    public void enqueue(String name,int marks){
+        if(count == 100){
+            System.out.println("Error!");
+        }
+        this.name[count]=name;
+        this.marks[count]=marks;
+        count++;
+    }
+
 }
