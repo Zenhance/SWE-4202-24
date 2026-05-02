ce88a17de7ae446779258bb7346945ac44d3a2a5
farnazrahman <farnaz28.rahman@gmail.com>
2026-05-03 00:15:07 +0600

    Main

--- Lab-02/task2/src/Main.java
+++ Lab-02/task2/src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
  public static void main(String[] args){
      StudentStack stack = new StudentStack();
+     StudentQueue queue=new StudentQueue();
+
+
      stack.push("Farnaz",100);
      stack.push("Rahman",100);
      stack.push("abc",67);
      stack.push("def",67);
      stack.push("ghi",67);
+
 
      stack.peek();
      stack.pop();
@@ -15,7 +19,18 @@ public class Main {
 
 
 
+     queue.enqueue("A1",100);
+     queue.enqueue("A2",90);
+     queue.enqueue("A3",80);
+     queue.enqueue("A4",70);
+     queue.enqueue("A5",60);
 
+
+     queue.front();
+     queue.dequeue();
+     queue.dequeue();
+     queue.dequeue();
+     queue.display();
 
  }
 }
