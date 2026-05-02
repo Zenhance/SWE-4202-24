12439fd7796b90353c59a7432e8fe2d0d5b85f30
farnazrahman <farnaz28.rahman@gmail.com>
2026-05-03 00:31:47 +0600

    init commit

--- Lab-02/task2/src/StudentStack.java
+++ Lab-02/task2/src/StudentStack.java
@@ -1,3 +1,13 @@
+/*a)Push and pop never shift any elements,
+ they just add and remove elements at the position
+ pointed by count.Count is always at the end of the array.
+ So both operations take constant time o(1).It doesn't matter if the stack has
+ 5 or 50 students. It will push and pop in the same amount of time.
+ b)Each copy of StudentStack has its own separate copy of "count".If I use "count" in both
+ StudentStack and StudentQueue, the "count" will not be the same. They will hold different values.
+ Changing one will never affect the other.
+ */
+
 public class StudentStack {
      String[] name=new String[100];
      int[] marks =new int[100];
