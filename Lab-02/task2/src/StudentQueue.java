a67afb22f6512cb479a29feeb12d984e00102b45
16-safwan <safwanbhuiyan@iut-dhaka.edu>
2026-05-02 17:44:45 +0600

    git commit

--- Lab-02/task2/src/StudentQueue.java
+++ Lab-02/task2/src/StudentQueue.java
@@ -4,15 +4,17 @@ class StudentQueue {
     int front, rear;
     final int MAX = 100;
 
-    public StudentQueue() {
+    public StudentQueue()
+    {
         names = new String[MAX];
         marks = new int[MAX];
         front = 0;
         rear = 0;
     }
-
-    public void enqueue(String name, int mark) {
-        if (rear >= MAX) {
+    public void enqueue(String name, int mark)
+    {
+        if (rear >= MAX)
+        {
             System.out.println("Queue is full!");
             return;
         }
@@ -20,5 +22,30 @@ class StudentQueue {
         names[rear] = name;
         marks[rear] = mark;
         rear++;
+    }
+    public void dequeue()
+    {
+        if (front == rear)
+        {
+            System.out.println("Queue is empty!");
+            return;
+        }
+        System.out.println("Removed: " + names[front] + " (" + marks[front] + ")");
+        front++;
+    }
+    public void peek()
+    {
+        if (front == rear) {
+            System.out.println("Queue is empty!");
+            return;
+        }
+        System.out.println("Front: " + names[front] + " (" + marks[front] + ")");
+    }
+    public void display()
+    {
+        for (int i = front; i < rear; i++)
+        {
+            System.out.println(names[i] + " (" + marks[i] + ")");
+        }
     }
 }
--- Lab-02/task2/src/StudentStack.java
+++ Lab-02/task2/src/StudentStack.java
@@ -4,19 +4,47 @@ class StudentStack {
     int count;
     final int MAX = 100;
 
-    public StudentStack() {
+    public StudentStack()
+    {
         names = new String[MAX];
         marks = new int[MAX];
         count = 0;
     }
-
-    public void push(String name, int mark) {
-        if (count >= MAX) {
+    public void push(String name, int mark)
+    {
+        if (count >= MAX)
+        {
             System.out.println("Stack is full!");
             return;
         }
         names[count] = name;
         marks[count] = mark;
         count++;
+    }
+    public void pop()
+    {
+        if (count == 0)
+        {
+            System.out.println("Stack is empty!");
+            return;
+        }
+        count--;
+        System.out.println("Popped:"+names[count]+"("+marks[count]+")");
+    }
+    public void peek()
+    {
+        if (count == 0)
+        {
+            System.out.println("Stack is empty!");
+            return;
+        }
+        System.out.println("Top:"+names[count-1]+"("+marks[count-1]+")");
+    }
+    public void display()
+    {
+        for (int i=0;i<count;i++)
+        {
+            System.out.println(names[i]+"("+marks[i]+")");
+        }
     }
 }
