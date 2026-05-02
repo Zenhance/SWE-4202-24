eb993c902ef8eb8a6ff7657bef2d06a1b29abdee
farnazrahman <farnaz28.rahman@gmail.com>
2026-05-02 17:09:03 +0600

    Restore task1 Main.java and BankAccount.java

---
+++ Lab-02/task1/src/BankAccount.java
@@ -0,0 +1,34 @@
+public class BankAccount {
+    String name;
+    double balance = 0;
+
+    BankAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Error");
+        } else {
+            balance += amount;
+        }
+
+    }
+
+    void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient");
+        } else {
+            balance -= amount;
+
+        }
+
+    }
+    void print(){
+        System.out.println(name);
+        System.out.println(balance);
+    }
+
+
+}
