9da83ca5f4b73347baf9abe88b23fbf5287ad19e
farnazrahman <farnaz28.rahman@gmail.com>
2026-05-02 23:45:08 +0600

    Main

--- Lab-02/task2/src/Main.java
+++ Lab-02/task2/src/Main.java
@@ -1,3 +1,21 @@
 public class Main {
+ public static void main(String[] args){
+     StudentStack stack = new StudentStack();
+     stack.push("Farnaz",100);
+     stack.push("Rahman",100);
+     stack.push("abc",67);
+     stack.push("def",67);
+     stack.push("ghi",67);
 
+     stack.peek();
+     stack.pop();
+     stack.pop();
+     stack.pop();
+     stack.display();
+
+
+
+
+
+ }
 }
