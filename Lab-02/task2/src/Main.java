941e72f9866434a273b54a5697eed03efa01c83e
wasifjafar <wasifjafar71@gmail.com>
2026-05-03 12:14:31 +0600

    init commit

--- Lab-02/task2/src/Main.java
+++ Lab-02/task2/src/Main.java
@@ -3,22 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-
         StudentStack stack = new StudentStack();
 
-        stack.push(scan.nextLine(), scan.nextInt());
+        stack.push(getName(), getMark());
         stack.display();
 
-        stack.push(scan.nextLine(), scan.nextInt());
+        stack.push(getName(), getMark());
         stack.display();
 
         stack.peek();
 
-        stack.push(scan.nextLine(), scan.nextInt());
+        stack.push(getName(), getMark());
         stack.display();
 
-        stack.push(scan.nextLine(), scan.nextInt());
+        stack.push(getName(), getMark());
         stack.display();
 
         stack.peek();
@@ -29,14 +27,30 @@ public class Main {
 
         StudentQueue queue = new StudentQueue();
 
-        queue.enqueue(scan.nextLine(), scan.nextInt());
-        queue.enqueue(scan.nextLine(), scan.nextInt());
-        queue.enqueue(scan.nextLine(), scan.nextInt());
+        queue.enqueue(getName(), getMark());
+        queue.enqueue(getName(), getMark());
+        queue.enqueue(getName(), getMark());
         queue.display();
         queue.front();
 
         queue.dequeue();
         queue.display();
         queue.front();
+    }
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static String getName() {
+        System.out.print("Enter student name: ");
+
+        String name = scan.nextLine();
+        return name;
+    }
+
+    public static int getMark() {
+        System.out.print("Enter mark: ");
+        int mark = scan.nextInt();
+        scan.nextLine();
+        return mark;
     }
 }
