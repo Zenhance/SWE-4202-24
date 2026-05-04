76eb4105d1296a2b2adb70fb8fd43e0d28874f18
tasnitaraida <tasnitar@gmail.com>
2026-05-04 19:39:13 +0600

    create class StudentQueue

--- task2/src/StudentQueue.java
+++ task2/src/StudentQueue.java
@@ -1,2 +1,10 @@
 public class StudentQueue {
+    String[] name = new String[1000];
+    float[] mark = new float[1000];
+    int students=1000;
+    int count;
+
+    public void enqueue() {
+
+    }
 }
