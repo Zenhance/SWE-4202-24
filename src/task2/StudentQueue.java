da00dddfbfb634800f04982b375d0a9b1a7e07dc
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-05-04 21:35:54 +0600

    StudentQueue class v1(enqueue)

--- src/task2/StudentQueue.java
+++ src/task2/StudentQueue.java
@@ -1,4 +1,19 @@
 package task2;
 
 public class StudentQueue {
+    private String[] name = new String[50];
+    private int[] marks = new int[50];
+    private int count =0;
+
+    public void enqueue(String name, int marks){
+        if(count ==50){
+            System.out.println("queue is full");
+            return;
+        }
+        this.name[count] =name;
+        this.marks[count]=marks;
+        count++;
+    }
+
+    public void dequeue()
 }
