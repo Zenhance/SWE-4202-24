48f63873ee632dc2080ceabc695df84577b81c69
Lamisa <lamisaibnat5d@gmail.com>
2026-05-05 12:58:39 +0600

    Third commit

---
+++ Lab-03/RideNow/src/Passenger.java
@@ -0,0 +1,25 @@
+public class Passenger {
+    int id;
+    String name;
+    double rating;
+    Passenger(int id,String name, double rating){
+        this.id=id;
+        this.name=name;
+        this.rating=rating;
+    }
+    Passenger(int id,String name){
+        this.id=id;
+        this.name=name;
+    }
+    public void getId(){
+
+    }
+    public void getName(){
+
+    }
+    public void getRating(){
+
+    }
+
+
+}
