dcdaef07544b254e15bd83110772c18462ba1379
hasnat-farhan <hasnatfarhan@iut-dhaka.edu>
2026-05-06 00:12:20 +0600

    none

---
+++ Lab-02/task2/StudentQueue.java
@@ -0,0 +1,15 @@
+public class StudentQueue {
+    private String[] name;
+    private int[] marks;
+    private int c;
+    private int max=10;
+
+    StudentQueue(String name, int marks)
+    {
+        this.name=name;
+        this.marks=marks;
+        this.c=0;
+    }
+
+
+}
