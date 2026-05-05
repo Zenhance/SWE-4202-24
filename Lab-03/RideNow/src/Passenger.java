80eb23da661c496d63a3422dc323ba76344d856a
Tnvr09 <rahmantanvir64@gmail.com>
2026-05-05 10:04:13 +0600

    Passenger class initiated

---
+++ Lab-03/RideNow/src/Passenger.java
@@ -0,0 +1,14 @@
+public class Passenger {
+    private int id;
+    private String name;
+    private double rating;
+
+    public Passenger(int id, String name, double rating) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+    public Passenger(int id, String name) {
+        this.rating = 5.0;
+    }
+}
