6e3cae3d98d8fb1d3697970a1e10bc75c7d811d1
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-05-05 12:56:44 +0600

    initial commit

---
+++ Lab-03/RideNow/src/location.java
@@ -0,0 +1,52 @@
+public class location {
+
+
+    String label;
+    double x;
+    double y;
+
+    public location(String label, double x, double y) {
+
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+
+    public location(double x, double y) {
+        this.label = "Unknown";
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public String getlabel() {
+        return label;
+    }
+    public double getX(){
+
+return x;
+    }
+public double getY()
+{
+    return y;
+
+}
+public double distanceTo(location)
+{double distance=0;
+    return distance;
+}
+
+public String toString()
+{
+    return "Airport (3.00, 7.50)";
+
+}
+
+}
+
+
+
+
+
+
+}
---
+++ Lab-03/RideNow/src/passenger.java
@@ -0,0 +1,27 @@
+public class location
+{
+
+String label;
+double x;
+double y;
+
+public Location (String label, double x, double y){
+
+    this.label=label;
+    this.x=x;
+    this.y=y;
+
+
+}
+
+
+
+
+
+
+}
+
+
+
+
+
