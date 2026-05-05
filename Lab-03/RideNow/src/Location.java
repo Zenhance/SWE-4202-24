e742b746d5bc31bbc6c1a37c7683844a4635aa41
Maomao-starry <aminarahmanshalu@gmail.com>
2026-05-05 12:58:18 +0600

    Task1

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,21 @@
+public class Location{
+        private String lebel;
+        double x;
+        double y;
+
+        public Location(String lebel,double x, double y){
+            this.lebel ="lebel" ;
+            this.x= x;
+            this.y= y;
+        }
+        public Location(double x, double y){
+            this.x=x;
+            this.y=y;
+            this.lebel="unknown";
+
+        }
+
+}
+
+
+
