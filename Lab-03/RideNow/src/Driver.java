a0c1bd03d15a8f305258b3ed5e58903142004718
tuireuit <jamilhasan@iut-dhaka.edu>
2026-05-05 13:05:40 +0600

    writing drive class

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -2,4 +2,40 @@ public class Driver {
     private int id;
     private String name;
     private String licencePlate;
+    private boolean isAvailable;
+
+
+
+    public Driver(int id, String name, String licencePlate){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=1;
+
+    }
+
+    public Driver(int id, String name, String licencePlate, boolean isAvailable){
+               this.id=id;
+               this.name=name;
+               this.licencePlate=licencePlate;
+               this.isAvailable=isAvailable;
+
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getLicencePlate(){
+         return this.licencePlate;
+    }
+
+
+    public boolean isAvailable(){
+        return this.isAvailable;
+    }
 }
