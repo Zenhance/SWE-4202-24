571585aa87b391d6a00405d8b491d40143851d17
cse <ayeshamahjabin@iut-dhaka.edu>
2026-05-05 10:30:23 +0600

    init commit

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -4,17 +4,33 @@ public class Location {
     private String label;
     private double x;
     private double y;
-    public Location(String label,double x,double y) {
+
+    public Location(String label, double x, double y) {
         this.label = label;
         this.x = x;
-        this.y=y;
+        this.y = y;
     }
-public String getLabel(){
+
+    public String getLabel() {
         return label;
-}
-public double getX(){
+    }
+
+    public double getX() {
         return x;
-}
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double distanceTo(Location other) {
+        double dx = other.x - this.x;
+        double dy = other.y - this.y;
+        return Math.sqrt(dx*dx+dy*dy);
+    }
+    public String toString(){
+        return String.format("%s(%.2f,%.2f)",label,x,y);
+    }
 
 
 
