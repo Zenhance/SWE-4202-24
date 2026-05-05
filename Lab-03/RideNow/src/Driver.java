aca6b791907b3ce3e044d8121655c5f3b4c4077e
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-05-05 10:17:44 +0600

    first constructor done

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -5,5 +5,12 @@ public class Driver {
     private String licencePlate;
     private boolean isAvailable;
 
+    Driver(int id, String name, String licencePlate){
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        isAvailable = true;
+    }
+
 
 }
