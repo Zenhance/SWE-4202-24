ac968ec012bc231a8242f0f427419e86294478f4
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-05-05 10:12:46 +0600

    first constructor

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -3,6 +3,12 @@ private int id;
 private String name;
 private double rating; // a number from 1.0 to 5.0
 
+    Passenger(int id, String name, double rating){
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+
 
 
 }
