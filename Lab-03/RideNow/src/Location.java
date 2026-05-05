b455d7bc78c131fa37309286afcc1415f3af6f8c
Tahmid <tahmidim99@gmail.com>
2026-05-05 12:54:39 +0600

    lab 3

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -3,29 +3,35 @@ public class Location {
     private double x;
     private double y;
 
-    public Location(String label, double x, double y) {
-        this.label = label;
-        this.x = x;
-        this.y = y;
+    public Location(String label, double x, double y){
+        this.label=label;
+        this.x=x;
+        this.y=y;
+    }
+    public Location(double x, double y){
+        this.label="unknown";
+        this.x=x;
+        this.y=y;
     }
 
-    public Location(double x, double y) {
-        this.label = "unknown";
-        this.x = x;
-        this.y = y;
-    }
-
-    public String getLabel() {
+    public String getLabel(){
         return label;
     }
 
-    public double getX() {
-        return x;
+    public double getX(){
+         return x;
     }
-
-    public double getY() {
+    public double getY(){
         return y;
     }
+    public double distanceTo(Location other){
+        double dx=other.x-this.x;
+        double dy=other.y-this.y;
+        return Math.sqrt(dx*dx+dy*dy);
+
+    }
+
+
 
 }
 
