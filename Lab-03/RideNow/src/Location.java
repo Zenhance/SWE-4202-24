a857cbf4dd13b3922e7e23277186ef215b7c5851
Zinti <zeffathzourin@iut-dhaka.edu>
2026-05-05 09:30:18 +0600

    init commit

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -2,4 +2,9 @@ public class Location {
     public  String label;
     public double x;
     public double y;
+    public Location(String label,double x,double y){
+        this.label=label;
+        this.x=x;
+        this.y=y;
+    }
 }
