555283942d3842fd30c88d1956eba59f91f58519
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-05-05 23:21:17 +0600

    Made changes to Driver class

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -1,50 +1,46 @@
 //Part C
 public class Driver {
-    private int id;
-    private String name;
-    private String licencePlate;
+    private final int id;
+    private final String name;
+    private final String licencePlate;
     private boolean isAvailable;
-
-    public Driver(int id, String name, String licencePlate){
+    
+    public Driver(int id, String name, String licencePlate) {
         this.id = id;
         this.name = name;
         this.licencePlate = licencePlate;
         this.isAvailable = true;
     }
-
-    public Driver(int id, String name, String licencePlate, boolean isAvailable){
+    
+    public Driver(int id, String name, String licencePlate, boolean isAvailable) {
         this.id = id;
         this.name = name;
         this.licencePlate = licencePlate;
         this.isAvailable = isAvailable;
     }
-
+    
     public int getId() {
         return id;
     }
-    public String getName(){
+    
+    public String getName() {
         return name;
     }
-    public String getLicencePlate(){
+    
+    public String getLicencePlate() {
         return licencePlate;
     }
+    
     public boolean isAvailable() {
         return isAvailable;
     }
-
-    void setAvailable(boolean available){
-        this.isAvailable=available;
+    
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
     }
-
+    
     //"Driver[7] Bob (DHA-1234) [AVAILABLE]" or "[BUSY]".
-    public String toString(){
-        String availability;
-        if(isAvailable) {
-            availability = new String("AVAILABLE");
-        }
-        else{
-            availability = new String("BUSY");
-        }
-        return String.format("Driver[%d] %s (%s) [%s]",id,name,licencePlate,availability);
+    public String toString() {
+        return String.format("Driver[%d] %s (%s) [%s]", id, name, licencePlate, isAvailable ? "AVAILABLE" : "BUSY");
     }
 }
