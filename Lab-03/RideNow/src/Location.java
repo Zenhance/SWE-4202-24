6501587c6ad450a350946ac2488b7b6f904a1f8d
waseefmunir-max <waseef05@gmail.com>
2026-05-05 09:37:34 +0600

    First commit

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,36 @@
+public class Location {
+    private String label;
+    private double x;
+    private double y;
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public Location(String label, double x, double y)
+    {
+       this.label = label;
+       this.x = x;
+       this.y = y;
+    }
+}
