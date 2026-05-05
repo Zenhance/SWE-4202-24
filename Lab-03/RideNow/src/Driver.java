b145c2b05225ff4b562362b16c8210b5068e727d
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-05-05 10:11:33 +0600

    Created Fields(All private), constructors, methods...

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,39 @@
+public class Driver {
+    private int id;
+    private String name;
+    private String licencePlate;
+    private boolean isAvailable;
+
+    public Driver(int id, String name, String licencePlate, boolean isAvailable) {
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = isAvailable;
+    }
+
+    public Driver(int id, String name, String licencePlate) {
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+    String toString(){
+
+    }
+}
