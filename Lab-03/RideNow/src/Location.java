6a1c35bb26e0afbe54fd02080cbdd7df7c486c47
md-musaddik <musaddikibna1@gmail.com>
2026-05-05 09:40:34 +0600

    Location class created, Varibales decleared and the constructors created

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,16 @@
+public class Location {
+    private String label;
+    private double x;
+    private double y;
+    Location(String label,double x, double y){
+        this.label=label;
+        this.x=x;
+        this.y=y;
+    }
+    Location(double x, double y){
+        this.label="Unknown";
+        this.x=x;
+        this.y=y;
+    }
+
+}
