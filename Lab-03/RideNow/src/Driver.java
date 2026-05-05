c95fe9d7533117e3df0b5c9473ce2b8f942dfc91
Zunied-57 <zunied0411@gmail.com>
2026-05-05 10:03:47 +0600

    5th commit

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,27 @@
+class Driver
+{
+    private int id;
+    private String name;
+    private String licencePlate;
+    private boolean isAvailable;
+
+    public Driver(int id, String name, String licencePlate)
+    {
+
+    }
+
+    public Driver(int id, String name, String licencePlate, boolean isAvailable)
+    {
+
+    }
+
+    public void setAvailable(boolean available)
+    {
+
+    }
+
+    public String toString()
+    {
+        return "Driver[7] Bob (DHA-1234) [AVAILABLE]";
+    }
+}
