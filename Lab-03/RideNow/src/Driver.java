3ab84b2fe810a984450a116bbded24a88362ee75
Muhtasim Moin <muhtasi13@gmail.com>
2026-05-05 10:31:53 +0600

    init commit

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,19 @@
+public class Driver {
+     int id;
+     String name;
+     String licencePlate;
+     boolean isAvailable;
+
+    public Driver(int id, String name, String licencePlate){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable= true;
+    }
+    public Driver(int id, String name, String licencePlate, boolean isAvailable){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=isAvailable;
+    }
+}
