8dbe0d1e36225b1fd76d7248d195fd1387ddcfb6
Rakin <rakinhasan001@gmail.com>
2026-05-05 09:46:28 +0600

    Constructors added in location class

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -3,3 +3,14 @@ public class Location {
     private double x;
     private double y;
 
+    Location(String label,double x,double y){
+        this.label=label;
+        this.x=x;
+        this.y=y;
+    }
+    Location(double x,double y){
+    this.label="Unknown";
+    this.x=x;
+    this.y=y;
+    }
+}
