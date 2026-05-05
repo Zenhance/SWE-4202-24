0dcaeeadf62be783eb8d8c061b197435fdefe47e
ayiham-1012 <ayiham1012@gmail.com>
2026-05-05 12:59:33 +0600

    Part A & Part B

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,62 @@
+import static java.lang.Math.sqrt;
+
+public class Location {
+
+    private String label = "Unknown";
+    private double x;
+    private double y;
+
+    Location(String label, double x, double y){
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+
+    Location(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getLabel(){
+        String label = this.label;
+        return label;
+    }
+
+    public double getX(){
+        double x = this.x;
+        return x;
+    }
+    public double getY(){
+        double y = this.y;
+        return y;
+    }
+
+    public double distanceTo(Location other) {
+        double x1 = this.getX();
+        double y1 = this.getY();
+        double x2 = other.getX();
+        double y2 = other.getY();
+
+        double distance = sqrt((x2-x2)*(x2-x1) + (y2-y1)*(y2-y1));
+        return distance;
+    }
+
+    public String toString(){
+
+        String label = getLabel();
+        double x = getX();
+        double y = getY();
+
+        String x_str = String.valueOf(x);
+        String y_str = String.valueOf(y);
+
+        String output;
+        output = label.concat(" (");
+        output = output.concat(x_str);
+        output = output.concat(", ");
+        output = output.concat(y_str);
+        output = output.concat(")");
+        // test: System.out.println(output);
+        return output;
+    }
+}
---
+++ Lab-03/RideNow/src/Passenger.java
@@ -0,0 +1,53 @@
+public class Passenger {
+    private int id;
+    private String name;
+    private double rating = 5.0;
+
+    Passenger(int id, String name, double rating){
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+    Passenger(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId(){
+        int id = this.id;
+        return id;
+    }
+
+    public String getName(){
+        String name = this.name;
+        return name;
+    }
+
+    public double getRating(){
+        double rating = this.rating;
+        return rating;
+    }
+
+    public void updateRating(double newRating){
+        rating = newRating;
+    }
+
+    public String toString(){
+        String name = getName();
+        int id = getId();
+        double rating = getRating();
+
+        String id_str = String.valueOf(id);
+        String rating_str = String.valueOf(rating);
+
+        String output = "Passenger[";
+        output = output.concat(id_str);
+        output = output.concat("] ");
+        output = output.concat(name);
+        output = output.concat(" (");
+        output = output.concat(rating_str);
+        output = output.concat(")");
+
+        return output;
+    }
+}
