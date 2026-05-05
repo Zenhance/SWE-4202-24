dd2a67a95a0802c414c6eb97123d3d8d2ebce570
Lamisa <lamisaibnat5d@gmail.com>
2026-05-05 12:49:11 +0600

    First commit

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,31 @@
+public class Location {
+    String lebel;
+    double x,y;
+    Location(String lebel,double x, double y){
+        this.lebel=lebel;
+        this.x=x;
+        this.y=y;
+    }
+    Location(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+    public String getlebel(String str){
+        return str;
+    }
+    public double getx(double x){
+       return x;
+    }
+    public double gety(double y){
+      return y;
+    }
+    public double distanceTo(double x1,double y1){
+        double distance=math.sqrt(((x1-x)*(x1-x))+((y1-y)*(y1-y)));
+        return distance;
+    }
+    public String toString(String lebel, double x, double y){
+        return String.format(lebel,x,y);
+    }
+
+}
+
