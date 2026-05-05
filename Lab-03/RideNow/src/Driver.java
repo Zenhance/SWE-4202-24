8984e8565a28b6cd2207b62df5f8e04bb1cb72c6
ahnaf-code <ahnafcode@gmail.com>
2026-05-05 12:57:46 +0600

    Driver Commit 1

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,20 @@
+public class Driver {
+    private int id;
+    private String name;
+    private String licencePlate;
+    private boolean isAvailable;
+
+    Driver(int id, String name, String licencePlate) {
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = true;
+    }
+
+    Driver(int id, String name, String licencePlate, boolean isAvailable) {
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = isAvailable;
+    }
+}
