18fcc1e7f03a96fd0981f4e3d7af61a07d6fc427
Sadman Raiyan <raiyansadman121@gmail.com>
2026-05-05 10:36:56 +0600

    Adding Constructors

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -1,6 +1,21 @@
 public class Driver {
     private int id;
-    private String name,licencePlate;
+    private String name;
+    private String licencePlate;
     boolean isAvailable;
+
+    Driver(int id, String name, String licencePlate){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable= true;
+    }
+
+    Driver(int id, String name, String licencePlate, boolean isAvailable){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable= isAvailable;
+    }
 
 }
