dda7398b6fd7313e463e53bfb9f48f755c2f01e5
elfie1502 <mumtahina@iut-dhaka.edu>
2026-05-05 13:21:51 +0600

    completed

--- Lab-02/task1/src/BankAccount.java
+++ Lab-02/task1/src/BankAccount.java
@@ -1,12 +1,50 @@
 public class BankAccount {
-    /* Part A
-        a.
+    /* Part A.The information every account need to store
          1.Owner's Name using String
-         2.Owner's Balance using double
+         2.Owner's  Current Balance using double
+         3.Account ID using integer
 
-        b.
-         1.Deposit (input) and add balance (change)
-         2.Withdraw ( input) and deduct balance from main balance ( change)
-         3. Check Balance (output) and print the current balance
+
+        b.Actions that have to perform:
+         1.Deposit money(input), for this nothing needs to know. And it works as current balance and add balance (change) which will be added in the current balance
+         2.Withdraw money ( input), for this have to check the current balance and deduct balance from main balance ( change). Yhe withdrawal money can't be bigger amount than the main balance otherwise it will give errors.
+         3. Check Balance (output) and print the current balance.
+
+         C. In handling 200 customers using just two arrays it will be difficult to manage and keep track of their names and balances. using class helps to combined and store the names and balance in order.
+         and also it will create Error prone whenever changing or remove any customer's details.
+         D.I would add the transaction limit in the BankAccount class so that it doesn't hamper other customer's transaction limit as it gives unique for every individual automatically.And if I use this in main I have to rewrite the limit logic every time.
+
+
      */
-}
+
+    String ownerName;
+    double balance;
+
+    public BankAccount(String accountName) {
+        this.ownerName = accountName;
+        this.balance = 0;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Error");
+        } else {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance");
+        }
+
+    }
+    public void printStatement() {
+            System.out.println("Owner's Name: +ownerName");
+            System.out.println("Current Balance: + balance");
+
+        }
+
+    }
--- Lab-02/task1/src/Main.java
+++ Lab-02/task1/src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+void main() {
+    BankAccount mumtahina = new BankAccount("Mumtahina");
+    BankAccount nunjiba = new BankAccount("Nunjiba");
+
+    mumtahina.deposit(10000);
+    nunjiba.deposit(20000);
+
+    mumtahina.withdraw(525);
+    nunjiba.withdraw(250);
+
+    mumtahina.printStatement();
+    nunjiba.printStatement();
+
 }
+
+/*
+(a) BankAccount is a class.Alice and Bob is an object of the BankAccount class.
+(b) Only Alice's balance changes. because when we do alice.method, it knows that the
+method should only change the attributes of the Alice object.In the methods one object is not connected with others.
+(c) Alice:
+    name = "Alice"
+    balance = 0
+
+   Bob:
+     name = "Bob:
+     balance = 0
+(d)
+
+ */
