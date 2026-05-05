564e962d779eb831d2fed37f8d56826564a4a728
Tnvr09 <rahmantanvir64@gmail.com>
2026-05-05 10:17:40 +0600

    Driver class: Fields & Constructors set

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -5,6 +5,17 @@ public class Driver {
     private boolean isAvailable;
 
     public Driver(int id, String name, String licencePlate) {
-
+        this.isAvailable = true;
     }
+
+    public Driver(int id, String name, String licencePlate, boolean isAvailable) {
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = isAvailable;
+    }
+
+
+
+
 }
