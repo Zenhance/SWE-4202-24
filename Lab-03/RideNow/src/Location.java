75500064f96274592d12f9962c6a1d69810462e1
sabbir240042147 <eftesabbir2725@gmail.com>
2026-05-05 12:49:39 +0600

    location

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,34 @@
+public class Location {
+    private String label;
+    private double x;
+    private double y;
+
+    Location(String label,double x,double y){
+        this.label=label;
+        this.x=x;
+        this.y=y;
+    }
+    Location(double x,double y){
+        this.x=x;
+        this.y=y;
+        label="Unknown";
+    }
+    public String getLabel(){
+        return label;
+    }
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public double distanceTo(Location other){
+        double x2=other.x-this.x;
+        double y2=other.y-this.y;
+        return Math.sqrt(x2*x2 + y2*y2);
+    }
+     public String toString(){
+        return label+ "("+x+","+y+")";
+    }
+
+}
