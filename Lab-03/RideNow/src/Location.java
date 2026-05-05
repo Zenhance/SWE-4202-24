1b5ebf47d0b24e39ca41cb9fe8dd77dde9a50373
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-05-05 10:07:35 +0600

    Created Fields(All private), constructors, methods.... Location file updated

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -6,33 +6,17 @@ public class Location {
     public String getLabel() {
         return label;
     }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
     public double getX() {
         return x;
     }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
     public double getY() {
         return y;
     }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Location(String label, double x, double y) {
         this.label = label;
         this.x = x;
         this.y = y;
     }
-
     public Location(double x, double y) {
         this.x = x;
         this.y = y;
