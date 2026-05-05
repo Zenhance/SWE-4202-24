a5543204525125ca4c0d799f0ae9f7dfa5516651
nusra <nusratnishi1103@gmail.com>
2026-05-05 12:47:52 +0600

    new

---
+++ Lab-03/RideNow/src/Passenger.java
@@ -0,0 +1,39 @@
+public class Passenger {
+    private int id;
+    private String name;
+    private double rating;
+
+    //full constructor
+    public Passenger(int id,String name,double rating){
+        this.id=id;
+        this.name=name;
+        this.rating=rating;
+    }
+
+    //convenience constructor
+    public Passenger(int id,String name){
+        this.id=id;
+        this.name=name;
+        this.rating=5.0;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getRating(){
+        return rating;
+    }
+
+    public void updateRating(double newRating){
+        this.rating=newRating;
+    }
+
+    public String toString(){
+        return String.format("Passenger[%d] %s (%.2f)",id,name,rating);
+    }
+}
