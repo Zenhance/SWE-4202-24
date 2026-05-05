2542e7fd2870a44bd4d83013aaed9b322c947493
Samin-init <mhsamin10@gmail.com>
2026-05-05 09:41:44 +0600

    1st Commit

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,47 @@
+public class Location {
+    private String label;
+    private double x;
+    private double y;
+
+    public Location(String label, double x,double y){
+        this.label = label;
+        this.x = x;
+        this.y = y;
+
+
+    }
+
+    public Location(double x, double y){
+        this.label = "Unknown";
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getLabel(){
+        return label;
+    }
+     public double getX(){
+        return x;
+     }
+    public double getY(){
+        return y;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
