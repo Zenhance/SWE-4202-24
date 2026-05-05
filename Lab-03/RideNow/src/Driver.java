ff2ac2e3534074ffa4c4287db6f5cc47b8062936
md-musaddik <musaddikibna1@gmail.com>
2026-05-05 10:26:47 +0600

    Driver class declared, variables declared and driver constructor declared.

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,14 @@
+public class Driver {
+    private int id;
+    private String name;
+    private String licencePlate;
+    private boolean isAvailable;
+
+    Driver(int id, String name, String licencePlate){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=true;
+    }
+
+}
