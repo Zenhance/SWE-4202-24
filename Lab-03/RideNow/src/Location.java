55889aad3439b4d739a95a679f3ae39c011e7180
nusra <nusratnishi1103@gmail.com>
2026-05-05 12:28:13 +0600

    new

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,41 @@
+public class Location {
+    private String label;
+    private double x;
+    private double y;
+
+    //full constructor
+    public Location(String label,double x,double y) {
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+
+    //convenience constructor
+    public Location(double x,double y) {
+        this.label = "Unknown";
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getLabel(){
+        return label;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public double distanceto(Location other){
+        double xx=other.x-this.x;
+        double yy=other.y-this.y;
+        return Math.sqrt(xx*xx + yy*yy);
+    }
+
+    public String toString(){
+        return String.format("%s (%.2lf,%.2lf)",label,x,y);
+    }
+        }
