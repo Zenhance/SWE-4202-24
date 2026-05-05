3904399feb6c926dbbe065028036ce71cb2cb6e6
mubashirulhasan-netizen <mubashirulhasan@iut-dhaka.edu>
2026-05-05 09:54:22 +0600

    commit 2

---
+++ Lab-03/RideNow/src/Passenger.java
@@ -0,0 +1,28 @@
+public class Passenger {
+    private int id;
+    private String name;
+    private double rating;
+
+
+    public Passenger(int id, String name, double rating){
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void updateRating(double newRating){
+        this.rating = newRating;
+    }
+}
