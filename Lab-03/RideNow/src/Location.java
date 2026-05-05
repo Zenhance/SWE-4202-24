d7123c362f05567ac13da4310c8942e9821e6390
inanjaki184 <inanjaki184@gmail.com>
2026-05-05 12:14:53 +0600

    partA

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,11 @@
+public class Location {
+    private String label;
+    double x;
+    double y;
+    public Location(String label, double x, double y){
+        this.x=x;
+        this.y=y;
+        this.label="Unknown";
+    }
+
+}
