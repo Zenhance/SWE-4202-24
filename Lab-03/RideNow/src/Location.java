c3d7f87e03047dc52b0738aced1eddbbaf21ec96
wasifjafar <wasifjafar71@gmail.com>
2026-05-05 12:44:41 +0600

    init commit

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,45 @@
+public class Location {
+
+    private String label;
+    private double x;
+    private double y;
+
+    Location(String label, double x, double y) {
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+
+    Location(double x, double y) {
+        this.label = "Unknown";
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double distanceTo(Location other) {
+        double x = other.getX();
+        double y = other.getY();
+
+        double distance = Math.sqrt( (this.x-x)*(this.x-x) + (this.y-y)*(this.y-y));
+
+        return distance;
+    }
+
+    public String toString() {
+        String string = label + " (" + x + ", " + y + ")";
+
+        return string;
+    }
+}
--- Lab-03/RideNow/src/Main.java
+++ Lab-03/RideNow/src/Main.java
@@ -2,14 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Location airport = new Location("Airport", 3.0, 7.5);
+
+        System.out.println("This is label: " + airport.getLabel());
+        System.out.println("This is x co-ordinate: " + airport.getX());
+        System.out.println("This is y co-ordinate: " + airport.getY());
+        System.out.println("This is  address: " + airport.toString());
+
+        Location iut = new Location(0.0, 0.0);
+
+        System.out.println("This is distance from IUT: " + airport.distanceTo(iut));
+
     }
 }
