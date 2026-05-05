e1741bd14fd3403c24957d234151f5bbf34c0110
arnoboic <arnobarafat@iut-dhaka.edu>
2026-05-05 09:58:58 +0600

    created Driver constructor

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -3,4 +3,11 @@ public class Driver {
     private String name;
     private String licencePlate;
     private boolean isAvailable;
+
+    public Driver (int id, String name, String licencePlate, boolean isAvailable){
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = isAvailable;
+    }
 }
