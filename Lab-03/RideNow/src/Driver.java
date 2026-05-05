1534dd955fb5ed3c2c0a9fdfbb96fe49a28d1ac3
arnoboic <arnobarafat@iut-dhaka.edu>
2026-05-05 10:00:58 +0600

    created second Driver constructor

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -10,4 +10,11 @@ public class Driver {
         this.licencePlate = licencePlate;
         this.isAvailable = isAvailable;
     }
+
+    public Driver(int id, String name, String licencePlate){
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        isAvailable = true;
+    }
 }
