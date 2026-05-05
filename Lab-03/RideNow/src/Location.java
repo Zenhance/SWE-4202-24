a0d4274b2cd92f6e8ad724ed56a8aeadf95affed
xenonono36-ctrl <xenonono36@gmail.com>
2026-05-05 10:30:20 +0600

    L2

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -1,4 +1,6 @@
+
 public class Location {
+
     private String label;
     double x;
     double y;
@@ -9,8 +11,32 @@ public class Location {
         this.y = y;
     }
 
-    public Location(double x, double y){
+    public Location(double x, double y) {
         this.x = x;
         this.y = y;
+        this.label = "Unknown";
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    double distanceTo(Location other) {
+        double x2 = other.x - this.x;
+        double y2 = other.y - this.y;
+        return Math.sqrt(x2*x2 + y2*y2);
+    }
+
+    public String toString() {
+        String ans = label + " (" + x + ", " + y + ")";
+        return ans;
     }
 }
