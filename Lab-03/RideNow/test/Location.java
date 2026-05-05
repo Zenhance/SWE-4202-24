2a189908a5de5bf90d6a9c5bd7fdeb5d4dc79842
elfie1502 <mumtahina24@iut-dhaka.edu>
2026-05-05 09:47:21 +0600

    git init

---
+++ Lab-03/RideNow/test/Location.java
@@ -0,0 +1,28 @@
+class Location {
+    private String label;
+    private double x;
+    private double y;
+
+    Location(String label, double x, double y){
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+
+    Location(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
