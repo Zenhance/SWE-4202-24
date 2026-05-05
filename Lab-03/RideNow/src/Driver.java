a42346fbba554d4180558d74b49384c1a678ddc1
mubashirulhasan-netizen <mubashirulhasan@iut-dhaka.edu>
2026-05-05 10:07:38 +0600

    commit 3

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,30 @@
+public class Driver {
+
+    private int id;
+    private String name;
+    private String licensePlate;
+    private boolean isAvailable;
+
+    public Driver(int id, String name, String licensePlate, boolean isAvailable){
+        this.id = id;
+        this.name = name;
+        this.licensePlate = licensePlate;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLicencePlate() {
+        return licensePlate;
+    }
+
+    public void setAvailable(boolean newBoolean){
+        this.isAvailable = newBoolean;
+    }
+}
