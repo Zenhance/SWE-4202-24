af94bfc2ad4ec5c41de7fc2ea4a2b8a4ab62b2db
tasnitaraida <tasnitar@gmail.com>
2026-05-05 19:29:49 +0600

    created constructor

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,14 @@
+public class Location {
+    private String label;
+    private double x;
+    private double y;
+
+    //Create Constructor
+    Location(String label, double x, double y) {
+        this.label=label;
+        this.x=x;
+        this.y=y;
+    }
+
+
+}
