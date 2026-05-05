fdc705c8cedc87ab4697408b7cc5ac0da7bfd947
16-safwan <safwanbhuiyan@iut-dhaka.edu>
2026-05-05 09:07:11 +0600

    git commit

--- Lab-02/task1/src/Withdraw.java
+++ Lab-02/task1/src/Withdraw.java
@@ -1,15 +1,14 @@
+
 //
 // Source code recreated from a .class file by IntelliJ IDEA
 // (powered by Fernflower decompiler)
 //
 
 public class Withdraw {
-    public Withdraw() {
-    }
 
     void Withdraw(BankAccount var1, double var2) {
         if (var2 > var1.balance) {
-            System.out.println(var1.owner + ":Insufficient funds");
+            System.out.println(var1.owner + ": Insufficient funds");
         } else {
             var1.balance -= var2;
         }
