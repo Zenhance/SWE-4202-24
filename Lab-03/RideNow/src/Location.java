62f7914b9450013818caa0bca783af32f2490de1
Sadman Raiyan <raiyansadman121@gmail.com>
2026-05-05 10:21:15 +0600

    Adding methods

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -15,4 +15,27 @@ public class Location {
         this.y=y;
         this.label="unknown";
     }
+
+    public String getLabel(){
+
+        return label;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public double distanceTo(Location other){
+        double x2 = other.x-this.x;
+        double y2 = other.y-this.y;
+        return Math.sqrt(x2+y2);
+    }
+
+    public String toString(){
+        return label+" ("+x+", "+y+")";
+    }
 }
