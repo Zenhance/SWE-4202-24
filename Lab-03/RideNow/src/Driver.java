7ad2ea6e9423fe8573ce5b81b29a0b47e636d8da
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-05-05 10:19:00 +0600

    added both constructors

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -2,12 +2,17 @@ public class Driver {
     private int id;
     private String name;
     private String licencePlate,
-    boolean isAvailable;
+    boolean isAvailable = true;
 
     Driver(int id, String name, String licencePlate, boolean isAvailable){
         this.id = id;
         this. name = name;
         this.licencePlate = licencePlate;
         this.isAvailable = isAvailable;
+    }
+    Driver(int id, String name, String licencePlate){
+        this.id = id;
+        this. name = name;
+        this.licencePlate = licencePlate;
     }
 }
