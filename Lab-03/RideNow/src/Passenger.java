f6ee688bf3a260395cf148a30ee8e4f107062aa2
Zunied-57 <zunied0411@gmail.com>
2026-05-05 10:17:14 +0600

    10th commit

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -21,29 +21,14 @@ class Passenger
         return id;
     }
 
-    public void setId(int id)
-    {
-        this.id = id;
-    }
-
     public String getName()
     {
         return name;
     }
 
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
     public double getRating()
     {
         return rating;
-    }
-
-    public void setRating(double rating)
-    {
-        this.rating = rating;
     }
 
     public void updateRating(double newRating)
