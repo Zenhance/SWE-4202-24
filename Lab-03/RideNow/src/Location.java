00da5d2c450afeffd0868931e794d465632fd866
tasnovatahia <tasnovatahia2014@gmail.com>
2026-05-05 09:39:32 +0600

    ini

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,25 @@
+public class Location {
+    private String label;
+    private double x;
+    private double y;
+
+    Location(String label,double x,double y){
+        this.label=label;
+        this.x=x;
+        this.y=y;
+    }
+    Location(double x,double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    String getLabel() {
+        return label;
+    }
+    double getX(){
+        return x;
+    }
+    double getY(){
+        return y;
+    }
+}
