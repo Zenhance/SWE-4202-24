29aecbd3007066ec6de536d838e6e97d4cc0ba8d
58-jahin <jahinhossain@iut-dhaka.edu>
2026-05-05 10:10:06 +0600

    driver java, final commit. takes input and matches the test run cases.

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,36 @@
+public class Driver {
+
+    private int id;
+    private String name;
+    private String licensePlate;
+    private boolean isAvailable;
+
+
+    public Driver(int id, String name, String LicensePlate, boolean isAvailable){
+        this.id=id;
+        this.name=name;
+        this.licensePlate = licensePlate;
+        this.isAvailable=isAvailable;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getLicencePlate() {
+        return licensePlate;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
