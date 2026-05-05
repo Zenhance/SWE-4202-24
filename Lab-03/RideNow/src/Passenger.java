a0bfff4dd47681ae37a2697e2d5621dabae360bd
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-05-05 09:54:32 +0600

    Created Fields(All private), constructors, methods

---
+++ Lab-03/RideNow/src/Passenger.java
@@ -0,0 +1,46 @@
+public class Passenger {
+    private int id;
+    private String name;
+    private double rating;
+
+    public Passenger(int id, String name, double rating) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public Passenger(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+    void updateRating(double newRating){
+
+    }
+    String toString(){
+
+    }
+}
