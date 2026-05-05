db59cf7e874f6a467c92a1d218fe57fc6fdcbdff
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-05-05 12:23:10 +0600

    initial commit

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,39 @@
+public class Location {
+    private String label;
+    private double x;
+    private double y;
+
+    public Location(String label, double x, double y) {
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Location(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double distanceTo() {
+
+    }
+
+    public String toString() {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
