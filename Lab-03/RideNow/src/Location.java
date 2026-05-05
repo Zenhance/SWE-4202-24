9568d7bd8062271a8dee2d871a9fe5b9db90c21c
hossdia <hossainmahdia22@gmail.com>
2026-05-05 12:53:26 +0600

    innit commit

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -1,14 +1,14 @@
-public class location{
+public class Location{
         private String label;
         private double x, y;
 
-        public location(String label, double x, double y){
+        public Location(String label, double x, double y){
             this.label = label;
             this.x = x;
             this.y = y;
         }
 
-        public location(double x, double y){
+        public Location(double x, double y){
             this.label = "UNKNOWN";
             this.x = x;
             this.y = y;
