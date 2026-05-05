7d6f18529f0795e0e2d19c139ed9af59002dd8cb
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-05-05 10:17:32 +0600

    Started part c

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+    private int id;
+    private String name;
+    private String licencePlate,
+    boolean isAvailable;
+
+    Driver(int id, String name, String licencePlate, boolean isAvailable){
+        this.id = id;
+        this. name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = isAvailable;
+    }
+}
