904e7cd08dd72fbe30b51ade9c812eb060da6f05
16-safwan <safwanbhuiyan@iut-dhaka.edu>
2026-05-05 10:12:16 +0600

    init commit

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,33 @@
+public class Location {
+    String label;//— a human-readable name (e.g. "Airport")
+    double x; //— east coordinate in kilometres
+    double y ;
+    public Location(double x, double y){
+
+
+    }
+
+    public void getLabel()
+    {
+
+
+    }
+    public void getX(){
+
+
+    }
+    public void getY(){
+
+    }
+    public String toString()
+    {
+
+        return "a";
+    }
+    double distanceTo(Location other){
+
+
+
+       return 9.5;
+    }
+}
