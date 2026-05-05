a1cd6df1c1ae57d529a93a4a1f85c1c169766444
Tnvr09 <rahmantanvir64@gmail.com>
2026-05-05 09:53:31 +0600

    Created necessary methods

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -3,13 +3,29 @@ public class Location {
     private double x;
     private double y;
 
-    public void Location(String label, double x, double y) {
+    public Location(String label, double x, double y) {
         this.label = label;
         this.x = x;
         this.y = y;
     }
 
-    public void Location(double x, double y) {
+    public Location(double x, double y) {
+        this.label = "Unknown";
+    }
 
+    public String getLabel() {
+        return label;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double distanceto(Location location) {
+        return Math.sqrt(Math.pow(this.x - location.x, 2) + Math.pow(this.y - location.y, 2));
     }
 }
