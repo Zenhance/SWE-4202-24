190ddb296c9ab9c7f08d2c8982d39935c990c29b
arnoboic <arnobarafat@iut-dhaka.edu>
2026-05-05 10:27:33 +0600

    Created getter methods for Trip class

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -3,6 +3,7 @@ public class Trip {
     private Passenger passenger;
     private Driver driver;
     private Location from;
+    private Location to;
     private double baseFare;
     private boolean completed;
 
@@ -13,6 +14,23 @@ public class Trip {
         driver.setAvailable(false);
     }
 
+    public Passenger getPassenger() {
+        return passenger;
+    }
 
+    public Driver getDriver(){
+        return driver;
+    }
 
+    public Location getFrom(){
+        return from;
+    }
+
+    public Location getTo(){
+        return to;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
 }
