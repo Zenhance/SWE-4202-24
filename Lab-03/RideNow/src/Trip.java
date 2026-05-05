6c3f083da97ab671915539b17b2cd3c87862a51e
tuireuit <jamilhasan@iut-dhaka.edu>
2026-05-05 13:11:40 +0600

    writing trip class

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -1,2 +1,9 @@
 public class Trip {
+
+    private Passenger passenger;
+private Driver driver;
+private Location from;
+private Location to;
+private double baseFare=10;
+private boolean completed=false;
 }
