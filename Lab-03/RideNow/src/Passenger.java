2e0a8ced84fb2fa24fff1430fde5661f260f00ab
wasifjafar <wasifjafar71@gmail.com>
2026-05-05 23:59:24 +0600

    init commit

---
+++ Lab-03/RideNow/src/Passenger.java
@@ -0,0 +1,40 @@
+public class Passenger {
+
+    private int id;
+    private String name;
+    private double rating;
+
+    Passenger(int id, String name, double rating) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+
+    Passenger(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.rating = 5.0f;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void updateRating(double newRating) {
+        this.rating = newRating;
+    }
+
+    public String toString() {
+        String string = String.format("Passenger[%d] %s (%.2f)", id, name, rating);
+
+        return string;
+    }
+}
