84fa15f8724b414d105eb7cf8265edd12bc274ac
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-05-05 10:18:14 +0600

    second constructor done

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -12,5 +12,12 @@ public class Driver {
         isAvailable = true;
     }
 
+    Driver(int id, String name, String licencePlate, boolean isAvailable){
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = isAvailable;
+    }
+
 
 }
