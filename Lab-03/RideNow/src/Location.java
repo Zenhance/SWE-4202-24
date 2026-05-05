615991b16f0d3b3ae44731ddfa9851002948fb27
Sadman Raiyan <raiyansadman121@gmail.com>
2026-05-05 09:24:26 +0600

    Adding constructors

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -4,5 +4,15 @@ public class Location {
     private double x;
     private double y;
 
+    Location(String label,double x,double y){
+        this.label=label;
+        this.x=x;
+        this.y=y;
+    }
 
+    Location(double x,double y){
+        this.x=x;
+        this.y=y;
+        this.label="unknown";
+    }
 }
