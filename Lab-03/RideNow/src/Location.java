3d17df75db25aaf369e03d69cdddf948b5ae2ba8
24-Raiyan <raiyansadman121@gmail.com>
2026-05-05 09:45:36 +0600

    innit commit

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,32 @@
+public class Location {
+
+    private String label;
+    private double x;
+    private double y;
+
+    public Location(){
+        this.label = null;
+        this.x = x;
+        this.y = y;
+    }
+
+    void getLabel(){
+
+    }
+
+    void getX(){
+
+    }
+
+    void getY(){
+
+    }
+
+    double distanceTo(){
+
+    }
+
+    String tostring(){
+
+    }
+}
