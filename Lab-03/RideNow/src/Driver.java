f7427f22364f2c817532846a8f2dc87bbc89ffa2
nusra <nusratnishi1103@gmail.com>
2026-05-05 12:58:41 +0600

    new

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+    private int id;
+    private String name;
+    private String licencePlate;
+    private boolean isAvailable;
+
+    public Driver(int id,String name,String licencePlate){
+
+    }
+}
