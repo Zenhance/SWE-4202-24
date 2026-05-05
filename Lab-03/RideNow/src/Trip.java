baebc2190203a317668c989d09254a054f793021
Zunied-57 <zunied0411@gmail.com>
2026-05-05 22:09:08 +0600

    18th commit

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -9,7 +9,14 @@ class Trip
 
     public Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare)
     {
+        this.passenger=passenger;
+        this.driver=driver;
+        this.from=from;
+        this.to=to;
+        this.baseFare=baseFare;
+        this.completed=false;
 
+        driver.setAvailable(false);
     }
 
     public double calculateFare()
