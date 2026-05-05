a744cc086d2ff4e72e267ccbf7a58967dc61eb13
Zunied-57 <zunied0411@gmail.com>
2026-05-05 10:17:57 +0600

    11th commit

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -22,28 +22,13 @@ class Location
         return label;
     }
 
-    public void setLabel(String label)
-    {
-        this.label = label;
-    }
-
     public double getX() {
         return x;
-    }
-
-    public void setX(double x)
-    {
-        this.x = x;
     }
 
     public double getY()
     {
         return y;
-    }
-
-    public void setY(double y)
-    {
-        this.y = y;
     }
 
     public double distanceTo()
