414b5122fadd6ee7c034098c123059e6c0f088c0
Muhtasim Moin <muhtasi13@gmail.com>
2026-05-06 02:07:06 +0600

    Trip class updated

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -12,5 +12,34 @@ public class Trip {
      this.from=from;
      this.to=to;
      this.baseFare=baseFare;
+     this.completed=false;
+     driver.setAvailable(false);
  }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public Location getFrom() {
+        return from;
+    }
+
+    public Location getTo() {
+        return to;
+    }
+
+    public double getBaseFare() {
+        return baseFare;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+    public double getDistance(){
+     return from.distanceTo(to);
+    }
 }
