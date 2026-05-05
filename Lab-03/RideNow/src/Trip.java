a2684e6f2e65420b9bd01e14abe5acaa801abdac
waseefmunir-max <waseef05@gmail.com>
2026-05-05 10:09:06 +0600

    Another commit

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -6,5 +6,11 @@ public class Trip {
     private double baseFare;
     private boolean completed;
 
-
+    public Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare, boolean completed) {
+        this.passenger = passenger;
+        this.driver = driver;
+        this.from = from;
+        this.to = to;
+        this.baseFare = baseFare;
+    }
 }
