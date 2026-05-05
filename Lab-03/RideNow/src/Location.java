0b1b5c2a0ecfcec5ff872606eb27a396421c7948
Tnvr09 <rahmantanvir64@gmail.com>
2026-05-05 09:26:25 +0600

    init commit

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,13 @@
+public class Location {
+    private String label;
+    private double x;
+    private double y;
+
+    public void Location(String label, double x, double y) {
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
