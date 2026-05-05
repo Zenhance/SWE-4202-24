209d793102d5dc7c440eaa4897acea2cbb470b29
Rakib <rakibulhasan4266@gmail.com>
2026-05-05 09:30:01 +0600

    Class created

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,12 @@
+public class Location
+{
+    private String label;
+    private double x;
+    private double y;
+}
+public Location(String x,double x,double y)
+{
+    this.label = label;
+    this.x = x;
+    this.y = y;
+}
