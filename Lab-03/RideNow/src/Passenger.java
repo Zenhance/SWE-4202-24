526eb91e03a1bd7b28d6606b8b26c170ad562d90
ahnaf-code <ahnafcode@gmail.com>
2026-05-05 12:58:36 +0600

    Passenger Commit 1

---
+++ Lab-03/RideNow/src/Passenger.java
@@ -0,0 +1,39 @@
+public class Passenger {
+
+    private int id;
+    private String name;
+    private double rating;
+
+    public Passenger(int id, String name){
+        this.id = id;
+        this.name = name;
+        this.rating = 5;
+    }
+
+    public Passenger(int id, String name, double rating){
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+
+    void updateRating (double newRating) {
+        rating = newRating;
+    }
+
+    // String toString() {
+      //  return
+    // }
+}
