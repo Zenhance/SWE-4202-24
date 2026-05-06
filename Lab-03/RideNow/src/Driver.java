0523c7f8cf298ef0610248893b7cd0d19c63a178
04-Labiba <sabihalabiba639@gmail.com>
2026-05-06 19:16:00 +0600

    init commit

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -9,4 +9,10 @@ public class Driver {
         this.licencePlate=licencePlate;
         this.isAvailable=true;
     }
+    public Driver(int id,String name,String licencePlate,boolean isAvailable){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=isAvailable;
+    }
 }
