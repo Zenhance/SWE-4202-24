5ba406bfdbfa993e0989fd5f2bbdb442a732bebf
glockmachigan <muniruzzaman24@iut-dhaka.edu>
2026-05-06 21:55:07 +0600

    adding Passenger

---
+++ Lab-03/RideNow/src/Passenger.java
@@ -0,0 +1,12 @@
+public class Passenger {
+    private int id;
+    private String name;
+    private double rating;
+
+    Passenger(int id, String name, double rating){
+        this.id = id ;
+        this.name = name;
+        this.rating = rating ;
+    }
+
+}
