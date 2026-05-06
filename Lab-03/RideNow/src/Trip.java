39e88dd830b41f89c8f1e76e3410e94e1659c565
tasnovatahia <tasnovatahia2014@gmail.com>
2026-05-06 22:23:57 +0600

    commit Passenger class

---
+++ Lab-03/RideNow/src/Trip.java
@@ -0,0 +1,18 @@
+public class Trip {
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare;
+    private boolean completed;
+
+    Trip(Passenger passenger,Driver driver,Location from,Location to,double basefare){
+        this.passenger=passenger;
+        this.driver=driver;
+        this.from=from;
+        this.to=to;
+        this.baseFare=baseFare;
+        this.completed=completed;
+    }
+
+}
