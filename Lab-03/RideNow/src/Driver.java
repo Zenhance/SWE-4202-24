e5a4039548e02b012ef71a24740446be74c8f7d0
inan <inanjaki184@gmail.com>
2026-05-06 15:57:02 +0600

    partC

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -1,21 +1,34 @@
 public class Driver {
-    private String id;
+    private int id;
     private String name;
     private String licencePlate;
     private boolean isAvailable;
 
-    public Driver(String id, String name, String licencePlate) {
+    public Driver(int id, String name, String licencePlate) {
         this.id = id;
         this.name = name;
         this.licencePlate = licencePlate;
         this.isAvailable = true;
     }
 
-    public Driver(String id, String name, String licencePlate, boolean isAvailable) {
+    public Driver(int id, String name, String licencePlate, boolean isAvailable) {
         this.id = id;
         this.name = name;
         this.licencePlate = licencePlate;
         this.isAvailable = isAvailable;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
     }
 
 
