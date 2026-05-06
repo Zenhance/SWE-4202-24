a1d23798c76b558054984cfca05a2b1e2f27c4a3
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-06 22:46:03 +0600

    init commit

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -4,8 +4,12 @@ public class Driver {
     private String licencePlate;
     private boolean isAvailable;
 
-
-
+    public Driver(int id,String name,String licencePlate,boolean isAvailable){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=isAvailable;
+    }
 
 
 
