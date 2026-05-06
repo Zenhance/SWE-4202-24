69d8b12d738ba1801fd9a217ae6bb31efbd98121
04-Labiba <sabihalabiba639@gmail.com>
2026-05-06 19:02:33 +0600

    init commit

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -10,5 +10,12 @@ public class Passenger {
     public Passenger(int id,String name){
         this.id=id;
         this.name=name;
+        this.rating=5.0;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
     }
 }
