bcd2b4292d9904067fb285e17e74e5780d18f0ce
Sadman Raiyan <raiyansadman121@gmail.com>
2026-05-06 21:35:02 +0600

    Adding getters methods

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -16,6 +16,28 @@ public class Trip {
         this.completed=false;
     }
 
+    public Passenger getPassenger(){
+        return passenger;
+    }
 
+    public Driver getDriver(){
+        return driver;
+    }
+
+    public Location getFrom(){
+        return from;
+    }
+
+    public Location getTo(){
+        return to;
+    }
+
+    public double getBaseFare(){
+        return baseFare;
+    }
+
+    public boolean isCompleted(){
+        return completed;
+    }
 
 }
