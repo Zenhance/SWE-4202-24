04189cfe61a89b5898af8fd2d23d222fac5ed004
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-05-06 19:12:03 +0600

    added private fields to the class Trip

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -1,2 +1,9 @@
 public class Trip {
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double basefare;
+    private boolean completed = false;
+
 }
