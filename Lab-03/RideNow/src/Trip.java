f1c34efdebe464d1f667b1068a844e28233e489b
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-05-07 03:19:25 +0600

    writing trip class

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -1,9 +1,22 @@
 public class Trip {
 
-    private Passenger passenger;
-private Driver driver;
-private Location from;
-private Location to;
-private double baseFare=10;
-private boolean completed=false;
+          private Passenger passenger;
+          private Driver driver;
+          private Location from;
+          private Location to;
+          private double baseFare=10;
+          private boolean completed=false;
+
+
+
+public Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare){
+
+    this.passenger=passenger;
+    this.driver=driver;
+    this.from=from;
+    this.to=to;
+    this.baseFare=baseFare;
+    completed=false;
+    driver.setAvailable(false);
+}
 }
