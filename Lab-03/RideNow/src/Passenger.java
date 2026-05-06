b1f2a8e530735e8aa29f9c673827375186b1ef5c
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-06 23:10:28 +0600

    init commit

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -24,7 +24,7 @@ public void updateRating(double newRating){
     this.rating = newRating;
 }
 public String toString(){
-    String res= "Passenger["+id+"]"+name+"("+String.format("%.2f",rating)+")";
-    return res;
+    String status= "Passenger["+id+"]"+name+"("+String.format("%.2f",rating)+")";
+    return status;
 }
 }
