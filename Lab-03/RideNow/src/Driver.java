271848d4d924f949ea1835c7e9421ab5022daa32
anonymous <farnaz28.rahman@gmail.com>
2026-05-07 00:15:49 +0600

    changed typing error

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -6,12 +6,12 @@ public class Driver {
 
     Driver(int id,String name,String licensePlate){
         this.id=id;
-        this.licensePlate=licensePlate;
+        this.licencePlate=licencePlate;
         this.name=name;
     }
-    Driver(int id,String name,String licensePlate,boolean isAvailable){
+    Driver(int id,String name,String licencePlate,boolean isAvailable){
         this.id=id;
-        this.licensePlate=licensePlate;
+        this.licencePlate=licencePlate;
         this.name=name;
         this.isAvailable=isAvailable;
     }
@@ -25,18 +25,18 @@ public String getName(){
 public boolean isAvailable(){
         return isAvailable;
 }
-public String getLicensePlate(){
-        return licensePlate;
+public String getLicencePlate(){
+        return licencePlate;
 }
 public void setAvailable(boolean isAvailable){
         this.isAvailable=isAvailable;
 }
 public String toString(){
         if(isAvailable) {
-            return "Driver" + "[" + id + "]" + name + "(" + licensePlate + ")" + "[AVAILABLE]";
+            return "Driver" + "[" + id + "]" + name + "(" + licencePlate + ")" + "[AVAILABLE]";
         }
         else{
-            return "Driver" + "[" + id + "]" + name + "(" + licensePlate + ")" + "[BUSY]";
+            return "Driver" + "[" + id + "]" + name + "(" + licencePlate + ")" + "[BUSY]";
         }
 }
 
