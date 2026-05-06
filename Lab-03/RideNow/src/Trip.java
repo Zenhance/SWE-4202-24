0efe59d3238f4af46d0f4b7520bbdb82d2d3e617
anonymous <farnaz28.rahman@gmail.com>
2026-05-06 20:13:38 +0600

    constructor

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -1,2 +1,16 @@
 public class Trip  {
+   private Passenger passenger;
+   private Driver driver;
+   private location from;
+   private location to;
+   private double baseFare;
+   private boolean completed = false;
+    Trip(Passenger passenger,Driver driver,location from,location to,double baseFare){
+        this.passenger=passenger;
+        this.driver=driver;
+        this.from=from;
+        this.to=to;
+        this.baseFare=baseFare;
+    }
+
 }
