e8f14e2e802d00575572d3a9658cb110b281a773
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-06 23:07:48 +0600

    init commit

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -5,36 +5,46 @@ public class Driver {
     private boolean isAvailable;
 
     //available constructor
-    public Driver(int id,String name,String licencePlate) {
+    public Driver(int id, String name, String licencePlate) {
         this(id, name, licencePlate, true);
     }
+    //full constructor
+    public Driver(int id, String name, String licencePlate, boolean isAvailable) {
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = isAvailable;
+    }
 
-    public Driver(int id,String name,String licencePlate,boolean isAvailable){
-        this.id=id;
-        this.name=name;
-        this.licencePlate=licencePlate;
-        this.isAvailable=isAvailable;
+    //getters
+    public int getId() {
+        return id;
     }
-    public int getId(){
-    return id;
+
+    public String getName() {
+        return name;
     }
-    public String getlicencePlate(){
-    return licencePlate;
+
+    public String getLicencePlate() {
+        return licencePlate;
     }
-    public boolean isAvailable(){
-    return isAvailable;
+
+    public boolean isAvailable() {
+        return isAvailable;
     }
-    public void setAvailable(boolean available){
-    this.isAvailable=available;
+
+    //setters
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
     }
-    public String toString(){
-    String status = "Driver["+id+"]"+name+"("+licencePlate");
-                if(isAvailable==true){
-                    status=status+"[AVAILABLE]";
-                }
-                else{
-                    status=status+"[BUSY]";
-                }
-                return status;
+
+    public String toString() {
+        String status = "Driver[" + id + "]" + name + "(" + licencePlate + ");
+        if (isAvailable == true) {
+            status = status + "[AVAILABLE]";
+        } else {
+            status = status + "[BUSY]";
+        }
+        return status;
     }
-    }
+}
