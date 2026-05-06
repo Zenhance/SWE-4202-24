4adaf05b3b60481ae2b5cace9153b4bc5383e99b
anonymous <farnaz28.rahman@gmail.com>
2026-05-06 14:56:01 +0600

    constructor

---
+++ Lab-03/RideNow/src/location.java
@@ -0,0 +1,11 @@
+public class location {
+    private String[] label;
+    private double x;
+    private double y;
+
+    public void Location(String label,double x,double y){
+        this.label=label;
+        this.x=x;
+        this.y=y;
+    }
+}
