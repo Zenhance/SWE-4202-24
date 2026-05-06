6a55476679ebdf310f40c655ce2f54d587223ac8
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-06 23:30:35 +0600

    init commit

---
+++ Lab-03/RideNow/src/Trip.java
@@ -0,0 +1,26 @@
+public class Trip {
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare;
+    private boolean completed;
+
+    public Trip(Passenger passenger, Driver drive, Location from, Location to, double baseFare) {
+        this.passenger = passenger;
+        this.driver = driver;
+        this.from = from;
+        this.to = to;
+        this.baseFare = baseFare;
+        this.completed = false;
+    }
+    driver.setAvailable(false);
+}
+public Passenger getPassenger() {
+    return passenger;
+}
+
+
+
+
+}
