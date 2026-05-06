78a4406c536066eb47d19785a8325e66ff3235f2
inan <inanjaki184@gmail.com>
2026-05-06 15:52:13 +0600

    partC

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -4,5 +4,19 @@ public class Driver {
     private String licencePlate;
     private boolean isAvailable;
 
+    public Driver(String id, String name, String licencePlate) {
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = true;
+    }
+
+    public Driver(String id, String name, String licencePlate, boolean isAvailable) {
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = isAvailable;
+    }
+
 
 }
