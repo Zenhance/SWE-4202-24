5799ba6d43e9492f7fa9ee1eec6f731d5d59a4fe
04-Labiba <sabihalabiba639@gmail.com>
2026-05-06 19:33:46 +0600

    init commit

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -1,2 +1,8 @@
 public class Trip {
+    private Passenger passenger;
+    private Driver driver;
+    private Location location;
+    private Location to;
+    private double baseFare;
+    private boolean completed;
 }
