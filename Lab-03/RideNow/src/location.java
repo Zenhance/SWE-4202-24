a0dd7748b1472f033ad7099d63e5679843e24424
anonymous <farnaz28.rahman@gmail.com>
2026-05-06 15:08:35 +0600

    Error fixing

--- Lab-03/RideNow/src/location.java
+++ Lab-03/RideNow/src/location.java
@@ -1,11 +1,13 @@
 public class location {
-    private String[] label;
-    private double x;
-    private double y;
+     private String label;
+     private double x;
+     private double y;
 
-    public void Location(String label,double x,double y){
+    public location(String label,double x,double y){
         this.label=label;
         this.x=x;
         this.y=y;
     }
+
+
 }
