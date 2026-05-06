1f7483620ea7a42626c7e7d65e0c8ced22f62c36
anonymous <farnaz28.rahman@gmail.com>
2026-05-06 21:29:09 +0600

    methods

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -14,5 +14,34 @@ public class Trip  {
         this.completed=false;
         driver.setAvailable(false);
     }
+    Passenger getPassenger(){
+        return this.passenger;
+    }
+    Driver getDriver(){
+        return this.driver;
+    }
+    location getFrom(){
+        return this.from;
+    }
+    location getTo(){
+        return this.to;
+    }
+    double busFare(){
+        return this.baseFare;
+    }
+    boolean isCompleted(){
+        return this.completed;
+    }
+    double getDistance(){
+        return from.distanceTo(this.to);
+    }
+    double calculateFare(){
+        return baseFare+15.0*getDistance();
+    }
+    void completeTrip(){
+        if(this.completed){
+            driver.setAvailable(true);
+        }
+    }
 
 }
