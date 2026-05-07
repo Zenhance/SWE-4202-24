1442b38b4777253e84dd800c8250b09ea26b38c2
shafin-75 <arafatshafin85@gmail.com>
2026-05-08 02:09:54 +0600

    add convinience location class and add getlabel,getX and getY methods

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -6,4 +6,18 @@ public class Location {
         this.x=x;
         this.y=y;
     }
+    Location(double x, double y){
+        this.label="Unknown";
+        this.x=x;
+        this.y=y;
+    }
+    String getLabel(){
+        return label;
+    }
+    double getX(){
+        return x;
+    }
+    double getY(){
+        return y;
+    }
 }
