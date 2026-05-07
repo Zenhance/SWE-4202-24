db6c2e151466f6ce2b1ba8c70dc5b756e466c2af
Zunied-57 <zunied0411@gmail.com>
2026-05-07 19:28:48 +0600

    20th commit

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -7,17 +7,22 @@ class Driver
 
     public Driver(int id, String name, String licencePlate)
     {
-
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
     }
 
     public Driver(int id, String name, String licencePlate, boolean isAvailable)
     {
-
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=isAvailable;
     }
 
     public void setAvailable(boolean available)
     {
-        this.isAvailable=available;
+        isAvailable=available;
     }
 
     public String toString()
