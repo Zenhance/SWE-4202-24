0412090d42dd086482985d3d9b1f8d539124d8cf
sabbir240042147 <eftesabbir2725@gmail.com>
2026-05-07 21:54:10 +0600

    passenger class

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -2,5 +2,29 @@ public class Passenger {
     private int id;
     private String name;
     private double rating;
-
+    Passenger(int id,String name,double rating){
+        this.id=id;
+        this.name=name;
+        this.rating=rating;
+    }
+    Passenger(int id,String name){
+        this.id=id;
+        this.name=name;
+        this.rating=5.0;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getRating(){
+        return rating;
+    }
+    public void updateRating(double newRating){
+        this.rating=newRating;
+    }
+    public String toString(){
+        return "Passenger["+this.id+"]"+this.name+"("+this.rating+")";
+    }
 }
