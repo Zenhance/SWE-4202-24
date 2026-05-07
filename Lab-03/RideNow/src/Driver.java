fc8f968f5bf79fd46e5daaec38fd958901d09fe4
sabbir240042147 <eftesabbir2725@gmail.com>
2026-05-07 22:22:30 +0600

    driver class

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,35 @@
+public class Driver {
+    private int id;
+    private String name;
+    private String licencePlate;
+    private boolean isAvailable;
+
+
+    Driver(int id,String name,String licencePlate){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=true;
+    }
+    Driver(int id,String name,String licencePlate,boolean isAvailable){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=isAvailable;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getLicencePlate(){
+        return licencePlate;
+    }
+    public void setAvailable(boolean available){
+        this.isAvailable=available;
+    }
+    public String toString(){
+        return "Driver["+this.id+"] "+this.name+" ("+this.licencePlate+") ["+isAvailable +"]";
+    }
+}
