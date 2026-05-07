af68f743ab82703ca6016115151bfca68d0772d2
ahnaf-code <ahnafcode@gmail.com>
2026-05-07 22:33:54 +0600

    Location.java

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,47 @@
+public class Location {
+    private String label;
+    private double x;
+    private double y;
+
+    public Location(String label, double x, double y) {
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public Location(double x, double y) {
+        this.label = "Unknown";
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public String getLabel() {
+        return label;
+    }
+
+
+    public double getX() {
+        return x;
+    }
+
+
+    public double getY() {
+        return y;
+    }
+
+
+    public double distanceTo(Location other) {
+        double dx = other.x - this.x;
+        double dy = other.y - this.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+
+    public String toString() {
+        return label + " (" + String.format("%.2f", x) + ", " + String.format("%.2f", y) + ")";
+    }
+
+
+}
