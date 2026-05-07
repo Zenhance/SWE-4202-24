686020afcbeb80ea55549c8f8a6f969eb01c6841
shafin-75 <arafatshafin85@gmail.com>
2026-05-08 01:42:31 +0600

    add attributes and location constructor of location class

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -2,6 +2,8 @@ public class Location {
     String label;
     double x,y;
     Location(String label,double x, double y){
-        
+        this.label=label;
+        this.x=x;
+        this.y=y;
     }
 }
