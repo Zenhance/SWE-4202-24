5ff4206908660b640d68ed0d2b127a8aec351d71
shafin-75 <arafatshafin85@gmail.com>
2026-05-08 02:24:54 +0600

    add attributes and all constructors of passenger class

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -1,2 +1,17 @@
+import java.util.Formatter;
+
 public class Passenger {
+    int id;
+    double rating;
+    String name;
+    Passenger(int id, String name, double rating){
+        this.id=id;
+        this.name=name;
+        this.rating=rating;
+    }
+    Passenger(int id, String name){
+        this.id=id;
+        this.name=name;
+        this.rating=5.0;
+    }
 }
