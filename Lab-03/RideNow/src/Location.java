e7f67c43b7e821ef6a88d564d8a06acba75382b6
Tasauf Abdullah <tasaufalter@gmail.com>
2026-05-07 14:59:40 +0600

    Add constructor 2

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -9,11 +9,12 @@ public class Location {
         this.y = y;
     }
 
-    Location(String label, double x, double y){
+    Location(double x, double y){
         this.label = "Unknown";
         this.x = x;
         this.y = y;
     }
+
 
 
 
