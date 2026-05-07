fe2886841932cbfba5fe43aaa4041c9c921a8bb2
shafin-75 <arafatshafin85@gmail.com>
2026-05-08 03:00:35 +0600

    add all fields and constructors of trip class

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -1,2 +1,17 @@
 public class Trip {
+    Passenger passenger;
+    Driver driver;
+    Location from;
+    Location to;
+    double baseFare;
+    boolean completed;
+    Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare){
+        this.passenger=passenger;
+        this.driver=driver;
+        this.from=from;
+        this.to=to;
+        this.baseFare=baseFare;
+        this.completed=false;
+        driver.setAvailable(false);
+    }
 }
