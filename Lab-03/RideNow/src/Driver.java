6decee6d022d1fd6cb8097d5b9406e046c73cb7e
shafin-75 <arafatshafin85@gmail.com>
2026-05-08 02:36:38 +0600

    add attributes and all constructors of Driver class

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -1,2 +1,18 @@
 public class Driver {
+    int id;
+    String name;
+    String licencePlate;
+    boolean isAvailable;
+    Driver(int id, String name, String licencePlate){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=true;
+    }
+    Driver(int id, String name, String licencePlate, boolean isAvailable){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=isAvailable;
+    }
 }
