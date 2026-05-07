38ffc14a46ed28066b4b3271ce9e3fcc6117314e
Tasauf Abdullah <tasaufalter@gmail.com>
2026-05-07 23:31:30 +0600

    Add constructors 'driver'

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -4,5 +4,20 @@ public class Driver {
     private String licensePlate;
     private boolean isAvailable;
 
+    public Driver(int id, String name, String licencePlate){
+        this.isAvailable = true;
+        this.id = id;
+        this.name = name;
+        this.licensePlate = licencePlate;
+    }
+
+    public Driver(int id, String name, String licencePlate, boolean isAvailable){
+        this.isAvailable = isAvailable;
+        this.id = id;
+        this.name = name;
+        this.licensePlate = licencePlate;
+
+    }
+
 
 }
