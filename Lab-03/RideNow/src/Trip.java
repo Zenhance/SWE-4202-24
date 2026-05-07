2ba042b8175299dee473c0f6c2a4420b223af103
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-05-07 16:32:06 +0600

    declared the private fields.

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -1,5 +1,11 @@
 public class Trip {
 
+private Passenger passenger;
+private Driver driver;
+private Location from;
+private Location to;
+private double baseFare; // a fixed per-trip charge in BDT.
+private boolean completed;
 
 
 }
