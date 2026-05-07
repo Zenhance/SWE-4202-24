3f88743d70ccc0ecc0dc2deee74494250fc89966
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-05-07 21:14:59 +0600

    trip class method completion

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -1,106 +1,95 @@
 public class Trip {
 
-          private Passenger passenger;
-          private Driver driver;
-          private Location from;
-          private Location to;
-          private double baseFare=10;
-          private boolean completed=false;
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare = 10;
+    private boolean completed = false;
 
 
+    public Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare) {
 
-public Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare){
-
-    this.passenger=passenger;
-    this.driver=driver;
-    this.from=from;
-    this.to=to;
-    this.baseFare=baseFare;
-    completed=false;
-    driver.setAvailable(false);
-}
-
-public Passenger getPassenger(){
-
-    return passenger;
-}
-
-public Driver getDriver(){
-
-    retrun driver;
-}
-
-public Location getFrom(){
-
-        retrun from;
+        this.passenger = passenger;
+        this.driver = driver;
+        this.from = from;
+        this.to = to;
+        this.baseFare = baseFare;
+        completed = false;
+        driver.setAvailable(false);
     }
 
-public Location getTo(){
+    public Passenger getPassenger() {
 
-        retrun to;
+        return passenger;
     }
 
-public double getBaseFare(){
-    return baseFare;
-}
+    public Driver getDriver() {
+
+        return driver;
+    }
+
+    public Location getFrom() {
+
+        return from;
+    }
+
+    public Location getTo() {
+
+        return to;
+    }
+
+    public double getBaseFare() {
+        return baseFare;
+    }
 
 
-public boolean isCompleted(){
+    public boolean isCompleted() {
         return completed;
     }
 
 
-public getDistance(){
-    return from.distanceTo(to);
-}
+    public double getDistance() {
+        return from.distanceTo(to);
+    }
 
-public double calculateFare(){
+    public double calculateFare() {
 
-    double fare = baseFare + (15.0*getDistance());
-    return fare;
+        double fare = baseFare + (15.0 * getDistance());
+        return fare;
 
-}
+    }
 
-public void completeTrip(){
+    public void completeTrip() {
 
-    completed=true;
-    driver.setAvailable(true);
-}
+        completed = true;
+        driver.setAvailable(true);
+    }
 
-public String getSummary(){
+    public String getSummary() {
 
-    String status;
+        String status;
 
-    if(completed){status= "COMPLETED";}
-    else {status="IN PROGRESS";}
-
-
-    String res=String.format(
-
-            "Trip Summary\n"+
-            "   Passenger   : %s\n" +
-            "   Driver      : %s (%s)\n"+
-            "   From        :%s\n" +
-            "   To          :%s\n" +
-            "   Distance    :%.2f km\n"+
-            "   Fare        :BDT %.2f\n"+
-            "   Status      :%s",passenger.getName(),driver.getName(),
+        if (completed) {
+            status = "COMPLETED";
+        } else {
+            status = "IN PROGRESS";
+        }
 
 
+        String res = String.format(
 
+                "Trip Summary\n" +
+                        "   Passenger   : %s\n" +
+                        "   Driver      : %s (%s)\n" +
+                        "   From        : %s\n" +
+                        "   To          : %s\n" +
+                        "   Distance    : %.2f km\n" +
+                        "   Fare        : BDT %.2f\n" +
+                        "   Status      : %s", passenger.getName(), driver.getName(), driver.getLicencePlate(), from.toString(), to.toString(), getDistance(), calculateFare(), status);
+        return res;
 
-
-
-
-
-}
-
-
-
-
-
-
-
-
+    }
 
 }
+
