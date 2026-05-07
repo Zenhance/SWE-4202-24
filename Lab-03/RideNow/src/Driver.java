a5c56a323d617a485b46f11a02f8dadcabff73b5
ahnaf-code <ahnafcode@gmail.com>
2026-05-07 22:40:54 +0600

    Updated Driver.java

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -4,17 +4,52 @@ public class Driver {
     private String licencePlate;
     private boolean isAvailable;
 
-    Driver(int id, String name, String licencePlate) {
+    public Driver(int id, String name, String licencePlate) {
         this.id = id;
         this.name = name;
         this.licencePlate = licencePlate;
         this.isAvailable = true;
     }
 
-    Driver(int id, String name, String licencePlate, boolean isAvailable) {
+    public Driver(int id, String name, String licencePlate, boolean isAvailable) {
         this.id = id;
         this.name = name;
         this.licencePlate = licencePlate;
         this.isAvailable = isAvailable;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public String toString() {
+        String status;
+
+        if (isAvailable) {
+            status = "[AVAILABLE]";
+        }
+
+        else {
+            status = "[BUSY]";
+        }
+
+        return "Driver[" + id + "] " + name + " (" + licencePlate + ") " + status;
+    }
+
 }
