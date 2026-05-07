b68809ca0a2b71d55494c048fcc8a0588fc40e74
mahibrahaman175-stack <mahibrahaman175@gmail.com>
2026-05-08 05:37:33 +0600

    passenger done!

---
+++ Lab-03/RideNow/src/Passenger.java
@@ -0,0 +1,39 @@
+public class Passenger {
+    private int id;
+    private String name;
+    private double rating;
+
+    public Passenger(int id, String name, double rating){
+        this.id=id;
+        this.name=name;
+        this.rating=rating;
+    }
+
+    public Passenger(int id, String name){
+        this.id=id;
+        this.name=name;
+        this.rating=5.0;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void updateRating(){
+         double newRating=rating;
+    }
+
+    public String toString(){
+        String information="Passenger"+"["+id+"]"+name+"("+rating+")";
+        return information;
+    }
+}
