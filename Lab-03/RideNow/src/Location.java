f7a9de74cd996ea69988401e5bf07af37b3f9fb1
Tasauf Abdullah <tasaufalter@gmail.com>
2026-05-07 14:54:39 +0600

    Add location 1

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -9,23 +9,6 @@ public class Location {
         this.y = y;
     }
 
-    Location(double x, double y){
-        this.x = x;
-        this.y = y;
-        this.label = "Unknown";
-    }
-
-    public String getLabel(){
-        return this.label;
-    }
-
-    public double getX(){
-        return this.x;
-    }
-
-    public double getY(){
-        return this.y;
-    }
 
 
 }
