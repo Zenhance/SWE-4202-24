fa24df24a1c8cc162f70f74dbec67f2e4f4b25c6
Tasauf Abdullah <tasaufalter@gmail.com>
2026-05-07 15:45:35 +0600

    add distanceTo method

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -1,19 +1,47 @@
+
+import java.lang.classfile.Label;
+import java.lang.math;
+
+
 public class Location {
     private String label;
     double x;
     double y;
 
-    Location(String label, double x, double y){
+    public Location(String label, double x, double y){
         this.label = label;
         this.x = x;
         this.y = y;
     }
 
-    Location(double x, double y){
+    public Location(double x, double y){
         this.label = "Unknown";
         this.x = x;
         this.y = y;
     }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public double distanceTo(Location other){
+        double dx = other.x - this.x;
+        double dy = other.y - this.y;
+        double distance;
+
+        distance = Math.sqrt(dx*dx + dy*dy);
+
+        return distance;
+    }
+
 
 
 
