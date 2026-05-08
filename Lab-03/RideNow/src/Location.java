90ab6ca72d6bb60d63db06437341efa48b4928ac
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-05-08 22:38:37 +0600

    added part A fields

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,13 @@
+public class Location {
+
+    //a human readable name
+    private String label;
+
+    //east coordinate in km
+    private double x;
+
+    //north coordinates in km
+    private double y;
+
+
+}
