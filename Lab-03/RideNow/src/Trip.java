115a1b94a1e9a7331fb80919fd29905412e8cc00
40sadeed <sadeedanwar@iut-dhaka.edu>
2026-05-08 15:14:20 +0600

    constructor added

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -6,5 +6,14 @@ public class Trip {
     private double baseFare;
     private boolean completed;
 
-
+    Trip(Passenger passenger, Driver driver, Location from, Location to, double
+            baseFare){
+        this.passenger = passenger;
+        this.driver = driver;
+        this.from = from;
+        this.to = to;
+        this.baseFare = baseFare;
+        this.completed = false;
+        driver.setAvailable(false);
+    }
 }
