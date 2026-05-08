76edac6dc2d2f943e4725a58e9cdcf27957d9621
elfie1502 <mumtahina@iut-dhaka.edu>
2026-05-08 21:43:40 +0600

    git init

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -1,21 +1,54 @@
-class Driver {
+ class Driver {
     private int id;
     private String name;
     private String licencePlate;
     private boolean isAvailable;
 
-    public Driver(int id, String name,String licencePlate ) {
+    public Driver(int id, String name, String licencePlate) {
         this.id = id;
         this.name = name;
         this.licencePlate = licencePlate;
         this.isAvailable = true;
     }
 
-    public Driver(int id, String name, String licencePlate, boolean isAvailable){
+    public Driver(int id, String name, String licencePlate, boolean isAvailable) {
         this.id = id;
         this.name = name;
         this.licencePlate = licencePlate;
-
+        this.isAvailable = isAvailable;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
+    }
+
+    String x;
+
+    @Override
+    public String toString() {
+        if (isAvailable) {
+
+            return "Driver[" + id + "]" + name + "(" + licencePlate +") [ AVAILABLE ]";
+
+        } else {
+            return "Driver[" + id + "]" + name + "(" + licencePlate + ") [ BUSY ]";
+
+        }
+    }
 }
--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -3,12 +3,11 @@ class Location {
     private double x;
     private double y;
 
-    Location(String label, double x, double y){
+    Location(String label, double x, double y) {
         this.label = label;
         this.x = x;
         this.y = y;
     }
-
 
 
     Location(double x, double y){
