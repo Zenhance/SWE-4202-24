12a1f2145f2d74bdac565ec33ce242b436e109e2
40sadeed <sadeedanwar@iut-dhaka.edu>
2026-05-08 14:20:13 +0600

    constructors added and variables declared

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,22 @@
+public class Driver {
+
+    private int id;
+    private String name;
+    private String licencePlate;
+    private boolean isAvailable;
+
+    public Driver(int id, String name, String licencePlate){
+
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        isAvailable = true;
+    }
+
+    public Driver(int id, String name, String licencePlate, boolean isAvailable){
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = isAvailable;
+    }
+}
