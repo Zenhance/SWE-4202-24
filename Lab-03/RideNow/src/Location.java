8fd9cefc7d65c3b09c86dc133dd049e49e56d076
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-05-08 22:33:12 +0600

    location disTo

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -11,17 +11,36 @@ public class Location {
 
     }
 
+    public Location(double x, double y){
+        this.label="Unknown";
+        this.x=x;
+        this.y=y;
+    }
+
     public String getLabel(){
         return label;
     }
-    //getter banai
-    public double getX(){
+
+    public double getX()
+    {
         return x;
     }
 
     public double getY(){
         return y;
     }
+
+    public double distanceTo(Location other){
+        double dx =other.x-this.x;
+        double dy =other.y-this.y;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+
+
+
+
+
+
 
 
 }
