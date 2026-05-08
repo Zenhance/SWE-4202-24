2b456be421deedeb26980575df0ddde54419d1ea
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-05-08 19:50:33 +0600

    initial commit

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -1,3 +1,23 @@
 public class Trip {
-    private passenger
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare;
+    private boolean completed = false;
 }
+
+public Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare) {
+    this.passenger = passenger;
+    this.driver = driver;
+    this.from = from;
+    this.to = to;
+    this.baseFare = baseFare;
+    this.completed = false;
+    this.driver = driver.setAvailable(false);
+}
+
+void main() {
+}
+
+
