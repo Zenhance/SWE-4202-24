b197d5452bda9eaf6a4cdc6bd90c104dc3a22913
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-05-08 20:26:09 +0600

    Commit C

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -1,1 +1,60 @@
+public class Driver
+  {
+private  int id;
+  private String name;
+ private String licensePlate;
+  boolean isAvailable;  
+ public Driver (int id, String name, String licensePlate )
+    {
+     
+      this.id=id;
+      this.name=name;
+      this.licensePlate=licensePlate;
+    }
+
+public Driver (int id, String name, String licensePlate, boolean isAvailable)
+    {
+     this.id=id;
+      this.name=name;
+      this.licensePlate=licensePlate;
+      this.isAvailable=isAvailable;
+         }
+
+public int getId()
+    {
+
+      return id;
+    }
+
+
+public String getName()
+    {
+      return name;
+    }
+public String getLicensePlate()
+    {
+     
+     return  licensePlate;
+    }
+
+public boolean isAvailable()
+    {
+     return isAvailable;
+      
+    }
+public String toString()
+    { String status;
+     if (isAvailable){ status= "AVAILABLE";}
+     else {status= "BUSY";}
+    return String.format (" Driver[%d] %s (%s) [%s]", 
+                          id, name, licensePlate, status);
+                          
+
+
+      
+    
+      
+    }
+    
+  }
 
