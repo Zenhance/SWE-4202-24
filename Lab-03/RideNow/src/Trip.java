6f7b2abe45b4804b7ff3a338b80a5411ab4dc97c
mahibrahaman175-stack <mahibrahaman175@gmail.com>
2026-05-08 20:27:45 +0600

    initialized upto getters!

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -7,6 +7,42 @@ public class Trip {
     private boolean completed;
 
     public Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare){
-
+         this.passenger=passenger;
+         this.driver=driver;
+         this.from=from;
+         this.to=to;
+         this.baseFare=baseFare;
+         this.completed=false;
+         driver.setAvailable(false);
     }
+
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public Location getFrom() {
+        return from;
+    }
+
+    public Location getTo() {
+        return to;
+    }
+
+    public double getBaseFare() {
+        return baseFare;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+
+
+
+
 }
