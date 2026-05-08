b7e70df870f7184cd48e6b937e0dfe00d905ecdc
16-safwan <safwanbhuiyan@iut-dhaka.edu>
2026-05-08 15:46:18 +0600

    git commit

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -3,17 +3,32 @@ public class Location {
     private double x;
     private double y;
 
-    public Location(String label, double x, double y) {
+    public Location(String label, double x, double y)
+    {
         this.label = label;
         this.x = x;
         this.y = y;
     }
 
-    public Location(double x, double y) {
-        this.label = "Unknown";
-        this.x = x;
-        this.y = y;
+    public Location(double x, double y)
+    {
+        this.label="Unknown";
+        this.x=x;
+        this.y=y;
 
+    }
+
+    public String getLabel()
+    {
+        return label;
+    }
+    public double getX()
+    {
+        return x;
+    }
+    public double getY()
+    {
+        return y;
     }
 
 }
