d62b6e6c8281f7943cdcc29c5dbec64208daffca
16-safwan <safwanbhuiyan@iut-dhaka.edu>
2026-05-08 15:44:09 +0600

    init commit

---
+++ Lab-03/RideNow/src/Trip.java
@@ -0,0 +1,19 @@
+public class Trip {
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare;
+    private boolean completed = false;
+
+
+    public Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare) {
+        this.passenger = passenger;
+        this.driver = driver;
+        this.from = from;
+        this.to = to;
+        this.baseFare = baseFare;
+        driver.setAvailable(false);
+
+    }
+}
