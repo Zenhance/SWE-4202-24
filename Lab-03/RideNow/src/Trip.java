83e6ac893cca521d726216297d8ac6bc75b6cc39
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-05-08 20:44:18 +0600

    trip v2

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -6,4 +6,14 @@ public class Trip {
     private Location to;
     private double baseFare;
     private boolean completed;
+
+    public Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare){
+        this.passenger=passenger;
+        this.driver=driver;
+        this.from=from;
+        this.to=to;
+        this.baseFare=baseFare;
+        this.completed=false;
+        driver.setAvailable(false);
+    }
 }
