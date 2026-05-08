d935f2b915692ed4660b9cb236207624c2bbda79
xenonono36-ctrl <xenonono36@gmail.com>
2026-05-08 19:59:17 +0600

    D3

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -17,4 +17,20 @@ public class Driver {
         this.licencePlate = licencePlate;
         this.isAvailable = isAvailable;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
 }
