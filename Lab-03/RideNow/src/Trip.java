f2a234957ad3398ab216b87026cb17ed5cf9a18f
xenonono36-ctrl <xenonono36@gmail.com>
2026-05-08 20:10:01 +0600

    T1

---
+++ Lab-03/RideNow/src/Trip.java
@@ -0,0 +1,8 @@
+public class Trip {
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare = 50;
+    private boolean completed = false;
+}
