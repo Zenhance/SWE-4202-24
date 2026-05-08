6b2e1c47c0c697d1d26904bc308309c97234b66e
Mahdia <hossainmahdia22@gmail.com>
2026-05-08 23:54:29 +0600

    innit commit

--- Lab-03/RideNow/src/Main.java
+++ Lab-03/RideNow/src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        Passenger sleepyCoder =
-                new Passenger(42, "Alice", 4.8);
-        Driver nightOwl =
-                new Driver(7, "Bob", "DHA-1234");
-        Location spawnPoint =
-                new Location("Airport", 3.0, 7.5);
-        Location finalBossArena =
-                new Location("University", 8.0, 4.0);
+        Passenger sleepyCoder =  new Passenger(42, "Alice", 4.8);
+        Driver nightOwl = new Driver(7, "Bob", "DHA-1234");
+        Location spawnPoint = new Location("Airport", 3.0, 7.5);
+        Location finalBossArena = new Location("University", 8.0, 4.0);
         Trip chaosRide = new Trip(sleepyCoder, nightOwl, spawnPoint, finalBossArena, 50.0);
         System.out.println(chaosRide.getSummary());
         System.out.println();
