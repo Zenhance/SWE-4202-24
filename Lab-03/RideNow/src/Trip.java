f014d32e25d771cf14966740261f9fc156ef373f
nusra <nusratnishi1103@gmail.com>
2026-05-08 21:32:20 +0600

    new

---
+++ Lab-03/RideNow/src/Trip.java
@@ -0,0 +1,72 @@
+public class Trip {
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare;
+    private boolean completed;
+
+    public Trip(Passenger passenger,Driver driver,Location from,Location to,double baseFare){
+        this.passenger=passenger;
+        this.driver=driver;
+        this.from=from;
+        this.to=to;
+        this.baseFare=baseFare;
+        this.completed=false;
+
+        driver.setAvailable(false);
+    }
+
+    public Passenger getPassenger(){
+        return passenger;
+    }
+
+    public Driver getDriver(){
+        return driver;
+    }
+
+    public Location getFrom(){
+        return from;
+    }
+
+    public Location getTo(){
+        return to;
+    }
+
+    public double getBaseFare(){
+        return baseFare;
+    }
+
+    public boolean isCompleted(){
+        return completed;
+    }
+
+    public double getDistance(){
+        return from.distanceTo(to);
+    }
+
+    public double calculateFare(){
+        return baseFare + 15.0*getDistance();
+    }
+
+    public void completeTrip(){
+        completed=true;
+        driver.setAvailable(true);
+    }
+
+    public String getSummary(){
+        String status;
+        if(completed)
+            status="COMPLETED";
+        else
+            status="IN PROGRESS";
+        return "Trip Summary\n" +
+                "Passenger : " + passenger.getName() + "\n" +
+                "Driver : " + driver.getName() + " (" +driver.getLicencePlate() + ") \n" +
+                "From : " + from.toString() + "\n" +
+                "To : " + to.toString() + "\n" +
+                "Distance : " + String.format("%.2f", getDistance()) + " km\n" +
+                "Fare : BDT " + String.format("%.2f", calculateFare()) + "\n" +
+                "Status : " + status;
+    }
+}
