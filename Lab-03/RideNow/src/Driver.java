65c99f02f3f072b066252d6510217a593d642248
jasinAnishaa2005 <anishaajasin.13.04.2005@gmail.com>
2026-05-08 22:56:27 +0600

    Updated the code

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -6,11 +6,16 @@ public class Driver {
     private boolean isAvailable;
 
     public Driver(int id, String name, String licencePlate){
-
+       this.id = id;
+       this.name = name;
+       this.licencePlate = licencePlate;
     }
 
     public Driver(int id, String name, String licencePlate, boolean isAvailable){
-
+      this.id = id;
+      this.name = name;
+      this.licencePlate = licencePlate;
+      this.isAvailable = isAvailable;
     }
 
     public void getId(){
