a7794bdd7cd45708504f89e075a8e4652f575266
tahsin1r <naylakagi@gmail.com>
2026-05-08 15:38:50 +0600

    Passenger file completed

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -14,5 +14,19 @@ public class Passenger {
         this.name=name;
     }
 
-
+    public int getId() {
+        return this.id;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public double getRating(){
+        return this.rating;
+    }
+    public void updateRating(double newRating){
+        this.rating=newRating;
+    }
+    public String toString(){
+        return "Passenger["+this.id+"] "+this.name+" ("+this.rating+")";
+    }
 }
