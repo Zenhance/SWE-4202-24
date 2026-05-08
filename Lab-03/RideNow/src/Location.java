abe4910c44f7c5b92a4219911780cb45f666f0d7
iam-tahmid <tahmidim99@gmail.com>
2026-05-08 23:16:40 +0600

    initial commit

---
+++ Lab-03/RideNow/src/Location.java
@@ -0,0 +1,16 @@
+public class Location {
+    private final String label;
+    private final double x;
+    private final double y;
+
+    public Location(String label, double x, double y) {
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Location(double x, double y) {
+        label = "Unknown";
+        this.x = x;
+        this.y = y;
+    }
