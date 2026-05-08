be169d8e4a59c4582cad4e0fc8ccbf90f765c243
iam-tahmid <tahmidim99@gmail.com>
2026-05-08 23:25:44 +0600

    initial commit

---
+++ Lab-03/RideNow/src/Trip.java
@@ -0,0 +1,21 @@
+public class Trip {
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare;
+    private boolean completed;
+
+    Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare) {
+        this.passenger = passenger;
+        this.driver = driver;
+        this.from = from;
+        this.to = to;
+        this.baseFare = baseFare;
+        this.completed = false;
+
+        driver.setAvailable(false);
+    }
+
+
+}
