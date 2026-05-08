7d3476f76ef31537b717ddf89f6c4e44fdaab2b9
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-05-08 23:06:27 +0600

    updated Location.java

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -11,10 +11,26 @@ public class Location {
 
     public Location(String label, double x, double y){
 
+        this.label = label;
+        this.x = x;
+        this.y = y;
     }
 
     public Location(double x, double y){
 
+        this.x = x;
+        this.y = y;
     }
 
+    public String getLabel() {
+        return label;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
