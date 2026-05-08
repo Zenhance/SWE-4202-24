8b835ce6c890ccd8b5d4500393f21c4f56352b4c
Mahdia <hossainmahdia22@gmail.com>
2026-05-08 21:45:32 +0600

    innit commit

--- Lab-03/RideNow/src/Main.java
+++ Lab-03/RideNow/src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        Passenger sleepyCoder =
+                new Passenger(42, "Alice", 4.8);
+        Driver nightOwl =
+                new Driver(7, "Bob", "DHA-1234");
+        Location spawnPoint =
+                new Location("Airport", 3.0, 7.5);
+        Location finalBossArena =
+                new Location("University", 8.0, 4.0);
+        Trip chaosRide = new Trip(
+                sleepyCoder,
+                nightOwl,
+                spawnPoint,
+                finalBossArena,
+                50.0
+        );
+        System.out.println(chaosRide.getSummary());
+        System.out.println();
+        chaosRide.completeTrip();
+        System.out.println(chaosRide.getSummary());
     }
 }
