d48f08abd965532bd0d744591086d18abfc253a1
Mahdia <hossainmahdia22@gmail.com>
2026-05-08 20:25:29 +0600

    innit commit

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,55 @@
+public class Driver {
+    private int id;
+    private String name;
+    private String licencePlate;
+    private boolean isAvailable;
+
+    // 3 arg constructor
+    public Driver(int id, String name, String licencePlate) {
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = true;
+    }
+
+    // 4 arg constructor
+    public Driver(int id, String name, String licencePlate, boolean isAvailable) {
+        this.id = id;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean statusPotion) {
+        this.isAvailable = statusPotion;
+    }
+
+    @Override
+    public String toString() {
+        String mood = isAvailable ? "AVAILABLE" : "BUSY";
+
+        return String.format(
+                "Driver[%d] %s (%s) [%s]",
+                id,
+                name,
+                licencePlate,
+                mood
+        );
+    }
+}
