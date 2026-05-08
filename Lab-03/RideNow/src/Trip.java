d889c423543f52a11b9d16494db619e936bb53d9
elfie1502 <mumtahina@iut-dhaka.edu>
2026-05-08 22:28:32 +0600

    git init

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -16,4 +16,62 @@ class Trip {
         this.completed = false;
         driver.setAvailable(false);
     }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public Driver getDriver(){
+        return driver;
+    }
+
+    public Location getFrom(){
+        return from;
+    }
+
+    public Location getTo (){
+        return to;
+    }
+
+    public double getBaseFare() {
+        return baseFare;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public double getDistance(){
+        return from.distanceTo(to);
+    }
+
+    public double calculateFare(){
+        return baseFare + 15.0 * getDistance();
+    }
+
+    public void completeTrip(){
+        this.completed = true;
+        driver.setAvailable(true);
+    }
+
+    public String getSummary(){
+        String status;
+
+        if(completed){
+            status = "Completed";
+        }else{
+            status = "In Progress";
+        }
+
+        String Summary = "Trip summary\n" +
+                "Passenger : " +passenger.getName() + "\n" +
+                "Driver    : " +driver.getName() + "(" + driver.getLicencePlate() + ")\n" +
+                "From      : " +from.toString() + "\n" +
+                "To        : " +passenger.getName() + "\n" +
+                "Distance  : "
+                "Fare      : "
+                "Status    : " + status;
+
+        return Summary;
+    }
 }
