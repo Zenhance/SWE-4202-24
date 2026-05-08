285a8edbe1e425f26b34f1b411638562f09f4419
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-05-08 21:57:08 +0600

    trip v3

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -3,28 +3,35 @@ public class Passenger {
     private String name;
     double rating;
 
-    public Passenger(int id, String name, double rating){
+    Passenger(int id, String name, double rating){
         this.id=id;
         this.name=name;
         this.rating=rating;
     }
 
-    public Passenger(int id, String name){
+    Passenger(int id, String name){
         rating=5.0;
         this.id=id;
         this.name=name;
     }
 
     public int getId(){
+
         return id;
     }
 
     public String getName(){
+
         return name;
     }
 
     public double getRating(){
+
         return rating;
+    }
+
+    public void updateRating(double newRating){
+        this.rating=newRating;
     }
 
 
