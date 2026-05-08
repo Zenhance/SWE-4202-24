02503ec3b291e52b2dcea3b3a33ba936082ca0b1
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-05-08 20:22:14 +0600

    driver v2

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -10,4 +10,14 @@ public class Driver {
      this.licencePlate=licencePlate;
  }
 
+ public Driver(int id, String name, String licencePlate, boolean isAvailable ){
+     this.id=id;
+     this.name=name;
+     this.licencePlate=licencePlate;
+     this.isAvailable=isAvailable;
+ }
+
+
+
+
 }
