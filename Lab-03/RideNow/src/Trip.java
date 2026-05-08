aedff7cacb6eab0f7bf2cf4308dccea0d8eba779
glockmachigan <muniruzzaman24@iut-dhaka.edu>
2026-05-08 23:38:07 +0600

    add constructor

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -5,4 +5,15 @@ public class Trip {
     private Location to;
     private double baseFare;
     private boolean completed;
+
+    Trip(Passenger passenger, Driver driver, Location from, Location to, double
+            baseFare){
+        this.passenger = passenger;
+        this.driver = driver;
+        this.from = from;
+        this.to = to;
+        this.baseFare = baseFare;
+        this.completed = false;
+        driver.setAvailable(false);
+    }
 }
