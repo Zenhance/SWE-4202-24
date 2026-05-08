768ce71f6ff7d8a61a856f924ac2158ebb2c6f07
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-05-08 22:21:11 +0600

    Commit D

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -14,10 +14,63 @@ this.passenger=passenger;
       this.to=to;
       this.baseFare=baseFare;
       this.completed=false; 
+       driver.setAvailable= false; //driver variable inside trip class calls the setAvailable method that we defined inside driver and sets it to false
 
-      
 
-      
+        public passenger getPassenger()
+          {  return this.passenger; 
+          }
+       public passenger getDriver()
+         {
+         return this.driver;
+         }
+      public location getFrom()
+        {
+        return from;
+        }
+      public location getTo()
+        {
+        return to;
+        }
+      public double getBaseFare()
+        {
+        return baseFare;
+        }
+      public boolean isCompleted()
+        {
+        return completed;
+        }
+      public double getDistance()
+        {
+        from.distanceTo(to); 
+ /* 
+ here, we are calling the distanceTo method that we defined inside location class
+ to calculate the distance between from and to, here according to our syntax, from is the caller object and 
+ to is being passed as the argument, so "this" keyword inside distanceTo keyword uses the value returned by from and "other"
+uses the value returned by to 
+ */       }
+      public double calculateFare()
+        {
+        return baseFare + (15*getDistance());
+        }
+      public void completeTrip()
+        {
+           this.completed=true;
+        driver.setAvailable(true);
+        
+        }
+      public String getSummary()
+        {
+
+        return "Trip Summary\n"
+                + "Passenger : " + passenger.getName() + "\n"
+                + "Driver    :" + driver.getName() + (" + driver.getLicensePlate()+ " )\n"
+                + "From      : " + from.toString() + " \n" 
+                + "To        :  " + to.toString() + "\n"
+                + "Distance  : " + String.format("%.2f", getDistance())" km \n" 
+                +" Fare      :BDT " + String.format(" %.2f", getDistance()+ )" \n"                                                           
+                + "Status    :" +status;                               
+        }
     }
 
 
