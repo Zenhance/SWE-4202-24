8049afdfe835649b1666419a6ba8362a0b5c10c8
iam-tahmid <tahmidim99@gmail.com>
2026-05-08 20:39:11 +0600

    declared get methods

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -3,7 +3,41 @@
         private int id;
         private String name;
         private String licensePlate;
-        private boolean isAvaialable;
+        private boolean isAvailable;
+
+        Driver(int id, String name, String licensePlate){
+            this.id= id;
+            this.name=name;
+            this.licensePlate= licensePlate;
+            this.isAvailable= true;
+        }
+
+        Driver (int id, String name, String licensePlate, boolean isAvailable){
+            this.id=id;
+            this.name=name;
+            this.licensePlate=licensePlate;
+            this.isAvailable=isAvailable;
+        }
+
+        public int getId(){
+            return id;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public String getLicensePlate(){
+            return licensePlate;
+        }
+
+        public boolean isAvailable(){
+            return isAvailable;
+        }
+
+        public void setAvailable(boolean available){
+            isAvailable = available;
+        }
 
     }
 
