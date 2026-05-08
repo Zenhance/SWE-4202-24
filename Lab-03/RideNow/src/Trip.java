9646424ae6d20307d9549023a32d05ac36c87d6a
tahsin1r <naylakagi@gmail.com>
2026-05-08 23:29:57 +0600

    Constructor created variables declared in the composition

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -1,2 +1,20 @@
 public class Trip {
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare;
+    private boolean completed;
+
+    Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare){
+        this.passenger=passenger;
+        this.driver=driver;
+        this.from=from;
+        this.to=to;
+        this.baseFare=baseFare;
+        completed=false;
+        driver.setAvailable(false);
+
+
+    }
 }
