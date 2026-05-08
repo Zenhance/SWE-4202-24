c230106da2f0625eacd1f13ba85884427fa3b32b
xenonono36-ctrl <xenonono36@gmail.com>
2026-05-08 20:21:32 +0600

    T3

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -15,4 +15,37 @@ public class Trip {
         completed = false;
         driver.setAvailable(false);
     }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public Location getFrom() {
+        return from;
+    }
+
+    public Location getTo() {
+        return to;
+    }
+
+    public double getBaseFare() {
+        return baseFare;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public double getDistance() {
+        return from.distanceTo(to);
+    }
+
+    double fare;
+    public double calculateFare() {
+        return fare = baseFare + 15.0 * from.distanceTo(to);
+    }
 }
