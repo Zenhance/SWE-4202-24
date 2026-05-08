98f56b1391b79e0a704f292554d3cbd0734130a5
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-05-08 22:38:42 +0600

    location final(i hope)

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -36,6 +36,11 @@ public class Location {
         return Math.sqrt(dx*dx + dy*dy);
     }
 
+    public String toString(){
+        return String.format("%s (%.2f, %.2f)", label, x, y);
+
+    }
+
 
 
 
