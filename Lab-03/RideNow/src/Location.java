1473d1e60f6f3fbb9e5abf5fd82e04793c0ffd2b
waseefmunir-max <waseef05@gmail.com>
2026-05-08 23:03:43 +0600

    Another commit

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -28,6 +28,10 @@ public class Location {
     }
 
     public double distanceTo(Location other) {
+        double dx = other.x - this.x; // difference of x1, x2, (x1 - x2)
+        double dy = other.y - this.y; // difference of y1, y2, (y1 - y2)
+        double distance = Math.sqrt(dx * dx - dy * dy);
 
+        return distance;
     }
 }
