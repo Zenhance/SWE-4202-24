bd5a692f6d341dc53eb64e452a1ec7114f22dcc4
glockmachigan <muniruzzaman24@iut-dhaka.edu>
2026-05-08 23:19:49 +0600

    adding constructor

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -11,4 +11,11 @@ public class Driver {
         this.isAvailable = true;
     }
 
+    Driver(int id, String name, String licencePlate, boolean isAvailable){
+        this.id = id;;
+        this.name = name;
+        this.licencePlate = licencePlate;
+        this.isAvailable = isAvailable;
+    }
+
 }
