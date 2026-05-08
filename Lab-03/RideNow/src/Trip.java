afe66be9a4b1db8ea746aa3b4f89080dfa4757fa
mahibrahaman175-stack <mahibrahaman175@gmail.com>
2026-05-08 20:10:39 +0600

    Started the trip part!

---
+++ Lab-03/RideNow/src/Trip.java
@@ -0,0 +1,12 @@
+public class Trip {
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare;
+    private boolean completed;
+
+    public Trip(Passenger passenger, Driver driver, Location from, Location to, double baseFare){
+
+    }
+}
