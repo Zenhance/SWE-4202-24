bbe81c73d4e233308e08437d33e1a678075bb959
tanbirulislam-hash <tanbirulislam@iut-dhaka.edu>
2026-05-09 15:10:26 +0600

    1st commit

---
+++ Lab-03/RideNow/src/Driver.java
@@ -0,0 +1,32 @@
+public class Driver{
+    private int id;
+    private String name;
+    private String licencePlate;
+    private boolean isAvailable;
+
+    public Driver(int id,String name,String licencePlate){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=true;
+    }
+
+    public Driver(int id,String name,String licencePlate,boolean isAvailable){
+        this.id=id;
+        this.name=name;
+        this.licencePlate=licencePlate;
+        this.isAvailable=isAvailable;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getLicencePlate(){
+        return licencePlate;
+    }
+
