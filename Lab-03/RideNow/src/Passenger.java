3f839a76bbbc7bf54bc8ae95483df73e17e21ecd
Rakin <rakinhasan001@gmail.com>
2026-05-09 22:50:55 +0600

    methods added for passenger class

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -12,4 +12,19 @@ public class Passenger {
         this.name=name;
         this.rating=5.0;
     }
+
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+
+        return name;
+    }
+    public double getRating(){
+        return rating;
+    }
+    public void updateRating( double newRating){
+        this.rating=newRating;
+    }
+
 }
