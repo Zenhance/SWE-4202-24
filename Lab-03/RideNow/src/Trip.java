9e0d8e18da23ab637f4aa77895b03b09b5fa5cfb
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-05-09 18:12:25 +0600

    updated constructor in Trip.java

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -1,5 +1,6 @@
 public class Trip {
 
+    //fields
     private Passenger passenger;
     private Driver driver;
 
@@ -9,9 +10,16 @@ public class Trip {
     private double baseFare;
     private boolean completed = false;
 
+
+    //constructor
     public Trip(Passenger passenger, Driver driver, Location from, Location to, double
             baseFare) {
 
+        this.passenger = passenger;
+        this.driver = driver;
+        this.from = from;
+        this.to = to;
+        this.baseFare = baseFare;
 
     }
 }
