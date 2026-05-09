924a17965d0c752d3c523d2ccdbd1097c9a4a9ec
tanbirulislam-hash <tanbirulislam@iut-dhaka.edu>
2026-05-09 15:46:49 +0600

    1st commit

---
+++ Lab-03/RideNow/src/Trip.java
@@ -0,0 +1,18 @@
+public class Trip{
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare;
+    private boolean completed;
+
+    public Trip(Passenger passenger,Driver driver,Location from,Location to,double baseFare){
+        this.passenger=passenger;
+        this.driver=driver;
+        this.from=from;
+        this.to=to;
+        this.baseFare=baseFare;
+        this.completed=false;
+        driver.setAvailable(false);
+    }
+
