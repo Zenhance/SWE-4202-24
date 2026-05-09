42134ce2f807e80c39f318c0741d6f2958f24473
tanbirulislam-hash <tanbirulislam@iut-dhaka.edu>
2026-05-09 14:43:30 +0600

    final

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -10,3 +10,24 @@ public class Passenger{
         this.rating=rating;
     }
 
+    public Passenger(int id,String name){
+        this.id=id;
+        this.name=name;
+        this.rating = 5.0;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getRating(){
+        return rating;
+    }
+    public void updateRating(double newRating){
+        rating=newRating;
+    }
+    public String toString(){
+        return String.format("Passenger[%d] %s (%.2f)",id,name,rating);
+    }
+}
