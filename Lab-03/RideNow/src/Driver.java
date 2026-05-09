385003fd4b69edee1743aafb1061b30ed91a4c77
Rakin <rakinhasan001@gmail.com>
2026-05-09 23:41:56 +0600

    added both default and explicitly set constructors  of Driver class

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -4,4 +4,14 @@ public class Driver {
     private String licensePlate;
     private boolean isAvailable;
 
+    public Driver(int id, String name, String licensePlate){
+        this.id=id;
+        this.name=name;
+        this.licensePlate=licensePlate;
+        this.isAvailable=isAvailable;
+    }
+
+    public Driver(int id, String name, String licensePlate){
+        this(id,name,licensePlate,true);s
+    }
 }
