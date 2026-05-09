4b5fe120e7b45ac00e67f1c7123b774b73e5c879
tanbirulislam-hash <tanbirulislam@iut-dhaka.edu>
2026-05-09 15:56:10 +0600

    2nd commit

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -16,3 +16,24 @@ public class Trip{
         driver.setAvailable(false);
     }
 
+    public Passenger getPassenger(){
+        return passenger;
+    }
+
+    public Driver getDriver(){
+        return driver;
+    }
+
+    public Location getFrom(){
+        return from;
+    }
+
+    public Location getTo(){
+        return to;
+    }
+
+    public double getBaseFare(){
+        return baseFare;
+    }
+
+
