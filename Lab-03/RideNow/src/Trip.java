92538bc7104d0b89335a5b9e13327b968b0cd10e
Mubtasim Fuad <mubtasimsmail@gmail.com>
2026-05-09 07:38:23 +0600

    getSummary method defined

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -46,4 +46,23 @@ public class Trip {
     public double calculateFare(){
         return baseFare + (15.0 * getDistance());
     }
+
+    public String getSummary(){
+        String tripStatus;
+
+        if(completed){
+            tripStatus = "COMPLETED";
+        }else{
+            tripStatus = "IN PROGRESS";
+        }
+
+        return "Trip Summary\n"
+                + "Passenger: " + passenger.getName() + "\n"
+                + "Driver   : " + driver.getName() + " (" + driver.getLicensePlate() + ")\n"
+                + "From     : " + from.toString() + "\n"
+                + "To       : " + to.toString() + "\n"
+                + "Distance : " + String.format("%.2f", getDistance()) + "km\n"
+                + "Fare     : BDT" + String.format("%.2f", calculateFare()) + "\n"
+                + "Status   : " + tripStatus;
+    }
 }
