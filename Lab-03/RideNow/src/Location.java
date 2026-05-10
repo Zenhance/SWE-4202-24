f9296f3f8afbda327165b1fd0b4e9224be9d0560
Rakin <rakinhasan001@gmail.com>
2026-05-10 14:02:44 +0600

    updated toString method in Location class

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -13,5 +13,21 @@ public class Location {
     this.x=x;
     this.y=y;
     }
+    String getLabel(){
+       return label;
+    }
+    double getX(){
+        return x;
+    }
+    public double getY(){
 
+        return y;
+    }
+    double distanceToLocation(Location other){
+        double d= Math.sqrt((this.x-other.x)*(this.x-other.x)+(this.y-other.y)*(this.y-other.y));
+         return d;
+    }
+    public String toString(){
+return String.format("%s (%.2f,%.2f)",label , x, y);
+    }
 }
