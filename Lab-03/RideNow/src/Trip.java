82d4cb232bbd10a62326da190154bf4a2ceb8318
Rakin <rakinhasan001@gmail.com>
2026-05-10 13:32:19 +0600

    added methods to Trip class

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -15,4 +15,25 @@ public class Trip {
         this.baseFare=baseFare;
         driver.isAvailable(false);
     }
+    public  double getDistance(){
+        return from.distanceToLocation(to);
+    }
+    public void setCompleted(){
+        this.completed=true;
+        driver.isAvailable(true);
+    }
+    public double calculateFare(){
+        return baseFare+15.0*getDistance();
+    }
+    public String getSummary(){
+        return String.format("Trip Summary\n"+"Passenger: %s\n"+
+        "Driver: %s (%s)\n"+
+                "From: %s\n"
+                +"To: %s \n"
+                +"Distance: %.2f km\n"+
+                "Fare: BDT %.2f\n"
+                +"Status: %s",
+                passenger.getName(),driver.getname(),driver.getLicensePlate(),from.toString(),to.toString(),
+                getDistance(),calculateFare(),completed ?  "Completed" : "In Progress");
+    }
 }
