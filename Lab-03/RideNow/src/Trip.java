32d94a2e9f6c899adfc75d04921acfc76b1c9498
Rakin <rakinhasan001@gmail.com>
2026-05-10 10:41:02 +0600

    added constructor Trip class

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -2,6 +2,17 @@ public class Trip {
     private Passenger passenger;
     private Driver driver;
     private Location from;
+    private Location to;
     private double baseFare;
     private boolean completed;
+
+    public Trip (Passenger passenger,Driver driver ,Location from, Location to,double baseFare){
+        this.passenger=passenger;
+        this.driver=driver;
+        this.from=from;
+        this.to=to;
+        this.completed=false;
+        this.baseFare=baseFare;
+        driver.isAvailable(false);
+    }
 }
