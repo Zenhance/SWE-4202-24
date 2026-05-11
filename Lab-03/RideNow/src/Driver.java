4b2c5fedaa3cfa8bdd9b60520013d133c8ac2166
Rakib <rakibulhasan4266@gmail.com>
2026-05-12 01:16:41 +0600

    Updated

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -1,12 +1,9 @@
-public class Driver
-{
-    public class Driver
+ public class Driver
     {
         private int id;
         private String name;
         private String licencePlate;
         private boolean isAvailable;
-    }
 
     public Driver(int id, String name, String licencePlate)
     {
