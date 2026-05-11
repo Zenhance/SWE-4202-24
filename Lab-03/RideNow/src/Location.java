9ea436b9957a68fc78e248dd5ac34a8ad0a08326
Rakib <rakibulhasan4266@gmail.com>
2026-05-12 00:17:08 +0600

    Created method getY of classes

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -1,4 +1,5 @@
-public class Location {
+public class Location
+{
     private String label;
     private double x;
     private double y;
@@ -11,8 +12,8 @@ public Location(String x, double x , double y)
 }
 public Location(double x, double y)
 {
-       this.x=x;
-       this.y=y;
+    this.x=x;
+    this.y=y;
 }
 public String getLabel()
 {
@@ -21,4 +22,8 @@ public String getLabel()
 public double getX()
 {
     return this.x;
+}
+public double getY()
+{
+    return this.y;
 }
