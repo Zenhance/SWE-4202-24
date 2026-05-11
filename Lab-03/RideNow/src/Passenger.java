e3cf79e95aaafe1eb35274f212f2f80ae41bde63
Rakib <rakibulhasan4266@gmail.com>
2026-05-12 01:00:36 +0600

    Updated

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -1,38 +1,37 @@
-public class Passenger
-{
+public class Passenger {
     private int id;
     private String name;
     private double rating;
-public Passenger(int id, String name, double rating)
-{
-    this.id = id;
-    this.name = name;
-    this.rating = rating;
-}
 
-public Passenger(int id, String name)
-{
-    this.id = id;
-    this.name = name;
-    this.rating = 5.0;
-}
-public int getId()
-{
-    return this.id;
-}
-public String getName()
-{
-    return this.name;
-}
-public double getRating()
-{
-    return this.rating;
-}
-public void updateRating(double newRating)
-{
-    this.rating = newRating;
-}
-public String toString()
-{
-    return String.format("Passenger[%d] %s (%.2f)", id, name, rating);
+    public Passenger(int id, String name, double rating) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public Passenger(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.rating = 5.0;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getRating() {
+        return this.rating;
+    }
+
+    public void updateRating(double newRating) {
+        this.rating = newRating;
+    }
+
+    public String toString() {
+        return String.format("Passenger[%d] %s (%.2f)", id, name, rating);
+    }
 }
