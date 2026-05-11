ea82de42aee02ee7209d779d6676424336ca026d
Rakib <rakibulhasan4266@gmail.com>
2026-05-12 01:03:30 +0600

    Driver class created

--- Lab-03/RideNow/src/Driver.java
+++ Lab-03/RideNow/src/Driver.java
@@ -1,4 +1,10 @@
 public class Driver
 {
-
+    public class Driver
+    {
+        private int id;
+        private String name;
+        private String licencePlate;
+        private boolean isAvailable;
+    }
 }
