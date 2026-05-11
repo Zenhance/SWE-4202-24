25f2b47c804dfb6c5af92eab3b4a67be00a63170
Rakib <rakibulhasan4266@gmail.com>
2026-05-12 00:22:10 +0600

    Distance mrthod

--- Lab-03/RideNow/src/Location.java
+++ Lab-03/RideNow/src/Location.java
@@ -27,3 +27,9 @@ public double getY()
 {
     return this.y;
 }
+public double distanceTo(Location other)
+{
+    double dx = other.x - this.x;
+    double dy = other.y - this.y;
+    return Math.sqrt(dx * dx + dy * dy);
+}
