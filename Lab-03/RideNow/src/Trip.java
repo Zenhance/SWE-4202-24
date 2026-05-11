726d4209a9ab252c98168d8d3cf6213790c3513c
Rakib <rakibulhasan4266@gmail.com>
2026-05-12 01:19:45 +0600

    Trip class created

---
+++ Lab-03/RideNow/src/Trip.java
@@ -0,0 +1,11 @@
+public class Trip
+{
+    private Passenger passenger;
+    private Driver driver;
+    private Location from;
+    private Location to;
+    private double baseFare;
+    private boolean completed;
+
+
+}
