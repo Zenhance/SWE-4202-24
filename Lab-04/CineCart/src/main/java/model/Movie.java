76e8bbb684710193a9a99b2722fec1668cab5e76
tuireuit <jamilhasan@iut-dhaka.edu>
2026-05-12 11:54:11 +0600

    writing movie class

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -1,4 +1,23 @@
 package model;
 
 public class Movie {
+
+
+    int id;
+    String title;
+    String rating;
+    int durationMin;
+    double basePrice;
+
+
+public Movie(){
+
+    this.id=id;
+    this.title=title;
+    this.rating=rating;
+    this durationMin=durationMin;
+    this basePrice=basePrice;
+}
+
+
 }
