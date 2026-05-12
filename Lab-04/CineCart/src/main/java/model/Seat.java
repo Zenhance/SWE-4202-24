ebe0d6ac2aee605994d4c41dbbfb80443f5347cb
jahinhossain-tech <jahinhossain@iut-dhaka.edu>
2026-05-12 10:25:49 +0600

    created the to string method, and other methods. Completing seat

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -12,4 +12,50 @@ public class Seat {
         this.isPremium = isPremium;
         this.isBooked = false;
     }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void book() {
+        isBooked = true;
+    }
+
+    public void release() {
+        isBooked = false;
+    }
+
+    public boolean isAvailable() {
+        return !isBooked;
+    }
+
+    public String toString() {
+        String result = "R" + row + "C" + col;
+
+        if (isPremium) {
+            result += "*";
+        }
+
+        if (isBooked) {
+            result += "#";
+        }
+
+        return result;
+    }
 }
+
+
+
+
