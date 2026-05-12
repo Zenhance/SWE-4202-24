f7a06645c704ef99c278bf77bab9c4c74ef3b313
jahinhossain-tech <jahinhossain@iut-dhaka.edu>
2026-05-12 10:18:27 +0600

    created Seat class.

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Seat {
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium) {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = false;
+    }
 }
