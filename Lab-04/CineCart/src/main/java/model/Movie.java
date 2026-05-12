dcb4b636f235d318efcd28ec5161a1b120ee319f
Tasauf Abdullah <tasaufalter@gmail.com>
2026-05-12 12:14:59 +0600

    Add getters for every field

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -13,4 +13,20 @@ public class Movie{
         this.durationMin = durationMin;
         this.basePrice = basePrice;
     }
+
+    public int getId(){
+        return id;
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public int getDurationMin() {
+        return durationMin;
+    }
+
+    public double getBasePrice(){
+    return basePrice;
 }
+
