623018784ac4e5e688591479e21a159e99aa694e
tahsin1r <naylakagi@gmail.com>
2026-05-12 09:25:35 +0600

    Added fields and a constructor in Movie.java

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -1,11 +1,20 @@
 package model;
 
 public class Movie {
-    private int id;
-    private String title;
-    private String rating;
-    private int durationMin;
-    private double basePrice;
+    public int id;
+    public String title;
+    public String rating;
+    public int durationMin;
+    public double basePrice;
+
+    public Movie(int id, String title, String rating, int durationMin, double basePrice){
+        this.id=id;
+        this.title=title;
+        this.rating=rating;
+        this.durationMin=durationMin;
+        this.basePrice=basePrice;
+    }
+
 
 
 
