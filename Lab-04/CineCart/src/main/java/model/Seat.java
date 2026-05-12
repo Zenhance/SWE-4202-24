ba6ef999dc5cc4fe7e4ec612c72955e98471acac
tuireuit <jamilhasan@iut-dhaka.edu>
2026-05-12 12:52:15 +0600

    seat class

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -1,4 +1,41 @@
 package model;
 
 public class Seat {
+
+    int row;
+    int col;
+    boolean isPremium;
+    boolean isBooked;
+
+Public Seat(){
+
+    this.row=row;
+    this.col=col;
+    this.isPremium=isPremium;
+    this.isBooked=false;
+}
+
+public int getRow(){
+    return row;
+}
+
+public int getCol(){
+
+    return rol;
+}
+
+public boolean getIsPremium(){
+    return isPremium;
+}
+
+public boolean getIsBooked(){
+     return isBooked;
+}
+
+
+
+
+
+
+
 }
