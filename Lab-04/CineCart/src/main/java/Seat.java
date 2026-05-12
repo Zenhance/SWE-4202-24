abf4d5d7ce3fcb169bb9395e6ad0e2ec34100964
glockmachigan <muniruzzaman24@iut-dhaka.edu>
2026-05-12 10:03:42 +0600

    add public Seat constructor

--- Lab-04/CineCart/src/main/java/Seat.java
+++ Lab-04/CineCart/src/main/java/Seat.java
@@ -3,4 +3,11 @@ public class Seat {
     private int col;
     private boolean isPremium;
     private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium, boolean isBooked){
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = isBooked;
+    }
 }
