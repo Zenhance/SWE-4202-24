9b4e143c9369017911e27c0466f2c55f8cb8d395
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-05-12 09:50:58 +0600

    adding two methods

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -35,6 +35,18 @@ public class Movie {
         return basePrice;
     }
 
+    public int getMinAge(){
+        if(rating.equals("G")) return 0;
+        if(rating.equals("PG")) return 7;
+        if(rating.equals("PG-13")) return 13;
+        if(rating.equals("R")) return 18;
+        return 1;
+    }
+
+    public String toString(){
+        String string = String.format("%s (%s) %dmin - BDT %.2f");
+        return string;
+    }
 
 }
 
