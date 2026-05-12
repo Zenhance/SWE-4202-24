f9fa06642b1beb1ba25e53053a965bd96b9d6fbe
mubashirulhasan-netizen <mubashirulhasan@iut-dhaka.edu>
2026-05-12 10:25:28 +0600

    Not completed, just initiated the classes a little bit....

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -1,4 +1,28 @@
 package model;
 
 public class Seat {
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium, boolean isBooked){
+        this.row = row;
+        this.col = col;
+        isPremium = false;
+        isBooked = false;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void book(){
+
+    }
+
 }
