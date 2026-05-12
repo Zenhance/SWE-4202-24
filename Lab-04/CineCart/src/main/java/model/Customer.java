69d200ecb56eb942419e8e67f4fdeeb7e6045d70
Mubtasim Fuad <mubtasimsmail@gmail.com>
2026-05-12 10:30:49 +0600

    created fields and constructors

---
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Customer {
+    private int id;
+    private String name;
+    private int age;
+    private String loyaltyTier;
+
+    public Customer(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier = "BASIC";
+    }
+
+    public Customer(int id, String name, int age, String loyaltyTier) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier = loyaltyTier;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLoyaltyTier() {
+        return loyaltyTier;
+    }
+
+
+}
---
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Hall {
+    private int id;
+    private int rows;
+    private int cols;
+    private Seat[][] grid;
+
+    public Hall(int id, int rows, int cols, int premiumRows) {
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                grid.isPremium = (i < premiumRows);
+            }
+        }
+    }
+}
