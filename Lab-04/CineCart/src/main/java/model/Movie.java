fd55e8e67dcc5545c5ac87a9ef0c9fb448f1eec9
Zunied-57 <zunied0411@gmail.com>
2026-05-12 09:30:06 +0600

    constructor and getters

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -6,4 +6,38 @@ class Movie
     private String rating;
     private int durationMin;
     private double basePrice;
+
+    public Movie(int id, String title, String rating, int durationMin, double basePrice)
+    {
+        this.id=id;
+        this.title=title;
+        this.rating=rating;
+        this.durationMin=durationMin;
+        this.basePrice=basePrice;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getRating()
+    {
+        return rating;
+    }
+
+    public int getDurationMin()
+    {
+        return durationMin;
+    }
+
+    public double getBasePrice()
+    {
+        return basePrice;
+    }
 }
