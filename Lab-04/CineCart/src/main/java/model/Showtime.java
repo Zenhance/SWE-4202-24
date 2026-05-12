c555e21880e8f81e96fc67e0f63013f34e620d1a
tuireuit <jamilhasan@iut-dhaka.edu>
2026-05-12 13:09:21 +0600

    showtime class 2

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -1,4 +1,18 @@
 package model;
 
 public class Showtime {
+
+
+    int id;
+    Movie movie;
+    Hall hall;
+    int startHour (0–23);
+    String dateTag;
+
+
+public Showtime(int id,Movie movie,Hall hall,int startHour (0–23),String dateTag){
+
+
+    }
+
 }
