e32c8fd736625328b3062aed28e3f0a41290da0b
inanjaki184 <inanjaki184@gmail.com>
2026-05-12 12:17:07 +0600

    partA.2

---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Seat {
+    int row;
+    int col;
+    boolean isPremium;
+    boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium, boolean isBooked)
+    {
+        this.row=row;
+        this.col=col;
+        this.isPremium=isPremium;
+
+    }
+
+
+}
