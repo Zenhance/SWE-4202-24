fa57448436b7546377ab01fadb14012b8b868f6a
Tasauf Abdullah <tasaufalter@gmail.com>
2026-05-12 12:22:58 +0600

    Add getMinAge

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -37,6 +37,24 @@ public class Movie{
         return rating;
     }
 
+    public int getMinAge(){
+    if(rating == "G"){
+        return 0;
+    }
+    else if (rating == "PG"){
+        return 7;
+    }
+    else if(rating == "PG-13"){
+        return 13;
+    }
+    else if(rating == "R"){
+        return 18;
+    }
+    }
+}
+
+
+
 
 
 
