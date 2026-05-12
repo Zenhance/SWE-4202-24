58abfef9e3a06ebfa0fbaf3a979a1bb0a379f973
Sadman Raiyan <kamrulseed@gmail.com>
2026-05-12 09:55:28 +0600

    Adding constructor to Seat.java

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -5,4 +5,12 @@ public class Seat {
     private int col;
     private boolean isPremium;
     private boolean isBooked;
+
+    Seat(int row, int col, boolean isPremium){
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = false;
+    }
+
 }
