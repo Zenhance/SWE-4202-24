3d30bb3fb3cb084cbfb7c0eae43ad003aad4ede4
ayiham-1012 <ayiham1012@gmail.com>
2026-05-12 12:31:56 +0600

    Class: Seat (Fields & Constructor)

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -1,4 +1,16 @@
 package model;
 
 public class Seat {
+    private int row;
+    private int col;
+    private boolean isBooked;
+    private boolean isPremium;
+
+    Seat(int row, int col, boolean isPremium) {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = true;
+    }
+
 }
