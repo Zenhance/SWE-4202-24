25af8194577aa55c0570ba4ba4612f403ab4effb
arnoboic <arnobarafat@iut-dhaka.edu>
2026-05-12 10:01:10 +0600

    created hall class

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -20,4 +20,44 @@ public class Hall {
             }
         }
     }
+
+    public Seat getSeat(int row, int col){
+        return grid[row][col];
+    }
+
+    public int countAvailable(){
+        int count = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if(grid[i][j].isBooked()) count++;
+            }
+        }
+        return count;
+    }
+
+    // getter methods
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols(){
+        return cols;
+    }
+
+    public void displayLayout() {
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j].isAvailable()) {
+                    if (grid[i][j].isPremium()) System.out.print("*");
+                    else System.out.print(".");
+                } else System.out.print("#");
+            }
+        }
+
+        System.out.println("");
+    }
 }
