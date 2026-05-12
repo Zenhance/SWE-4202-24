ca9f3115ea965edd01c74e10fa5f4754291840f3
tanbirulislam-hash <tanbirulislam@iut-dhaka.edu>
2026-05-12 13:01:07 +0600

    1st

---
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Movie{
+    private int id;
+    private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
+
+    public Movie(int id,String title,String rating,int durationMin,double basePrice){
+      this.id=id;
+      this.title=title;
+      this.rating=rating;
+      this.durationMin=durationMin;
+      this.baseprice=baseprice;}
+
+
+      public int getId(){
+          return id;}
+
+      public String getTitle(){
+          return title;
+        }
+
+      public String getRating(){
+          return rating;}
+
+      public int getDurationMin(){
+          return durationMin;
+      }
+
+      public double getBasePrice(){
+          return baseprice; }
+
+      public int getMinAge(){
+          switch(rating){
+              case "G":
+                  return 0;
+              case "PG":
+                  return 7;
+              case "PG-13":
+                  return 13;
+              case "R":
+                  return 18;}
+       return 0;
