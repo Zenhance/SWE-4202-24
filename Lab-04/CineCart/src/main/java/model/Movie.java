2fc2008f7b440cbaed3ef34473c91bafc8fa3849
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-05-12 09:16:34 +0600

    Added two classes movie and seat inside package model

---
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
+
+    Movie()
+}
