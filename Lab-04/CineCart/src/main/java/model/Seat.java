e35a426c54b7e4c235a522a2ba1f43b55bc8ea3a
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-05-12 13:10:11 +0600

    seat class v1

---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Seat {
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium, boolean isBooked){
+        this.row=row;
+        this.col=col;
+        this.isPremium=isPremium;
+        this.isBooked=false;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getCol(){
+        return col;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public void book(){
+        isBooked =true;
+    }
+
+
+}
