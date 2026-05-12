5a3ae95ff32c97b231251e5113c093efc66530e8
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-05-12 09:24:18 +0600

    created all the fields,constructor,methods(getters) and updated int getMinAge

---
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
+    public Movie(int id, String title, String rating, int durationMin, double basePrice) {
+        this.id = id;
+        this.title = title;
+        this.rating = rating;
+        this.durationMin = durationMin;
+        this.basePrice = basePrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public int getDurationMin() {
+        return durationMin;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+    public int getMinAge(){
+        if (rating.compareTo("G")==0){
+            return 0;}
+        else if (rating.compareTo("PG")==0) {
+            return 7;}
+        else if (rating.compareTo("PG-13")==0) {
+            return 13;}
+        else if (rating.compareTo("R")==0) {
+            return 18;}
+    }
+    public String toString(){
+
+    }
+}
