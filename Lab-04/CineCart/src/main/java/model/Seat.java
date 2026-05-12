46522c19013bb676e55791c9feae6dfd95adad86
tahsin1r <naylakagi@gmail.com>
2026-05-12 09:41:44 +0600

    changed the fields from public to private. And added constructor accordingly

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -1,10 +1,18 @@
 package model;
 
 public class Seat {
-    public int row;
-    public int col;
-    public boolean isPremium;
-    public boolean isBooked;
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium, boolean isBooked){
+        this.row=row;
+        this.col=col;
+        this.isPremium=isPremium;
+        this.isBooked=false;
+    }
+
 
 
     
