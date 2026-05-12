501e0442409cd0788a0b3b31150e8e17f099c1ae
tasnitaraida <tasnitar@gmail.com>
2026-05-12 12:56:36 +0600

    created toString method

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -51,6 +51,6 @@ public class Movie {
     }
 
     public String toString() {
-        return String.format("Inceotion (%s) %dmin - BDT %.2f",);
+        return String.format("Inceotion (%s) %dmin - BDT %.2f",rating,durationMin,basePrice);
     }
 }
