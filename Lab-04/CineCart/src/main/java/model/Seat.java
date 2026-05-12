9e2c9293fb70d25c9d1721000b380280cddb38fe
arnoboic <arnobarafat@iut-dhaka.edu>
2026-05-12 09:23:46 +0600

    created seat class

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -1,4 +1,57 @@
 package model;
 
 public class Seat {
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium) {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = false;
+    }
+
+    // Getter methods
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    // others
+    public void book(){
+        isBooked = true;
+    }
+
+    public void release(){
+        isBooked = false;
+    }
+
+    public boolean isAvailable(){
+        if (isBooked){
+            return false;
+        }
+        else return true;
+    }
+
+    public String toString(){
+        String trailing = "#";
+
+        if (isPremium) trailing = "*";
+
+        return "R"+row+"C"+col+trailing;
+    }
 }
