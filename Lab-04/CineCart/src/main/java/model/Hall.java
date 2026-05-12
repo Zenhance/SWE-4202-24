f56b02989398fdbcec102b555385f72189b6f0ac
Tnvr09 <rahmantanvir64@gmail.com>
2026-05-12 10:31:27 +0600

    Hall class initiated

---
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Hall {
+    private int id;
+    private int rows;
+    private int cols;
+    private Seat[][] grid;
+
+    public Hall(int id, int rows, int cols, int premiumRows) {
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+
+    }
+
+
+
+
+
+
+
+}
