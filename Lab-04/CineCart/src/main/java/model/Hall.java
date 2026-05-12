132d25f4280d89308ba5778065ba3979484bcc0d
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-05-12 13:06:59 +0600

    initial commit

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -1,4 +1,24 @@
 package model;
 
 public class Hall {
+    int id;
+    int rows;
+    int cols;
+    Seat[][] grid;
+
+    public Hall(int id, int rows, int cols, int premiumRows) {
+
+    }
+
+    Seat getSeat(int row, int col) {
+
+    }
+
+    int countAvailable() {
+
+    }
+
+    void displayLayout() {
+
+    }
 }
--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -1,4 +1,33 @@
 package model;
 
 public class Showtime {
+    int id;
+    Movie movie;
+    Hall hall;
+    int startHour;
+    String dateTag;
+
+    public Showtime(int id, Movie movie, Hall hall, int startHour, String dateTag) {
+        this.id = id;
+        this.movie = movie;
+        this.hall = hall;
+        this.startHour = startHour;
+        this.dateTag = dateTag;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public Movie getMovie() {
+        return movie;
+    }
+    public Hall hall() {
+        return hall;
+    }
+    public int getStartHour() {
+        return startHour;
+    }
+    public String getDateTag() {
+        return dateTag;
+    }
 }
