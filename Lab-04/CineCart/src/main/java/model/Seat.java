7b1477f8a661995e09beb31ccf2c887359833c35
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-05-12 10:30:33 +0600

    upto this

---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,55 @@
+package model;
+
+public class Seat {
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium, boolean isBooked){
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        isBooked = false;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void book(){
+        isBooked = true;
+    }
+
+    public void release(){
+        isBooked = false;
+    }
+
+    boolean isAvailable(){
+        if(isBooked)
+            return false;
+        else return true;
+    }
+
+    public String toString() {
+        String string = String.format("R%dC%d", row, col);
+        String flag1;
+        if (isPremium) {
+            flag1 = "*";
+            return flag1;
+        }
+
+    }
+
