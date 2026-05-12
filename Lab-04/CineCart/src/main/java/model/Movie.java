db57c378e2b1b10106a981081e28bff248d74f23
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-05-12 09:40:09 +0600

    updated Movie.java

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -3,7 +3,18 @@ public class Movie {
     private int id;
     private String title;
     private String rating;
-    private durationMin;
-    double basePrice;
+    private int durationMin;
+    private double basePrice;
+
+    //constructors
+    public Movie(int id, String title, String rating, int durationMin, double basePrice) {
+
+        this.id = id;
+        this.title = title;
+        this.rating = rating;
+        this.durationMin = durationMin;
+        this.basePrice = basePrice;
+    }
+
 
 }
