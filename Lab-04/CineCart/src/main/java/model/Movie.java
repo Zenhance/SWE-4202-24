cf6b260cef9b7448840c56182b58ff410c1d9478
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-12 09:27:31 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -6,36 +6,50 @@ public class Movie {
     public String rating;
     public int durationMin;
     public double basePrice;
-}
-//constructor
-public Movie(int id,String title,String rating,int durationMin,double basePrice){
-    this.id=id;
-    this.title=title;
-    this.rating=rating;
-    this.duration=durationMin;
-    this.basePrice=basePrice;
-}
-//getters
-public int getId(){
-    return id;
-}
-public String getTitle(){
-    return title;
-}
-public String getRating(){
-    return rating;
-}
-public int getDurationMin(){
-    return durationMin;
-}
-public double getBasePrice(){
-    return basePrice;
-}
-public int getMinAge(){
-    switch(rating){
-        case "G": return 0;
-        case "PG": return 7;
-        case "PG-13": return 13;
-        case "R": return 18;
+
+    //constructor
+    public Movie(int id, String title, String rating, int durationMin, double basePrice) {
+        this.id = id;
+        this.title = title;
+        this.rating = rating;
+        this.durationMin = durationMin;
+        this.basePrice = basePrice;
     }
+
+    //getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public int getDurationMin() {
+        return durationMin;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public int getMinAge() {
+        switch (rating) {
+            case "G":
+                return 0;
+            case "PG":
+                return 7;
+            case "PG-13":
+                return 13;
+            case "R":
+                return 18;
+            default:
+                return 0;
+        }
+    }
+
 }
