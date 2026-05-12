03e1f47e1f4d5d981b0a7cb9996b4ab8634e452a
inanjaki184 <inanjaki184@gmail.com>
2026-05-12 12:48:50 +0600

    partB

---
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Ticket {
+    Showtime showtime;
+    int row;
+    int col;
+    double pricePaid;
+
+    public Ticket(Showtime showtime, int row, int col, double pricePaid){
+        this.showtime=showtime;
+        this.row=row;
+        this.col=col;
+        this.pricePaid=pricePaid;
+    }
+
+
+}
