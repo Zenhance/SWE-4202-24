035e6c220440568083605b7ac1801f1dcabde51d
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-05-12 10:31:11 +0600

    last commit

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,3 +1,5 @@
 public class Cart {
 
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
 }
--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -7,4 +7,24 @@ public class Hall {
     private int Seat[][];
 
 
+    //constructors
+    public Hall(int id, int rows, int cols, int premiumRows) {
+
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+    //methods
+    public Seat getSeat(int row, int col) {
+
+    }
+
+    public int countAvailable() {
+
+    }
+
+    public void displayLayout() {
+
+    }
 }
