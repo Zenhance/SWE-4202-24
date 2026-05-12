50e0d4a8f47f2089db223a6674b23d7b3da68c8d
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-05-12 09:35:21 +0600

    methods of movie class commit

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -1,14 +1,37 @@
 package model;
 
 public class Movie {
-     int id;
-     String title;
-    String rating;
-    int durationMin;
-    double basePrice;
+     private int id;
+      private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
 
-    Movie(int id,String title,int durationMin,double basePrice){
-
+    public Movie(int id,String title,String rating,int durationMin,double basePrice){
+   this.id=id;
+   this.title=title;
+   this.rating=rating;
+   this.durationMin=durationMin;
+   this.basePrice=basePrice;
     }
+public int getId(){
+   return id;
+}
 
+public String getTitle(){
+        return title;
+}
+public String getRating(){
+       return rating;
+}
+public int getDurationMin(){
+        return durationMin;
+}
+public double getBasePrice(){
+        return basePrice;
+}
+
+public int getMinAge(){
+
+}
 }
