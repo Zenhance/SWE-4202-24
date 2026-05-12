af045c7b6123b3958a26b984a0672e458be2c64a
Rakib <rakibulhasan4266@gmail.com>
2026-05-12 10:27:51 +0600

    Constructor Created

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -7,4 +7,12 @@ public class Seat
     boolean isPremium;
     boolean isBooked;
 
+    public Seat(int row, int col, boolean isPremium, boolean isBooked)
+    {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = isBooked;
+    }
+
 }
