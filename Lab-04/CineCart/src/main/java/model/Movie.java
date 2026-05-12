0459872a0b86a0fda778bc45b3f199fd9cba9a2f
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-05-12 09:28:05 +0600

    Add constructor

---
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
+
+    public Movie(int id, String title, String rating, int durationMin, double basePrice){
+        this.id = id;
+        this.title = title;
+        this.rating = rating;
+        this.durationMin = durationMin;
+        this.basePrice = basePrice;
+    }
+}
+
