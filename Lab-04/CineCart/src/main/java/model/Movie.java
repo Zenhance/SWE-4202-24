07731e44b40584913771579f2b5341339ce0f9ff
Sadman Raiyan <kamrulseed@gmail.com>
2026-05-12 09:22:38 +0600

    Adding getters method to Movie.java

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -1,11 +1,11 @@
 package model;
 
 public class Movie {
-    int id;
-    String title;
-    String rating;
-    int durationMin;
-    double basePrice;
+    private int id;
+    private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
 
     Movie(int id, String title, String rating, int durationMin, double basePrice) {
         this.id = id;
@@ -13,5 +13,21 @@ public class Movie {
         this.rating = rating;
         this.durationMin = durationMin;
         this.basePrice = basePrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getRating() {
+        return rating;
+    }
+    public int getDurationMin() {
+        return durationMin;
+    }
+    public double getBasePrice() {
+        return basePrice;
     }
 }
