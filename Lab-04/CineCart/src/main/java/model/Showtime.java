ef5eec119b40bd1e31593cd52c51432ff6483c0a
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-05-12 10:03:38 +0600

    S1

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -1,4 +1,17 @@
 package model;
 
 public class Showtime {
+    int id;
+    Movie m;
+    Hall h;
+    int startHour;
+    String dateTag;
+
+    public Showtime(int id, Movie m, Hall h, int startHour, String dateTag) {
+        this.id = id;
+        this.m = m;
+        this.h = h;
+        this.startHour = startHour;
+        this.dateTag = dateTag;
+    }
 }
