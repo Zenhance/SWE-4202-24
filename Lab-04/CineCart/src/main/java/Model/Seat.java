ad792ea624b51f1ab1f5a0200f78e6eaf8c72490
Samin-init <mhsamin10@gmail.com>
2026-05-12 10:24:45 +0600

    Seat class written

---
+++ Lab-04/CineCart/src/main/java/Model/Seat.java
@@ -0,0 +1,58 @@
+package Model;
+
+public class Seat {
+        private int row;
+        private int col;
+        private boolean isPremium;
+        private boolean isBooked;
+
+        public Seat(int row, int col, boolean isPremium) {
+            this.row = row;
+            this.col = col;
+            this.isPremium = isPremium;
+            this.isBooked = false;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getCol() {
+            return col;
+        }
+
+        public boolean isPremium() {
+            return isPremium;
+        }
+
+        public boolean isBooked() {
+            return isBooked;
+        }
+
+        public void book() {
+            isBooked = true;
+        }
+
+        public void release() {
+            isBooked = false;
+        }
+
+        public boolean isAvailable() {
+            return !isBooked;
+        }
+
+        public String toString() {
+            String result = "R" + row + "C" + col;
+
+            if (isPremium) {
+                result += "*";
+            }
+
+            if (isBooked) {
+                result += "#";
+            }
+
+            return result;
+        }
+    }
+
