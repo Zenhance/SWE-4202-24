563c975875a4f53051deab0cdf0497344b978679
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-12 10:31:59 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -31,3 +31,20 @@ public class Hall {
                 }
             }
         }
+        public void displayLayout(){
+            for(int r=0;r<rows;r++){
+                for(int c=0;c<cols;c++){
+                    Seat seat= grid[r][c];
+                    if(seat.isBooked()){
+                        System.out.println("#");
+                    }
+                    else if(seat.isPremium()){
+                    System.out.println("*")
+                    }
+                    else{
+                        System.out.println(".");
+                    }
+                }
+                System.out.println();
+            }
+        }
