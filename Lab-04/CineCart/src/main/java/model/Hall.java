d4b4b98cb47825e53377552f0c7a690217bdf5c5
ayiham-1012 <ayiham1012@gmail.com>
2026-05-12 13:11:04 +0600

    Class: Hall (Fields & Constructor)

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Hall {
+    private int id;
+    private int rows;
+    private int cols;
+    private Seat[][] grid;
+
+    Hall(int id, int rows, int cols, int premiumRows){
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        grid = new Seat[rows+premiumRows][cols];
+    }
 }
