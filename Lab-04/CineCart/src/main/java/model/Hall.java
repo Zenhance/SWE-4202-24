b4d3ed6f352f6ab07f5f75a6dca2bb065cce47ca
Sadman Raiyan <kamrulseed@gmail.com>
2026-05-12 10:36:33 +0600

    Unfinished work

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -6,5 +6,14 @@ public class Hall {
     private int cols;
     private Seat[][] grid;
 
-
+    Hall(int id, int rows, int cols,int premiumRows){
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        grid=new Seat[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+            }
+        }
+    }
 }
