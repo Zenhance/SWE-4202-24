5d911202eee42aea9eacfa6adb510dd64b058ef8
arnoboic <arnobarafat@iut-dhaka.edu>
2026-05-12 10:29:25 +0600

    created Showtime class

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -1,4 +1,42 @@
 package model;
 
 public class Showtime {
+    private int id;
+    private Movie movie;
+    private int start;
+    private int startHour;
+    private String dateTag;
+
+    public Showtime(int id, Movie movie, int start, int startHour, String dateTag){
+        this.id = id;
+        this.movie = movie;
+        this.start = start;
+        this.startHour = startHour;
+        this.dateTag = dateTag;
+    }
+
+    // getters
+    public int getId() {
+        return id;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getStartHour() {
+        return startHour;
+    }
+
+    public String getDateTag() {
+        return dateTag;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public boolean isPeak(){
+
+    }
 }
