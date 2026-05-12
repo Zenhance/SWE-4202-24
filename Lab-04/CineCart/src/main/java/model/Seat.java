e3e155749821d018eb43315c869d75655fdea33d
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-05-12 09:39:18 +0600

    created all the fields and constructor

---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Seat {
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium, boolean isBooked) {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = false;
+    }
+}
