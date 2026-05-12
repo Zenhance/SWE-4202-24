2a9b87c4023c7a10f2f2acda1d80c34063e32385
jahinhossain-tech <jahinhossain@iut-dhaka.edu>
2026-05-12 09:39:39 +0600

    created the constructor Movie, created methods for movie.

---
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Movie {
+
+    private int id;
+    private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
+
+    public Movie(int id, String title, String rating, int durationMin, double basePrice){
+
+        this.id=id;
+        this.title=title;
+        this.rating=rating;
+        this.durationMin=durationMin;
+        this.basePrice=basePrice;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public int getDurationMin() {
+        return durationMin;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+}
