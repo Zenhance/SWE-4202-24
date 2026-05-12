d28e5abbe548b386b8a1a85df41c9cf167fd4340
ayiham-1012 <ayiham1012@gmail.com>
2026-05-12 13:27:16 +0600

    Class: Hall (Incomplete Constructor & Methods)

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -8,8 +8,21 @@ public class Hall {
 
     Hall(int id, int rows, int cols, int premiumRows){
         this.id = id;
-        this.rows = rows;
+        this.rows = rows+premiumRows;
         this.cols = cols;
-        grid = new Seat[rows+premiumRows][cols];
+        grid = new Seat[this.rows][this.cols];
+    }
+
+    public Seat getSeat(int row, int col){
+        return grid[row][col];
+    }
+
+    int countAvailable(){
+        int availCount = 0;
+        for(int i = 0; i < rows)
+    }
+
+    void displayLayout(){
+        for()
     }
 }
