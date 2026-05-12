5086c2f14b8f7f3a6a11dcd7fddf4ad4c7bd53d2
wasifjafar <wasifjafar71@gmail.com>
2026-05-12 09:44:03 +0600

    creates getMinAge

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -30,4 +30,12 @@ public class Movie {
     public double getBasePrice() {
         return basePrice;
     }
+
+    public int getMinAge() {
+        if (rating.equals("G")) return 0;
+        if (rating.equals("PG")) return 7;
+        if (rating.equals("PG-13")) return 13;
+        if (rating.equals("R")) return 18;
+        return -1;
+    }
 }
