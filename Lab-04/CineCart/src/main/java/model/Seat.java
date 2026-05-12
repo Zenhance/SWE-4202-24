abd2069c7cd55698e7783200d6fb49396cd05b98
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-05-12 10:32:18 +0600

    created methods and Part A.2 completed

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -12,4 +12,36 @@ public class Seat {
         this.isPremium = isPremium;
         this.isBooked = false;
     }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+    public boolean isAvailable(){
+        return isBooked;
+    }
+    public void book(){
+        isBooked = true;
+    }
+    public void release(){
+        isBooked = false;
+    }
+    public String toString(){
+        String premium = "";
+        String booked = "";
+        if (isPremium){
+            premium = "*";
+        }
+        if (isBooked){
+            booked = "#";
+        }
+        return "R" + row + "C" + col + premium + booked;
+    }
 }
