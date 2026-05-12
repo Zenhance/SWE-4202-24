c7d5f560c6c2a80a2c6a01c4eda8229ce88a971a
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-12 10:33:33 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -13,8 +13,6 @@ public class Hall {
         grid = new Seat[rows][cols];
         for(int r=0;r<rows;r++){
             for(int c=0;c<cols;c++){
-                boolean isPremium = (r < premiumRows);
-                grid[r][c]=
             }
             }
         }
