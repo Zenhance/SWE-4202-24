856f69286c741b5a2b6db29f1c4c9542e7a609b4
Mubtasim Fuad <mubtasimsmail@gmail.com>
2026-05-12 09:32:36 +0600

    created fields, contructor and defined book method

---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Seat {
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium) {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = false;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void book() {
+        isBooked = true;
+    }
+
+}
