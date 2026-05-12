cac5ddc0aa2a0e70c93974ee43148d13116df118
Tnvr09 <rahmantanvir64@gmail.com>
2026-05-12 09:43:03 +0600

    Seat class, constructor, getters create

---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Seat {
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium, boolean isBooked) {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = isBooked;
+    }
+
+    public int getRow() {
+        return row;
+    }
+    public int getCol() {
+        return col;
+    }
+    public boolean getIsPremium() {
+        return isPremium;
+    }
+    public boolean getIsBooked() {
+        return isBooked;
+    }
+
+
+
+
+
+
+
+}
+
