602dbc8f69bf789c0554b61f271c911ffba2cbd0
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-05-12 09:33:57 +0600

    C1

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -1,4 +1,16 @@
 package model;
 
 public class Customer {
+
+    int id;
+    String name;
+    int age;
+    String tier;
+
+    public Customer(int id, String name, int age, String tier) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.tier = tier;
+    }
 }
