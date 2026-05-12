8fcf9f5ba14aa9957e3d99fc4e02b37354e1a1dc
md-musaddik <musaddikibna1@gmail.com>
2026-05-12 09:08:10 +0600

    Movie constructor created

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -6,6 +6,13 @@ public class Movie {
     private String rating;
     private int durationMin;
     private double basePrice;
+    public Movie(int id, String title,String rating, int durationMin, double basePrice){
+        this.id=id;
+        this.title=title;
+        this.rating=rating;
+        this.durationMin=durationMin;
+        this.basePrice=basePrice;
+    }
 
 
 }
