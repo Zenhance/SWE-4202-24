70d99aa95220fa406deb62b747b5fcae21f18156
elfie1502 <mumtahina@iut-dhaka.edu>
2026-05-12 10:25:59 +0600

    seat constructor

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -5,4 +5,11 @@ public class Seat {
     private int col;
     private boolean isPremium;
     private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium, boolean isBooked){
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = false;
+    }
 }
