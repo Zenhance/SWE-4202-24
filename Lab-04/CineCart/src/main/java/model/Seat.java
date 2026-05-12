e39d3989f403e68c94ba8a530e9bcacb90a28ff0
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-05-12 09:48:28 +0600

    Added class with private fields and constructor

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -1,4 +1,16 @@
 package model;
 
 public class Seat {
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    Seat(int row, int col, boolean isPremium, boolean isBooked){
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = isBooked;
+    }
+
 }
