ef97cb2dfe2ae593be648095f1eda58e957ac81f
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-05-12 09:24:53 +0600

    H1

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Hall {
+    int id;
+    int rows;
+    int cols;
+    int premiumRows;
+
+    public Hall(int id, int rows, int cols, int premiumRows) {
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        this.premiumRows = premiumRows;
+    }
 }
