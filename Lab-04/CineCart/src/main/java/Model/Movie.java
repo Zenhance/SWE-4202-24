c61787cd80b33d1a8c162c44dc5103d2566526ce
Samin-init <mhsamin10@gmail.com>
2026-05-12 10:14:03 +0600

    Just wrote the class, bug fix still needed

---
+++ Lab-04/CineCart/src/main/java/Model/Movie.java
@@ -0,0 +1,52 @@
+package Model;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
+}
+    public Movie(int id, String title, String rating, int durationMin, double basePrice){
+    this.id = id;
+    this.title = title;
+    this.rating = rating;
+    this.durationMin = durationMin;
+    this.base price = basePrice;
+    }
+
+    public int getId(){
+        return id;
+ }
+    public String getTitle(){
+        return title;
+        }
+
+        public String getRating(){
+         return rating;
+        }
+        public String getDurationMin(){
+         return durationMin;
+        }
+public double getBasePrice() {
+    return basePrice;
+}
+
+public int getMinAge() {
+    if (rating.equals("G")) {
+        return 0;
+    } else if (rating.equals("PG")) {
+        return 7;
+    } else if (rating.equals("PG-13")) {
+        return 13;
+    } else if (rating.equals("R")) {
+        return 18;
+    }
+    return 0;
+}
+
+public String toString() {
+    return title + " (" + rating + ") " + durationMin + "min - BDT " +
+            String.format("%.2f", basePrice);
+}
+}
