85d23065b2414095c6f3118eb9e10e21b698068c
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-12 09:52:24 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -28,5 +28,12 @@ public boolean getIsBooked(){
 public void book(){
     isBooked=true;
 }
+public void release(){
+    isBooked= false;
+}
+public boolean isAvailable(){
+    return isBooked=false;
+}
+
 
 }
