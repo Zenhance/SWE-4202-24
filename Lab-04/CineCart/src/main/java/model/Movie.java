07d087f5e2608ebecfd25afdf3ed55d13d16e676
Hasnat-Farhan <hasnatfarhan@iut-dhaka.edu>
2026-05-12 11:59:09 +0600

    movie seat class untested

---
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.util.Objects;
+
+public class Movie {
+    int id;
+    String title;
+    String rating;
+    int durationMin;
+    double basePrice;
+    Movie(int id, String title, String rating, int durationMin, double basePrice){
+        this.id=id;
+        this.title=title;
+        this.rating=rating;
+        this.durationMin=durationMin;
+        this.basePrice=basePrice;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public int getDurationMin() {
+        return durationMin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getMinAge(){
+        if(Objects.equals(rating, "G")) return 0;
+        else if(Objects.equals(rating, "PG")) return 7;
+        else if(Objects.equals(rating, "PG-13")) return 13;
+        else if(Objects.equals(rating, "R")) return 18;
+        else return -1;
+    }
+    public String toString(){
+        return String.format("%s (%s) %d - BDT %.2f",title,rating,durationMin,basePrice);
+    }
+}
---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Seat {
+    int row, col;
+    boolean isPremium, isBooked=false;
+    Seat(int row,int col, boolean isPremium, boolean isBooked){
+        this.row=row;
+        this.col=col;
+        this.isBooked=isBooked;
+        this.isPremium=isPremium;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+    public void book(){
+        isBooked=true;
+    }
+    public void release()
+    {
+        isBooked=false;}
+    public boolean isAvilable(){
+        return !isBooked;
+    }
+
+    public String toString(){
+        if (isBooked&&isPremium) return String.format("R%dC%d*#",row,col);
+        else if(isPremium&&!isBooked) return String.format("R%dC%d*",row,col);
+        else if (!isPremium&&isBooked)return String.format("R%dC%d#",row,col);
+        else return String.format("R%dC%d",row,col);
+    }
+}
