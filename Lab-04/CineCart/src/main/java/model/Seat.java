03f2ea72a3cac26cf9d54dc68c2fff060fe8b364
wasifjafar <wasifjafar71@gmail.com>
2026-05-12 09:55:10 +0600

    creates Seat class and constructor

---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Seat {
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium) {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        isBooked = false;
+    }
+
+}
