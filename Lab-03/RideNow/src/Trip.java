6b947d4a678bce25529751ccc154acc06e223b1b
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-05-12 08:30:14 +0600

    update on lab 03

--- Lab-03/RideNow/src/Trip.java
+++ Lab-03/RideNow/src/Trip.java
@@ -15,11 +15,33 @@ public class Trip {
     public Trip(Passenger passenger, Driver driver, Location from, Location to, double
             baseFare) {
 
+        //storing five arguments
         this.passenger = passenger;
         this.driver = driver;
         this.from = from;
         this.to = to;
         this.baseFare = baseFare;
+
+        completed = false;
+
+        driver.setAvailable = false;
+
+
+        //methods
+
+        //getters
+        public void getPassenger(){
+
+        }
+
+        public void getDriver(){
+
+        }
+
+        public void
+
+
+
 
     }
 }
