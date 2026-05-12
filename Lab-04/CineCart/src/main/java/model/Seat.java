5d829bb1d6399de7844a1d8f0477ff2fa32e8fa7
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-05-12 12:16:35 +0600

    seat-1

---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Seat {
+    int row;
+    int col;
+    boolean isPremium;
+    boolean isBooked;
+
+    public Seat(int row,int col, boolean isPremium,boolean isBooked){
+        this.row=row;
+        this.col=col;
+        this.isPremium=isPremium;
+        this.isBooked=false;
+    }
+
+
+}
