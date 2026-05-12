56b3e8723570318fe1cadcf0fba4923b182618cd
elfie1502 <mumtahina@iut-dhaka.edu>
2026-05-12 09:32:07 +0600

    git init

---
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
+}
+
+public Movie(int id, String title, String rating, int durationMin, double basePrice){
+    this.id = id;
+    this.title = title;
+    this.rating = rating;
+    this.durationMin = durationMin;
+    this.basePrice = basePrice;
+}
