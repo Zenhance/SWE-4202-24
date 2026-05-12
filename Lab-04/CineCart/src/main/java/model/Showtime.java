a3b5d2bc84e789a9d7bf009c09fefb1ff139b171
inanjaki184 <inanjaki184@gmail.com>
2026-05-12 12:33:01 +0600

    partB

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -1,5 +1,13 @@
 package model;
 
 public class Showtime {
+    int id;
+    Movie movie;
+    Hall hall;
+    int starHour;
+    String dataTag;
 
+    public Showtime(int id, Movie movie, Hall hall, int starHour, String dataTag){
+
+    }
 }
