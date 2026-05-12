cd334cbefaa56bc69038f314e65c4cdbff74fd01
jahinhossain-tech <jahinhossain@iut-dhaka.edu>
2026-05-12 09:55:59 +0600

    created the to string method, also added the get min age method, tried to use a switch statement but didn't work so worked with nested if else.

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -1,21 +1,18 @@
 package model;
 
 public class Movie {
-
     private int id;
     private String title;
     private String rating;
     private int durationMin;
     private double basePrice;
 
-    public Movie(int id, String title, String rating, int durationMin, double basePrice){
-
-        this.id=id;
-        this.title=title;
-        this.rating=rating;
-        this.durationMin=durationMin;
-        this.basePrice=basePrice;
-
+    public Movie(int id, String title, String rating, int durationMin, double basePrice) {
+        this.id = id;
+        this.title = title;
+        this.rating = rating;
+        this.durationMin = durationMin;
+        this.basePrice = basePrice;
     }
 
     public int getId() {
@@ -36,5 +33,23 @@ public class Movie {
 
     public double getBasePrice() {
         return basePrice;
+    }
+
+    public int getMinAge() {
+        if (rating.equals("G")) {
+            return 0;
+        } else if (rating.equals("PG")) {
+            return 7;
+        } else if (rating.equals("PG-13")) {
+            return 13;
+        } else if (rating.equals("R")) {
+            return 18;
+        }
+        return 0;
+    }
+
+    public String toString() {
+        return title + " (" + rating + ") " + durationMin + "min - BDT " +
+                String.format("%.2f", basePrice);
     }
 }
