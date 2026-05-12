86546817ff45253a07c420c0ffb38696041404ed
tuireuit <jamilhasan@iut-dhaka.edu>
2026-05-12 12:57:06 +0600

    hall class

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -1,4 +1,19 @@
 package model;
 
 public class Hall {
+
+    int id;
+    int rows;
+    int cols;
+    Seat[][] grid;
+}
+
+public Hall(int id, int rows, int cols, int premiumRows){
+
+    this.id=id;
+    this.rows=rows;
+    this.cols=cols;
+
+
+
 }
