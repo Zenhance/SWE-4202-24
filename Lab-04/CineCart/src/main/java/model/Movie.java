6b18d40c12a50cc4112aaeff839a8f9d09c3512e
Rakib <rakibulhasan4266@gmail.com>
2026-05-12 09:38:31 +0600

    Constructor created

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -8,4 +8,14 @@ public class Movie
         private int durationMin;
         private double basePrice;
 
+    public Movie(int id, String title, String rating, int durationMin, double basePrice)
+    {
+        this.id = id;
+        this.title = title;
+        this.rating = rating;
+        this.durationMin = durationMin;
+        this.basePrice = basePrice;
+    }
+
+
 }
