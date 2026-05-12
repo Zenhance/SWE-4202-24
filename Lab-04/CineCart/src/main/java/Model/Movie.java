11c92d6b0de3333eb7d051cec9233b93effdf88f
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-05-12 12:40:02 +0600

    movie fields

---
+++ Lab-04/CineCart/src/main/java/Model/Movie.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Movie {
+
+
+
+    private int id;
+    private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
+
+
+
+
+
+}
