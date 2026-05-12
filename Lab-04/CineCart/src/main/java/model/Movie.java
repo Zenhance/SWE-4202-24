ed6eb724c078ae261523d9799330d887b59f4458
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-05-12 09:24:25 +0600

    writting on movie class commit

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Movie {
+     int id;
+     String title;
+    String rating;
+    int durationMin;
+    double basePrice;
+
+    Movie(int id,String title,int durationMin,double basePrice){
+
+    }
+
 }
