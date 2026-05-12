7d4510088d42c17a739f461ff4de1b5944cee902
glockmachigan <muniruzzaman24@iut-dhaka.edu>
2026-05-12 09:39:57 +0600

    add gatId

--- Lab-04/CineCart/src/main/java/Movie.java
+++ Lab-04/CineCart/src/main/java/Movie.java
@@ -5,12 +5,17 @@ public class Movie {
     private int durationMin;
     private double basePrice;
 
-    pulic Movie(int id, String title, String rating, int durationMin, double basePrice){
+    public Movie (int id, String title, String rating, int durationMin, double basePrice){
         this.id = id;
         this.title = title;
         this.rating = rating;
         this.durationMin = durationMin;
         this.basePrice = basePrice;
     }
+
+    public int getId(){
+        return id;
+    }
+
 
 }
