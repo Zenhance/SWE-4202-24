77075212c4af85cc298bf179c635d16cb7289203
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-12 10:04:55 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -5,6 +5,12 @@ public class Hall {
     public int rows;
     public int cols;
     public Seat[][]grid;
+    //constructor
+    public Hall(int id, int rows, int cols, int premiumRows){
+        this.id=id;
+        this.rows=rows;
+        this.cols=cols;
+    }
 
 
 
