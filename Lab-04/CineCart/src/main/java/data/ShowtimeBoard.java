24b6a4efb53432b5c5d69767df8c022c0246f21e
arnoboic <arnobarafat@iut-dhaka.edu>
2026-05-19 04:30:36 +0600

    created ShowtimeBoard class

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,4 +1,35 @@
 package data;
 
+import model.Showtime;
+
 public class ShowtimeBoard {
+    private Showtime[] shows;
+    private int count;
+    private static final int MAX = 30;
+
+    public void add(Showtime s){
+        if (count < MAX){
+            shows[count] = s;
+        }
+    }
+
+    public Showtime findById(int id){
+        Showtime res = null;
+
+        for (Showtime s : shows){
+            if(id == s.getId()){
+                res = s;
+                break;
+            }
+        }
+
+        return res;
+    }
+
+
+    public void displayAll(){
+        for (int i = 0; i < count; i++){
+            System.out.println(shows[i].toString());
+        }
+    }
 }
