4b766fb0bbf026a60edc11ec2b636af00f13d7b2
arnoboic <arnobarafat@iut-dhaka.edu>
2026-05-19 04:51:34 +0600

    created CheckoutEngine class

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -1,4 +1,37 @@
 package engine;
 
+import data.ConcessionMenu;
+import data.ShowtimeBoard;
+import model.Cart;
+import model.Showtime;
+
 public class CheckoutEngine {
+    private ShowtimeBoard board;
+    private ConcessionMenu menu;
+
+    public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu){
+        this.board = board;
+        this.menu = menu;
+    }
+
+    public String bookTicket(Cart cart, int showtimeId, int row, int col){
+        Showtime showtime = board.findById(showtimeId);
+        if (showtime == null){
+            return "Showtime not found";
+        }
+
+        if (cart.getOwner().getAge() < showtime.getMovie().getMinAge()){
+            return String.format("Underage for rating <%s>", showtime.getMovie().getRating());
+        }
+
+        if (showtime.getHall().getSeat(row, col).isBooked()){
+            return "Seat unavailable;"
+        }
+
+        double price = showtime.getMovie().basePrice * (show.isPremium ? 1.30 ∶ 1.00) × (showtime.isPeak() ? 1.20 ∶ 1.00);
+    }
+
+    public String addConcession(Cart cart, String codem int qty){
+
+    }
 }
