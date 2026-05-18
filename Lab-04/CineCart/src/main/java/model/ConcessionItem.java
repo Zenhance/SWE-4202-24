02ca95622b5d18f6792dc3819cd01ff354286fce
arnoboic <arnobarafat@iut-dhaka.edu>
2026-05-19 04:09:44 +0600

    created ConcessionItem class

--- Lab-04/CineCart/src/main/java/model/ConcessionItem.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -1,4 +1,29 @@
 package model;
 
 public class ConcessionItem {
+    private String name;
+    private String code;
+    private double unitPrice;
+
+    public ConcessionItem(String name, String code, double unitPrice){
+        this.name = name;
+        this.code = code;
+        this.unitPrice = unitPrice;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String toString(){
+        return String.format("%s - %s - BDT %.2f", code, name, unitPrice);
+    }
 }
