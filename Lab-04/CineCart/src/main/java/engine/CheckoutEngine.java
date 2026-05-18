41cdce13d5c28038281126bf8429e476c7bb7b64
arnoboic <arnobarafat@iut-dhaka.edu>
2026-05-19 05:01:23 +0600

    finished CheckoutEngine

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -3,6 +3,7 @@ package engine;
 import data.ConcessionMenu;
 import data.ShowtimeBoard;
 import model.Cart;
+import model.ConcessionItem;
 import model.Showtime;
 import model.Ticket;
 
@@ -40,7 +41,19 @@ public class CheckoutEngine {
     }
 
 
-    public String addConcession(Cart cart, String codem int qty){
+    public String addConcession(Cart cart, String code, int qty){
+        ConcessionItem item = menu.findByCode(code);
 
+        if (item == null){
+            return "Item not found";
+        }
+
+        else if (qty <= 0){
+            return "Invalid quantity";
+        }
+
+        else{
+            cart.addItem(item, qty);
+        }
     }
 }
