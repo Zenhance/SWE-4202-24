6128a88143a8ae7baf3c1bfbe34622df6c748b7b
arnoboic <arnobarafat@iut-dhaka.edu>
2026-05-19 04:35:25 +0600

    created ConcessionMenu class

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,4 +1,37 @@
 package data;
 
+import model.ConcessionItem;
+import model.Showtime;
+
+
 public class ConcessionMenu {
+    private ConcessionItem[] items;
+    private int count;
+    private static final int MAX = 30;
+
+    public void add(ConcessionItem c){
+        if (count < MAX){
+            items[count] = c;
+        }
+    }
+
+    public ConcessionItem findByCode(String code){
+        ConcessionItem res = null;
+
+        for (ConcessionItem c : items){
+            if(code.equals(c.getCode())){
+                res = c;
+                break;
+            }
+        }
+
+        return res;
+    }
+
+
+    public void displayAll(){
+        for (int i = 0; i < count; i++){
+            System.out.println(items[i].toString());
+        }
+    }
 }
