867e06b59c646d445f0f4d7ee520d3ed69afac67
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-05-19 04:33:18 +0600

    Update Passenger.java

--- Lab-03/RideNow/src/Passenger.java
+++ Lab-03/RideNow/src/Passenger.java
@@ -1,29 +1,43 @@
-public class Passenger {
+//capitalized the p in passenger
+public class Passenger
+{
+    int id;
+    String name;
+    double rating;
 
-    private final int    id;
-    private final String name;
-    private double       rating;
-
-    public Passenger(int id, String name, double rating) {
-        this.id     = id;
-        this.name   = name;
-        this.rating = rating;
+    public Passenger (int id, String name, double rating)
+    {
+        this.id=id;
+        this.name=name;
+        this.rating=rating;
+    }
+    public Passenger (int id, String name)
+    { this.rating=5.0;
+        this.id=id;
+        this.name=name;
     }
 
-    public Passenger(int id, String name) {
-        this(id, name, 5.0);   // delegate to full constructor
+
+    public int getId()
+    {
+        return id;
     }
-
-    public int    getId()     { return id; }
-    public String getName()   { return name; }
-    public double getRating() { return rating; }
-
-    public void updateRating(double newRating) {
-        this.rating = newRating;
+    public String getName()
+    {
+        return name;
     }
+    public double getRating ()
+    {
+        return rating;
+    }
+    public void updateRating (double newRating)
+    {
+        this.rating=newRating;
 
-    @Override
-    public String toString() {
+    }
+    public String toString()
+    {
         return String.format("Passenger[%d] %s (%.2f)", id, name, rating);
+
     }
 }
