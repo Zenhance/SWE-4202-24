e120425d6a07d075877177e2babef2e07fca3e87
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-05-19 10:35:38 +0600

    initiated with method

---
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Ticket {
+    private Showtime showtime;
+    private int row;
+    private int col;
+    private double pricePaid;
+
+    Ticket(Showtime showtime, int row, int col, double pricePaid){
+        this.showtime = showtime;
+        this.row = row;
+        this.col = col;
+        this.pricePaid = pricePaid;
+    }
+}
