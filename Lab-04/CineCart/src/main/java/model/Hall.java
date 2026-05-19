381f95e2fada779d704f7ec0082bdba1e32731e8
23-Fahad <ifahad2k@gmail.com>
2026-05-19 11:10:53 +0600

    push4

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -5,4 +5,22 @@ public class Hall {
     private int rows;
     private int cols;
     private Seat[][] grid;
+    public Hall(int id, int rows, int cols, int premiumRows) {
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        grid = new Seat[rows][cols];
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+            if(r<premiumRows){
+                grid[r][c]=new Seat(r,c,true);
+            }
+            else {
+                grid[r][c]= new Seat(r,c,false);
+            }
+            }
+        }
+
+
+    }
 }
