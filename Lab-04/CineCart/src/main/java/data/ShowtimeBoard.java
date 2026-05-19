0babfc5dbfd3fb90696f68a0e95b6e7fc0991bf5
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-05-19 12:44:47 +0600

    ShowtimeBoard added

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,5 +1,33 @@
 package data;
+import model.Showtime;
 
 public class ShowtimeBoard {
+    static final int MAX = 30; //Constant
 
+    private final Showtime[] shows;
+    private int count;
+
+    public ShowtimeBoard() {
+        this.shows = new Showtime[MAX];
+    }
+
+    public void add(Showtime s){
+        if(count < MAX){
+            shows[count] = s;
+            count++;
+        }
+    }
+
+    public Showtime findById(int id){
+        for(int i=0;i<count;i++){
+            if(shows[i].getId()==id) return shows[i];
+        }
+        return null;
+    }
+
+    public void displayAll(){
+        for(int i=0;i<count;i++){
+            System.out.println(shows[i].toString());
+        }
+    }
 }
