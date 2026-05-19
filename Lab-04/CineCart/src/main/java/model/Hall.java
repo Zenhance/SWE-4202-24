5a1583d8bbf1010fbc4b05cbafc11e4fec7339b9
shafin-75 <arafatshafin85@gmail.com>
2026-05-19 11:46:19 +0600

    Fully Complete code for class-Hall under Model

---
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Hall {
+    int id;
+    int rows;
+    int cols;
+    Seat [][] grid;
+    Hall(int id, int rows, int cols, int premiumRows){
+        this.id=id;
+        this.rows=rows;
+        this.cols=cols;
+        this.grid= new Seat[rows][cols];
+        for(int r=0;r<rows;r++){
+            for(int c=0;r<cols;c++){
+               boolean isPremium = (r < premiumRows);
+               grid[r][c]=new Seat(r,c,isPremium);
+            }
+        }
+    }
+    int getId(){
+        return id;
+    }
+    int getCols(){
+        return cols;
+    }
+    Seat[][] getGrid(){
+        return grid;
+    }
+    Seat getSeat(int rows,int cols){
+        return grid[rows][cols];
+    }
+    int countAvailable(){
+        int count=0;
+        for(int r=0;r<rows;r++){
+            for(int c=0;r<cols;c++){
+                if(grid[r][c].isAvailable()){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    void displayLayout(){
+        for(int r=0;r<rows;r++){
+            for(int c=0;r<cols;c++){
+                System.out.printf(grid[r][c].toString()+" ");
+            }
+            System.out.printf("\n");
+        }
+    }
+}
--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -6,7 +6,7 @@ public class Seat {
     boolean isPremium;
     boolean isBooked;
 
-    Seat(int row,int col,boolean isPremium,boolean isBooked){
+    Seat(int row,int col,boolean isPremium){
         this.row=row;
         this.col=col;
         this.isPremium=isPremium;
