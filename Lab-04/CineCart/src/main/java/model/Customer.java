a7c54af1bcb11256994322d945cd713b93da7377
jasinAnishaa2005 <anishaajasin.13.04.2005@gmail.com>
2026-05-19 12:39:02 +0600

    updated code

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -8,21 +8,37 @@ public class Customer {
     public Customer(int id, String name, int age) {
         this.id = id;
         this.name = name;
-    loyaltyTier = "BASIC";
+        this.age = age;
+        loyaltyTier = "BASIC";
     }
    public Customer(int id, String name, int age, String tier){
         this.id = id;
         this.name = name;
         this.age = age;
-        tier = sc.nextLine();
+        this.loyaltyTier = tier;
    }
-   double getTierDiscount(){
-     if (loyaltyTier == "GOLD"){
+   public int getId(){
+        return id;
+   }
+   public String getName(){
+        return name;
+   }
+   public int getAge(){
+        return age;
+   }
+   public String getLoyaltyTier(){
+        return loyaltyTier;
+   }
+  public double getTierDiscount(){
+     if (loyaltyTier.equals("GOLD")){
          return 0.15;
      }
-     if(loyaltyTier == )
+     else if(loyaltyTier.equals("SILVER")){
+         return 0.08;
+     }
+     return 0.0;
    }
-   String toString(){
-        return id + name + loyaltyTier;
+   public String toString(){
+        return id +" - " + name +" (" + age +") " + loyaltyTier;
    }
 }
