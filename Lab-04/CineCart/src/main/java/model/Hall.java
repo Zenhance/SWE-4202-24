bb601702ea3a846f0b82a8d9d6dfe1b279b3cbfa
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-05-19 08:36:07 +0600

    Constructor created with given instruction.

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -6,4 +6,34 @@ public class Hall {
     private int cols;
     private Seat[][] grid;
 
+    public Hall(int id, int rows, int cols, int premiumRows) {
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new Seat[rows][cols];
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                boolean premium;
+                if (r < premiumRows) {
+                    premium = true;
+                } else {
+                    premium = false;
+                }
+                grid[r][c] = new Seat(r, c, premium);
+            }
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
 }
