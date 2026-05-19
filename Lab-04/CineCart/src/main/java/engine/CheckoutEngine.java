1d86835722b7a759e554e502f292a5097beb17e5
jasinAnishaa2005 <anishaajasin.13.04.2005@gmail.com>
2026-05-19 13:14:21 +0600

    updated code

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -1,4 +1,13 @@
 package engine;
-
+import data.ShowtimeBoard;
+import data.ConcessionMenu;
 public class CheckoutEngine {
+    ShowtimeBoard board;
+    ConcessionMenu menu;
+
+    public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu){
+        this.board = board;
+        this.menu = menu;
+    }
+    
 }
