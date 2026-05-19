09c74a1767977c5397a5aa1e723c5096d44bdb98
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-05-19 12:30:47 +0600

    initial commit

--- Lab-04/CineCart/src/main/java/model/ConcessionItem.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -1,4 +1,17 @@
 package model;
 
 public class ConcessionItem {
+    String code;
+    String name;
+    double unitPrice;
+
+    public ConcessionItem(String code, String name, double unitPrice) {
+        this.code = code;
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getCode() {
+
+    }
 }
