bfb1b4af9e4f477049344fc1f532fadfc2868eca
md-musaddik <musaddikibna1@gmail.com>
2026-05-19 11:45:06 +0600

    Getters Methods declared.

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -20,7 +20,20 @@ public class Customer {
         this.loyaltyTier=tier;
 
     }
+    public int getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public int getAge() {
+        return age;
+    }
+
+    public String getLoyaltyTier() {
+        return loyaltyTier;
+    }
 
 }
