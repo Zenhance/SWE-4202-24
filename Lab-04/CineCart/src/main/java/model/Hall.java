ffd3e04c4883d57ca06988637fcc616b9c5b329f
58-jahin <jahinhossain@iut-dhaka.edu>
2026-05-19 08:30:17 +0600

    created constructor of hall

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -1,4 +1,16 @@
 package model;
 
 public class Hall {
+    private int id;
+    private int rows;
+    private int cols;
+    private Seat[][] grid;
+
+    public Hall(int id, int rows, int cols, int premiumRows) {
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new Seat[rows][cols];
+
+    }
 }
