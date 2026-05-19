d86a53dac3a11bfe70f442310686a5f33dbc0cfe
Zunied-57 <zunied0411@gmail.com>
2026-05-19 12:46:03 +0600

    getter

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -15,6 +15,26 @@ public class Ticket
         this.pricePaid = pricePaid;
     }
 
+    public Showtime getShowtime()
+    {
+        return showtime;
+    }
+
+    public int getRow()
+    {
+        return row;
+    }
+
+    public int getCol()
+    {
+        return col;
+    }
+
+    public double getPricePaid()
+    {
+        return pricePaid;
+    }
+
     public String toString()
     {
         return "T"+showtime.getId()+" - "+"R"+row+"C"+col+" @ BDT "+String.format("%.2f",pricePaid);
