8c2845f857b3b9b221497296a34555ca10ab7aaf
ayiham-1012 <ayiham1012@gmail.com>
2026-05-19 13:13:00 +0600

    D.1 ShowtimeBoard: Fields and add

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,4 +1,16 @@
 package data;
 
+import model.ConcessionItem;
+import model.Showtime;
+
 public class ShowtimeBoard {
+    private static final int MAX = 30;
+    private Showtime[] shows = new Showtime[MAX];
+    private int count = 0;
+
+    public void add(Showtime s){
+        shows[count] = s;
+        count++;
+    }
+
 }
