646ca05f0e17991ac9cb1e5a72992fcaa0567c08
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-05-19 09:59:10 +0600

    Completed Part B.4.

--- Lab-04/CineCart/src/main/java/model/ConcessionItem.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -10,4 +10,22 @@ public class ConcessionItem {
         this.name = name;
         this.unitPrice = unitPrice;
     }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+    public String toString(){
+        return code
+                +" - "
+                +" - BDT"
+                +String.format("%.2f",unitPrice);
+    }
 }
