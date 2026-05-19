bba272db47958cb175d4fc0e7e5dfd7930b28ad2
Nowshin77 <nowshinnipuni007@gmail.com>
2026-05-19 12:36:02 +0600

    hall and showtime class

---
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Showtime {
+    private int id;
+    private Movie movie;
+    private Hall hall;
+    private int startHour;
+    private String dateTag;
+
+    public showTime(int id, )
+
+}
