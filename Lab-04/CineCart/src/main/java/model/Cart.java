319fd702f15747876ce9bb727539f5c5830f78bb
Zunied-57 <zunied0411@gmail.com>
2026-05-19 12:30:26 +0600

    constructor

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -12,5 +12,15 @@ public class Cart
     public static final int MAX_TICKETS = 20;
     public static final int MAX_ITEMS = 20;
 
+    public Cart(Customer owner)
+    {
+        this.owner = owner;
+        this.itemCount = 0;
+        this.ticketCount = 0;
+        tickets = new Ticket[MAX_TICKETS];
+        items = new ConcessionItem[MAX_ITEMS];
+        qtys = new int[MAX_ITEMS];
+    }
+
 
 }
