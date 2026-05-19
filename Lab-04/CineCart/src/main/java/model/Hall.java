fd2e8fb7d5267568fc9f634e122f61777214482d
mubashir <mubashirulhasan@iut-dhaka.edu>
2026-05-19 09:24:12 +0600

    commit displayLayout

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -44,5 +44,15 @@ public class Hall {
         return count;
     }
 
+    public void displayLayout(){
+        for (int r = 0; r<rows ; r++){
+            for (int c = 0; c<cols; c++){
+                if (grid[r][c].isAvailable()) System.out.println(".");
+                if (grid[r][c].isPremium) System.out.println("*");
+                else System.out.println("#");
+            }
+        }
+    }
+
 }
 
