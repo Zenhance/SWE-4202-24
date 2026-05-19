ea3f9915c41a3f9708415ebf9fc56a06615cd94a
Muhtasim Moin <muhtasim24@iut-dhaka.edu>
2026-05-19 09:45:05 +0600

    Showtime class updated

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -14,4 +14,35 @@ public class Showtime {
         this.startHour=startHour;
         this.dateTag=dateTag;
     }
-}
+
+    public int getId() {
+        return id;
+    }
+
+    public Movie getM() {
+        return m;
+    }
+
+    public Hall getH() {
+        return h;
+    }
+
+    public int getStartHour() {
+        return startHour;
+    }
+
+    public String getDateTag() {
+        return dateTag;
+    }
+    public boolean isPeak(){
+        if (startHour>=18&&startHour<=21)
+            return true;
+        else return false;
+        }
+     public String toString(){
+        String statement ="["+id+"]"+" "+m.title+" @ Hall"+h.id+" - "+startHour+"("+dateTag+")";
+        return statement;
+         }
+    }
+
+
