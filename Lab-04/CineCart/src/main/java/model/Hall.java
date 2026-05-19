16b84e4ff18ced1ae26a7c87384ba068298bbfde
Zunied-57 <zunied0411@gmail.com>
2026-05-19 11:38:27 +0600

    methods

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -6,11 +6,46 @@ public class Hall
     private int cols;
     private Seat[][] grid;
 
-    public Hall(int id, int rows, int cols, Seat grid)
+    public Hall(int id, int rows, int cols, int premiumRows)
     {
         this.id=id;
         this.rows=rows;
         this.cols=cols;
+
+        grid = new Seat[rows][cols];
+        for(int r=0;r<rows;r++)
+        {
+            for(int c=0;c<cols;c++)
+            {
+                boolean premium = (r<premiumRows);
+                grid[r][c] = new Seat(r,c,premium);
+            }
+        }
+    }
+
+    public Seat getSeat(int row, int col)
+    {
+        return grid[row][col];
+    }
+
+    public int countAvailable()
+    {
+        int count=0;
+        for(int r=0;r<rows;r++)
+        {
+            for(int c=0;c<cols;c++)
+            {
+                if(grid[r][c].isAvailable())
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public void displayLayout()
+    {
 
     }
 
