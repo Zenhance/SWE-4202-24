bc76f24b25d669b9b08e2abe26eefd6e66b4ea3a
Zunied-57 <zunied0411@gmail.com>
2026-05-19 12:32:16 +0600

    getters

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -22,5 +22,35 @@ public class Cart
         qtys = new int[MAX_ITEMS];
     }
 
+    public Customer getOwner()
+    {
+        return owner;
+    }
+
+    public Ticket[] getTickets()
+    {
+        return tickets;
+    }
+
+    public int getTicketCount()
+    {
+        return ticketCount;
+    }
+
+    public ConcessionItem[] getItems()
+    {
+        return items;
+    }
+
+    public int[] getQtys()
+    {
+        return qtys;
+    }
+
+    public int getItemCount()
+    {
+        return itemCount;
+    }
+
 
 }
