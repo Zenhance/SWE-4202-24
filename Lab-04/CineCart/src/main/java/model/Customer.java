75deb1bd819ea491b068817e91a70aa7fde31305
23-Fahad <ifahad2k@gmail.com>
2026-05-19 11:30:23 +0600

    push9

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -1,5 +1,32 @@
 package model;
 
 public class Customer {
-
+    int id;
+    String name;
+    int age;
+    String loyaltyTier;
+    public Customer(int id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier = "BASIC";
+    }
+    public Customer(int id, String name, int age, String tier){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier =tier;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getLoyaltyTier() {
+        return loyaltyTier;
+    }
 }
