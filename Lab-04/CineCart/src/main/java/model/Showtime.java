fc3f9481410bf2d5129cdf5d94972da48fbc5522
ahnaf-code <ahnafcode@gmail.com>
2026-05-19 12:54:47 +0600

    make toString( )

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -53,5 +53,8 @@ public class Showtime {
 
     //toString() e.g. "[T17] Inception @ Hall 2 - 19:00 (Fri)".
 
+    public String toString() {
+        return String.format("[T%d] %s @ Hall %d - %d:00 (%s)",id, movie.getTitle(), hall.getId(), startHour, dateTag);
+    }
 
 }
