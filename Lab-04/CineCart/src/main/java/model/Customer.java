e35f01d7b6c12d7138965a79311c3968a381696f
elfie1502 <mumtahina@iut-dhaka.edu>
2026-05-19 09:31:15 +0600

    Customer class filed created and constructor created!

---
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Customer {
+    private int id;
+    private String name;
+    private int age;
+    private String loyaltyTier;
+
+    public Customer(int id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+        loyaltyTier = "BASIC";
+    }
+    public Customer(int id, String name, int age, String tier){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+        this.loyaltyTier = tier;
+    }
+
+
+}
