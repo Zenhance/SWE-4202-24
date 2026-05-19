cb85dc74e93c150845242def1a82c45e029ab507
sabbir240042147 <eftesabbir2725@gmail.com>
2026-05-19 11:47:40 +0600

    showtime class

---
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Showtime {
+    private int id;
+    private Movie movie;
+    private Hall hall;
+    private int startHour;
+    private String dateTag;
+
+    public Showtime (
+    int id, Movie
+    movie,
+    Hall hall, int startHour, String
+    dateTag)
+
+    {
+        this.id = id;
+        this.hall = hall;
+        this.startHour = startHour;
+        this.dateTag = dateTag;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public Hall getHall() {
+        return hall;
+    }
+
+    public int getStartHour() {
+        return startHour;
+    }
+
+    public String getDateTag() {
+        return dateTag;
+    }
+
+    public boolean isPeak() {
+        boolean status = false;
+        if (startHour >= 18 && startHour <= 21) {
+            status = true;
+        }
+        return status;
+    }
+    public String toString(){
+        return "[T"+id+"]"+ movie.getTitle()+"@ Hall"+hall.getId()+"-"+startHour+":00"+"("+dateTag+")";
+    }
+}
