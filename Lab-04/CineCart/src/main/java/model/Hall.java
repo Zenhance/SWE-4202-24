304cc3d92568e905bca499a2ac000678af16f651
jasinAnishaa2005 <anishaajasin.13.04.2005@gmail.com>
2026-05-19 12:27:06 +0600

    updated code

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -10,24 +10,29 @@ public class Hall {
     public int getId(){
         return this.id;
     }
-    public int rows(){
+    public int getRows(){
         return this.rows;
     }
+    public int getCols(){return this.cols;}
+    public Seat[][] getGrid(){return grid;}
+
     public int cols(){
         return this.cols;
+    }
+    public Seat getSeat(int row, int col){
+        return grid[row][col];
     }
     public Hall(int id, int rows, int cols, int premiumRows){
          grid = new Seat[rows][cols];
          for(int r =0; r<rows; r++){
              for(int c=0; c<cols; c++){
-                 boolean isPremium = r<premiumRows;
+                 boolean premium = r<premiumRows;
+                 grid[r][c] = new Seat(r,c,premium);
              }
          }
     }
-    Seat getSeat(int row, int col){
-        return grid[row][col];
-    }
-    int countAvailable(){
+
+    public int countAvailable(){
         int count = 0;
         for(int r=0; r<rows; r++){
             for(int c = 0; c<cols; c++){
@@ -39,7 +44,21 @@ public class Hall {
         return count;
     }
     void displayLayout() {
-
+          for(int r = 0; r<rows; r++){
+              for(int c = 0;c<cols; c++){
+                  Seat s = grid[r][c];
+                  if(!s.isAvailable()){
+                      System.out.print("# ");
+                  }
+                  else if(s.isPremium()){
+                      System.out.print("* ");
+                  }
+                  else{
+                      System.out.print(". ");
+                  }
+              }
+              System.out.println();
+          }
     }
 
 }
