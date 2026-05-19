6132ac7837054d02834416f6412a61700952dd74
Zunied-57 <zunied0411@gmail.com>
2026-05-19 13:09:01 +0600

    add & findById

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,16 +1,37 @@
 package data;
 
+import model.Showtime;
+
 public class ShowtimeBoard
 {
-    private ShowtimeBoard[] shows;
+    private Showtime[] shows;
     private int count;
     private static final int MAX = 30;
 
     public ShowtimeBoard()
     {
-        shows = new ShowtimeBoard[MAX];
+        shows = new Showtime[MAX];
         this.count=0;
     }
 
+    public void add(Showtime s)
+    {
+        if(count<MAX)
+        {
+            shows[count]=s;
+            count++;
+        }
+    }
 
+    public Showtime findById(int id)
+    {
+        for(int i=0;i<count;i++)
+        {
+            if(shows[i].getId()==id)
+            {
+                return shows[i];
+            }
+        }
+        return null;
+    }
 }
