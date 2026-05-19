f6d368b3e83ae4631ac3a540903e06a6e223df35
shafin-75 <arafatshafin85@gmail.com>
2026-05-19 11:14:59 +0600

    Complete the whole code for Seat class under model

---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Seat {
+    int row;
+    int col;
+    boolean isPremium;
+    boolean isBooked;
+
+    Seat(int row,int col,boolean isPremium,boolean isBooked){
+        this.row=row;
+        this.col=col;
+        this.isPremium=isPremium;
+        this.isBooked=false;
+    }
+
+    int getRow(){
+        return row;
+    }
+    int getCol(){
+        return col;
+    }
+    boolean isPremium(){
+        return isPremium;
+    }
+    boolean isBooked(){
+        return isBooked;
+    }
+    void book(){
+        this.isBooked=true;
+    }
+    void release(){
+        this.isBooked=false;
+    }
+    boolean isAvailable(){
+        return !isBooked;
+    }
+    public String toString(){
+        if(isPremium){
+            return String.format("R%dC%d*",row,col);
+        }
+        else if(isBooked){
+            return String.format("R%dC%d#",row,col);
+        }
+        else return String.format("R%dC%d",row,col);
+    }
+}
