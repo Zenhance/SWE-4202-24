c4a331d90403984c09bec51cf236c0371935a82a
ahnaf-code <ahnafcode@gmail.com>
2026-05-19 11:41:24 +0600

    Construct Seat

---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,52 @@
+package model;
+public class Seat {
+
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium) {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+
+        this.isBooked= false;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+
+    public String toString() {
+        String print = String.format("R%dC%d",row,col);
+        if (isBooked)   return print + "#";
+        if (isPremium)  return print + "*";
+        return print;
+    }
+
+    public void book() {
+        isBooked = true;
+    }
+    public void release() {
+        isBooked = false;
+    }
+    public boolean isAvailable() {
+        if (isBooked) return false;
+        return true;
+    }
+
+}
