b8c5c88943bed7aa8bf64905d164c2d492d62bc9
md-musaddik <musaddikibna1@gmail.com>
2026-05-19 11:00:11 +0600

    countAvailable method is declared.

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -26,5 +26,17 @@ public class Hall {
     public Seat getSeat(int row,int col){
         return grid[row][col];
     }
+    public int countAvailable() {
+        int count = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c].isAvailable()) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 
 }
