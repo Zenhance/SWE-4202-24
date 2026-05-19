39c129575dbf0d294b734ba3c889609d04868dde
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-05-19 11:27:17 +0600

    customer class

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -18,9 +18,25 @@ public Customer(int id, String name, int age){
 
 public Customer(int id, String name, int age, String loyaltyTier){
 
+    this.id=id;
+    this.name=name;
+    this.age=age;
+    this.loyaltyTier=loyaltyTier;
+}
 
+public int getID(){
+    return id;
+}
 
+public String getName(){
+    return name;
+}
 
+public int getAge(){
+    return age;
+}
 
+public String getLoyaltyTier(){
+    return loyaltyTier;
 }
 }
