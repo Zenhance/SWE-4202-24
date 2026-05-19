ab40b28b76b65d34bfed9d43f84ee673ff8a186c
Mahdia Hossain <hossainmahdia22@gmail.com>
2026-05-19 12:51:04 +0600

    innit commit

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -2,13 +2,39 @@ package model;
 
 public class Hall {
     private int id;
-    private int row;
+    private int rows;
     private int cols;
     private Seat[][] grid;
 
-    public Hall(int id, int row, int cols, int premiumRows){
+    public Hall(int id, int rows, int cols, int premiumRows){
         this.id = id;
-        this.row = row;
-
+        this.rows = rows;
+        this.cols = cols;
+        grid = new Seat[rows][cols];
+        for (int banana = 0; banana<rows; banana++){
+            for (int potato = 0; potato<cols; potato++){
+                boolean drama = banana <premiumRows;
+                grid[banana][potato] = new Seat(banana, potato, drama, false);
+            }
+        }
+    }
+    public int getId(){
+        return id;
+    }
+    public int getRows() {
+        return rows;
+    }
+    public int getCols(){
+        return cols;
+    }
+    public Seat[][] getGrid(){
+        return grid;
+    }
+    public Seat getSeat(int rows, int cols){
+        return grid[rows][cols];
+    }
+    public int countAvailable(){
+        int freeChair = 0;
+        for (int sleep = 0; sleep < rows; sleep++);
     }
 }
