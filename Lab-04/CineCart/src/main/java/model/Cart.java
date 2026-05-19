b55ac1934824d810dd234c59603acf39ba7c58a6
04-Labiba <sabihalabiba639@gmail.com>
2026-05-19 10:11:29 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -52,4 +52,19 @@ public ConcessionItem[] getItems() {
 public int getitemCount(){
     return itemCount;
 }
+public double sumTicketsPaid(){
+    double sum=0;
+    for(int i=0;i<itemCount;i++){
+        sum+=items[i].getUnitPrice()*qtys[i];
+    }
+    return sum;
+}
+public boolean hasItem(String code){
+    for(int i=0;i<itemCount;i++){
+        if(items[i].getCode().equals(code)){
+            return true;
+        }
+    }
+    return false;
+}
 }
