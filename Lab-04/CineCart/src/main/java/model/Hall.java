4d08f352ae20a9085d49ec6fbc7838e50d8fbbb3
58-jahin <jahinhossain@iut-dhaka.edu>
2026-05-19 08:39:08 +0600

    created 2 loops including: 1. to store data of the rows and cols, used getters to return the private fields, did another loop to see how many seats are available

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -12,5 +12,45 @@ public class Hall {
         this.cols = cols;
         this.grid = new Seat[rows][cols];
 
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                boolean premium = r < premiumRows;
+                grid[r][c] = new Seat(r, c, premium);
+            }
+        }
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+    public Seat getSeat(int row, int col) {
+        return grid[row][col];
+    }
+
+    public int countAvailable() {
+        int count = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c].isAvailable()) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+
+
+
 }
