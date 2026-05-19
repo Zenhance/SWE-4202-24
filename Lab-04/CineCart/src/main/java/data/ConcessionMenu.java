81fcc57f6a029f9e1b72684bbb1aefe10c3a70a8
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-05-19 08:32:07 +0600

    Start 2

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,4 +1,9 @@
 package data;
 
+import model.ConcessionItem;
+
 public class ConcessionMenu {
+    public void add(ConcessionItem concessionItem) {
+
+    }
 }
--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,4 +1,12 @@
 package data;
 
+import model.Showtime;
+
 public class ShowtimeBoard {
+    public void add(Showtime showtime) {
+        int id;
+        int movieId;
+        int hallId;
+        int startHour;
+    }
 }
