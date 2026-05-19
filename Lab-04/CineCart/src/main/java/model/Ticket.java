27ccbf0c0fd6262480cfcb849c9606473224a1ef
shafin-75 <arafatshafin85@gmail.com>
2026-05-19 12:30:05 +0600

    Complete class-Ticket under model

---
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Ticket {
+    Showtime showtime; int row; int col; double pricePaid;
+    Ticket(Showtime showtime, int row, int col, double pricePaid){
+        this.showtime=showtime;
+        this.row=row;
+        this.col=col;
+        this.pricePaid=pricePaid;
+    }
+    Showtime getShowtime(){
+        return showtime;
+    }
+    int getRow(){
+        return row;
+    }
+    int getCol(){
+        return col;
+    }
+    double getPricePaid(){
+        return pricePaid;
+    }
+    public String toString(){
+        return String.format("T%d - R%dC%d @ BDT %.2f", showtime.getId(), row,col,pricePaid);
+    }
+}
