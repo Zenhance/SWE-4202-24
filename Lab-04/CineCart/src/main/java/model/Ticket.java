c12020232bd51f6eab52f01dce77b99f2310ba75
tahsin1r <naylakagi@gmail.com>
2026-05-19 10:10:15 +0600

    declared variables created constructor and added getters in ticket class

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -1,4 +1,28 @@
 package model;
 
 public class Ticket {
+    private Showtime showtime;
+    private int row;
+    private int col;
+    private double pricePaid;
+
+    public Ticket(Showtime showtime, int row, int col, double pricePaid){
+        this.showtime=showtime;
+        this.row=row;
+        this.col=col;
+        this.pricePaid=pricePaid;
+    }
+    public Showtime getShowtime(){
+        return this.showtime;
+    }
+    public int getRow(){
+        return this.row;
+    }
+    public int getCol(){
+        return this.col;
+    }
+    public double getPricePaid(){
+        return this.pricePaid;
+    }
+
 }
