ff38522f09043c5c26f0d05be81a50306744de54
waseefmunir-max <waseef05@gmail.com>
2026-05-19 09:05:25 +0600

    Part A

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -1,31 +1,17 @@
 package model;
 
 public class Seat {
-    int id;
-    int col;
-    boolean isPremium;
-    boolean isBooked;
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
 
-    public Seat(int id, int col, boolean isPremium, boolean isBooked) {
-        this.id = id;
+    public Seat(int row, int col, boolean isPremium, boolean isBooked) {
+        this.row = row;
         this.col = col;
         this.isPremium = isPremium;
         this.isBooked = false;
     }
 
-    public int getId() {
-        return id;
-    }
 
-    public int getCol() {
-        return col;
-    }
-
-    public boolean isPremium() {
-        return isPremium;
-    }
-
-    public boolean isBooked() {
-        return isBooked;
-    }
 }
