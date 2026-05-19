043f7e584b07b9c86dadc8d8001f278c10db0811
Zunied-57 <zunied0411@gmail.com>
2026-05-19 11:46:44 +0600

    cons created

---
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Showtime
+{
+    private int id;
+    private Movie movie;
+    private Hall hall;
+    private int startHour;
+    private String dateTag;
+
+    public Showtime(int id, Movie movie, Hall hall, int startHour, String dateTag)
+    {
+        this.id = id;
+        this.movie = movie;
+        this.hall = hall;
+        this.startHour = startHour;
+        this.dateTag = dateTag;
+    }
+}
