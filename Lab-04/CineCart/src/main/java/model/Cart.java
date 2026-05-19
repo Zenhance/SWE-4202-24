feb80ba77f5b5344a01d2ba8277f6e7ef7038951
shafin-75 <arafatshafin85@gmail.com>
2026-05-19 13:39:47 +0600

    Complete class-Cart under model and model is fully complete now

---
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -0,0 +1,73 @@
+package model;
+
+public class Cart {
+    public static final int MAX_TICKETS =20;
+    public  static  final int MAX_ITEMS=20;
+    Customer owner;
+    Ticket[] tickets;
+    int ticketCount;
+    ConcessionItem[] items;
+    int[] qtys;
+    int itemCount;
+
+    public Cart(Customer owner, Ticket[] tickets, int ticketCount, ConcessionItem[] items, int[] qtys, int itemCount) {
+        this.owner = owner;
+        this.tickets = new Ticket[MAX_TICKETS];
+        this.ticketCount = 0;
+        this.items = new ConcessionItem[MAX_ITEMS];
+        this.qtys = new int[MAX_ITEMS];
+        this.itemCount = 0;
+    }
+    void addTicket(Ticket t){
+        if(ticketCount<MAX_ITEMS){
+            tickets[ticketCount]=t;
+            this.itemCount=0;
+        }
+    }
+    public void addItem(ConcessionItem c, int qty){
+        if(itemCount>=MAX_ITEMS || qty<=0){
+            return;
+        }
+        items[itemCount]=c;
+        qtys[itemCount]=qty;
+        itemCount++;
+    }
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public Ticket[] getTickets() {
+        return tickets;
+    }
+
+    public int getTicketCount() {
+        return ticketCount;
+    }
+
+    public ConcessionItem[] getItems() {
+        return items;
+    }
+
+    public int[] getQtys() {
+        return qtys;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+    public double sumTicketsPaid(){
+        double sum  = 0.0;
+        for(int i=0; i<itemCount ; i++){
+            sum+=items[i].getUnitPrice()*qtys[i];
+        }
+        return sum;
+    }
+    boolean hasItem(String code){
+        for(int i=0;i<itemCount;i++){
+            if(items[i].getCode().equals(code)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
