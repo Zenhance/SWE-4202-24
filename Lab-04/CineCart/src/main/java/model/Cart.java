de75cc2a1a0b6a3c319b01cd8bc4839b37adae38
wasifjafar <wasifjafar71@gmail.com>
2026-05-19 10:28:04 +0600

    create partial Cart

---
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Cart {
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
+
+    private Customer owner;
+    private Ticket[] tickets;
+    private int ticketCount;
+    private ConcessionItem[] items;
+    private int[] qtys;
+    private int itemCount;
+
+    public Cart(Customer owner) {
+        this.tickets = new Ticket[MAX_TICKETS];
+        this.items = new ConcessionItem[MAX_ITEMS];
+        ticketCount = 0;
+        itemCount = 0;
+    }
+}
