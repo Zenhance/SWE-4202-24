afdf156eeb1e1443795f7d365439734783906f89
Tasauf Abdullah <tasaufalter@gmail.com>
2026-05-19 11:15:56 +0600

    add constructor to hall

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -5,4 +5,21 @@ public class Hall {
     private int rows;
     private int cols;
     private Seat[][] grid;
+
+    public Hall(int id, int rows, int cols, int premiumRows){
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new Seat[rows][cols]
+
+
+        for(int r = 0; r < rows, r++){
+            for (int c = 0; c < cols; c++){
+                grid[r][c] = new Seat(r, c, r < premiumRows);
+            }
+        }
+
+
+
+    }
 }
