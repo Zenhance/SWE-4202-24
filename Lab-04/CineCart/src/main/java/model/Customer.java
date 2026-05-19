5325e8da44f585af9195bfec11f0b08e4d7b4d9d
04-Labiba <sabihalabiba639@gmail.com>
2026-05-19 09:02:46 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -11,4 +11,11 @@ public class Customer {
         this.age=age;
         this.loyaltyTier="BASIC";
     }
+    public Customer(int id,String name,int age,String tier){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.loyaltyTier=tier;
+    }
+
 }
