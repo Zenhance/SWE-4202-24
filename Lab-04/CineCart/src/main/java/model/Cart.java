fa5a9130db8d2fa5b56b4831516cf63336b9c839
ayiham-1012 <ayiham1012@gmail.com>
2026-05-19 12:36:01 +0600

    C Cart: Fields

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Cart {
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
+
+    private Customer owner;
+    private  Ticket[] tickets = new Ticket[MAX_TICKETS];
+    private  int ticketCount = 0;
+    private  ConcessionItem[] items = new ConcessionItem[MAX_ITEMS];
+    private  int[] qtys = new int[MAX_ITEMS];
+    private  int itemCount = 0;
+
 }
