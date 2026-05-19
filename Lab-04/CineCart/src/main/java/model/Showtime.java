8ddd8f921f87fca145a2ba7448bdade420630932
sabbir240042147 <eftesabbir2725@gmail.com>
2026-05-19 12:12:57 +0600

    customer class

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -7,13 +7,7 @@ public class Showtime {
     private int startHour;
     private String dateTag;
 
-    public Showtime (
-    int id, Movie
-    movie,
-    Hall hall, int startHour, String
-    dateTag)
-
-    {
+    public Showtime (int id, Movie movie, Hall hall, int startHour, String dateTag) {
         this.id = id;
         this.hall = hall;
         this.startHour = startHour;
