46ec6f1decf4620565e788e904b9e7a53315010a
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-05-19 12:34:59 +0600

    initial commit

--- Lab-04/CineCart/src/main/java/model/ConcessionItem.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -12,6 +12,18 @@ public class ConcessionItem {
     }
 
     public String getCode() {
+        return code;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public String toString() {
+        return String.format("%s - %s - BDT %.2lf", code, name, unitPrice);
     }
 }
