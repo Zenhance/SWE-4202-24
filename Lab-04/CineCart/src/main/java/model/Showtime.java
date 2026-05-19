601471312d6978f467ab53721b277f93ab6cce8f
04-Labiba <sabihalabiba639@gmail.com>
2026-05-19 08:49:01 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -13,4 +13,22 @@ public class Showtime {
         this.startHour=startHour;
         this.dateTag=dateTag;
     }
+    public int getId(){
+        return id;
+    }
+    public Movie getmovie(){
+        return movie;
+    }
+
+    public Hall getHall() {
+        return hall;
+    }
+
+    public int getStartHour() {
+        return startHour;
+    }
+
+    public String getDateTag() {
+        return dateTag;
+    }
 }
