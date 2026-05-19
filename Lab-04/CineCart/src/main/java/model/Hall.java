6f67e4173ba60049e2781b6fd8d0188a6f1df269
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-05-19 11:07:48 +0600

    hall modified

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -1,4 +1,36 @@
 package model;
 
 public class Hall {
+
+private int id;
+private int rows;
+private int cols;
+private Seat[][] grid;
+
+
+    Hall(int id, int rows, int cols, int premiumRows) {
+
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new Seat[rows][cols];
+
+        for(int i = 0; i < rows; i++){
+            boolean premium = (r < premiumRows);
+            for(int j = 0;j < cols;j++){
+                grid[r][c] = new Seat(r,c, premium);
+            }
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
 }
