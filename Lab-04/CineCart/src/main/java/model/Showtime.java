59c36c04c7f0ac62eef0b5dec742dc4b6e397f05
Maomao-starry <aminarahmanshalu@gmail.com>
2026-05-19 11:14:04 +0600

    task

---
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Showtime {
+    private int id;
+    private  Movie movie;
+    private Hall hall;
+    private int startHour;
+    private String dateTag;
+
+    public Showtime(int id,Movie movie,Hall hall,int startHour,String dateTag){
+        this.id=id;
+        this.movie=movie;
+        this.hall=hall;
+        this.startHour=startHour;
+        this.dateTag=dateTag;
+    }
+
+  public int getId(){
+        return id;
+  }
+  public Movie getMovie(){
+        return movie;
+  }
+  public  Hall  getHall(){
+        return hall;
+  }
+    public int getStartHour(){
+        return startHour;
+    }
+    public String getDateTag(){
+        return dateTag;
+    }
+
+    public boolean isPeak(){
+        return startHour>=18 && startHour<=23;
+    }
+
+    @Override
+
+    public String toString(){
+        return "["+id+"]"+movie+"@"+hall+"-"+String.format("%02d:00",startHour)+"("+dateTag+")";
+    }
+
+
+
+
+}
+
