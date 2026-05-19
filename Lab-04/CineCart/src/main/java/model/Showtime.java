f5e71fb8947e0d36493059b0d299cce8b69f865d
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-05-19 08:49:11 +0600

    Added fields

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Showtime {
+    private int id;
+    private Movie movie;
+    private Hall hall;
+    private int startHour;
+    private String dateTag;
+
+    Showtime(int id, Movie movie, Hall hall, int startHour, String dateTag){
+        this.id = id;
+        this.movie = movie;
+        Hall hall = new Hall
+    }
 }
