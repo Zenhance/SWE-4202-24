00970af103057984988968c7dab5c38eb69e52f4
23-Fahad <ifahad2k@gmail.com>
2026-05-19 12:21:43 +0600

    push14

---
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Cart {
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
+    private Customer owner;
+    private Ticket[] tickets;
+    private int ticketCount;
+    private ConcessionItem[] items;
+    private int[] qtys;
+    private int itemCount;
+    public Cart(Customer owner) {
+        this.owner = owner;
+        tickets = new Ticket[MAX_TICKETS];
+        items = new ConcessionItem[MAX_ITEMS];
+        qtys = new int[MAX_ITEMS];
+        ticketCount = 0;
+        itemCount = 0;
+    }
+
+}
