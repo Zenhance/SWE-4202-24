d9e39cde0ac972eff91e3b6077a496726b158633
Rakib <rakibulhasan4266@gmail.com>
2026-05-19 09:50:19 +0600

    Class created

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -18,9 +18,29 @@ public class Hall
             for(int c = 0; c < cols; c++)
             {
                 if(r < premiumRows) grid[r][c] = new Seat(r, c, true);
-                else grid[r][c] = new Seat(r, c, false);
+                else
+                {
+                    grid[r][c] = new Seat(r, c, false);
+                }
             }
         }
     }
+    public Seat getSeat(int row, int col)
+    {
+        return grid[row][col];
+    }
+    public int countAvailable()
+    {
+        int count = 0;
+        for(int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if(grid[i][j].isBooked()) count++;
+            }
+        }
+        return count;
+    }
+
 
 }
