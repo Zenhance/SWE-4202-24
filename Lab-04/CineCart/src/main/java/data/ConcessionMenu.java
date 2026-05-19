13d32262b55409b25dd62979bd1b5209ed1015b6
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-19 09:54:56 +0600

    init commit

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,9 +1,15 @@
 package data;
-import.model.ConcessionItem;
+import
+import model.ConcessionItem;.model.ConcessionItem;
 
 public class ConcessionMenu {
     private static final int MAX=30;
     private ConcessionItem[] items;
     private int count;
+
+    //constructor
+    public ConcessionMenu(){}
+    items= new ConcessionItem[MAX];
+    count=0;
 
 }
