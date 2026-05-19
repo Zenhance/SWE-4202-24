4100f104bde5e609a580355359feb49492e30a2a
Mohammad Safwan Bhuiyan <safwanbhuiyan@iut-dhaka.edu>
2026-05-19 09:47:08 +0600

    git commit

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -11,14 +11,35 @@ public class Showtime {
         this.startHour=startHour;
 
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public Hall getHall() {
+        return hall;
+    }
+
+    public int getStartHour() {
+        return startHour;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public String getDateTag() {
+        return dateTag;
+    }
+
     public boolean isPeak() {
         if (startHour >= 18 && startHour <= 21) {
             return true;
         } else
             return false;
     }
-//    public String toString(){
-//
-//
-//    }
+    public String toString(){
+    return "[T"+movie.getId()+"]"+" "+movie+"@ Hall"+hall.getId()+"-"+startHour+"("+dateTag+")";
+
+    }
     }
