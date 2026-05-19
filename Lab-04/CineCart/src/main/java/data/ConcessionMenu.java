32923325e3844d47ceca90afd9ce384c2af858fa
Zunied-57 <zunied0411@gmail.com>
2026-05-19 13:06:19 +0600

    cons created

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,6 +1,16 @@
 package data;
 
+import model.ConcessionItem;
+
 public class ConcessionMenu
 {
+    private ConcessionItem[] items;
+    private int count;
+    private static final int MAX = 30;
 
+    public ConcessionMenu()
+    {
+        items = new ConcessionItem[MAX];
+        this.count=0;
+    }
 }
