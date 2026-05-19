d51de1331ad5c4cda869b6f24309f2719800bc40
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-05-19 09:22:38 +0600

    Fields set

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -1,10 +1,10 @@
 package model;
 
 public class Hall {
-    int id;
-    int rows;
-    int cols;
-    Seat[][] grid;
+    private int id;
+    private int rows;
+    private int cols;
+    private Seat[][] grid;
 
     Hall(int id, int rows, int cols, int premiumRows){
         this.id = id;
