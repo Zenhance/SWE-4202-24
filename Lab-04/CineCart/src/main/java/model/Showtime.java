7ff046cdbcdb3aae3db963e3fa2ba3ae44568fd2
23-Fahad <ifahad2k@gmail.com>
2026-05-19 11:24:00 +0600

    push8

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -13,4 +13,22 @@ public class Showtime {
         this.startHour = startHour;
         this.dateTag = dateTag;
     }
+    public int getId() {
+        return id;
+    }
+    public Movie getMovie() {
+        return movie;
+    }
+    public Hall getHall() {
+        return hall;
+    }
+    public int getStartHour() {
+        return startHour;
+    }
+    public String getDateTag() {
+        return dateTag;
+    }
+    public boolean isPeak() {
+        return startHour >= 18 && startHour <= 21;
+    }
 }
