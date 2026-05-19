65915ca855296e8b3c85d28003ae3b4014552085
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-05-19 11:25:34 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -1,4 +1,65 @@
 package model;
 
 public class Seat {
+private int row;
+private int col;
+private boolean isPremium;
+private boolean isBooked;
+
+
+public Seat (int row, int col, boolean isPremium)
+{
+    this.row=row;
+    this.col=col;
+    this.isPremium=isPremium;
+    this.isBooked = false;
+
 }
+
+public int getRow()
+{
+return row;
+
+}
+
+public int getCol()
+{
+
+    return col;
+}
+public boolean getIsPremium()
+
+{
+    return isPremium;
+}
+public boolean getIsBooked()
+{
+    return isBooked;
+}
+public void book()
+{
+    isBooked= true;
+}
+public void release()
+{
+    isBooked=false;
+}
+public boolean isAvailable ()
+{
+    return isBooked;
+}
+public String toString ()
+{
+if (isPremium)
+{
+    return  "R3C5*";
+}
+elseif(isBooked)
+    { return "R3C5#";
+    }
+return "";
+}
+
+}
+
+
