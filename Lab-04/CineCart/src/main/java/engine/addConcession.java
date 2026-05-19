8d501064c304179868b14fd359f80cb7857e39c7
ayiham-1012 <ayiham1012@gmail.com>
2026-05-19 13:31:13 +0600

    E CheckoutEngine Constructors

---
+++ Lab-04/CineCart/src/main/java/engine/addConcession.java
@@ -0,0 +1,11 @@
+package engine;
+
+import data.ConcessionMenu;
+
+public class addConcession {
+    private ConcessionMenu menu;
+
+    public addConcession(ConcessionMenu menu){
+        this.menu = menu;
+    }
+}
---
+++ Lab-04/CineCart/src/main/java/engine/bookTicket.java
@@ -0,0 +1,12 @@
+package engine;
+
+import data.ShowtimeBoard;
+
+public class bookTicket {
+    private ShowtimeBoard board;
+
+    public bookTicket(ShowtimeBoard board){
+        this.board = board;
+    }
+
+}
