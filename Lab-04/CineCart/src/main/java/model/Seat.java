3ab6dae46e57ed604f5d7b351a069cb8b313f044
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-05-19 10:51:36 +0600

    seat-1

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -6,7 +6,7 @@ public class Seat {
     boolean isPremium;
     boolean isBooked;
 
-    public Seat(int row,int col, boolean isPremium,boolean isBooked){
+    public Seat(int row,int col, boolean isPremium){
         this.row=row;
         this.col=col;
         this.isPremium=isPremium;
@@ -16,4 +16,28 @@ public class Seat {
     public int getRow(){
         return row;
     }
+    public int getCol(){
+        return col;
+    }
+    public boolean isPremium(){
+        return isPremium;
+    }
+    public boolean isBooked(){
+        return isBooked;
+    }
+
+    public boolean book(){
+        return this.isBooked=true;
+    }
+    public boolean release(){
+        return this.isBooked=false;
+    }
+    public boolean isAvailable(){
+        return !isBooked;
+    }
+
+    public String toString(){
+        return "R"+row+"C"+col+isPremium+isBooked;
+    }
+
 }
