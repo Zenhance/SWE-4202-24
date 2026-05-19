df9648dc20a1211c1031bed416127800e0ee5ab2
mubashir <mubashirulhasan@iut-dhaka.edu>
2026-05-19 08:59:53 +0600

    Hall constructor

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -4,7 +4,18 @@ public class Hall {
     private int id;
     private int rows;
     private int cols;
-    private int Seat[][];
-    private int grid;
+    private Seat[][] grid;
+
+    Hall(int id, int rows, int cols, int premiumRows){
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = Seat[rows][cols];
+        for (int r = 0; r<rows; r++){
+            for (int c = 0; c<cols; c++){
+                grid[r][c] = Seat(r,c,r<premiumRows);
+            }
+        }
+    }
 }
 
