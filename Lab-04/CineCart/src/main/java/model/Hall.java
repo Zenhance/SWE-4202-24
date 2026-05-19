f7c075ca89a7bf9f9e17f2de60ab647f6a68a3ff
elfie1502 <mumtahina@iut-dhaka.edu>
2026-05-19 08:36:58 +0600

    Hall constructor declared!

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -6,5 +6,22 @@ public class Hall {
     private int cols;
     private Seat [][] grid;
 
+public Hall(int id, int rows, int cols, int premiumRows){
+    this.id = id;
+    this.rows = rows;
+    this.cols = cols;
+    boolean premium;
+    for (int r = 0; r < rows; r++){
+        for (int c = 0; c < cols; c++){
+            if(r < premiumRows){
+                premium = true;
+            }
+            else {
+                premium = false;
+            }
 
+            grid[r][c] = new Seat(r, c, premium);
+        }
+    }
+}
 }
