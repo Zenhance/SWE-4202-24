c2e70961ce12238b77cc04c46d4ae254182f814f
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-05-19 08:35:33 +0600

    Added methods

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -1,4 +1,38 @@
 package model;
 
 public class Hall {
+    private int id;
+    private int rows;
+    private int cols;
+    private Seat[][] grid;
+
+    Hall(int id, int rows, int cols, int premiumRows){
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        grid = new Seat[rows][cols];
+
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                boolean isPremium = (r<premiumRows);
+                grid[r][c] = new Seat(r,c,isPremium);
+            }
+        }
+    }
+    Seat getSeat(int row,int col){
+        return Seat[row][col];
+    }
+    int countAvailable(){
+        int count=0;
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                if(grid[r][c].isAvailable()){
+                    count++;}
+            }
+        }
+        return count;
+    }
+    void displayLayout(){
+
+    }
 }
