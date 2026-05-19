d99c3eab3a4ff0dde7139f0d4d8991e6b319b664
tahsin1r <naylakagi@gmail.com>
2026-05-19 09:28:28 +0600

    Added displayLayout method in Hall

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -50,6 +50,24 @@ public class Hall {
         return count;
     }
 
+    public void displayLayout(){
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                if(grid[r][c].isBooked()){
+                    System.out.println("#");
+                }
+                else if (grid[r][c].isPremium()) {
+                    System.out.println("*");
+
+                }
+                else{
+                    System.out.println(".");
+                }
+            }
+        }
+    }
+
+
 
 
 
