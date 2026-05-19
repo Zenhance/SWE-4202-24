0c0be88472810bc6f5034ebc1e5c5829bdfb3001
ahnaf-code <ahnafcode@gmail.com>
2026-05-19 11:43:07 +0600

    Set fields and make constructor of Hall.java

---
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Hall {
+    // Fields: int id, int rows, int cols, Seat[][] grid.
+    // Constructor: Hall(int id, int rows, int cols, int premiumRows) — builds the grid; for each
+    // row index r and column index c, create a Seat with isPremium = (r < premiumRows).
+
+    public int id;
+    public int rows;
+    public int cols;
+    public Seat[][] grid;
+
+    public Hall(int id, int rows, int cols, int premiumRows) {
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new Seat[rows][cols];
+
+        for (int r=0; r<rows; r++) {
+            for (int c = 0; c <cols; c++) {
+                boolean isPremium = (r< premiumRows);
+                grid[r][c] = new Seat(r, c, isPremium);
+
+                }
+            }
+        }
+
+
+
+    }
