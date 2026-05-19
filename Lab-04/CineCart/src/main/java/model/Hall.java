d52638906ad2f8fcaa36b1a3ec54a7d7d8bad6ce
sabbir240042147 <eftesabbir2725@gmail.com>
2026-05-19 11:18:10 +0600

    hall class update

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -9,6 +9,62 @@ public class Hall {
         this.id=id;
         this.rows=rows;
         this.cols=cols;
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                if(r<premiumRows){
+                    grid[r][c]=new Seat(r,c,true);
+                }
+                else{
+                    grid[r][c]=new Seat(r,c,false);
+                }
+            }
+        }
 
+    }
+    public Seat getSeat(int row,int col){
+        return grid[row][col];
+    }
+    public int countAvailable(){
+        int count=0;
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                if(grid[r][c].isBooked()){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public int getId(){
+        return id;
+    }
+    public int getRows(){
+        return rows;
+    }
+    public int getCols(){
+        return cols;
+    }
+
+    public Seat[][] getGrid() {
+        return grid;
+    }
+
+    public void displayLayout() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j].isAvailable()) {
+                    if (grid[i][j].isPremium()) {
+                        System.out.println("*");
+                    }
+                    else{
+                        System.out.println(".");
+                    }
+                }
+                else {
+                    System.out.println("#");
+                }
+            }
+        }
+        System.out.println("");
     }
 }
