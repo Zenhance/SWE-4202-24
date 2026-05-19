86dcb7cdefc611075a072fd1a7f6a6e05015d2e6
Mahdia Hossain <hossainmahdia22@gmail.com>
2026-05-19 11:38:03 +0600

    innit commit

--- Lab-04/CineCart/src/main/java/module/Movie.java
+++ Lab-04/CineCart/src/main/java/module/Movie.java
@@ -27,10 +27,13 @@ public class Movie{
         return basePrice;
     }
     public int getMinAge(){
-        if (rating equals("G")) return 0;
-        if (rating equals("PG")) return 7;
-        if (rating equals("PG-13")) return 13;
-        if (rating equals("R")) return 18;
+        if (rating.equals("G")) return 0;
+        if (rating.equals("PG")) return 7;
+        if (rating.equals("PG-13")) return 13;
+        if (rating.equals("R")) return 18;
         return 0;
+    }
+    public String toString(){
+        return title + " (" + rating +") " + durationMin + "min - BDT" + String.format("%.2f", basePrice);
     }
 }
