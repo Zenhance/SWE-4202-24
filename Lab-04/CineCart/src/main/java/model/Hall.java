58fc3a30570e172e03ec18b883a7eec658d0485d
ayiham-1012 <ayiham1012@gmail.com>
2026-05-19 11:29:56 +0600

    Hall: Updated

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -5,24 +5,64 @@ public class Hall {
     private int rows;
     private int cols;
     private Seat[][] grid;
+    int availCount = 0;
 
-    Hall(int id, int rows, int cols, int premiumRows){
+    public Hall(int id, int rows, int cols, int premiumRows){
         this.id = id;
-        this.rows = rows+premiumRows;
+        this.rows = rows;
         this.cols = cols;
         grid = new Seat[this.rows][this.cols];
+
+        for (int r = 0; r < rows; r++ ) {
+            for( int c = 0; c < cols; c++) {
+                boolean isp = r < premiumRows;
+                grid[r][c] = new Seat(r, c, isp);
+            }
+        }
     }
 
-    public Seat getSeat(int row, int col){
-        return grid[row][col];
+    public int getRows() {
+        return rows;
     }
 
-    int countAvailable(){
-        int availCount = 0;
-        for(int i = 0; i < rows)
+    public int getCols() {
+        return cols;
+    }
+
+    public Seat[][] getGrid() {
+        return grid;
+    }
+
+    public int getAvailCount() {
+        return availCount;
+    }
+
+    public int getId() {return id;}
+    public Seat getSeat(int row, int col){return grid[row][col];}
+
+    public int countAvailable(){
+        availCount = 0;
+        for (int r = 0; r < rows; r++ ) {
+            for( int c = 0; c < cols; c++){
+                if(grid[r][c].isAvailable())
+                    availCount++;
+            }
+        }
+        return availCount;
     }
 
     void displayLayout(){
-        for()
+        for (int r = 0; r < rows; r++ ) {
+            for( int c = 0; c < cols; c++){
+                Seat s = grid[r][c];
+                if(s.isAvailable()) {
+                    if(s.isPremium())
+                        System.out.print("*");
+                    else System.out.print(".");
+                }
+                else System.out.print("#");
+            }
+            System.out.println();
+        }
     }
 }
