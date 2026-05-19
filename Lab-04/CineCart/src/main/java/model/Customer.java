c4f8f1a7c9e2244f9ea66820878d1aa93b49625c
Sadman Raiyan <kamrulseed@gmail.com>
2026-05-19 09:14:25 +0600

    Adding fields and constructor to Customer.java

---
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Customer {
+    private int id;
+    private String name;
+    private int age;
+    private String loyaltyTier;
+
+    Customer(int id, String name, int age, String loyaltyTier){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier = loyaltyTier;
+    }
+    Customer(int id, String name, int age){
+        this(id, name, age, "BASIC");
+    }
+}
