00451b57af9cf6030e8f76dc52ec6e68f7e62fd6
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-05-19 11:15:28 +0600

    Customer-1

---
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Customer {
+    int id;
+    String name;
+    int age;
+    String loyaltyTier;
+
+    public Customer(int id, String name, int age){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.loyaltyTier="BASIC";
+    }
+
+    public Customer(int id, String name, int age, String loyaltyTier){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.loyaltyTier=loyaltyTier;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+}
