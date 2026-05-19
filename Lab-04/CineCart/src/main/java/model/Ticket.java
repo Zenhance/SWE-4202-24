ba72a6f46b34cfb3d9728409619f8a145ea38605
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-05-19 10:31:09 +0600

    Added Ticket class

---
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Ticket {
+    private Showtime showtime;
+    private int row;
+    private int col;
+    private double pricePaid;
+
+    Ticket(Showtime showtime, int row, int col, double pricePaid){
+        this.showtime = showtime;
+        this.row = row;
+        this.col = col;
+        this.pricePaid = pricePaid;
+    }
+
+    public Showtime getShowtime(){
+        return this.showtime;
+    }
+    public int getRow(){
+        return this.row;
+    }
+    public int getCol(){
+        return this.col;
+    }
+    public double pricePaid(){
+        return this.pricePaid;
+    }
+    public String toString(){
+        return "T"+showtime.id+" - R"+row+"C"+col+" @ BDT "+pricePaid;
+    }
+}
