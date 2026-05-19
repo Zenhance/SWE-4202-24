3ca80e04b06ffe5a623d3e70cdfda0a3504bf9ab
Muhtasim Moin <muhtasim24@iut-dhaka.edu>
2026-05-19 10:14:21 +0600

    ConcessionItem  class updated

--- Lab-04/CineCart/src/main/java/model/ConcessionItem.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -5,5 +5,23 @@ public class ConcessionItem {
     String name;
     double unitPrice;
     public ConcessionItem(String code, String name, double unitPrice) {
+    this.code=code;
+    this.name=name;
+    this.unitPrice=unitPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public String getCode() {
+        return code;
+    }
+    public String toString(){
+        return code+" - "+name+" - BDT "+unitPrice;
     }
 }
