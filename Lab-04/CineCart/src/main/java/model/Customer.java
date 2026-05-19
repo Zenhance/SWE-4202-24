3c971b9fea3c6fefe9ae396996f70196a5eae0fe
04-Labiba <sabihalabiba639@gmail.com>
2026-05-19 09:09:45 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -29,4 +29,18 @@ public class Customer {
     public String getloyaltyTier(){
         return loyaltyTier;
     }
+    public double getTierDiscount(){
+        if(loyaltyTier.equals("GOLD")){
+            return 0.15;
+        }
+        else if(loyaltyTier.equals("SILVER")){
+            return 0.08;
+        }
+        else{
+            return 0.00;
+        }
+    }
+    public String toString(){
+        return id+" - "+name+" ("+loyaltyTier+") ";
+    }
 }
