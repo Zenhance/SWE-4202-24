1896260fa8ac7613a2d22d159dae11ee5170a0b8
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-05-19 12:56:27 +0600

    ticket modification

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Ticket {
+
+    private Showtime showtime;
+    private int row;
+    private int col;
+    private double pricePaid;
+
+    Ticket(Showtime showtime, int row, int col, double pricePaid){
+    this.Showtime = Showtime;
+    this.row = row;
+    this.col = col;
+    this.pricePaid = pricePaid;
+
+
+    }
+
+
 }
