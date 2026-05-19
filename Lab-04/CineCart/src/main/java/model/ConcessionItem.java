56f4ea9636bc0783a148a690f3865d225e18e2d2
md-musaddik <musaddikibna1@gmail.com>
2026-05-19 12:13:24 +0600

    ConcessionItem's private fields and constructor declared.

---
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -0,0 +1,14 @@
+package model;
+
+public class ConcessionItem {
+    private String code;
+    private String name;
+    private double unitPrice;
+
+    public ConcessionItem(String code, String name, double unitPrice){
+        this.code=code;
+        this.name=name;
+        this.unitPrice=unitPrice;
+    }
+
+}
