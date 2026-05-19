89435cef46ff0084a5ed669e761afa20643cfc44
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-05-19 11:57:34 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -1,4 +1,52 @@
 package model;
 
 public class Showtime {
+
+private int id;
+private Movie movie;
+private Hall hall;
+private int startHour;
+private String dateTag;
+
+
+public Showtime (int id, Movie movie, Hall hall, int startHour, String dateTag)
+
+{
+    this.id=id;
+    this.movie=movie;
+    this.hall = hall;
+    this.startHour = startHour;
+    this.dateTag = dateTag;
 }
+public int getId ()
+{return id;}
+    public Movie getMovie()
+    {
+        return movie;
+
+    }
+public Hall getHall()
+{
+    return hall;
+}
+public int getStartHour()
+{
+    return startHour;
+}
+public String getDateTag()
+{
+    return dateTag;
+}
+public boolean isPeak()
+{
+    if (startHour >= 18 && startHour < 21)
+    {
+        return true;
+    } return false;
+}
+public String toString()
+{
+    return ("["+id+"]"+" "+movie.getTitle()+" "+"@"+" "+hall+" "+"-"+" "+startHour+" "+"("+dateTag+")");
+}
+}
+
