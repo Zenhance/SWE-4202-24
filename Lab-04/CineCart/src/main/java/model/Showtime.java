9e7008929f6b95e2ce4c010b65684f7133be3996
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-05-19 11:38:37 +0600

    Showtime class added

---
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Showtime {
+    //int id, Movie movie, Hall hall, int startHour (0–23), String dateTag.
+    private final int id;
+    private final Movie movie;
+    private final Hall hall;
+    private final int startHour;
+    private final String dateTag;
+
+    public Showtime(int id, Movie movie, Hall hall, int startHour, String dateTag){
+        this.id = id;
+        this.movie = movie;
+        this.hall = hall;
+        this.startHour = startHour;
+        this.dateTag = dateTag;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public Movie getMovie(){
+        return movie;
+    }
+    public Hall getHall(){
+        return hall;
+    }
+    public int getStartHour(){
+        return  startHour;
+    }
+    public String getDateTag(){
+        return dateTag;
+    }
+
+    public boolean isPeak(){
+        return startHour>=18 && startHour<=21;
+    }
+    //"[T17] Inception @ Hall 2 - 19:00 (Fri)".
+    public String toString(){
+        return String.format("[T%d] %s @ Hall %d - %d:00 (%s)",id,movie.getTitle(),hall.getId(),startHour,dateTag);
+    }
+}
