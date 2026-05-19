32dacef0a87ad64065abd3cd4f904f320b0a2cb8
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-05-19 10:27:46 +0600

    getters updated

--- Lab-04/CineCart/src/main/java/model/ConcessionItem.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -11,6 +11,18 @@ public class ConcessionItem {
         this.unitPrice = unitPrice;
     }
 
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
     public String toString(){
         String string = String.format("%s - %s - BDT %d.00",code,name,unitPrice);
         return string;
