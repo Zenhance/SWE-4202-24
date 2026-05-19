de224ea41141f10f549c707ebd43e1f4e549cb63
shafin-75 <arafatshafin85@gmail.com>
2026-05-19 12:38:15 +0600

    Complete class-Concessionitem under model

---
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -0,0 +1,24 @@
+package model;
+
+public class ConcessionItem {
+    String code;
+    String name;
+    double unitPrice;
+    ConcessionItem(String code, String name, double unitPrice){
+        this.code=code;
+        this.name=name;
+        this.unitPrice=unitPrice;
+    }
+    String getCode(){
+        return code;
+    }
+    String getName(){
+        return name;
+    }
+    double getUnitPrice(){
+        return unitPrice;
+    }
+    public String toString(){
+        return String.format("%s - %s - BDT .2f", code,name,unitPrice);
+    }
+}
