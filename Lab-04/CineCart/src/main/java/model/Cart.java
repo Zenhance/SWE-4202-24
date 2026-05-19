bb6d8870b60b196967b8690a9842fc15e576cc86
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-05-19 10:20:55 +0600

    fields added

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Cart {
+    private Customer owner;
+    private Ticket[] tickets;
+    private int ticketCount;
+    private ConcessionItem[] items;
+    private int[] qtys;
+    private int itemCount;
+
+
 }
