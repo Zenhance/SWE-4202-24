e1a4c02b7074d138ecbb66a61fd705e57d392d17
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-05-19 10:20:39 +0600

    initiated with fields and constructor

---
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Showtime {
+    private int id;
+    private Movie movie;
+    private Hall hall;
+    private int startHour; // (0-23)
+    private String dateTag;
+
+    Showtime(int id, Movie movie, Hall hall, int startHour, String dateTag){
+        this.id = id;
+        this.movie = movie;
+        this.hall = hall;
+        this.startHour = startHour;
+        this.dateTag = dateTag;
+    }
+
+
+}
+
+
