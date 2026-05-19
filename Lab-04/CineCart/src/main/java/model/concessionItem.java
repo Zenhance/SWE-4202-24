7b80790d40784373ca97fbd8a336b175147e8004
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-05-19 11:50:13 +0600

    concessionItem-1

---
+++ Lab-04/CineCart/src/main/java/model/concessionItem.java
@@ -0,0 +1,27 @@
+package model;
+
+public class concessionItem {
+    String code;
+    String name;
+    double unitPrice;
+
+    public concessionItem(String code,String name,double unitPrice){
+        this.code=code;
+        this.name=name;
+        this.unitPrice=unitPrice;
+    }
+
+    public String getCode(){
+        return code;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getUnitPrice(){
+        return unitPrice;
+    }
+
+    public String toString(){
+        return String.format("%s %s BDT %lf",code,name,unitPrice);
+    }
+}
