82029bf085a90c919756901491e65fbd71403bb7
elfie1502 <mumtahina@iut-dhaka.edu>
2026-05-19 09:30:22 +0600

    Showtime class is declared!

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -6,7 +6,7 @@ public class Showtime {
     private Hall hall;
     private int startHour;
     private String dateTag;
-public Showtime( int id, Movie movie, Hall hall, int startHour, String dateTag){
+public Showtime(int id, Movie movie, Hall hall, int startHour, String dateTag){
     this.id = id;
     this.movie = movie;
     this.hall = hall;
