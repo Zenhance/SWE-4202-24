6314a133ed25501131f381cd0f57a0de41830ef1
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-05-19 11:13:06 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -1,24 +1,54 @@
 package model;
-
 public class Movie {
-private int id;
-private String title;
-private String rating;
-private int durationMin;
-private double basePrice;
+    private int id;
+    private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
+public Movie (int id, String title, String rating, int durationMin, double basePrice) {
 
-public Movie (int id. String title, String rating, int durationMin, double basePrice )
+
+    this.id = id;
+    this.title = title;
+    this.rating = rating;
+    this.durationMin = durationMin;
+    this.basePrice=basePrice;
+}
+public int getid()
 {
-    this.id=id;
-    this.title=title;
-    this.rating=rating;
-    this.durationMin=durationMin;
-    this.baseprice=basePrice;
+    return id;
+}
+public String getTitle()
+{
+    return title;
+}
+public String getRating ()
+{
+    return rating;
+
+}
+public double getBasePrice()
+{
+
+    return basePrice;
+}
+public int getDurationMin ()
+{
+    return durationMin;
 }
 public int getMinAge()
 {
-    return
+    if (rating == "G") return 0;
+    if (rating == "PG") return 13;
+    if (rating == "R") return 18;
+return 0;
 }
+public String toString()
+{
+
+    return title + " " + rating + " " + durationMin + " " + basePrice;
+}
+
 
 
 
