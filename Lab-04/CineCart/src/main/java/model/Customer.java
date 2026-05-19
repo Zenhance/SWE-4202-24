037e99b2530de3f2d3173ac45aa55ec754b88cab
waseefmunir-max <waseef05@gmail.com>
2026-05-19 09:37:28 +0600

    Part A

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -35,4 +35,14 @@ public class Customer {
     public String getLoyaltyTier() {
         return loyaltyTier;
     }
+
+    public double getTierDiscount() {
+        if (loyaltyTier.equals("GOLD")) {
+            return 0.15;
+        } else if (loyaltyTier.equals("SILVER")) {
+            return 0.08;
+        } else {
+            return 0.0;
+        }
+    }
 }
