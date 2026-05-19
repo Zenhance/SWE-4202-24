71a31bd28f7f89e9086cd40e5488e8470b77d471
Mahdia Hossain <hossainmahdia22@gmail.com>
2026-05-19 11:27:22 +0600

    innit commit

--- Lab-04/CineCart/src/main/java/module/Movie.java
+++ Lab-04/CineCart/src/main/java/module/Movie.java
@@ -1,6 +1,4 @@
-package module;
-
-public class Movie {
+public class Movie{
     private int id;
     private int durationMin;
     private String title;
@@ -13,5 +11,26 @@ public class Movie {
         this.durationMin = durationMin;
         this.basePrice = basePrice;
     }
-
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getRating() {
+        return rating;
+    }
+    public int getDurationMin() {
+        return durationMin;
+    }
+    public double getBasePrice() {
+        return basePrice;
+    }
+    public int getMinAge(){
+        if (rating equals("G")) return 0;
+        if (rating equals("PG")) return 7;
+        if (rating equals("PG-13")) return 13;
+        if (rating equals("R")) return 18;
+        return 0;
+    }
 }
