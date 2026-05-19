5550bed0ec520dea072211ee3836e36ca6851a11
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-05-19 10:38:15 +0600

    Implemented upto getters of Customer class

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -2,15 +2,38 @@ package model;
 
 public class Customer {
 
-    int id;
-    String name;
-    int age;
-    String tier;
+    private int id;
+    private String name;
+    private int age;
+    private String loyaltyTier;
+
+    public Customer(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        loyaltyTier = "BASIC";
+    }
 
     public Customer(int id, String name, int age, String tier) {
         this.id = id;
         this.name = name;
         this.age = age;
-        this.tier = tier;
+        this.loyaltyTier = tier;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLoyaltyTier() {
+        return loyaltyTier;
     }
 }
