10d6810f001c945a56971d96ee11f8fcb5e2b05c
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-05-19 10:08:57 +0600

    Implemented getters for hall class

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -4,16 +4,35 @@ public class Hall {
     private int id;
     private int rows;
     private int cols;
-    private int premiumRows;
+    private Seat[][] grid;
 
     public Hall(int id, int rows, int cols, int premiumRows) {
         this.id = id;
         this.rows = rows;
         this.cols = cols;
-        this.premiumRows = premiumRows;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (r < premiumRows) grid[r][c] = new Seat(r, c, true);
+                else grid[r][c] = new Seat(r, c, false);
+            }
+        }
     }
 
+    //implementing getters
     public int getId() {
         return id;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public Seat[][] getGrid() {
+        return grid;
     }
 }
