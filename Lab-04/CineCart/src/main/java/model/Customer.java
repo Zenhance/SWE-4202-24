d74152dcf0f107152ff86d4cc9a373adc840f439
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-05-19 11:47:32 +0600

    Customer class added

---
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Customer {
+    //int id, String name, int age, String loyaltyTier (one of "BASIC", "SILVER", "GOLD").
+    private final int id;
+    private final String name;
+    private final int age;
+    private final String loyaltyTier;
+
+    public Customer(int id, String name, int age, String tier){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier = tier;
+    }
+    public Customer(int id, String name, int age){
+        this(id,name,age,"BASIC");
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getLoyaltyTier(){
+        return loyaltyTier;
+    }
+     public double getTierDiscount(){
+        return switch(loyaltyTier){
+            case "GOLD" -> 0.15;
+            case "SILVER" -> 0.08;
+            default -> 0.00;
+        };
+     }
+
+     public String toString(){
+        return String.format("[%d] %s, Age: %d, Tier: %s", id,name,age,loyaltyTier);
+     }
+}
