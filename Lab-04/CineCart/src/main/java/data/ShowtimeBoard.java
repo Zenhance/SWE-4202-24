587b0b322c822453c8705ea37b3933f3350f96bf
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-19 09:46:41 +0600

    init commit

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,7 +1,16 @@
 package data;
 
+import model.Showtime;
+
+import java.security.PublicKey;
+
 public class ShowtimeBoard {
     private Showtime[] shows;
     private int count;
     private static final int MAX=30;
+}
+
+public ShowtimeBoard(){
+    shows = new Showtime[MAX];
+    count=0;
 }
