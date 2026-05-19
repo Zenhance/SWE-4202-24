d13656301ae7c97c3ac24e8dd6d943a03e783643
Tnvr09 <rahmantanvir64@gmail.com>
2026-05-19 09:11:08 +0600

    Customer class created

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -20,6 +20,29 @@ public class Customer {
         this.loyaltyTier = loyaltyTier;
     }
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getLoyaltyTier() {
+        return loyaltyTier;
+    }
+
+    public double getTierDiscount() {
+        if(loyaltyTier.equals("GOLD")){return 0.15;}
+        else if(loyaltyTier.equals("SILVER")){return 0.08;}
+        else return 0.00;
+    }
+
+    public String toString(){
+        return (id + " " + name + " " + age + " " + loyaltyTier);
+    }
+
 
 
 
