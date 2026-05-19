55fb1227ed65520bd30dccbe65c77b5639ca89b9
Mahdia Hossain <hossainmahdia22@gmail.com>
2026-05-19 12:30:57 +0600

    innit commit

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -1,5 +1,14 @@
 package model;
 
 public class Hall {
+    private int id;
+    private int row;
+    private int cols;
+    private Seat[][] grid;
 
+    public Hall(int id, int row, int cols, int premiumRows){
+        this.id = id;
+        this.row = row;
+
+    }
 }
