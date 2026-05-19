b3cfbc123a3f9dcbc336333255a0e9ddf7b0d852
Rakib <rakibulhasan4266@gmail.com>
2026-05-19 09:46:42 +0600

    created constructor

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -7,4 +7,20 @@ public class Hall
     private int cols;
     private Seat[][] grid;
 
+    public Hall(int id, int rows, int cols, int premiumRows)
+    {
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+
+        for(int r = 0; r < rows; r++)
+        {
+            for(int c = 0; c < cols; c++)
+            {
+                if(r < premiumRows) grid[r][c] = new Seat(r, c, true);
+                else grid[r][c] = new Seat(r, c, false);
+            }
+        }
+    }
+
 }
