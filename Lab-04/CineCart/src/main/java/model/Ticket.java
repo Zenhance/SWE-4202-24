37e6916c40d9591e0932ebb450155243fd42bcb2
ayiham-1012 <ayiham1012@gmail.com>
2026-05-19 12:13:34 +0600

    B.3 Ticket - Fields, Constructors & Getters

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -1,4 +1,30 @@
 package model;
 
 public class Ticket {
+    private Showtime  showtime;
+    private int row;
+    private int col;
+    private double pricePaid;
+
+    public Ticket(Showtime showtime, int row, int col, double pricePaid) {
+        this.showtime = showtime;
+        this.row = row;
+        this.col = col;
+        this.pricePaid = pricePaid;
+    }
+
+    public Showtime getShowtime() {
+        return showtime;
+    }
+
+    public int getRow() {
+        return row;
+    }
+    public int getCol() {
+        return col;
+    }
+
+    public double getPricePaid() {
+        return pricePaid;
+    }
 }
