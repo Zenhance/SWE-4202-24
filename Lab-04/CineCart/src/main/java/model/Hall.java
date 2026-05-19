50f02d46ff82d8d1d682529b3a0e214c276efcaf
Sadman Raiyan <kamrulseed@gmail.com>
2026-05-19 08:24:57 +0600

    Adding Getters to Hall.java

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -6,7 +6,23 @@ public class Hall {
     private int cols;
     private Seat[][] grid;
 
-    Hall(int id, int rows, int cols,int premiumRows){
+    public int getId() {
+        return id;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public Seat[][] getGrid() {
+        return grid;
+    }
+
+    Hall(int id, int rows, int cols, int premiumRows){
         this.id = id;
         this.rows = rows;
         this.cols = cols;
@@ -16,6 +32,7 @@ public class Hall {
                 grid[r][c]=new Seat(r,c,r<premiumRows);
             }
         }
+
 
     }
 }
