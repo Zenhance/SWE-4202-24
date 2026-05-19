f8ee75e62a401b85a09827677ca7e3d88a72a242
wasifjafar <wasifjafar71@gmail.com>
2026-05-19 09:03:03 +0600

    create Customer

---
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Customer {
+    private int id;
+    private String name;
+    private int age;
+    private String loyaltyTier;
+
+    public Customer(int id, String name, int age, String tier) {
+        this.id = id;
+        this.name= name;
+        this.age = age;
+        loyaltyTier = tier;
+    }
+
+    public Customer(int id, String name, int age) {
+        this(id, name, age, "BASIC");
+    }
+}
