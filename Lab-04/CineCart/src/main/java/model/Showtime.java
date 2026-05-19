a126c1901953d1c822ce916654e3c06c7cf32d3b
wasifjafar <wasifjafar71@gmail.com>
2026-05-19 08:51:40 +0600

    create Showtime

---
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Showtime {
+    private int id;
+    private Movie movie;
+    private Hall hall;
+    private int startHour;
+    private String dateTag;
+
+    public Showtime(int id, Movie movie, Hall hall, int startHour, String dateTag) {
+        this.id = id;
+        this.movie = movie;
+        this.hall = hall;
+        if (startHour>=0 && startHour<=23)
+            this.startHour = startHour;
+        this.dateTag = dateTag;
+
+    }
+}
