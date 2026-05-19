e39fb2f48de7335ea4b5dea06ddf1f4b0a9e88ef
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-19 09:14:20 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/ConcessionItem.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -4,4 +4,17 @@ public class ConcessionItem {
     private String code;
     private String name;
     private double unitPrice;
+
+    //constructor
+    public ConcessionItem{
+        this.code=code;
+        this.name=name;
+        this.unitPrice=unitPrice;
+    }
+
+
+
+
+
+
 }
