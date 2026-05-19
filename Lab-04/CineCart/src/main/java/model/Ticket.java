94d6a072b15ace1d8877cf6cb53ec6cd74978dd3
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-05-19 10:37:54 +0600

    all getters and commented out toString method

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -12,4 +12,21 @@ public class Ticket {
         this.col = col;
         this.pricePaid = pricePaid;
     }
+
+    public Showtime getShowtime(){
+        return showtime;
+    }
+    public int getRow(){
+        return row;
+    }
+    public int getCol(){
+        return col;
+    }
+    public double getPricePaid(){
+        return pricePaid;
+    }
+
+//    public String toString(){
+//
+//    }
 }
