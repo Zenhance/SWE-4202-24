02e3f3926042c8d2dd5c10b19015b8d49ab5db46
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-19 09:04:54 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -5,14 +5,18 @@ public class Ticket {
     private int row;
     private int col;
     private double pricePaid;
+    //constructor
     public Ticket{
         this.showtime=showtime;
         this.row=row;
         this.col=col;
         this.pricePaid=pricePaid;
     }
-
-
-
-
+    //getters
+    public Showtime getShowtime() {
+        return Showtime;
+    }
+    public int getRow(){
+        return row;
+    }
 }
