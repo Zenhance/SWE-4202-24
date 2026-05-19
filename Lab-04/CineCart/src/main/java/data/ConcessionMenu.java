bfecdaebbeb043a2e096cc9e90694219bd7d4722
ayiham-1012 <ayiham1012@gmail.com>
2026-05-19 13:21:01 +0600

    D.2 ConcessionMenu: Fields & add

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,4 +1,18 @@
 package data;
 
+import model.ConcessionItem;
+import model.Showtime;
+
 public class ConcessionMenu {
+    private static final int MAX = 30;
+    private ConcessionItem[] items = new ConcessionItem[MAX];
+    private int count;
+
+    public void add(ConcessionItem c){
+        if(count >= MAX)
+            throw new IllegalArgumentException("MAX items exceeded");
+        items[count] = c;
+        count++;
+    }
+
 }
