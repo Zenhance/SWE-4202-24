4ca784c960866519c4810e9ddc67dc16abf24740
Mubtasim Fuad <mubtasimsmail@gmail.com>
2026-05-19 08:49:04 +0600

    created constructor, fields and addTicket method for Cart class

---
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Cart {
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
+
+    private Customer owner;
+    private Ticket[] tickets;
+    private int ticketcount;
+    private ConcessionItem[] items;
+    private int[] qtys;
+    private int itemCount;
+
+    public Cart(Customer owner) {
+        this.owner = owner;
+        this.tickets = new Ticket[MAX_TICKETS];
+        this.items = new ConcessionItem[MAX_ITEMS];
+        this.qtys = new int[MAX_ITEMS];
+        this.ticketcount = 0;
+        this.itemCount = 0;
+    }
+
+    public void addTicket(Ticket t){
+        if(ticketcount < MAX_TICKETS){
+            tickets[ticketcount] = t;
+            ticketcount++;
+        }
+    }
+}
