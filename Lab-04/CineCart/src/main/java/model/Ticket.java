278e16d7eae8097a6f8ea3f301f0f1c8debd3d72
Sadman Raiyan <kamrulseed@gmail.com>
2026-05-19 09:31:57 +0600

    Adding toString to Ticket.java

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -12,4 +12,24 @@ public class Ticket {
         this.col=col;
         this.pricePaid=pricePaid;
     }
+
+    public Showtime getShowtime() {
+        return showtime;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public double getPricePaid() {
+        return pricePaid;
+    }
+
+    public String toString(){
+        return "T"+showtime.getId()+" - R"+row+"C"+col+" @ BDT "+String.format("%.2f",pricePaid);
+    }
 }
