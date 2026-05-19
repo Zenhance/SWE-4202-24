14f20da2fa71c87812ef04deb2654dd7f2b8d75a
jasinAnishaa2005 <anishaajasin.13.04.2005@gmail.com>
2026-05-19 13:32:46 +0600

    updated code

--- Lab-04/CineCart/src/main/java/app/CineCartApp.java
+++ Lab-04/CineCart/src/main/java/app/CineCartApp.java
@@ -1,75 +1,8 @@
 package app;
-import model.Movie;
-import io.CsvLoader;
-import data.ShowtimeBoard;
-import model.Hall;
-import data.ConcessionMenu;
-import model.Customer;
-import engine.CheckoutEngine;
-import model.Cart;
-
 
 public class CineCartApp {
 
     public static void main(String[] args) {
 
-
-        Movie[] movies =
-                CsvLoader.loadMovies("movies.csv");
-
-        Hall[] halls =
-                CsvLoader.loadHalls("halls.csv");
-
-        ShowtimeBoard board =
-                CsvLoader.loadShowtimes(
-                        "showtimes.csv",
-                        movies,
-                        halls
-                );
-
-        ConcessionMenu menu =
-                CsvLoader.loadConcessions(
-                        "concessions.csv"
-                );
-
-        Customer[] customers =
-                CsvLoader.loadCustomers(
-                        "customers.csv"
-                );
-
-
-        CheckoutEngine engine =
-                new CheckoutEngine(board, menu);
-
-
-
-        System.out.println("=== SHOWTIMES ===");
-        board.displayAll();
-
-        System.out.println(
-                engine.bookTicket(cart, 17, 0, 1)
-        );
-
-        System.out.println(
-                engine.bookTicket(cart, 17, 1, 2)
-        );
-        System.out.println(
-                engine.addConcession(cart, "POP", 1)
-        );
-
-        System.out.println(
-                engine.addConcession(cart, "SODA", 1)
-        );
-        double total = engine.checkout(cart);
-
-        System.out.println("\n=== RECEIPT ===");
-        System.out.println(
-                engine.getReceipt(cart)
-        );
-
-        System.out.println(
-                "Final Total: BDT "
-                        + String.format("%.2f", total)
-        );
     }
 }
