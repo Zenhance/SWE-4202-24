a1f2cef5954d1eb3b571f398bb0d4508da30e712
23-Fahad <ifahad2k@gmail.com>
2026-05-19 11:52:19 +0600

    push12

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -12,4 +12,19 @@ public class Ticket {
         this.col = col;
         this.pricePaid = pricePaid;
     }
+    public Showtime getShowtime() {
+        return showtime;
+    }
+    public int getRow() {
+        return row;
+    }
+    public int getCol() {
+        return col;
+    }
+    public double getPricePaid() {
+        return pricePaid;
+    }
+    public String toString(){
+        return String.format("[T%d] - R%dC%d @ BDT %lf",showtime.getId(),row,col,pricePaid);
+    }
 }
