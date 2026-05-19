2852d5cd2155433050e8868f9cd0198ba4989c7c
58-jahin <jahinhossain@iut-dhaka.edu>
2026-05-19 09:19:54 +0600

    created two constructors, including one with the basic loyalty tier and another one with the loyalty tier of the user, set getters

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -1,4 +1,40 @@
 package model;
 
 public class Customer {
+    private int id;
+    private String name;
+    private int age;
+    private String loyaltyTier;
+
+    public Customer(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier = "BASIC";
+    }
+
+    public Customer(int id, String name, int age, String tier) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier = tier;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLoyaltyTier() {
+        return loyaltyTier;
+    }
+
+
 }
