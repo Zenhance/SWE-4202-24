d431002f205cd7dff2224be6dfafa7060e80ac58
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-05-19 09:27:27 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,4 +1,21 @@
 package model;
 
 public class Cart {
+    //constants
+    public static final int MAX_TICKETS=20;
+    public static final int MAX_ITEMS=20;
+    //fields
+    private Customer owner;
+    private Ticket[] tickets;
+    private int ticketCount;
+    private ConcessionItem[] items;
+    private int[] qtys;
+    private int itemCount;
+    //constructor
+    public Cart(Customer owner){
+        this.owner=owner;
+        tickets = new Ticket[MAX_TICKETS];
+        items = new ConcessionItem[MAX_ITEMS];
+        qtys= new int[MAX_ITEMS];
+    }
 }
