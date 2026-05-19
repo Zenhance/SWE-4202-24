d049a3e7cdf2680e86ebe26c2fbe8629ca369047
tahsin1r <naylakagi@gmail.com>
2026-05-19 10:31:26 +0600

    In the Cart class declared final constants and variables. Added constructor.

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,4 +1,23 @@
 package model;
 
 public class Cart {
+    public static final int MAX_TICKETS=20;
+    public static final int MAX_ITEMS=20;
+
+
+    private Customer owner;
+    private Ticket[] tickets;
+    private int ticketCount;
+    private ConcessionItem[] items;
+    private int[] qtys;
+    private int itemCount;
+
+    public Cart(Customer owner){
+        this.owner = owner;
+        this.tickets = new Ticket[MAX_TICKETS];
+        this.items = new ConcessionItem[MAX_ITEMS];
+        this.qtys = new int[MAX_ITEMS];
+        this.ticketCount = 0;
+        this.itemCount = 0;
+    }
 }
