ead0b82566738029bf44b7d96d3950171f4e5be3
Tahmid <tahmidim99@gmail.com>
2026-05-19 11:28:10 +0600

    movie

---
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String rating;
+    private int durationMin;
+    private double basePrice;
+
+    public Movie(int id,String title, String rating, int durationMin,double basePrice){
+        this.id=id;
+        this.title=title;
+        this.rating=rating;
+        this.durationMin=durationMin;
+        this.basePrice=basePrice;
+    }
+    public int getId() {
+        return id;
+    }
+    public int getDurationMin(){
+        return durationMin;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getRating(){
+        return rating;
+    }
+    public double getBasePrice(){
+        return basePrice;
+    }
+    public int getMinAge(){
+        if(rating.equals("G")) return 0;
+        if(rating.equals("PG")) return 7;
+        if(rating.equals("PG-13")) return 13;
+        if(rating.equals("R")) return 18;
+        return 0;
+    }
+    public String toString(){
+        return title + "("+rating+")" +durationMin+"min - BDT" + String.format("%.2f",basePrice);
+    }
+}
