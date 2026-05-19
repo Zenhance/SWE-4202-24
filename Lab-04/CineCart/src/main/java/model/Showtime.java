3c371e6c1ccc6556811c4f70c7a9ccbf1a7aa2c5
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-05-19 11:19:31 +0600

    showtime class

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -45,7 +45,11 @@ public boolean isPeak(){
     else return false;
 }
 
-public String toString(){
+public String toString(){  //"[T17] Inception @ Hall 2 - 19:00 (Fri)".
+
+        String res=String.format("[%d] %s @ Hall %d - %d (%s)",id,movie.getTitle(),hall.getID(),startHour,dateTag);
+
+        return res;
 
 
     }
