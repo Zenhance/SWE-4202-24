51cc59319b7921f27aff9f7ccc2e6a8a61ba0b7e
Muhtasim Moin <muhtasim24@iut-dhaka.edu>
2026-05-19 10:07:03 +0600

    ticket  class updated

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -1,4 +1,35 @@
 package model;
 
 public class Ticket {
+    Showtime showtime;
+    int row;
+    int col;
+    double pricePaid;
+
+    public Ticket(Showtime showtime,int row,int col,double pricePaid)
+    {
+        this.showtime=showtime;
+        this.row=row;
+        this.col=col;
+        this.pricePaid=pricePaid;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public double getPricePaid() {
+        return pricePaid;
+    }
+
+    public Showtime getShowtime() {
+        return showtime;
+    }
+    public String toString(){
+        return showtime.getId()+" - R"+row+"C"+col+" @ BDT "+pricePaid;
+    }
 }
