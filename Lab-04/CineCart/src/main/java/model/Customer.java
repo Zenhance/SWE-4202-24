fa9bdd767e50832405e9563a72e8d139d50a06bf
nusra <nusratnishi1103@gmail.com>
2026-05-19 11:37:58 +0600

    First attempt

---
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Customer {
+    private int id;
+    private String name;
+    private int age;
+    private String loyaltyTier;
+
+    public Customer(int id, String name, int age){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.loyaltyTier="BASIC";
+    }
+
+    public Customer(int id, String name, int age, String tier){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.loyaltyTier=loyaltyTier;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getLoyaltytier(){
+        return loyaltyTier;
+    }
+
+    public double getTierDiscount(){
+        if(loyaltyTier.equals("GOLD")){
+            return 0.15;
+        }
+        else if(loyaltyTier.equals("SILVER")){
+            return 0.08;
+        }
+        else{
+            return 0.00;
+        }
+    }
+
+    public String toString(){
+        return id + " - " + name + " (" + loyaltyTier + ")";
+    }
+}
