2c61aa48c3ddcd7111c00a6ad416884e5afd8bce
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-05-19 08:52:59 +0600

    fixed constructors

---
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Hall {
+    private int id;
+    private int rows;
+    private int cols;
+    private Seat[][] grid;
+
+    public Hall(int id, int rows, int cols, int premiumRows){
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        for (int r=0; r<rows; r++){
+            for (int c=0; c<cols; c++){
+                if (r<premiumRows)
+                    grid[r][c] = new Seat(r,c,true);
+                else grid[r][c] = new Seat(r,c,false);
+            }
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+
+}
