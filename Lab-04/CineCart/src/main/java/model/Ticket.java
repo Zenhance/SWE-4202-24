e969394b5f58cd7ff834edcfd5ac61f33f6af0ac
mahibrahaman175-stack <mahibrahaman175@gmail.com>
2026-05-19 21:07:46 +0600

    Ticket part completed

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -1,4 +1,37 @@
 package model;
 
 public class Ticket {
+    private Showtime showtime;  //--> showtime will contain everything of the class "Showtime";
+    private int row;
+    private int col;
+    private double pricePaid;
+
+    public Ticket(Showtime showtime, int row, int col, double pricePaid) {
+        this.showtime = showtime;
+        this.row = row;
+        this.col = col;
+        this.pricePaid = pricePaid;
+    }
+
+    public Showtime getShowtime() {
+        return showtime;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public double getPricePaid() {
+        return pricePaid;
+    }
+
+
+
+    public String toString(){
+        return String.format("T%d - R%dC%d @ BDT %lf",  showtime.getId(), row, col, pricePaid);
+    }
 }
