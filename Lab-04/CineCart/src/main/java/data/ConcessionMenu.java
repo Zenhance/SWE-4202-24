6ad4a135fbf82e8007b3b62a22748f6a9f601d0f
Zunied-57 <zunied0411@gmail.com>
2026-05-19 13:14:42 +0600

    add & display

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -13,4 +13,21 @@ public class ConcessionMenu
         items = new ConcessionItem[MAX];
         this.count=0;
     }
+
+    public void add(ConcessionItem c)
+    {
+        if(count<MAX)
+        {
+            items[count]=c;
+            count++;
+        }
+    }
+
+    public void displayAll()
+    {
+        for(int i=0;i<count;i++)
+        {
+            System.out.println(items[i]);
+        }
+    }
 }
