b75b40744cf403c77d35fc964f94e1d15f78b252
Mubtasim Fuad <mubtasimsmail@gmail.com>
2026-05-19 09:30:11 +0600

    created fields, add and findById method

---
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -0,0 +1,27 @@
+package data;
+
+
+import model.Showtime;
+
+public class ShowtimeBoard {
+    private Showtime[] shows;
+    private int count;
+    private static final int MAX = 30;
+
+
+    public void add(Showtime s){
+        if(count < MAX){
+            shows[count++] = s;
+        }
+    }
+    public Showtime findById(int id){
+        for(int i = 0; i < count; i++){
+            if(shows[i].getId() == id){
+                return shows[i];
+            }
+        }
+        return null;
+    }
+
+
+}
