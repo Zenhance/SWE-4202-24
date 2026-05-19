dd5c5550b195982b71aee7a96732f0a7acac2663
ayiham-1012 <ayiham1012@gmail.com>
2026-05-19 11:58:39 +0600

    B.2 Customer: Fields & Constructors

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -1,4 +1,21 @@
 package model;
 
 public class Customer {
+    private int id;
+    private String name;
+    private int age;
+    private String loyaltyTier;
+
+    public Customer(int id, String name, int age, String tier) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier = tier;
+    }
+
+    public Customer(int id, String name, int age){
+        this(id, name, age, "BASIC");
+    }
 }
+
+
