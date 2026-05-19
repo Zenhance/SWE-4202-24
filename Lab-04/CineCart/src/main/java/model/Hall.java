836c24102d9b99f1e3532d2dbcc7e06e4e1f3f60
anonymous <farnaz28.rahman@gmail.com>
2026-05-19 11:10:29 +0600

    hall seat

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -9,9 +9,45 @@ public class Hall {
         this.id=id;
         this.rows=rows;
         this.cols=cols;
-        int r=this.rows;
-        int c=this.cols;
+        grid = new Seat[rows][cols];
 
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                boolean premium = r<premiumRows;
+                grid[r][c]=new Seat(r,c,premium);
+            }
+        }
 
     }
+    public Seat getSeat(int rows,int cols){
+        return Seat[rows][cols];
+
+    }
+    int count=0;
+    public int countAvailable(){
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[rows][cols].isAvailable()){
+                    count++;
+                }
+            }
+        }
+    }
+    public void displayLayout(){
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                if(grid[r][c].isAvailable()){
+                    System.out.println(".");
+                }
+                else if(grid[r][c].isBooked()){
+                    System.out.println("#");
+                }
+                else if(grid[r][c].isPremimum()){
+                    System.out.println("*");
+                }
+            }
+        }
+    }
+
+
 }
