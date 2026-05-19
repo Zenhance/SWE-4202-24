cb46ec89995693b10d802a7f61ae152529d8d17f
waseefmunir-max <waseef05@gmail.com>
2026-05-19 09:32:21 +0600

    Part A

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -6,6 +6,13 @@ public class Customer {
     private int age;
     private String loyaltyTier;
 
+    public Customer(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier = "BASIC";
+    }
+
     public Customer(int id, String name, int age, String loyaltyTier) {
         this.id = id;
         this.name = name;
