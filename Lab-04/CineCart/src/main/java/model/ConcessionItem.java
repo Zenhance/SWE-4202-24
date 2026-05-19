b831f7e6d993d3c82a0dcdaddcc5713da0d8ec83
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-05-19 09:43:25 +0600

    Fields and Constructor created for Part B.4.

---
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -0,0 +1,13 @@
+package model;
+
+public class ConcessionItem {
+    private String code;
+    private String name;
+    private double unitPrice;
+
+    public ConcessionItem(String code, String name, double unitPrice) {
+        this.code = code;
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+}
