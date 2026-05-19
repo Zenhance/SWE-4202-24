820120b1da272f65a45d86e45aa17615c2ba979e
cse <ayeshamahjabin@iut-dhaka.edu>
2026-05-19 09:32:39 +0600

    Completed the full Customer and passed all the tests successfully

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -1,4 +1,60 @@
 package model;
 
 public class Customer {
+
+    private int id;
+    private String name;
+    private int age;
+    private String loyaltyTier;
+
+
+    public Customer(int id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier = "BASIC";
+    }
+
+    public Customer(int id, String name, int age, String tier){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.loyaltyTier = tier;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLoyaltyTier() {
+        return loyaltyTier;
+    }
+
+    public double getTierDiscount(){
+        if (loyaltyTier.equals("GOLD")) {
+            return 0.15;
+        } else if (loyaltyTier.equals("SILVER")) {
+            return 0.08;
+        } else {
+            return 0.00;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", loyaltyTier='" + loyaltyTier + '\'' +
+                '}';
+    }
 }
