80d3957ab346d69b2ea504f3f247cf449022c647
md-musaddik <musaddikibna1@gmail.com>
2026-05-19 11:40:24 +0600

    Customer's Constructor declared

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -6,4 +6,21 @@ public class Customer {
     private int age;
     private String loyaltyTier;
 
+    public Customer(int id,String name, int age, String loyaltyTier){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.loyaltyTier="BASIC";
+
+    }
+    public Customer(int id,String name, int age, String loyaltyTier,String tier){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.loyaltyTier=tier;
+
+    }
+
+
+
 }
