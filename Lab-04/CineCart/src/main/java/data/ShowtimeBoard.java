1df41d27dc5f03de6159d463cbdf56cbb3f7027a
Zunied-57 <zunied0411@gmail.com>
2026-05-19 12:59:10 +0600

    cons created

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -2,5 +2,15 @@ package data;
 
 public class ShowtimeBoard
 {
+    private ShowtimeBoard[] shows;
+    private int count;
+    private static final int MAX = 30;
+
+    public ShowtimeBoard()
+    {
+        shows = new ShowtimeBoard[MAX];
+        this.count=0;
+    }
+
 
 }
