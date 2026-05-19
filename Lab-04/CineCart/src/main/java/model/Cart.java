3371f6440bf8ca4bd13f33b4e655e3e55288e594
anonymous <farnaz28.rahman@gmail.com>
2026-05-19 11:57:35 +0600

    constructor

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -10,5 +10,39 @@ int ticketCount;
 ConcessionItem[] items;
 int[] qtys;
  int itemCount;
- public Cart(Customer owner,Ticket[] tickets,int )
+ public Cart(Customer owner,Ticket[] tickets,int ticketCount,ConcessionItem[] items,int[] qtys,int itemCount){
+     this.itemCount=itemCount;
+     this.owner=owner;
+     this.tickets=tickets;
+     this.qtys=qtys;
+     this.itemCount=itemCount;
+     this.items=items;
+
+ }
+ public void addTicket(Ticket t){
+
+ }
+ public Customer getOwner(){
+     return owner;
+ }
+
+    public ConcessionItem[] getItems() {
+        return items;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public int[] getQtys() {
+        return qtys;
+    }
+
+    public int getTicketCount() {
+        return ticketCount;
+    }
+}
+
+public Ticket getTicket(){
+    return
 }
