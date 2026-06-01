fef1911e123982b5db89818d71dae08930febf0f
waseefmunir-max <waseef05@gmail.com>
2026-06-01 19:23:19 +0600

    B.3: Added constructor

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -6,6 +6,12 @@ public class Ticket {
     private int col;
     private double pricePaid;
 
+    public Ticket(Showtime showtime, int row, int col, double pricePaid) {
+        this.showtime = showtime;
+        this.row = row;
+        this.col = col;
+        this.pricePaid = pricePaid;
+    }
 
 
 }
