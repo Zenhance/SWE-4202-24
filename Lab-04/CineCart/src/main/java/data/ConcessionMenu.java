9d0e6c16181e0d29bf42a1c7a1376b44a218ff99
waseefmunir-max <waseef05@gmail.com>
2026-06-01 23:17:47 +0600

    D.2: Added methods - void add(ConcessionItem c); ConcessionItem findByCode(String code); void displayAll()

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -7,5 +7,26 @@ public class ConcessionMenu {
     private int count = 0;
     private ConcessionItem[] items = new ConcessionItem[MAX];
 
+    public void add(ConcessionItem c) {
+        if (count < MAX) {
+            items[count] = c;
+            count++;
+        }
+    }
 
+    public ConcessionItem findByCode(String code) {
+        for (int i = 0; i < count; i++) {
+            if (items[i].getCode().equals(code)) {
+                return items[i];
+            }
+        }
+
+        return null;
+    }
+
+    public void displayAll() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(items[i]);
+        }
+    }
 }
