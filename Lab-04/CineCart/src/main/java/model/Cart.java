ef0ce6ffb359fd4a1aaa7477b2701f411f3c8d3a
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-06-02 08:48:54 +0600

    cart 2

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -19,6 +19,52 @@ public class Cart {
         this.qtys=new int[MAX_ITEMS];
         this.itemCount=0;
 }
+public void addTicket(Ticket t) {
+    if (ticketCount < MAX_TICKET) {
+        tickets[ticketCount] = t;
+        ticketCount++;
+    }
+}
+
+public void addItem(ConcessionItem c,int qty) {
+    if (itemCount < MAX_ITEMS && qty > 0) {
+        items[itemCount] = c;
+        qtys[itemCount] = qty;
+        itemCount++;
+    }
+}
+
+public Customer getOwner() {
+    return owner;
+}
+
+public Ticket[] getTickets() {
+    return tickets;
+}
+
+public int getTicketCount() {
+        return ticketCount;
+    }
+
+public ConcessionItem[] getItems() {
+    return items;
+}
+public int[] getQtys() {
+    return qtys;
+}
+public int getItemCount() {
+    return itemCount;
+}
+
+public double sumTicektsPaid() {
+    double total = 0.0;
+
+    for (int i = 0; i < ticketCount; i++) {
+        total += tickets[i].getPricePaid();
+    }
+    return total;
+}
+
 
 
 
