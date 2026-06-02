adf37c3ab7fad2a0960b66e8a9a56571d1d12d2b
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-06-02 09:04:36 +0600

    showtime 2

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -7,6 +7,20 @@ public class Showtime {
     private int startHour;
     private String dateTag;
     public ShowTime(int id,Movie movie,Hall hall,int startHour,String dateTag){
-
+      this.id=id;
+      this.movie=movie;
+      this.hall=hall;
+      this.startHour=startHour;
+      this.dateTag=dateTag;
     }
+    public int getId(){
+        return id;
+    }
+    public Movie getMovie(){
+        return movie;
+    }
+    public Hall getHall(){
+        return hall;
+    }
+
 }
