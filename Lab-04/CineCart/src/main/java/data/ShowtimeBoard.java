ffe7990b0640bd126ec5ab071913233738222219
Maomao-starry <aminarahmanshalu@gmail.com>
2026-06-02 12:07:23 +0600

    task

---
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -0,0 +1,17 @@
+package data;
+
+import model.Showtime;
+
+public class ShowtimeBoard {
+    private Showtime[] shows;
+    private int count;
+    private static final int MAX = 30;
+
+    public ShowtimeBoard(Showtime[] shows,int count){
+        this.shows=shows;
+        this.count=count;
+    }
+    public void add(Showtime s){
+
+    }
+}
