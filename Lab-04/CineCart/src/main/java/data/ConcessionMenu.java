10302eae8e16b67dab7f8e2098eb3bf21f4f622d
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-06-02 19:33:19 +0600

    ConcessionItem methods creating

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,6 +1,29 @@
 package data;
 
+import model.ConcessionItem;
+
 public class ConcessionMenu {
+
+
+private static final int MAX = 30;
+
+
+private ConcessionItem[] items;
+private int count;
+
+
+    public ConcessionMenu(){
+        items = new ConcessionItem[MAX];
+        count = 0;
+    }
+
+    public void add(ConcessionItem c){
+        if (count < MAX) {
+            items[count] = c;
+            count++;
+        }
+    }
+
 
 
 
