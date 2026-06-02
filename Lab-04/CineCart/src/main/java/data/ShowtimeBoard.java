7a7b5dd0522da263f642fe7470e9cdd52ad8cfbc
Tasauf Abdullah <tasaufalter@gmail.com>
2026-06-02 11:14:19 +0600

    add ShowtimeBoard

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,4 +1,34 @@
 package data;
 
+import model.Showtime;
+
 public class ShowtimeBoard {
+    private Showtime[] shows;
+    private int count;
+    private static final int MAX = 30;
+
+    public void add(Showtime s){
+        if(count < MAX){
+            shows[count] = s;
+            count++;
+        }
+    }
+
+    public Showtime findById(int id){
+        Showtime result = null;
+
+        for(int i = 0; i < MAX; i++){
+            if(shows[i].getId() == id){
+                result = shows[i];
+                break;
+            }
+        }
+        return result;
+    }
+
+    public void displayALL(){
+        for(int i = 0; i < MAX; i++){
+            System.out.println(shows[i].toString());
+        }
+    }
 }
