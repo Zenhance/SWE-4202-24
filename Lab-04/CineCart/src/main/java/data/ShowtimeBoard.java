b4c090361088c24a6faea1fbcb47377650d3ab50
wasifjafar <wasifjafar71@gmail.com>
2026-06-02 08:58:31 +0600

    create ShowtimeBoard class

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,4 +1,15 @@
 package data;
 
+import model.ConcessionItem;
+import model.Showtime;
+
 public class ShowtimeBoard {
+    private Showtime[] shows;
+    private int count;
+    private static final int MAX = 30;
+
+    public ShowtimeBoard() {
+        this.shows = new Showtime[MAX];
+        this.count = 0;
+    }
 }
