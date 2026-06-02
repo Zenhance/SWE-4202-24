daafa97c1acbdc50a125ea07154c1b87200ba9e7
04-Labiba <sabihalabiba639@gmail.com>
2026-06-02 08:44:16 +0600

    init commit

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -4,8 +4,26 @@ public class ConcessionMenu {
     private static final int MAX=30;
     private int count=0;
     private ConcessionItem[] items;
-    public void add(ConcessionItem c){
+    public ConcessionMenu(){
         items=new ConcessionItem[MAX];
         count=0;
     }
+    public void add(ConcessionItem c){
+        if(count<MAX){
+            items[count]=c;
+            count++;
+    }
+    public ConcessionItem findByCode(String code){
+        for(int i=0;i<count;i++){
+            if(items[i].getCode().equals(code)){
+                return items[i];
+            }
+        }
+        return null;
+    }
+        public void displayAll() {
+            for (int i = 0; i < count; i++) {
+                System.out.println(items[i]);
+            }
+        }
 }
