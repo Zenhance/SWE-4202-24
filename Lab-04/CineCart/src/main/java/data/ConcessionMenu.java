6320e5e3f4f2f173223860e32829e18cd792e2a7
ASUS <tasintajwar991@gmail.com>
2026-06-02 17:13:38 +0600

    Fixed all the compilation errors related to "package not found" and "couldnt locate ConcessionMenu"

---
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -0,0 +1,44 @@
+package data;
+
+import model.ConcessionItem;
+
+public class ConcessionMenu {
+
+ConcessionItem[] items=new ConcessionItem[MAX];
+
+int count =0;
+
+private static final int MAX = 30;
+public void add (ConcessionItem c)
+{
+    items [count++]=c;
+}
+public ConcessionItem findByCode(String code)
+
+{
+for (int i=0; i<count;i++)
+{
+    if (items[i].getCode().equals(code))
+    {
+        return items[i];
+    }
+
+}
+return null;
+
+}
+public void displayAll()
+{
+for (int i=0;i<count;i++)
+{
+
+    System.out.println(items[i].toString());
+}
+
+
+
+}
+
+
+
+}
