8411011b2e5201702364a98ebbea9fd753ea1eff
Muhtasim Moin <muhtasim24@iut-dhaka.edu>
2026-06-02 08:35:44 +0600

    ShowtimeBoard  class updated

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -3,6 +3,21 @@ package data;
 import model.Showtime;
 
 public class ShowtimeBoard {
-    public void add(Showtime showtime) {
+    Showtime[] shows;
+    int count;
+    private static final int MAX =30;
+    public void add(Showtime s) {
+        shows[count]=s;
+        count++;
+    }
+    public Showtime findbyId(int id){
+        for (int i=0;i<MAX;i++){
+            if (shows[i].getId()==id);
+            return shows[i];
+        }
+        return null;
+    }
+    public void displayAll(){
+        System.out.println(toString());
     }
 }
