9ab96994749480d5b8c5f8ebe6dc5c398b06455c
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-02 08:36:52 +0600

    final push

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -70,6 +70,25 @@ public class Cart {
         return sum;
     }
 
+    public double sumConcessionsRaw() {
+        double sum = 0.0;
+
+        for (int i = 0; i < itemCount; i++) {
+            sum += items[i].getUnitPrice() * qtys[i];
+        }
+
+        return sum;
+    }
+
+    public boolean hasItem(String code) {
+        for (int i = 0; i < itemCount; i++) {
+            if (items[i].getCode().equals(code)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 
 
