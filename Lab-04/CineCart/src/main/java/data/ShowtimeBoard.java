0e0c08b0f8ab8ad901533244aad05c1b10b3a551
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-06-02 12:35:01 +0600

    showtimeboard

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,8 +1,19 @@
 package data;
 
+import model.Showtime;
+
 public class ShowtimeBoard {
     private int count=0;
     private static final int MAX = 30;
+    private Showtime[] shows = new Showtime[MAX];
+
+    public void add(Showtime s){
+
+
+
+    }
+
+
 
 
 }
