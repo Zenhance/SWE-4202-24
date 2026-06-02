754008459aa385b296cabdac09cef05c80795228
tanbirulislam-hash <tanbirulislam@iut-dhaka.edu>
2026-06-02 12:43:18 +0600

    1st commit

---
+++ Lab-04/CineCart/src/main/java/model/ShowtimeBoard.java
@@ -0,0 +1,15 @@
+package data;
+import model.Showtime;
+
+public class ShowtimeBoard{
+
+    private static final int MAX = 30;
+    private Showtime[] shows;
+    private int count;
+
+    public ShowtimeBoard(){
+        shows=new Showtime[MAX];
+        count=0;
+    }
+
+
