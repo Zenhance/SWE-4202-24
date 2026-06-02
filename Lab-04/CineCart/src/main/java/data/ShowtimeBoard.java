d979e1687c125ebc00e3f95acb6dedb8a44e6a40
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-02 08:49:03 +0600

    Added methods

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -24,4 +24,9 @@ public class ShowtimeBoard {
         }
         return null;
     }
+    public void displayAll(){
+        for(int i=0;i<count;i++){
+            System.out.println(shows[i]);
+        }
+    }
 }
