df106bb885eb92a78ac7e4d9a69e1208228ba4b4
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-02 09:10:38 +0600

    added add and ConcessionItem methods

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -12,5 +12,19 @@ public class ConcessionMenu {
         items = new ConcessionItem[MAX];
         count = 0;
     }
+    public void add(ConcessionItem c){
+        if(count < MAX){
+            items[count++] = c;
+        }
+    }
+
+    public ConcessionItem findByCode(String code){
+        for(int i = 0; i < count; i++){
+            if(items[i].getCode().equals(code)){
+                return items[i];
+            }
+        }
+            return null;
+    }
 }
 
