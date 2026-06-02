6ad5cbf4eb9ace2dc01452b74e0d58ef658d5870
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-02 08:31:58 +0600

    initiated cart class with the constructor

---
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Cart {
+
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
+
+    private final Customer owner;
+    private final Ticket[] tickets;
+    private int ticketCount;
+    private final ConcessionItem[] items;
+    private final int[] qtys;
+    private int itemCount;
+
+    public Cart(Customer owner){
+        this.owner = owner;
+        this.tickets = new Ticket[MAX_TICKETS];
+        this.ticketCount = 0;
+        this.items = new ConcessionItem[MAX_ITEMS];
+        this.qtys = new int[MAX_ITEMS];
+        this.itemCount = 0;
+    }
+
+}
