8127b0f55994ab301b9e680ce359fedda7791cc6
nusra <nusratnishi1103@gmail.com>
2026-06-02 12:30:44 +0600

    Second attempt

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -32,7 +32,7 @@ public class CheckoutEngine {
             price *=1.20;
         }
         seat.book();
-        Ticket ticket= new Ticket();
+        Ticket ticket= new Ticket(showtime,row,col,price);
         cart.addTicket(ticket);
         return "OK";
     }
