a93ebb84fe11d08fa0f42b0f26b6a5f87fdc39d8
ahnaf-code <ahnafcode@gmail.com>
2026-06-02 11:09:56 +0600

    Ticket.java: make Constructor

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -9,5 +9,13 @@ public class Ticket {
     private int col;
     private double pricePaid;
 
+    //Constructor: four-argument full constructor
+
+    public Ticket(Showtime showtime, int row, int col, double pricePaid) {
+        this.showtime = showtime;
+        this.row = row;
+        this.col = col;
+        this.pricePaid = pricePaid;
+    }
 
 }
