4d8925bbba6218a09dd5227b006bbc7d62ff7681
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-02 08:22:21 +0600

    changed

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -10,7 +10,7 @@ public class Customer {
         this.id = id;
         this.name = name;
         this.age = age;
-        loyaltyTier = "BASIC";
+        this.loyaltyTier = "BASIC";
     }
     Customer(int id, String name, int age, String tier){
         this.id = id;
