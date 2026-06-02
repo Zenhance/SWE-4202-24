288577c4fc37f46e3cf75f0441a6fafe539c5ca1
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-06-02 08:29:39 +0600

    getters updated

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -33,4 +33,29 @@ public class Cart {
         itemCount++;
     }
 
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public Ticket[] getTickets() {
+        return tickets;
+    }
+
+    public int getTicketCount() {
+        return ticketCount;
+    }
+
+    public ConcessionItem[] getItems() {
+        return items;
+    }
+
+    public int[] getQtys() {
+        return qtys;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+
 }
