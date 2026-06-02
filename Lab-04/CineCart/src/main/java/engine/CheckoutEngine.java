a079a3f2618e9690dee2f5218927cbfde184383c
tasnitaraida <tasnitar@gmail.com>
2026-06-02 17:23:49 +0600

    constructor

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -2,10 +2,19 @@ package engine;
 
 import data.ConcessionMenu;
 import data.ShowtimeBoard;
+import model.Showtime;
+import model.Cart;
 
 public class CheckoutEngine {
     private final ShowtimeBoard;
     private ConcessionMenu;
+    public checkoutEngine(ShowtimeBoard board,ConcessionMenu menu){
+        this.board=board;
+        this.menu=menu;
+    }
+    public String bookTicket(Cart cart,int showtimeId,int row,int col){
+        return null;
+    }
 
 
 
