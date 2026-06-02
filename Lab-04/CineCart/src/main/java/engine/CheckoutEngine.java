42740ba2f9b1753f32714e81023be50196aae32f
tasnitaraida <tasnitar@gmail.com>
2026-06-02 17:29:52 +0600

    complete

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -12,10 +12,21 @@ public class CheckoutEngine {
         this.board=board;
         this.menu=menu;
     }
+
     public String bookTicket(Cart cart,int showtimeId,int row,int col){
         return null;
     }
 
 
+    public double checkout(Cart cart){
+        return 123;
+    }
 
+    public String addReceipt(Cart cart) {
+        return null;
+    }
+
+    public String addConcession(Cart cart, String code, int qty) {
+        return null;
+    }
 }
