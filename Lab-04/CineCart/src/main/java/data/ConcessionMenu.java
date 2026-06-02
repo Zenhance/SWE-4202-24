a6a750935fc9de6ba71541f69de54f09e8c938af
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-06-02 09:35:34 +0600

    showtimeBoard 1

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,4 +1,38 @@
 package data;
 
+import model.ConcessionItem;
+
 public class ConcessionMenu {
+    public static final int MAX=30;
+    private ConcessionItem[] items;
+    private int count;
+    public ConcessionMenu(){
+        items=new ConcessionItem[MAX];
+        count=0;
+    }
+    public void add(ConcessionItem c){
+        if(count<MAX){
+            items[count]=c;
+            count++;
+        }
+    }
 }
+
+
+public ConcessionItem  findByCode(String code) {
+    for (int i = 0; i < count; i++) {
+        if (items[i].getCode().equals(code)) {
+            for (int i = 0; i < count; i++) {
+                return items[i];
+            }
+        }
+        return null;
+    }
+
+    public void displayAll() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(items[i].toString());
+        }
+    }
+}
+
