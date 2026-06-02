85d85a888b32f085c70a8f787d542c6f912b1279
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-06-02 08:37:20 +0600

    write code in cart

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,4 +1,24 @@
 package model;
 
 public class Cart {
+    public static final int MAX_TICKET=20;
+    public static final int MAX_ITEMS=20;
+
+    private Customer owner;
+    private Ticket[] tickets;
+    private int ticketCount;
+    private ConcessionItem[] items;
+    private int[] qtys;
+    private int itemCount;
+
+    public Cart(Customer owner){
+        this.owner=owner;
+        this.tickets=new Ticket[MAX_TICKET];
+        this.ticketCount=0;
+        this.items=new ConcessionItem[MAX_ITEMS];
+        this.qtys=new int[MAX_ITEMS];
+        this.itemCount=0;
 }
+
+
+
