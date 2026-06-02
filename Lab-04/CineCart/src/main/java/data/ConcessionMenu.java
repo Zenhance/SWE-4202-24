25cae8b4b3ce35e3b40d416ddf3be0b4ea1906d3
23-Fahad <ifahad2k@gmail.com>
2026-06-02 11:40:10 +0600

    fixed

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -4,16 +4,19 @@ public class ConcessionMenu {
     private ConcessionItem[] items;
     private int count;
     private static final int MAX = 30;
+
     public ConcessionMenu() {
         this.items = new ConcessionItem[MAX];
         this.count = 0;
     }
+
     public void add(ConcessionItem c) {
         if (count < MAX) {
             items[count] = c;
             count++;
         }
     }
+
     public ConcessionItem findByCode(String code) {
         for (int i = 0; i < count; i++) {
             if (items[i].getCode().equals(code)) {
@@ -22,16 +25,10 @@ public class ConcessionMenu {
         }
         return null;
     }
+
     public void displayAll() {
         for (int i = 0; i < count; i++) {
             System.out.println(items[i].toString());
         }
-    }
-    public static void main(String[] args) {
-        ConcessionMenu menu = new ConcessionMenu();
-        menu.add(new ConcessionItem("POP", "Popcorn", 220));
-        System.out.println("Testing ConcessionMenu:");
-        menu.displayAll();
-        System.out.println("Find POP: " + menu.findByCode("POP").getName());
     }
 }
