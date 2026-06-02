119f56b3de8f973ca49518be1e41f2a5c830141b
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-02 09:52:39 +0600

    done with bookTicket method

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -12,4 +12,30 @@ public class CheckoutEngine {
         this.board = board;
         this.menu = menu;
     }
+    public String bookTicket(Cart cart, int showtimeId, int row, int col) {
+        Showtime showtime = board.findById(showtimeId);
+        if (showtime == null){
+            String error = "Showtime not found";
+            return error;
+        }
+
+        Movie movie = showtime.getMovie();
+        if (cart.getOwner().getAge() < showtime.getMovie().getMinAge()){
+            String message = "Underage for rating " + movie.getRating();
+            return message;
+        }
+
+        Seat seat = showtime.getHall().getSeat(row, col);
+        if (!seat.isAvailable()) return "Seat unavailable";
+
+        double price = movie.getBasePrice()
+                * (seat.isPremium() ? 1.30 : 1.00)
+                * (showtime.isPeak() ? 1.20 : 1.00);
+
+        seat.book();
+        Ticket ticket = new Ticket(showtime, row, col, price);
+        cart.addTicket(ticket);
+
+        return "OK";
+    }
 }
