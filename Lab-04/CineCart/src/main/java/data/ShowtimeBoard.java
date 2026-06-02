0c5827e8db85a968047252efb3e831fc9ccccd06
04-Labiba <sabihalabiba639@gmail.com>
2026-06-02 08:25:39 +0600

    init commit

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -8,23 +8,18 @@ public class ShowtimeBoard {
         shows=new Showtime[MAX];
         count=0;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void add(Showtime s){
+        if(count<MAX){
+            shows[count++]=s;
+        }
+    }
+    public Showtime findById(int id) {
+        for (int i = 0; i < count; i++) {
+        if(shows[i].getId()==id){
+            return shows[i];
+        }
+        }
+        return null;
+    }
 
 }
