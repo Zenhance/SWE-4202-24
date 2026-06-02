56dfa0a863f9ac3d495780833d7a4ede757dc369
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-02 08:26:36 +0600

    isPremium done

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -10,6 +10,14 @@ public class Hall {
         this.id = id;
         this.rows = rows;
         this.cols = cols;
+        this.grid = new Seat[rows][cols];
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < cols; c++){
+                boolean isPremium = (r < premiumRows);
+                grid[r][c] = new Seat(r, c, isPremium);
+            }
+        }
+    }
 
     }
 
