3b3e9f1fec79215a11df36c42321ec0e4d78c2ec
Sadman Raiyan <kamrulseed@gmail.com>
2026-06-02 08:44:16 +0600

    Halfway in bookTicket method for CheckoutEngine.java

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -2,6 +2,7 @@ package engine;
 
 import data.ConcessionMenu;
 import data.ShowtimeBoard;
+import model.*;
 
 public class CheckoutEngine {
     private ShowtimeBoard board;
@@ -12,5 +13,21 @@ public class CheckoutEngine {
         this.menu = menu;
     }
 
+    public String bookTicket(Cart cart,int showtimeId,int row,int col){
+        Showtime showtime=board.findById(showtimeId);
+        if(showtime==null){
+            return "Showtime not found";
+        }
 
+        if(cart.getOwner().getAge()<showtime.getMovie().getMinAge()){
+            return "Underage for rating "+showtime.getMovie().getRating();
+        }
+
+        Seat seat = showtime.getHall().getSeat(row, col);
+        if(seat.isBooked()){
+            return "Seat unavailable";
+        }
+
+
+    }
 }
