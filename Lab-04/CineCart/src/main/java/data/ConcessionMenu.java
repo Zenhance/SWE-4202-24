2a4337f37c0132d2eed52d090a970e31ae181c6a
tasnitaraida <tasnitar@gmail.com>
2026-06-02 17:06:46 +0600

    methods

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -5,4 +5,25 @@ public class ConcessionMenu {
     private ConcessionItem[] items;
     private int count;
     private static final int MAX=30;
+
+    public void add(ConcessionItem c) {
+        if(count<MAX) {
+            items[count]=c;
+        }
+        count++;
+    }
+
+    public ConcessionItem findByCode(String code) {
+        for(int i=0;i<count;i++) {
+            if(items[i].getCode().equals(code)) {
+                return items[i];
+            }
+        }
+    }
+
+    void displayAll() {
+        for(int i=0;i<count;i++) {
+            System.out.println(items[i].toString());
+        }
+    }
 }
