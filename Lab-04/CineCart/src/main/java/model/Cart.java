9388700cc79726352da0721935459ddf2baabee3
Tasauf Abdullah <tasaufalter@gmail.com>
2026-06-02 10:51:18 +0600

    add constants and field to Cart

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,5 +1,14 @@
 package model;
 
 public class Cart{
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
+
+    private Customer owner;
+    private Ticket[] tickets;
+    private ConcessionItem[] items;
+    private int ticketCount;
+    private int[] qtys;
+    private int itemCount;
 
 }
