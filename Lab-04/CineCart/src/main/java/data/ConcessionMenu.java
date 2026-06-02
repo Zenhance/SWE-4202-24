24319ebdb9815a08a71bc886aef7339c8dfdd844
Maomao-starry <aminarahmanshalu@gmail.com>
2026-06-02 11:59:36 +0600

    task

---
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -0,0 +1,16 @@
+package data;
+
+import model.Showtime;
+
+public class ConcessionMenu {
+    private Showtime[] shows;
+    private int count;
+    private static final int MAX = 30;
+
+    public ConcessionMenu(Showtime[] shows,int count){
+        this.shows=shows;
+        this.count=count;
+    }
+    public void
+
+}
