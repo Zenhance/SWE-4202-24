650a5737e80e16bdd64211f2c14f90cbed623d93
wasif-22 <twasif68@gmail.com>
2026-06-02 12:05:29 +0600

    added findbyID

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -11,5 +11,12 @@ public class ShowtimeBoard {
         shows=new Showtime[MAX];
         count=0;
     }
-
+    public Showtime findById(int id) {
+        for (int i = 0; i < count; i++) {
+            if (shows[i].getId() == id) {
+                return shows[i];
+            }
+        }
+        return null; // not found
+    }
 }
