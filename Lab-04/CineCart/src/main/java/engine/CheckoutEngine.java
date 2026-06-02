eeaf061f68e9e5a3df6aacc2cc6b88dbd845c864
elfie1502 <mumtahina@iut-dhaka.edu>
2026-06-02 10:03:27 +0600

    CheckoutEngine constructor and bookTicket method created!

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -2,6 +2,9 @@ package engine;
 
 import data.ConcessionMenu;
 import data.ShowtimeBoard;
+import model.Cart;
+import model.Seat;
+import model.Showtime;
 
 public class CheckoutEngine {
     private ShowtimeBoard board;
@@ -11,5 +14,16 @@ public class CheckoutEngine {
         this.board = board;
         this.menu = menu;
     }
+     public  String bookTicket(Cart cart, int showtimeId, int row, int col){
+    Showtime show = board.findById(showtimeId);
+         if (show == null) {
+             return " Showtime not found";
+         }
+
+         Seat seat = show.getHall().getSeat(row, col);
+         if(!seat.isAvailable()){
+             return "Seat unavailable";
+         }
+
 
 }
