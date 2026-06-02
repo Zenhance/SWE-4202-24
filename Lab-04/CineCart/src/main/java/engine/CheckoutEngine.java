f241e779fb3c38e4a4d5987b39bcd4d3c201de95
wasifjafar <wasifjafar71@gmail.com>
2026-06-02 09:27:05 +0600

    create bookTicket

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -2,6 +2,7 @@ package engine;
 
 import data.ConcessionMenu;
 import data.ShowtimeBoard;
+import model.Cart;
 
 public class CheckoutEngine {
     private ShowtimeBoard board;
@@ -10,5 +11,16 @@ public class CheckoutEngine {
     public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu) {
         this.board = board;
         this.menu = menu;
+    }
+
+    public String bookTicket(Cart cart, int showtimeId, int row, int col) {
+        if (board.findbyId(showtimeId) == null) return "Showtime not found";
+        if (cart.getOwner().getAge() < board.findbyId(showtimeId).getMovie().getMinAge()) {
+            return String.format("Underage for rating %s", board.findbyId(showtimeId).getMovie().getRating());
+        }
+        if (board.findbyId(showtimeId).getHall().getSeat(row,col).isBooked() == true)
+            return "Seat unavailable";
+
+        return "OK";
     }
 }
