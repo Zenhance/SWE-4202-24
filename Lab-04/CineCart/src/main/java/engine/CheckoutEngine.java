f0589ad50d9e5fad6f3d24239bcf31a2f8b7b7c8
anonymous <farnaz28.rahman@gmail.com>
2026-06-02 13:55:51 +0600

    CheckoutEngine - Line 16-fixing checkout engine

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -6,17 +6,23 @@ import data.ShowtimeBoard;
 import model.Cart;
 
 public class CheckoutEngine {
-
+    private final ShowtimeBoard board;
+    private final ConcessionMenu menu;
+  public   CheckoutEngine(ShowtimeBoard board,ConcessionMenu menu){
+      this.board=board;
+      this.menu=menu;
+  }
   public  String bookTicket(Cart cart, int showtimeId, int row, int col){
         return null;
     }
-    public  checkoutEngine(ShowtimeBoard board, ConcessionMenu menu){
-        return null;
+    public double checkout(Cart cart){
+        return 0.0;
     }
-    public String bookTicket(){
+
+  public String addConcession(Cart cart, String code, int qty){
       return null;
-    }
-  public String addConcession(){
-      return null;
+  }
+  public String getReceipt(Cart cart){
+      return "";
   }
 }
