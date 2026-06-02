d5735de54002ddc031988c4b0daf0671f3ccb2f7
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-02 08:33:14 +0600

    final

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -7,7 +7,7 @@ public class Cart {
 
     private final Customer owner;
     private final Ticket[] tickets;
-    private int ticketCount;
+    private final int ticketCount;
     private final ConcessionItem[] items;
     private final int[] qtys;
     private int itemCount;
