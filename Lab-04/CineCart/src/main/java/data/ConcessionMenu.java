f473a4091e99bc5b19ba9aaf97b90181e210840c
Mubtasim Fuad <mubtasimsmail@gmail.com>
2026-06-02 08:29:26 +0600

    defined constructor

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -7,6 +7,10 @@ public class ConcessionMenu {
     private int count;
     private static final int MAX = 30;
 
+    public ConcessionMenu() {
+        items = new ConcessionItem[MAX];
+    }
+
     public void add(ConcessionItem c){
         if(count < MAX){
             items[count++] = c;
