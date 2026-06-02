9a990d2d11f72159ef5c97a07569d4c29c934692
Mohammad Safwan Bhuiyan <safwanbhuiyan@iut-dhaka.edu>
2026-06-02 11:43:17 +0600

    git commit

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -5,27 +5,31 @@ import data.ConcessionMenu;
 import model.Cart;
 import model.Showtime;
 import model.Seat;
+import model.Ticket;
 
-public class CheckoutEngine{
+public class CheckoutEngine {
     private ShowtimeBoard board;
     private ConcessionMenu menu;
-
     public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu) {
-        this.board=board;
-        this.menu=menu;
+        this.board = board;
+        this.menu = menu;
     }
-    String bookTicket(Cart cart, int showtimeId, int row, int col) {
-        Showtime showtime = board.findById(showtimeId);
-        if (cart.getOwner().getAge() < showtime.getMovie().getMinAge()) {
-            return "Underage for rating " + showtime.getMovie().getRating();
+
+    public String bookTicket(Cart cart, int showtimeId, int row, int col) {
+        if (board.findById(showtimeId) == null) return "Showtime not found";
+        if (cart.getOwner().getAge() < board.findById(showtimeId).getMovie().getMinAge()) {
+            return "Underage for rating" + board.findById(showtimeId).getMovie().getRating();
         }
-        Seat seat = showtime.getHall().getSeat(row, col);
-        if (seat.isBooked()) {
+        if (board.findById(showtimeId).getHall().getSeat(row, col).isBooked()) {
             return "Seat unavailable";
         }
-
-        return "x";
+        double price = board.findById(showtimeId).getMovie().getBasePrice() *(board.findById(showtimeId).getHall().getSeat(row, col).isPremium() ? 1.30 : 1.00)
+                *(board.findById(showtimeId).isPeak() ? 1.20 : 1.00);
+        Ticket t=new Ticket(board.findById(showtimeId), row, col, price);
+        cart.addTicket(t);
+        return "OK";
     }
-
 }
+
+
 
