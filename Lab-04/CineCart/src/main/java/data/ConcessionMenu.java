e8838aeb3c37eec97940ad63afc43716b11adada
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-02 09:01:01 +0600

    Change : Add void displayAll method to ConcessionMenu class

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -27,4 +27,10 @@ public class ConcessionMenu {
         }
         return null;
     }
+
+    public void displayAll() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(items[i].toString());
+        }
+    }
 }
