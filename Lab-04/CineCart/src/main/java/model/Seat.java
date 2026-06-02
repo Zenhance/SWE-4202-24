d0b284e5a430638a2deafc29b1868ec29503f8fd
tasnitaraida <tasnitar@gmail.com>
2026-06-02 12:37:30 +0600

    find mistake

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -7,7 +7,7 @@ public class Seat {
     boolean isBooked;
 
     //Constructor
-    Seat(int row, int col,boolean isPremium, boolean isBooked) {
+    Seat(int row, int col,boolean isPremium) {
         this.row=row;
         this.col=col;
         this.isPremium=isPremium;
