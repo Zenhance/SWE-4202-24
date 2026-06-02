de26b3d5d4f202204a6d68b5ded2bd3b2077d98b
tahsin1r <naylakagi@gmail.com>
2026-06-02 09:22:21 +0600

    Added the bookticket method and worked on the findId, age, rating, seat and price.

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -2,6 +2,10 @@ package engine;
 
 import data.ConcessionMenu;
 import data.ShowtimeBoard;
+import model.Cart;
+import model.Movie;
+import model.Seat;
+import model.Showtime;
 
 public class CheckoutEngine {
     private ShowtimeBoard board;
@@ -19,6 +23,24 @@ public class CheckoutEngine {
 
     public ConcessionMenu getMenu() {
         return menu;
+    }
+    public String bookTicket(Cart cart, int showTimeId, int row, int col){
+        Showtime showtime=board.findById(showTimeId);
+        if(showtime == null){
+            return "Showtime not Found";
+        }
+        Movie movie=showtime.getMovie();
+        if(cart.getOwner().getAge()<movie.getMinAge()){
+            return "Underage for rating"+movie.getRating();
+
+        }
+        Seat seat=showtime.getHall().getSeat(row, col);
+        if(!seat.isAvailable()){
+            return "Seat unavailable";
+        }
+        double price=movie.getBasePrice()*(seat.isPremium()?1.30:1.00)*(showtime.isPeak()?1.20:1.00);
+
+
     }
 
 }
