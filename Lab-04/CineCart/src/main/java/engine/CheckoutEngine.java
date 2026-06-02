f953693d6bdd165554822315886093ac56a37596
Zunied-57 <zunied0411@gmail.com>
2026-06-02 08:44:31 +0600

    bookTicket half completed

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -2,8 +2,7 @@ package engine;
 
 import data.ConcessionMenu;
 import data.ShowtimeBoard;
-import model.Cart;
-import model.ConcessionItem;
+import model.*;
 
 public class CheckoutEngine
 {
@@ -18,7 +17,20 @@ public class CheckoutEngine
 
     public String bookTicket(Cart cart, int showtimeId, int row, int col)
     {
-
+        Showtime showtime = board.findById(showtimeId);
+        if(showtime==null)
+        {
+            return "Showtime not found";
+        }
+        if(cart.getOwner().getAge()<showtime.getMovie().getMinAge())
+        {
+            return "Underage for rating "+showtime.getMovie().getRating();
+        }
+        Seat seat = showtime.getHall().getSeat(row, col);
+        if(seat.isBooked())
+        {
+            return "Seat unavailable";
+        }
         return "OK";
     }
 
