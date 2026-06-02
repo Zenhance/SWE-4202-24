2a579b264c21bc3d077382f6bab9288c55191b70
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-06-02 17:00:40 +0600

    showtimeboard is modified

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,4 +1,31 @@
 package data;
-
+import model.Showtime;
 public class ShowtimeBoard {
+
+private Showtime[] shows;
+
+private int count;
+
+private static final int MAX = 30;
+
+
+
+void add(Showtime s) {
+    int count = 0;
+    for(int i = 0; i < MAX;i++){
+        shows[count] = s;
+            count++;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
 }
