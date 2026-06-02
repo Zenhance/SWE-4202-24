78143e3cb4a3bf81a86d29c21ccfd7957f31ce8b
md-musaddik <musaddikibna1@gmail.com>
2026-06-02 13:02:15 +0600

    Fixed the errors by declaring dummy methods on CheckoutEngine class

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -14,6 +14,15 @@ public class CheckoutEngine {
 
     public String bookTicket(Cart cart, int showtimeId, int row, int col){
         if(board.findById(showtimeId)==null) return "Showtime not found";
-
+        return "hello";
+    }
+    public String addConcession(Cart cart, String code, int qty){
+        return "Hello";
+    }
+    public double checkout(Cart cart){
+        return 0.0;
+    }
+    public String getReceipt(Cart cart){
+        return "Hello";
     }
 }
