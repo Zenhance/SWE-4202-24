765f0c5c3f5b246d75ec3ce03333b70e0b3bd9b5
ahnaf-code <ahnafcode@gmail.com>
2026-06-02 11:11:29 +0600

    Ticket.java: make methods

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -18,4 +18,27 @@ public class Ticket {
         this.pricePaid = pricePaid;
     }
 
+    // Methods: getters; toString() e.g. "T17 - R3C5 @ BDT 455.00".
+
+    public Showtime getShowtime() {
+        return showtime;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public double getPricePaid() {
+        return pricePaid;
+    }
+
+    public String toString() {
+        return String.format("T%d - R%dC%d @ BDT %.2f",
+                showtime.getId(), row, col, pricePaid);
+    }
+
 }
