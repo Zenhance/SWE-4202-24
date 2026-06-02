39122ef6e18fd4655d96203c21afda527b526a63
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-06-02 11:51:11 +0600

    showtime

---
+++ Lab-04/CineCart/src/main/java/Data/ShowtimeBoard.java
@@ -0,0 +1,30 @@
+package Data;
+
+import model.Showtime;
+
+public class ShowtimeBoard {
+    Showtime[] shows;
+    int count;
+    private static final int MAX = 30;
+
+    public void add(Showtime s) {
+        count = 0;
+        shows[count] = s;
+        count++;
+    }
+
+    public Showtime findByID(int id) {
+        for (int i = 0; i < count; i++) {
+            if (shows[i].getId() == id) {
+                return shows[i];
+            }
+        }
+        return null;
+    }
+
+    public void displayAll(){
+        for(int i=0;i<count;i++){
+            System.out.println(shows[i]);
+        }
+    }
+}
