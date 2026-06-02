19ff8f16ee79ad16a46203cdeca66439d5b1e20f
04-Labiba <sabihalabiba639@gmail.com>
2026-06-02 08:31:21 +0600

    init commit

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -3,5 +3,9 @@ import model.ConcessionItem;
 public class ConcessionMenu {
     private static final int MAX=30;
     private int count=0;
-    private ConcessionItem[] item;
+    private ConcessionItem[] items;
+    public void add(ConcessionItem c){
+        items=new ConcessionItem[MAX];
+        count=0;
+    }
 }
