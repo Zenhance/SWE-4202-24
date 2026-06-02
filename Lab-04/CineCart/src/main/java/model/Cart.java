95dd3dc7003701bcc103722d5c38b4c0f6932459
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-06-02 10:17:27 +0600

    updated fields in part C

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,5 +1,23 @@
+import model.ConcessionItem;
+import model.Customer;
+import model.Ticket;
+
 public class Cart {
 
+    //constants
     public static final int MAX_TICKETS = 20;
     public static final int MAX_ITEMS = 20;
+
+    //fields
+    public Customer owner;
+    public Ticket[] tickets;
+    public int ticketcount;
+    public ConcessionItem[] items;
+
+    public int[] qtys;
+    public int itemCount;
+
+
+
+
 }
