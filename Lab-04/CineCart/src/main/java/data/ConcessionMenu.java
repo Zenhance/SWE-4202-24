ca2cae343e1d31069c59b4ab1fc32f79399022c1
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-06-02 13:00:06 +0600

    concesssion menu constructor

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,9 +1,17 @@
 package data;
 
+import model.ConcessionItem;
+
 
 
 public class ConcessionMenu {
 
     private int count;
     private static final int MAX = 30;
+    private ConcessionItem[] items = new ConcessionItem[MAX];
+
+
+
+
+
 }
