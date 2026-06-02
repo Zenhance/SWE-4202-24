6d5e4db454f8b96b5f5e052280c67581024e2395
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-06-02 08:50:01 +0600

    Displays the string for the show found from id

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -29,5 +29,9 @@ public class ShowtimeBoard {
     return null;
     }
 
-
+    public void displayAll() {
+        for (int i=0; i<count; i++) {
+            System.out.println(shows[i].toString());
+        }
+    }
 }
