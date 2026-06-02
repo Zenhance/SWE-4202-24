ea5ef906e4a6f6aee636f81b8bf12140636fe132
tanbirulislam-hash <tanbirulislam@iut-dhaka.edu>
2026-06-02 11:50:00 +0600

    1st commit

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -1,4 +1,29 @@
 package model;
 
 public class Ticket {
+    private Showtime showtime;
+    private int row;
+    private int col;
+    private double pricePaid;
 }
+
+public Ticket{
+        this.showtime=showtime;
+        this.row=row;
+        this.col=col;
+        this.pricePaid=pricepaid;}
+
+public Showtime getShowtime(){
+    return Showtime;}
+
+public int getRow(){return row;}
+public int getCol(){return col;}
+public double getPricePaid(){return pricePaid;}
+
+}
+public String toString() {
+    return String.format(
+            "T%d R%dC%dC%BDT %.2f", showtime.getId(), row, col, pricePaid);
+}
+}
+
