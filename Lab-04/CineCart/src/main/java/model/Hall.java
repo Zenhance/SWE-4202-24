cc573c199b4d4b08b20d7510041fe6ef4190df12
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-02 08:27:22 +0600

    getGrid named Seat class array

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -32,6 +32,9 @@ public class Hall {
     public Seat getSeat(int row, int col){
         return grid[row][col];
     }
+    public Seat[][] getGrid(){
+        return grid;
+    }
 
     public int countAvailable(){
         int cnt = 0;
