95e30e5ce1c397ebc18b249b3acb5a09828796aa
tasnitaraida <tasnitar@gmail.com>
2026-06-02 16:36:26 +0600

    Fix  error- in Customer class line no 15, make Customer constructor public

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -12,7 +12,7 @@ public class Customer {
         this.age=age;
     }
 
-    Customer(int id, String name, int age, String tier) {
+    public Customer(int id, String name, int age, String tier) {
         this.id=id;
         this.name=name;
         this.age=age;
