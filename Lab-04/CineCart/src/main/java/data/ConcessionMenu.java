884335c69be38cf5ca109fa5f2f7840b89a4c5ff
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-02 08:56:06 +0600

    Change : Add void add variables , constructors and (ConcessionItem c) method to ConcessionMenu class

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -3,7 +3,19 @@ package data;
 import model.ConcessionItem;
 
 public class ConcessionMenu {
-    public void add(ConcessionItem concessionItem) {
+    private ConcessionItem[] items;
+    private int count;
+    private static final int MAX = 30;
 
+    public ConcessionMenu(ConcessionItem[] items, int count) {
+        this.items = new ConcessionItem[MAX];
+        this.count = 0;
+    }
+
+    public void add(ConcessionItem c) {
+        if (count < MAX) {
+            items[count] = c;
+            count++;
+        }
     }
 }
