b0d22322e9a909b19ee8f2b5d1a406887a5e3d82
anonymous <farnaz28.rahman@gmail.com>
2026-06-02 15:56:21 +0600

    Fixed error of Customer.java in line 12 & 19

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -9,13 +9,14 @@ public class Customer {
         this.id=id;
         this.name=name;
         this.age=age;
+        this.loyaltyTier="BASIC";
 
     }
     public Customer(int id, String name, int age, String tier){
         this.id=id;
         this.name=name;
         this.age=age;
-        this.loyaltyTier="BASIC";
+        this.loyaltyTier=tier;
     }
 
     public int getId() {
@@ -36,7 +37,7 @@ public class Customer {
         if(loyaltyTier.equals("GOLD")){
             return 0.15;
         }
-        else if(loyaltyTier.equals("SILVER"){
+        else if(loyaltyTier.equals("SILVER")){
             return 0.08;
         }
         else{
