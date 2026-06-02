10283ac959b34c96ede228925be0c7ac44eec1c5
sabbir240042147 <eftesabbir2725@gmail.com>
2026-06-02 12:31:07 +0600

    concessionmenu update

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -5,9 +5,25 @@ import model.ConcessionItem;
 public class ConcessionMenu {
     ConcessionItem[] items;
     int count;
-    private static final int MAX=30;
+    private static final int MAX=3;
 
      public void add(ConcessionItem c){
+      count=0;
+      if(count<MAX){
+                 items[count]=c;
+                 count++;
 
-     }
+             }
+         }
+      public ConcessionItem findByCode(String code) {
+          for (int i = 0; i < count; i++) {
+              if(items[i].getCode().equals(code)){
+                  return items[i];
+              }
+
+          }
+          return null;
+      }
+
+
 }
