2425546a182148e18707ce074217c0549d517ca6
tanbirulislam-hash <tanbirulislam@iut-dhaka.edu>
2026-06-02 12:50:22 +0600

    2st commit

--- Lab-04/CineCart/src/main/java/model/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/model/ShowtimeBoard.java
@@ -12,4 +12,24 @@ public class ShowtimeBoard{
         count=0;
     }
 
+    public void add(Showtime s){
+        if(count < MAX){
+            shows[count]=s;
+            count++;
+        }
+    }
+    public Showtime findByID(int id){
+        for (int i=0;i<count;i++) {
+            if (shows[i].getId()==id) {
+                return shows[i];
+            }
+        }
+        return null;
+    }
 
+    public void displayAll(){
+        for (int i=0;i<count;i++) {
+            System.out.println(shows[i]);
+        }
+    }
+}
