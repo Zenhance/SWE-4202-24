b3270d8b99abc6b51e7973b0b05c9f46db66f13f
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-02 08:41:29 +0600

    created first method

---
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -0,0 +1,16 @@
+package data;
+
+import model.Showtime;
+
+public class ShowtimeBoard {
+    private Showtime[] shows;
+    private int count;
+    private static final int MAX = 30;
+
+    public ShowtimeBoard() {
+        shows = new Showtime[MAX];
+        count = 0;
+    }
+
+
+}
