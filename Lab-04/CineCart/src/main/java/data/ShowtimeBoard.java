ed0831b6a60e3bef7bd26a15d84e944f068192c7
wasif-22 <twasif68@gmail.com>
2026-06-02 11:59:58 +0600

    added fields and constructor

---
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -0,0 +1,15 @@
+package data;
+
+import model.Showtime;
+
+public class ShowtimeBoard {
+    private Showtime[] shows;
+    private int count;
+    public static final int MAX=30;
+
+    public ShowtimeBoard(){
+        shows=new Showtime[MAX];
+        count=0;
+    }
+
+}
