50c97e7983d995f6d67f61a600b7e80ac38be146
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-06-02 10:50:55 +0600

    hall

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -13,9 +13,13 @@ public Hall(int id, int rows, int cols, int premiumRows){
     this.id=id;
     this.rows=rows;
     this.cols=cols;
+    Seat[][] grid=new Seat[rows][cols];
 
-    for (int r=0;r<rows;r++)
-        for(int c=0;c<column;c++)
+    for (int r=0;r<rows;r++){
+        for(int c=0;c<column;c++){
+
+
+        }
 }
 
 
