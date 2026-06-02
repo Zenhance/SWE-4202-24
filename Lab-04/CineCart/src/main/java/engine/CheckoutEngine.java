518e0679e9262efd5be663d15ecafc51731381eb
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-02 08:32:42 +0600

    init commit

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -56,6 +56,10 @@ public class CheckoutEngine {
             combo=50.0;
         }
         double preDiscount = ticketSubtotal + concessionSubtotal - combo;
+        double group= 0.0;
+        if(cart.getTicketCount()>=4){
+            group=0.10*preDiscount;
+        }
 
 
 
