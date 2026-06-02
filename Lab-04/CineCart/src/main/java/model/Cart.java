a0dd5644f4493534846d8ec766bc767629434ba0
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-06-02 11:32:57 +0600

    cart

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -5,10 +5,15 @@ public class  Cart {
     public static final int MAX_TICKETS = 20;
     public static final int MAX_ITEMS = 20;
 
-    private Customer owner;
-private Ticket[] tickets;
-private int ticketCount;
-private ConcessionItem[] items;
-private int[] qtys;
-private int itemCount;
+      private Customer owner;
+      private Ticket[] tickets;
+      private int ticketCount;
+      private ConcessionItem[] items;
+      private int[] qtys;
+      private int itemCount;
+
+public Cart (Customer owner){
+    this.owner=owner;
+
+}
 }
