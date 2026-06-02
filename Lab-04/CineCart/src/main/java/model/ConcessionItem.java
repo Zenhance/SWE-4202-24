9c8643ffa7abc7f15169a33e8bb7a3b45f9e1e77
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-06-02 09:36:13 +0600

    showtimeBoard 2

--- Lab-04/CineCart/src/main/java/model/ConcessionItem.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -5,4 +5,24 @@ public class ConcessionItem {
     private String name;
     private double unitPrice;
 
+
+    public ConcessionItem(String code,String name,double unitPrice){
+        this.code=code;
+        this.name=name;
+        this.unitPrice=unitPrice;
+    }
+    public String getCode(){
+        return code;
+    }
+    public String getName(){
+        return name;
+
+    }
+    public double getUnitPrice(){
+        return unitPrice;
+    }
+    public String toString(){
+        return code + " - " +name + " - BDT "+String.format("%.2f",unitPrice);
+    }
+
 }
