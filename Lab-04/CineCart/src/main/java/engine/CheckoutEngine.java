ad65a88221929a0bfb58db003803db277a00b943
37-inan <inanjaki184@gmail.com>
2026-06-02 13:52:22 +0600

    e1

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -1,4 +1,20 @@
 package engine;
 
+import data.ConcessionMenu;
+import data.ShowtimeBoard;
+import model.Cart;
+import model.Showtime;
+
 public class CheckoutEngine {
+    private ShowtimeBoard board;
+    private ConcessionMenu menu;
+
+    public CheckoutEngine(ShowtimeBoard board,ConcessionMenu menu){
+        this.board=board;
+        this.menu=menu;
+    }
+
+    public String bookTicket(Cart cart, int showtimeId, int row, int col){
+        return "OK";
+    }
 }
