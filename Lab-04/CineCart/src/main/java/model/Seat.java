4d48b82e5a072db621ac857271e0d62e419d8995
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-02 09:09:42 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -6,7 +6,7 @@ public class Seat {
     public boolean isPremium;
     public boolean isBooked;
 //constructor
-public Seat(int row,int col,boolean isPremium,boolean isBooked){
+public Seat(int row,int col,boolean isPremium){
     this.row=row;
     this.col=col;
     this.isPremium=isPremium;
