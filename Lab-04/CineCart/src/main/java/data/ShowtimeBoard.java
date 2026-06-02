7954bc246bd01b90deb3f07cef5b5022b4a5b95a
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-06-02 17:39:36 +0600

    showtimeboard is modified in line21

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -2,30 +2,40 @@ package data;
 import model.Showtime;
 public class ShowtimeBoard {
 
-private Showtime[] shows;
-
-private int count;
-
-private static final int MAX = 30;
+   Showtime[] shows;
+   int count;
+   static final int MAX = 30;
 
 
+public ShowtimeBoard() {
+    shows = new Showtime[MAX];
+    count = 0;
+}
 
-void add(Showtime s) {
-    int count = 0;
+public void add(Showtime s) {
     for(int i = 0; i < MAX;i++){
         shows[count] = s;
             count++;
         }
     }
-}
 
 
+public Showtime findById(int id) {
+    for (int i = 0; i < count; i++) {
+        if (shows[i].getId() == id) {
+            return shows[i];
+        }
+        else{
+            return null;
+    }
+}}
 
+public void displayAll() {
+    for(int i = 0 ; i < count ; i++){
+        System.out.println(shows[i]);
+    }
 
-
-
-
-
+    }
 
 
 }
