25028c85e5e0e6da965d05036ae6ff20ed9d7783
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-06-02 09:01:30 +0600

    showtime

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -1,41 +1,12 @@
 package model;
 
 public class Showtime {
-private int id;
-    private  Movie movie;
+    private int id;
+    private Movie movie;
     private Hall hall;
     private int startHour;
     private String dateTag;
+    public ShowTime(int id,Movie movie,Hall hall,int startHour,String dateTag){
 
-    public Showtime(int id,Movie movie,Hall hall,int startHour,String dateTag){
-        this.id=id;
-        this.movie=movie;
-        this.hall=hall;
-        this.startHour=startHour;
-        this.dateTag=dateTag;
     }
-    public int getId(){
-     return id;
-    }
-    public Movie getMovie(){
-      return movie;
-    }
-    public Hall getHall(){
-      return hall;
-    }
-    public int getStartHour(){
-        return startHour;
-    }
-    public String getDateTag(){
-        return dateTag;
-    }
-
-    public boolean isPeak(){
-       return startHour>=18 && startHour<=21;
-    }
-
-    public String toString(){
-        return "[T" +id + "]" +movie.getTitle()+" @Hall " + hall.getId()+" - "+startHour +":00"+" ("+dateTag+")";
-    }
-
 }
