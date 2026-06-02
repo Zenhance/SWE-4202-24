5b30e7dceae6048488f1fc7e502ccedbd3d515bb
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-02 08:11:50 +0600

    init commit

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -11,8 +11,9 @@ public class CheckoutEngine {
         this.menu = menu;
     }
 
-    public String bookTicket(Cart cart, int showtimeId, int row, int col) {
-        if (show == null) {
+    public String bookTicket(Cart cart, int showtimeId, int row, int col)
+    Showtime showtime = board.findById(showtimeId){
+        if (showtime == null) {
             return "Showtime not found";
         }
 
