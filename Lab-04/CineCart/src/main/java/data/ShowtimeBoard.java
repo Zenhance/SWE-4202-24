723f8c603d6443425461e9990185f868d473e56c
ASUS <tasintajwar991@gmail.com>
2026-06-02 17:38:22 +0600

    bare minimum code for ShowtimeBoard so that it can at least compile and fix compilation errors relevant with ShowtimeBoard

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,4 +1,26 @@
 package data;
 
+import model.Showtime;
+
 public class ShowtimeBoard {
+
+private int count=0;
+private static final int MAX=30;
+private Showtime[] shows = new Showtime[MAX];
+public void add (Showtime s)
+{}
+
+public Showtime findById(int id)
+{
+return null;
+
+
+}
+
+public void displayAll()
+{}
+
+
+
+
 }
