5b358a293ecda00c400d5ea7f5de5b47ad7808f3
Sadman Raiyan <kamrulseed@gmail.com>
2026-06-02 08:21:50 +0600

    Finishing ShowtimeBoard.java

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -24,4 +24,10 @@ public class ShowtimeBoard {
         }
         return null;
     }
+
+    public void displayAll(){
+        for(int i=0;i<count;i++){
+            System.out.println(shows[i]);
+        }
+    }
 }
