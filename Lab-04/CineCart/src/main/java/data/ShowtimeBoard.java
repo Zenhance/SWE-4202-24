64b16e2c893bb6a39dd0fbec801363a07cb24dc4
anonymous <farnaz28.rahman@gmail.com>
2026-06-02 16:58:05 +0600

    Initializing

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -6,6 +6,9 @@ public class ShowtimeBoard {
     Showtime[] shows;
     int count=0;
     static final int MAX=30;
+    public ShowtimeBoard(){
+        shows=new Showtime[MAX];
+    }
    public void add(Showtime s){
           shows= new Showtime[MAX];
           if(count<MAX){
