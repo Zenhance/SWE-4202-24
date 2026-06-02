ee596e2e8338d5cfd947b5ebb723a848dae2c96f
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-06-02 08:55:13 +0600

    constructor added

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -7,5 +7,10 @@ public class ConcessionMenu {
     private int count;
     private static final int MAX = 30;
 
+    public ConcessionMenu(){
+        items = new ConcessionItem[MAX];
+        count = 0;
+    }
+
 
 }
