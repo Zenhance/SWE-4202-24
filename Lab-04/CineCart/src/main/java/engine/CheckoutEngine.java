c5da57dd741d0c72d68a2d43891f55a66f6071c5
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-06-02 09:59:23 +0600

    engine 1

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -1,7 +1,17 @@
 package engine;
+import data.ShowtimeBoard;
+ import data.ConcessionMenu;
 
 public class CheckoutEngine {
+    private ShowtimeBoard board;
+    private ConcessionMenu menu;
+
+ public CheckoutEngine(ShowtimeBoard board,ConcessionMenu menu){
+     this.board=board;
+     this.menu=menu;
+ }
 }
+
 
 
 
