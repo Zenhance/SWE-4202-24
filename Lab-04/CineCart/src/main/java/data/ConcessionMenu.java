c561886e92c347657b1461739d6d723da5d3de44
Mahdia Hossain <hossainmahdia22@gmail.com>
2026-06-02 12:45:07 +0600

    innit commit

---
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -0,0 +1,24 @@
+package data;
+import model.ConcessionItem;
+public class ConcessionMenu {
+    public static final int MAX = 30;
+    private ConcessionItem[] items;
+    private int count;
+    public ConcessionMenu(){
+        items = new ConcessionItem[MAX];
+        count = 0;
+    }
+    public void add(ConcessionItem c){
+        if (count>=MAX){return;;}
+        items[count]=c;
+        count++;
+    }
+    public ConcessionItem findByCode(String code){
+        for(int s=0; s<count; s++){
+            if(items[s].getCode().equals(code)){
+                return items[s];
+            }
+        }
+        return null;
+    }
+}
