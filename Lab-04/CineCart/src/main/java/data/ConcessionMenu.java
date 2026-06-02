cf3b060c74e2b04cd16e7aa12b357004e6b00025
Tasauf Abdullah <tasaufalter@gmail.com>
2026-06-02 11:20:41 +0600

    add fields to ConcessionMenu

---
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -0,0 +1,9 @@
+package data;
+
+import model.ConcessionItem;
+
+public class ConcessionMenu {
+    private ConcessionItem[] items;
+    private int count;
+    private static final int MAX = 30;
+}
