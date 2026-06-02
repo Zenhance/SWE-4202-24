58aab7c98f39e3e9644b72a94175373fdbb02653
shafin-75 <arafatshafin85@gmail.com>
2026-06-02 13:43:07 +0600

    showtimeboard class line-18 compilation error fixed

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -15,7 +15,7 @@ public class ShowtimeBoard {
             count++;
         }
     }
-    public Showtime findByID(int id){
+    public Showtime findById(int id){
         for(int i=0;i<count;i++){
             if(shows[i].getId()==id){
                 return shows[i];
