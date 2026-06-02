3195f6c712d76a545409ac20f5d26101d8e8cffb
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-06-02 16:43:02 +0600

    Engine

---
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -0,0 +1,35 @@
+package engine;
+
+import data.ShowtimeBoard;
+import data.ConcessionMenu;
+import model.Cart;
+import model.Seat;
+import model.Showtime;
+
+public class CheckoutEngine {
+    private ShowtimeBoard board;
+    private ConcessionMenu menu;
+
+    public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu){
+        this.board=board;
+        this.menu=menu;
+    }
+
+    public String bookTicket(Cart cart, int showtimeId, int row, int col){
+        Showtime showtime= board.findById(showtimeId);
+
+        if(showtime==null) {
+            return "Showtime not found";
+        }
+        if(cart.getOwner().getAge() < showtime.getMovie().getMinAge()) {
+            return "Underage for rating "+showtime.getMovie().getRating();
+        }
+
+        Seat seat= showtime.getHall().getSeat(row,col);
+
+        if(seat.isBooked()){
+            return "Seat Unavailable";
+        }
+
+    }
+}
