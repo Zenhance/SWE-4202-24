2981f2ef626e71ef056ee7ff1e1dbe7317ce48ef
md-musaddik <musaddikibna1@gmail.com>
2026-06-02 11:43:42 +0600

    add method declared.

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,4 +1,16 @@
 package data;
 
+import model.Showtime;
+
 public class ShowtimeBoard {
+    private Showtime[] shows;
+    private int count;
+    private static final int MAX=30;
+
+    public void add(Showtime s){
+        shows[count]=s;
+        count ++;
+    }
+
+
 }
