eab484cbe5163629fc171e2d0c406637cf90cda3
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-02 08:51:18 +0600

    Added fields

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,4 +1,8 @@
 package data;
-
+import model.ConcessionItem;
 public class ConcessionMenu {
+    private static final int MAX = 30;
+    private ConcessionItem[] items;
+    private int count;
+
 }
