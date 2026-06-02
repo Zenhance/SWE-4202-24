4f6b3e55ecabf67693959823d6fac05c105b93a4
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-06-02 10:33:38 +0600

    Lab 4 finished.

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -13,11 +13,11 @@ public class CheckoutEngine{
     private ShowtimeBoard board;
     private ConcessionMenu menu;
 
-    public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu) {
+    public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu){
         this.board = board;
         this.menu = menu;
     }
-    public String bookTicket(Cart cart, int showtimeId, int row, int col) {
+    public String bookTicket(Cart cart, int showtimeId, int row, int col){
         Showtime show = board.findById(showtimeId);
         if (show == null) {
             return "Showtime not found";
