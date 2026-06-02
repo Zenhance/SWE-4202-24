1e3404977547006f81741fd86175adb7dd3dae21
wasif-22 <twasif68@gmail.com>
2026-06-02 11:56:02 +0600

    added hasItem

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -54,4 +54,12 @@ public class Cart {
         return total;
 
      }
+    public boolean hasItem(String code) {
+        for (int i = 0; i < itemCount; i++) {
+            if (items[i].getCode().equals(code)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
