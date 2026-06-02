29071f2a05f2c2342be2aac49b62064e5c5433da
Mubtasim Fuad <mubtasimsmail@gmail.com>
2026-06-02 08:38:09 +0600

    defind main partially

--- Lab-04/CineCart/src/main/java/app/CineCartApp.java
+++ Lab-04/CineCart/src/main/java/app/CineCartApp.java
@@ -1,7 +1,18 @@
 package app;
 
+
+import io . CsvLoader ;
+import model .*;
+import data .*;
+
+
 public class CineCartApp {
     public static void main(String[] args) {
+        Movie[] movies = CsvLoader.loadMovies("movies.csv");
+        Hall[] halls = CsvLoader.loadHalls("halls.csv");
+        ShowtimeBoard board = CsvLoader.loadShowtimes("showtimes.csv", movies, halls);
+        ConcessionMenu menu = CsvLoader.loadConcessions("concessions.csv");
+        Customer[]  customers = CsvLoader.loadCustomers("customers.csv");
 
     }
 }
