efa04437bdd91696be486901942139d6af046c41
ayiham-1012 <ayiham1012@gmail.com>
2026-06-02 10:56:04 +0600

    E. CheckoutEngine: Fields and Constructor

---
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -0,0 +1,14 @@
+package engine;
+
+import data.ConcessionMenu;
+import data.ShowtimeBoard;
+
+public class CheckoutEngine {
+    private ShowtimeBoard board;
+    private ConcessionMenu menu;
+
+    public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu) {
+        this.board = board;
+        this.menu = menu;
+    }
+}
