3b78f5d02205c129bf03847062a67e06be9dad2e
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-02 08:27:20 +0600

    first push on cart, declared, made cart,add ticket and add item method.

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,4 +1,42 @@
 package model;
 
 public class Cart {
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
+
+    private Customer owner;
+    private Ticket[] tickets;
+    private int ticketCount;
+    private ConcessionItem[] items;
+    private int[] qtys;
+    private int itemCount;
+
+    public Cart(Customer owner) {
+        this.owner = owner;
+        this.tickets = new Ticket[MAX_TICKETS];
+        this.ticketCount = 0;
+        this.items = new ConcessionItem[MAX_ITEMS];
+        this.qtys = new int[MAX_ITEMS];
+        this.itemCount = 0;
+    }
+
+    public void addTicket(Ticket t) {
+        if (ticketCount < MAX_TICKETS) {
+            tickets[ticketCount] = t;
+            ticketCount++;
+        }
+    }
+
+    public void addItem(ConcessionItem c, int qty) {
+        if (itemCount < MAX_ITEMS && qty > 0) {
+            items[itemCount] = c;
+            qtys[itemCount] = qty;
+            itemCount++;
+        }
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
 }
