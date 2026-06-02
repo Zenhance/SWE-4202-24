5409bbd333af2eab36b88850df4b33d25277930d
ahnaf-code <ahnafcode@gmail.com>
2026-06-02 11:44:45 +0600

    ConcessionMenu.java: declare fields

---
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -0,0 +1,12 @@
+package data;
+
+import model.ConcessionItem;
+
+public class ConcessionMenu {
+
+    private ConcessionItem[] items;
+    private int count;
+    private static final int MAX = 30;
+
+
+}
