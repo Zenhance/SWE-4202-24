a02d0413e0b1c946ade02ece6826d5e34efaf0c7
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-02 08:52:15 +0600

    Added constructor

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,8 +1,15 @@
 package data;
 import model.ConcessionItem;
+
 public class ConcessionMenu {
     private static final int MAX = 30;
+
     private ConcessionItem[] items;
     private int count;
+
+    public ConcessionMenu(){
+        items = new ConcessionItem[MAX];
+        count = 0;
+    }
 
 }
