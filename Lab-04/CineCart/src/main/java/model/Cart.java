b69ebebb0f0df3ec51b389ed9153ea0cc1dc8e62
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-02 08:21:04 +0600

    Added private fields

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Cart {
+    public static  final int MAX_TICKETS = 20;
+    public static  final int MAX_ITEMS = 20;
+
+    private Customer owner;
+    private Ticket[] tickets;
+    private int ticketCount;
+    private ConcessionItem[] items;
+    private int[] qtys;
+    private int itemCount;
+
 }
