f0ccb05a41f98c5f3b6fa6dfe3fad7438575a0c4
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-06-02 12:15:58 +0600

    checkoutEngine

--- Lab-04/CineCart/src/main/java/engine/checkoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/checkoutEngine.java
@@ -1,6 +1,14 @@
 package engine;
 
+import Data.ShowtimeBoard;
+import Data.concessionMenu;
+
 public class checkoutEngine {
+    private ShowtimeBoard board;
+    private concessionMenu menu;
 
-
+    public checkoutEngine(ShowtimeBoard board,concessionMenu menu){
+        this.board=board;
+        this.menu=menu;
+    }
 }
