2903911d9e39bf9ad46352a0182db53911abaa5a
tasnitaraida <tasnitar@gmail.com>
2026-06-02 12:23:29 +0600

    toString done

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -4,13 +4,19 @@ public class Hall {
     private int id;
     private int rows;
     private int cols;
-    Seat[][] grid;
+    private Seat[][] grid;
 
     Hall(int id,int rows,int cols, int premiumRows) {
         this.id=id;
         this.rows=rows;
         this.cols=cols;
         grid=new Seat[rows][cols];
+
+        for(int r=0;r<rows;r++) {
+            for(int c=0;c<cols;c++) {
+                isPremium = r<premiumRows;
+            }
+        }
     }
 
     public Seat getSeat(int row, int col) {
