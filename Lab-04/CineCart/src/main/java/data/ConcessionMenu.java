d4342955d3429233d5cf630be7fe904f770d495c
wasif-22 <twasif68@gmail.com>
2026-06-02 12:27:16 +0600

    added fields, constructor and count

---
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -0,0 +1,23 @@
+package data;
+
+import model.ConcessionItem;
+
+public class ConcessionMenu {
+
+    private static final int MAX = 30;
+
+    private ConcessionItem[] items;
+    private int count;
+
+    public ConcessionMenu() {
+        items = new ConcessionItem[MAX];
+        count = 0;
+    }
+    public void add(ConcessionItem c) {
+        if (count < MAX) {
+            items[count] = c;
+            count++;
+        }
+    }
+
+}
