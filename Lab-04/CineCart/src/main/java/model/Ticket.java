56829e22f3e617baa8e74b18b67dc16773342d79
37-inan <inanjaki184@gmail.com>
2026-06-02 12:43:12 +0600

    B.3

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -13,4 +13,21 @@ public class Ticket {
         this.pricePaid=pricePaid;
     }
 
+    public Showtime getShowtime() {
+        return showtime;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public double getPricePaid() {
+        return pricePaid;
+    }
+
+
 }
