29775276634d47398f2c5ae8826fb3f43a3ae1d6
Lamisa Ibnat Zaman <lamisaibnat5d@gmail.com>
2026-06-02 12:39:49 +0600

    Third commit

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -4,41 +4,57 @@ public class Seat {
     int row;
     int col;
     boolean isPremium;
-    boolean isBooked= false;
-    public Seat(int row,int col,boolean isPremium, boolean isBooked){
-        this.row=row;
-        this.col=col;
-        this.isPremium=isPremium;
-        this.isBooked=isBooked;}
-        public int row(){
-            return row;
-        }
-        public int col(){
-            return col;
-        }
-        public boolean isPremium(){
-            return isPremium;
-        }
-        public boolean isBooked(){
-            return isBooked;
-        }
-        public double basePrice(){
-            return basePrice;
-        }
-        public void book(){
-        isBooked=true;
-    }
-    public void release(){
-        isBooked=false;
-    }
-    public void isAvailable(){
-       if(isBooked){
-           isAvailable=true;
-       }
-       else{
-           isAvailable=false;
-       }
+    boolean isBooked = false;
+    double basePrice;
 
+    public Seat(int row, int col, boolean isPremium, boolean isBooked) {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = isBooked;
     }
 
+    public int row() {
+        return row;
+    }
+
+    public int col() {
+        return col;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public double basePrice() {
+        return basePrice;
+    }
+
+    public void book() {
+        isBooked = true;
+    }
+
+    public void release() {
+        isBooked = false;
+    }
+
+    public boolean isAvailable() {
+        if (isBooked) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        if (isPremium) {
+            return "*" + "R" + row + "C" + col;
+        }
+        if (isBooked) {
+            return "#" + "R" + row + "C" + col;
+        }}
 }
