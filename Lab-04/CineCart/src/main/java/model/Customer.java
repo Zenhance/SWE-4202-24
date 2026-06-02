7966a0c1046229f907d71dea29444cbf32ba143c
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-06-02 20:38:57 +0600

    added getter in 26 line in customer class

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -23,6 +23,9 @@ public Customer(int id, String name, int age) {
 public String getLoyaltyTier() {
     return loyaltyTier;
 }
+    public int getId() {
+        return id;
+    }
 public int getAge() {
     return age;
 }
