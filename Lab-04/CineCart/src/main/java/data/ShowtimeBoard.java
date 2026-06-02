5a8426ed9291a324e587d612d99ffea85fb58bfe
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-06-02 09:18:05 +0600

    showtimeBoard 1

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,4 +1,33 @@
 package data;
 
+import model.Showtime;
+
 public class ShowtimeBoard {
+    private static final int MAX=30;
+    private Showtime[] shows;
+    private int count;
+
+    public ShowTimeBoard(){
+        shows=new Showtime[MAX];
+        count=0;
+    }
+    public void add(Showtime s){
+        if(count<MAX){
+            shows[count]=s;
+            count++;
+        }
+    }
+  public Showtime findById(int id){
+        for(int i=0;i<count;i++){
+            if(shows[i].getId()==id){
+                return shows[i];
+            }
+        }
 }
+
+
+
+
+
+
+
