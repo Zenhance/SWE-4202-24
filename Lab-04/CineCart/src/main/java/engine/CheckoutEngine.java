e6fc77da97cdbe0bfa680e1f48c4967b5e040377
ayiham-1012 <ayiham1012@gmail.com>
2026-06-02 11:00:31 +0600

    E.1 bookTicket: Finding Showtime

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -2,6 +2,8 @@ package engine;
 
 import data.ConcessionMenu;
 import data.ShowtimeBoard;
+import model.Cart;
+import model.Showtime;
 
 public class CheckoutEngine {
     private ShowtimeBoard board;
@@ -10,5 +12,12 @@ public class CheckoutEngine {
     public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu) {
         this.board = board;
         this.menu = menu;
+    }
+
+    String bookTicket(Cart cart, int showtimeId, int row, int col){
+        Showtime showtime = board.findById(showtimeId);
+        if(showtime == null) return "Showtime not found";
+
+
     }
 }
