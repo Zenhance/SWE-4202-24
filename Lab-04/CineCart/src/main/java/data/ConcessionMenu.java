ea1c353a3539ff74afe43b10212c24e460d6820f
Sadman Raiyan <kamrulseed@gmail.com>
2026-06-02 08:22:48 +0600

    Adding fields and constructors for ConcessionMenu.java

---
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -0,0 +1,17 @@
+package data;
+
+import model.ConcessionItem;
+
+public class ConcessionMenu
+{
+    private ConcessionItem[] items;
+    private int count;
+    private static final int MAX = 30;
+
+    public ConcessionMenu() {
+        items = new ConcessionItem[MAX];
+        this.count = 0;
+    }
+
+
+}
