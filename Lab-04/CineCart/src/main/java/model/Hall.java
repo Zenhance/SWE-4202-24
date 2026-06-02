7b4fe88cc87ff1c555390f0f68d8091725297012
tasnitaraida <tasnitar@gmail.com>
2026-06-02 12:37:30 +0600

    find mistake

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -15,7 +15,7 @@ public class Hall {
         for(int r=0;r<rows;r++) {
             for(int c=0;c<cols;c++) {
                 boolean isPremium = r<premiumRows;
-                grid[r][c]=new Seat (r,c,isPremium);
+                grid[r][c]=new Seat(r,c,isPremium);
             }
         }
     }
