5088dcba821ce93b187f5d33adbfced0fd47dc34
ahnaf-code <ahnafcode@gmail.com>
2026-06-02 11:38:38 +0600

    ShowtimeBoard.java: make methods

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -8,5 +8,31 @@ public class ShowtimeBoard {
     private int count;
     private static final int MAX = 30;
 
+    public ShowtimeBoard() {
+        this.shows = new Showtime[MAX];
+        this.count = 0;
+    }
+
+    public void add(Showtime s) {
+        if (count < MAX) {
+            shows[count] = s;
+            count++;
+        }
+    }
+
+    public Showtime findById(int id) {
+        for (int i = 0; i < count; i++) {
+            if (shows[i].getId() == id) {
+                return shows[i];
+            }
+        }
+        return null;
+    }
+
+    public void displayAll() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(shows[i].toString());
+        }
+    }
 
 }
