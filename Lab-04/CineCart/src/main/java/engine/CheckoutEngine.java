5152411d12b1bad9763a0cf610cae2543f77245e
Muhtasim Moin <muhtasim24@iut-dhaka.edu>
2026-06-02 09:23:43 +0600

    CheckoutEngine  class updated

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -1,4 +1,28 @@
 package engine;
 
+import data.ConcessionMenu;
+import data.ShowtimeBoard;
+import model.Cart;
+import model.Showtime;
+
 public class CheckoutEngine {
+    private ShowtimeBoard board;
+    private ConcessionMenu menu;
+    private Showtime s;
+
+  public CheckoutEngine(ShowtimeBoard board,ConcessionMenu menu){
+      this.menu=menu;
+      this.board=board;
+  }
+
+
+    public String bookTicket(Cart cart,int showtimeId,int row,int col) {
+      board.findbyId(showtimeId);
+      if (board.findbyId(showtimeId)==null) return "Showtime not found";
+      if (cart.getOwner().getAge()<s.getM().getMinAge()) return "Underage for rating "+s.getM().getRating() ;
+      s.getH().getSeat(row, col);
+      if (s.getH().getSeat(row, col).isBooked())return "Seat unavailable";
+      int price=m
+
+    }
 }
