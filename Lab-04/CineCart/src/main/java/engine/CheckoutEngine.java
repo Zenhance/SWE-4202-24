16375b8fe4582ae061927ab3502cef8daba6fe1f
anonymous <farnaz28.rahman@gmail.com>
2026-06-02 13:39:44 +0600

    CheckoutEngine - Line 16-adding constructor & methods

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -1,10 +1,20 @@
 package engine;
 
+
 import data.ConcessionMenu;
 import data.ShowtimeBoard;
+import model.Cart;
 
 public class CheckoutEngine {
-    private ShowtimeBoard board;
-    private ConcessionMenu menu;
+
+  public  String bookTicket(Cart cart, int showtimeId, int row, int col){
+        return null;
+    }
+    public String checkoutEngine(ShowtimeBoard board, ConcessionMenu menu){
+        return null;
+    }
+    public String bookTicket(){
+      return null;
+    }
 
 }
