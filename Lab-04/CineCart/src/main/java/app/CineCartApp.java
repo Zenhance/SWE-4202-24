c164345a0f0dedeaf2ff5fbec83d4ad3b6928f11
04-Labiba <sabihalabiba639@gmail.com>
2026-06-02 10:30:37 +0600

    init commit

--- Lab-04/CineCart/src/main/java/app/CineCartApp.java
+++ Lab-04/CineCart/src/main/java/app/CineCartApp.java
@@ -3,6 +3,10 @@ import data.*;
 import model.*;
 import data.*;
 public class CineCartApp {
-
-
+    Movie [] movies = CsvLoader . loadMovies (" movies . csv ") ;
+    Hall [] halls = CsvLoader . loadHalls (" halls . csv ") ;
+    ShowtimeBoard board = CsvLoader . loadShowtimes (
+            " showtimes . csv", movies , halls ) ;
+    ConcessionMenu menu = CsvLoader . loadConcessions (" concessions . csv ") ;
+    Customer [] customers = CsvLoader . loadCustomers (" customers . csv ") ;
 }
