3734894ff5704bb1fc68f2a704fda0d0c6c95f48
37-inan <inanjaki184@gmail.com>
2026-06-02 11:52:41 +0600

    A.1

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -7,6 +7,17 @@ public class Hall {
     Seat[][] grid;
 
     public Hall(int id, int rows, int cols, int premiumRows){
+        this.id=id;
+        this.rows=rows;
+        this.cols=cols;
 
+        grid=new Seat[rows][cols];
+
+        for(int r=0;r<rows;r++)
+            for(int c=0;c<cols;c++)
+        {
+            boolean premium=(r<premiumRows);
+            grid[r][c]=Seat(r,c,premium);
+        }
     }
 }
