895eac05d0a9599e5ec32de80219f0f81759a1b3
xenonono36-ctrl <xenonono36@gmail.com>
2026-06-02 09:15:12 +0600

    add Constructor

--- Lab-04/CineCart/src/main/java/model/ConcessionItem.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -4,4 +4,10 @@ public class ConcessionItem {
     private String code;
     private String name;
     private double unitPrice;
+
+    public ConcessionItem(String code, String name, double unitPrice) {
+        this.code = code;
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
 }
