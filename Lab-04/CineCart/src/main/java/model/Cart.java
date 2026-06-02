773817470edd1380b4da84835c72ae4a5c68c91e
wasifjafar <wasifjafar71@gmail.com>
2026-06-02 08:49:35 +0600

    create hasItem

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -75,4 +75,12 @@ public class Cart {
 
         return sum;
     }
+
+    public boolean hasItem(String code) {
+        for (int i=0; i<itemCount; i++) {
+            if (code.equals(items[i].getCode()))
+                return true;
+        }
+        return false;
+    }
 }
