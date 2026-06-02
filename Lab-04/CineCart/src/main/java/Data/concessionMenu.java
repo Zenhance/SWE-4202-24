dd69eae1652fe8a37dd888b3c9e24b29b7de451a
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-06-02 11:58:24 +0600

    concession

--- Lab-04/CineCart/src/main/java/Data/concessionMenu.java
+++ Lab-04/CineCart/src/main/java/Data/concessionMenu.java
@@ -1,6 +1,30 @@
 package Data;
 
+import model.concessionItem;
+
 public class concessionMenu {
     concessionItem[] items;
+    int count;
+    static final int MAX=30;
 
+    public void add(concessionItem c){
+        count=0;
+        items[count]=c;
+        count++;
+    }
+
+    public concessionItem findByCode(String code){
+        for(int i=0;i<count;i++){
+            if(items[i].getCode().equals(code)){
+                return items[i];
+            }
+        }
+        return null;
+    }
+
+    public void displayAll(){
+        for(int i=0;i<count;i++){
+            System.out.println(items[i]);
+        }
+    }
 }
