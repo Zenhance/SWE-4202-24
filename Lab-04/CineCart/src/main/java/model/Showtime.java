0f7ebe51e1a15371f0f94ed9e99ca5a83dbd8ac5
xenonono36-ctrl <xenonono36@gmail.com>
2026-06-02 08:27:47 +0600

    add constructors

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -6,4 +6,14 @@ public class Showtime {
     private Hall hall;
     private int startHour;
     private String dateTag;
+
+    public Showtime(int id, Movie movie, Hall hall, int startHour, String dateTag) {
+        this.id = id;
+        this.movie = movie;
+        this.hall = hall;
+        if (startHour>=0 && startHour<=23)
+            this.startHour = startHour;
+        this.dateTag = dateTag;
+    }
+
 }
