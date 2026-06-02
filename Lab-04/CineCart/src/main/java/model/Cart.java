1e2a5cb9db9a76bf2366ddae442e5836f0b3d910
sabbir240042147 <eftesabbir2725@gmail.com>
2026-06-02 11:25:06 +0600

    cart update

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -64,5 +64,14 @@ public class Cart {
         }
         return total;
     }
+    public double sumConcessionsRaw(){
+        double total=0;
+        for(int i=0;i<itemCount;i++){
+            total+=items[i].getUnitPrice()* qtys[i];
+
+
+        }
+        return total;
+    }
 
 }
