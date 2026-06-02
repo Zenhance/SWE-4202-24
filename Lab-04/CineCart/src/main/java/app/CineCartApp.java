5e63847180987d4fc627677542baf92d055a12f6
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-02 09:05:18 +0600

    init commit

--- Lab-04/CineCart/src/main/java/app/CineCartApp.java
+++ Lab-04/CineCart/src/main/java/app/CineCartApp.java
@@ -9,7 +9,7 @@ public class CineCartApp {
     public static void main(String[] args){
         Movie[] movies = CsvLoader.loadMovies("movies.csv");
         Hall[] halls= CsvLoader.loadHalls("halls.csv");
-        ShowtimeBoard board =CsvLoader.loadShowtimes{
+        ShowtimeBoard board =CsvLoader.loadShowtimes(
             "showtimes.csv",movies,halls);
 ConcessionMenu menu =CsvLoader.loadConcessions("concessions.csv");
 Customer[]customers = CsvLoader.loadConcessions("concessions.csv");
