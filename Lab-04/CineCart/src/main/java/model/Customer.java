d8470803ce031d5e27f187962675654ad8413f5a
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-06-02 09:47:59 +0600

    updating b.2

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -2,5 +2,20 @@ package model;
 
 public class Customer {
 
+    //fields
+    public int id;
+    public String name;
+    public int age;
+    public String loyaltyTier;
+
+    //constructors
+    public Customer(int id, String name, int age){
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+    }
+
 
 }
