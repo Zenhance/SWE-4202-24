196b380bb8345f347dbca6db077e13a3daff078e
tahsin1r <naylakagi@gmail.com>
2026-06-02 08:37:02 +0600

    Getters added in showtimeBoard

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -6,5 +6,19 @@ public class ShowtimeBoard {
     private Showtime[] shows;
     private int count;
     private static final int MAX=30;
+
+    public Showtime[] getShows() {
+        return shows;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public static int getMAX() {
+        return MAX;
+    }
 }
+
+
 
