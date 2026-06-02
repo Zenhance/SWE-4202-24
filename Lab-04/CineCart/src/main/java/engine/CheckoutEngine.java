aeeda3430290a7790284f111593a6b5275ca3f67
Zunied-57 <zunied0411@gmail.com>
2026-06-02 08:17:37 +0600

    constructor

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -1,6 +1,25 @@
 package engine;
 
+import data.ConcessionMenu;
+import data.ShowtimeBoard;
+import model.Cart;
+
 public class CheckoutEngine
 {
+    private ShowtimeBoard board;
+    private ConcessionMenu menu;
+
+    public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu)
+    {
+        this.board = board;
+        this.menu = menu;
+    }
+
+    public String bookTicket(Cart cart, int showtimeId, int row, int col)
+    {
+
+        return "OK";
+    }
+
 
 }
