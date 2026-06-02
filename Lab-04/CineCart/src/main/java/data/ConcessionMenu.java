da6322e2444158b7bb8b6cbaf373d6a94fa82850
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-06-02 09:47:51 +0600

    concessionMenu end

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -3,29 +3,35 @@ package data;
 import model.ConcessionItem;
 
 public class ConcessionMenu {
-    public static final int MAX=30;
+    public static final int MAX = 30;
     private ConcessionItem[] items;
     private int count;
-    public ConcessionMenu(){
-        items=new ConcessionItem[MAX];
-        count=0;
+
+    public ConcessionMenu() {
+        items = new ConcessionItem[MAX];
+        count = 0;
     }
-    public void add(ConcessionItem c){
-        if(count<MAX){
-            items[count]=c;
+
+    public void add(ConcessionItem c) {
+        if (count < MAX) {
+            items[count] = c;
             count++;
         }
     }
-public ConcessionItem findByCode(String code){
-        for(int i=0;i<count;i++){
-            if(items[i].getCode().equals(code)){
+
+    public ConcessionItem findByCode(String code) {
+        for (int i = 0; i < count; i++) {
+            if (items[i].getCode().equals(code)) {
                 return items[i];
             }
         }
         return null;
+    }
+
+    public void displayAll() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(items[i].toString());
+        }
+    }
 }
-
-
-
-
 
