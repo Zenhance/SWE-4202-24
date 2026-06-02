2f009f5ced1c163c53c47352e5230f4267cbbb1d
04-Labiba <sabihalabiba639@gmail.com>
2026-06-02 08:50:26 +0600

    init commit

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -2,6 +2,10 @@ package engine;
 import data.ShowtimeBoard;
 import data.ConcessionMenu;
 public class CheckoutEngine {
-    private ShowtimeBoard baord;
+    private ShowtimeBoard board;
     private ConcessionMenu menu;
+    public CheckoutEngine(ShowtimeBoard board,ConcessionMenu menu){
+        this.board=board;
+        this.menu=menu;
+    }
 }
