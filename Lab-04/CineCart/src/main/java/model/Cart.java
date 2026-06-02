bc6a60bff214a2c30e5c2a152a907b15552e3cdd
tasnitaraida <tasnitar@gmail.com>
2026-06-02 15:46:23 +0600

    initialize MAX_tICKETS and MAX_ITEMS

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,6 +1,9 @@
 package model;
 
 public class Cart {
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
+
     Customer owner;
     Ticket[] tickets;
     int ticketCount;
