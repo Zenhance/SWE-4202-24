ca8120ebb6d2e276b82cbe580cb203f8149af50a
shafin-75 <arafatshafin85@gmail.com>
2026-06-02 12:47:46 +0600

    Write code for engine/checkoutengine/String addConcesssion

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -36,4 +36,15 @@ public class CheckoutEngine {
         cart.addTicket(ticket);
         return "OK";
     }
+    public String addConcession(Cart cart, String code, int qty){
+        ConcessionItem item = menu.findByCode(code);
+        if(item==null){
+            return "Item not found";
+        }
+        if(qty<=0){
+            return "Invalid quantity";
+        }
+        cart.addItem(item,qty);
+        return "OK";
+    }
 }
