6995eb04e17df59bfa27c6af1d1b8da906fd174b
ahnaf-code <ahnafcode@gmail.com>
2026-06-02 11:22:38 +0600

    Cart.java: declare fields

---
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Cart {
+
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
+
+    private Customer owner;
+    private Ticket[] tickets;
+    private int ticketCount;
+    private ConcessionItem[] items;
+    private int[] qtys;
+    private int itemCount;
+
+}
