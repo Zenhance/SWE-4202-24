975eb25aa620da0851ba7ac4b48b7514b5302763
anonymous <farnaz28.rahman@gmail.com>
2026-06-02 11:28:44 +0600

    showtime_Board

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -6,6 +6,20 @@ public class ShowtimeBoard {
     Showtime[] shows;
     int count;
     static final int MAX=30;
-
-
+   void add(Showtime s){
+          shows= new Showtime[MAX];
+          count++;
+   }
+   Showtime findById(int id){
+       for(int i=0;i<count;i++){
+           if(shows[i].getId()==id){
+               return shows[i];
+           }
+       }return null;
+   }
+   public void displayAll(){
+       for(int i=0;i<count;i++){
+           System.out.println(shows[i].toString());
+       }
+   }
 }
