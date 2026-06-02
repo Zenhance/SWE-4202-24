b0b3e56eca05e03646e521b74e552aead57a5a31
elfie1502 <mumtahina@iut-dhaka.edu>
2026-06-02 08:35:51 +0600

    Cart's getters created!

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -35,6 +35,30 @@ public class Cart {
         }
     }
 
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public Ticket[] getTickets() {
+        return tickets;
+    }
+
+    public int getTicketCount() {
+        return ticketCount;
+    }
+
+    public ConcessionItem[] getItems() {
+        return items;
+    }
+
+    public int[] getQtys() {
+        return qtys;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
 
 
 }
