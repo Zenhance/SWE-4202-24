514e45ef3313879b1daf259ce5f1072bb067b011
xenonono36-ctrl <xenonono36@gmail.com>
2026-06-02 09:01:57 +0600

    add getters

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -13,5 +13,20 @@ public class Ticket {
         this.pricePaid = pricePaid;
     }
 
+    public Showtime getShowtime() {
+        return showtime;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public double getPricePaid() {
+        return pricePaid;
+    }
 
 }
