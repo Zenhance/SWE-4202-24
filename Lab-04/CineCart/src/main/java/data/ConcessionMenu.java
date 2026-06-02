01b9102804a1f3a27b5b847f774a3c460e6b4133
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-02 08:56:35 +0600

    Added methods

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -26,6 +26,11 @@ public class ConcessionMenu {
         }
         return null;
     }
+    public void displayAll(){
+        for(int i=0;i<count;i++){
+            System.out.println(items[i]);
+        }
+    }
 
 
 }
