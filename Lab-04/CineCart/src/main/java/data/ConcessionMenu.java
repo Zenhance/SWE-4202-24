508ea1684a0191c038283f62d412c9beebb48e5e
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-02 09:07:40 +0600

    constructor done

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -6,5 +6,11 @@ public class ConcessionMenu {
     private static final int MAX = 30;
     private ConcessionItem[] items;
     private int count;
+    private static final int MAX = 30;
+
+    public ConcessionMenu(){
+        items = new ConcessionItem[MAX];
+        count = 0;
+    }
 }
 
