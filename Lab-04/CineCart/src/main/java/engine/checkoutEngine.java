45665d0d9c0e567bb0911ece7c1112380f107307
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-06-02 12:48:58 +0600

    checkoutEngine

--- Lab-04/CineCart/src/main/java/engine/checkoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/checkoutEngine.java
@@ -2,6 +2,8 @@ package engine;
 
 import Data.ShowtimeBoard;
 import Data.concessionMenu;
+import model.Cart;
+import model.Showtime;
 
 public class checkoutEngine {
     private ShowtimeBoard board;
@@ -11,4 +13,13 @@ public class checkoutEngine {
         this.board=board;
         this.menu=menu;
     }
+
+    public String bookTicket(Cart cart, int showtimeId, int row, int col){
+        if(board.findByID(showtimeId)==null){
+            System.out.println("Showtime not found");
+        }
+        return "";
+    }
+
+
 }
