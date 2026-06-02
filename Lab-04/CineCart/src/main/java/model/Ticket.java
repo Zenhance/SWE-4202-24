4f5234eac0190f37ec460e6b89431da6ab008274
ahnaf-code <ahnafcode@gmail.com>
2026-06-02 11:05:29 +0600

    Ticket.java: declare fields

---
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -0,0 +1,13 @@
+package model;
+
+// Fields: Showtime showtime, int row, int col, double pricePaid.
+
+public class Ticket {
+
+    private Showtime showtime;
+    private int row;
+    private int col;
+    private double pricePaid;
+
+
+}
