d01d9012c86a992db7b714af72b634d8e94cc898
Sadman Raiyan <kamrulseed@gmail.com>
2026-06-02 08:26:09 +0600

    Finishing ConcessionMenu.java

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -12,6 +12,26 @@ public class ConcessionMenu
         items = new ConcessionItem[MAX];
         this.count = 0;
     }
+    public void add(ConcessionItem c){
+        if(count>=MAX) return;
+        items[count]=c;
+        count++;
+    }
+
+    public ConcessionItem findByCode(String code){
+        for(int i=0;i<count;i++){
+            if(items[i].getCode().equals(code)){
+                return items[i];
+            }
+        }
+        return null;
+    }
+
+    public void displayAll(){
+        for(int i=0;i<count;i++){
+            System.out.println(items[i]);
+        }
+    }
 
 
 }
