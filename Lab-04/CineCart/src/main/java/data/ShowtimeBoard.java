1b5fa393b04f6dc22f9087830f1c0180f1d3c9ef
04-Labiba <sabihalabiba639@gmail.com>
2026-06-02 08:19:24 +0600

    init commit

---
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -0,0 +1,30 @@
+package data;
+import model.Showtime;
+public class ShowtimeBoard {
+    private int count;
+    private static final int MAX=30;
+    private Showtime[] shows;
+    public ShowtimeBoard(){
+        shows=new Showtime[MAX];
+        count=0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
