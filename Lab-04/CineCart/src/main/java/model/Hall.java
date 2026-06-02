bb5a9f2d18da8eb60df98a082f88e065651808d3
tanbirulislam-hash <tanbirulislam@iut-dhaka.edu>
2026-06-02 10:55:33 +0600

    final commit

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -7,24 +7,59 @@ public class Hall {
     private Seat[][] grid;
 
     public Hall(int id, int rows,int cols,int premiumRows){
-        this.id=rows;
+        this.id=id;
+        this.rows=rows;
         this.cols=cols;
         this.grid=new Seat[rows][cols];
 
         for(int r=0;r<rows;r++){
             boolean isPremium=(r<premiumRows);
-            for(int c=0;c<cols ) {
-
-                grid[r][c]= new Seat(r,c,premium);
+            for(int c=0;c<cols;c++) {
+                grid[r][c]= new Seat(r,c,isPremium);
             }
         }
     }
-    public int getID(){
+    public int getId(){
         return id;
     }
-    public int getRow(){
-        return row;}
-    public int getCol(){
-        return col;}
+    public int getRows(){
+        return rows;}
+    public int getCols(){
+        return cols;}
+    public Seat[][] getGrid() {
+        return grid;
+    }
 
+
+    public Seat getSeat(int row,int col){
+        return grid[row][col];
+    }
+    public int countAvailable(){
+        int count=0;
+        for(int r=0;r<rows;r++){
+            for (int c=0;c<cols;c++) {
+                if(grid[r][c].isAvailable()){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public void displayLayout(){
+        for (int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                Seat s=grid[r][c];
+                if(!s.isAvailable()){
+                    System.out.print("# ");
+                }
+                else if(s.isPremium()) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
