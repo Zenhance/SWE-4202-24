9393b33d916b960382a2f6c9e6b7a4fbcaf281d8
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-02 09:04:09 +0600

    findById

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -13,4 +13,15 @@ public class ShowtimeBoard {
         }
     }
 
+    public Showtime findById(int id){
+        for(int i = 0; i < count; i++){
+            if(shows[i].getId() == id){
+                return shows[i];
+            }
+        }
+        return null;
+    }
+
+    
+
 }
