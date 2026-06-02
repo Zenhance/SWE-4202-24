7f2b4b06a1c46f4ebc2b6b19de50e0431217aafb
04-Labiba <sabihalabiba639@gmail.com>
2026-06-02 09:45:17 +0600

    init commit

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,29 +1,33 @@
 package data;
 import model.ConcessionItem;
 public class ConcessionMenu {
-    private static final int MAX=30;
-    private int count=0;
+    private static final int MAX = 30;
+    private int count = 0;
     private ConcessionItem[] items;
-    public ConcessionMenu(){
-        items=new ConcessionItem[MAX];
-        count=0;
+
+    public ConcessionMenu() {
+        items = new ConcessionItem[MAX];
+        count = 0;
     }
-    public void add(ConcessionItem c){
-        if(count<MAX){
-            items[count]=c;
+
+    public void add(ConcessionItem c) {
+        if (count < MAX) {
+            items[count] = c;
             count++;
-    }
-    public ConcessionItem findByCode(String code){
-        for(int i=0;i<count;i++){
-            if(items[i].getCode().equals(code)){
-                return items[i];
-            }
         }
-        return null;
     }
-        public void displayAll() {
+        public ConcessionItem findByCode (String code){
+            for (int i = 0; i < count; i++) {
+                if (items[i].getCode().equals(code)) {
+                    return items[i];
+                }
+            }
+            return null;
+        }
+        public void displayAll () {
             for (int i = 0; i < count; i++) {
                 System.out.println(items[i]);
             }
         }
-}
+    }
+
