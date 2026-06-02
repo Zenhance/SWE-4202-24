6f2f0f981b6d7943bb7674c329ab8cc1f78184a6
23-Fahad <ifahad2k@gmail.com>
2026-06-02 12:00:29 +0600

    fixed

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -17,6 +17,19 @@ public class Cart {
         ticketCount = 0;
         itemCount = 0;
     }
+    public void addTicket(Ticket t) {
+        if (ticketCount < MAX_TICKETS) {
+            tickets[ticketCount] = t;
+            ticketCount++;
+        }
+    }
+    public void addItem(ConcessionItem c, int qty) {
+        if (itemCount < MAX_ITEMS && qty > 0) {
+            items[itemCount] = c;
+            qtys[itemCount] = qty;
+            itemCount++;
+        }
+    }
 
     public Customer getOwner() {
         return owner;
