8bb42beedd312bc99ac5b5b87a1801d0ea0cc3c5
Zunied-57 <zunied0411@gmail.com>
2026-06-02 10:08:07 +0600

    main slightly done

--- Lab-04/CineCart/src/main/java/app/CinecartApp.java
+++ Lab-04/CineCart/src/main/java/app/CinecartApp.java
@@ -1,9 +1,20 @@
 package app;
 
+import data.ConcessionMenu;
+import data.ShowtimeBoard;
+import io.CsvLoader;
+import model.Customer;
+import model.Hall;
+import model.Movie;
+
 public class CinecartApp
 {
     public static void main(String[] args)
     {
-
+        Movie[] movies = CsvLoader.loadMovies("movies.csv");
+        Hall[] halls = CsvLoader.loadHalls("halls.csv");
+        ShowtimeBoard board = CsvLoader.loadShowtimes("showtimes.csv",movies,halls);
+        ConcessionMenu menu = CsvLoader.loadConcessions("concessions.csv");
+        Customer[] customers = CsvLoader.loadCustomers("customers.csv");
     }
 }
