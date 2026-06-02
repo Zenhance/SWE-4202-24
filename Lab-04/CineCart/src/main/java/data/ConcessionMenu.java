a387d50ee3c2967b74eaa4a7a40f84198e89a67c
tahsin1r <naylakagi@gmail.com>
2026-06-02 08:45:54 +0600

    Added fields and their setters to the ConcessionMenu class

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -1,4 +1,21 @@
 package data;
 
+import model.ConcessionItem;
+
 public class ConcessionMenu {
+    private ConcessionItem[] items;
+    private int count;
+    private static final int MAX=30;
+
+    public ConcessionItem[] getItems() {
+        return items;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public static int getMAX() {
+        return MAX;
+    }
 }
