823ea2abed9aaade9156bc3c1c813341d93feca5
Rakin <rakinhasan001@gmail.com>
2026-06-02 09:28:44 +0600

    ConcessionMenu class added

---
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -0,0 +1,27 @@
+package data;
+import model.ConcessionItem;
+public class ConcessionMenu {
+    ConcessionItem[] items;
+    int count;
+    private static final int MAX=30;
+
+
+    public void add(ConcessionItem c){
+        if(count<MAX){
+            items[count++]=c;
+        }
+    }
+    public ConcessionItem findByCode(String code){
+        for (int i=0;i<count;i++) {
+            if (items[i].getCode()==code) {
+                return items[i];
+            }
+        }
+        return null;
+    }
+    public void dsiplayALl(){
+        for (int i=0;i<count;i++) {
+            System.out.println(items[i].toString());
+        }
+    }
+}
