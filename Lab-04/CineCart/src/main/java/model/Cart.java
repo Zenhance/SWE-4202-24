d7d9e3753698f4140243d8e4e59992dd61158c38
wasif-22 <twasif68@gmail.com>
2026-06-02 11:45:55 +0600

    added concessionsRaw

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -46,4 +46,12 @@ public class Cart {
         }
         return total;
     }
+    public double sumConcessionsRaw() {
+        double total = 0;
+        for (int i = 0; i < itemCount; i++) {
+            total += items[i].getUnitPrice() * qtys[i];
+        }
+        return total;
+
+     }
 }
