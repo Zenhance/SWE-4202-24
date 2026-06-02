917b3c22f5bfae637b620c4cbc7fc5e7533e94e3
anonymous <farnaz28.rahman@gmail.com>
2026-06-02 12:01:32 +0600

    error

--- Lab-04/CineCart/src/main/java/model/Movie.java
+++ Lab-04/CineCart/src/main/java/model/Movie.java
@@ -30,10 +30,13 @@ public class Movie {
     }
 
     public int getMinAge(){
-        if(rating.equals("G"))return 0;
-        else if(rating.equals("PG"))return 7;
-        else if(rating.equals("PG-13"))return 13;
-        else if(rating.equals("18"))return 18;
+        if(rating.equals("G")){return 0;}
+        else if(rating.equals("PG")){
+            return 7;}
+        else if(rating.equals("PG-13")){return 13;}
+        else if(rating.equals("18")){
+            return 18;
+        }return 0;
     }
     public String toString(){
         return "Inception "+"("+"PG-13"+") "+this.durationMin+" -BDT "+String.format(".02%f",this.basePrice);
