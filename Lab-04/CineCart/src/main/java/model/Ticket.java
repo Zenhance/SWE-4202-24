da7b95ceeb0762916f44195582e5d1f48dfaac6a
Lamisa Ibnat Zaman <lamisaibnat5d@gmail.com>
2026-06-02 12:01:00 +0600

    First commit

---
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Ticket {
+    Showtime showtime;
+    int row;
+    int col;
+    double pricePaid;
+    public Ticket(Showtime showtime,int row,int col,double pricePaid){
+        this.showtime=showtime;
+        this.row=row;
+        this.col=col;
+        this.pricePaid=pricePaid;
+    }
+    public Showtime getShowtime(){
+        return showtime;
+    }
+    public int getRow(){
+        return row;
+    }
+    public int getCol(){
+        return col;
+    }
+    public String toString(){
+ return "T"+showtime+"R"+row+"C"+col+" @ BDT "+pricePaid;
+    }
+
+}
