36b13a9279e7471f8e358509b9689ddfa35b8d3d
tahsin1r <naylakagi@gmail.com>
2026-06-02 09:02:24 +0600

    Started working on the CheckoutEngine class and added fields constructor and getters.

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -1,4 +1,24 @@
 package engine;
 
+import data.ConcessionMenu;
+import data.ShowtimeBoard;
+
 public class CheckoutEngine {
+    private ShowtimeBoard board;
+    private ConcessionMenu menu;
+
+    public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu){
+        this.board=board;
+        this.menu=menu;
+
+    }
+
+    public ShowtimeBoard getBoard() {
+        return board;
+    }
+
+    public ConcessionMenu getMenu() {
+        return menu;
+    }
+
 }
