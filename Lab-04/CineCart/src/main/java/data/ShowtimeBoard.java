967eb624dbe26290f15e6257f41257e32e4a8d3a
tasnitaraida <tasnitar@gmail.com>
2026-06-02 16:53:14 +0600

    methods

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -1,7 +1,26 @@
 package data;
+import model.Showtime;
 
 public class ShowtimeBoard {
     Showtime[] shows;
     int count;
     public static final int MAX = 30;
+
+    public void add(Showtime s) {
+        if(count<MAX) {
+            shows[count]=s;
+            count++;
+        }
+    }
+
+    public Showtime findById(int id) {
+        for(int i=0;i<count;i++) {
+            if(shows[i].getId()==id) {
+                return shows[i];
+            }
+            else {
+                return null;
+            }
+        }
+    }
 }
