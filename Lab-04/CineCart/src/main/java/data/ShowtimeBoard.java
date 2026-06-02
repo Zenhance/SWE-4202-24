4d0fd9230c51da69d61127f2ac1ac2a2a50e285c
Mubtasim Fuad <mubtasimsmail@gmail.com>
2026-06-02 08:31:13 +0600

    defined constructor

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -8,6 +8,10 @@ public class ShowtimeBoard {
     private int count;
     private static final int MAX = 30;
 
+    public ShowtimeBoard(){
+        shows = new Showtime[MAX];
+    }
+
 
     public void add(Showtime s){
         if(count < MAX){
