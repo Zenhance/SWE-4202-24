ac522d704f541e781bf4ada1154f0e2cd184ed9e
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-02 08:57:06 +0600

    initiated class with fields

---
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -0,0 +1,10 @@
+package data;
+
+import model.ConcessionItem;
+
+public class ConcessionMenu {
+    private static final int MAX = 30;
+    private ConcessionItem[] items;
+    private int count;
+}
+
