daa9f7190d5a1801322c39cb3c5691a4ec200c1f
37-inan <inanjaki184@gmail.com>
2026-06-02 13:19:57 +0600

    C

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -1,6 +1,10 @@
 package model;
 
 public class Cart {
+
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
+
     Customer owner;
     Ticket[] tickets;
     int ticketCount;
@@ -8,5 +12,13 @@ public class Cart {
     int[] qtys;
     int itemCount;
 
+    public Cart(Customer owner){
+        this.owner=owner;
+        this.tickets=new Ticket[MAX_TICKETS];
+        this.ticketCount=0;
+        this.items=new ConcessionItem[MAX_ITEMS];
+        this.qtys=new int[MAX_ITEMS];
+        this.itemCount=0;
 
+    }
 }
