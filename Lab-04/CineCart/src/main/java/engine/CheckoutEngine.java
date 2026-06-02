2c2cb8c44734df0b615fdae02e080f63f034ddb9
Sadman Raiyan <kamrulseed@gmail.com>
2026-06-02 09:14:45 +0600

    Finished checkout method for CheckoutEngine.java

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -43,4 +43,26 @@ public class CheckoutEngine {
         return "OK";
 
     }
+
+    public double checkout(Cart cart){
+        double ticketSubtotal= cart.sumTicketPaid();
+        double concessionSubtotal= cart.sumConcessionRaw();
+        double combo=0;
+        if(cart.hasItem("POP")&& cart.hasItem("SODA")){
+            combo=50;
+        }
+        double preDiscount= ticketSubtotal+concessionSubtotal-combo;
+
+        double group=0;
+        if(cart.getTicketCount()>=4){
+            group=preDiscount*0.10;
+        }
+
+        double tier=cart.getOwner().getTierDiscount()*preDiscount;
+
+        double afterDiscount=preDiscount-group-tier;
+
+        double tax=0.05*afterDiscount;
+        return Math.round((afterDiscount+tax)*100.0)/100.0;
+    }
 }
