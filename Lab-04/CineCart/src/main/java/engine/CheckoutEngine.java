a081e556d0a0e1d8b116992ece06810e1f308c8e
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-02 09:22:39 +0600

    initiated class with fields and constructor

---
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -0,0 +1,15 @@
+package engine;
+
+import data.ConcessionMenu;
+import data.ShowtimeBoard;
+import model.*;
+
+public class CheckoutEngine {
+    private ShowtimeBoard board;
+    private ConcessionMenu menu;
+
+    CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu){
+        this.board = board;
+        this.menu = menu;
+    }
+}
