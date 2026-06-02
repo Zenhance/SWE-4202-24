b6a2d633d02c5431928e96e7290ddbc90f2a5f1c
37-inan <inanjaki184@gmail.com>
2026-06-02 12:16:52 +0600

    B.1

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -8,6 +8,11 @@ public class Showtime {
     String dataTag;
 
     public Showtime(int id, Movie movie, Hall hall, int starHour, String dataTag){
-
+        this.id=id;
+        this.movie=movie;
+        this.hall=hall;
+        this.starHour=starHour;
+        this.dataTag=dataTag;
     }
+
 }
