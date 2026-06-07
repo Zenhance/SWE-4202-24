cab8dbe14990535144f50032ae39b26dde53d3c0
wasifjafar <wasifjafar71@gmail.com>
2026-06-07 17:43:32 +0600

    fix fields modifiers of Cart, Customer, Ticket

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -6,11 +6,11 @@ public class Cart {
     public static final int MAX_TICKETS = 20;
     public static final int MAX_ITEMS = 20;
 
-    private Customer owner;
-    private Ticket[] tickets;
+    private final Customer owner;
+    private final Ticket[] tickets;
     private int ticketCount;
-    private ConcessionItem[] items;
-    private int[] qtys;
+    private final ConcessionItem[] items;
+    private final int[] qtys;
     private int itemCount;
 
     public Cart(Customer owner) {
