39fa6a60f4c6db340b9d318fca9d614a8c604bf8
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-06-09 03:56:40 +0600

    concessiomiem encps

--- Lab-04/CineCart/src/main/java/model/ConcessionItem.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -11,4 +11,21 @@ public class ConcessionItem {
         this.unitPrice = unitPrice;
     }
 
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public String toString(){
+        return String.format("%s - %s - BDT %.2f",  code, name, unitPrice);
+    }
+
 }
