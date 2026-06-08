63c509d07768f3f80f5c7c7c0e487b14669e601f
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-06-09 04:34:36 +0600

    chckoutengine encps

---
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -0,0 +1,19 @@
+package engine;
+
+import data.ConcessionMenu;
+import data.ShowtimeBoard;
+import model.Showtime;
+import model.ConcessionItem;
+import model.*;
+
+public class CheckoutEngine {
+    private ShowtimeBoard board;
+    private ConcessionMenu menu;
+
+
+    public CheckoutEngine(ShowtimeBoard board, ConcessionMenu menu) {
+        this.board = board;
+        this.menu = menu;
+    }
+
+}
