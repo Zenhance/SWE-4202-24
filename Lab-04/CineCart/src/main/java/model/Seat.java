f3001622b4eb04c2075b027b68eac90b5c5a36fa
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-06-09 03:30:43 +0600

    seat...ekhono bki

--- Lab-04/CineCart/src/main/java/model/Seat.java
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -1,48 +1,43 @@
 package model;
 
 public class Seat {
-    private int row;
-    private int col;
-    private boolean isPremium;
+    private final int row;
+    private final int col;
+    private final boolean isPremium;
     private boolean isBooked;
 
-    public Seat(int row, int col, boolean isPremium, boolean isBooked){
-        this.row=row;
-        this.col=col;
-        this.isPremium=isPremium;
-        this.isBooked=false;
+    public Seat(int row, int col, boolean isPremium) {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = false;
     }
 
-    public int getRow(){
+    public int getRow() {
         return row;
     }
 
-    public int getCol(){
+    public int getCol() {
         return col;
-    }
-
-    public boolean isBooked() {
-        return isBooked;
     }
 
     public boolean isPremium() {
         return isPremium;
     }
-
-    public void book(){
-        isBooked =true;
+    public boolean isAvailable() {
+        return !isBooked;
+    }
+    public boolean isBooked() {
+        return isBooked;
     }
 
-    public void release(){ isBooked =false; }
-
-    public boolean isAvailable(){ return isBooked;}
-
-
-    public String toString(){
-        String base ="R" + row + "C" + col;
-        if(isPremium) base += "*";
-        if(isBooked) base += "#";
-        return base;
+    public boolean book() {
+        if (isBooked) return false;
+        isBooked = true;
+        return true;
+    }
+    public void release() {
+        isBooked = false;
     }
 
 
