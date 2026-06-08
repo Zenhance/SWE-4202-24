d7ced833cf0f0de5eb81ea064d508b0fdeacf588
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-06-09 04:30:36 +0600

    shboard encps

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -18,7 +18,20 @@ public class ShowtimeBoard {
             shows[count++] = s;
         }
     }
+    public Showtime findById(int id){
+        for(int i = 0; i < count; i++){
+            if(shows[i].getId() == id){
+                return shows[i];
+            }
+        }
+        return null;
+    }
 
+    public void displayAll(){
+        for(int i = 0; i < count; i++){
+            System.out.println(shows[i]);
+        }
+    }
 
 
 }
