c471bf786cc7ce376f72f10f3f4180991cd2a6a4
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-06-09 02:51:50 +0600

    hall i hope done

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -1,23 +1,21 @@
 package model;
 
 public class Hall {
-    private int id;
-    private int rows;
-    private int cols;
-    private Seat[][] grid;
+    private final int id;
+    private final int rows;
+    private final int cols;
+    private final Seat[][] grid;
 
-    public Hall(int id, int rows, int cols, int premiumRows){
-        this.rows =rows;
-        this.cols= cols;
-        this.grid= new Seat[rows][cols];
+    public Hall(int id, int rows, int cols, int premiumRows) {
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new Seat[rows][cols];
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                grid[r][c] = new Seat(r, c, r< premiumRows);
 
-        for(int r=0; r<rows; r++){
-            boolean isPremium=( r< premiumRows);
-            for(int c=0; c<cols; c++)
-            {
-                grid[r][c] = new Seat(r, c, premium);
             }
-
         }
     }
 
@@ -33,12 +31,32 @@ public class Hall {
         return cols;
     }
 
-    public Seat getSeat(int rows, int cols) {
-        return grid[rows][cols];
+    public Seat getSeat(int row, int col) {
+        return grid[row][col];
     }
 
-    public int countAvailable(){
-
+    public int countAvailable() {
+        int count = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c].isAvailable()) count++;
+            }
+        }
+        return count;
     }
+    public void displayLayout() {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                Seat s = grid[r][c];
+                if (!s.isAvailable())   System.out.print('#');
+                else if (s.isPremium()) System.out.print('*');
+                else                    System.out.print('.');
+            }
+            System.out.println();
+        }
+    }
+
+
+
 
 }
