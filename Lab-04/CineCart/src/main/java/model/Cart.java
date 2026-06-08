da5a138085ebdcde54442a09abfcf9ae0de2adf4
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-06-09 04:21:03 +0600

    cart encps

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -19,6 +19,69 @@ public class Cart {
         this.itemCount = 0;
         this.qtys = new int[MAX_ITEMS];
     }
+    public boolean addTicket(Ticket t){
+        if (ticketcount >= MAX_TICKETS) return false;
+        tickets[ticketcount++] = t;
+        return true;
+    }
+
+    public boolean addItem(ConcessionItem c, int qty){
+        if (qty <= 0 || itemCount >= MAX_ITEMS) return false;
+        items[itemCount] = c;
+        qtys[itemCount] = qty;
+        itemCount++;
+        return true;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public Ticket[] getTickets() {
+        return tickets;
+    }
+
+    public int getTicketCount() {
+        return ticketcount;
+    }
+
+    public ConcessionItem[] getItems() {
+        return items;
+    }
+
+    public int[] getQtys() {
+        return qtys;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public double sumTicketsPaid(){
+        int sum = 0;
+        for(int i = 0; i < ticketcount; i++){
+            sum += tickets[i].getPricePaid();
+        }
+        return sum;
+    }
+
+    public double sumConcessionsRaw(){
+        int sum = 0;
+        for(int i = 0; i < itemCount; i++){
+            sum += (items[i].getUnitPrice() * qtys[i]);
+        }
+        return sum;
+    }
+
+    public boolean hasItem(String code){
+        for(int i = 0; i < itemCount; i++){
+            if(items[i].getCode().equals(code)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 
 
