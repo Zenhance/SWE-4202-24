d35b65898f331ef5383745c7c9081540426a45e2
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-06-09 03:54:36 +0600

    concessiomiem encps

---
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -0,0 +1,14 @@
+package model;
+
+public class ConcessionItem {
+    private final String code;
+    private final String name;
+    private final double unitPrice;
+
+    public ConcessionItem(String code, String name, double unitPrice) {
+        this.code = code;
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+}
