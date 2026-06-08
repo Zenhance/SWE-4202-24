a7a4a57d5da84aaec5b739cc6e21a03bf99baeb1
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-06-09 04:31:13 +0600

    shboard encps

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -16,5 +16,20 @@ public class ConcessionMenu {
             items[count++] = c;
         }
     }
+    public ConcessionItem findByCode(String code){
+        for(int i = 0; i < count; i++){
+            if(items[i].getCode().equals(code)){
+                return items[i];
+            }
+        }
+        return null;
+    }
+
+    public void displayAll(){
+        for(int i = 0; i < count; i++){
+            System.out.println(items[i]);
+        }
+    }
+
 
 }
