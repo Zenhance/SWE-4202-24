e576b6de08b0c564b1e962c218b9a7878ee5fbbf
ahnaf-code <ahnafcode@gmail.com>
2026-06-09 12:50:16 +0600

    create EmailMessage.java

---
+++ Lab-07/Relay/src/main/java/EmailMessage.java
@@ -0,0 +1,12 @@
+import java.util.Collection;
+
+public class EmailMessage {
+    public EmailMessage(Recipient r, String body) {
+    }
+
+    public String deliver() {
+    }
+
+    public Collection<Object> describe() {
+    }
+}
