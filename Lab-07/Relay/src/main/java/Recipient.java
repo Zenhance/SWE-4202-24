3d1f3f035b6191b3575e88e0ad008cc901c219f0
Rakib <rakibulhasan4266@gmail.com>
2026-06-09 10:15:10 +0600

    method

--- Lab-07/Relay/src/main/java/Recipient.java
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -7,5 +7,12 @@ public class Recipient
         this.name=name;
         this.address=address;
     }
-
+    public String getName()
+    {
+         return name;
+    }
+    public String getAddress()
+    {
+        return address;
+    }
 }
