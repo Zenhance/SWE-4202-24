929f5df548866e65f0b4d4cb9d458fff3556022f
Sadman Raiyan <kamrulseed@gmail.com>
2026-06-09 09:26:06 +0600

    Adding attribute and constructors for Notice class

---
+++ Lab-07/Relay/src/test/java/Notice.java
@@ -0,0 +1,17 @@
+public abstract class Notice {
+    private String text;
+    private Recipient recipient;
+
+    public Notice(String text, Recipient recipient) {
+        this.text = text;
+        this.recipient = recipient;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Recipient getRecipient() {
+        return recipient;
+    }
+}
