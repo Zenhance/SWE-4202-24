b0d894f80a4cbd378b7dc62826911eeab768e850
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-09 09:07:30 +0600

    added constructor

--- Lab-07/Relay/src/main/java/Notice.java
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -2,5 +2,10 @@ abstract class Notice {
     private final Recipient recipient;
     private final String text;
 
+    public Notice(Recipient recipient, String text){
+        this.recipient = recipient;
+        this.text = text;
+    }
+
 
 }
