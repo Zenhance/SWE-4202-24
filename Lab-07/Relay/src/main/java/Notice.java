ea4c84e270d0677931a95a17801cc5e89f2cbf68
arnoboic <arnobarafat@iut-dhaka.edu>
2026-06-09 09:08:00 +0600

    wrote parent abstract class for notice

--- Lab-07/Relay/src/main/java/Notice.java
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -1,6 +1,6 @@
 public abstract class Notice {
-    Recipient r;
-    String txt;
+    protected Recipient r;
+    protected String txt;
 
     public Notice (r, txt){
         if(r == null || r.isBlank()){
@@ -11,4 +11,9 @@ public abstract class Notice {
         this.txt = txt;
         this.r = r;
     }
+
+    public abstract String deliver();
+
+    public abstract double cost();
+
 }
