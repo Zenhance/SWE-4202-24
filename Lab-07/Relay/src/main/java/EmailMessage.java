029a4423b5e3f9a3ceb628c660fc78822e6bcf9d
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-06-09 10:16:15 +0600

    method of delivering the text

--- Lab-07/Relay/src/main/java/EmailMessage.java
+++ Lab-07/Relay/src/main/java/EmailMessage.java
@@ -7,3 +7,8 @@ public class EmailMessage extends Notice{
     public double cost() {
         return 0.0;
     }
+
+    @Override
+    public String deliver() {
+        return String.format("%s\n %s",recipient.getName());
+    }
