bab5ba8b0d3728244499c33ef0348998a8029d84
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-06-09 13:02:03 +0600

    Constructor and abstract class created

--- Lab-07/Relay/src/main/java/Notice.java
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -2,7 +2,18 @@ public class Notice {
     public  abstract class Notice{
         private Recipient recipient;
         private  String text;
-
-
+        public Notice(Recipient recipient, String text){
+            this.recipient = recipient;
+            this.text = text;
+        }
+        public Recipient getRecipient(){
+            return recipient;
+        }
+        public String getText(){
+            return text;
+        }
+        public abstract String deliver();
+        public abstract double cost();
+        public abstract String describe();
     }
 }
