f5ed582293463b29109473b077fac4292c5192f5
23-Fahad <ifahad2k@gmail.com>
2026-06-09 12:37:11 +0600

    Notice created as parent class

---
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -0,0 +1,14 @@
+public class Notice {
+     private String notice_text;
+     private String recipient;
+     public Notice(String notice_text, String recipient){
+         this.notice_text = notice_text;
+         this.recipient = recipient;
+     }
+    public String getRecipient() {
+        return recipient;
+    }
+    public String getNotice_text() {
+        return notice_text;
+    }
+}
