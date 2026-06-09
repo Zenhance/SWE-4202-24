5943b633c425c5d494e586d8ea85f29fb94ce63e
04-Labiba <sabihalabiba639@gmail.com>
2026-06-09 08:52:12 +0600

    init commit

---
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -0,0 +1,9 @@
+public class Recipient{
+    public String name;
+    public String address;
+    public  Recipient(String name,String address){
+        this.name=name;
+        this.address=address;
+    }
+
+}
