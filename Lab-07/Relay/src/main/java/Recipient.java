2bfdbddffeb4f21867a52db345f65d3660343a0a
waseefmunir-max <waseef05@gmail.com>
2026-06-09 09:10:38 +0600

    Recipient.java: Added getters for name and address

--- Lab-07/Relay/src/main/java/Recipient.java
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -6,4 +6,12 @@ public class Recipient {
         this.name = name;
         this.address = address;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
