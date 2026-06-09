89e4ebc18c0a4424f73b230f3eb2ec50954964cb
waseefmunir-max <waseef05@gmail.com>
2026-06-09 09:26:14 +0600

    Notice.java: Added constructor

--- Lab-07/Relay/src/main/java/Notice.java
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -2,5 +2,8 @@ public abstract class Notice {
     private Recipient recipient;
     private String text;
 
-
+    public Notice(Recipient recipient, String text) {
+        this.recipient = recipient;
+        this.text = text;
+    }
 }
