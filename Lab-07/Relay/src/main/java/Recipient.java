61b16aa7872294ba9ff229f553c9bd5c96e4a55e
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-09 08:59:48 +0600

    init commit

---
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -0,0 +1,16 @@
+public class Recipient {
+    private String name;
+    private String address;
+
+    public Recipient(String name,String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public String getAddress(){
+        return this.address;
+    }
+}
