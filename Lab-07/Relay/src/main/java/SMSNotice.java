c88b3bef591e3b66003b636f02c59db6e4638fd6
04-Labiba <sabihalabiba639@gmail.com>
2026-06-09 09:23:45 +0600

    init commit

--- Lab-07/Relay/src/main/java/SMSNotice.java
+++ Lab-07/Relay/src/main/java/SMSNotice.java
@@ -2,4 +2,9 @@ public class SMSNotice extends Notice{
     public SMSNotice(Recipient recipient, String text) {
         super(recipient, text);
     }
+    private int segments(){
+        return (text.length()+159)/160;
+    }
+
+    @Override
 }
