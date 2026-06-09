78f96a873b8c142bf887911df84c0335d3c33732
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-09 09:48:26 +0600

    child class email notice done

---
+++ Lab-07/Relay/src/main/java/EmailNotice.java
@@ -0,0 +1,20 @@
+public class EmailNotice extends Notice {
+    public EmailNotice(Recipient recipient, String text) {
+        super(recipient, text);
+    }
+
+    @Override
+    public String deliver() {
+        return "to " + getRecipient().getName()
+                + " <" + getRecipient().getAddress() + ">: " + getText();
+    }
+    @Override
+    public double cost(){
+        return 0.0;
+    }
+    @Override
+    public String describe(){
+        return "Email to " + getRecipient().getName()
+                + " at: " + getRecipient().getAddress();
+    }
+}
