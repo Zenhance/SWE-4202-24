f553345023250499bdac2b756b71dc0570af11c1
tahsin1r <naylakagi@gmail.com>
2026-06-09 09:37:50 +0600

    Added fields constructors for Recipient class. Created parent class Message and added methods also finished EmailMessage

--- Lab-07/Relay/src/main/java/Message.java
+++ Lab-07/Relay/src/main/java/Message.java
@@ -1,2 +1,21 @@
-public class Message {
+public abstract class Message {
+    private Recipient recipient;
+    private String body;
+
+    public Message(Recipient recipient, String body){
+        this.recipient=recipient;
+        this.body=body;
+    }
+
+    public Recipient getRecipient() {
+        return recipient;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public abstract String deliver();
+    public abstract double cost();
+    public abstract String describe();
 }
--- Lab-07/Relay/src/main/java/SmsMessage.java
+++ Lab-07/Relay/src/main/java/SmsMessage.java
@@ -1,2 +1,4 @@
 public class SmsMessage extends Message{
+    private static final int    SMS_SEGMENT_SIZE   = 160;   // chars per SMS segment
+    private static final double SMS_PER_SEGMENT    = 0.50;
 }
