994242da6431fe62d288a721fd0f68fcff36a83d
Lamisa Ibnat Zaman <lamisaibnat5d@gmail.com>
2026-06-09 12:51:48 +0600

    First commit

---
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -0,0 +1,14 @@
+public class Recipient {
+    public String name;public String address;
+    public Recipient(String name,String address){
+        this.name=name;
+        this.address=address;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAddress(){
+        return address;
+    }
+}
+
