b94479e47ecd9a9268c5c9a32641133eba468975
Nowshin77 <nowshinnipuni007@gmail.com>
2026-06-09 13:03:49 +0600

    email class

--- Lab-07/Relay/src/main/java/Email.java
+++ Lab-07/Relay/src/main/java/Email.java
@@ -1,2 +1,17 @@
-public class Email {
+public class Email extends Notice{
+    public Email(Recipient recipient, String text){
+        super(recipient,text);
+    }
+    @Override
+    public String deliver(){
+        return " Email to " +  recipient.getName() + "<" + recipient.getAddress() + ">:" + text;
+    }
+    @Override
+    public double cost(){
+     return 0.0;
+    }
+    @Override
+    public String describe(){
+        return "Email Notice";
+    }
 }
