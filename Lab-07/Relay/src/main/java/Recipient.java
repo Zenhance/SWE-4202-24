3119a48d3aac6338a184f089bceee4e7286b1467
Mubtasim Fuad <mubtasimsmail@gmail.com>
2026-06-09 12:55:54 +0600

    created constructor for Recipient class

--- Lab-07/Relay/src/main/java/Recipient.java
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -2,6 +2,9 @@ public class Recipient {
     private String  name;
     private String address;
     public Recipient(String name, String address) {
+        if(name.isEmpty() || address.isEmpty()){
+            throw new IllegalArgumentException("Recipient name or address cannot be empty");
+        }
         this.name = name;
         this.address = address ;
 
