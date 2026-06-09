0aad908b406f62d7ff7029ad03f922e38cfd2ee3
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-09 09:00:48 +0600

    init commit

---
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -0,0 +1,8 @@
+public class Recipient {
+    public String name;
+    public String address;
+    public Recipient(String name,String address){
+        this.name=name;
+        this.address=address;
+    }
+}
