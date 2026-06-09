12d13855befef86c218af913bebd4ddce2fc86a1
anonymous <farnaz28.rahman@gmail.com>
2026-06-09 12:33:16 +0600

    Notice class constructor

---
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -0,0 +1,11 @@
+public abstract class Notice {
+       private Recipient recipient;
+       private String str;
+
+
+    public Notice(Recipient recipient, String str){
+    this.recipient=recipient;
+    this.str=str;
+}
+
+}
