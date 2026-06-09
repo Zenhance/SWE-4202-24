3fafcbd55b6b98d11b3640a01f9338f2f488ea67
iam-tahmid <tahmidim99@gmail.com>
2026-06-09 13:04:59 +0600

    condition applied

--- Lab-07/Relay/src/main/java/recepient.java
+++ Lab-07/Relay/src/main/java/recepient.java
@@ -3,5 +3,13 @@ public class recepient {
     private String address;
 
     public recepient (String name, String address){
+        if (name==null) || name.isBlank(){
+            throw new IllegalArgumentException(" No Name ");
+        }
+        if (address == null) || address.isBlank(){
+            throw new IllegalArgumentException("No Address");
+        }
+        this.name = name;
+        this.address = address;
     }
 }
