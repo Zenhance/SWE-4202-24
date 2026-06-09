a6a9013d8f687f3ac0364a17911a5798abf84e87
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-09 09:15:54 +0600

    init commit

--- Lab-07/Relay/src/main/java/Notice.java
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -5,4 +5,8 @@ public abstract class Notice {
         this.recipient=recipient;
         this.text=text;
     }
+    //method
+    public abstract String deliver();
+    public abstract double cost();
+    public abstract String describe();
 }
