9c9f69ccfbeb6e1f5ea922d75fecb7e77564abcb
jasin <anishaajasin.13.04.2005@gmail.com>
2026-06-09 09:23:06 +0600

    Notice

---
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -0,0 +1,14 @@
+public class Notice {
+     private Recipient recipient;
+     private String text;
+     public Notice(Recipient recipient, String text) {
+        this.recipient = recipient;
+        this.text = text;
+     }
+     public Recipient getRecipient() {
+         return recipient;
+     }
+     public String getText() {
+         return text;
+     }
+}
