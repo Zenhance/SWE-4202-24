13c43e8a10f6632a1b9c10e0fad2b2274a68acac
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-09 09:44:04 +0600

    init commit

--- Lab-07/Relay/src/main/java/PushNotice.java
+++ Lab-07/Relay/src/main/java/PushNotice.java
@@ -1,5 +1,17 @@
+import com.sun.jdi.event.StepEvent;
+
 public class PushNotice extends Notice {
     public PushNotice(Recipient recipient, String text) {
         super(recipient, text);
     }
+    @Override
+    public String deliver(){
+        String preview;
+        if(text.length()<=40){
+            preview=text;
+        }
+        else{
+
+        }
+
 }
