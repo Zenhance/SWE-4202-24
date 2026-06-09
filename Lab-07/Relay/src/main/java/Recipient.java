da664634f233793f25c68e23b877e3a84c6dbac4
anonymous <farnaz28.rahman@gmail.com>
2026-06-09 12:49:01 +0600

    Recipient getters

--- Lab-07/Relay/src/main/java/Recipient.java
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -6,4 +6,11 @@ public class Recipient {
         this.address=address;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
