dec8e3761be8396a86d30fe5265e7146b2291e64
Maomao-starry <aminarahmanshalu@gmail.com>
2026-06-09 12:44:59 +0600

    task

--- Lab-07/Relay/src/main/java/Email.java
+++ Lab-07/Relay/src/main/java/Email.java
@@ -2,6 +2,17 @@ public class Email extends Notice{
     public Email(Recipent recipent ,String text){
         super(recipent,text);
     }
-
+    @Override
+    public String deliver(){
+        return "To: " + recipent.getName() + " (" + recipent.getAdrress() +" )" +test;
+    }
+    @Override
+    public double price(){
+        return 0.0;
+    }
+    @Override
+    public String describe(){
+        return "Email to "+recipent.getName();
+    }
 
 }
