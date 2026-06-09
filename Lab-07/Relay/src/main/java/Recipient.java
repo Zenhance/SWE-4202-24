aee5f46313f900663fe56cca8625f0053c186dfc
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-06-09 12:41:22 +0600

    receipt java class

---
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -0,0 +1,19 @@
+public class Recipient {
+    private final String name;
+    private final String address;
+
+    public Recipient(String name, String address){
+        this.name= name;
+        this.address=address;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+
+}
