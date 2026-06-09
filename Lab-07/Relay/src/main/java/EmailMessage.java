fad01e8dc627a9693af13ce615921532a1941855
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-09 09:57:51 +0600

    Change : add overrides to EmailMessage  class

--- Lab-07/Relay/src/main/java/EmailMessage.java
+++ Lab-07/Relay/src/main/java/EmailMessage.java
@@ -3,4 +3,19 @@ public class EmailMessage extends Message {
     public EmailMessage(Recipient recipient, String text) {
         super(recipient, text);
     }
+
+    @Override
+    public String deliver() {
+        return "";
+    }
+
+    @Override
+    public double cost() {
+        return 0;
+    }
+
+    @Override
+    public String describe() {
+        return "";
+    }
 }
