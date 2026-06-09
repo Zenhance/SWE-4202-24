e1e0c407f47685a6b05805babf832348f1c6548a
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-09 09:53:24 +0600

    added constructor

--- Lab-07/Relay/src/main/java/PriorityEmailMessage.java
+++ Lab-07/Relay/src/main/java/PriorityEmailMessage.java
@@ -1,4 +1,8 @@
 public class PriorityEmailMessage extends EmailMessage{
     private static final double PRIORITY_SURCHARGE = 0.30;
 
+    public PriorityEmailMessage(Recipient recipient, String text){
+        super(recipient,text);
+    }
+
 }
