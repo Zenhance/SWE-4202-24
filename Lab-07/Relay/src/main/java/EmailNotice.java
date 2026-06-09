0c1e3e44f2589d010b0d78b6726d90d3744ee4da
04-Labiba <sabihalabiba639@gmail.com>
2026-06-09 09:16:23 +0600

    init commit

---
+++ Lab-07/Relay/src/main/java/EmailNotice.java
@@ -0,0 +1,18 @@
+public class EmailNotice extends Notice{
+
+    @Override
+    public String deliver() {
+        return "";
+    }
+
+    @Override
+    public double cost() {
+        return 0;
+
+    }
+
+    @Override
+    public String describe() {
+        return "";
+    }
+}
