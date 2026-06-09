bd6ca0c188d305e65c52820d63adbe964be28ec8
tahsin1r <naylakagi@gmail.com>
2026-06-09 08:38:05 +0600

    Added RelayTest

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -4,16 +4,14 @@ import model.Showtime;
 
 public class ShowtimeBoard {
     private Showtime[] shows;
-    private int count;
+    private int count=0;
     private static final int MAX=30;
 
     public Showtime[] getShows() {
         return shows;
     }
 
-    public int getCount() {
-        return count;
-    }
+
 
     public static int getMAX() {
         return MAX;
