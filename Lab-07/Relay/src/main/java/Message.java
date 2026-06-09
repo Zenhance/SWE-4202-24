150033f40eafd40d5ddcfbf1b64b167f5765a445
Tnvr09 <rahmantanvir64@gmail.com>
2026-06-09 09:21:53 +0600

    Message class initiated

---
+++ Lab-07/Relay/src/main/java/Message.java
@@ -0,0 +1,11 @@
+public class Message {
+    private Recipient recipient;
+    private String body;
+
+    public Message(Recipient recipient, String body) {
+        this.recipient = recipient;
+        this.body = body;
+    }
+
+
+}
