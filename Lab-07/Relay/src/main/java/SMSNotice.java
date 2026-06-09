f1d2baaa2b31e3bdfebb9383fc0c5e79e1ecab97
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-09 09:24:06 +0600

    added constructor

--- Lab-07/Relay/src/main/java/SMSNotice.java
+++ Lab-07/Relay/src/main/java/SMSNotice.java
@@ -1,4 +1,8 @@
 public class SMSNotice extends Notice{
     private static final int    SMS_SEGMENT_SIZE   = 160;
     private static final double SMS_PER_SEGMENT    = 0.50;
+
+    public SMSNotice(Recipient recipient,String text){
+        super(recipient,text);
+    }
 }
