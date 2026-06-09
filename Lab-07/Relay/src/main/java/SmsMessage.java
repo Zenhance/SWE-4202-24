30d669ecf39d7dd452e5c134f2c6e7665d1c33de
nusra <nusratnishi1103@gmail.com>
2026-06-09 12:52:00 +0600

    first

---
+++ Lab-07/Relay/src/main/java/SmsMessage.java
@@ -0,0 +1,10 @@
+public class SmsMessage extends Message{
+    private static final int SEGMENT_SIZE = 160;
+    private static final double COST_PER_SEGMENT=0.50;
+    public SmsMessage(Recipient recipient,String text){
+        super(recipient,text);
+    }
+    private int getSegments(){
+     return Math.max(1,(int)Math.ceil(text.length()/(double)SEGMENT_SIZE));
+    }
+}
