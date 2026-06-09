cba0da2c1e24d9a2c6771fc467cd5938fe2955d4
04-Labiba <sabihalabiba639@gmail.com>
2026-06-09 09:44:30 +0600

    init commit

--- Lab-07/Relay/src/main/java/PriorityEmailNotice.java
+++ Lab-07/Relay/src/main/java/PriorityEmailNotice.java
@@ -3,4 +3,19 @@ public class PriorityEmailNotice extends EmailNotice{
     public PriorityEmailNotice(Recipient recipient, String text) {
         super(recipient, text);
     }
+    @Override
+    public String deliver() {
+        return "[PRIORITY]"+super.deliver();
+    }
+
+    @Override
+    public double cost() {
+        return super.cost()+SURCHARGE;
+
+    }
+
+    @Override
+    public String describe() {
+        return "Priority Email Notice";
+    }
 }
