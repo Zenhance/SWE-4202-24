520bb6d8cf8970c6afa3fc7dcdd683a104ab9749
Tnvr09 <rahmantanvir64@gmail.com>
2026-06-09 09:38:14 +0600

    EmailMessage class initiated & completed

---
+++ Lab-07/Relay/src/main/java/EmailMessage.java
@@ -0,0 +1,17 @@
+public class EmailMessage extends Message{
+    public EmailMessage(Recipient recipient, String body) {
+        super(recipient, body);
+    }
+
+    public String deliver() {
+        return ("To" + getRecipient().getName() + " " + getRecipient().getAddress() + getBody());
+    }
+
+    public double cost() {
+        return 0.0;
+    }
+
+    public String describe() {
+        return ("Mail send to " +getRecipient().getName() + getRecipient().getAddress());
+    }
+}
