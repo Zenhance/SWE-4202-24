cb37eb43524bf806c414eceb15522c81155fcbe3
04-Labiba <sabihalabiba639@gmail.com>
2026-06-09 09:35:42 +0600

    init commit

--- Lab-07/Relay/src/main/java/PushNotice.java
+++ Lab-07/Relay/src/main/java/PushNotice.java
@@ -2,4 +2,16 @@ public class PushNotice extends Notice{
     public PushNotice(Recipient recipient, String text) {
         super(recipient, text);
     }
+@Override
+public String deliver(){
+        String preview;
+        if(text.length()<=40){
+            preview=text;
+        }
+}
+    @Override
+    public double cost() {
+        return 0;
+    }
+
 }
