174d8e918db6903b2172856c15eb17ed8f73ac70
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-06-09 12:41:06 +0600

    Constructor created.

--- Lab-07/Relay/src/main/java/Recipient.java
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -1,4 +1,21 @@
 public class Recipient {
     private String name;
     private String address;
+    public Recipient(String name, String address){
+        if ((name==null)|| name.isBlank()){
+            throw new IllegalArgumentException("No name");
+        }
+        if ((address==null)|| address.isBlank()){
+            throw new IllegalArgumentException("No address");
+        }
+        this.name = name;
+        this.address = address;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAddress(){
+        return address;
+    }
+}
 
