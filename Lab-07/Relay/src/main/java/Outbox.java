0570665ab93926413c8e0ac20e3f60356d12575f
04-Labiba <sabihalabiba639@gmail.com>
2026-06-09 10:02:39 +0600

    init commit

--- Lab-07/Relay/src/main/java/Outbox.java
+++ Lab-07/Relay/src/main/java/Outbox.java
@@ -5,4 +5,16 @@ public class Outbox {
         notices=new Notice[100];
         count=0;
     }
+    public void queue(Notice n){
+       notices[count++]=n;
+    }
+    public void queue(Notice n,int repeat){
+        for(int i=0;i<repeat;i++){
+            notices[count++]=n;
+        }
+    }
+    public int waitingCount(){
+        return count;
+    }
+
 }
