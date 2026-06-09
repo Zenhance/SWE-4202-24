b01b6e6585f7751dca017628955954b76a8119ea
Mubtasim Fuad <mubtasimsmail@gmail.com>
2026-06-09 12:39:42 +0600

    basic structure of Recipient Class

---
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -0,0 +1,10 @@
+public class Recipient {
+    private String  name;
+    private String address;
+    public Recipient(String name, String address) {
+        this.name = name;
+        this.address = address ;
+
+    }
+
+}
