9f409d8ed1af5d73f3019abdd3b03cac954586eb
ayiham-1012 <ayiham1012@gmail.com>
2026-06-09 12:27:20 +0600

    3. Editing Notice.java

--- Lab-07/Relay/src/main/java/Notice.java
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -1,9 +1,15 @@
 public class Notice{
     String channel;
     String body;
+    static int noticeCount = 0;
 
     public Notice(String channel, String body) {
         this.channel = channel;
         this.body = body;
+        noticeCount++;
+    }
+
+    public void addRecipient(){
+
     }
 }
