4b0119e6cac010fcb3fa18e8ef37977d5a4965a2
sabbir240042147 <eftesabbir2725@gmail.com>
2026-06-09 12:47:33 +0600

    notice class

---
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -0,0 +1,20 @@
+public abstract class Notice {
+    protected Recipient recipient;
+    protected String text;
+    public Notice(Recipient recipient,String text){
+        this.recipient=recipient;
+        this.text=text;
+    }
+
+    public Recipient getRecipient() {
+        return recipient;
+    }
+
+    public String getText() {
+        return text;
+    }
+    public abstract String deliver();
+    public abstract double cost;
+    public abstract String describe();
+
+}
