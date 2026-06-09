1a1db248c3711aa1ebc415925add38644557dd41
Mubtasim Fuad <mubtasimsmail@gmail.com>
2026-06-09 12:45:45 +0600

    created all Classes

---
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -0,0 +1,8 @@
+public abstract class Notice {
+    private String channel;
+    private String body;
+
+
+    public abstract String transmit(String channel, String body);
+    public abstract String 
+}
