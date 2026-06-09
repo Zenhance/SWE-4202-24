f49e2ca8094fdfce73d0001ffbd79e1f599d0127
jasin <anishaajasin.13.04.2005@gmail.com>
2026-06-09 09:17:05 +0600

    Notice

---
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -0,0 +1,22 @@
+public class Recipient {
+    private String name;
+    private String address;
+
+    public Recipient(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
