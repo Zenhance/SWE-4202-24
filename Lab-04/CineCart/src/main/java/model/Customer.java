73d7458e5a979caf6d7bb6cc40b7adf9652dbd24
iam-tahmid <tahmidim99@gmail.com>
2026-06-09 10:45:14 +0600

    classes created for customer class

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -1,4 +1,21 @@
 package model;
 
 public class Customer {
+     int id;
+     String name;
+     int age;
+     String loyaltyTier;
+    public Customer(int id, String name, int age, String tier){
+        this.id = id;
+        this.name = name;
+        this.age =age;
+        this.loyaltyTier = tier;
+    }
+
+    public Customer(int id, String name, int age){
+        this.id= id;
+        this.age=age;
+        this.name=name;
+        loyaltyTier = "Basic";
+    }
 }
