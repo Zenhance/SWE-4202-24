06ce25cb2ae893113ade41da50c96602997e2990
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-06-09 12:29:02 +0600

    recipient

--- Lab-07/Relay/src/main/java/Recipient.java
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -3,5 +3,12 @@ public class Recipient {
     private String name;
     private String address;
 
+
+public Recipient(String name,String address){
+
+    this.name=name;
+    this.address=address;
+}
+
 }
 
