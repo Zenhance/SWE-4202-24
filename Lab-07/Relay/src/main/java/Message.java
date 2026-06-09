60d9be4687165a56b4a534feefc6df2faaa7a318
nusra <nusratnishi1103@gmail.com>
2026-06-09 12:32:07 +0600

    first

---
+++ Lab-07/Relay/src/main/java/Message.java
@@ -0,0 +1,11 @@
+public abstract class Message {
+    protected Recipient recipient;
+    protected String text;
+    public Message(Recipient recipient,String text){
+        this.recipient=recipient;
+        this.text=text;
+    }
+    public abstract String deliver();
+    public abstract double cost();
+    public abstract String describe();
+}
