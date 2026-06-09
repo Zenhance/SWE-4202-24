ec7a6798f444de2763f7f6b3dbe26a950f017ccb
Nowshin77 <nowshinnipuni007@gmail.com>
2026-06-09 12:47:03 +0600

    recipient class

--- Lab-07/Relay/src/main/java/Notice.java
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -2,11 +2,15 @@ public abstract class Notice {
     protected Recipient recipient;
     protected String text;
 
-    public Notice(Recipient recipient, String text){
-        this.recipient=recipient;
-        this.text=text;
+    public Notice(Recipient recipient, String text) {
+        this.recipient = recipient;
+        this.text = text;
     }
-public abstract String deliver();
 
+    public abstract String deliver();
 
+    public abstract double cost();
+
+    public abstract String describe();
 }
+
