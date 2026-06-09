94390116ed8e1223d2c476edad368bb217b11585
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-09 09:22:16 +0600

    init commit

--- Lab-07/Relay/src/main/java/EmailNotice.java
+++ Lab-07/Relay/src/main/java/EmailNotice.java
@@ -2,4 +2,8 @@ public class EmailNotice extends Notice {
     public EmailNotice(Recipient recipient,String text){
         super(recipient,text);
     }
+    @Override
+    public String deliver(){
+        return "EMAIL TO: " + recipient.getName()+"\n" + text;
+    }
 }
