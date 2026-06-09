94f34a253833a5a431e70e7ef3b9031fd479c81f
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-06-09 09:05:22 +0600

    notice 1

---
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -0,0 +1,9 @@
+public abstract class Notice {
+    private Recipient recipient;
+    private String text;
+
+    public Notice(Recipient recipient, String text){
+        this.recipient=recipient;
+        this.text=text;
+    }
+}
