6e89539715908cae768c3ff4f717f2af8db32f90
tasnitaraida <tasnitar@gmail.com>
2026-06-09 12:41:54 +0600

    constructor

--- Lab-07/Relay/src/main/java/Notices.java
+++ Lab-07/Relay/src/main/java/Notices.java
@@ -1,4 +1,9 @@
-public class Notices {
+public abstract class Notices {
     private Recipient recipient;
     private String text;
+
+    Notices(Recipient recipient, String text) {
+        this.recipient=recipient;
+        this.text=text;
+    }
 }
