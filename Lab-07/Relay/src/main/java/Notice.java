dc6126aac684c25eabe085fb5543e660f8488512
Zunied-57 <zunied0411@gmail.com>
2026-06-09 09:11:50 +0600

    notice created

---
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -0,0 +1,13 @@
+public class Notice
+{
+    private String channel;
+    private String body;
+
+    public Notice(String channel, String body)
+    {
+        this.channel = channel;
+        this.body = body;
+    }
+
+
+}
