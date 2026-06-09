d6e5e21c9399ec1f60fd45db0d6a3ad80d1889a2
Rakib <rakibulhasan4266@gmail.com>
2026-06-09 09:58:49 +0600

    constructor

--- Lab-07/Relay/src/main/java/Recipient.java
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -2,5 +2,10 @@ public class Recipient
 {
     private final String name;
     private final String address;
+    public Recipient(String name,String address)
+    {
+        this.name=name;
+        this.address=address;
+    }
 
 }
