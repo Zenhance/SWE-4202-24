757aa694c12e8231178aeb5a877648b2f0f7c62a
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-06-09 10:15:53 +0600

    cost of email add

--- Lab-07/Relay/src/main/java/EmailMessage.java
+++ Lab-07/Relay/src/main/java/EmailMessage.java
@@ -2,3 +2,8 @@ public class EmailMessage extends Notice{
     public EmailMessage (Recipient recipient, String text){
         super(recipient,text);
     }
+
+    @Override
+    public double cost() {
+        return 0.0;
+    }
