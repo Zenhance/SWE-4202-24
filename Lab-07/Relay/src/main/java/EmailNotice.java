c0282a9c9bbda73d3b8915573dd54f4b36f9be44
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-06-09 12:36:20 +0600

    updating EmailNotice

--- Lab-07/Relay/src/main/java/EmailNotice.java
+++ Lab-07/Relay/src/main/java/EmailNotice.java
@@ -1,8 +1,11 @@
 public class EmailNotice extends Notice {
 
+    private double cost;
 
     //constructor
-    public EmailNotice (Recipient recipient, String text, String ) {
+    public EmailNotice (Recipient recipient, String text, String cost) {
         super(text, recipient);
+
+        this.cost = 0.0;
     }
 }
