babf3a1981792e7708218c1641297f0b15aab869
arnoboic <arnobarafat@iut-dhaka.edu>
2026-06-09 09:36:57 +0600

    updated PriorityEmailMessage

--- Lab-07/Relay/src/main/java/PriorityEmailMessage.java
+++ Lab-07/Relay/src/main/java/PriorityEmailMessage.java
@@ -1,2 +1,12 @@
 public class PriorityEmailMessage extends EmailMessage {
+    String priorityFlag = "priority";
+
+    public PriorityEmailMessage(Recipient r, String txt) {
+        super(r, txt);
+    }
+
+    @Override
+    public String deliver(){
+        return priorityFlag+txt;
+    }
 }
