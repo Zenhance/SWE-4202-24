45f0343f1dc7307f90a2305db3f6276be8b67c9f
anonymous <farnaz28.rahman@gmail.com>
2026-06-09 12:48:17 +0600

    Recipient constructor

--- Lab-07/Relay/src/main/java/Recipient.java
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -1,5 +1,9 @@
 public class Recipient {
     private String name;
     private String address;
+    public Recipient(String name,String address){
+        this.name=name;
+        this.address=address;
+    }
 
 }
