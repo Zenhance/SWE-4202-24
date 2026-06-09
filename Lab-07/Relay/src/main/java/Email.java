d214022a23f5f51db0e98330e7e107ef4909c9ea
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-06-09 12:48:27 +0600

    email methods

--- Lab-07/Relay/src/main/java/Email.java
+++ Lab-07/Relay/src/main/java/Email.java
@@ -3,4 +3,20 @@ public class Email extends Message{
     public Email(Recipient recipient,String bodytext){
         super(recipient,bodytext);
     }
+
+
+    public String deliver(){
+
+
+    }
+
+    public double cost(){
+
+
+    }
+
+    public String describe(){
+
+
+    }
 }
