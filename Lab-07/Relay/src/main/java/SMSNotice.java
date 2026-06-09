857e16cc8766f0d393992a0e99928b3b25262653
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-09 09:32:15 +0600

    init commit

--- Lab-07/Relay/src/main/java/SMSNotice.java
+++ Lab-07/Relay/src/main/java/SMSNotice.java
@@ -2,4 +2,7 @@ public class SMSNotice extends Notice{
     public SMSNotice (Recipient recipient,String text){
         super (recipient,text);
     }
+    private int segments(){
+        return (text.length()+159)/160;
+    }
 }
