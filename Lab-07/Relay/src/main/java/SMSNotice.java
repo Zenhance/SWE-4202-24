029447ec27f19d30bbe220f0ca8c26ffdec04c14
cse <ayeshamahjabin@iut-dhaka.edu>
2026-06-09 09:59:29 +0600

    Completed SMSNotice class

---
+++ Lab-07/Relay/src/main/java/SMSNotice.java
@@ -0,0 +1,32 @@
+public class SMSNotice extends Notice {
+
+    private static final int SIZE = 160;
+    private static final double COST= 0.50;
+
+    public SMSNotice(Recipient recipient, String text) {
+        super(recipient, text);
+    }
+
+    private int segments() {
+        int length = getText().length();
+        if (length == 0) {
+            return 1;
+        }
+        return (length + SIZE - 1) / SIZE;
+    }
+
+    @Override
+    public String deliver() {
+        return "SMS to " + getRecipient().getName() + " <" + getRecipient().getAddress() + "> [" + segments() + " segment(s)]: " + getText();
+    }
+
+    @Override
+    public double cost() {
+        return segments() * COST;
+    }
+
+    @Override
+    public String describe() {
+        return "SMS notice to " + getRecipient().getName() + " using " + segments() + " segment(s)";
+    }
+}
