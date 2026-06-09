b1e8f9c0ccddc075e95eb12ce232017dd1546ba2
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-09 10:01:08 +0600

    added constructor

--- Lab-07/Relay/src/main/java/Outbox.java
+++ Lab-07/Relay/src/main/java/Outbox.java
@@ -1,5 +1,13 @@
 public class Outbox {
-private final Notice[] notices;
-private int count;
+  private final Notice[] notices;
+  private int count;
 
+  public Outbox(){
+      notices = new Notice[100];
+      count = 0;
+  }
+
+  public void queue(Notice notice){
+      notices[count]
+  }
 }
