32dca92dcb66e7fd39688ac9d62cd1cc4f16689f
Zunied-57 <zunied0411@gmail.com>
2026-06-09 12:46:23 +0600

    priority email created

--- Lab-07/Relay/src/main/java/PriorityEmailMessage.java
+++ Lab-07/Relay/src/main/java/PriorityEmailMessage.java
@@ -8,4 +8,21 @@ public class PriorityEmailMessage extends EmailMessage
         super(recipient,text);
     }
 
+    @Override
+    public double cost()
+    {
+        return SURCHARGE;
+    }
+
+    @Override
+    public String describe()
+    {
+        return "PRIORITY";
+    }
+
+    @Override
+    public String deliver()
+    {
+        return priorityFlag+" "+recipient.getName()+" "+recipient.getAddress()+" "+text;
+    }
 }
