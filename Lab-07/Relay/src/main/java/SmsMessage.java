fdd6ddae9c9604408106fc315cdf14d430dafc28
nusra <nusratnishi1103@gmail.com>
2026-06-09 13:01:32 +0600

    first

--- Lab-07/Relay/src/main/java/SmsMessage.java
+++ Lab-07/Relay/src/main/java/SmsMessage.java
@@ -5,6 +5,15 @@ public class SmsMessage extends Message{
         super(recipient,text);
     }
     private int getSegments(){
-     return Math.max(1,(int)Math.ceil(text.length()/(double)SEGMENT_SIZE));
+        return Math.max(1,(int)Math.ceil(text.length()/(double)SEGMENT_SIZE));
+    }
+    public String deliver(){
+        return "SMS (" + getSegments() + " segments): " + text;
+    }
+    public double cost(){
+        return getSegments()* COST_PER_SEGMENT;
+    }
+    public String describe(){
+        return "SMS Message";
     }
 }
