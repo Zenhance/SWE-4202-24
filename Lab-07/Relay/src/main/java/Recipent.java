90084f7601e5503b287b049495dd7fa10605c0ea
Maomao-starry <aminarahmanshalu@gmail.com>
2026-06-09 12:33:58 +0600

    task

---
+++ Lab-07/Relay/src/main/java/Recipent.java
@@ -0,0 +1,16 @@
+public class Recipent{
+    private String name;
+    private String address;
+    public Recipent(String name,String address){
+        this.name=name;
+        this.address=address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
