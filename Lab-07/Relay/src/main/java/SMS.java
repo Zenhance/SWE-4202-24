91ec5fc0b59d1db5e6a9d12e1ae5b817ffe6dc34
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-06-09 12:50:56 +0600

    sms skeleton writing

---
+++ Lab-07/Relay/src/main/java/SMS.java
@@ -0,0 +1,22 @@
+public class SMS extends Message{
+
+    public SMS(Recipient recipient,String bodytext){
+        super(recipient,bodytext);
+    }
+
+
+    public String deliver(){
+
+
+    }
+
+    public double cost(){
+
+
+    }
+
+    public String describe(){
+
+
+    }
+}
