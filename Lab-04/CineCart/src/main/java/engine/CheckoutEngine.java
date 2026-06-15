43a425f1cfc9a31abe7201c15f79f4073965dc7b
Rakib <rakibulhasan4266@gmail.com>
2026-06-15 14:27:39 +0600

    define getReceipt method

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -68,4 +68,13 @@ public class CheckoutEngine
         return Math.round((afterDiscounts + tax) * 100.0) / 100.0;
     }
 
+    public String getReceipt(Cart cart)
+    {
+        double total = checkout(cart);
+        String receipt = "Receipt\n";
+        receipt += "Customer: " + cart.getOwner().getName() + "\n";
+        receipt += "Discount applied\n";
+        receipt += String.format("Total: BDT %.2f\n", total);
+        return receipt;
+    }
 }
