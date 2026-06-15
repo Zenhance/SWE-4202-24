85cfafd940fdf6d48760b1babf701857de3bf236
Rakib <rakibulhasan4266@gmail.com>
2026-06-15 12:17:36 +0600

    class and Constructor created

---
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Cart
+{
+    public static final int MAX_TICKETS = 20;
+    public static final int MAX_ITEMS = 20;
+    private Customer owner;
+    private Ticket[] tickets;
+    private int ticketCount;
+    private ConcessionItem[] items;
+    private int[] qtys;
+    private int itemCount;
+
+    public Cart(Customer Owner)
+    {
+        this.owner = owner;
+        this.tickets = new Ticket[MAX_TICKETS];
+        this.ticketCount = 0;
+        this.items = new ConcessionItem[MAX_ITEMS];
+        this.qtys = new int[MAX_ITEMS];
+        this.itemCount = 0;
+    }
+
+
+}
