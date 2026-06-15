5cdb83b957f2326e4110e13f202fcebe1c25050e
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-06-15 23:25:36 +0600

    initial commit

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -3,4 +3,57 @@ package model;
 public class Cart {
     public static final int MAX_TICKETS = 20;
     public static final int MAX_ITEMS = 20;
+
+    Customer owner;
+    Ticket[] tickets;
+    int ticketCount;
+    ConcessionItem[] items;
+    int[] qtys;
+    int itemCount;
+
+    public Cart(Customer owner) {
+        this.owner = owner;
+        tickets = new Ticket[MAX_TICKETS];
+        items = new ConcessionItem[MAX_ITEMS];
+        ticketCount = 0;
+        itemCount = 0;
+    }
+
+    public void addTicket(Ticket t) {
+
+    }
+
+    void addItem(ConcessionItem c, int qty) {
+
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+    public Ticket[] getTickets() {
+        return tickets;
+    }
+    public int getTicketCount() {
+        return ticketCount;
+    }
+    public ConcessionItem[] getItems() {
+        return items;
+    }
+    public int[] getQtys() {
+        return qtys;
+    }
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public double sumTicketPaid() {
+
+    }
+
+    public double sumConcessionRaw() {
+
+    }
+
+    public boolean hasItem(String code) {}
+
 }
