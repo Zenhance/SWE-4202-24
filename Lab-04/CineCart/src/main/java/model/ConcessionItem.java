933159fc18c7ccf1d691bac9c006548a43bedb82
Rakib <rakibulhasan4266@gmail.com>
2026-06-15 11:49:00 +0600

    class and constructor created

---
+++ Lab-04/CineCart/src/main/java/model/ConcessionItem.java
@@ -0,0 +1,16 @@
+package model;
+
+public class ConcessionItem
+{
+    private String code;
+    private String name;
+    private double unitPrice;
+
+    public ConcessionItem(String code, String name,String unitPrice)
+    {
+        this.code = code;
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+}
