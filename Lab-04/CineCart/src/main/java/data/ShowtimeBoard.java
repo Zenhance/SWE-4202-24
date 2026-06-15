6bf17450ece18af573e88d14a7cd18dd779ff540
Rakib <rakibulhasan4266@gmail.com>
2026-06-15 13:18:38 +0600

    add method created

--- Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/data/ShowtimeBoard.java
@@ -14,4 +14,13 @@ public class ShowtimeBoard
         count = 0;
     }
 
+    public void add(Showtime s)
+    {
+        if(count<MAX)
+        {
+            shows[count] = s;
+            count++;
+        }
+    }
+
 }
