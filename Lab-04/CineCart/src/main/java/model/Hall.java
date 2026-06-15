c2e5b83205425b88ba98f6e7bd4ba09dbe35ea0d
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-06-15 21:58:21 +0600

    initial commit

--- Lab-04/CineCart/src/main/java/model/Hall.java
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -10,6 +10,13 @@ public class Hall {
         this.id = id;
         this.rows = rows;
         this.cols = cols;
+        this.grid = new Seat[rows][cols];
+
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
+               grid[r][c] = new Seat(r, c, r < premiumRows);
+            }
+        }
     }
 
     Seat getSeat(int row, int col) {
