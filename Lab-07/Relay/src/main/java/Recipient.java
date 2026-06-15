4c7524b4c90a025bb10e2c2312f3b478c1f96461
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-16 05:29:50 +0600

    constructor build with error handling

--- Lab-07/Relay/src/main/java/Recipient.java
+++ Lab-07/Relay/src/main/java/Recipient.java
@@ -2,5 +2,14 @@ public class Recipient {
     private String name;
     private String address;
 
-
+    public Recipient(String name, String address){
+        if(name == null || name.isEmpty()){
+            throw new IllegalArgumentException("Name needed");
+        }
+        if(address == null || address.isEmpty()){
+            throw new IllegalArgumentException("Address needed");
+        }
+        this.name = name;
+        this.address = address;
+    }
 }
