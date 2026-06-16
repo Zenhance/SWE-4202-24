a273840f0f788fa1daefe58b4c1a0de333d26a47
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-06-16 09:34:25 +0600

    constructor and method added for StudentTicket

--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -1,4 +1,13 @@
 package model;
 
-public class StudentTicket {
+public class StudentTicket extends AbstractTicket{
+    public StudentTicket(Showtime showtime, int row, int col){
+        super(showtime, row, col);
+    }
+
+    public double subtotal(){
+        if (getShowtime().isPeak())
+            return 350.00 * 0.50 * 1.20;
+        else return 200.00 * 0.50;
+    }
 }
