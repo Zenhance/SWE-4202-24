5bd627de5291ffa3cd2284a43653461232e19565
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-06-16 12:09:58 +0600

    StudentTicket class

---
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -0,0 +1,22 @@
+package model;
+
+public class StudentTicket extends Ticket{
+
+protected static final double STUDENT = 0.50;
+public StudentTicket (Showtime showtime, int row, int col)
+
+{
+    super (showtime,row, col);
+
+}
+@Override
+    public double subTotal ()
+{
+    return base*STUDENT*peakTime();
+
+
+}
+
+
+
+}
