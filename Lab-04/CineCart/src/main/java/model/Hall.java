d1b4b6b61a4b418c238d27460521260e06838eb5
IbCoder25 <ibrahimababa500@gmail.com>
2026-06-16 09:49:27 +0600

    class seat

---
+++ Lab-04/CineCart/src/main/java/model/Hall.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Hall {
+    private int id;
+    private int rows;
+    private int cols;
+    private Seat[][] grid;
+
+    public Hall(int id, int rows, int cols, int premiumRows) {
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+
+        grid = new Seat[rows][cols];
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                grid[r][c] = new Seat(r, c, r < premiumRows);
+            }
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Seat getSeat(int row, int col) {
+        return grid[row][col];
+    }
+
+    public int countAvailable() {
+        int count = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c].isAvailable()) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public void displayLayout() {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                Seat seat = grid[r][c];
+
+                if (seat.isBooked()) {
+                    System.out.print("# ");
+                } else if (seat.isPremium()) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
