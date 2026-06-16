210b8d59e22c5d0dcf72394da740af3734cf6cff
IbCoder25 <ibrahimababa500@gmail.com>
2026-06-16 09:48:43 +0600

    class seat

---
+++ Lab-04/CineCart/src/main/java/model/Seat.java
@@ -0,0 +1,56 @@
+package model;
+
+public class Seat {
+    private int row;
+    private int col;
+    private boolean isPremium;
+    private boolean isBooked;
+
+    public Seat(int row, int col, boolean isPremium) {
+        this.row = row;
+        this.col = col;
+        this.isPremium = isPremium;
+        this.isBooked = false;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public boolean isAvailable() {
+        return !isBooked;
+    }
+
+    public void book() {
+        isBooked = true;
+    }
+
+    public void release() {
+        isBooked = false;
+    }
+
+    @Override
+    public String toString() {
+        String s = "R" + row + "C" + col;
+
+        if (isPremium)
+            s += "*";
+
+        if (isBooked)
+            s += "#";
+
+        return s;
+    }
+}
