cb2b27d20bf9ab207e7325cc7fc1a522a52ba496
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-06-16 12:08:54 +0600

    PremiumTicket class created

---
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -0,0 +1,16 @@
+package model;
+
+public class PremiumTicket extends AbstractTicket{
+
+    public PremiumTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+    @Override
+    public double subtotal(){
+        return base * 1.30 * peakMultiplier();
+    }
+    @Override
+    public String describe(){
+        return super.describe() + " Premium";
+    }
+}
