35c696b34693cc7f4029d7c0160d60ef7b6d7380
mubashirulhasan-netizen <mubashirulhasan@iut-dhaka.edu>
2026-06-16 09:42:17 +0600

    Initially modified Ticket class to implement the LineItem

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -1,17 +1,16 @@
 package model;
 
-public class Ticket {
+public abstract class Ticket implements LineItem {
+    protected Showtime showtime;
+    protected int row;
+    protected int col;
+    protected double basePrice;
 
-    private Showtime showtime;
-    private int row;
-    private int col;
-    private double pricePaid;
-
-    public Ticket(Showtime showtime, int row, int col, double pricePaid){
+    public Ticket(Showtime showtime, int row, int col) {
         this.showtime = showtime;
         this.row = row;
         this.col = col;
-        this.pricePaid = pricePaid;
+        this.basePrice = showtime.getMovie().getBasePrice();
     }
 
     public Showtime getShowtime(){
@@ -27,11 +26,26 @@ public class Ticket {
     }
 
     public double getPricePaid(){
-        return pricePaid;
+        return subtotal();
+    }
+
+    protected double peakMultiplier() {
+        return showtime.isPeak() ? 1.20 : 1.00;
+    }
+
+    @Override
+    public boolean isTicketLine() {
+        return true;
+    }
+
+    @Override
+    public String describe() {
+        return "T" + showtime.getId() + " - R" + row + "C" + col
+                + " @ BDT " + String.format("%.2f", subtotal());
     }
 
     @Override
     public String toString() {
-        return String.format("T" + showtime.getId() + " - " + "R" + row + "C" + col + " @ " + "BDT" + String.format("%,2f", pricePaid));
+        return describe();
     }
 }
