09d5787bb69d3999da2378d8c3490c12780fe95d
Tasauf Abdullah <tasaufalter@gmail.com>
2026-06-16 11:58:31 +0600

    add PremiumTicket skeleton

---
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -0,0 +1,17 @@
+package model;
+
+public class PremiumTicket extends AbstractTicket {
+    public PremiumTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+
+    public double subtotal() {
+        return 0.0;
+    }
+
+
+    public String describe() {
+        return "";
+    }
+}
