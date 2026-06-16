1dae464fbcda73bcd4f7b320cbe2acd982b8518d
wasifjafar <wasifjafar71@gmail.com>
2026-06-16 09:13:23 +0600

    create overriden subtotal method in PremiumTicket

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -1,7 +1,12 @@
 package model;
 
 public class PremiumTicket extends AbstractTicket {
-    public PremiumTicket(Showtime showtime, int row, int col, double pricePaid) {
-        super(showtime, row, col, pricePaid);
+    public PremiumTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+    @Override
+    public double subtotal() {
+        return (getShowtime().isPeak() ? 350.00 * 1.30 * 1.20 : 200.00 * 1.30);
     }
 }
