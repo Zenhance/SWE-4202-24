7ad9b62616c45aff4838bd63326823add5e74e61
tahsin1r <naylakagi@gmail.com>
2026-06-16 09:47:27 +0600

    changed the AbstractTicket access modifier to protected to access it withing classes

--- Lab-04/CineCart/src/main/java/model/AbstractTicket.java
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -2,19 +2,19 @@ package model;
 
 public abstract class AbstractTicket extends LineItem {
 
-    private final Showtime showtime;
-    private final int row;
-    private final int col;
-    private final double base;
+    protected final Showtime showtime;
+    protected final int row;
+    protected final int col;
+    protected final double base;
 
-    public AbstractTicket(Showtime showtime, int row, int col) {
+    protected AbstractTicket(Showtime showtime, int row, int col) {
         this.showtime = showtime;
         this.row = row;
         this.col = col;
         this.base = showtime.getMovie().getBasePrice();
     }
 
-    public double peakMultiplier() {
+    protected double peakMultiplier() {
         return showtime.isPeak() ? 1.20 : 1.00;
     }
 
