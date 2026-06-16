ac7715434ef2989ce864987b946508c3c09dff28
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-16 08:53:40 +0600

    abstract class

--- Lab-04/CineCart/src/main/java/model/LineItem.java
+++ Lab-04/CineCart/src/main/java/model/LineItem.java
@@ -3,4 +3,12 @@ package model;
 public abstract class LineItem {
     public abstract double subtotal();
     public abstract String describe();
+
+    public boolean isTicket(){
+        return false;
+    }
+    public boolean hasCode(String code){
+        return false;
+    }
+
 }
