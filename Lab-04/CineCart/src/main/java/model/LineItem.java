5c6f1a540653ef87323a063bd6eb63ce3b32cf8d
waseefmunir-max <waseef05@gmail.com>
2026-06-16 10:33:12 +0600

    LineItem.java: Undo changes

--- Lab-04/CineCart/src/main/java/model/LineItem.java
+++ Lab-04/CineCart/src/main/java/model/LineItem.java
@@ -1,6 +1,7 @@
 package model;
 
-public abstract class LineItem {
-    public abstract double subtotal();
-    public abstract String describe();
+public class LineItem {
+    public double subtotal() {
+        return 0.0;
+    }
 }
