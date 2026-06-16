a16cbed34577376467ccfc09c00987bff24350ff
tasnitaraida <tasnitar@gmail.com>
2026-06-16 11:39:11 +0600

    methods

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -4,4 +4,12 @@ public class StandardTicket extends Ticket {
     public StandardTicket(Showtime showtime, int row, int col){
         super(showtime,row,col);
     }
+
+    public double subtotal() {
+        return 0;
+    }
+
+    public String describe() {
+        return null;
+    }
 }
