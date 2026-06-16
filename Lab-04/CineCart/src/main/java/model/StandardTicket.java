5bd47d7d9a4b5222e243e2dd83ffce04f7373a31
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-16 08:55:50 +0600

    init commit

---
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -0,0 +1,9 @@
+package model;
+
+import data.ShowtimeBoard;
+
+public class StandardTicket extends Ticket{
+    public StandardTicket(Showtime showtime,int row,int col){
+        super(showtime,row,col);
+    }
+}
