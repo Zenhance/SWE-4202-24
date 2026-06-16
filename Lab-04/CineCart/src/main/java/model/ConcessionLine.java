bb87075fc20160b767d98101b1deee9d6a62f1ea
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-16 09:15:40 +0600

    new child class of lineitem

--- Lab-04/CineCart/src/main/java/model/ConcessionLine.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionLine.java
@@ -4,5 +4,16 @@ public class ConcessionLine extends LineItem{
     private final ConcessionItem item;
     private final int qty;
 
-    public ConcessionLine(ConcessionItem item,int qty)
+    public ConcessionLine(ConcessionItem item,int qty){
+        this.item = item;
+        this.qty = qty;
+    }
+    @Override
+    public double subtotal(){
+        return item.getUnitPrice()*qty;
+    }
+    @Override
+    public String describe(){
+        return item.getCode()+" x"+qty;
+    }
 }
