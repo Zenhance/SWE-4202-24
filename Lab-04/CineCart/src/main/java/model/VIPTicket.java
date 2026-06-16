cd62e018b2c721c499540f7fdf12fba91c865633
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-16 10:43:32 +0600

    Change: add overrides

--- Lab-04/CineCart/src/main/java/model/VIPTicket.java
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -4,4 +4,14 @@ public class VIPTicket extends PremiumTicket {
     public VIPTicket(Showtime showtime, int row, int col) {
         super(showtime, row, col);
     }
+
+    @Override
+    public double subtotal() {
+        return super.subtotal() + 200.00;
+    }
+
+    @Override
+    public String describe() {
+        return super.describe() + " [VIP Lounge Access]";
+    }
 }
