667630039e97ebd2bbfbf48398591c52bf7ed464
sameer-abdullah-240042107 <sameerabdullah1117@gmail.com>
2026-06-16 12:08:51 +0600

    third commit

---
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -0,0 +1,14 @@
+package model;
+
+public class StandardTicket extends AbstractTicket {
+    public StandardTicket(Showtime showtime,int row,int col){
+        super(showtime,col,row);
+    }
+    public double subtotal(){
+        return basePrice*peak();
+    }
+    public String describe(){
+        return "Standard ticket";
+    }
+
+}
