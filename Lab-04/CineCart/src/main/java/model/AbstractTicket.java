166d51b165fdf5abf902c899bc555d4943000513
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-16 10:26:30 +0600

    Change: add variables and constructors to AbstractTicket class

--- Lab-04/CineCart/src/main/java/model/AbstractTicket.java
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -1,4 +1,16 @@
 package model;
 
-public class AbstractTicket {
+public abstract class AbstractTicket extends LineItem {
+    private Showtime showtime;
+    private int row;
+    private int col;
+    protected double base;
+
+    public AbstractTicket(Showtime showtime, int row, int col) {
+        this.showtime = showtime;
+        this.row = row;
+        this.col = col;
+        this.base = showtime.getMovie().getBasePrice();
+    }
+
 }
