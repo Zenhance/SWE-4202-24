aa0a0d28b5bdc98ea815ae3a032ae0b87b2f896a
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-06-16 11:58:38 +0600

    changed access modifiers

--- Lab-04/CineCart/src/main/java/model/AbstractTicket.java
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -2,20 +2,20 @@ package model;
 
 public abstract  class AbstractTicket extends LineItem {
 
-    private final Showtime showtime;
-    private final int row;
-    private final int col;
-    private final double basePrice;
+    protected final Showtime showtime;
+    protected final int row;
+    protected final int col;
+    protected final double basePrice;
 
 
-    public AbstractTicket(Showtime showtime, int row, int col){
+    protected AbstractTicket(Showtime showtime, int row, int col){
         this.showtime = showtime;
         this.row = row;
         this.col = col;
         this.basePrice = showtime.getMovie().getBasePrice();
     }
 
-    public double peakMultiplier(){
+    protected double peakMultiplier(){
         return (showtime.isPeak()?1.20:1.00);
     }
 }
