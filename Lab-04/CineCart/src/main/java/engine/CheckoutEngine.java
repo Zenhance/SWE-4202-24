d15b14101d35a49de25f547dca872071da1468a0
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-16 10:22:38 +0600

    init commit

--- Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/engine/CheckoutEngine.java
@@ -2,11 +2,20 @@ package engine;
 import data.*;
 import model.*;
 public class CheckoutEngine {
-    public static double checkout(Cart cart){
-        double subtotal= cart.grandSubtotal();
-        double comboDiscount=0;
-        if(cart.hasCode("POP")&& cart.hasCode("SODA")){
-            comboDiscount=50.0;
+    public static double checkout(Cart cart) {
+        double subtotal = cart.grandSubtotal();
+        double comboDiscount = 0;
+        if (cart.hasCode("POP") && cart.hasCode("SODA")) {
+            comboDiscount = 50.0;
         }
+        double preDiscount = subtotal - comboDiscount;
+        double groupDiscount = 0;
+        if (cart.ticketCount() >= 4) {
+            groupDiscount = 0.10 * preDiscount;
+        }
+        double tierDiscount=cart.getOwner().getTierDiscount()*preDiscount;
+
+
+
     }
 }
