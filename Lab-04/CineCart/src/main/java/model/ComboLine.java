98f9c1d1e93358e6c7393a5e53522bfb73cc39eb
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-06-16 11:56:21 +0600

    comboline.java

--- Lab-04/CineCart/src/main/java/model/ComboLine.java
+++ Lab-04/CineCart/src/main/java/model/ComboLine.java
@@ -1,4 +1,14 @@
 package model;
 
-public class ComboLine {
+public abstract class ComboLine extends LineItem{
+    private ConcessionItem a;
+    private ConcessionItem b;
+
+
+
+    public ComboLine(ConcessionItem a,ConcessionItem b){
+        this.a=a;
+        this.b=b;
+    }
+
 }
