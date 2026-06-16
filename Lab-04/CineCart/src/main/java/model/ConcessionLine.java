098e5178c82042478ba59b2cc735bc53ef3cfc95
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-16 09:26:35 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/ConcessionLine.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionLine.java
@@ -1,10 +1,20 @@
 package model;
 
+import java.security.PublicKey;
+
 public class ConcessionLine extends LineItem{
     private ConcessionItem item;
     private int qty;
     public ConcessionLine(ConcessionItem item,int qty){
         this.item=item;
         this.qty=qty;
+    }
+    @Override
+    public double subtotal(){
+        return item.getUnitPrice()*qty;
+    }
+    @Override
+    public String describe(){
+        return item.getName()+ " x "+qty;
     }
 }
