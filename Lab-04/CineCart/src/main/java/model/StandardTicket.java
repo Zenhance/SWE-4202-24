008517735ddbeda42cfece86ec584f649cb6ab6e
wasif-22 <twasif68@gmail.com>
2026-06-16 12:02:36 +0600

    made standardticket class

---
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -0,0 +1,15 @@
+package model;
+
+public class StandardTicket extends AbstractTicket {
+
+    public StandardTicket(Showtime showtime, int row, int col) {
+
+        super(showtime, row, col);
+    }
+
+    @Override
+    public double subtotal() {
+        return base * peakMultiplier();
+    }
+
+}
