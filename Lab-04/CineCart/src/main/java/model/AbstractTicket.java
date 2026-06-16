420e8032e1e5d535d6a7e0cc50222fc6438d2ebb
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-06-16 11:46:29 +0600

    1

--- Lab-04/CineCart/src/main/java/model/AbstractTicket.java
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -1,6 +1,6 @@
 package model;
 
-public abstract class AbstractTicket extends LineItem {
+public abstract class AbstractTicket {
     protected Showtime showtime;
     protected int row;
     protected int col;
@@ -11,5 +11,12 @@ public abstract class AbstractTicket extends LineItem {
         this.row=row;
         this.col=col;
         this.basePrice=basePrice;
+    }
+    public double peak(){
+        if(showtime.isPeak()){
+            return basePrice*1.20;
+        }else{
+            return basePrice*1.00;
+        }
     }
 }
