e458c48fe82978b7a6b23be420a0fb35f51019d5
Tahmid <tahmidim99@gmail.com>
2026-06-16 12:59:22 +0600

    1

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -6,18 +6,18 @@ public abstract class Ticket extends LineItem {
     int col;
     double base;
 
-    public Ticket(Showtime showtime, int row, int col, double pricePaid){
-        this.showtime=showtime;
-        this.row=row;
-        this.col=col;
-        this.base=showtime.getMovie().getBasePrice();
+    public Ticket(Showtime showtime, int row, int col, double pricePaid) {
+        this.showtime = showtime;
+        this.row = row;
+        this.col = col;
+        this.base = showtime.getMovie().getBasePrice();
     }
 
     public Showtime getShowtime() {
         return showtime;
     }
 
-    public int getRow(){
+    public int getRow() {
         return row;
     }
 
@@ -31,12 +31,13 @@ public abstract class Ticket extends LineItem {
 
 
     public String toString() {
-        return String.format("T%d - R%dC%d @ BDT %.2f",showtime.getId(),row,col,pricePaid);
+        return String.format("T%d - R%dC%d @ BDT %.2f", showtime.getId(), row, col, pricePaid);
+    }
+
+    public double PeakTime() {
+        if (showtime.isPeak) {
+            return base * 1.20;
+        }
+        return base;
     }
 }
-    public double PeakTime(){
-    if(showtime.isPeak){
-        return base*1.20;
-    }
-    return base;
-    }
