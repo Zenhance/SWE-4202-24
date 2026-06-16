a7e90d7e3b2696cfc8f4dd8edd1ed83f1610a51a
ayiham-1012 <ayiham1012@gmail.com>
2026-06-16 11:27:32 +0600

    StandardTicket

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -1,5 +1,13 @@
 package model;
 
-public class StandardTicket {
-
+public class StandardTicket extends Ticket {
+    public StandardTicket(Showtime showtime, int row, int col, double pricePaid){
+        super(showtime, row, col, pricePaid);
+    }
+    public double getPricePaid() {
+        double price = getPricePaid();
+        boolean peak = getShowtime().isPeak();
+        if(peak) return price*1.20;
+        return price*1.00;
+    }
 }
