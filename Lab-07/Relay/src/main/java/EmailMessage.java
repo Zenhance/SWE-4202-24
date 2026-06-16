435bc4c2035118a96fc09d5ec9aed22529a44807
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-06-16 06:11:43 +0600

    Wrote the EmailMessage class with constructor.

---
+++ Lab-07/Relay/src/main/java/EmailMessage.java
@@ -0,0 +1,22 @@
+import java.beans.ConstructorProperties;
+
+public class EmailMessage extends Message {
+    public EmailMessage(Recipient recipient, String message){
+        super(recipient, message);
+    }
+    @Override
+    public String deliver(){
+        String t = "E-Mail to " + recipient.getName()+" "+recipient.getAddress()+" "+message;
+        return t;
+    }
+
+    @Override
+    public double cost(){
+        return 0.0d;
+    }
+
+    @Override
+    public String describe(){
+        return "E-Mail";
+    }
+}
