5ede33be260aeea3cfd50f5a3925ed022adfdbf1
Sadman Raiyan <kamrulseed@gmail.com>
2026-06-16 10:00:25 +0600

    PremiumTicket class updated

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -1,4 +1,15 @@
 package model;
 
-public class PremiumTicket {
+public class PremiumTicket extends AbstractTicket{
+    public PremiumTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+    @Override
+    public double subtotal() {
+        return basePrice*1.30*peak();
+    }
+    @Override
+    public String describe(){
+        return "Premium Ticket";
+    }
 }
