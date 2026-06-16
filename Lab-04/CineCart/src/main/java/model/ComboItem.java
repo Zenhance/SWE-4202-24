5895de99de09474c2f052b438541456f5adcd993
glockmachigan <muniruzzaman24@iut-dhaka.edu>
2026-06-16 09:20:57 +0600

    commit variable contructor and getter

--- Lab-04/CineCart/src/main/java/model/ComboItem.java
+++ Lab-04/CineCart/src/main/java/model/ComboItem.java
@@ -1,5 +1,13 @@
 package model;
 
 public class ComboItem extends ConcessionItem {
+    private String name;
 
+    public ComboItem() {
+        this.name = "Combo Item";
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {}
 }
