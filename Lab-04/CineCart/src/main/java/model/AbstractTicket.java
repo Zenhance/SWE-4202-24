f674103163674c84ec47f817be02f9e7d24efb5c
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-16 08:57:59 +0600

    abstract class

--- Lab-04/CineCart/src/main/java/model/AbstractTicket.java
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -5,4 +5,19 @@ public abstract class AbstractTicket extends LineItem{
     protected final int row;
     protected final int col;
     protected final double base;
+
+    public AbstractTicket(Showtime showtime, int row, int col){
+        this.showtime = showtime;
+        this.row = row;
+        this.col = col;
+        this.base = showtime.getMovie().getBasePrice();
+    }
+
+    protected double peakMultiplier(){
+        return showtime.isPeak()?1.20:1.00;
+    }
+    @Override
+    public boolean isTicket(){
+        re
+    }
 }
