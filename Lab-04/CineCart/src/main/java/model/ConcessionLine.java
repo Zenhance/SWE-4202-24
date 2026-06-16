225172bdd61dc15f95decdc5458a2b44caf0baa4
tasnitaraida <tasnitar@gmail.com>
2026-06-16 12:53:15 +0600

    fixed error

--- Lab-04/CineCart/src/main/java/model/ConcessionLine.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionLine.java
@@ -1,4 +1,13 @@
 package model;
 
-public class ConcessionLine {
+public class ConcessionLine extends LineItem {
+    private ConcessionItem item;
+    private int qty;
+
+    public ConcessionLine(ConcessionItem item, int qty) {
+        this.item=item;
+        this.qty=qty;
+    }
+
+
 }
