2aa602da1fdde96bf4c6fc37cee18477fa4b27fc
wasifjafar <wasifjafar71@gmail.com>
2026-06-16 08:41:16 +0600

    create PremiumTicket class

---
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -0,0 +1,7 @@
+package model;
+
+public class PremiumTicket extends AbstractTicket {
+    public PremiumTicket(Showtime showtime, int row, int col, double pricePaid) {
+        super(showtime, row, col, pricePaid);
+    }
+}
