1b12f8a7c05ef70db8b0fa2fd0ca9cccf6fb0eeb
wasifjafar <wasifjafar71@gmail.com>
2026-06-16 08:35:11 +0600

    create StandardTicket class

---
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -0,0 +1,9 @@
+package model;
+
+public class StandardTicket extends AbstractTicket{
+    public StandardTicket(Showtime showtime, int row, int col, double pricePaid) {
+        super(showtime, row, col, pricePaid);
+    }
+
+
+}
