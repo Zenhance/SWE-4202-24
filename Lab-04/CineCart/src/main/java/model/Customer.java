c026421291597654d04a7fc630c103d0d9aade4d
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-16 10:16:19 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -1,46 +1,13 @@
 package model;
 
 public class Customer {
-    private int id;
-    private String name;
-    private int age;
-    private String loyaltyTier;
-    //constructor
-    public Customer(int id,String name,int age){
-       this(id,name,age,"BASIC");
-    }
-    //2nd constructor
-    public Customer(int id,String name,int age,String loyaltyTier){
-        this.id=id;
-        this.name=name;
-        this.age=age;
-        this.loyaltyTier="tier";
-    }
-//getters
-    public int getId(){
-        return id;
-    }
-    public String getName(){
-        return name;
-    }
-    public int getAge(){
-        return age;
-    }
-    public String getLoyaltyTier(){
-        return "tier";
-    }
-    public double getTierDiscount() {
-        if(loyaltyTier.equals("GOLD")){
-            return 0.15;
-        }
-        if(loyaltyTier.equals("SILVER"))
-        {
-            return 0.08;
-        }
-        return 0.0;
-    }
-    public String toString(){
-        return "Customer ID:"+ id + ",Name:"+ name +",Age:" + age + ",Tier:"+ loyaltyTier;
-    }
+    private double tierDiscount;
+ public Customer(double tierDiscount){
+     this.tierDiscount=tierDiscount;
+ }
 
+    public double getTierDiscount() {
+        return tierDiscount;
+    }
 }
+
