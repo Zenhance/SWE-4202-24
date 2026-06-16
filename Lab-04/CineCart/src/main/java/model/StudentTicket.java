e09c3aad00ca13e5607f82b404577034ee5e439f
glockmachigan <muniruzzaman24@iut-dhaka.edu>
2026-06-16 09:25:22 +0600

    commit variable

--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -2,4 +2,12 @@ package model;
 
 public class StudentTicket extends AbstractTicket {
 
+    private int id;
+    private int studentId;
+
+    public StudentTicket() {
+        this.id = 0;
+        this.studentId = 0;
+    }
+
 }
