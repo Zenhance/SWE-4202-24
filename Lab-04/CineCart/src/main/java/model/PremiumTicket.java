58cb2c6e3fe189f90f25c01b5dc5b8592ed1a7bc
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-16 09:17:27 +0600

    completed premium ticket, it extends ticket but need to work on ticket further

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -1,4 +1,17 @@
 package model;
 
-public class PremiumTicket {
+public class PremiumTicket extends Ticket {
+    public PremiumTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+    @Override
+    public double subtotal() {
+        return basePrice * 1.30 * peakMultiplier();
+    }
+
+    @Override
+    public String describe() {
+        return "Premium " + super.describe();
+    }
 }
