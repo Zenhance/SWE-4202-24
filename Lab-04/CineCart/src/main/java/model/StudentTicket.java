05633f6b2aa72ff036f67cbcad17f24b9d2a680b
Tasauf Abdullah <tasaufalter@gmail.com>
2026-06-16 12:01:03 +0600

    add StudentTicket skeleton

---
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -0,0 +1,17 @@
+package model;
+
+public class StudentTicket extends AbstractTicket {
+    public StudentTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+
+    public double subtotal() {
+        return 0.0;
+    }
+
+
+    public String describe() {
+        return "";
+    }
+}
