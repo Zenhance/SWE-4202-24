ab046fbb6276d5d668059115e4a35836600e1d0e
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-06-16 12:19:47 +0600

    concessionline

--- Lab-04/CineCart/src/main/java/model/ConcessionLine.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionLine.java
@@ -1,6 +1,14 @@
 package model;
 
 public class ConcessionLine extends LineItem{
+    private ConcessionItem item;
+    private int qty;
+
+    public ConcessionLine(ConcessionItem item,int qty){
+
+        this.item=item;
+        this.qty=qty;
+    }
 
 
 
