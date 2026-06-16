0ca959313358500536f9fa2c41142ceb44690e9e
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-16 08:45:05 +0600

    Change: update code from test to PremiumTicket class

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -1,4 +1,13 @@
 package model;
 
 public class PremiumTicket {
+    public PremiumTicket(Showtime showtime, int i, int i1) {
+    }
+
+    public double subtotal() {
+    }
+
+    public String describe() {
+        return null;
+    }
 }
