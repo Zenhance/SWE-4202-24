b28f16d2e5af7661364264cc5e13f248f9d33afa
Sadman Raiyan <kamrulseed@gmail.com>
2026-06-16 09:54:46 +0600

    AbstractTicket class updated

--- Lab-04/CineCart/src/main/java/model/AbstractTicket.java
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -1,4 +1,21 @@
 package model;
 
-public class AbstractTicket {
+public abstract class AbstractTicket extends LineItem{
+    protected Showtime showtime;
+    protected int row;
+    protected int col;
+    protected double basePrice;
+
+    public AbstractTicket(Showtime showtime, int row, int col) {
+        this.showtime = showtime;
+        this.row = row;
+        this.col = col;
+        this.basePrice = basePrice;
+    }
+
+    public double peak(){
+        return showtime.isPeak()?basePrice*1.20:basePrice*1.00;
+    }
+
+
 }
