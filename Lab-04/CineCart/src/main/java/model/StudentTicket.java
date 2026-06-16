d8795cfc07e26fd31ebe06fc4b97b0a7ea69d2a6
Rakin <rakinhasan001@gmail.com>
2026-06-16 09:08:07 +0600

    StudentTicket constructed

---
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -0,0 +1,15 @@
+package model;
+
+public class StudentTicket extends Ticket{
+    public StudentTicket(Showtime showtime,int row, int col){
+        super(showtime,row,col);
+    }
+    @Override
+    public double subtotal(){
+        return base*0.50*peakMultiplier();
+    }
+    @Override
+    public String describe(){
+        return "Student "+super.describe();
+    }
+}
