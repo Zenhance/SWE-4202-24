68c01abc80ad0e0616fbdcf1e39275468a9c8b6a
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-06-16 09:33:09 +0600

    added getCharge class

--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -2,4 +2,15 @@ package model;
 
 public class StudentTicket extends Ticket{
 
+    public StudentTicket(Showtime showtime, int row, int col, double pricePaid, double peak){
+        super(showtime, row, col, pricePaid, peak);
+
+    }
+
+    @Override
+    public double getCharge() {
+        if (showtime.isPeak())
+            return pricePaid*0.5*peak;
+        else return pricePaid*0.5;
+    }
 }
