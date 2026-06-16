a86875129e4e9772987e123c2664fb1f4e59e305
Samin <mhsamin10@gmail.com>
2026-06-16 08:55:39 +0600

    Modified cart for the encapsulation test file

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -20,18 +20,24 @@ public class Cart {
         this.itemCount = 0;
     }
 
-    public void addTicket(Ticket t) {
+    public boolean addTicket(Ticket t) {
         if (ticketCount < MAX_TICKETS) {
             tickets[ticketCount] = t;
             ticketCount++;
+            return true;
+        }else {
+            return false;
         }
     }
 
-    public void addItem(ConcessionItem c, int qty) {
+    public boolean addItem(ConcessionItem c, int qty) {
         if (itemCount < MAX_ITEMS && qty > 0) {
             items[itemCount] = c;
             qtys[itemCount] = qty;
             itemCount++;
+            return true;
+        }else{
+            return false;
         }
     }
 
