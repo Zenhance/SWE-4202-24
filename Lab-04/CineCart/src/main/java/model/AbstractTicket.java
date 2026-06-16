6cf4890adb4fc3b71eb0c61a3a23ec1765a2996e
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-16 08:59:52 +0600

    abstract class

--- Lab-04/CineCart/src/main/java/model/AbstractTicket.java
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -22,6 +22,6 @@ public abstract class AbstractTicket extends LineItem{
     }
     @Override
     public String describe(){
-        return "T"+showtime.getId()+" - R"+row+col
+        return "T"+showtime.getId()+" - R"+row+"C"+col;
     }
 }
