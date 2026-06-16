88d92f75aaddcc53178ec98ad2b9468a4a459a46
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-16 10:11:22 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Showtime.java
+++ Lab-04/CineCart/src/main/java/model/Showtime.java
@@ -7,45 +7,13 @@ public class Showtime {
         this.basePrice=basePrice;
         this.peak=peak;
     }
-
-    //constructor
-    public Showtime(int id, Movie movie,Hall hall, int startHour, String dateTag) {
-        this.id = id;
-        this.movie = movie;
-        this.hall = hall;
-        this.startHour = startHour;
-        this.dateTag = dateTag;
+    public double getBasePrice(){
+        return basePrice;
+    }
+    public boolean isPeak(){
+        return peak;
     }
 
-    //getters
-    public int getId(){
-        return id;
-    }
-
-    public Movie getMovie() {
-        return movie;
-    }
-
-    public Hall getHall() {
-        return hall;
-    }
-
-    public int getStartHour() {
-        return startHour;
-    }
-
-    public String getDateTag() {
-        return dateTag;
-    }
-
-    public boolean isPeak() {
-        return startHour >= 18 && startHour <= 21;
-    }
-
-    @Override
-    public String toString() {
-        return " [T" + id + "]" + movie.getTitle() + " @ Hall" + hall.getId() + " - " + String.format("%02d:00", startHour) + "(" + dateTag + ")";
-    }
 }
 
 
