5817aaf06c3440a8c73d9e500b682d78d0e20832
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-16 09:31:19 +0600

    changed ticket instance variables from private to protected so childclasses can access it, added peakmultiplier method, price paid method.

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -1,16 +1,16 @@
 package model;
 
-public class Ticket {
-    private Showtime showtime;
-    private int row;
-    private int col;
-    private double pricePaid;
+public abstract class Ticket extends LineItem {
+    protected Showtime showtime;
+    protected int row;
+    protected int col;
+    protected double basePrice;
 
-    public Ticket(Showtime showtime, int row, int col, double pricePaid) {
+    public Ticket(Showtime showtime, int row, int col) {
         this.showtime = showtime;
         this.row = row;
         this.col = col;
-        this.pricePaid = pricePaid;
+        this.basePrice = showtime.getMovie().getBasePrice();
     }
 
     public Showtime getShowtime() {
@@ -26,12 +26,29 @@ public class Ticket {
     }
 
     public double getPricePaid() {
-        return pricePaid;
+        return basePrice;
     }
 
-    public String toString() {
+    protected double peakMultiplier() {
+        if (showtime.isPeak()) {
+            return 1.20;
+        }
+
+        return 1.00;
+    }
+
+    public boolean ticketLine() {
+        return true;
+    }
+
+    @Override
+    public String describe() {
         return "T" + showtime.getId() + " - R" + row + "C" + col
-                + " @ BDT " + String.format("%.2f", pricePaid);
+                + " @ BDT " + String.format("%.2f", getPricePaid());
     }
 
+    @Override
+    public String toString() {
+        return describe();
+    }
 }
