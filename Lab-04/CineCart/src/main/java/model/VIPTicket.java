00c3d3588d3afc26a055b207f79be629c9e4f3cf
glockmachigan <muniruzzaman24@iut-dhaka.edu>
2026-06-16 09:38:17 +0600

    commit VIP

--- Lab-04/CineCart/src/main/java/model/VIPTicket.java
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -4,4 +4,21 @@ public class VIPTicket extends Premium {
     private String premium;
     private int id;
 
+    public VIPTicket(String premium, int id) {
+        this.premium = premium;
+        this.id = id;
+    }
+    public String getPremium() {
+        return premium;
+    }
+    public void setPremium(String premium) {
+        this.premium = premium;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
