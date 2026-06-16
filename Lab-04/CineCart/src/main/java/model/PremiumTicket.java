c8b3ee1d4aebad5fa0465b2e96efd36beda360d7
04-Labiba <sabihalabiba639@gmail.com>
2026-06-16 09:14:13 +0600

    init commit

---
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -0,0 +1,13 @@
+package model;
+
+public class PremiumTicket extends Ticket {
+    public PremiumTicket(Showtime showtime,int row,int col){
+        super(showtime,row,col);
+    }
+    public double subtotal(){
+        return basePrice*1.30*peakMultiplier();
+    }
+    public String describe(){
+        return "Premium "+super.describe();
+    }
+}
