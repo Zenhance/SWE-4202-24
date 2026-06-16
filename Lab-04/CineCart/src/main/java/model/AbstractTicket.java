9ad8579596aea6a67dcc74eb336f4e0c777241cf
tahsin1r <naylakagi@gmail.com>
2026-06-16 09:35:32 +0600

    Added the required Classes according to the tests

--- Lab-04/CineCart/src/main/java/model/AbstractTicket.java
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -8,6 +8,7 @@ public abstract class AbstractTicket extends LineItem{
     private int col;
     private double pricePaid;
 
+
     public AbstractTicket(Showtime showtime, int row, int col, double pricePaid){
         this.showtime=showtime;
         this.row=row;
