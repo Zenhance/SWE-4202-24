d0dd56b02bb9f0956c8899bda5856a22fa8ca13b
Zunied-57 <zunied0411@gmail.com>
2026-06-16 09:12:03 +0600

    subTotal method overridden

---
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -0,0 +1,15 @@
+package model;
+
+public class StandardTicket extends Ticket
+{
+    public StandardTicket(Showtime showtime, int row, int col)
+    {
+        super(showtime, row, col);
+    }
+
+    @Override
+    public double subTotal()
+    {
+        return base*peakTime();
+    }
+}
