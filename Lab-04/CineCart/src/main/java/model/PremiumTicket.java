ac758553bdde4754f1f20d867da7a666d8364d74
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-16 08:59:50 +0600

    init commit

---
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -0,0 +1,9 @@
+package model;
+
+import data.ShowtimeBoard;
+
+public class PremiumTicket extends Ticket{
+    public PremiumTicket(Showtime showtime,int row,int col){
+        super(showtime,row,col);
+    }
+}
