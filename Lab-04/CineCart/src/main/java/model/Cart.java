b4fbfa05699baded333e15a95de353a6432b374b
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-16 09:05:47 +0600

    fixed cart to match encapsulation test case. changed add ticket to boolean to check if it exceeds max ticket or not, and did the same for add item,

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -20,25 +20,30 @@ public class Cart {
         this.itemCount = 0;
     }
 
-    public void addTicket(Ticket t) {
+    public boolean addTicket(Ticket t) {
         if (ticketCount < MAX_TICKETS) {
             tickets[ticketCount] = t;
             ticketCount++;
+            return true;
+        }else {
+            return false;
         }
     }
 
-    public void addItem(ConcessionItem c, int qty) {
+    public boolean addItem(ConcessionItem c, int qty) {
         if (itemCount < MAX_ITEMS && qty > 0) {
             items[itemCount] = c;
             qtys[itemCount] = qty;
             itemCount++;
+            return true;
+        }else{
+            return false;
         }
     }
 
     public Customer getOwner() {
         return owner;
     }
-
 
     public Ticket[] getTickets() {
         return tickets;
@@ -89,8 +94,4 @@ public class Cart {
 
         return false;
     }
-
-
-
 }
-
