8fbb4236a51d53888c7d969b6418041e7dfb9d0e
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-16 08:41:14 +0600

    changes for new test case

--- Lab-04/CineCart/src/main/java/model/Customer.java
+++ Lab-04/CineCart/src/main/java/model/Customer.java
@@ -1,10 +1,10 @@
 package model;
 
 public class Customer {
-    private int id;
-    private String name;
-    private int age;
-    private String loyaltyTier;
+    private final int id;
+    private final String name;
+    private final int age;
+    private final String loyaltyTier;
     private Ticket[] ticket;
 
     public Customer(int id,String name,int age){
