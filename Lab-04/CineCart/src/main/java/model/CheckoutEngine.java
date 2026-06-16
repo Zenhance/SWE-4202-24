3de5d44405b7c76a0764b47cebb1b529f062a096
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-06-16 09:53:11 +0600

    initial commit

--- Lab-04/CineCart/src/main/java/model/CheckoutEngine.java
+++ Lab-04/CineCart/src/main/java/model/CheckoutEngine.java
@@ -10,7 +10,17 @@ public class CheckoutEngine {
     }
 
     public String bookTicket(Cart cart, int showtimeId, int row, int col) {
+        Showtime showtime = board.findById(showtimeId);
+        if(showtime == null)
+            return "Showtime not found";
 
+        if(cart.getOwner().getAge() < showtime.getMovie().getMinAge()) {
+            return "Underage for rating <%s>" + showtime.getMovie().getRating();
+        }
+
+        Seat seat = showtime.getMovie().getMinAge();
+        if(seat.isBooked())
+            return "Seat unavailable";
     }
 
     public String addConcession(Cart cart, String code, int qty) {
