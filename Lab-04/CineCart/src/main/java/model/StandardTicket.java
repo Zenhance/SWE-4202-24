f5870d99ad5ae165e84e42b5e8d03717d457b14e
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-06-16 12:10:06 +0600

    standard and student ticket skeleton

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -1,4 +1,19 @@
 package model;
 
-public class StandardTicket {
+public class StandardTicket extends AbstractTicket{
+
+
+    public StandardTicket(Showtime showtime, int row, int col, double pricePaid) {
+        super(showtime, row, col, pricePaid);
+    }
+
+    @Override
+    public double Subtotal() {
+        return 0;
+    }
+
+    @Override
+    public String describe() {
+        return "";
+    }
 }
--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -1,4 +1,17 @@
 package model;
 
-public class StudentTicket {
+public class StudentTicket extends AbstractTicket{
+    public StudentTicket(Showtime showtime, int row, int col, double pricePaid) {
+        super(showtime, row, col, pricePaid);
+    }
+
+    @Override
+    public double Subtotal() {
+        return 0;
+    }
+
+    @Override
+    public String describe() {
+        return "";
+    }
 }
