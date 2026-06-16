12189dc824383b0a22ceb25a465b7ad9e1c72973
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-16 09:02:53 +0600

    new child class of abstract ticket class

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -1,5 +1,12 @@
 package model;
 
 public class StandardTicket extends AbstractTicket{
-    public StandardTicket(Showtime showtime)
+    public StandardTicket(Showtime showtime, int row, int col){
+        super(showtime,row,col);
+    }
+    @Override
+    public double subTotal(){
+        return base*peakMultiplier();
+    }
+
 }
