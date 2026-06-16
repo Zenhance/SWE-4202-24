7ea8bfd8014f5d6eec45b3fe7c12a81736b0b052
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-06-16 12:14:48 +0600

    StudentTicket class created

---
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -0,0 +1,18 @@
+package model;
+
+public class StudentTicket extends AbstractTicket{
+
+    public StudentTicket(Showtime showtime, int row, int col){
+        super(showtime, row, col);
+    }
+
+    @Override
+    public double subtotal(){
+        return base * 0.50 * peakMultiplier();
+    }
+
+    @Override
+    public String describe(){
+        return super.describe() + " Student";
+    }
+}
