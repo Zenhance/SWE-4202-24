3a24528e0f4a19ccaee14ee3d5442336b3074649
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-16 09:19:41 +0600

    new child class comboline of lineitem

--- Lab-04/CineCart/src/main/java/model/ComboLine.java
+++ Lab-04/CineCart/src/main/java/model/ComboLine.java
@@ -3,4 +3,13 @@ package model;
 public class ComboLine extends LineItem{
     private final ConcessionItem itemA;
     private final ConcessionItem itemB;
+
+    public ComboLine(ConcessionItem itemA,ConcessionItem itemB){
+        this.itemA=itemA;
+        this.itemB=itemB;
+    }
+    @Override
+    public double subtotal(){
+        return itemA.getUnitPrice()+itemB.getUnitPrice();
+    }
 }
