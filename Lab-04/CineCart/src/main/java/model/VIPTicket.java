ed9fe34159f538c3156641139c94ec5c3aa8e708
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-06-16 09:53:14 +0600

    updated VIPTicket.java

--- Lab-04/CineCart/src/main/java/model/VIPTicket.java
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -1,16 +1,13 @@
 package model;
 
-public class VIPTicket extends Ticket {
+public class VIPTicket extends PremiumTicket {
 
-    public VIPTicket(Showtime showtime, int row, int col, double pricePaid, double peak) {
-        super(showtime, row, col, pricePaid, peak);
+    public VIPTicket(Showtime showtime, int row, int col, double basePrice, double peak) {
+        super(showtime, row, col, basePrice, peak);
     }
 
     @Override
     public double getCharge() {
-        if (showtime.isPeak())
-            return pricePaid*peak*1.3 + 200.00;
-
-        else return pricePaid*1.3 + 200.00;
+        return super.getCharge() + 200.00;
     }
 }
