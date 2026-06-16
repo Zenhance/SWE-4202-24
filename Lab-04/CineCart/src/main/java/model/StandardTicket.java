6d6d8c950c8b3d0088fa8fbda4766820a44cddee
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-16 09:39:19 +0600

    completed standard ticket, by extending ticket.

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -1,4 +1,17 @@
 package model;
 
-public class StandardTicket {
+public class StandardTicket extends Ticket {
+    public StandardTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+    @Override
+    public double subtotal() {
+        return basePrice * peakMultiplier();
+    }
+
+    @Override
+    public String describe() {
+        return "Standard " + super.describe();
+    }
 }
