0f60a19def303a75b7b306d97b49d2c11b6fe2e7
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-06-16 09:13:57 +0600

    updating StandardTicket.javaTicket.java

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -2,4 +2,7 @@ package model;
 
 public class StandardTicket extends Ticket{
 
+    public StandardTicket(Showtime showtime, int row, int col, double pricePaid){
+        super(showtime, row, col, pricePaid);
+    }
 }
