cc695a592b6e03885fe05ebf39e7372718465035
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-06-16 12:22:23 +0600

    StudentTicket and VIPTicket class created

--- Lab-04/CineCart/src/main/java/model/VIPTicket.java
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -2,17 +2,15 @@ package model;
 
 public class VIPTicket extends PremiumTicket{
 
-    public VIPTicket(Showtime showtime, int row, int col) {
+    public VIPTicket(Showtime showtime, int row, int col){
         super(showtime, row, col);
     }
-
     @Override
-    public double subtotal() {
+    public double subtotal(){
         return super.subtotal() + 200.0;
     }
-
     @Override
-    public String describe() {
+    public String describe(){
         return super.describe() + " Lounge";
     }
 }
