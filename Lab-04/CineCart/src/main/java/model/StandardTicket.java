7353f3215fcc57ec60cf8b81e16d730d4db3bf1b
wasifjafar <wasifjafar71@gmail.com>
2026-06-16 09:10:57 +0600

    create overriden subtotal method in StandardTicket

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -1,9 +1,12 @@
 package model;
 
 public class StandardTicket extends AbstractTicket{
-    public StandardTicket(Showtime showtime, int row, int col, double pricePaid) {
-        super(showtime, row, col, pricePaid);
+    public StandardTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
     }
 
-
+    @Override
+    public double subtotal() {
+        return (getShowtime().isPeak() ? 350.00 * 1.20 : 200.0);
+    }
 }
