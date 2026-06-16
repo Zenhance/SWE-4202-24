faca1a5a8790d86d6bb4fef725cd4a5ea7ade86d
Sadman Raiyan <kamrulseed@gmail.com>
2026-06-16 09:56:57 +0600

    StandardTicket class updated

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -1,4 +1,17 @@
 package model;
 
-public class StandardTicket {
+public class StandardTicket extends AbstractTicket{
+    public StandardTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+    @Override
+    public double subtotal() {
+        return basePrice*peak();
+    }
+
+    @Override
+    public String describe(){
+        return "Standard Ticket";
+    }
 }
