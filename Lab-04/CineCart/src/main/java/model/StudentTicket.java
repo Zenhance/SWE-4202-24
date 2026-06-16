c309246404ef18d6e7d28d5bac2b7af2799a8bf9
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-16 09:06:08 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -6,4 +6,8 @@ public class StudentTicket extends Ticket{
     public StudentTicket(Showtime showtime,int row,int col){
         super(showtime,row,col);
     }
+    @Override
+    public double subtotal(){
+        return super.subtotal() + 200.0;
+    }
 }
