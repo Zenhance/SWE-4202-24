c7994311a4eabc2ecf988c50e4121caeb67b4ecf
Tahmid <tahmidim99@gmail.com>
2026-06-16 12:44:14 +0600

    1

--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -1,5 +1,11 @@
 package model;
 
 public class StudentTicket extends Ticket{
-
+    public StudentTicket(Showtime showtime, int row, int col){
+        super(showtime,row,col);
+    }
+    @Override
+    public double subtotal(){
+        return base*PeakTime()*0.50;
+    }
 }
