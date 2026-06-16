4ed18f106e336a6c88801d97695e2b1df78d3796
04-Labiba <sabihalabiba639@gmail.com>
2026-06-16 08:48:40 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -1,15 +1,18 @@
 package model;
 
-public class Ticket {
-    public Showtime showtime;
-    public int row;
-    public int col;
-    public double pricePaid;
+public abstract class Ticket extends LineItem{
+    protected Showtime showtime;
+    protected int row;
+    protected int col;
+    protected double pricePaid;
     public Ticket(Showtime showtime,int row,int col,double pricePaid){
         this.showtime=showtime;
         this.row=row;
         this.col=col;
         this.pricePaid=pricePaid;
+    }
+    protected double peakMultiplier(){
+        return showtime.isPeak() ? 1.20:1.00;
     }
     public Showtime getShowtime(){
         return showtime;
