a2e8dfff288f75cd9fb6eecf81549a85300d8b49
nusra <nusratnishi1103@gmail.com>
2026-06-16 11:47:20 +0600

    attempt

---
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -0,0 +1,15 @@
+package model;
+
+public class PremiumTicket extends Ticket{
+    public PremiumTicket(Showtime showtime,int row,int col,double basePrice){
+        super(showtime,row,col,basePrice);
+    }
+
+    public double subtotal(){
+        return basePrice + 1.30 + peakMultiplier();
+    }
+
+    public String describe(){
+        return "Premium Ticket R" + row + "C" + col;
+    }
+}
