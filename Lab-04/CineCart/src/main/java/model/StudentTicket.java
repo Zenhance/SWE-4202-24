c26d4c64759f5278231c12fb8abbd20e3f0e9713
wasifjafar <wasifjafar71@gmail.com>
2026-06-16 08:36:26 +0600

    create StudentTicket class

---
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -0,0 +1,7 @@
+package model;
+
+public class StudentTicket extends AbstractTicket{
+    public StudentTicket(Showtime showtime, int row, int col, double pricePaid) {
+        super(showtime, row, col, pricePaid);
+    }
+}
