14d15fb81b7fca43b8453106cbc233ab5edcc86f
nafisaahmedfaz <nafisaahmed1610@gmail.com>
2026-06-16 11:39:01 +0600

    skeleton complete

---
+++ Lab-07/Relay/src/main/java/Notice.java
@@ -0,0 +1,8 @@
+public class Notice {
+    private Recipient recipient;
+    private String test;
+    public Notice(Recipient recipient,String test){
+        this.recipient=recipient;
+        this.text=text;
+    }
+}
