e5af38bdff4b08fc1406b24b75018b68b0139c27
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-16 08:44:36 +0600

    changes for new test case

--- Lab-04/CineCart/src/main/java/model/Cart.java
+++ Lab-04/CineCart/src/main/java/model/Cart.java
@@ -4,12 +4,14 @@ public class Cart {
     public static  final int MAX_TICKETS = 20;
     public static  final int MAX_ITEMS = 20;
 
-    private Customer owner;
-    private Ticket[] tickets;
+    private final Customer owner;
+    private final Ticket[] tickets;
     private int ticketCount;
-    private ConcessionItem[] items;
-    private int[] qtys;
+    private final ConcessionItem[] items;
+    private final int[] qtys;
     private int itemCount;
+    private final LineItem[] lines;
+    private int lineCount;
 
     public Cart(Customer owner){
         this.owner = owner;
