d855cf74af2384bb67f1e4d8f19b29a582de705b
wasifjafar <wasifjafar71@gmail.com>
2026-06-16 09:34:23 +0600

    create overridden describe method in StudentTicket

--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -9,4 +9,12 @@ public class StudentTicket extends AbstractTicket{
     public double subtotal() {
         return (getShowtime().isPeak() ? 350.00 * 0.50 * 1.20 : 200.00 * 0.50);
     }
+
+    @Override
+    public String describe() {
+        String string = String.format("Student" +
+                "subtotal : %f BDT", subtotal());
+
+        return string;
+    }
 }
