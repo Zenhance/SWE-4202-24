247cc449858aaeafa01ca10f6b5926482d911d1d
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-06-16 09:58:08 +0600

    Completed standard ticket

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -4,4 +4,12 @@ public class StandardTicket extends AbstractTicket {
     public StandardTicket(Showtime base, int row, int col){
         super(base,row,col);
     }
+    @Override
+    public double subtotal(){
+        return applyPeak(getBasePrice());
+    }
+    @Override
+    public String describe(){
+        return "Standard Ticket";
+    }
 }
