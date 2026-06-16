f5e8bfcbfa7ae0a199c9b128673f1fb74debd5bf
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-06-16 09:49:21 +0600

    methods updated for all ticket child classes

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -5,9 +5,15 @@ public class StandardTicket extends AbstractTicket{
         super(showtime, row, col);
     }
 
+    @Override
     public double subtotal(){
         if (getShowtime().isPeak())
             return 350.00 * 0.50;
         else return 200.00 ;
+    }
+
+    @Override
+    public String describe() {
+        return "";
     }
 }
--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -5,9 +5,15 @@ public class StudentTicket extends AbstractTicket{
         super(showtime, row, col);
     }
 
+    @Override
     public double subtotal(){
         if (getShowtime().isPeak())
             return 350.00 * 0.50 * 1.20;
         else return 200.00 * 0.50;
+    }
+
+    @Override
+    public String describe() {
+        return "";
     }
 }
