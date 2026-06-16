105eb12e982a090231d9abf357f7a80b9582698d
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-16 09:12:21 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/VIPTicket.java
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -6,4 +6,8 @@ public class VIPTicket extends PremiumTicket{
     public VIPTicket(Showtime showtime,int row,int col){
         super(showtime,row,col);
     }
+    @Override
+    public double subtotal(){
+        return super.subtotal()+200.0;
+    }
 }
