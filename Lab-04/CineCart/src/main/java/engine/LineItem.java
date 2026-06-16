1107699e3d2b8887b82687c7bb6c440ae1be76fd
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-16 08:32:56 +0600

    init commit

--- Lab-04/CineCart/src/main/java/engine/LineItem.java
+++ Lab-04/CineCart/src/main/java/engine/LineItem.java
@@ -1,6 +1,9 @@
 public abstract class LineItem {
     public abstract double subtotal(){
         public abstract String describe();
+
+        public boolean isTicket(){
+            return false;
         }
     }
 }
