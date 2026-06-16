98c63677b9655a945934941d1abae9611650dfef
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-06-16 09:19:46 +0600

    init commit

--- Lab-04/CineCart/src/main/java/model/ConcessionLine.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionLine.java
@@ -3,4 +3,8 @@ package model;
 public class ConcessionLine extends LineItem{
     private ConcessionItem item;
     private int qty;
+    public ConcessionLine(ConcessionItem item,int qty){
+        this.item=item;
+        this.qty=qty;
+    }
 }
