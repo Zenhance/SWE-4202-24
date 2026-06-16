17324f602fd3f63754b939fb0f1e4c63e7f35721
nusra <nusratnishi1103@gmail.com>
2026-06-16 12:15:39 +0600

    attempt

---
+++ Lab-04/CineCart/src/main/java/model/ComboLine.java
@@ -0,0 +1,23 @@
+package model;
+
+public class ComboLine extends LineItem{
+    private final ConcessionItem itemA;
+    private final ConcessionItem itemB;
+
+    public ComboLine(ConcessionItem itemA, ConcessionItem itemB){
+        this.itemA=itemA;
+        this.itemB=itemB;
+    }
+
+    public double subtotal(){
+        return itemA.getUnitPrice() + itemB.getUnitPrice()-50.0;
+    }
+
+    public String describe(){
+        return itemA.getName() + " + " + itemB.getName() + " Combo";
+    }
+
+    public boolean hasCode(String code){
+        return itemA.getCode().equals(code) ||  itemB.getCode().equals(code);
+    }
+}
