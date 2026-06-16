1c906e36a190f404b1398b18cb61d347b7032a1a
Sadman Raiyan <kamrulseed@gmail.com>
2026-06-16 10:24:53 +0600

    StudentTicket class method added

--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -1,4 +1,17 @@
 package model;
 
-public class StudentTicket {
+public class StudentTicket extends AbstractTicket{
+    public StudentTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+    @Override
+    public double subtotal() {
+        return basePrice*0.50*peak();
+    }
+
+    @Override
+    public String describe(){
+        return "Student Ticket";
+    }
 }
