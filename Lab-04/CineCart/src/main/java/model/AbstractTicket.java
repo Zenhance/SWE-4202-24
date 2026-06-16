665437f96ac5c58a27ca066881ff38cc376ced8b
wasif-22 <twasif68@gmail.com>
2026-06-16 11:55:41 +0600

    added showtime, peakmultiplier and desccribe string

--- Lab-04/CineCart/src/main/java/model/AbstractTicket.java
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -14,4 +14,23 @@ public abstract class AbstractTicket extends LineItem {
         this.col = col;
         this.base = showtime.getMovie().getBasePrice();
     }
+    public Showtime getShowtime() { return showtime; }
+    public int getRow()           { return row; }
+    public int getCol()           { return col; }
+
+
+    protected double peakMultiplier() {
+        if (showtime.isPeak()) {
+            return 1.20;
+        } else {
+            return 1.00;
+        }
+    }
+
+
+    @Override
+    public String describe() {
+        return "T" + showtime.getId() + " - R" + row + "C" + col;
+    }
+
 }
