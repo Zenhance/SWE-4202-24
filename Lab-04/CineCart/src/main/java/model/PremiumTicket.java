0826ad745935d60b181d62284f2fdaa2ee38b781
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-16 09:17:36 +0600

    Change: update code from test and return 0

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -1,10 +1,11 @@
 package model;
 
-public class PremiumTicket {
+public class PremiumTicket extends LineItem {
     public PremiumTicket(Showtime showtime, int i, int i1) {
     }
 
     public double subtotal() {
+        return 0;
     }
 
     public String describe() {
