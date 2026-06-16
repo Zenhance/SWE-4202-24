4d4d2c16df029cd758dbb82bd4929f05f813fc35
ayiham-1012 <ayiham1012@gmail.com>
2026-06-16 11:34:05 +0600

    PremiumTicket (copied StandardTicket and made some adjustment)

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -1,4 +1,13 @@
 package model;
 
-public class PremiumTicket {
+public class PremiumTicket extends Ticket {
+    public PremiumTicket(Showtime showtime, int row, int col, double pricePaid){
+        super(showtime, row, col, pricePaid);
+    }
+    public double getPricePaid() {
+        double price = getPricePaid() * 1.30;
+        boolean peak = getShowtime().isPeak();
+        if(peak) return price*1.20;
+        return price*1.00;
+    }
 }
