0b98eb1bf2fa677b350c953c7e0319cca7fd4c6e
tahsin1r <naylakagi@gmail.com>
2026-06-16 10:34:25 +0600

    Finished

--- Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/data/ConcessionMenu.java
@@ -28,6 +28,7 @@ public class ConcessionMenu {
 
 
     }
+
     public ConcessionItem findByCode(String code){
         for (int i = 0; i < count; i++) {
             if (items[i].getCode().equals(code)) return items[i];
