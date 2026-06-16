11b2744d49c6c8f96928e156a516260c3b4443d0
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-06-16 09:28:51 +0600

    subtotal method add for PremiumTicket

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -7,7 +7,7 @@ public class PremiumTicket extends AbstractTicket {
 
     public double subtotal(){
         if (getShowtime().isPeak())
-            return 350.00 * 1.20;
-        else return 200.00;
+            return 350.00 * 0.50 * 1.20;
+        else return 200.00 * 0.50;
     }
 }
