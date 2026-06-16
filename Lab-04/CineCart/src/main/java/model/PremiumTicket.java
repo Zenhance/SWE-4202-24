affae62c54fb3801665e18657bcb9000f2d06353
wasifjafar <wasifjafar71@gmail.com>
2026-06-16 09:23:15 +0600

    create overridden describe method in PremiumTicket

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -9,4 +9,12 @@ public class PremiumTicket extends AbstractTicket {
     public double subtotal() {
         return (getShowtime().isPeak() ? 350.00 * 1.30 * 1.20 : 200.00 * 1.30);
     }
+
+    @Override
+    public String describe() {
+        String string = String.format("Premium\n" +
+                "subtotal : %f BDT", subtotal());
+
+        return string;
+    }
 }
