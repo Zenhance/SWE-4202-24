30f377ac4cacc60c621e5645b1ba6364288cde60
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-16 10:15:45 +0600

    completed concession line,

--- Lab-04/CineCart/src/main/java/model/ConcessionLine.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionLine.java
@@ -1,4 +1,41 @@
 package model;
 
-public class ConcessionLine {
+public class ConcessionLine extends LineItem {
+    private ConcessionItem item;
+    private int qty;
+
+    public ConcessionLine(ConcessionItem item, int qty) {
+        this.item = item;
+        this.qty = qty;
+    }
+
+    public ConcessionItem getItem() {
+        return item;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    @Override
+    public double subtotal() {
+        double total = 0.0;
+
+        for (int i = 1; i < qty; i++) {
+            total += item.getUnitPrice();
+        }
+
+        return total;
+    }
+
+    @Override
+    public String describe() {
+        return item.getCode() + " x" + qty + " - BDT "
+                + String.format("%.2f", subtotal());
+    }
+
+    @Override
+    public boolean carriesCode(String code) {
+        return item.getCode().equalsIgnoreCase(code);
+    }
 }
