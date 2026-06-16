2f93426bf493db24706a1cc6ed0c8e06c77e9bd5
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-16 09:11:58 +0600

    new vipticket child class of abstract ticket class

--- Lab-04/CineCart/src/main/java/model/VIPTicket.java
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -4,4 +4,12 @@ public class VIPTicket extends AbstractTicket{
     public VIPTicket(Showtime showtime, int row, int col){
         super(showtime,row,col);
     }
+    @Override
+    public double subtotal(){
+        return super.subtotal() + 200.00;
+    }
+    @Override
+    public String describe(){
+        return super.describe()+" + Lounge";
+    }
 }
