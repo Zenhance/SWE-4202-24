68d34d6ff68e7f164473222e9b860e0faa4218b2
Tahmid <tahmidim99@gmail.com>
2026-06-16 12:41:31 +0600

    1

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -1,4 +1,11 @@
 package model;
 
-public class PremiumTicket {
+public class PremiumTicket extends Ticket {
+    public PremiumTicket(Showtime showtime,int row,int col){
+        super(showtime,row,col);
+    }
+    @Override
+    public double subtotal(){
+        return base*PeakTime()*1.30;
+    }
 }
