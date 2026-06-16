8adb06563687365835558982ccf140a1985d19c1
wasifjafar <wasifjafar71@gmail.com>
2026-06-16 08:41:50 +0600

    create VipTicket class extending PremiumTicket

---
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -0,0 +1,7 @@
+package model;
+
+public class VIPTicket extends PremiumTicket{
+    public VIPTicket(Showtime showtime, int row, int col, double pricePaid) {
+        super(showtime, row, col, pricePaid);
+    }
+}
