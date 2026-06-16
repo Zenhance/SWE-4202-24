43c1a5e3086dcd9213a25ec6bda454c001c65098
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-16 09:17:15 +0600

    Change: update code from test and return 0

--- Lab-04/CineCart/src/main/java/model/VIPTicket.java
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -1,10 +1,11 @@
 package model;
 
-public class VIPTicket {
+public class VIPTicket extends LineItem {
     public VIPTicket(Showtime st, int i, int i1) {
     }
 
     public double subtotal() {
+        return 0;
     }
 
     public String describe() {
