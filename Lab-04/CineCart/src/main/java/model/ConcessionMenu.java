ccbfcff9341b5e1ecc933398e230c150bb4690aa
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-06-16 09:21:14 +0600

    initial commit

--- Lab-04/CineCart/src/main/java/model/ConcessionMenu.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionMenu.java
@@ -1,4 +1,19 @@
 package model;
 
 public class ConcessionMenu {
+    ConcessionItem[] items;
+    int count;
+    private static final int MAX = 30;
+
+    public void add(ConcessionItem c) {
+
+    }
+
+    public ConcessionItem findByCode(String code) {
+        return null;
+    }
+
+    public void displayAll() {
+
+    }
 }
--- Lab-04/CineCart/src/main/java/model/ShowtimeBoard.java
+++ Lab-04/CineCart/src/main/java/model/ShowtimeBoard.java
@@ -1,4 +1,23 @@
 package model;
 
 public class ShowtimeBoard {
+    Showtime[] shows;
+    int count;
+    private static final int MAX = 30;
+
+    public void add(Showtime s) {
+
+    }
+
+    public Showtime findById(int id) {
+        return null;
+    }
+
+    public void displayAll() {
+
+    }
+
+    public String tiString() {
+
+    }
 }
