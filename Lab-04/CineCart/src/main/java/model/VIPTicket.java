7ee2b90471d090e2d96a04e192748140d4a6cab2
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-16 09:57:17 +0600

    completed vip ticket

--- Lab-04/CineCart/src/main/java/model/VIPTicket.java
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -1,4 +1,17 @@
 package model;
 
-public class VIPTicket {
+public class VIPTicket extends PremiumTicket {
+    public VIPTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+    @Override
+    public double subtotal() {
+        return super.subtotal() + 200.00;
+    }
+
+    @Override
+    public String describe() {
+        return super.describe() + " + VIP Lounge";
+    }
 }
