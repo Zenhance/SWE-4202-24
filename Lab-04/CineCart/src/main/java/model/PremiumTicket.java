a496fdbedf08fa9477235c429a0e0dbb7918f99a
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-16 09:07:23 +0600

    new premiumticket child class of abstract ticket class

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -4,5 +4,13 @@ public class PremiumTicket extends AbstractTicket{
     public PremiumTicket(Showtime showtime, int row, int col){
         super(showtime,row,col);
     }
+    @Override
+    public double subtotal(){
+        return base*1.30*peakMultiplier();
+    }
+    @Override
+    public String describe(){
+        return super.describe()+" Premium";
+    }
 
 }
