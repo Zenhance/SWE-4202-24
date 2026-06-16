e061e0aeed1882f75781e0bd997cbab668d93db4
Tasauf Abdullah <tasaufalter@gmail.com>
2026-06-16 11:55:13 +0600

    add StandardTicket skeleton

---
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -0,0 +1,16 @@
+package model;
+
+public class StandardTicket extends AbstractTicket {
+    public StandardTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+    public double subtotal() {
+        return 0.0;
+    }
+
+
+    public String describe() {
+        return "";
+    }
+}
