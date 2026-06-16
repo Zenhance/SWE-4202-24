263bb133b16efbd722daba363f2d681bc2042639
Maomao-starry <aminarahmanshalu@gmail.com>
2026-06-16 12:48:19 +0600

    task

--- Lab-04/CineCart/src/main/java/model/ConcessionLine.java
+++ Lab-04/CineCart/src/main/java/model/ConcessionLine.java
@@ -2,13 +2,19 @@ package model;
 
 public class ConcessionLine extends LineItem{
     private String name;
-    private double unitPrice;
+    private final double unitPrice;
     private int qty;
     public ConcessionLine(String name,double unitPrice,int qty){
         this.name=name;
         this.unitPrice=unitPrice;
         this.qty=qty;
     }
+    @Override
+    public double subtotal(){
+        return unitPrice*qty;
+    }
+    @Override
+    public String describe
 
 
 }
