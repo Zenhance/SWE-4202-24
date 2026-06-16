f334195c2a10de263f2687154af51ef3171538da
Zunied-57 <zunied0411@gmail.com>
2026-06-16 09:08:49 +0600

    peakTime method updated

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -41,6 +41,10 @@ public abstract class Ticket extends LineItem
         {
             base=base*1.20;
         }
+        else
+        {
+            base=base*1.00;
+        }
         return base;
     }
 
