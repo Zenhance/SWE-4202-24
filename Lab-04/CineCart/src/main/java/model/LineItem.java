3e61203b74c5f2cb14aa6e243b756d7e992bae65
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-16 10:22:52 +0600

    Change: update LineItem class

--- Lab-04/CineCart/src/main/java/model/LineItem.java
+++ Lab-04/CineCart/src/main/java/model/LineItem.java
@@ -4,3 +4,9 @@ public abstract class LineItem {
     public abstract double subtotal();
     public abstract String describe();
 }
+
+//public class LineItem {
+//    public double subtotal() {
+//        return 0;
+//    }
+//}
