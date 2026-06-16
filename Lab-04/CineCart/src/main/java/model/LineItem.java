addfd4afe1f860749910dc74d2f587d35d38a2e1
glockmachigan <muniruzzaman24@iut-dhaka.edu>
2026-06-16 09:39:53 +0600

    commit LineItem variable and contructor

--- Lab-04/CineCart/src/main/java/model/LineItem.java
+++ Lab-04/CineCart/src/main/java/model/LineItem.java
@@ -1,4 +1,15 @@
 package model;
 
 public class LineItem {
+    private int id;
+    private int price;
+    private int quantity;
+    private StandardTicket ticket;
+
+    public LineItem(int id, int price, int quantity, StandardTicket ticket) {
+        this.id = id;
+        this.price = price;
+        this.quantity = quantity;
+        this.ticket = ticket;
+    }
 }
