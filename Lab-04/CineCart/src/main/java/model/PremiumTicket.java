80161b5e97c8533ea42cc478eeee4260802da70d
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-06-16 09:25:11 +0600

    added constructor

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -1,4 +1,13 @@
 package model;
 
 public class PremiumTicket extends Ticket{
+
+    public PremiumTicket(Showtime showtime, int row, int col, double pricePaid, double peak) {
+        super(showtime, row, col, pricePaid, peak);
+    }
+
+    @Override
+    public double getCharge() {
+
+    }
 }
