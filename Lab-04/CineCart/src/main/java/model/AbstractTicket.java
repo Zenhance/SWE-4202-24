6364e75c9bc5762f0307cd3d51fe83acaa42eb3e
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-16 10:28:42 +0600

    completed abstract  ticket,

--- Lab-04/CineCart/src/main/java/model/AbstractTicket.java
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -1,4 +1,28 @@
 package model;
 
-public class AbstractTicket {
+public abstract class AbstractTicket extends LineItem {
+    protected final Showtime showtime;
+    protected final int row;
+    protected final int col;
+    protected final double base;
+
+    protected AbstractTicket(Showtime showtime, int row, int col) {
+        this.showtime = showtime;
+        this.row = row;
+        this.col = col;
+        this.base = showtime.getMovie().getBasePrice();
+    }
+
+    protected double peakMultiplier() {
+        return showtime.isPeak() ? 1.20 : 1.0;
+    }
+
+    protected String seatText() {
+        return "R" + row + "C" + col;
+    }
+
+    @Override
+    public boolean isTicketLine() {
+        return true;
+    }
 }
