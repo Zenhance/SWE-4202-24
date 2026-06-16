79851ac040a57e2d320ae3669691e34654587afd
58-jahin <jahinhossain@iut-dhaka.edu>
2026-06-16 10:24:56 +0600

    completed combo line,

--- Lab-04/CineCart/src/main/java/model/ComboLine.java
+++ Lab-04/CineCart/src/main/java/model/ComboLine.java
@@ -1,4 +1,41 @@
 package model;
 
-public class ComboLine {
+public class ComboLine extends LineItem {
+    private ConcessionItem itemA;
+    private ConcessionItem itemB;
+
+    public ComboLine(ConcessionItem itemA, ConcessionItem itemB) {
+        this.itemA = itemA;
+        this.itemB = itemB;
+    }
+
+    public ConcessionItem getItemA() {
+        return itemA;
+    }
+
+    public ConcessionItem getItemB() {
+        return itemB;
+    }
+
+    @Override
+    public double subtotal() {
+        double subtotal = itemA.getUnitPrice() + itemB.getUnitPrice();
+
+        if (itemA.getCode().equals("POP") && itemB.getCode().equals("SODA")) {
+            subtotal -= 50.00;
+        }
+
+        return subtotal;
+    }
+
+    @Override
+    public String describe() {
+        return "Combo: " + itemA.getCode() + " + " + itemB.getCode()
+                + " - BDT " + String.format("%.2f", subtotal());
+    }
+
+    @Override
+    public boolean carriesCode(String code) {
+        return itemA.getCode().equals(code) || itemB.getCode().equals(code);
+    }
 }
