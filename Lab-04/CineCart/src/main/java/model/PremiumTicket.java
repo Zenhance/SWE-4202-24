2de120e2960eb40f2d8ad47f41b5f86a11654c67
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-06-16 10:13:26 +0600

    Completed PremiumTicket.

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -4,4 +4,12 @@ public class PremiumTicket extends AbstractTicket{
     public PremiumTicket(Showtime base, int row, int col){
         super(base, row, col);
     }
+    @Override
+    public double subtotal(){
+        return applyPeak(getBasePrice()*1.30);
+    }
+    @Override
+    public String describe(){
+        return "Premium Ticket";
+    }
 }
