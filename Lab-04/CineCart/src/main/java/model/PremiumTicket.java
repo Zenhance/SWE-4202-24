fc436e676287681aa1aa5fd25a8a27cd668711c9
tahsin1r <naylakagi@gmail.com>
2026-06-16 09:56:48 +0600

    PremiumTicket and StudentTicket done

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -1,4 +1,17 @@
 package model;
 
-public class PremiumTicket {
+public class PremiumTicket extends AbstractTicket{
+
+
+    protected PremiumTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+    @Override
+    public double subtotal() {
+        return 0;
+    }
+    public String describe(){
+        return super.describe()+"Premium Fare";
+    }
 }
--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -1,4 +1,14 @@
 package model;
 
-public class StudentTicket {
+public class StudentTicket extends StandardTicket{
+    public StudentTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+    public double subtotal(){
+        return base*0.50*peakMultiplier();
+    }
+    public String describe(){
+        return super.describe();
+    }
 }
