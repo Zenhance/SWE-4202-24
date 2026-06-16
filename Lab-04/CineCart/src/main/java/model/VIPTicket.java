5acb25bc1d51306c143444d4b6b8496c208691e5
Tahmid <tahmidim99@gmail.com>
2026-06-16 12:50:26 +0600

    1

---
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -0,0 +1,11 @@
+package model;
+
+public class VIPTicket extends PremiumTicket{
+    public VIPTicket(Showtime showtime, int row, int col){
+        super(showtime,row,col);
+    }
+    @Override
+    public double subtotal(){
+        return super.subtotal()+200.00;
+    }
+}
