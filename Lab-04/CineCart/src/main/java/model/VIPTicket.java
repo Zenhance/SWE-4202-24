acec84fca241f8c3bdd9e87575fd3e785d7efb2d
Sadman Raiyan <kamrulseed@gmail.com>
2026-06-16 10:05:43 +0600

    VIPTicket class method added

--- Lab-04/CineCart/src/main/java/model/VIPTicket.java
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -1,4 +1,16 @@
 package model;
 
-public class VIPTicket {
+public class VIPTicket extends PremiumTicket{
+    public VIPTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+    @Override
+    public double subtotal() {
+        return (basePrice*1.50*peak())+200.0;
+    }
+    @Override
+    public String describe(){
+        return "VIP Ticket";
+    }
+
 }
