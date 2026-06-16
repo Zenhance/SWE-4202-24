3baf6bfd55aaf53334338d76df64900a9d0f2804
wasifjafar <wasifjafar71@gmail.com>
2026-06-16 09:14:29 +0600

    create overridden subtotal method in VIPTicket

--- Lab-04/CineCart/src/main/java/model/VIPTicket.java
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -1,7 +1,12 @@
 package model;
 
 public class VIPTicket extends PremiumTicket{
-    public VIPTicket(Showtime showtime, int row, int col, double pricePaid) {
-        super(showtime, row, col, pricePaid);
+    public VIPTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
+    }
+
+    @Override
+    public double subtotal() {
+        return super.subtotal() + 200.00;
     }
 }
