c14404f66c98b1b0aa63952691473b73cc05c234
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-06-16 09:09:35 +0600

    new studentticket child class of abstract ticket class

--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -4,4 +4,12 @@ public class StudentTicket extends StandardTicket{
     public StudentTicket(Showtime showtime, int row, int col){
         super(showtime,row,col);
     }
+    @Override
+    public double subtotal(){
+        return base*0.50*peakMultiplier();
+    }
+    @Override
+    public String describe(){
+        return super.describe()+" Standard";
+    }
 }
