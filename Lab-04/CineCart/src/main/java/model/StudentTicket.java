ba9583190c2773818dd0e80b0973a54a68072c63
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-16 09:17:25 +0600

    Change: update code from test and return 0

--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -1,11 +1,11 @@
 package model;
 
-public class StudentTicket {
+public class StudentTicket extends LineItem {
     public StudentTicket(Showtime showtime, int i, int i1) {
     }
 
     public double subtotal() {
-    }
+        return 0;}
 
     public String describe() {
         return null;
