0af38d922bb85b84db1b28bd524793830479ab9c
Rakin <rakinhasan001@gmail.com>
2026-06-16 09:22:56 +0600

    ConcessionLine class constructed

---
+++ Lab-04/CineCart/src/main/java/model/ConcessionLine.java
@@ -0,0 +1,24 @@
+package model;
+
+public class ConcessionLine extends LineItem {
+    private ConcessionItem item;
+    private int qty;
+    public ConcessionLine(ConcessionItem item,int qty){
+        this.item=item;
+        this.qty=qty;
+    }
+    public ConcessionItem getItem(){
+        return item;
+    }
+    public int getQty(){
+        return qty;
+    }
+    @Override
+    public double subtotal(){
+        return item.getUnitPrice()*qty;
+    }
+    @Override
+    public String describe(){
+        return String.format(item.toString() + " x" + qty + " - BDT " + subtotal() );
+    }
+}
