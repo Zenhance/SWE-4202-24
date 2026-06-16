6492314abcfa446eaa5386371c5bb797af1a7367
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-16 08:46:04 +0600

    Change: update code from test to StandardTicket class

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -1,4 +1,13 @@
 package model;
 
 public class StandardTicket {
+    public StandardTicket(Showtime showtime, int i, int i1) {
+    }
+
+    public double subtotal() {
+    }
+
+    public String describe() {
+        return null;
+    }
 }
