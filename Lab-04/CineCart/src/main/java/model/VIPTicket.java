054299eae105f6b2c4eda6a5b652cdc440c1d8a8
ayiham-1012 <ayiham1012@gmail.com>
2026-06-16 11:37:19 +0600

    VIPTicket

--- Lab-04/CineCart/src/main/java/model/VIPTicket.java
+++ Lab-04/CineCart/src/main/java/model/VIPTicket.java
@@ -1,4 +1,12 @@
 package model;
 
-public class VIPTicket {
+public class VIPTicket extends PremiumTicket{
+    VIPTicket(Showtime showtime, int row, int col, double pricePaid){
+        super(showtime, row, col, pricePaid);
+    }
+
+    public double getPricePaid() {
+        return getPricePaid() + 200.00;
+    }
+
 }
