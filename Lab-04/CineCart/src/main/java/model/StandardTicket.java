409cb739aed2848fb9288761d1b12343434cc6a9
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-16 10:39:32 +0600

    Change: fix StandardTicket class

--- Lab-04/CineCart/src/main/java/model/StandardTicket.java
+++ Lab-04/CineCart/src/main/java/model/StandardTicket.java
@@ -1,14 +1,12 @@
 package model;
 
-public class StandardTicket extends LineItem {
-    public StandardTicket(Showtime showtime, int i, int i1) {
+public class StandardTicket extends AbstractTicket {
+    public StandardTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
     }
 
+    @Override
     public double subtotal() {
-        return 0;
-    }
-
-    public String describe() {
-        return null;
+        return calculateBase();
     }
 }
