e15542f972b6e97afa390ef5cea671865ebb834e
sameer-abdullah-240042107 <sameerabdullah1117@gmail.com>
2026-06-16 12:17:50 +0600

    error solving

--- Lab-04/CineCart/src/main/java/model/AbstractTicket.java
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -2,13 +2,15 @@ package model;
 
 public abstract class AbstractTicket extends LineItem {
     protected Showtime showtime;
-    protected int rows;
-    protected int cols;
+    protected int row;
+    protected int col;
     protected double basePrice;
 public AbstractTicket(Showtime showtime,int cols,int rows){
     this.showtime=showtime;
-    this.cols=cols;
-    this.rows=rows;
+    this.col=col;
+    this.row=row;
+    this.basePrice=basePrice;
+
     }
     public double peak(){
     return showtime.isPeak()?basePrice*1.20:basePrice*1.00;
