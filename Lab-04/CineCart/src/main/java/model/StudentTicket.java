b4d47bffd92ae495fe5875fa48dcbf2ca9bbacfc
jasin <anishaajasin.13.04.2005@gmail.com>
2026-06-16 12:29:59 +0600

    Commit Student Ticket

--- Lab-04/CineCart/src/main/java/model/StudentTicket.java
+++ Lab-04/CineCart/src/main/java/model/StudentTicket.java
@@ -2,5 +2,20 @@ package model;
 
 public class StudentTicket extends Ticket {
 
+    public StudentTicket(Showtime showtime,
+                         int row,
+                         int col) {
 
+        super(showtime, row, col);
+    }
+
+
+    public double subtotal() {
+        return basePrice * 0.50 * peakMultiplier();
+    }
+
+
+    public String describe() {
+        return super.describe() + " Student";
+    }
 }
