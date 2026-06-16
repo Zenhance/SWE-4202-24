45f1f99be236489a6da4f0805290e6be236cb81d
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-06-16 11:33:16 +0600

    1

---
+++ Lab-04/CineCart/src/main/java/model/AbstractTicket.java
@@ -0,0 +1,15 @@
+package model;
+
+public abstract class AbstractTicket extends LineItem {
+    protected Showtime showtime;
+    protected int row;
+    protected int col;
+    protected double basePrice;
+
+    public AbstractTicket(Showtime showtime, int row, int col, double basePrice){
+        this.showtime=showtime;
+        this.row=row;
+        this.col=col;
+        this.basePrice=basePrice;
+    }
+}
