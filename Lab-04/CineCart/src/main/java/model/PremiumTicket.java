45f5bbfca18ae305de05370f48608389e72c1f0e
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-06-16 10:41:29 +0600

    Change: fix PremiumTicket class , update the constructor and Add override

--- Lab-04/CineCart/src/main/java/model/PremiumTicket.java
+++ Lab-04/CineCart/src/main/java/model/PremiumTicket.java
@@ -1,14 +1,12 @@
 package model;
 
-public class PremiumTicket extends LineItem {
-    public PremiumTicket(Showtime showtime, int i, int i1) {
+public class PremiumTicket extends AbstractTicket {
+    public PremiumTicket(Showtime showtime, int row, int col) {
+        super(showtime, row, col);
     }
 
+    @Override
     public double subtotal() {
-        return 0;
-    }
-
-    public String describe() {
-        return null;
+        return calculateBase() * 1.30;
     }
 }
