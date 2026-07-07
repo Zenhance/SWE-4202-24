2617b38ed65d90de0455728e8de2fa503f2eb4bc
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-07-07 11:40:41 +0600

    Added Connection abstract class

---
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -0,0 +1,25 @@
+public abstract class Connection {
+    public double fuelPercent = 0.10;
+    public double taxPercent  = 0.05;
+
+    private final Meter meter;
+
+    public Connection(Meter meter) {
+        this.meter = meter;
+    }
+
+    public abstract double energyCharge();
+    public abstract double fixedCharge();
+
+    public double fuelSurcharge() {
+        return energyCharge()*fuelPercent;
+    }
+
+    public double tax(){
+        return (energyCharge()+fixedCharge()+fuelSurcharge())*taxPercent;
+    }
+
+    public double total(){
+        return energyCharge()+fixedCharge()+fuelSurcharge()+tax();
+    };
+}
