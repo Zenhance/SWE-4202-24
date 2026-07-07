ab9a65c22b2d1a641a60c5081ed7d311a36d5005
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-07 09:18:31 +0600

    change : add error conditions

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -1,14 +1,18 @@
 public class Meter {
     private int openingReading;
     private int closingReading;
+
     public Meter(int openingReading, int closingReading) {
         if (openingReading < 0 || closingReading < 0) {
             throw new IllegalArgumentException("No negative values allowed");
         }
 
-        if (openingReading >  closingReading ) {
+        if (openingReading > closingReading) {
             throw new IllegalArgumentException("Closing reading has to be above the opening value");
         }
+
+        this.openingReading = openingReading;
+        this.closingReading = closingReading;
     }
 
     public int getUnitsConsumed() {
