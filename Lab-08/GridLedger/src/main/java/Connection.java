ef90532573cfd07b19940dea9064c47e5822a0af
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-07 09:38:06 +0600

    defined the common bill formula, also created two abstract methods, energy charge and fixedcharge will force classes and child classes to provide the energy and fixed charge

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -14,9 +14,36 @@ public abstract class Connection {
         this.meter = meter;
         this.fuelSurchargeRate = DEFAULT_FUEL_SURCHARGE_RATE;
     }
-
     public Connection() {
 
         meter = null;
+    }
+    public Meter meter() {
+        return meter;
+    }
+    public int unitsConsumed() {
+        return meter.unitsConsumed();
+    }
+    public abstract double energyCharge();
+
+    public abstract double fixedCharge();
+
+    public double fuelSurcharge() {
+        return energyCharge()*fuelSurchargeRate;
+    }
+    public double tax() {
+        return (energyCharge()+fixedCharge()+fuelSurcharge())*TAX_RATE;
+    }
+    public double total() {
+        return energyCharge()+ fixedCharge()+ fuelSurcharge()+ tax();
+    }
+    public void setFuelSurchargeRate(double fuelSurchargeRate) {
+        if (fuelSurchargeRate < 0) {
+            throw new IllegalArgumentException("Fuel surcharge rate cannot be negative.");
+        }
+        this.fuelSurchargeRate = fuelSurchargeRate;
+    }
+    public double fuelSurchargeRate() {
+        return fuelSurchargeRate;
     }
 }
