5b475abf0a6b80db4bd4614254d4c65a7eee6aa9
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-07-07 10:15:13 +0600

    classes created to reduce compilation errors

---
+++ Lab-08/GridLedger/src/main/java/BillingRun.java
@@ -0,0 +1,23 @@
+public class BillingRun {
+
+    public BillingRun(){
+
+    }
+
+    public void register(){
+
+    }
+
+    public int size(){
+
+    return 0;
+    }
+
+    public double grandTotal(){
+
+    return 0;
+    }
+
+
+
+}
