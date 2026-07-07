bc1bafc81913303310643f8c289045b933f0e97a
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-07 09:52:55 +0600

    CommercialConnection class completed

--- Lab-08/GridLedger/src/main/java/CommercialConnection.java
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -2,5 +2,20 @@ public class CommercialConnection extends Connection {
     private double Rate_1 = 9.0;
     private double Rate_2 = 13.0;
     private double Rate_Fixed = 500.0;
+    private Meter meter;
+
+    @Override
+    public double fixedCharge() {
+        return Rate_Fixed;
+    }
+
+    public double energyCharge() {
+        int units = meter.getUnitsConsumed();
+        if(units <= 100) {
+            return units*Rate_1;
+        }
+        return (100*Rate_1) + ((units-100)*Rate_2);
+
+    }
 
 }
