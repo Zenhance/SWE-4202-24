9f423d2a5992b81177a70f3da323aba54c9c8c7f
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-07-07 12:08:22 +0600

    Updated Connection Class

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,27 +1,36 @@
 public abstract Class Connection
         {
 
-            private Meter meter;
-            private static final double FUEL_DEFAULT = 0.10;
-            private static final double TAX_RATE = 0.05;
+private Meter meter;
+private static final double FUEL_DEFAULT = 0.10;
+private static final double TAX_RATE = 0.05;
 
 
-            private Connection (Meter meter)
-            {
-                this.meter=meter;
+private Connection(Meter meter) {
+    this.meter = meter;
 
-            }
-            public abstract double energyCharge();
-            public abstract double fixedCharge();
+}
 
-            public double fuelSurcharge()
-            {
-                return energyCharge()*FUEL_DEFAULT;
-            }
-            public double tax()
-            {
-                return (energyCharge() + fixedCharge() + fuelSurcharge())*TAX_RATE;
-            }
+public Meter getMeter() {
+    return meter;
+}
+
+public abstract double energyCharge();
+
+public abstract double fixedCharge();
+
+public double fuelSurcharge() {
+    return energyCharge() * FUEL_DEFAULT;
+}
+
+public double tax() {
+    return (energyCharge() + fixedCharge() + fuelSurcharge()) * TAX_RATE;
+}
+public double total()
+{
+    return energyCharge()+fixedCharge()+fuelSurcharge()+tax();
+}
 
                 }
+
 
