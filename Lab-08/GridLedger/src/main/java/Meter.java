ed413984f66fbc589b6fb31882623341483782ce
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-07-07 13:09:18 +0600

    Meter class - variable and constructor creation

---
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -0,0 +1,16 @@
+public class Meter {
+    private final int openingReading;
+    private final int closingReading;
+
+    public Meter(int openingReading, int closingReading){
+    if(openingReading < 0 || closingReading < 0){
+        throw new IllegalArgumentException("It can't be negative.");
+    }
+    if(closingReading < openingReading){
+        throw new IllegalArgumentException("Closing reading must be greater than oepning reading.");
+    }
+    this.openingReading = openingReading;
+    this.closingReading = closingReading;
+    }
+
+}
