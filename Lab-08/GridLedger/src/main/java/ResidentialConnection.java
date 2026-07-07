5af82f630be346792bc07fdbdb077af8bdb1b2ef
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-07 09:37:38 +0600

    ResidentialConnection class completed

--- Lab-08/GridLedger/src/main/java/ResidentialConnection.java
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -3,6 +3,8 @@ public class ResidentialConnection extends Connection{
     private double Rate_2 = 7.0;
     private double Rate_3 = 11.0;
     private double Rate_Fixed = 100.0;
+    private Meter meter;
+
 
     @Override
     public double fixedCharge() {
@@ -11,6 +13,14 @@ public class ResidentialConnection extends Connection{
 
     @Override
     public double energyCharge() {
+        int units = meter.getUnitsConsumed();
+        if(units <= 50) {
+            return units*Rate_1;
+        }
+        if(units <= 200) {
+            return (50*Rate_1) + ((units-50)*Rate_2);
+        }
+        return (50*Rate_1) + (150*Rate_2) + ((units-200)*Rate_3);
 
     }
 
