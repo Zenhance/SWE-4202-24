854a0605bf6d69ea55c3d1dca6128748401e2909
04-Labiba <sabihalabiba639@gmail.com>
2026-07-07 09:10:53 +0600

    init commit

---
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -0,0 +1,10 @@
+public class LifelineConnection extends ResidentialConnection{
+    private static final double REBATE=0.30;
+    public LifelineConnection(Meter meter){
+        super(meter);
+    }
+    @Override
+    public double energyCharge(){
+        return super.energyCharge()*(1.0-REBATE);
+    }
+}
