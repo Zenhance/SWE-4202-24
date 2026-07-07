e093699adb2b8f4dc269fe28208948bdf4b97764
Maomao-starry <aminarahmanshalu@gmail.com>
2026-07-07 11:50:11 +0600

    task

---
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -0,0 +1,20 @@
+public abstract class Connection {
+    private static final double fuelpercent=0.10;
+    private static final double taxpercent=0.05;
+    private Meter meter;
+    public Connection(Meter meter){
+        this.meter=meter;
+    }
+    public abstract double energyCharge();
+    public abstract double fixedCharge();
+    public double fuelSurcharge(){
+        return energyCharge()* Connection.fuelpercent;}
+    public double tax(){
+        return (energyCharge()+fixedCharge()+fuelSurcharge())*taxpercent;
+    }
+    public double sum(){
+        return energyCharge()+fixedCharge()+fuelSurcharge()+tax();
+    }
+
+
+}
