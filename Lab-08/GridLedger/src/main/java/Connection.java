d4818dc95c4eb49788f00c4a0e851517cc80ccd2
Muhtasim Moin <muhtasi13@gmail.com>
2026-07-07 09:14:57 +0600

    Connection class is updated by making energycharge,fixedcharge,fuelsurcharge,tax and total methods

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,2 +1,24 @@
-public abstract class Connection {
+public abstract class Connection { private static final double FUEL_DEFAULT    = 0.10; // fuel surcharge = this % of the energy charge
+    private static final double TAX_RATE        = 0.05;
+    Meter meter;
+    public Connection(Meter meter){
+        this.meter=meter;
+    }
+    protected int getUnitsConsumed(){
+        return meter.getUnitsConsumed();
+    }
+
+    public abstract double energyCharge() ;
+    public abstract  double fixedCharge();
+    public double fuelSurcharge(){
+        return energyCharge()*FUEL_DEFAULT;
+    }
+    public double tax(){
+        return (energyCharge()+fixedCharge()+fuelSurcharge())*TAX_RATE;
+    }
+   public double total(){
+        return energyCharge()+fixedCharge()+fuelSurcharge()+tax();
+   }
+
+
 }
