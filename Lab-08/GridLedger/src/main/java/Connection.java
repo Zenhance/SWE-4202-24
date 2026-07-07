4114f9d0559a917cd16f2354e3b07e86afdae06a
md-musaddik <musaddikibna1@gmail.com>
2026-07-07 11:46:07 +0600

    Important methods declared for Connection class. also some abastract classes declared, because every class will have their own implimentation way.

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -2,12 +2,23 @@ public abstract class Connection {
     final double DEF_FUEL_PER=0.10;
     final double TAX_RATE=0.05;
     private final Meter meter;
-    double fuelPer=DEF_FUEL_PER;
+    double fuelPercentage=DEF_FUEL_PER;
 
     public Connection(Meter meter) {
         this.meter = meter;
     }
     public int getUnitsConsumed() {
         return meter.getUnitsConsumed();
+    }
+    void setFuelPercentage(double fuelPercentage) {
+        this.fuelPercentage = fuelPercentage;
+    }
+
+    public abstract double energyCharge();
+
+    public abstract double fixedCharge();
+
+    public double fuelSurcharge() {
+        return energyCharge() * fuelPercentage;
     }
 }
