ab9896a9b092677b7f3d543ddc7b587f1fe59b7a
Rakib <rakibulhasan4266@gmail.com>
2026-07-07 09:29:24 +0600

    Class and Constructor

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -3,4 +3,19 @@ public abstract class Connection
 
     public static final double DEFAULT_FUEL_SURCHARGE_PERCENTAGE = 0.10;
     public static final double TAX_PERCENTAGE = 0.05;
+    private final Meter meter;
+    private double fuelSurchargePercentage;
+    protected Connection(Meter meter)
+    {
+        this(meter,DEFAULT_FUEL_SURCHARGE_PERCENTAGE);
+    }
+    protected Connection(Meter meter, double fuelSurchargePercentage)
+    {
+        if(meter == null)
+        {
+            throw new IllegalArgumentException("A connection must have a meter.");
+        }
+        this.meter = meter;
+        this.fuelSurchargePercentage = fuelSurchargePercentage;
+    }
 }
