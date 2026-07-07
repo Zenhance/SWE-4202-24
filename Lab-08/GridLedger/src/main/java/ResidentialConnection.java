95ed2bdd75cba28522b0255f13b6647fa793626a
md-musaddik <musaddikibna1@gmail.com>
2026-07-07 12:06:43 +0600

    ResedentialConnection's energyCharge method declared.

--- Lab-08/GridLedger/src/main/java/ResidentialConnection.java
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -4,9 +4,24 @@ public class ResidentialConnection extends Connection {
     final double RATE_BAND1 = 4.0;
     final double RATE_BAND2 = 7.0;
     final double RATE_BAND3 = 11.0;
-
+    final double FIXED_CHARGE = 100.0
 
     public ResidentialConnection(Meter meter) {
-        super();
+        super(meter);
     }
+
+    @Override
+    public double energyCharge() {
+        int units = getUnitsConsumed();
+        if (units <= BAND1_LIMIT) {
+            return (units * RATE_BAND1);
+        }else if (units <= BAND2_LIMIT) {
+            return (BAND1_LIMIT * RATE_BAND1 + (units - BAND1_LIMIT) * RATE_BAND2);
+        } else {
+            return (BAND1_LIMIT * RATE_BAND1
+                    + (BAND2_LIMIT - BAND1_LIMIT) * RATE_BAND2
+                    + (units - BAND2_LIMIT) * RATE_BAND3);
+        }
+    }
+
 }
