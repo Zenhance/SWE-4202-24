82e4b8e4459dfb000cfc7953db89714f72982718
md-musaddik <musaddikibna1@gmail.com>
2026-07-07 12:15:03 +0600

    Industrial Connection's attributes and energyCharge method declared.

--- Lab-08/GridLedger/src/main/java/IndustrialConnection.java
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -1,5 +1,12 @@
 public class IndustrialConnection extends Connection {
+    final double RATE = 15.0;
+    final double FIXED_CHARGE = 2000.0;
     public IndustrialConnection(Meter meter) {
-        super();
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge() {
+        return getUnitsConsumed() * RATE;
     }
 }
