f604df5cde188ae41a45af40e19fd76d0976b96a
tryhardFuad <mubtasimsmail@gmail.com>
2026-07-07 08:56:57 +0600

    created abstract methods energyCharge and fixedCharge, and fuelSurcharge method for Connection Class

---
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -0,0 +1,14 @@
+public abstract class Connection {
+    private double fuelSurchargePercentage = 0.10;
+    private double taxSurchargePercentage = 0.05;
+
+
+    abstract public double energyCharge();
+    abstract public double fixedCharge();\
+
+    public double fuelSurcharge() {
+        return energyCharge() * fuelSurchargePercentage;
+    }
+
+
+}
