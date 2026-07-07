9767fd650583bb2533164f2fad480ac740e0472f
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-07 10:17:48 +0600

    change : add super and overrides to CommercialConnection  class

--- Lab-08/GridLedger/src/main/java/IndustrialConnection.java
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -1,4 +1,10 @@
-public class IndustrialConnection {
+public class IndustrialConnection extends Connection {
     public IndustrialConnection(Meter meter) {
+        super(meter);
+    }
+    
+    @Override
+    public double energyCharge(){
+        return getUnits() * 15.0;
     }
 }
