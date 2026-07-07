472171025309a3d2a3ee91a13f5b1e49ba658fb5
23-Fahad <ifahad2k@gmail.com>
2026-07-07 11:38:59 +0600

    build Connection.java

---
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -0,0 +1,32 @@
+package main.java;
+
+import java.Meter;
+
+public abstract class Connection {
+    private final Meter meter;
+    private static double fuelPercent = 0.10;
+    private static double taxPercent  = 0.05;
+    protected Connection(Meter meter){
+        this.meter = meter;
+    }
+    protected int unitConsumed(){
+        return meter.totalUsages();
+    }
+    public abstract double energyCharge();
+
+
+    public abstract double fixedCharge();
+    public double fuelSurcharge(){
+        return energyCharge() * fuelPercent;
+    }
+    public double tax(){
+        return (energyCharge() + fixedCharge() + fuelSurcharge()) * taxPercent;
+    }
+    public double totalBill(){
+        return energyCharge()+fixedCharge()+fuelSurcharge()+tax();
+    }
+
+
+
+
+}
