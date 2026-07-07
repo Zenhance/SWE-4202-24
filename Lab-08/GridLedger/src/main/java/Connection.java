431ade887d94a6bad79dad49c0130ca765d4191e
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-07 09:06:54 +0600

    added fields

---
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -0,0 +1,10 @@
+public abstract class Connection {
+
+    protected double energyCharge;
+    protected double fixedCharge;
+    protected double fuelSurCharge;
+    public  double billTotal;
+    public double tax;
+
+    public Connection(double energyCharge, double fixedCharge)
+}
