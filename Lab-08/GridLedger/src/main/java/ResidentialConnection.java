ebce98f1cb6b74c289e4b15bb6b31ebee615991e
md-musaddik <musaddikibna1@gmail.com>
2026-07-07 12:01:46 +0600

    ResidentialConnection class's attributes declared.

--- Lab-08/GridLedger/src/main/java/ResidentialConnection.java
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -1,4 +1,11 @@
 public class ResidentialConnection extends Connection {
+    final double BAND1_LIMIT = 50;
+    final double BAND2_LIMIT = 200;
+    final double RATE_BAND1 = 4.0;
+    final double RATE_BAND2 = 7.0;
+    final double RATE_BAND3 = 11.0;
+
+
     public ResidentialConnection(Meter meter) {
         super();
     }
