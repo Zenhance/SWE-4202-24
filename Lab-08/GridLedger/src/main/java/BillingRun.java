8a7f48ffcfc4c391e97a4727587aea4b916143f1
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-07-07 09:27:42 +0600

    billing run WIP

---
+++ Lab-08/GridLedger/src/main/java/BillingRun.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class BillingRun {
+    private List<Connection> connections = new ArrayList<>();
+
+    public void register(Connection c){
+        connections.add(c);
+    }
+}
