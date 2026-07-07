aff2db26a2e7878f4e1f2a3f11b0daf7d7918155
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-07-07 11:07:53 +0600

    Created Meter Class

---
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -0,0 +1,29 @@
+public class Meter
+{
+private int openingReading;
+private int closingReading;
+
+
+public Meter (int closingReading,int openingReading) {
+    if (closingReading < openingReading)
+        throw new IllegalArgumentException(" closing reading below the opening reading is a broken record, not a cheap month");
+    if (closeReading < 0) throw new IllegalArgumentException("a negative opening reading is impossible");
+    this.openingReading = openingReading;
+    this.closingReading = closingReading;
+}
+
+
+
+    public int getUnitsConsumed() { return closingReading - openingReading; }
+
+
+
+
+}
+
+
+
+
+
+
+}
