c213f0ba5284424ea9c7f0340873525e100681e6
Rakin <rakinhasan001@gmail.com>
2026-07-07 12:25:10 +0600

    IndustrialConnection class built

--- Lab-08/GridLedger/src/main/java/IndustrialConnection.java
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -1,6 +1,16 @@
 public class IndustrialConnection extends Connection{
 
     public IndustrialConnection(Meter meter){
-        this.meter=meter;
+        super(meter);
+    }
+
+    @Override
+    public double fixedCharge() {
+        return 500.0;
+    }
+
+    @Override
+    public double energyCharge() {
+        return meter.getUnitsConsumed()*10.0;
     }
 }
