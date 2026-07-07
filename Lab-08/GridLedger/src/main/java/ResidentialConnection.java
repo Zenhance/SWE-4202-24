c5e6e6018072fe4b3446100df2207fede7681ad1
37-inan <inanjaki184@gmail.com>
2026-07-07 12:09:21 +0600

    class

--- Lab-08/GridLedger/src/main/java/ResidentialConnection.java
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -1,2 +1,15 @@
-public class ResidentialConnection {
+public class ResidentialConnection extends Connection{
+    private static final double RES_RATE_1=4.0;
+    private static final double RES_RATE_2=7.0;
+    private static final double RES_RATE_3=11.0;
+    private static final double RES_FIXED=100.0;
+
+    public ResidentialConnection(Meter meter){
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge() {
+
+    }
 }
