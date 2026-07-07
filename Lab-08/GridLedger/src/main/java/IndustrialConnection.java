23693897c26e40aaddfbbceafe743421665d51ed
Zunied-57 <zunied0411@gmail.com>
2026-07-07 09:33:47 +0600

    all methods added

---
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -0,0 +1,23 @@
+public class IndustrialConnection extends Connection
+{
+    private static final double IND_RATE   = 15.0;
+    private static final double IND_FIXED  = 2000.0;
+
+    public IndustrialConnection(Meter meter)
+    {
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge()
+    {
+        int unit = getMeter().getUnitsConsumed();
+        return unit*IND_RATE;
+    }
+
+    @Override
+    public double fixedCharge()
+    {
+        return IND_FIXED;
+    }
+}
