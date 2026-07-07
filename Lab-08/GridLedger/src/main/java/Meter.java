4f382fa4730ef5d669330b7a74ce81f1b9da5c30
Tasnita Raida <tasnitar@gmail.com>
2026-07-07 11:36:14 +0600

    meter class

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -3,6 +3,9 @@ public class Meter{
     public final int closingReading;
 
     public Meter(int openingReading, int closingReading){
-
+        if (openingReading<0)
+            throw new IllegalArgumentException("A meter readingcannot be negative.");
+        if (closingReading<0)
+            throw new IllegalArgumentException("A meter readingcannot be negative.");
     }
 }
