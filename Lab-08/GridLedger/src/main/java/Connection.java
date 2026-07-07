5cb2952f95d7650b28c1832e00f87817e48c789c
sabbir240042147 <eftesabbir2725@gmail.com>
2026-07-07 11:23:26 +0600

    connection class

---
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -0,0 +1,16 @@
+package main.java;
+
+public abstract class Connection {
+    private Meter meter;
+    private static final double FUEL_DEFAULT=0.10;
+    private static final double TAX_RATE=0.05;
+    public Connection(Meter meter){
+        this.meter=meter;
+    }
+    public abstract double energyCharge();
+    public abstract double fixedCharge();
+    public double fuelSurCharge(){
+        return energyCharge()*FUEL_DEFAULT;
+    }
+
+}
