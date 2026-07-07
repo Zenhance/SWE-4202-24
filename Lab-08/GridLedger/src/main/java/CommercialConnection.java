54f5d16e8686652667635a1965877fdf567a8aed
tasnitaraida <tasnitar@gmail.com>
2026-07-07 12:40:19 +0600

    done

---
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -0,0 +1,16 @@
+public class CommercialConnection extends Connection{
+    public CommercialConnection(Meter meter){
+        super(meter);
+    }
+
+    public double energyCharge(){
+        int units=units();
+        if(units<=100){
+            return units*9.0
+        }return 100*9.0+(units-100)*13.0;
+    }
+
+    public double fixedCharge(){
+        return 500.0;
+    }
+}
