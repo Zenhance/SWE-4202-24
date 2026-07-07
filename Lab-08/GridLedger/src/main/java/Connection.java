3758745bcd1a63931b67f812109944b8abb13ec9
waseefmunir-max <waseef05@gmail.com>
2026-07-07 08:56:18 +0600

    Create fixedCharge() method

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -3,4 +3,8 @@ public class Connection {
     public @org.jspecify.annotations.Nullable Byte energyCharge() {
         return null;
     }
+
+    public double fixedCharge() {
+        return 0.0;
+    }
 }
