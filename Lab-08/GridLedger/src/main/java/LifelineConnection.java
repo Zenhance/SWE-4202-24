db6542148af354b50533d69af94d2af284a1e920
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-07-07 12:08:53 +0600

    1

---
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class LifelineConnection extends ResidentialConnection{
+    private static final double REBATE_RATE=0.30;
+
+    public LifelineConnection(Meter meter){
+        super(meter);
+    }
+
+
+}
