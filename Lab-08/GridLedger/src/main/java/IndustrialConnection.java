07a513987e13c93607839393cec549298bcf54ad
Munman <214246799+bollamokles@users.noreply.github.com>
2026-07-07 09:43:46 +0600

    commit IndustrialConnection

---
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -0,0 +1,16 @@
+public class IndustrialConnection extends Connection{
+
+    public IndustrialConnection(Meter meter) {
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge() {
+        return 0;
+    }
+
+    @Override
+    public double fixedCharge() {
+        return 2000.0;
+    }
+}
