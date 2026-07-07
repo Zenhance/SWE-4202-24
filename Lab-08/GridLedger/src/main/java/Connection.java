23220a1ae04cfafadba2cada3114b858a1ed7277
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-07 08:53:19 +0600

    new connection class

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,3 +1,12 @@
-public class Connection {
+public abstract class Connection {
+    private final Meter meter;
+    private double fuelPercent = 0.10;
+    private static final double TAX_RATE = 0.05;
 
+    public Connection(Meter meter){
+        if(meter==null){
+            throw new IllegalAccessException("Meter cannot be null");
+        }
+        this.meter = meter;
+    }
 }
