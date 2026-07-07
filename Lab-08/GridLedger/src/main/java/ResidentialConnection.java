4a1c3f53dca8286e3d5a580000d97a90133f6674
Rakib <rakibulhasan4266@gmail.com>
2026-07-07 10:13:07 +0600

    Clas Constant

---
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -0,0 +1,9 @@
+public class ResidentialConnection extends Connection
+{
+    private static final int BAND1_LIMIT = 50;
+    private static final int BAND2_LIMIT = 200;
+    private static final double BAND1_RATE = 4.0;
+    private static final double BAND2_RATE = 7.0;
+    private static final double BAND3_RATE = 11.0;
+    private static final double FIXED_CHARGE = 100.0;
+}
