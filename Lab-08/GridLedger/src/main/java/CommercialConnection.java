f47c2bc31c5d99ab4882375a3991c34dd1726910
arnoboic <arnobarafat@iut-dhaka.edu>
2026-07-07 11:41:45 +0600

    finished IndustrialConnection  Class

--- Lab-08/GridLedger/src/main/java/CommercialConnection.java
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -5,12 +5,19 @@ public class CommercialConnection extends Connection{
 
     @Override
     public double energyCharge() {
-        return units * 15.0;
+        double c = 0;
+        if (units <= 100) {
+            c += units * 9.0;
+        } else {
+            c += 100 * 9.0;
+            c += (units - 100) * 13.0;
+        }
+        return c;
     }
 
     @Override
     public double fixedCharge() {
-        return 2000.0;
+        return 500.0;
     }
 
 }
--- Lab-08/GridLedger/src/main/java/IndustrialConnection.java
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -1,2 +1,15 @@
-public class IndustrialConnection {
+public class IndustrialConnection extends Connection {
+    public IndustrialConnection(Meter M){
+        super(M);
+    }
+
+    @Override
+    public double energyCharge() {
+        return units * 15.0;
+    }
+
+    @Override
+    public double fixedCharge() {
+        return 2000.0;
+    }
 }
