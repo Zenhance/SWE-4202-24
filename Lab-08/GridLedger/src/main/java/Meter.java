8ef02828546ae558192a3488a50e265184a13e36
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-07 08:54:32 +0600

    init commit

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -1,9 +1,21 @@
+import com.sun.source.tree.BreakTree;
+
 public class Meter {
     private int openingReading;
-    private int closeReading;
+    private int closingReading;
 
     public Meter(int openingReading,int closeReading){
-
+      if(openingReading< 0|| closingReading< 0){
+          throw new IllegalArgumentException("Reading cant be negative");
+      }
+      if(closingReading<openingReading) {
+          throw new IllegalArgumentException("closing reading cant be smaller");
+      }
+      this.openingReading=openingReading;
+      this.closingReading=closeReading;
+    }
+    public int getOpeningReading(){
+        return openingReading;
     }
 
 
