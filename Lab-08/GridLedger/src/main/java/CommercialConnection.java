d91ff0d4f5ab28fc46e957ebe4a93ff4526e23d1
anonymous <farnaz28.rahman@gmail.com>
2026-07-07 12:19:49 +0600

    override energycharge,fixedcharge in CommercialConnection

---
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -0,0 +1,17 @@
+public class CommercialConnection extends Connection{
+    public CommercialConnection(Meter meter){
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge() {
+        int units=getUnitsConsumed();
+        if (units <= 100) return units * 9.0;
+        return 100 * 9.0+ (units - 100) * 13.0;
+    }
+
+    @Override
+    public double fixedCharge() {
+        return 500.0;
+    }
+}
