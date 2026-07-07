2aa2bebe97a42053a1ff40239cd6d9d5c64de35e
Sadman Raiyan <kamrulseed@gmail.com>
2026-07-07 09:04:23 +0600

    Added Connection.java class with necessary fields and methods

---
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -0,0 +1,19 @@
+public abstract class Connection {
+    private Meter meter;
+    private static final double FUEL_DEFAULT = 0.10;
+    private static final double TAX_RATE = 0.05;
+
+    public Connection(Meter meter) {
+        this.meter = meter;
+    }
+
+    public abstract double energyCharge();
+    public abstract double fixedCharge();
+
+    public double fuelSurcharge(){
+        return energyCharge() * FUEL_DEFAULT;
+    }
+    public double tax(){
+        return (energyCharge() + fixedCharge() + fuelSurcharge()) * TAX_RATE;
+    }
+}
