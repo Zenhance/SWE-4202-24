a720b7ff5c52c32018685929ca0c1e59a582ec94
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-07 09:19:31 +0600

    edited Connection.java

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,7 +1,11 @@
 public abstract class Connection {
 
+    protected Meter meter;
+    private double fuelSurChargePercentage = 0.4;
 
-    public Connection() {
+    public Connection(Meter meter, double fuelSurChargePercentage ) {
+        this.meter = meter;
+        this.fuelSurChargePercentage = fuelSurChargePercentage;
 
     }
 }
