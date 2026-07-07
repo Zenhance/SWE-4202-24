1b11b73be7dbd0cce5ae5d36826f367a6992bdcb
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-07 10:00:21 +0600

    IndustrialConnection class completed

--- Lab-08/GridLedger/src/main/java/IndustrialConnection.java
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -1,2 +1,20 @@
-public class IndustrialConnection {
+public class IndustrialConnection extends Connection{
+    private double Rate = 15.0;
+    private double Fixed_Rate = 2000.0;
+    private Meter meter;
+
+    @Override
+    public double fixedCharge() {
+        return Fixed_Rate;
+    }
+
+    public double energyCharge() {
+        int units = meter.getUnitsConsumed();
+        return units*Rate;
+
+    }
+
+
+
+
 }
