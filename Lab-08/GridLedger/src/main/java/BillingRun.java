1ca4a6caf37b9a2d1087b9b343e2fbbcd62322e5
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-07-07 13:19:57 +0600

    BillingRun initialized

---
+++ Lab-08/GridLedger/src/main/java/BillingRun.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class BillingRun {
+    public void register(Connection c) {
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public double grandTotal() {
+        return 0;
+    }
+
+    public ArrayList<Invoice> execute() {
+        return new ArrayList<>();
+    }
+
+    public void register(Connection res, double v) {
+
+    }
+}
