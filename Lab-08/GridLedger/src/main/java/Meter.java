cce5e86ea54cb2c8b52d4272fc9952b2fe36493d
sameer-abdullah-240042107 <sameerabdullah1117@gmail.com>
2026-07-07 11:05:57 +0600

    meter class

---
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -0,0 +1,31 @@
+package main.java;
+
+public class Meter {
+    private int openingReading;
+    private int closingReading;
+    public Meter(int openingReading,int closingReading){
+        if(closingReading<openingReading) {
+            throw new IllegalArgumentException("Closing Reading can not be greater than Opening Reading");
+        }
+        if(openingReading<0){
+                throw new IllegalArgumentException("Reading can not be negative");
+            }
+        if(closingReading<0){
+            throw new IllegalArgumentException("Reading can not be negative");
+    }
+        this.openingReading=openingReading;
+        this.closingReading=closingReading;
+
+        }
+
+    public int getOpeningReading() {
+        return openingReading;
+    }
+
+    public int getClosingReading() {
+        return closingReading;
+    }
+    public int getUnitConsumed(){
+        return closingReading-openingReading;
+    }
+}
