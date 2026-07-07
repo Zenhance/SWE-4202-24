6279a54069211286df648a7ed92cc4f41af9b5a9
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-07-07 11:48:08 +0600

    meter class getters built

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -18,9 +18,46 @@ public class Meter {
         this.openingReading = openingReading;
 
 
+    }
+    public class Meter {
+
+        private int openingReading;
+        private int closingReading;
+
+        public Meter(int openingReading , int closingReading){
+
+            if(openingReading < 0) {
+                throw new IllegalArgumentException("Opening reading can't be negative");
+            }
+
+            if(openingReading > closingReading){
+                throw new IllegalArgumentException("closingReading can't be less than openingReading");
+            }
+
+
+            this.closingReading = closingReading;
+            this.openingReading = openingReading;
+
+
+        }
+
+    }
+    public int getopeningReading() {
+        return openingReading;
+    }
+
+    public int getclosingReading() {
+        return closingReading;
+    }
+
+    public int getUnitsConsumed(){
+        return closingReading - openingReading;
 
 
     }
+
+
+
 }
 
 
