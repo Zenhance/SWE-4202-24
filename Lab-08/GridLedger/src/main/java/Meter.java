b596642b4fc687f6d781dbf266a4549c2f7ea263
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-07-07 11:10:08 +0600

    creating classes

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -3,6 +3,15 @@ private class Meter{
     private int opening;
     private int closing;
 
+    public Meter(int opening,int closing){
+
+        if(opening<0)
+            throw new IllegalArgumentException("Illegal value for opening")
+
+
+
+    }
+
 
 
 }
