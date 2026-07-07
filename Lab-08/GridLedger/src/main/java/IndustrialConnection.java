fe628c2e9fc377bb67e840c4cca46d8e04c94024
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-07-07 12:03:00 +0600

    Added IndustrialConnection class with essentials

---
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -0,0 +1,24 @@
+public class IndustrialConnection extends Connection
+{
+    private static final double IND_RATE =15.0;
+    private static final double IND_FIXED = 2000.0;
+
+    public IndustrialConnection (Meter meter)
+    {
+        super(meter);
+    }
+
+     @Override
+    public double energyCharge()
+     {
+         return getMeter().getUnitsConsumed()*IND_RATE;
+     }
+    @Override
+
+       public double fixedCharge()
+    {
+        return IND_FIXED;
+    }
+
+
+}
--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -4,7 +4,7 @@ private int openingReading;
 private int closingReading;
 
 
-public Meter (int closingReading,int openingReading) {
+public Meter (int openingReading,int closingReading) {
     if (closingReading < openingReading)
         throw new IllegalArgumentException(" closing reading below the opening reading is a broken record, not a cheap month");
     if (closeReading < 0) throw new IllegalArgumentException("a negative opening reading is impossible");
