321bdf82a3a52c1fa64e5d292cffda0b7a411b58
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-07 10:26:02 +0600

    change : add super and overrides to LifelineConnection  class

--- Lab-08/GridLedger/src/main/java/LifelineConnection.java
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -1,4 +1,10 @@
-public class LifelineConnection {
+public class LifelineConnection extends ResidentialConnection {
     public LifelineConnection(Meter meter) {
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge() {
+        return super.energyCharge() * (1.0 - 0.30);
     }
 }
