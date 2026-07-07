54c6596d4d9a536778d259d4bb4cf3373be431da
37-inan <inanjaki184@gmail.com>
2026-07-07 12:03:20 +0600

    class

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -1,2 +1,26 @@
 public class Meter {
+private int OpeningReading;
+private int ClosingReading;
+
+public Meter(int OpeningReading, int ClosingReading){
+    if(OpeningReading<0){
+        throw new IllegalArgumentException("Opening reading has to be positive");
+    }
+    this.OpeningReading=OpeningReading;
+    if(ClosingReading<OpeningReading){
+        throw new IllegalArgumentException("Closing reading must be greater or equal to opening reading");
+    }
+    this.ClosingReading=ClosingReading;
+}
+public int getOpeningReading(){
+    return this.OpeningReading;
+    }
+
+    public int getClosingReading(){
+    return this.ClosingReading;
+    }
+
+    public int getUnitsConsumed(){
+    return this.ClosingReading-this.OpeningReading;
+    }
 }
