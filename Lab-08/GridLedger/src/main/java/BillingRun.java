e6b4e2aac8ca43972f3f861f1dffc42876c6231d
wasifjafar <wasifjafar71@gmail.com>
2026-07-07 09:07:13 +0600

    create BillingRun class

---
+++ Lab-08/GridLedger/src/main/java/BillingRun.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class BillingRun {
+
+    private ArrayList<Connection> connections;
+
+    public BillingRun() {
+        connections = new ArrayList<>();
+    }
+}
+
+
