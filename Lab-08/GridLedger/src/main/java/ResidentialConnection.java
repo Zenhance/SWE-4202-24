aa5ca9570074ae1774d6a65003d4bbd9d60a6a7b
Sadman Raiyan <kamrulseed@gmail.com>
2026-07-07 09:24:41 +0600

    Added ResidentialConnection.java class with necessary fields and methods

---
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -0,0 +1,29 @@
+public class ResidentialConnection extends Connection{
+    private static final double RES_FIXED=100.0;
+    private static final double RES_RATE_1 = 4.0;
+    private static final double RES_RATE_2 = 7.0;
+    private static final double RES_RATE_3 = 11.0;
+    private Meter meter;
+    public ResidentialConnection(Meter meter) {
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge() {
+        int units= meter.getUnitsConsumed();
+        if (units <= 50) {
+            return units * RES_RATE_1;
+        }
+        else if(units<=200) {
+            return 50 * RES_RATE_1 + (units - 50) * RES_RATE_2;
+        }
+        else {
+            return 50 * RES_RATE_1 + 150 * RES_RATE_2 + (units - 200) * RES_RATE_3;
+        }
+    }
+
+    @Override
+    public double fixedCharge() {
+        return RES_FIXED;
+    }
+}
