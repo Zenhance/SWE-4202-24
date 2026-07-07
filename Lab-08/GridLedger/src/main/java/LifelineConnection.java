f751ad25b61c72ef6afa386d4ad479d9166f2f70
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-07 09:14:22 +0600

    added constructor to LifelineConnection class

--- Lab-08/GridLedger/src/main/java/LifelineConnection.java
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -1,3 +1,7 @@
 public class LifelineConnection extends Connection{
+    private static final double LIFELINE_REBATE = 0.30;
 
-}
+    public LifelineConnection(Meter meter){
+        super(meter);
+    }
+    }
