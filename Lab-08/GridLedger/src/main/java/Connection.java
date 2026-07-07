a4f7022b5d5ccc1e45754bc1c27c55f1521363bd
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-07 09:32:07 +0600

    Completed the connection  class with all required items observing the bill

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,5 +1,20 @@
 public abstract class Connection {
-    private Meter meter;
+    private final Meter meter;
     private double fuelPercent=.10;
     public Connection(Meter meter){
+        this.meter=meter;
+    }
+    public Meter getMeter(){
+        return meter;
+    }
+    public void setFuelPercent(double fuelPercent) {
+        this.fuelPercent = fuelPercent;}
+    public abstract double energyCharge();
+    public abstract double fixedCharge();
+    public double furlSurcharge(){
+        return energyCharge()*fuelPercent;}
+    public double tax(){
+        return (energyCharge()+fixedCharge()+furlSurcharge()) * 0.05;}
+    public double total(){
+        return energyCharge()+fixedCharge()+furlSurcharge()+tax();}
     }
