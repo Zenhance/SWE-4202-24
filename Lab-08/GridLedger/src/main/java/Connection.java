6ec918b0fd143f60fbb8a57b9241c42f5164fbdc
arnoboic <arnobarafat@iut-dhaka.edu>
2026-07-07 11:12:13 +0600

    finished Connection abstract Class

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,2 +1,14 @@
-public class Connection {
+public abstract class Connection {
+    protected Meter meter;
+    protected double fuelPercent = 0.10;
+
+    public Connection(Meter meter){
+        this.meter = meter;
+    }
+
+    public abstract double energyCharge();
+    public abstract double fixedCharge();
+    public abstract double fuelSurcharge();
+    public abstract double tax();
+    public abstract double total();
 }
