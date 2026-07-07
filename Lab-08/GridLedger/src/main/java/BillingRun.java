c74115b5ae87a85dcf067a3ad56becd2037b2770
tryhardFuad <mubtasimsmail@gmail.com>
2026-07-07 09:33:25 +0600

    created size and grandTotal methods for BillingRun class

--- Lab-08/GridLedger/src/main/java/BillingRun.java
+++ Lab-08/GridLedger/src/main/java/BillingRun.java
@@ -12,4 +12,16 @@ public class BillingRun {
         c.changeFuelSurcharge(nfs);
         connections.add(c);
     }
+
+    public int size(){
+        return connections.size();
+    }
+
+    public double grandTotal(){
+        double sum = 0.0;
+        for(Connection c : connections){
+            sum += c.total();
+        }
+        return sum;
+    }
 }
