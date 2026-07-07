e58f7b658a0fe73b64c7a23f3b70775a497d8ac0
nusra <nusratnishi1103@gmail.com>
2026-07-07 12:03:24 +0600

    FIRST

---
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -0,0 +1,12 @@
+public class LifelineConnection extends ResidentialConnection{
+    private static final double REBATE_PERCENTAGE=0.30;
+
+    public LifelineConnection(Meter meter){
+        super(meter);
+    }
+
+    public double energyCharge(){
+        double baseResidentialCharge=super.energyCharge();
+        return baseResidentialCharge*(1.0-REBATE_PERCENTAGE);
+    }
+}
