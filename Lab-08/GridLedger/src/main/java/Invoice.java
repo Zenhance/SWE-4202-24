e60cca0a2a1b4913d475d9f6605abec9c792e51c
sabbir240042147 <eftesabbir2725@gmail.com>
2026-07-07 12:50:50 +0600

    invoice class

---
+++ Lab-08/GridLedger/src/main/java/Invoice.java
@@ -0,0 +1,18 @@
+package main.java;
+
+public class Invoice {
+    private Connection connection;
+    private double total;
+    public Invoice(Connection connection){
+        this.connection=connection;
+        this.total=connection.total();
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+}
