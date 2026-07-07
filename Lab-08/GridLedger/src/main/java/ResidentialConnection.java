f0731a1f4ccfec7287bcda98444df87b9498fdbc
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-07 10:10:54 +0600

    change : add override conditions and write the ResidentialConnection class

--- Lab-08/GridLedger/src/main/java/ResidentialConnection.java
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -1,4 +1,18 @@
-public class ResidentialConnection {
+public class ResidentialConnection extends Connection {
     public ResidentialConnection(Meter meter) {
+        super(meter);
     }
+
+    @Override
+    public double energyCharge() {
+        int units = getUnits();
+        if (units <= 50) {
+            return units * 4.0;
+        }
+        if (units <= 200) {
+            return (50 * 4.0) + ((units - 50) * 7.0);
+        }
+        return (50 * 4.0) + (150 * 7.0) + ((units - 200) * 11.0);
+    }
+
 }
