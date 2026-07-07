0049c0d315e99b5eb23c2517820158e9389b73e7
wasifjafar <wasifjafar71@gmail.com>
2026-07-07 09:50:46 +0600

    fix ResidentialConnection class's energy method

--- Lab-08/GridLedger/src/main/java/ResidentialConnection.java
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -6,7 +6,17 @@ public class ResidentialConnection extends Connection{
 
     @Override
     public double energyCharge() {
-        return 0;
+        int units = super.getMeter().getUnitsConsumed();
+        double subtotal = 0;
+
+        if (units <= 50)
+            subtotal += units * 4.0;
+        else if (units > 50 && units <= 200) {
+            subtotal += ((50 * 4.0) + ((units - 50) * 7.0));
+        }
+        else
+            subtotal += ((50 * 4.0) + (150 * 7.0) + ((units - 200) * 11.0));
+        return subtotal;
     }
 
     @Override
