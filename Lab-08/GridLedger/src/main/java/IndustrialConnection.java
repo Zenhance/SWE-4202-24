94038c77884235853291af8f5e92c219e4724a44
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-07 10:05:23 +0600

    completed INDUSTRIAL connection and implemented the the INDUSTRIAL connection formula.

---
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -0,0 +1,16 @@
+public class IndustrialConnection extends Connection {
+    private static final double RATE_PER_UNIT= 15.0;
+    private static final double FIXED_CHARGE= 500.0;
+    public IndustrialConnection(Meter meter) {
+        super(meter);
+    }
+    @Override
+    public double energyCharge() {
+        return unitsConsumed() * RATE_PER_UNIT;
+    }
+
+    @Override
+    public double fixedCharge() {
+        return FIXED_CHARGE;
+    }
+}
