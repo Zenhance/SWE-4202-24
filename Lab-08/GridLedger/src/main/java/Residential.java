4f7fc460dd5c09ecc7a91d9b7e4200385a1c17b9
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-07 09:11:47 +0600

    init commit

--- Lab-08/GridLedger/src/main/java/Residential.java
+++ Lab-08/GridLedger/src/main/java/Residential.java
@@ -2,7 +2,13 @@ public class Residential extends Connection{
     public Residential (Meter meter){
         super(meter);
     }
-
+    @Override
+    public double energyCharge(){
+        int units= meter.getUnitsConsumed();
+        double charge=0;
+if(units<=50){
+charge = units * 4.0;
+}
 
 
 
