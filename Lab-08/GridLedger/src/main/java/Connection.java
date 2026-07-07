a2f77c928aadc931e5f2555e757f725e49b7e681
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-07 09:30:16 +0600

    initiating the meter in the connection. connection has a meter. so it takes the value from meter.

---
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -0,0 +1,22 @@
+public abstract class Connection {
+    private final Meter meter;
+
+    private double fuelSurchargeRate;
+
+    protected static final double DEFAULT_FUEL_SURCHARGE_RATE = 0.0;
+    protected static final double TAX_RATE = 0.05;
+
+    public Connection(Meter meter) {
+        if (meter == null) {
+            throw new IllegalArgumentException("Connection must have a meter.");
+        }
+
+        this.meter = meter;
+        this.fuelSurchargeRate = DEFAULT_FUEL_SURCHARGE_RATE;
+    }
+
+    public Connection() {
+
+        meter = null;
+    }
+}
