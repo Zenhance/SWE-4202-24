94dc4b42545958843722416b443f7fe0d9831e05
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-07 09:20:15 +0600

    init commit

--- Lab-08/GridLedger/src/main/java/Lifeline.java
+++ Lab-08/GridLedger/src/main/java/Lifeline.java
@@ -3,4 +3,9 @@ public class Lifeline extends Residential{
     public Lifeline(Meter meter){
         super(meter);
     }
+
+    @Override
+    public double energyCharge() {
+        return super.energyCharge()(super.energyCharge()*rebate);
+    }
 }
