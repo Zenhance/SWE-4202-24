99d1e8770be3d12f229dc9baa8e476e3953f3baa
23-Fahad <ifahad2k@gmail.com>
2026-07-07 11:54:47 +0600

    build ResidentialConnection.java

---
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -0,0 +1,29 @@
+package main.java;
+
+import java.Meter;
+
+public class ResidentialConnection extends Connection{
+    private Meter  meter;
+    public ResidentialConnection(Meter  meter){
+        super(meter);
+    }
+    public double energyCharge(){
+        int units = unitConsumed();
+        double c = 0;
+        if (units <= 50) {
+            c += units * 4.0;
+        } else {
+            c += 50 * 4.0;
+            if (units <= 200) {
+                c += (units - 50) * 7.0;
+            } else {
+                c += 150 * 7.0;
+                c += (units - 200) * 11.0;
+            }
+        }
+        return c;
+    }
+    public double fixedCharge(){
+        return 100.0;
+    }
+}
