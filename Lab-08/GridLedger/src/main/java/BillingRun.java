4c091728508f21c60e7ccc3f50987c0dcab4e14b
ayiham-1012 <ayiham1012@gmail.com>
2026-07-07 11:27:33 +0600

    BillingRun: modifications

--- Lab-08/GridLedger/src/main/java/BillingRun.java
+++ Lab-08/GridLedger/src/main/java/BillingRun.java
@@ -1,17 +1,31 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class BillingRun{
 
+    List<Connection> connections = new ArrayList<>(); //just found out how List works :0
+    List<Invoice> invoices = new ArrayList<>();
 
-    public int size(){return 0;}
+    public List<Invoice> execute(){
+
+        return invoices;
+    }
+
+
+
+    public int size(){return invoices.size();}
 
     public int grandTotal(){return 0;}
 
-    public void register(Connection connection){ }
+    public void register(Connection connection){
+        connections.set(connections.size(), connection);
+    }
 
-    public void register(Connection connection, double d){}
+    public void register(Connection connection, double d){
+        connections.set(connections.size(), connection);
+    }
 
-    public List<Invoice> execute(){return null;}
+
 
 
 
