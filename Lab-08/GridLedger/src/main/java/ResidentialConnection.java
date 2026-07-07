877694bd6f37721eaf3b8a6ebc6e25b3febeb085
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-07 09:44:06 +0600

    Created a residentialconnection constructor and implemented all the reqirements and completed the class.

--- Lab-08/GridLedger/src/main/java/ResidentialConnection.java
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -2,4 +2,19 @@ public class ResidentialConnection extends Connection {
     public ResidentialConnection(Meter meter){
         super(meter);
     }
+    @Override
+    public double energyCharge(){
+        int units= getMeter().getUnitsConsumed();
+        if (units<= 50){
+            return units * 4.0;
+        }
+        if (units<= 200){
+            return (50 * 4.0) + ((units - 50) * 7.0);
+        }
+        return (50 * 4.0)+(150 * 7.0)+ ((units - 200) * 11.0);
+    }
+    @Override
+    public  double fixedCharge(){
+        return 100.0;
+    }
 }
