34c326a57d1cae4695ec47767776026b449f8730
jasin <anishaajasin.13.04.2005@gmail.com>
2026-07-07 12:03:45 +0600

    Commercialconnection class updates

--- Lab-08/GridLedger/src/main/java/CommercialConnection.java
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -1,2 +1,27 @@
-public class CommercialConnection {
+public class CommercialConnection extends Connection {
+    private static final double rate1 = 9.0;
+    private static final double rate2 = 13.0;
+    private static final double  FIXED_CHARGE_RATE = 500;
+
+    public CommercialConnection(Meter meter) {
+        super(meter);
+    }
+
+
+    public double energyCharge() {
+        int units = meter.getUnitsConsumed();
+
+        if(units <= 100){
+            return units*rate1;
+        }
+
+            return 100*rate1 + (units-100)*rate2;
+
+    }
+
+    @Override
+    public double fixedCharge() {
+        return FIXED_CHARGE_RATE;
+    }
+
 }
