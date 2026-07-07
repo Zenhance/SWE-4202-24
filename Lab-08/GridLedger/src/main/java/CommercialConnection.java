926945eb0e123e030b99fbb599f42f463958bb52
tasnitaraida <tasnitar@gmail.com>
2026-07-07 12:46:23 +0600

    fixed

--- Lab-08/GridLedger/src/main/java/CommercialConnection.java
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -6,7 +6,7 @@ public class CommercialConnection extends Connection{
     public double energyCharge(){
         int units = units();
         if(units<=100){
-            return units*9.0
+            return units*9.0;
         }return 100*9.0+(units-100)*13.0;
     }
 
