b9c7706b4a70e2f301dd734ff907459608445e1b
tryhardFuad <mubtasimsmail@gmail.com>
2026-07-07 08:41:37 +0600

    created constructor for meter class

---
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -0,0 +1,15 @@
+public class Meter {
+    private final int openingReading;
+    private final int closingReading;
+
+    public Meter(int openingReading, int closingReading) {
+        if(openingReading < 0 || closingReading < 0) {
+            throw new IllegalArgumentException("meter readings can't be negative");
+        }
+        if(closingReading < openingReading) {
+            throw new IllegalArgumentException("meter can't run backwords");
+        }
+        this.openingReading = openingReading;
+        this.closingReading = closingReading;
+    }
+}
