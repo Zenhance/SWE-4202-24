d82bf11654a3ca677fdda69d5387bc940c5c9a36
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-07 09:32:45 +0600

    init commit

--- Lab-08/GridLedger/src/main/java/Industrial.java
+++ Lab-08/GridLedger/src/main/java/Industrial.java
@@ -2,4 +2,13 @@ public class Industrial extends Connection{
     public Industrial(Meter meter){
         super(meter);
     }
+    @Override
+    public double energyCharge(){
+        int units= meter.getUnitsConsumed();
+        return units*15.0;
+    }
+    @Override
+    public double fixedCharge(){
+        return 1000;
+    }
 }
