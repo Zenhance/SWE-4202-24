2f1201939dffecf6ad4ccd6c4eb77455a97518b3
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-07 09:20:30 +0600

    Connection class completed

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -2,8 +2,20 @@ public abstract class Connection {
     public double Fuel_Charge = 0.10;
     public double Tax = 0.05;
 
-    public abstract double fuelSurcharge();
+    public abstract double fixedCharge();
     public abstract double energyCharge();
+
+    public double fuelSurcharge() {
+        return energyCharge()*Fuel_Charge;
+    }
+
+    public double tax() {
+        return (energyCharge()+fixedCharge()+fuelSurcharge())*Tax;
+    }
+
+    public double total() {
+        return (energyCharge()+fixedCharge()+fuelSurcharge()+tax());
+    }
 
 
 
