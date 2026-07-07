5a1e74ba558b1b93dacb7e335b2c94c9db0668f3
37-inan <inanjaki184@gmail.com>
2026-07-07 13:01:10 +0600

    1

--- Lab-08/GridLedger/src/main/java/Invoice.java
+++ Lab-08/GridLedger/src/main/java/Invoice.java
@@ -1,2 +1,17 @@
 public class Invoice {
+    private Connection connection;
+    private double total;
+
+    public Invoice(Connection connection){
+        this.connection=connection;
+        this.total=connection.total();
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public double getTotal() {
+        return total;
+    }
 }
