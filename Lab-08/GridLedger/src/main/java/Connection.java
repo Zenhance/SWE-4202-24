96620676a0d08f9e91a87a883bb6c1aa8c189d86
jasin <anishaajasin.13.04.2005@gmail.com>
2026-07-07 11:37:23 +0600

    Connection class updates

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,3 +1,22 @@
-public class Connection {
+public abstract class Connection {
+     protected Meter meter;
+     protected double fualPercentage = 0.1;
+     protected static final double TAX_RATE = 0.05;
 
-}
+     public Connection(Meter meter) {
+         this.meter = meter;
+     }
+            public abstract double energyCharge();
+            public abstract double fixedCharge ();
+
+            public double fuelSurcharge () {
+                return energyCharge()+ fualPercentage;
+          }
+
+            public double tax () {
+                return (energyCharge()+ fualPercentage)*TAX_RATE;
+        }
+            public double total () {
+                return energyCharge()+ fixedCharge() + fuelSurcharge() + tax();
+             }
+        }
