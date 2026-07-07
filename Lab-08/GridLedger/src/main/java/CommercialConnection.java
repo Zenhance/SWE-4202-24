ba81fd06f4f3e0cab68aa59a79f653ceae4a831a
tryhardFuad <mubtasimsmail@gmail.com>
2026-07-07 09:17:04 +0600

    created energyCharge  method for CommercialConnection class

---
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -0,0 +1,11 @@
+public class CommercialConnection extends Connection{
+    public double energyCharge(){
+        int units = meter.getUnitsConsumed();
+        if(units <= 100){
+            return units * 9.0;
+        }
+        return 100 * 9.0 + (units - 100) * 13.0;
+    }
+
+
+}
