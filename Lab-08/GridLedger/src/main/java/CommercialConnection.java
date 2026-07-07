b268746a42c29a764a421ec26f5ddd3beae2b1d3
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-07-07 10:05:59 +0600

    commercial connection class has been created using the same features and energycharge method not updated, the method is used that has been directly given in the question

---
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -0,0 +1,20 @@
+public class CommercialConnection extends Connection{
+
+    public CommercialConnection(Meter meter){
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge() {
+        units = meter.getUnitsConsumed();
+        double c = 0;
+        if ( units <= 100) c += units * 9.0;
+        else { c += 100 * 9.0; c += ( units -100) * 13.0; }
+        return c ;
+    }
+
+    @Override
+    public double fixedCharge() {
+        return 500.0;
+    }
+}
