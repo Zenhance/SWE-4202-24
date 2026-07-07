d24e7bb6281adc3ac82343e0888fb9c6559f63be
arnoboic <arnobarafat@iut-dhaka.edu>
2026-07-07 10:58:13 +0600

    made constructor for Meter and created Invoice

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -1,6 +1,16 @@
 public class Meter {
 
     private int previousReading;
-    private int currentReadin
+    private int currentReading;
+
+    public Meter(int previousReading, int currentReading){
+        if(previousReading < 0) throw new IllegalArgumentException("previousReading cannot be negative");
+        if (previousReading > currentReading) throw new IllegalArgumentException(("currentReading cannot be more than previousReadin");
+
+        this.previousReading = previousReading;
+        this.currentReading = currentReading;
+    }
+
+
 
 }
