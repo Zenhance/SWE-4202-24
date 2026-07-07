c0a3b4878885f227fefc1a408af0f66333f948dd
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-07 09:57:16 +0600

    Completed the Commercial Connection class

--- Lab-08/GridLedger/src/main/java/CommercialConnection.java
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -2,6 +2,13 @@ public class CommercialConnection extends Connection{
     public CommercialConnection(Meter meter){
         super(meter);
     }
-
-
+    @Override
+    public double energyCharge(){
+        int units = getMeter().getUnitsConsumed();
+        if (units<= 100){
+            return units * 9.0;}
+        return (100 * 9.0) + ((units - 100) * 13.0);}
+    @Override
+    public double fixedCharge(){
+        return 500.0;}
 }
