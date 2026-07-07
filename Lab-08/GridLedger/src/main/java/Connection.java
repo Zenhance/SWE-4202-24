a33f6a8d5d72922732d65d7ec48a37db8f7b23ea
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-07-07 11:24:04 +0600

    connection class

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -11,8 +11,16 @@ public abstract class Connection{
     public abstract double fixedCharge();
 
     public double fuelSurcharge(){
-        return this.energyCharge() * 0.50;
+        return this.energyCharge() * 0.10;
     }
+
+    public double tax(){
+
+
+        return
+    }
+
+
 
 
 
