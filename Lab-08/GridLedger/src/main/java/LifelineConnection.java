a8300cf821898e4f00dad33af14588220b9f7dcd
Rakin <rakinhasan001@gmail.com>
2026-07-07 12:47:58 +0600

    LifelineConnection class built

--- Lab-08/GridLedger/src/main/java/LifelineConnection.java
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -1,5 +1,12 @@
 public class LifelineConnection extends ResidentialConnection{
+    private static final double REBATE=0.3;
     public LifelineConnection(Meter meter) {
         super(meter);
+    }
+
+    @Override
+    public double energyCharge() {
+        double residentialCharge=super.energyCharge();
+        return residentialCharge-(residentialCharge*REBATE);
     }
 }
