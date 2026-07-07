eaf92557d1665b4692cd8211bf57edc0f6068b38
ahnaf-code <ahnafcode@gmail.com>
2026-07-07 12:55:56 +0600

    update IndustrialConnection.java

---
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -0,0 +1,9 @@
+public class IndustrialConnection extends Connection {
+    @Override
+    public double energyCharge() {
+        if (units <= 100)
+            c += units * 9.0;
+        else { c += 100 * 9.0; c += (units-100) * 13.0; }
+        return C;
+    }
+}
