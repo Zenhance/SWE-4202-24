3b34687d767e59098bffaae45a4b9e445288c2e9
hossdia <hossainmahdia22@gmail.com>
2026-07-07 13:03:19 +0600

    connection commercial

--- Lab-08/GridLedger/src/main/java/IndustrialConnection.java
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -4,4 +4,8 @@ public class IndustrialConnection extends Connection{
     public IndustrialConnection(Meter meter){
         super(meter, FIXED_CHARGE);
     }
+    @Override
+    public double energyCharge(){
+        return getUnitsConsumed()*RATE;
+    }
 }
