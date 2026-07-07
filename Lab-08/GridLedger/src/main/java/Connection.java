878edae27521aed1de288f9b3e793c993225b03d
Zunied-57 <zunied0411@gmail.com>
2026-07-07 09:14:19 +0600

    constructor created

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -5,6 +5,11 @@ public abstract class Connection
 
     private Meter meter;
 
+    public Connection(Meter meter)
+    {
+        this.meter = meter;
+    }
+
     public abstract double energyCharge();
     public abstract double fixedCharge();
     public double fuelSurcharge()
