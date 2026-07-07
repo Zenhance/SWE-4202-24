1826a545b5f06f153d2771d428b6618cb8ace31e
tanbirulislam-hash <tanbirulislam@iut-dhaka.edu>
2026-07-07 11:08:45 +0600

    problem

--- Lab-04/CineCart/src/main/java/model/Ticket.java
+++ Lab-04/CineCart/src/main/java/model/Ticket.java
@@ -1,10 +1,10 @@
 package model;
 
-public class Ticket {
-    private Showtime showtime;
-    private int row;
-    private int col;
-    private double pricePaid;
+public class abstract class Ticket implements LineItem {
+    private final Showtime showtime;
+    private final int row;
+    private final int col;
+    private final double pricePaid;
 }
 
 public Ticket{
@@ -13,13 +13,18 @@ public Ticket{
         this.col=col;
         this.pricePaid=pricePaid;}
 
+protected double peakMultiplier(){
+
 public Showtime getShowtime(){
     return Showtime;}
-public int getRow(){return row;}
-public int getCol(){return col;}
-public double getPricePaid(){return pricePaid;}
+public int getRow(){
+    return row;}
+public int getCol(){
+    return col;}
+public double getPricePaid(){
+    return pricePaid;}
 
-}
+
 public String toString() {
     return String.format(
             "T%d - R%dC%dC%BDT %.2f", showtime.getId(), row, col, pricePaid);}
