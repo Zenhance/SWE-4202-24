ceb9bf89ec016c1288a51aa8f82fadc07d612458
Maomao-starry <aminarahmanshalu@gmail.com>
2026-07-07 12:31:30 +0600

    task

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -20,7 +20,7 @@ public class Meter {
     public int getClosing_reading(){
         return closing_reading;
     }
-    public int getUnitsConsumed(int closing_reading, int opening_reading){
+    public int getUnitsConsumed(){
         return closing_reading-opening_reading;
     }
 
