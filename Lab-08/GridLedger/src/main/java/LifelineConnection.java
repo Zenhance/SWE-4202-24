45fbe92f46035b5bc8af4dbc2fe0a7af25638f08
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-07 10:04:15 +0600

    LifelineConnection class initiated

--- Lab-08/GridLedger/src/main/java/LifelineConnection.java
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -1,2 +1,9 @@
-public class LifelineConnection {
+public class LifelineConnection extends ResidentialConnection{
+    private double Rebate = 0.3;
+
+
+    @Override
+    public double energyCharge() {
+        return super.energyCharge() * (1.0 - Rebate);
+    }
 }
