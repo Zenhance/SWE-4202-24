f293c3ecddb08989d7454b03cdcfc60412697eed
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-07 09:04:38 +0600

    created a constructor with getters... and completes the meter file...

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -1,14 +1,26 @@
 public class Meter {
     private int openingReading;
     private int closingReading;
-    public Meter(int openingReading,int closingReading){
-        if (openingReading<0 || closingReading<0){
+    public Meter(int openingReading,int closingReading) {
+        if (openingReading < 0 || closingReading < 0) {
             throw new IllegalArgumentException("Reading cannot be negative");
         }
-        if (openingReading>closingReading){
+        if (openingReading > closingReading) {
             throw new IllegalArgumentException("Closing reading cannot be smaller than opening reading");
         }
-
-
+        this.openingReading = openingReading;
+        this.closingReading = closingReading;
     }
+
+
+    public int getOpeningReading() {
+        return openingReading;
+    }
+
+    public int getClosingReading() {
+        return closingReading;
+    }
+    public int getUnitsConsumed(){
+        return closingReading-openingReading;
+}
 }
