e4c708614a9ec736e37b332f4df946fda799b233
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-07-07 12:08:41 +0600

    fixed Meter class

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -1,7 +1,5 @@
 public class Meter {
-    private final int openingReading;
-    private final int closingReading;
-    private int unitsConsumed;
+    private final int unitsConsumed;
 
     public Meter(int openingReading, int closingReading) {
         if (openingReading < 0 || closingReading < 0) {
@@ -10,18 +8,10 @@ public class Meter {
         if(closingReading < openingReading){
             throw new IllegalArgumentException();
         }
-        this.openingReading = openingReading;
-        this.closingReading = closingReading;
 
         this.unitsConsumed = openingReading - closingReading;
     }
 
-    /*public int getOpeningReading() {
-        return openingReading;
-    }
-    public int getClosingReading() {
-        return closingReading;
-    }*/
     public int getUnitsConsumed() {
         return unitsConsumed;
     }
