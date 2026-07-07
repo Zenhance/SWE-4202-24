7ab72266ab1e6ada52dbd8fb6a45e7dfdd8764d3
23-Fahad <ifahad2k@gmail.com>
2026-07-07 11:16:42 +0600

    inital meter.java

---
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -0,0 +1,28 @@
+package java;
+
+public class Meter {
+    private final int  currentReading;
+    private final int  previousReading;
+
+    public Meter(int currentReading, int previousReading){
+        if(currentReading<0 || previousReading<0){
+            throw new IllegalArgumentException("Reading cant be negative.")
+        }
+        if(currentReading<previousReading){
+            throw new IllegalArgumentException("Usages cant be negative");
+        }
+        this.currentReading = currentReading;
+        this.previousReading = previousReading;
+    }
+
+    public int getCurrentReading() {
+        return currentReading;
+    }
+
+    public int getPreviousReading() {
+        return previousReading;
+    }
+    public int totalUsages(){
+        return previousReading-currentReading;
+    }
+}
