c35647627fc086279ba8e61fe65c674f37deab41
Muhtasim Moin <muhtasi13@gmail.com>
2026-07-07 09:35:17 +0600

    LifelineConnection class updated

--- Lab-08/GridLedger/src/main/java/LifelineConnection.java
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -1,2 +1,11 @@
-public class LifelineConnection extends Connection{
+public class LifelineConnection extends ResidentialConnection{
+    private static final double LIFELINE_REBATE = 0.30;
+    public LifelineConnection(Meter meter){
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge() {
+        return super.energyCharge()*(1-LIFELINE_REBATE);
+    }
 }
