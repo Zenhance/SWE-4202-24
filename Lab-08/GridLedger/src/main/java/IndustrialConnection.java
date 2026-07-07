432923bfd85282f35b3cd43dfde8a1f4d615df79
jasin <anishaajasin.13.04.2005@gmail.com>
2026-07-07 12:08:17 +0600

    Industrialconnection class updates

--- Lab-08/GridLedger/src/main/java/IndustrialConnection.java
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -1,2 +1,18 @@
-public class IndustrialConnection {
+public class IndustrialConnection extends Connection {
+    private static final double rate = 15.0;
+    private static final double FIXED_CHARGE_RATE = 2000;
+
+    public IndustrialConnection(Meter meter) {
+        super(meter);
+    }
+
+    public double energyCharge() {
+        int units = meter.getUnitsConsumed();
+        return units*rate;
+    }
+
+    @Override
+    public double fixedCharge() {
+        return FIXED_CHARGE_RATE;
+    }
 }
