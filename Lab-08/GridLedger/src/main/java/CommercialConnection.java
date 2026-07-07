633cd7002a08ac5c9330beb3c0c3537f40fd53ca
Zunied-57 <zunied0411@gmail.com>
2026-07-07 09:31:26 +0600

    all methods added

---
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -0,0 +1,31 @@
+public class CommercialConnection extends Connection
+{
+    private static final double COM_RATE_1 = 9.0;   // first 100 units
+    private static final double COM_RATE_2 = 13.0;  // units above 100
+    private static final double COM_FIXED  = 500.0;
+
+    public CommercialConnection(Meter meter)
+    {
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge()
+    {
+        int unit = getMeter().getUnitsConsumed();
+        if(unit<=100)
+        {
+            return unit*COM_RATE_1;
+        }
+        else
+        {
+            return 100*COM_RATE_1+(unit-100)*COM_RATE_2;
+        }
+    }
+
+    @Override
+    public double fixedCharge()
+    {
+        return COM_FIXED;
+    }
+}
