9e90d051bf700b40cd7c1e02341712ada6bc7115
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-07 09:18:07 +0600

    added constructor to invoice class

--- Lab-08/GridLedger/src/main/java/Invoice.java
+++ Lab-08/GridLedger/src/main/java/Invoice.java
@@ -1,2 +1,9 @@
 public class Invoice {
+    private final Connection connection;
+    private final double total;
+
+    public Invoice(Connection connection){
+        this.connection = connection;
+        this.total = connection.total();
+    }
 }
