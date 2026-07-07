b9d473081a88c13a79b51234388cee294b1418ea
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-07 09:12:04 +0600

    added methods to IndustrialConnection class

--- Lab-08/GridLedger/src/main/java/IndustrialConnection.java
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -1,2 +1,13 @@
 public class IndustrialConnection extends Connection{
+    public IndustrialConnection(Meter meter){
+        super(meter);
+    }
+    @Override
+    public double energyCharge(){
+        return units()*15.0;
+    }
+    @Override
+    public double fixedCharge(){
+        return 2000.0;
+    }
 }
