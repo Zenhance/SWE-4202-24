71b6e3d9c1b06c15eeea56d50af292e046403bae
Munman <214246799+bollamokles@users.noreply.github.com>
2026-07-07 09:07:15 +0600

    commit connection abstract class Coustomer

---
+++ Lab-08/GridLedger/src/main/java/Coustomer.java
@@ -0,0 +1,13 @@
+public abstract class Coustomer {
+    public static final double FUEL_DEFAULT = 0.10;
+    public static final double TAX_RATE = 0.05;
+
+    private Meter meter;
+
+    public abstract double energyCharge();
+    public abstract double fixedCharge();
+    public abstract double fuelSurcharge();
+    public abstract double tax();
+    public abstract double total();
+
+}
