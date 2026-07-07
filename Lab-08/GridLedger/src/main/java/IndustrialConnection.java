065b25a18163052ae76cf35cefb555feeaa8b348
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-07 10:04:33 +0600

    Completed Industrial connection class.

--- Lab-08/GridLedger/src/main/java/IndustrialConnection.java
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -2,5 +2,14 @@ public class IndustrialConnection extends Connection{
     public IndustrialConnection(Meter meter){
         super(meter);
     }
+    @Override
+    public double energyCharge(){
+        int units= getMeter().getUnitsConsumed();
+        return units * 15.0;
+    }
+    @Override
+    public  double fixedCharge(){
+        return 2000.0;
+    }
 }
 
