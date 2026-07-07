ac6a55e2b59e29173ca89677b9a9ddaea9b38615
Munman <214246799+bollamokles@users.noreply.github.com>
2026-07-07 09:18:00 +0600

    commit connection abstract class Coustomer(improvement)

---
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -0,0 +1,25 @@
+public abstract class Connection{
+
+    private Meter meter;
+
+    public Connection(Meter meter) {
+        this.meter = meter;
+    }
+
+    public abstract double energyCharge();
+
+    public abstract double fixedCharge();
+
+    public double fuelSurcharge() {
+        return this.energyCharge() * 0.10;
+    }
+
+    public double tax() {
+        double subtotal = this.energyCharge() + this.fixedCharge() + this.fuelSurcharge();
+        return subtotal * 0.05;
+    }
+
+    public double total() {
+        return this.energyCharge() + this.fixedCharge() + this.fuelSurcharge() + this.tax();
+    }
+}
