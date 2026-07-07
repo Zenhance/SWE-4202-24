77ab21f52bb3a3028f8aef042379640e5d12352d
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-07 09:04:39 +0600

    added overridden method to ResidentialConnection class

--- Lab-08/GridLedger/src/main/java/ResidentialConnection.java
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -2,5 +2,16 @@ public class ResidentialConnection extends Connection{
     public ResidentialConnection(Meter mete){
         super(meter);
     }
+    @Override
+    public double energyCharge(){
+        int units = units();
 
+        if (units<=50){
+            return units*4.0;
+        }
+        if(units<=200){
+            return 50*4.0+(units-50)*7.0;
+        }
+        return 50*4.0+150*7.0+(units-200)*11.0;
+    }
 }
