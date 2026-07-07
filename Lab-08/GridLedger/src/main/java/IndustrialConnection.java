94cfdb6c1a207b8a38db9e80ba76cae08fa42110
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-07-07 12:04:16 +0600

    added IndustrialConnection class

---
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -0,0 +1,15 @@
+public class IndustrialConnection extends Connection {
+    public IndustrialConnection(Meter meter) {
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge(){
+        return meter.getUnitsConsumed()*15.0;
+    }
+
+    @Override
+    public double fixedCharge(){
+        return 2000.0;
+    }
+}
