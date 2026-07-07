b270a5edc98181b36a6ad01b8ed94fafc729ff4f
nafisaahmedfaz <nafisaahmed1610@gmail.com>
2026-07-07 12:17:03 +0600

    2nd commit

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,2 +1,32 @@
 public class Connection {
+    private String customerType;
+            private boolean subsided;
+            public Connection (String customerType){
+                this.customerType=customerType;
+                        this.subsided=false;
+            }
+            public String getCustomerType(){
+                return customerType;
+            }
+            public void setCustomerType(String customerType){
+                this.customerType=customerType;
+            }
+            public boolean isSubsided(){
+                return subsided;
+            }
+            public void setSubsided(boolean subsided){
+                this.subsided=subsided;
+            }
+            public double getFixedCharge(){
+                switch (customerType){
+                    case "RESIDENTIAL":
+                        return 100.0;
+                    case "INDUSTRIAL":
+                        return 2000.0;
+                    case "COMMERCIAL":
+                        return  500.0;
+                    default:
+                        return 0.0;
+                }
+            }
 }
