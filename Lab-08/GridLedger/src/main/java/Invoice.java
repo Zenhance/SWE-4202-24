af59bcd778e38d63fa08ba714d570adfa7454464
04-Labiba <sabihalabiba639@gmail.com>
2026-07-07 09:24:36 +0600

    init commit

---
+++ Lab-08/GridLedger/src/main/java/Invoice.java
@@ -0,0 +1,9 @@
+public class Invoice {
+    private final double total;
+    public Invoice(double total){
+        this.total=total;
+    }
+    public double getTotal(){
+        return total;
+    }
+}
