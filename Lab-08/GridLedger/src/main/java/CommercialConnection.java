ac6a373d6eb166866f674bf0857355501640ec48
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-07 10:21:31 +0600

    updated subclasses

--- Lab-08/GridLedger/src/main/java/CommercialConnection.java
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -1,3 +1,14 @@
 public class CommercialConnection extends Connection {
 
+    protected Meter meter;
+    private final double fuelSurChargePercentage;
+
+    public ResidentialConnection(Meter meter, double fuelSurChargePercentage) {
+
+        super(meter, fuelSurChargePercentage);
+    }
+
+    public double energyCharge() {
+
+    }
 }
--- Lab-08/GridLedger/src/main/java/ResidentialConnection.java
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -4,13 +4,32 @@ public class ResidentialConnection extends Connection {
     private double fuelSurChargePercentage;
 
     public ResidentialConnection(Meter meter, double fuelSurChargePercentage) {
+
         super(meter, fuelSurChargePercentage);
     }
 
 
     @Override
-    double energyCharge() {
+    public double energyCharge() {
+        double unit = meter.getUnitsConsumed();
+        double charge = 0;
 
+        if (unit<=50) {
+            charge += unit*4.0;
+
+        }
+        else if (unit > 50 && unit<=200 ) {
+            charge += unit*7.0;
+        }
+
+        else {
+            charge += unit*11.0;
+        }
+        return charge;
     }
+
+
+
+
 
 }
