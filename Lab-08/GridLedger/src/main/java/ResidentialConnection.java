3aed928dd7d06e015956a864e0771d48ace26017
IbCoder25 <alhadjiibrahim500@gmail.com>
2026-07-07 12:16:17 +0600

    implement ResidentialConnection with 3-tier slab math

---
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -0,0 +1,30 @@
+public class ResidentialConnection extends Connection {
+
+    public ResidentialConnection(Meter meter) {
+        super(meter);
+    }
+
+    @Override
+    public double fixedCharge() {
+        return 100.0;
+    }
+
+    @Override
+    public double energyCharge() {
+        int units = meter.getUnitsConsumed();
+        double c = 0;
+
+        if (units <= 50) {
+            c += units * 4.0;
+        } else {
+            c += 50 * 4.0;
+            if (units <= 200) {
+                c += (units - 50) * 7.0;
+            } else {
+                c += 150 * 7.0;
+                c += (units - 200) * 11.0;
+            }
+        }
+        return c;
+    }
+}
