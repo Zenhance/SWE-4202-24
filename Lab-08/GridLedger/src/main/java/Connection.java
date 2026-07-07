b78a622c7ec6e38a71626f026de09bdd494b063c
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-07 09:11:18 +0600

    edited Connection.java

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,10 +1,7 @@
 public abstract class Connection {
 
-    protected double energyCharge;
-    protected double fixedCharge;
-    protected double fuelSurCharge;
-    public  double billTotal;
-    public double tax;
 
-    public Connection(double energyCharge, double fixedCharge)
+    public Connection() {
+
+    }
 }
