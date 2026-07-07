fc31bc9e4b32e96f1718bdf52eadbe695984fe7a
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-07-07 11:45:39 +0600

    meter class constructor built

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -1,4 +1,26 @@
 public class Meter {
+
+    private int openingReading;
+    private int closingReading;
+
+    public Meter(int openingReading , int closingReading){
+
+        if(openingReading < 0) {
+            throw new IllegalArgumentException("Opening reading can't be negative");
+        }
+
+        if(openingReading > closingReading){
+            throw new IllegalArgumentException("closingReading can't be less than openingReading");
+        }
+
+
+        this.closingReading = closingReading;
+        this.openingReading = openingReading;
+
+
+
+
+    }
 }
 
 
