3e7a4a5034958f060b4575204e7fea5add19168a
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-07 10:09:52 +0600

    Completed lifeline connection class.

--- Lab-08/GridLedger/src/main/java/LifelineConnection.java
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -1,2 +1,9 @@
 public class LifelineConnection extends ResidentialConnection{
+    public LifelineConnection(Meter meter){
+        super(meter);
+    }
+    @Override
+    public double energyCharge(){
+        double residentialCharge = super.energyCharge();
+        return residentialCharge - (residentialCharge * 0.30);}
 }
