dfa4e9caaa995f7eb94dd9438adb0b71f6780c74
tasnitaraida <tasnitar@gmail.com>
2026-07-07 12:23:55 +0600

    constructor

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,3 +1,10 @@
 public abstract class Connection {
-
+    private Meter meter;
+    private double fuelPercentage=0.0;
+    public Connection(Meter meter){
+        if(meter==null){
+            throw new IllegalArgumentException("Meter required");
+        }
+        this.meter=meter;
+    }
 }
