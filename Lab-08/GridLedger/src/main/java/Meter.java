06aab7ee9f321bb0d30ef88d2f83b747bd3a6897
sabbir240042147 <eftesabbir2725@gmail.com>
2026-07-07 10:58:08 +0600

    meter class

---
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -0,0 +1,17 @@
+package main.java;
+
+public class Meter {
+    private int openingReading;
+    private int closingReading;
+    public Meter(int openingReading,int closingReading){
+        if(openingReading<0 || closingReading<0){
+            throw new IllegalArgumentException("Reading has to be positive");
+
+        }
+        if(closingReading<openingReading){
+            throw new IllegalArgumentException("Closing reading must be greater or equal than Opening reading");
+        }
+        this.closingReading=closingReading;
+        this.openingReading=openingReading;
+    }
+}
