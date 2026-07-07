b35f3746f5b59dd20007156f8df35771eb191fc2
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-07-07 09:23:44 +0600

    meter WIP v1

---
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -0,0 +1,8 @@
+public class Meter {
+    private double units;
+
+    public Meter(double units){
+        if(units<0) throw new IllegalArgumentException("Units cannot be negative");
+        this.units=units;
+    }
+}
