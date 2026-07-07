3048913dc616252ea332f09651df88cd28ff19d9
37-inan <inanjaki184@gmail.com>
2026-07-07 12:56:46 +0600

    1

--- Lab-08/GridLedger/src/main/java/BillingRun.java
+++ Lab-08/GridLedger/src/main/java/BillingRun.java
@@ -5,6 +5,24 @@ public class BillingRun {
     List<Connection> connections=new ArrayList<>();
 
     public int size(){
+        return connections.size();
+    }
+    public List<Invoice> execute(){
+        List<Invoice> invoices=new ArrayList<>();
+        for (Connection c : connections){
+            invoices.add(new Invoice(c));
+        }
+        return invoices;
+    }
+
+    public void register(Connection c){
+        connections.add(c);
+    }
+    public void register(Connection c,double fuel){
+        c.setFuelPercentage(fuel);
+        connections.add(c);
+    }
+    public double grandTotal(){
 
     }
 }
