9e892544f09ebc184ec65020841b3ffb2a17c39e
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-07 08:46:19 +0600

    meter class

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -1,2 +1,13 @@
 public class Meter {
+    private final int openingReading;
+    private final int closingReading;
+
+    public Meter(int openingReading,int closingReading){
+        if(openingReading<0||closingReading<0){
+            throw new IllegalAccessException("Reading cannot be negative");
+        }
+        if(closingReading<openingReading){
+
+        }
+    }
 }
