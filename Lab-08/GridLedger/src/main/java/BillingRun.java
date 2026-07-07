0663ef3b45e748bec9a1f47b79414de4141c1fd0
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-07 09:48:24 +0600

    init commit

--- Lab-08/GridLedger/src/main/java/BillingRun.java
+++ Lab-08/GridLedger/src/main/java/BillingRun.java
@@ -3,6 +3,6 @@ import java.util.List;
 public class BillingRun {
     private ArrayList<Connection> connections= new ArrayList<>();
     public void register(Connection c){
-
+      connections.add(c);
     }
 }
