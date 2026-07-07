8970f355fc69f8d4507f9e7a08915cfecb1cce66
arnoboic <arnobarafat@iut-dhaka.edu>
2026-07-07 11:45:24 +0600

    finished LifelineConnection  Class

--- Lab-08/GridLedger/src/main/java/LifelineConnection.java
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -1,2 +1,12 @@
-public class LifelineConnection {
+public class LifelineConnection extends ResidentialConnection{
+    public LifelineConnection(Meter M) {
+        super(M);
+    }
+
+    @Override
+    public double energyCharge(){
+        double c = super.energyCharge();
+
+        return c - (c * 0.30);
+    }
 }
--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -5,7 +5,7 @@ public class Meter {
 
     public Meter(int previousReading, int currentReading){
         if(previousReading < 0) throw new IllegalArgumentException("previousReading cannot be negative");
-        if (previousReading > currentReading) throw new IllegalArgumentException(("currentReading cannot be more than previousReadin");
+        if (previousReading > currentReading) throw new IllegalArgumentException("currentReading cannot be more than previousReadin");
 
         this.previousReading = previousReading;
         this.currentReading = currentReading;
