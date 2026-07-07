42d2f85dd83810387b0b3b3c59e3896a6b7120cb
Maomao-starry <aminarahmanshalu@gmail.com>
2026-07-07 11:30:27 +0600

    task

---
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -0,0 +1,19 @@
+public class Meter {
+    private int opening_reading;
+    private int closing_reading ;
+    public Meter(int opening_reading ,int closing_reading){
+        if(closing_reading<opening_reading){
+            throw new IllegalArgumentException("not valid");
+        }
+        if(closing_reading<0){
+            throw  new IllegalArgumentException("Not valid");
+        }
+        if(opening_reading<0) {
+            throw new IllegalArgumentException("not valid");
+        }
+            this.opening_reading=opening_reading;
+            this.closing_reading=closing_reading;
+    }
+
+
+}
