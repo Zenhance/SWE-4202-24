66f3da4b8119618be1ae3754b2a64d199db72811
wasifjafar <wasifjafar71@gmail.com>
2026-07-07 10:14:34 +0600

    add LifelineConnection class's overriden energyCharge method with rebate

--- Lab-08/GridLedger/src/main/java/LifelineConnection.java
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -3,4 +3,10 @@ public class LifelineConnection extends ResidentialConnection{
     public LifelineConnection(Meter meter) {
         super(meter);
     }
+
+    @Override
+    public double energyCharge() {
+        double rebate = super.energyCharge() * .30;
+        return super.energyCharge() - rebate;
+    }
 }
