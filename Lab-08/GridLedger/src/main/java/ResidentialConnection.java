0f3260d34ae46e94bfa875a105be75f0c6646d22
hossdia <hossainmahdia22@gmail.com>
2026-07-07 12:36:22 +0600

    connection abstract

--- Lab-08/GridLedger/src/main/java/ResidentialConnection.java
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -1,3 +1,8 @@
-public class ResidentialConnection {
-
+public class ResidentialConnection extends Connection {
+    private static final double FIXED_CHARGE = 100.0;
+    private static final int BAND_1_LIMIT = 50;
+    private static final int BAND_2_lIMIT = 200;
+    private static final double RATE_1 = 4.0;
+    private static final double RATE_2 = 7.0;
+    private static final double RATE_3 = 11.0;
 }
