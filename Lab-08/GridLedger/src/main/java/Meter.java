39e02e25f36379bbb7a9cb1d038b815fd7d58458
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-07-07 11:51:49 +0600

    meter class fixed

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -13,33 +13,9 @@ public class Meter {
             throw new IllegalArgumentException("closingReading can't be less than openingReading");
         }
 
-
         this.closingReading = closingReading;
         this.openingReading = openingReading;
 
-
-    }
-    public class Meter {
-
-        private int openingReading;
-        private int closingReading;
-
-        public Meter(int openingReading , int closingReading){
-
-            if(openingReading < 0) {
-                throw new IllegalArgumentException("Opening reading can't be negative");
-            }
-
-            if(openingReading > closingReading){
-                throw new IllegalArgumentException("closingReading can't be less than openingReading");
-            }
-
-
-            this.closingReading = closingReading;
-            this.openingReading = openingReading;
-
-
-        }
 
     }
     public int getopeningReading() {
