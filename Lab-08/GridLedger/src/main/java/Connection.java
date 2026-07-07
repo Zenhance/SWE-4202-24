231ff6cb6dc9eb12e4cf15c763d7db3f65c642b0
arnoboic <arnobarafat@iut-dhaka.edu>
2026-07-07 11:29:40 +0600

    finished Residential  Class

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,14 +1,21 @@
 public abstract class Connection {
+
     protected Meter meter;
     protected double fuelPercent = 0.10;
+    protected double taxPercent  = 0.05;
+
 
     public Connection(Meter meter){
         this.meter = meter;
     }
 
+    protected int units = meter.getUnitsConsumed();
+
     public abstract double energyCharge();
     public abstract double fixedCharge();
     public abstract double fuelSurcharge();
     public abstract double tax();
-    public abstract double total();
+    public double total(){
+        return energyCharge()+fixedCharge()+fuelSurcharge()+tax();
+    };
 }
--- Lab-08/GridLedger/src/main/java/ResidentialConnection.java
+++ Lab-08/GridLedger/src/main/java/ResidentialConnection.java
@@ -1,2 +1,40 @@
-public class ResidentialConnection {
+public class ResidentialConnection extends Connection {
+
+    public ResidentialConnection(Meter M){
+        super(M);
+    }
+
+
+
+    @Override
+    public double energyCharge() {
+        double c = 0;
+        if (units <= 50) {
+            c += units * 4.0;
+        } else {
+            c += 50 * 4.0;
+            if (units <= 200) {
+                c += (units - 50) * 7.0;
+            } else {
+                c += 150 * 7.0;
+                c += (units - 200) * 11.0;
+            }
+            return c;
+        }
+    }
+
+    @Override
+    public double fixedCharge() {
+        return 100.0;
+    }
+
+    @Override
+    public double fuelSurcharge() {
+        return energyCharge()*fuelPercent;
+    }
+
+    @Override
+    public double tax() {
+        return  (energyCharge() + fixedCharge() + fuelSurcharge()) * taxPercent;
+    }
 }
