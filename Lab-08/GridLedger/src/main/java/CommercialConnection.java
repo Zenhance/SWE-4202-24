432489f4ddb87a0bdd1ef84f5dc3c6ed18d951bf
37-inan <inanjaki184@gmail.com>
2026-07-07 12:22:32 +0600

    class

--- Lab-08/GridLedger/src/main/java/CommercialConnection.java
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -1,11 +1,20 @@
 public class CommercialConnection extends Connection{
     private static final double COM_RATE_1 = 9.0;
-    private static final double OM_RATE_2 = 13.0;
+    private static final double COM_RATE_2 = 13.0;
     private static final double COM_FIXED  = 500.0;
 
     public CommercialConnection(Meter meter){
         super(meter);
     }
 
-
+    @Override
+    public double energyCharge() {
+        int unit= getMeter().getUnitsConsumed();
+        if (unit<=100){
+            return unit*COM_RATE_1;
+        }
+        else {
+           return 100*COM_RATE_1+(unit-100)*COM_RATE_2;
+        }
+    }
 }
