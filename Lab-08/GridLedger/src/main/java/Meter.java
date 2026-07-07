7d206ab8efa37e8330a1b77227ccd374be6f31c5
Tasnita Raida <tasnitar@gmail.com>
2026-07-07 11:39:09 +0600

    meter class

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -12,4 +12,12 @@ public class Meter{
         this.openingReading = openingReading;
         this.closingReading = closingReading;
     }
+
+    public int getOpeningReading() {
+        return openingReading;
+    }
+
+    public int getClosingReading() {
+        return closingReading;
+    }
 }
