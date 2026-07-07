e66c82754ed65879e968937e89d23a04e5a57ed8
iam-tahmid <tahmidim99@gmail.com>
2026-07-07 12:06:50 +0600

    getters added and UnitsConsumed getter also added

--- Lab-08/GridLedger/src/main/java/Meter.java
+++ Lab-08/GridLedger/src/main/java/Meter.java
@@ -2,12 +2,27 @@ public class Meter {
     private int openingReading;
     private int closingReading;
 
-    public Meter (int openingReading, int closingReading){
+    public int Meter (int openingReading, int closingReading){
         if(openingReading < 0 || closingReading <0 ){
             throw new IllegalArgumentException("Invalid rating");
         }
         if(closingReading < openingReading){
             throw new IllegalArgumentException("closing reading can't be less than the opening rating");
         }
+        else {
+            this.openingReading = openingReading;
+            this.closingReading = closingReading;
+        }
+       public int getOpenreading(){
+            return openingReading;
+        }
+
+       public int getClosereading(){
+            return closingReading;
+        }
+        public int getUnitsConsumed(){
+            return closingReading - openingReading;
+        }
+
     }
 }
