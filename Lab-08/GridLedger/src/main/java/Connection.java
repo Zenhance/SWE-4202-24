5e8fb76f25bebb618849b24cb4ede25fd353be50
Rakin <rakinhasan001@gmail.com>
2026-07-07 11:50:07 +0600

    Connection class constructed

--- Lab-08/GridLedger/src/main/java/Connection.java
+++ Lab-08/GridLedger/src/main/java/Connection.java
@@ -1,2 +1,29 @@
-public class abstract Connection {
+public abstract class Connection {
+    private static final double DEFAULT_FUEL_SURCHARGE=0.05;
+    private static final double TAX=0.05;
+    protected final Meter meter;
+    private double fuelSurcharge=DEFAULT_FUEL_SURCHARGE;
+
+    public Connection(Meter meter){
+        if(meter==null) throw new IllegalArgumentException("Error!");
+           else  this.meter=meter;
+    }
+    public abstract double energyCharge();
+
+    public abstract double fixedCharge();
+
+    public double fuelSurcharge(){
+        return energyCharge()*fuelSurcharge;
+    }
+    public double tax(){
+        return (energyCharge()+fixedCharge()+fuelSurcharge())*TAX;
+    }
+
+    public double total(){
+        return energyCharge()+fixedCharge()+tax();
+    }
+
+    public void setFuelSurcharge(double fuelSurcharge) {
+        this.fuelSurcharge = fuelSurcharge;
+    }
 }
