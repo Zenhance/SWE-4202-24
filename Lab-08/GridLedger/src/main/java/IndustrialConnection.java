4d43f58129e7034bbf856f2dd1e9a95ea101be1e
37-inan <inanjaki184@gmail.com>
2026-07-07 12:26:56 +0600

    class

--- Lab-08/GridLedger/src/main/java/IndustrialConnection.java
+++ Lab-08/GridLedger/src/main/java/IndustrialConnection.java
@@ -1,2 +1,19 @@
-public class IndustrialConnection {
+public class IndustrialConnection extends Connection{
+    private static final double IND_RATE   = 15.0;
+    private static final double IND_FIXED  = 2000.0;
+
+    public IndustrialConnection(Meter meter){
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge() {
+        int unit= getMeter().getUnitsConsumed();
+        return unit*IND_RATE;
+    }
+
+    @Override
+    public double fixedCharge() {
+        return IND_FIXED;
+    }
 }
