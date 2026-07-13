3952a340850a4a2723ab4f93136760dca761ea1e
40-sadeed <sadeedanwar@iut-dhaka.edu>
2026-07-14 05:52:07 +0600

    classes created to reduce compilation errors

--- Lab-08/GridLedger/src/main/java/BillingRun.java
+++ Lab-08/GridLedger/src/main/java/BillingRun.java
@@ -1,23 +1,37 @@
+import java.util.ArrayList;
+
 public class BillingRun {
 
-    public BillingRun(){
+    private ArrayList<Connection> connections;
+    private ArrayList<Invoice> invoices;
 
+    public BillingRun() {
+        connections = new ArrayList<>();
+        invoices = new ArrayList<>();
     }
 
-    public void register(){
-
+    public void register(Connection c) {
+        connections.add(c);
     }
 
-    public int size(){
-
-    return 0;
-    }
-
-    public double grandTotal(){
-
-    return 0;
+    public void register(Connection c, double fuelRate) {
+        connections.add(c);
     }
 
 
+    public int size() {
+        return connections.size();
+    }
 
+    public double grandTotal() {
+        double subtotal = 0;
+        for (Connection c : connections) {
+            subtotal += c.total();
+        }
+        return subtotal;
+    }
+    public ArrayList<Invoice> execute() {
+
+        return invoices;
+    }
 }
--- Lab-08/GridLedger/src/main/java/CommercialConnection.java
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -6,7 +6,7 @@ public class CommercialConnection extends Connection{
 
     @Override
     public double energyCharge() {
-        units = meter.getUnitsConsumed();
+        double units = meter.getUnitsConsumed();
         double c = 0;
         if ( units <= 100) c += units * 9.0;
         else { c += 100 * 9.0; c += ( units -100) * 13.0; }
