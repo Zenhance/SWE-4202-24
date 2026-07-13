aa9ee036591f41798839608fd5f2986305c70be0
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-07-13 13:20:03 +0600

    Invoice.java

---
+++ Lab-08/GridLedger/src/main/java/Invoice.java
@@ -0,0 +1,11 @@
+public class Invoice {
+    private final double total;
+
+    public Invoice(double total){
+        this.total =total;
+    }
+
+    public double getTotal(){
+        return total;
+    }
+}
