a8424de746fe5adf73f1c67ba24d07d6ba2adcbb
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-07-13 09:40:08 +0600

    updated CommercialConnection.java engryCHrge polymorphic methd

---
+++ Lab-08/GridLedger/src/main/java/CommercialConnection.java
@@ -0,0 +1,18 @@
+public class CommercialConnection extends Connection{
+    private static final double RATE1 = 9.0;
+    private static final double RATE2 = 13.0;
+    private static final double FIXED = 500;
+
+    public CommercialConnection(Meter meter){
+        super(meter);
+    }
+
+    @Override
+    public double energyCharge(){
+        int units = meter.getUnitsConsumed();
+        if(units <=100){
+            return units*RATE1;
+        }
+        return  100*RATE1 + (units - 100)*RATE2;
+    }
+}
