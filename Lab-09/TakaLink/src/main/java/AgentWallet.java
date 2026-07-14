867504b6bd7cb6dc2c4b7c33fda8ee468cdc4b4f
shafin-75 <arafatshafin85@gmail.com>
2026-07-14 12:12:08 +0600

    Done agentWallet.java

---
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -0,0 +1,19 @@
+public class AgentWallet extends Wallet{
+    private static  final double DAILY_LIMIT = 500_000.0;
+    public AgentWallet(String id, double openingBalance, String pin){
+        super(id, openingBalance,pin);
+    }
+
+    @Override
+    protected  double dailyLimit(){
+        return DAILY_LIMIT;
+    }
+    @Override
+    boolean canBePayerOf(TransactionKind kind){
+        return kind==TransactionKind.SEND;
+    }
+    @Override
+    boolean canBeRecipientOf(TransactionKind kind){
+        return kind==TransactionKind.CASHOUT || kind== TransactionKind.TOPUP;
+    }
+}
