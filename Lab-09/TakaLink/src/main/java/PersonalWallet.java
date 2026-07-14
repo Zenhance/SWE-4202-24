fd83b80f241cb2873d8e289e58c24fb6f24d71e7
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-14 09:51:07 +0600

    created the constructor

--- Lab-09/TakaLink/src/main/java/PersonalWallet.java
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -1,3 +1,9 @@
 public class PersonalWallet extends Wallet{
+    private static final double DAILY_LIMIT = 25_000.0;
+    public PersonalWallet(
+            String id, double openingBalance, String pin) {
+        super(id, openingBalance, pin);
+    }
+
 
 }
