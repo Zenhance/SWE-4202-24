e697468b8cdeead7832d73b90a9351bd0d5d51e4
sabbir240042147 <eftesabbir2725@gmail.com>
2026-07-14 11:28:10 +0600

    wallet

---
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public  abstract class Wallet {
+    private String id;
+    private double balance;
+    private String pin;
+    private boolean frozen;
+    private double spentToday;
+
+}
