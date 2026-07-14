69292661e443234304fc3d6359fd820963a8fa8b
hossdia <hossainmahdia22@gmail.com>
2026-07-14 12:28:51 +0600

    transaction<-send money

--- Lab-09/TakaLink/src/main/java/SendMoney.java
+++ Lab-09/TakaLink/src/main/java/SendMoney.java
@@ -1,3 +1,16 @@
 public class SendMoney extends Transaction {
+    private static final double FLAT_FEE = 5.0;
+    public SendMoney(Wallet from, Wallet to, double amount, String pin) {
+        super(from, to, amount, pin);
+    }
 
+    @Override
+    public double fee() {
+        return FLAT_FEE;
+    }
+
+    @Override
+    protected void validateOperation() throws OperationNotAllowedException {
+        if (!from.canSend()) throw new OperationNotAllowedException(from.name() + " may not send money");
+    }
 }
