282761a3da5a4d1a2c88564a96194c4d9cef2af1
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-07-14 11:52:06 +0600

    payment class

--- Lab-09/TakaLink/src/main/java/Payment.java
+++ Lab-09/TakaLink/src/main/java/Payment.java
@@ -1,2 +1,16 @@
-public class Payment {
+public class Payment extends Transaction{
+
+    public Payment(Wallet payWallet,Wallet receiveWallet,double amount,String pin){
+        super(payWallet, receiveWallet, amount, pin);
+    }
+
+    @Override
+    public double fee() {
+        return 0.0;
+    }
+
+    @Override
+    public void settle() {
+
+    }
 }
