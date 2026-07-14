824a27697d708c368b1021dd27c1049675bed6b2
Sadman Raiyan <kamrulseed@gmail.com>
2026-07-14 08:54:53 +0600

    Finished AgentWallet.java

---
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -0,0 +1,22 @@
+public class AgentWallet extends Wallet{
+    private static final double DAILY_LIMIT = 500000.0;
+
+    public AgentWallet(String id, double balance, String pin) {
+        super(id, balance, pin);
+    }
+
+    @Override
+    public double getDailyLimit() {
+        return DAILY_LIMIT;
+    }
+
+    @Override
+    public boolean canSend() {
+        return true;
+    }
+
+    @Override
+    public boolean canCashOut() {
+        return true;
+    }
+}
