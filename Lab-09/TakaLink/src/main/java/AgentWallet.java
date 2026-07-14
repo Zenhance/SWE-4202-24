b4ef7057189a422ea28abce4b6adb902390d37f8
sabbir240042147 <eftesabbir2725@gmail.com>
2026-07-14 12:21:04 +0600

    agent wallet update

---
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class AgentWallet extends Wallet{
+    private static final double LIMIT=500000.0;
+    public AgentWallet(String id,double balance,String pin){
+        super(id,balance,pin);
+    }
+    public double getDailyLimit(){
+        return LIMIT;
+    }
+}
