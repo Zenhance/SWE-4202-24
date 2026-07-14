76d31eed9ce06ed630738c0488aef70279da2cad
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-14 09:48:02 +0600

    init commit

--- Lab-09/TakaLink/src/main/java/PaymentTransaction.java
+++ Lab-09/TakaLink/src/main/java/PaymentTransaction.java
@@ -7,4 +7,15 @@ public class PaymentTransaction extends Transaction{
    public double getFee(){
         return 0;
     }
+    @Override
+    public String getType(){
+      return "PAYMENT";
+
+    }
+    @Override
+    protected void moveMoney()
+        throws TransactionException{
+        from.debit(amount);
+        to.credit(amount);
+    }
 }
