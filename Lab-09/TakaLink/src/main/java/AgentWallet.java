fa38e22175825574a1bdd8a2d22b767248f35472
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-14 09:43:06 +0600

    done with agent wallet

--- Lab-09/TakaLink/src/main/java/AgentWallet.java
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -1,7 +1,31 @@
-public class AgentWallet {
-    public AgentWallet(String g, double v, String number) {
+public class AgentWallet extends Wallet {
+
+    public AgentWallet(String id, double openingBalance, String pin) {
+        super(id, openingBalance, pin);
     }
 
-    public double balance() {
+    @Override
+    protected double dailyLimit() {
+        return 0.0;
+    }
+
+    @Override
+    public boolean canSendMoney() {
+        return false;
+    }
+
+    @Override
+    public boolean canCashOut() {
+        return false;
+    }
+
+    @Override
+    public boolean canMakePayment() {
+        return false;
+    }
+
+    @Override
+    public boolean canTopUp() {
+        return false;
     }
 }
