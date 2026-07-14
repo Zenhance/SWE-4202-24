2d29dd3fcc09a2fc98853e6eefc73efcec49db56
Zunied-57 <zunied0411@gmail.com>
2026-07-14 10:31:54 +0600

    all methods overriden

---
+++ Lab-09/TakaLink/src/main/java/Payment.java
@@ -0,0 +1,29 @@
+public class Payment extends Transaction
+{
+    public Payment(Wallet fromId, Wallet toId, double amount, String pin)
+    {
+        super(fromId, toId, amount, pin);
+    }
+
+    @Override
+    public double fee()
+    {
+        return 0.0;
+    }
+
+    @Override
+    public void validateOperation() throws OperationNotAllowedException
+    {
+        if(!fromId.canSendMoney())
+            throw new OperationNotAllowedException();
+        if(!(toId instanceof MerchantWallet))
+            throw new OperationNotAllowedException();
+    }
+
+    @Override
+    public void moveMoney() throws TransactionException
+    {
+        fromId.debit(amount);
+        toId.credit(amount);
+    }
+}
