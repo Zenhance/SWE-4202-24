20f5120ac6f4c184854352ec51e1c38fd648f524
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-07-14 12:00:06 +0600

    added agentwallet class

---
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -0,0 +1,35 @@
+public class AgentWallet extends Wallet{
+
+private static final double LIMIT=500000.0;
+
+public AgentWallet(String id, double balance, String pin)
+{
+    super(id, balance, pin);
+}
+@Override
+    public double dailyLimit()
+{
+    return LIMIT;
+}
+
+@Override
+    public boolean canSendMoney()
+{
+    return true;
+}
+@Override
+    public boolean canPayment()
+{
+    return true;
+}
+@Override
+    public boolean canCashOut()
+{
+    return true;
+}
+public boolean canTopUp()
+{
+    return true;
+}
+
+}
