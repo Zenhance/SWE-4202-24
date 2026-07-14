19dae0cc69dde58c487ca87d58bd891f9c865362
wasifjafar <wasifjafar71@gmail.com>
2026-07-14 09:52:34 +0600

    create SettlementRun class

---
+++ Lab-09/TakaLink/src/main/java/SettlementRun.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class SettlementRun {
+    private ArrayList<Transaction> transactions;
+
+    public SettlementRun() {
+        transactions = new ArrayList<>();
+    }
+}
