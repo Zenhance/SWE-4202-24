dc2ebe716fdecf3b56d74faf53a98684165da56e
tryhardFuad <mubtasimsmail@gmail.com>
2026-07-14 13:19:44 +0600

    feat: created fee method for Topup class

--- Lab-09/TakaLink/src/main/java/TopUp.java
+++ Lab-09/TakaLink/src/main/java/TopUp.java
@@ -2,4 +2,8 @@ public class TopUp extends Transaction {
     public TopUp(Wallet w, AgentWallet a, double amount, String pin) {
         super(w, a, amount, pin);
     }
+
+    public double fee() {
+        return 0.0;
+    }
 }
