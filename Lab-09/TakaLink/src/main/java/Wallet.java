436ec28df98c20d69b1ad2d6d71e5bb2bcb85922
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-14 08:46:35 +0600

    Wallet class upgraded

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -6,8 +6,22 @@ public abstract class Wallet {
     private double sentAmount = 0.0;
 
     public Wallet(String id, double balance, String pin) {
-
+        if(balance < 0) {
+            throw new IllegalArgumentException("Balance can't be negative");
+        }
+        if(pin == null) {
+            throw new IllegalArgumentException("Pin can't be null");
+        }
+        this.id = id;
+        this.balance = balance;
+        this.pin = pin;
     }
+
+    public double balance() {
+        return this.balance;
+    }
+
+
 
 
 
