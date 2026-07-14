075186dacb603239f65859ecd4bc4d2064c4e83b
sabbir240042147 <eftesabbir2725@gmail.com>
2026-07-14 11:35:21 +0600

    validation

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -6,5 +6,13 @@ public  abstract class Wallet {
     private String pin;
     private boolean frozen;
     private double spentToday;
-
+    public Wallet(String id,double balance,String pin) {
+        if (id == null || id.isBlank()) throw new IllegalArgumentException("You must provide your id");
+        if(balance<0){
+            throw new IllegalArgumentException("balance cannot be negative");
+        }
+        if(pin== null || pin.isBlank()){
+            throw new IllegalArgumentException("pin cannot be null");
+        }
+    }
 }
