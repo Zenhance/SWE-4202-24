a984382166c9e08db25ebada2ba9343e5791f84a
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-07-14 12:12:00 +0600

    replacing faulty fields and constructors

--- Lab-09/TakaLink/src/main/java/Transaction.java
+++ Lab-09/TakaLink/src/main/java/Transaction.java
@@ -8,17 +8,34 @@
  * waves straight through, because nothing here is ever checked.
  */
 public class Transaction {
-    public String type;        // "SEND", "CASHOUT", "PAYMENT", "TOPUP"
+           // "SEND", "CASHOUT", "PAYMENT", "TOPUP"
     public double amount;
     public String fromId;
     public String toId;
     public String pin;
 
-    public Transaction(String type, double amount, String fromId, String toId, String pin) {
-        this.type = type;
+    public Transaction(String fromId, String toId, double amount, String pin) {
+
         this.amount = amount;
         this.fromId = fromId;
         this.toId = toId;
         this.pin = pin;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getFromId() {
+        return fromId;
+    }
+
+    public String getPin() {
+
+        return pin;
+    }
+
+    public String getToId() {
+        return toId;
     }
 }
