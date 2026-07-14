98a37ae616a0f2e5e81d530d318c35815e540bf1
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-07-14 11:38:33 +0600

    Added  new essential methods to Wallet Class(namely, debit credit and remainingLimit methods)

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -1,26 +1,26 @@
-public class Wallet {
+public abstract class Wallet {
 
 private String id;
-private double amount;
+private double balance;
 private String pin;
 private boolean frozen;
 private double spentToday;
 
 
-public Wallet (String id, double amount, String pin)
+public Wallet (String id, double balance, String pin)
 
 {
 
 
     if ((id==null) || id.isBlank())
         throw new IllegalArgumentException("Id can not be null");
-    if(amount<=0)
+    if(balance<=0)
         throw new IllegalArgumentException("Amount must be positive");
     if(pin==null || pin.isBlank())
         throw new IllegalArgumentException("Pin can not be null");
 
           this.id=id;
-          this.amount=amount;
+          this.balance=balance;
           this.pin=pin;
           this.frozen=false;
           this.spentToday=0;
@@ -44,11 +44,14 @@ public String getid()
 {
     return id;
 }
-public double getAmount()
+public double getBalance()
 {
-    return amount;
+    return balance;
 }
-
+public double getSpentToday()
+{
+    return spentToday;
+}
 public void freeze()
 {
     frozen=true;
@@ -57,7 +60,25 @@ public boolean isFrozen()
 {
     return frozen;
 }
+public void debit (double amount) throws InsufficientBalanceException
+{
+    if (amount<=0)
+        throw new IllegalArgumentException("Amount must be positive");
+    if (balance-amount<=0)
+        throw new InsufficientBalanceException();
+    balance=balance-amount;
+}
+public void credit (double amount)
+{
+    if(amount<=0)
+        throw new IllegalArgumentException("Amount must be positive");
+          balance=balance+amount;
 
-
-
+}
+public double remainingLimit()
+{
+    return dailyLimit()-spentToday;
+}
+public abstract double fee();
+public abstract double dailyLimit();
 }
