01c4ac5b95e22407bb96a4cc55d1e2e04cf5c537
wasifjafar <wasifjafar71@gmail.com>
2026-07-15 00:36:48 +0600

    fix PersonalWallet class's settle method's logic to handle daily limit

--- Lab-09/TakaLink/src/main/java/PersonalWallet.java
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -1,6 +1,16 @@
 public class PersonalWallet extends Wallet{
+    private final double DAILY_LIMIT = 25_000;
+    private double remainingLimit;
 
     public PersonalWallet(String name, double balance, String PIN) {
         super(name, balance, PIN);
+        remainingLimit = DAILY_LIMIT;
+    }
+
+    @Override
+    public void debit(double amount) throws TransactionException {
+        if (amount > remainingLimit)
+            throw new DailyLimitExceededException("Daily limit exceeded");
+        super.debit(amount);
     }
 }
