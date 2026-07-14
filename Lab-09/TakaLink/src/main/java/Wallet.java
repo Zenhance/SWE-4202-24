c9606ca18f1c753c7487b1a84e5a64db1b2d57be
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-14 08:43:35 +0600

    init commit

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -9,14 +9,14 @@ public abstract class Wallet {
             throw new IllegalArgumentException("Invalid wallet ID");
         if(balance<0)
             throw new IllegalArgumentException("Balance cant be negative");
+        if(pin == null)
+            throw new IllegalArgumentException("PIN cant be null");
 
-
-
-
-
-
-
-
+        this.id=id;
+        this.balance;
+        this.pin=pin;
+        this.frozen= false;
+        this.spentToday=0;
     }
 
 
