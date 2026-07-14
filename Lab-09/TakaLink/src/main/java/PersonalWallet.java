3c0b027e5f1a219b5e2b1eb3d9e27de693b02a70
Samin-init <mhsamin10@gmail.com>
2026-07-14 10:03:00 +0600

    Personal Wallets class completed

---
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -0,0 +1,35 @@
+public class PersonalWallet extends Wallet {
+
+    public PersonalWallet(
+            String id,
+            double openingBalance,
+            String pin
+    ) {
+        super(id, openingBalance, pin);
+    }
+
+    @Override
+    protected double dailyLimit() {
+        return 0;
+    }
+
+    @Override
+    public boolean canSendMoney() {
+        return false;
+    }
+
+    @Override
+    public boolean canCashOut() {
+        return false;
+    }
+
+    @Override
+    public boolean canMakePayment() {
+        return false;
+    }
+
+    @Override
+    public boolean canTopUp() {
+        return false;
+    }
+}
