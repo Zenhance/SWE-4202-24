35fb423db0d9cbe3606b93011cd4edce4c6bba08
hossdia <hossainmahdia22@gmail.com>
2026-07-14 11:27:53 +0600

    personal wallet

--- Lab-09/TakaLink/src/main/java/PersonalWallet.java
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -3,4 +3,23 @@ public class PersonalWallet extends Wallet {
     public PersonalWallet(String name, double balance, String pin) {
         super(name, balance, pin);
     }
+    @Override
+    public double dailyLimit() {
+        return DAILY_LIMIT;
+    }
+
+    @Override
+    public boolean canSend() {
+        return true;
+    }
+
+    @Override
+    public boolean canCashOut() {
+        return true;
+    }
+
+    @Override
+    public boolean isAgent() {
+        return false;
+    }
 }
