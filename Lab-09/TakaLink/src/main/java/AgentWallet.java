a19fdda6ea202c25bb6d360b84f64633fdfed7af
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-14 10:03:35 +0600

    completed agent wallet class that extends from wallet

--- Lab-09/TakaLink/src/main/java/AgentWallet.java
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -1,8 +1,27 @@
 public class AgentWallet extends Wallet{
     private static final double DAILY_LIMIT = 500000.0;
 
-    public AgentWallet(
-            String id, double openingBalance, String pin) {
+    public AgentWallet(String id, double openingBalance, String pin){
         super(id, openingBalance, pin);
+    }
+    @Override
+    public double dailyLimit(){
+        return DAILY_LIMIT;
+    }
+    @Override
+    public boolean canSendMoney(){
+        return true;
+    }
+    @Override
+    public boolean canCashOut(){
+        return true;
+    }
+    @Override
+    public boolean canMakePayment(){
+        return true;
+    }
+    @Override
+    public boolean canTopUp(){
+        return true;
     }
 }
