9b1d18c09cabe146c0dae1d4449cbeccf02dd6ea
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-14 09:52:04 +0600

    write : add the constructor and Override method to MerchantWallet class

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -1,7 +1,10 @@
-public class MerchantWallet {
-    public MerchantWallet(String m, double v, String number) {
+public class MerchantWallet extends Wallet {
+    public MerchantWallet(String id, double balance, String pin) {
+        super(id, balance, pin);
     }
 
-    public double balance() {
+    @Override
+    public double dailyLimit() {
+        return 0.0;
     }
 }
