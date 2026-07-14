30fb5d624a3364fdcc73246417fa9710ef0a37cd
md-musaddik <musaddikibna1@gmail.com>
2026-07-14 11:34:05 +0600

    getBalance and debit method declared

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -6,16 +6,26 @@ public abstract class Wallet {
     private double spentToday;
     protected Wallet(String id, double balance, String pin){
         if(id==null || id.isBlank()){
-            throw new IllegalArgumentException("The Id can't be blank.")
+            throw new IllegalArgumentException("The Id can't be blank.");
         }
         if(balance <0){
-            throw new IllegalArgumentException("The initial deposit can't be negative")
+            throw new IllegalArgumentException("The initial deposit can't be negative");
         }
         if(pin==null || pin.isBlank()){
-            throw new IllegalArgumentException(("The pin can't be emplty"))
+            throw new IllegalArgumentException(("The pin can't be emplty"));
         }
         this.id=id;
         this.balance=balance;
         this.pin=pin;
+    }
+    public double getBalance(){return balance;}
+    public void debit(double amount) throws InsufficientBalanceException {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Withdraw amount must be positive");
+        }
+        if (balance - amount < 0) {
+            throw new InsufficientBalanceException(id + " has insufficient balance for " + amount);
+        }
+        balance -= amount;
     }
 }
