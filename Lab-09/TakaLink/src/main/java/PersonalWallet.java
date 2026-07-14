c9b6d5c07e7b26b4b8397c8c802c588255ec599f
tasnitaraida <tasnitar@gmail.com>
2026-07-14 12:26:28 +0600

    done

--- Lab-09/TakaLink/src/main/java/PersonalWallet.java
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -1,3 +1,11 @@
 public class PersonalWallet extends Wallet{
+    private static final double DAILY_LIMIT=25_000.0;
 
+    public PersonalWallet(String id, double balance, String pin){
+        super(id,balance,pin);
+    }
+
+    public double dailyLimit(){
+        return DAILY_LIMIT;
+    }
 }
