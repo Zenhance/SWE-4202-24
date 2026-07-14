60236f3f73d3f1b74679d6e0e19549fc3e8f88e9
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-14 09:18:56 +0600

    AgentWallet class designed

--- Lab-09/TakaLink/src/main/java/AgentWallet.java
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -1,2 +1,9 @@
-public class AgentWallet {
+public class AgentWallet extends Wallet {
+    public AgentWallet(String id, double balance, String pin) {
+        super(id, balance, pin);
+    }
+    @Override
+    public double getDailyLimit() {
+        return 500000.0;
+    }
 }
