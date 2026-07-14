cf631acc8a0a2b7b2a25dbf3bac8e3f702cd58ce
hossdia <hossainmahdia22@gmail.com>
2026-07-14 12:51:43 +0600

    payment

--- Lab-09/TakaLink/src/main/java/Payment.java
+++ Lab-09/TakaLink/src/main/java/Payment.java
@@ -1,2 +1,10 @@
 public class Payment extends Transaction {
+    public Payment(Wallet from, Wallet to, double amount, String pin) {
+        super(from, to, amount, pin);
+    }
+
+    @Override
+    public double fee() {
+        return 0.0;
+    }
 }
