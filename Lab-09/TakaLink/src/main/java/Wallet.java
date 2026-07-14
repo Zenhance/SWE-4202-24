842a0513e89b0cbc2dfb2cb88c7e780b1f1ae470
md-musaddik <musaddikibna1@gmail.com>
2026-07-14 11:25:08 +0600

    Wallet's Constructor declared

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -4,4 +4,18 @@ public abstract class Wallet {
     private final String pin;
     private boolean frozen;
     private double spentToday;
+    protected Wallet(String id, double balance, String pin){
+        if(id==null || id.isBlank()){
+            throw new IllegalArgumentException("The Id can't be blank.")
+        }
+        if(balance <0){
+            throw new IllegalArgumentException("The initial deposit can't be negative")
+        }
+        if(pin==null || pin.isBlank()){
+            throw new IllegalArgumentException(("The pin can't be emplty"))
+        }
+        this.id=id;
+        this.balance=balance;
+        this.pin=pin;
+    }
 }
