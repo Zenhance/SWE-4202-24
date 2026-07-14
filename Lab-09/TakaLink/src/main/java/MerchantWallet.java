9cb295713c4c13e346ea66556379d66be5d3b6ee
md-musaddik <musaddikibna1@gmail.com>
2026-07-14 12:16:31 +0600

    dailyLimit class declared and it is zero because the merchant does not sends money and we have thrown a exception named assertCanSend, so that the merchant can not send any money.

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -1,5 +1,14 @@
 public class MerchantWallet extends Wallet{
+    private final double DAILY_LIMIT = 0.0
     public MerchantWallet(String id, double balance, String pin) {
         super(id, balance, pin);
+    }
+    @Override
+    public double dailyLimit() {
+        return DAILY_LIMIT;
+    }
+    @Override
+    protected void assertCanSend() throws OperationNotAllowedException {
+        throw new OperationNotAllowedException(id() + " is a merchant and it does not send money");
     }
 }
