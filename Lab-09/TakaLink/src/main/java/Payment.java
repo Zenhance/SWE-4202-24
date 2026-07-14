ddd0e531139b11c0098149e7beedd9378002a89b
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-14 10:05:26 +0600

    new payment class

---
+++ Lab-09/TakaLink/src/main/java/Payment.java
@@ -0,0 +1,20 @@
+public final class Payment extends Transaction{
+    public Payment( payer, Wallet receiver, double amount, String offeredPin){
+        super(payer, receiver, amount, offeredPin);
+    }
+    @Override
+    public double fee(){
+        return 0.0;
+    }
+    @Override
+    protected Operation operation(){
+        return Operation.PAYMENT;
+    }
+    @Override
+    protected void validateParties()
+        throws OperationNotAllowedException {
+        if (!(receiver() instanceof MerchantWallet)) {
+            throw new OperationNotAllowedException("payment destination must be a merchant");
+        }
+    }
+}
