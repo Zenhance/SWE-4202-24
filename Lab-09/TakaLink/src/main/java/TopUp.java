8ff3fcc961e49f0dfc15e39e7ad29cc749172b2e
Zunied-57 <zunied0411@gmail.com>
2026-07-14 10:37:33 +0600

    all methods overriden

---
+++ Lab-09/TakaLink/src/main/java/TopUp.java
@@ -0,0 +1,27 @@
+public class TopUp extends Transaction
+{
+    public TopUp(Wallet fromId, Wallet toId, double amount, String pin)
+    {
+        super(fromId, toId, amount, pin);
+    }
+
+    @Override
+    public double fee()
+    {
+        return 0.0;
+    }
+
+    @Override
+    public void validateOperation() throws OperationNotAllowedException
+    {
+        if(!fromId.canSendMoney())
+            throw new OperationNotAllowedException();
+    }
+
+    @Override
+    public void moveMoney() throws TransactionException
+    {
+        fromId.debit(amount);
+        toId.credit(amount);
+    }
+}
