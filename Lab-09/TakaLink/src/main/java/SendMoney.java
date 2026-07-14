92db248e63072f81a6e81bf761c08379594fe5be
tryhardFuad <mubtasimsmail@gmail.com>
2026-07-14 12:29:02 +0600

    fix: fixed constructor for SendMoney Class

--- Lab-09/TakaLink/src/main/java/SendMoney.java
+++ Lab-09/TakaLink/src/main/java/SendMoney.java
@@ -1,13 +1,8 @@
 public class SendMoney extends Transaction {
-    public SendMoney(PersonalWallet a, PersonalWallet b, double v, String number) throws InvalidPinException, InsufficientBalanceException {
+    public SendMoney(PersonalWallet a, PersonalWallet b, double v, String number)  {
         super(a, b, v, number);
-        if(!a.verifyPin(number)){
-            throw new InvalidPinException(number);
-        }
-        if(v > a.balance()){
-            throw new InsufficientBalanceException(v, a.balance());
-        }
-        a.debit(v + 5.0);
-        b.credit(v);
+    }
+    public double fee(){
+        return 5.00;
     }
 }
