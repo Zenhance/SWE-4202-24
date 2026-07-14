8a26e7b53c7145bd970c2c7423aa9f951be83ab0
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-14 10:15:59 +0600

    init commit

--- Lab-09/TakaLink/src/main/java/SettlementRun.java
+++ Lab-09/TakaLink/src/main/java/SettlementRun.java
@@ -7,5 +7,29 @@ public class SettlementRun {
         batch = new ArrayList<>();
         report = new SettlementReport();
     }
+   public void addTransaction(Transaction t){
+        batch.add(t);
+   }
+   public void addTransaction(Transaction t, int repeat){
+        for(int i=0;i<report;i++)
+            batch.add(t);
+   }
+   public int getWaitingCount(){
+        return batch.size();
+   }
 
+    public SettlementReport execute(){
+        for(Transaction t: batch){
+            try{
+                t.settle();
+                report.addSuccess(t);
+
+            }
+            catch(TransactionException e){
+                report.addFailure(t,e);
+            }
+
+            }
+        return report;
+        }
 }
