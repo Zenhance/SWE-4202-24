4fa2024c5df58cb0481dc744c5dbf208f5911d39
wasifjafar <wasifjafar71@gmail.com>
2026-07-14 10:03:31 +0600

    create SettlementReport class's skeleton methods

--- Lab-09/TakaLink/src/main/java/SettlementReport.java
+++ Lab-09/TakaLink/src/main/java/SettlementReport.java
@@ -1,2 +1,22 @@
 public class SettlementReport {
+    private int settledTransactions;
+    private int rejectedTransactions;
+
+    public int settledCount() {
+        return settledTransactions;
+    }
+
+    public int rejectedCount() {
+        return rejectedTransactions;
+    }
+
+    public double totalMoved() {
+        return 0.0;
+    }
+
+    public double totalFees() {
+        return 0.0;
+    }
+
+
 }
