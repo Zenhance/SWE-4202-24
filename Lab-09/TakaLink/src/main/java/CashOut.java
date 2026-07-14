75dd88e0adf08d09967c7625c6285dd1a5f83f57
Zunied-57 <zunied0411@gmail.com>
2026-07-14 10:34:44 +0600

    all methods overriden

---
+++ Lab-09/TakaLink/src/main/java/CashOut.java
@@ -0,0 +1,29 @@
+public class CashOut extends Transaction
+{
+    public CashOut(Wallet fromId, Wallet toId, double amount, String pin)
+    {
+        super(fromId, toId, amount, pin);
+    }
+
+    @Override
+    public double fee()
+    {
+        return getAmount()*0.0185;
+    }
+
+    @Override
+    public void validateOperation() throws OperationNotAllowedException
+    {
+        if(!fromId.canSendMoney())
+            throw new OperationNotAllowedException();
+        if(!(toId instanceof AgentWallet))
+            throw new OperationNotAllowedException();
+    }
+
+    @Override
+    public void moveMoney() throws TransactionException
+    {
+        fromId.debit(amount+fee());
+        toId.credit(amount);
+    }
+}
