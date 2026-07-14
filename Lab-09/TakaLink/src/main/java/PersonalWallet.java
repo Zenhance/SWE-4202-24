c36888ba5eaea0acdc56dd58b0959e245cbfc775
arnoboic <arnobarafat@iut-dhaka.edu>
2026-07-14 09:03:25 +0600

    finished wallet parent class with proper naming and made PersonalWallet as a subclass

--- Lab-09/TakaLink/src/main/java/PersonalWallet.java
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -1,46 +1,7 @@
-public class PersonalWallet {
-
-    private String id;
-    private double balance;
-    private boolean frozen;
-    private double spentToday;
-    private String pin;
+public class PersonalWallet extends Wallet{
 
     public PersonalWallet(String id, double balance, String pin) {
-        if (id == null) throw new NullPointerException("Must provide Identity");
-        if(balance < 0) throw new IllegalArgumentException("Cannot create Account with negative balance");
-        if(pin == null) throw new NullPointerException("Must provide pin");
-
-        spentToday = 0;
-        this.balance = balance;
-        this.id = id;
-        this.pin = pin;
+        super(id, balance, pin);
     }
 
-    public void debit(double amount) {
-        if (amount <= 0) throw new IllegalArgumentException("amount must be positive, got" + amount);
-        if (amount > balance) throw new IllegalStateException("balance " + balance + " too low for " + amount);
-
-        spentToday += amount;
-        balance -= amount;
-    }
-
-    public double balance() {
-        return balance;
-    }
-
-    public void credit(double amount) {
-        if (amount <= 0) throw new IllegalArgumentException("amount must be positive, got" + amount);
-
-        balance += amount;
-    }
-
-    public boolean verifyPin(String number) {
-        if (pin.equals(number)) return true;
-        return false;
-    }
-
-    public void freeze() {
-        this.frozen = true;
-    }
 }
---
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -0,0 +1,46 @@
+public class Wallet {
+
+    private String id;
+    private double balance;
+    private boolean frozen;
+    private double spentToday;
+    private String pin;
+
+    public Wallet(String id, double balance, String pin) {
+        if (id == null) throw new NullPointerException("Must provide Identity");
+        if(balance < 0) throw new IllegalArgumentException("Cannot create Account with negative balance");
+        if(pin == null) throw new NullPointerException("Must provide pin");
+
+        spentToday = 0;
+        this.balance = balance;
+        this.id = id;
+        this.pin = pin;
+    }
+
+    public void debit(double amount) {
+        if (amount <= 0) throw new IllegalArgumentException("amount must be positive, got" + amount);
+        if (amount > balance) throw new IllegalStateException("balance " + balance + " too low for " + amount);
+
+        spentToday += amount;
+        balance -= amount;
+    }
+
+    public double balance() {
+        return balance;
+    }
+
+    public void credit(double amount) {
+        if (amount <= 0) throw new IllegalArgumentException("amount must be positive, got" + amount);
+
+        balance += amount;
+    }
+
+    public boolean verifyPin(String number) {
+        if (pin.equals(number)) return true;
+        return false;
+    }
+
+    public void freeze() {
+        this.frozen = true;
+    }
+}
