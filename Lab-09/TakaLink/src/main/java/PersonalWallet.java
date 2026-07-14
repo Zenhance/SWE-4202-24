d0dda95a9ca502bf6f78995c48b8d64f295b0620
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-07-14 11:56:46 +0600

    8

---
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -0,0 +1,7 @@
+package src.main.java;
+
+public class PersonalWallet extends Wallet{
+    public PersonalWallet(String name, double balance, String pin){
+        super(name,balance,pin);
+    }
+}
