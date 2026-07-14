58dbc66ef6d32dc505f73fbefca0d662b84cf355
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-14 09:13:00 +0600

    new java class with constructor

---
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -0,0 +1,7 @@
+public final class MerchantWallet extends Wallet{
+    private static final double DAILY_LIMIT = 0.0;
+
+    public MerchantWallet(String id, double openingBalance, String pin){
+        super(id, openingBalance, pin);
+    }
+}
