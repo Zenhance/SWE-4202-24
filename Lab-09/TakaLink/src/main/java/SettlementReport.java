6df5927f4337e0dff8c396e1c0adee4097908881
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-14 10:19:39 +0600

    new classes

--- Lab-09/TakaLink/src/main/java/SettlementReport.java
+++ Lab-09/TakaLink/src/main/java/SettlementReport.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.IdentityHashMap;
+import java.util.Map;
 public class SettlementReport {
+
+    public int settledCount(){
+        return 0;
+    }
+    public int rejectedCount(){
+        return 0;
+    }
+    public double totalMoved(){
+        return 0.0;
+    }
+    public double totalFees(){
+        return 0.0;
+    }
+    public TransactionException errorOf(Transaction t){
+        return null;
+    }
+    public boolean isSettled(Transaction t){
+        return false;
+    }
+    void recordSettled(Transaction t){
+    }
+    void recordRejected(Transaction t, TransactionException e){
+    }
 }
