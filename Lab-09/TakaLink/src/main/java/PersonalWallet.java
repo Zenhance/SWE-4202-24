97f599237605e7530b83497e79cbcfad8757df11
shafin-75 <arafatshafin85@gmail.com>
2026-07-14 11:59:01 +0600

    done personalWallet.java

---
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -0,0 +1,20 @@
+public class PersonalWallet extends Wallet{
+    private static  final double DAILY_LIMIT = 25_000.0;
+
+    public PersonalWallet(String id, double openingBalance, String pin){
+        super(id, openingBalance,pin);
+    }
+
+    @Override
+    protected  double dailyLimit(){
+        return DAILY_LIMIT;
+    }
+    @Override
+    boolean canBePayerOf(TransactionKind kind){
+        return true;
+    }
+    @Override
+    boolean canBeRecipientOf(TransactionKind kind){
+        return kind==TransactionKind.SEND;
+    }
+}
