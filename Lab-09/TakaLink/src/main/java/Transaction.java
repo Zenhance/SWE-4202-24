61611b55acc3386acbcd495fba16886247db759f
Zunied-57 <zunied0411@gmail.com>
2026-07-14 10:14:03 +0600

    constructor and variables reassigned

--- Lab-09/TakaLink/src/main/java/Transaction.java
+++ Lab-09/TakaLink/src/main/java/Transaction.java
@@ -9,20 +9,20 @@
  */
 public abstract class Transaction
 {
-    public double amount;
-    public String fromId;
-    public String toId;
-    public String pin;
+    protected double amount;
+    protected Wallet fromId;
+    protected Wallet toId;
+    protected Wallet pin;
 
-    public Transaction(String fromId, String toId, double amount, String pin)
+    public Transaction(Wallet fromId, Wallet toId, double amount, Wallet pin)
     {
-        if(fromId==null || fromId.isBlank())
+        if(fromId==null)
             throw new IllegalArgumentException("Id can not be null");
-        if(toId==null || toId.isBlank())
+        if(toId==null)
             throw new IllegalArgumentException("Id can not be null");
         if(amount<=0)
             throw new IllegalArgumentException("Amount has to be positive");
-        if(pin==null || pin.isBlank())
+        if(pin==null)
             throw new IllegalArgumentException("Pin can not be null");
         this.fromId = fromId;
         this.toId = toId;
@@ -30,30 +30,11 @@ public abstract class Transaction
         this.pin = pin;
     }
 
-    public double getAmount()
-    {
-        return amount;
-    }
-
-    public String getFromId()
-    {
-        return fromId;
-    }
-
-    public String getToId()
-    {
-        return toId;
-    }
-
-    public String getPin()
-    {
-        return pin;
-    }
-
     public abstract double fee();
 
-    public void settle()
-    {
+    public abstract void validOperation() throws OperationNotAllowedException;
 
-    }
+    public abstract void moveMoney() throws TransactionException;
+
+
 }
