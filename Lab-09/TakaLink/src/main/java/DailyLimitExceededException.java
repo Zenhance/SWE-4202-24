939f69d2c178ee4d7c5a5c1da8f72433a1bf77b9
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-14 09:04:46 +0600

    write : Complete the DailyLimitExceededException class

--- Lab-09/TakaLink/src/main/java/DailyLimitExceededException.java
+++ Lab-09/TakaLink/src/main/java/DailyLimitExceededException.java
@@ -1,2 +1,5 @@
-public class DailyLimitExceededException {
+public class DailyLimitExceededException extends TransactionException {
+    public DailyLimitExceededException(String message) {
+        super(message);
+    }
 }
