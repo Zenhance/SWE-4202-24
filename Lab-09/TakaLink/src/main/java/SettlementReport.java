f6997de697e344e334e90d40fbcd845c3454b4ff
tryhardFuad <mubtasimsmail@gmail.com>
2026-07-14 12:59:07 +0600

    feat: created settledCount and rejectedCount method for SettlementReport class

---
+++ Lab-09/TakaLink/src/main/java/SettlementReport.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SettlementReport {
+    List<Transaction> settledTransactions = new ArrayList<>();
+    List<Transaction> rejectedTransactions = new ArrayList<>();
+
+
+    public int settledCount() {
+        return settledTransactions.size();
+    }
+    public int  rejectedCount() {
+        return rejectedTransactions.size();
+    }
+
+}
