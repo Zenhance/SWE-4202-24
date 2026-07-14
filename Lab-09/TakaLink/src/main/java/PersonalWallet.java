a87a408423daeb3432b35511d6590d26b54b5a37
Munman <214246799+bollamokles@users.noreply.github.com>
2026-07-14 08:45:49 +0600

    commit wallet

---
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -0,0 +1,4 @@
+public class PersonalWallet extends Wallet {
+    pr
+
+}
---
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -0,0 +1,27 @@
+public abstract class Wallet {
+    private String name;
+    private double balance;
+    private String PIN;
+    private boolean frozenStatus;
+    private double sentToday;
+    public Wallet(String name, double balance, String PIN) {
+        if(name==null || name.isEmpty()){
+            throw new IllegalArgumentException("Wallet name cannot be empty");
+        }else {
+            this.name = name;
+        }
+        if(balance<0){
+            throw new IllegalArgumentException("Balance cannot be negative");
+        }else{
+            this.balance = balance;
+        }
+        if(PIN==null || PIN.isEmpty()){
+        throw new IllegalArgumentException("PIN cannot be empty");
+        }else {
+            this.PIN = PIN;
+        }
+        this.frozenStatus = false;
+        this.sentToday = 0;
+    }
+
+}
