5948db266f69fb3929bea5ade0ab1cf06307b695
wasifjafar <wasifjafar71@gmail.com>
2026-07-15 00:48:53 +0600

    fix last commit for MerchantWallet class's debit method

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -3,9 +3,4 @@ public class MerchantWallet extends Wallet{
     public MerchantWallet(String name, double balance, String PIN) {
         super(name, balance, PIN);
     }
-
-    @Override
-    public void debit(double amount) throws TransactionException {
-        throw new OperationNotAllowedException("Operation not allowed");
-    }
 }
