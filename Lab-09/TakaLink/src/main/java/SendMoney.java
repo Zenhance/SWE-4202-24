28098ea70411d6174d4e3c8a8399d008a727e282
wasifjafar <wasifjafar71@gmail.com>
2026-07-14 09:19:51 +0600

    create SendMoney Class and  fee and settle overridden methods

---
+++ Lab-09/TakaLink/src/main/java/SendMoney.java
@@ -0,0 +1,15 @@
+public class SendMoney extends Transaction{
+    public SendMoney(Wallet payingWallet, Wallet receivingWallet, double amount, String PIN) {
+        super(payingWallet, receivingWallet, amount, PIN);
+    }
+
+    @Override
+    public double fee() {
+        return 0.0;
+    }
+
+    @Override
+    public void settle() {
+
+    }
+}
