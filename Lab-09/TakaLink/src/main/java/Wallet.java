b267a1a3badcfd0904201c9f59a3a6cb1188f9c4
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-14 08:39:18 +0600

    init commit

---
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -0,0 +1,22 @@
+public abstract class Wallet {
+    private String id;
+    private double balance;
+    private String pin;
+    private boolean frozen;
+    private double spentToday;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
