b48a793904d349f6c21df73a569223edab62fc4a
Intezar Mahmud Wasi <intezarmahmud@iut-dhaka.edu>
2026-07-14 11:10:52 +0600

    fields initialized

---
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -0,0 +1,13 @@
+public abstract class Wallet {
+
+    private String id;
+    private double balance;
+    private String pin;
+    private boolean frozen;
+    private double spentToday;
+
+
+    protected Wallet(String id,double openingBalance,String pin){
+
+
+}
