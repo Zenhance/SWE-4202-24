5173cd91a9e9207dba8a765e5b04fb83d2a07664
leigion-broken11 <mohaiminurrahman8@gmail.com>
2026-07-14 11:29:14 +0600

    Wallet.java v1

---
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -0,0 +1,44 @@
+public abstract class Wallet {
+    private final String id;
+    private double balance;
+    private final String pin;
+    private boolean frozen;
+    private double spentToday;
+
+    protected Wallet(String id, double balance, String pin) {
+        if (id == null || id.isBlank()) throw new IllegalArgumentException("Invalid id");
+        if (pin == null || pin.isBlank()) throw new IllegalArgumentException("Invalid pin");
+        if (balance < 0) throw new IllegalArgumentException("Negative balance");
+        this.id = id;
+        this.balance = balance;
+        this.pin = pin;
+        this.frozen = false;
+        this.spentToday = 0.0;
+    }
+
+    public String id() { return id; }
+
+    public double balance() { return balance; }
+
+    public boolean isFrozen() { return frozen; }
+
+    public void freeze() { this.frozen = true; }
+
+    public boolean verifyPin(String offered) {
+        if (offered == null) return false;
+        return this.pin.equals(offered);
+    }
+
+    public void debit(double amount) throws InsufficientBalanceException{
+        if(amount <= 0) throw new IllegalArgumentException("Debit ammount must be positive");
+        if(balance-amount < 0) throw new InsufficientBalanceException(id);
+        balance += amount;
+    }
+
+    public void credit(double amount){
+        if(amount <=0) throw new IllegalArgumentException("Credit must be positive");
+        balance +=amount;
+    }
+
+
+}
