a381f5698ee3057856271c20171b48af45fc988f
mubashirulhasan-netizen <mubashirulhasan@iut-dhaka.edu>
2026-07-14 10:10:12 +0600

    Completed the MerchantWallet and PersonalWallet

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -1,2 +1,42 @@
-public class MerchantWallet {
+public final class MerchantWallet extends Wallet {
+
+    private static final double DAILY_LIMIT = 0.0;
+
+    public MerchantWallet(
+            String id,
+            double openingBalance,
+            String pin
+    ) {
+        super(id, openingBalance, pin);
+    }
+
+    @Override
+    protected double dailyLimit() {
+        return DAILY_LIMIT;
+    }
+
+    @Override
+    public boolean canSendMoney() {
+        return false;
+    }
+
+    @Override
+    public boolean canCashOut() {
+        return false;
+    }
+
+    @Override
+    public boolean canMakePayment() {
+        return true;
+    }
+
+    @Override
+    public boolean canTopUp() {
+        return true;
+    }
+
+    @Override
+    public boolean canReceivePayment() {
+        return true;
+    }
 }
--- Lab-09/TakaLink/src/main/java/PersonalWallet.java
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -1,2 +1,27 @@
-public class PersonalWallet {
+public final class PersonalWallet extends Wallet {
+
+    private static final double DAILY_LIMIT = 25_000.0;
+    public PersonalWallet(String id, double openingBalance, String pin) {
+        super(id, openingBalance, pin);
+    }
+    @Override
+    protected double dailyLimit() {
+        return DAILY_LIMIT;
+    }
+    @Override
+    public boolean canSendMoney() {
+        return true;
+    }
+    @Override
+    public boolean canCashOut() {
+        return true;
+    }
+    @Override
+    public boolean canMakePayment() {
+        return true;
+    }
+    @Override
+    public boolean canTopUp() {
+        return true;
+    }
 }
