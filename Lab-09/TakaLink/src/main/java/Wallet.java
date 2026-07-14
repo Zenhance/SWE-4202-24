8ae2efa17b11991dd6320b97ea28e8c467e38422
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-14 08:58:19 +0600

    started wallet, made a constructor, that takes the id, opening balance and pin. uses three arguement exceptions, null, opening balance, and pin errors.

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -1,2 +1,33 @@
-public class Wallet {
+public abstract class Wallet {
+
+    private final String id;
+    private double balance;
+    private final String pin;
+    private boolean frozen;
+    private double spentToday;
+
+    protected Wallet(String id, double openingBalance, String pin)
+    {
+        if (id== null || id.isBlank()) {
+            throw new IllegalArgumentException("Wallet ID cannot be null or blank");
+        }
+        if (openingBalance< 0.0) {
+            throw new IllegalArgumentException("Opening balance cannot be negative");
+        }
+        if (pin== null) {
+            throw new IllegalArgumentException("PIN cannot be null");
+        }
+        this.id = id;
+        this.balance = openingBalance;
+        this.pin = pin;
+        this.frozen = false;
+        this.spentToday = 0.0;
+    }
+    public final String id() {
+        return id;
+    }
+    public final double balance() {
+        return balance;
+    }
+
 }
