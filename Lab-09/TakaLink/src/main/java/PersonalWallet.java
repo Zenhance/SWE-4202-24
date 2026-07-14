293f590dec3ff52cfa3b398f8d19962e6a9a5f9c
nusra <nusratnishi1103@gmail.com>
2026-07-14 11:17:41 +0600

    first

---
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -0,0 +1,22 @@
+public class PersonalWallet extends Wallet{
+    private static final double DAILY_LIMIT=25000.0;
+
+    public PersonalWallet(String id,double balance,String pin){
+        super(id,balance,pin);
+    }
+
+    @Override
+    public double dailyLimit(){
+        return DAILY_LIMIT;
+    }
+
+    @Override
+    public boolean canSend(){
+        return true;
+    }
+
+    @Override
+    public boolean canCashOut(){
+        return true;
+    }
+}
