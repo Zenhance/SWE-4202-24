91369a9a274ab36680832c0e54ca52d01bee546f
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-14 08:30:11 +0600

    Wallet class initiated

---
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -0,0 +1,11 @@
+public abstract class Wallet {
+    private String id;
+    private double balance;
+    private String pin;
+    private boolean frozen = false;
+    private double sentAmount = 0.0;
+
+
+
+
+}
