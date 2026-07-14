f465d7f4bb30e139db4eb3e7bbeae786f15dc69f
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-14 09:45:57 +0600

    done with merchant wallet

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -1,10 +1,38 @@
-public class MerchantWallet extends Wallet {
+public final class MerchantWallet extends Wallet {
 
-    public MerchantWallet(
-            String id,
-            double openingBalance,
-            String pin
-    ) {
+    private static final double DAILY_LIMIT = 0.0;
+
+    public MerchantWallet(String id, double openingBalance, String pin) {
         super(id, openingBalance, pin);
+    }
+
+    @Override
+    protected double dailyLimit() {
+        return DAILY_LIMIT;
+    }
+
+    @Override
+    public boolean canSendMoney() {
+        return false;
+    }
+
+    @Override
+    public boolean canCashOut() {
+        return false;
+    }
+
+    @Override
+    public boolean canMakePayment() {
+        return true;
+    }
+
+    @Override
+    public boolean canTopUp() {
+        return true;
+    }
+
+    @Override
+    public boolean canReceivePayment() {
+        return true;
     }
 }
