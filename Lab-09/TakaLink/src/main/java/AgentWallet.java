1f580260a69716e3299e93c185c2d5a442f31589
mubashirulhasan-netizen <mubashirulhasan@iut-dhaka.edu>
2026-07-14 09:53:12 +0600

    Completed agent wallet and cashout after all the exceptions

--- Lab-09/TakaLink/src/main/java/AgentWallet.java
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -1,2 +1,31 @@
-public class AgentWallet {
+public class AgentWallet extends Wallet {
+
+    public AgentWallet(String id, double openingBalance, String pin) {
+        super(id, openingBalance, pin);
+    }
+
+    @Override
+    protected double dailyLimit() {
+        return 0.0;
+    }
+
+    @Override
+    public boolean canSendMoney() {
+        return false;
+    }
+
+    @Override
+    public boolean canCashOut() {
+        return false;
+    }
+
+    @Override
+    public boolean canMakePayment() {
+        return false;
+    }
+
+    @Override
+    public boolean canTopUp() {
+        return false;
+    }
 }
--- Lab-09/TakaLink/src/main/java/CashOut.java
+++ Lab-09/TakaLink/src/main/java/CashOut.java
@@ -1,2 +1,16 @@
-public class CashOut {
+public class CashOut extends Transaction {
+
+    public CashOut(
+            Wallet payer,
+            Wallet receiver,
+            double amount,
+            String offeredPin
+    ) {
+        super(payer, receiver, amount, offeredPin);
+    }
+
+    @Override
+    public double fee() {
+        return amount() * 0.0185;
+    }
 }
