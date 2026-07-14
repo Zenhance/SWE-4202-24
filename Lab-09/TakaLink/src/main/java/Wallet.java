a4469d08c7648263f1b1e63b36092b3967c229b3
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-14 09:56:41 +0600

    completed the wallet class..

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -18,6 +18,48 @@ public abstract class Wallet {
             this.frozen=false;
             this.spentToday=0.0;
         }
+        public String id(){
+            return id;
+        }
+        public double balance(){
+            return balance;
+        }
+        public boolean verifyPin(String enteredPin){
+            return pin.equals(enteredPin);
+        }
+        public boolean isFrozen(){
+            return frozen;
+        }
+        public void freeze(){
+            frozen = true;
+        }
+        public void debit(double amount) {
+            throws InsufficientBalanceException {
+                if (amount <= 0) {
+                    throw new IllegalArgumentException("Debit amount must be positive.");
+                }
+                if (amount > balance) {
+                    throw new IllegalArgumentException("The wallet does not have enough balance.");
+                }
+                balance = balance - amount;
+            }
+                public void credit(double amount) {
 
-
-}
+                    if (amount <= 0) {
+                        throw new IllegalArgumentException("Credit amount must be positive.");
+                    }
+                    balance = balance + amount;
+                }
+            }
+            public double remainingLimit() {
+                return dailyLimit() - spentToday;
+            }
+            protected void addSpent(double amount){
+                spentToday = spentToday + amount;
+            }
+            public abstract double dailyLimit();
+            public abstract boolean canSendMoney();
+            public abstract boolean canCashOut();
+            public abstract boolean canMakePayment();
+            public abstract boolean canTopUp();
+        }
