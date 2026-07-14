8586a1576a0c59e224fc68fca2b5f575b73c8212
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-14 09:07:13 +0600

    new java class

--- Lab-09/TakaLink/src/main/java/PersonalWallet.java
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -1,3 +1,15 @@
 public class PersonalWallet extends Wallet{
+    private static final double DAILY_LIMIT = 25_000.0;
 
+    public PersonalWallet(String id, double openingBalance, String pin){
+        super(id, openingBalance, pin);
+    }
+@Override
+    public double dailyLimit(){
+        return DAILY_LIMIT;
+}
+@Override
+    protected boolean allows(Operation operation){
+        return true;
+}
 }
