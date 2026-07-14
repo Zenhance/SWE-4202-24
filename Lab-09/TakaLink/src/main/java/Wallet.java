60117af9579ef18141e3db544bb43d9be46bbc8d
inanjaki184 <inanjaki184@gmail.com>
2026-07-14 11:18:06 +0600

    1

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -6,6 +6,19 @@ public abstract class Wallet {
     private double spentToday;
 
     public Wallet(String id,double balance, String pin){
-
+        if(id==null || id.isBlank()){
+            throw new IllegalArgumentException("Id cannot be null");
+        }
+        if(balance<0){
+            throw new IllegalArgumentException("Balance must be positive");
+        }
+        if(pin==null || pin.isBlank()){
+            throw new IllegalArgumentException("Pin cannot be null");
+        }
+        this.id=id;
+        this.balance=balance;
+        this.pin=pin;
     }
+
+
 }
