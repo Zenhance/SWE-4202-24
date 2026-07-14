eb426744f753bfda0b6d8f8167b6ed40ecb0a0f2
hossdia <hossainmahdia22@gmail.com>
2026-07-14 10:59:34 +0600

    wallet

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -1,3 +1,8 @@
 public abstract class Wallet {
+    private final String name;
+    private double balance;
+    private final String pin;
+    private boolean frozen;
+    private double spentToday;
 
 }
