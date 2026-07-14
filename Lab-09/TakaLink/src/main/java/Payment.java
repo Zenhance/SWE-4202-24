753974312edce79e156a372ded9629694291e8d7
jasin <anishaajasin.13.04.2005@gmail.com>
2026-07-14 12:20:24 +0600

    payment

--- Lab-09/TakaLink/src/main/java/Payment.java
+++ Lab-09/TakaLink/src/main/java/Payment.java
@@ -1,2 +1,5 @@
-public class Payment {
+public class Payment extends Transaction {
+    public Payment(Wallet fromId, Wallet toId, double amount, String pin) {
+        super(TransactionType.PAYMENT, amount, fromId, toId, pin);
+    }
 }
