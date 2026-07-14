a8937069626266ef70a16bf799cfbaf9a0ffcdfe
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-14 10:09:11 +0600

    init commit

--- Lab-09/TakaLink/src/main/java/SettlementRun.java
+++ Lab-09/TakaLink/src/main/java/SettlementRun.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class SettlementRun {
+    private ArrayList<Transaction> batch;
+    private SettlementReport report;
+    public SettlementRun(){
+        batch = new ArrayList<>();
+        report = new SettlementReport();
+    }
+
 }
