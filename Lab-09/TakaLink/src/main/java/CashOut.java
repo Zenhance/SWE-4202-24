c1dcc9854ac29b55b0feb2daaece51ab523cc87a
hossdia <hossainmahdia22@gmail.com>
2026-07-14 12:46:02 +0600

    transaction<-cashout

--- Lab-09/TakaLink/src/main/java/CashOut.java
+++ Lab-09/TakaLink/src/main/java/CashOut.java
@@ -3,5 +3,23 @@ public class CashOut extends Transaction {
     public CashOut(Wallet from, Wallet to, double amount, String pin) {
         super(from, to, amount, pin);
     }
+    @Override
+    public double fee() {
+        return amount * FEE_RATE;
+    }
 
+    @Override
+    protected double creditAmount() {
+        return amount + fee();
+    }
+
+    @Override
+    protected void validateOperation() throws OperationNotAllowedException {
+        if (!from.canCashOut()) {
+            throw new OperationNotAllowedException(from.name() + " may not cash out");
+        }
+        if (!to.isAgent()) {
+            throw new OperationNotAllowedException("cash-out destination must be an agent");
+        }
+    }
 }
