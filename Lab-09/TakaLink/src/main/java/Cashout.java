54c370fcb9fa27b12f64476aafb3562ee54386fd
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-07-14 11:41:56 +0600

    transaction extension classes

--- Lab-09/TakaLink/src/main/java/Cashout.java
+++ Lab-09/TakaLink/src/main/java/Cashout.java
@@ -1,2 +1,4 @@
-public class Cashout {
+public class Cashout extends Transaction{
+
+    public Cashout(Wallet payingWallet)
 }
--- Lab-09/TakaLink/src/main/java/Transaction.java
+++ Lab-09/TakaLink/src/main/java/Transaction.java
@@ -7,18 +7,43 @@
  * that PULLS a thousand taka out of the recipient -- a theft the type system
  * waves straight through, because nothing here is ever checked.
  */
-public class Transaction {
-    public String type;        // "SEND", "CASHOUT", "PAYMENT", "TOPUP"
+public abstract class Transaction {
+
+
+      /*public String type;        // "SEND", "CASHOUT", "PAYMENT", "TOPUP"
     public double amount;
     public String fromId;
     public String toId;
     public String pin;
 
+
+
     public Transaction(String type, double amount, String fromId, String toId, String pin) {
+        if(amount<=0)
+            throw new IllegalArgumentException("Transaction needs to be positive");
+
         this.type = type;
         this.amount = amount;
         this.fromId = fromId;
         this.toId = toId;
         this.pin = pin;
     }
+}        */
+
+
+private Wallet paywallet;
+private Wallet receivewallet;
+private double amount;
+private String pin;
+
+
+public Transaction(Wallet paywallet,Wallet receivewallet,double amount,String pin){
+
+
+
+    this.paywallet=paywallet;
+    this.receivewallet=receivewallet;
+    this.amount=amount;
+    this.pin=pin;
+}
 }
