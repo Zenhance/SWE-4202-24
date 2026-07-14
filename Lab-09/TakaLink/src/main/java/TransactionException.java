ce676a2c3fade731b11a5d65a8efc1b50c23591f
sameer-abdullah-240042107 <sameerabdullah1117@gmail.com>
2026-07-14 11:06:45 +0600

    a

---
+++ Lab-09/TakaLink/src/main/java/TransactionException.java
@@ -0,0 +1,8 @@
+package src.main.java;
+
+public abstract class TransactionException extends Exception{
+    private String message;
+    public TransactionException(String message){
+        super(message);
+    }
+}
