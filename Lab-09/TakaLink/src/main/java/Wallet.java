320bd854ae4dde2baa30e68f800a15adb5d6fc8f
inanjaki184 <inanjaki184@gmail.com>
2026-07-14 11:07:14 +0600

    test

---
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -0,0 +1,11 @@
+public abstract class Wallet {
+    private String id;
+    private String pin;
+    private double balance;
+    private boolean frozen;
+    private double spentToday;
+
+    public Wallet(String id,double balance, String pin){
+
+    }
+}
