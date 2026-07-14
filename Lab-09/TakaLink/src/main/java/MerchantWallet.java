40d61c43fd721788011a05f82a13e4dec1a35a00
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-14 09:10:47 +0600

    init commit

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -2,4 +2,14 @@ public class MerchantWallet extends Wallet{
     public MerchantWallet(String id, double balance, String pin){
         super(id,balance,pin);
     }
+    @Override
+    public double getDailyLimit(){
+        return 0;
+    }
+    @Override
+    public void validateOperation(String transactionType){
+        throws OperationNotAllowedException{
+
+        }
+    }
 }
