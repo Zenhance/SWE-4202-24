f4d3cd6011168db477a488cf2990f9fc2dc7c099
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-14 09:39:19 +0600

    done with personal wallet

--- Lab-09/TakaLink/src/main/java/PersonalWallet.java
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -1,10 +1,27 @@
-public class PersonalWallet extends Wallet {
+public final class PersonalWallet extends Wallet {
 
-    public PersonalWallet(
-            String id,
-            double openingBalance,
-            String pin
-    ) {
+    private static final double DAILY_LIMIT = 25_000.0;
+    public PersonalWallet(String id, double openingBalance, String pin) {
         super(id, openingBalance, pin);
+    }
+    @Override
+    protected double dailyLimit() {
+        return DAILY_LIMIT;
+    }
+    @Override
+    public boolean canSendMoney() {
+        return true;
+    }
+    @Override
+    public boolean canCashOut() {
+        return true;
+    }
+    @Override
+    public boolean canMakePayment() {
+        return true;
+    }
+    @Override
+    public boolean canTopUp() {
+        return true;
     }
 }
