513336c4dea311773fb752a890b9250f164ece67
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-14 10:06:07 +0600

    created the constructor..

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -1,2 +1,6 @@
 public class MerchantWallet extends Wallet{
-}
+    private static final double DAILY_LIMIT = 0.0;
+    public MerchantWallet(String id, double openingBalance, String pin){
+        super(id, openingBalance, pin);
+    }
+
