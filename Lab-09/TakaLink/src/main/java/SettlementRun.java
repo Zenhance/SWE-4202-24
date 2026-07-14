f27fb69d59ddb1ff819455bf08f398902486bfca
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-07-14 12:18:17 +0600

    settlement run

---
+++ Lab-09/TakaLink/src/main/java/SettlementRun.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class SettlementRun {
+    private ArrayList<Transaction> transactions;
+    private SettlementReport report;
+
+    public SettlementRun(){
+        transactions=new ArrayList<>();
+    }
+
+
+}
