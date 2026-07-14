86929b0d8e1d4348d7cab2c795f788b75c13efc1
sabbir240042147 <eftesabbir2725@gmail.com>
2026-07-14 12:08:27 +0600

    personal wallet

---
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class PersonalWallet extends Wallet{
+    private static final double LIMIT=25000.00;
+    public PersonalWallet(String id,double balance,String pin){
+        super(id,balance,pin);
+    }
+}
