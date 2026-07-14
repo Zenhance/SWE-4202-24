472c015688c9d152548043b3bf25ffcb273c4cd7
jasin <anishaajasin.13.04.2005@gmail.com>
2026-07-14 11:15:10 +0600

    Update Lab-08

--- Lab-08/GridLedger/src/main/java/LifelineConnection.java
+++ Lab-08/GridLedger/src/main/java/LifelineConnection.java
@@ -1,6 +1,6 @@
-public class LifeLineConnection extends ResidentialConnection {
+public class LifelineConnection extends ResidentialConnection {
     private static final double rate = 0.30;
-    public LifeLineConnection(Meter meter) {
+    public LifelineConnection(Meter meter) {
         super(meter);
     }
 
