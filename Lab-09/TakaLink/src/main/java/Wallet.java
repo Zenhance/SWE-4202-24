b9922c00a4d76d1994c6eb8b764cc8381da245d6
Tasauf Abdullah <tasaufalter@gmail.com>
2026-07-14 11:23:28 +0600

    add constructor to Wallet

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -8,4 +8,16 @@ public class Wallet {
         private String pin;
 
 
+        public Wallet(String id, double balance, String pin) {
+            if (id == null) throw new NullPointerException("Must provide an ID");
+            if(balance < 0) throw new IllegalArgumentException("Balance cannot be negative");
+            if(pin == null) throw new NullPointerException("Must provide pin");
+
+            spentToday = 0.0;
+            this.balance = balance;
+            this.id = id;
+            this.pin = pin;
+        }
+
+
 }
