d8586107dc4a17ae6106e7c444249ab95faba3fb
sameer-abdullah-240042107 <sameerabdullah1117@gmail.com>
2026-07-14 12:08:21 +0600

    g2

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -14,7 +14,7 @@ public abstract class Wallet {
             throw new IllegalArgumentException("balance can not be negative");
         }
         if(pin== null || pin.isBlank()){
-            throw new IllegalArgumentException("pin can't be null"):
+            throw new IllegalArgumentException("pin can't be null");
         }
         this.id=id;
         this.balance=balance;
@@ -22,4 +22,59 @@ public abstract class Wallet {
         this.frozen=false;
         this.spentToday=0.0;
     }
-}
+
+    public String Id() {
+        return id;
+    }
+
+    public double Balance() {
+        return balance;
+    }
+
+    public boolean isFrozen() {
+        return frozen;
+    }
+
+    public double spentToday() {
+        return spentToday;
+    }
+    public String pin(){
+        return pin;
+    }
+    public abstract double getDailyLimit();
+    public abstract boolean canSend();
+    public abstract boolean canCashOut();
+    public void debit(double amount){
+        if(amount>balance){
+        throw new IllegalArgumentException("Insufficient Balance");
+        }
+        else if(amount<0){
+            throw new IllegalArgumentException("Amount can not be negative");
+        }
+        balance-=amount;
+    }
+    public void Credit(double amount){
+        if(amount<0){
+            throw new IllegalArgumentException("Amount can not be negative");
+        }
+        balance+=amount;
+    }
+    public boolean verifyPin(String pin){
+        return pin().equals(pin);
+    }
+    public void freeze(){
+        frozen=true;
+    }
+    public void checkFrozen() throws FrozenAccountException{
+        if(frozen)
+            throw new FrozenAccountException();
+    }
+    public double remainingLimit(){
+        return getDailyLimit()-spentToday();
+    }
+    public void addSpent(double amount){
+        spentToday+=amount;
+    }
+    }
+
+
