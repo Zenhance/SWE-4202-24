ef0f07693a1e12ba6f1c48a0d515ed700dbce9c3
hossdia <hossainmahdia22@gmail.com>
2026-07-14 12:32:49 +0600

    transaction<-send money

--- Lab-09/TakaLink/src/main/java/SendMoney.java
+++ Lab-09/TakaLink/src/main/java/SendMoney.java
@@ -13,4 +13,14 @@ public class SendMoney extends Transaction {
     protected void validateOperation() throws OperationNotAllowedException {
         if (!from.canSend()) throw new OperationNotAllowedException(from.name() + " may not send money");
     }
+
+    @Override
+    protected void validateLimit() throws DailyLimitExceededException {
+        if (from.spentToday() + amount > from.dailyLimit())
+            throw new DailyLimitExceededException(from.name() + " would exceed its daily limit of " + from.dailyLimit());
+    }
+    @Override
+    protected void onSettled(){
+        from.recordSpend(amount);
+    }
 }
