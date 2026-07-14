75bd9e493bfb2ee44bdf38c3b84979b1d9fb9290
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-07-14 12:09:12 +0600

    initial commit

--- Lab-09/TakaLink/src/main/java/PersonalWallet.java
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -1,2 +1,24 @@
-public class PersonalWallet {
+public class PersonalWallet extends Wallet{
+    private String type;
+    private String pin;
+    private double balance;
+
+    public PersonalWallet(String type, String pin, double balance){
+        super(type, pin, balance);
+    }
+
+    public double cashOut(double amount){
+        balance = balance - amount;
+        return balance;
+    }
+
+    public double sendMoney(double amount){
+        balance = balance - amount;
+        return balance;
+    }
+
+    public double receiveMoney(double amount){
+        balance = balance + amount;
+        return balance;
+    }
 }
