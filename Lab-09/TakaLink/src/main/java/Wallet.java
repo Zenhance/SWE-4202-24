e4badd7464d5bf8012679dedaf1c1f97b05853aa
tasnitaraida <tasnitar@gmail.com>
2026-07-14 11:19:57 +0600

    Constructor

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -4,4 +4,10 @@ public class Wallet {
     private String pin;
     private double spentToday;
     private String frozen;
+
+    public Wallet(String id, double balance, String pin) {
+        if(balance<0) {
+            throw new IllegalArgumentException("Balance cannot be negative");
+        }
+    }
 }
