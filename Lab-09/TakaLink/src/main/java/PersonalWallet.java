b8d373daa17b52ac272d769096f6d553e7c56cc5
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-14 09:46:35 +0600

    write : add the Override method to PersonalWallet class

--- Lab-09/TakaLink/src/main/java/PersonalWallet.java
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -2,4 +2,9 @@ public class PersonalWallet extends Wallet {
     public PersonalWallet(String id, double balance, String pin) {
         super(id, balance, pin);
     }
+
+    @Override
+    public double dailyLimit() {
+        return 25000.0;
+    }
 }
