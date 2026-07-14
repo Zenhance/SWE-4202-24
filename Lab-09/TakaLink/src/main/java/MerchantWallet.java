2dd9941721a4f3f977da27d8b34f5e65e720fc32
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-14 09:21:30 +0600

    MerchantWallet class designed

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -1,2 +1,10 @@
-public class MerchantWallet {
+public class MerchantWallet extends Wallet {
+    public MerchantWallet(String id, double balance, String pin) {
+        super(id, balance, pin);
+    }
+
+    @Override
+    public double getDailyLimit() {
+        return 0.0;
+    }
 }
