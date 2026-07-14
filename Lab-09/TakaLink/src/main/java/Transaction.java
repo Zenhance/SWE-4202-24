e2b870b303522ffaf46f160c402ce6967a1b7dab
md-musaddik <musaddikibna1@gmail.com>
2026-07-14 12:22:57 +0600

    The constructor declared of Transiction class.

--- Lab-09/TakaLink/src/main/java/Transaction.java
+++ Lab-09/TakaLink/src/main/java/Transaction.java
@@ -4,5 +4,19 @@ public abstract class Transaction {
     protected final Wallet to;
     protected final double amount;
     protected final String pin;
-
+    protected Transaction(Wallet from, Wallet to, double amount, String pin) {
+        if (from == null ) {
+            throw new IllegalArgumentException("The from wallet is null");
+        }
+        if(to==null){
+            throw new IllegalArgumentException("The to wallet is null");
+        }
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be positive");
+        }
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+        this.pin = pin;
+    }
 }
