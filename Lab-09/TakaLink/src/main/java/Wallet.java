cae773cb1eeafd9fa840c908293b94b20193ca21
jasin <anishaajasin.13.04.2005@gmail.com>
2026-07-14 11:26:15 +0600

    Wallet class

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -1,4 +1,18 @@
 public abstract class Wallet {
+    private String id;
+    private double balance;
+    private String pin;
+    private boolean frozen;
+    private double spentToday;
+
+    protected Wallet(String id, double openingBalance, String pin) {
+
+        this.id = id;
+        this.balance = openingBalance;
+        this.pin = pin;
+        this.frozen = false;
+        this.spentToday = 0;
+    }
 
 
 }
