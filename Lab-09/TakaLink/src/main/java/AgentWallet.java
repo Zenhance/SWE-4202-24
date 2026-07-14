e22c24d5c8b7e7e4190790c24cbc4774de89e599
inanjaki184 <inanjaki184@gmail.com>
2026-07-14 12:15:30 +0600

    2

--- Lab-09/TakaLink/src/main/java/AgentWallet.java
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -1,3 +1,36 @@
 public class AgentWallet extends Wallet{
+    public static final double LIMIT=500000.0;
+    public PersonalWallet(String id,double balance, String pin){
+        super(id, balance, pin);
+    }
 
+    @Override
+    public double dailyLimit()
+    {
+        return LIMIT;
+    }
+
+    @Override
+    public boolean canSendMoney()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean canPayment()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean canCashOut()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean canTopUp()
+    {
+        return true;
+    }
 }
