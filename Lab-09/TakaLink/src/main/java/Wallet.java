76525996fbded21cce71a7f221e99e97cc694beb
Tasauf Abdullah <tasaufalter@gmail.com>
2026-07-14 11:21:16 +0600

    create Wallet class

---
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -0,0 +1,11 @@
+public class Wallet {
+    public class Wallet {
+
+        private String id;
+        private double balance;
+        private boolean frozen;
+        private double spentToday;
+        private String pin;
+
+
+}
