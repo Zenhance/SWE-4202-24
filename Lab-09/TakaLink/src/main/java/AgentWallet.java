16763a08a20c0ee5723d8d5d5912490a2ae47bfe
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-14 09:10:48 +0600

    added methods

--- Lab-09/TakaLink/src/main/java/AgentWallet.java
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -1,5 +1,15 @@
 public final class AgentWallet extends Wallet{
     private static final double DAILY_LIMIT = 500_000.0;
 
-    public AgentWallet
+    public AgentWallet(String id, double openingBalance, String pin){
+        super(id, openingBalance, pin);
+    }
+    @Override
+    public double dailyLimit(){
+        return DAILY_LIMIT;
+    }
+    @Override
+    protected boolean allows(Operation operation){
+        return true;
+    }
 }
