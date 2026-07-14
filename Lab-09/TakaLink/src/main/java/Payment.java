bae07f528874d73252b55d08e9ddde926283d5e1
md-musaddik <musaddikibna1@gmail.com>
2026-07-14 12:41:00 +0600

    Fee and checkPermission Methods declared.

--- Lab-09/TakaLink/src/main/java/Payment.java
+++ Lab-09/TakaLink/src/main/java/Payment.java
@@ -2,4 +2,10 @@ public class Payment extends Transaction{
     public Payment(Wallet from, Wallet to, double amount, String pin) {
         super(from, to, amount, pin);
     }
+    @Override
+    public double fee() {
+        return 0.0;
+    }
+    @Override
+    protected void checkPermission() {}
 }
