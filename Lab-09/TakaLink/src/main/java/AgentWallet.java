30418917621214de7b6e94b1f4380cb5841dcaf1
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-14 10:00:26 +0600

    crated the constructor..

--- Lab-09/TakaLink/src/main/java/AgentWallet.java
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -1,2 +1,8 @@
-public class AgentWallet {
+public class AgentWallet extends Wallet{
+    private static final double DAILY_LIMIT = 500000.0;
+
+    public AgentWallet(
+            String id, double openingBalance, String pin) {
+        super(id, openingBalance, pin);
+    }
 }
