138d5253d26a501ef605d1f58f0d16f628f96efc
nafisaahmedfaz <nafisaahmed1610@gmail.com>
2026-07-14 12:51:58 +0600

    4th commit

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -1,2 +1,22 @@
-public class MerchantWallet {
+public class MerchantWallet extends Wallet{
+    //Change this value if your test file specifies other limit.
+    private static final double DAILY_LIMIT=0.0;
+    public MerchantWallet(String id,double openingBalance,String pin){
+        super(id,openingBalance,pin);
+    }
+    @Override
+    public double getDailyLimit(){
+        return DAILY_LIMIT;
+    }
+    @Override
+    public void checkOperationAllowed(Transaction transaction)
+        throws OperationNotAllowedException{
+        if(transaction instanceof SendTransaction){
+            throw new OperationNotAllowedException("Merchant wallets cannot send money.");
+        }
+        if(transaction instanceof CashOuttransaction){
+            throw new OperationNotAllowedException("Merchant wallets cannot cash out.");
+        }
+        //PaymentTransaction and TopUpTransaction are allowed.
+    }
 }
