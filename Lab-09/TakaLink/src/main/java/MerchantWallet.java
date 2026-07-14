ae7c6f6ecfa47af0e7fa14712a457f6a4097cf55
wasifjafar <wasifjafar71@gmail.com>
2026-07-15 00:45:40 +0600

    fix MerchantWallet class's debit method to unallowed operations

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -3,4 +3,9 @@ public class MerchantWallet extends Wallet{
     public MerchantWallet(String name, double balance, String PIN) {
         super(name, balance, PIN);
     }
+
+    @Override
+    public void debit(double amount) throws TransactionException {
+        throw new OperationNotAllowedException("Operation not allowed");
+    }
 }
