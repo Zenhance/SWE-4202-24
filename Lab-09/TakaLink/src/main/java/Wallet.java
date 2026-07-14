a8ea584e7624a02b4fd48e336af441bf6e3081f7
wasifjafar <wasifjafar71@gmail.com>
2026-07-14 08:34:53 +0600

    fix abstract Wallet Class constructor

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -2,10 +2,19 @@ public abstract class Wallet {
     private String name;
     private double balance;
     private String PIN;
+    private boolean frozenStatus;
+    private double sentToday;
 
     public Wallet(String name, double balance, String PIN) {
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("Name cannot be null or empty.");
+        if (balance < 0)
+            throw new IllegalArgumentException("Balance cannot be negative.");
+        if (PIN == null || PIN.isBlank())
+            throw new IllegalArgumentException("PIN cannot null or empty");
         this.name = name;
         this.balance = balance;
         this.PIN = PIN;
+        this.frozenStatus = false;
     }
 }
