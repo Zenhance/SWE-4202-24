900c0d0b197d2d969dbbab08b5b2a3edf2fc0cac
wasif-22 <twasif68@gmail.com>
2026-07-14 11:56:26 +0600

    added send money class

---
+++ Lab-09/TakaLink/src/main/java/SendMoney.java
@@ -0,0 +1,19 @@
+/** Moves money between two wallets. Flat fee charged to the sender. */
+public class SendMoney extends Transaction {
+
+    private static final double FLAT_FEE = 5.0;
+
+    public SendMoney(Wallet from, Wallet to, double amount, String pin) {
+        super(from, to, amount, pin);
+    }
+
+    @Override
+    public double fee() {
+        return FLAT_FEE;
+    }
+
+    @Override
+    protected TransactionKind kind() {
+        return TransactionKind.SEND;
+    }
+}
