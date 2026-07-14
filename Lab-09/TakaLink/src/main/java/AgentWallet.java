b775205deca2126930abcc5e5e2d6410df9bf8bd
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-14 09:07:22 +0600

    init commit

--- Lab-09/TakaLink/src/main/java/AgentWallet.java
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -8,9 +8,9 @@ public class AgentWallet extends Wallet{
         return DAILY_LIMIT;
     }
 
+    @Override
+    public void validateOperation(String transactionType)
+        throws OperationNotAllowedException{
 
-
-
-
-
+        }
 }
