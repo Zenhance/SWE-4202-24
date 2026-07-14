474187f0397e9063b41777902364386bd7143b6a
waseefmunir-max <waseef05@gmail.com>
2026-07-14 09:17:08 +0600

    Add private fields and constructor to abstract Wallet class

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -1,2 +1,13 @@
 public abstract class Wallet {
+    private final String id;
+    private double balance;
+    private final String pin;
+    private boolean frozen;
+    private double spentToday;
+
+    public Wallet(String id, String pin, double openingBalance) {
+        this.id = id;
+        this.pin = pin;
+        this.balance = openingBalance;
+    }
 }
