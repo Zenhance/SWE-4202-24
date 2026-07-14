0d5a59d00ad16a51f3d9d8d07ac6d54219d6fe2a
Tasauf Abdullah <tasaufalter@gmail.com>
2026-07-14 12:26:53 +0600

    add structure to MerchantWallet skeleton

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -1,3 +1,9 @@
 public class MerchantWallet extends Wallet{
 
+    public final double limit = 0;
+
+    public MerchantWallet(String id, double balance, String pin) {
+        super(id, balance, pin);
+    }
+
 }
