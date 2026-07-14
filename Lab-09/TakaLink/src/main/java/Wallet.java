9abddc53534a7ca46402174ec3a55425869f67cc
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-14 09:23:01 +0600

    created the constructor..

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -1,3 +1,23 @@
 public abstract class Wallet {
+        private final String id;
+        private double balance;
+        private final String pin;
+        private boolean frozen;
+        private double spentToday;
+
+        public Wallet(String id, double openingBalance, String pin){
+            if (id == null || id.isBlank()){
+                throw new IllegalArgumentException("Wallet ID cannot be null or blank.");
+            }
+            if (pin == null){
+                throw new IllegalArgumentException("PIN cannot be null.");
+            }
+            this.id=id;
+            this.balance=openingBalance;
+            this.pin=pin;
+            this.frozen=false;
+            this.spentToday=0.0;
+        }
+
 
 }
