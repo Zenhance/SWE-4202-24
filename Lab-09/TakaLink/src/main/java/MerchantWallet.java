cd67a9ef436a4b57e4819345f45e49c06aea8838
inanjaki184 <inanjaki184@gmail.com>
2026-07-14 12:17:16 +0600

    2

---
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -0,0 +1,35 @@
+public class MerchantWallet extends Wallet{
+    public PersonalWallet(String id,double balance, String pin){
+        super(id, balance, pin);
+    }
+
+    @Override
+    public double dailyLimit()
+    {
+        return 0.0;
+    }
+
+    @Override
+    public boolean canSendMoney()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean canPayment()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean canCashOut()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean canTopUp()
+    {
+        return true;
+    }
+}
