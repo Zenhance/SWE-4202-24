446c345f4572ac906a4f53db8a80f6d5dced4113
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-07-14 11:56:23 +0600

    topup and send money

--- Lab-09/TakaLink/src/main/java/SendMoney.java
+++ Lab-09/TakaLink/src/main/java/SendMoney.java
@@ -1,2 +1,16 @@
-public class SendMoney {
+public class SendMoney extends Transaction{
+
+    public SendMoney(Wallet payWallet,Wallet receiveWallet,double amount,String pin){
+        super(payWallet, receiveWallet, amount, pin);
+    }
+
+    @Override
+    public double fee() {
+        return 0.0;
+    }
+
+    @Override
+    public void settle() {
+
+    }
 }
--- Lab-09/TakaLink/src/main/java/TopUp.java
+++ Lab-09/TakaLink/src/main/java/TopUp.java
@@ -1,2 +1,16 @@
-public class TopUp {
+public class TopUp extends Transaction{
+
+    public TopUp(Wallet payWallet,Wallet receiveWallet,double amount,String pin){
+        super(payWallet, receiveWallet, amount, pin);
+    }
+
+    @Override
+    public double fee() {
+        return 0.0;
+    }
+
+    @Override
+    public void settle() {
+
+    }
 }
