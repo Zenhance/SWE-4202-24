b5813009cea9f3390d0624ac6f47240924edb646
Munman <214246799+bollamokles@users.noreply.github.com>
2026-07-14 09:27:48 +0600

    commit Transaction

--- Lab-09/TakaLink/src/main/java/Transaction.java
+++ Lab-09/TakaLink/src/main/java/Transaction.java
@@ -8,36 +8,44 @@
  * waves straight through, because nothing here is ever checked.
  */
 public abstract class Transaction {
-   /* public String type;        // "SEND", "CASHOUT", "PAYMENT", "TOPUP"
-    public double amount;
-    public String fromId;
-    public String toId;
-    public String pin;
+//    public String type;        // "SEND", "CASHOUT", "PAYMENT", "TOPUP"
+//    public double amount;
+//    public String fromId;
+//    public String toId;
+//    public String pin;
 
-    public Transaction(String type, double amount, String fromId, String toId, String pin) {
-        this.type = type;
-        this.amount = amount;
-        this.fromId = fromId;
-        this.toId = toId;
-        this.pin = pin;
-    }*/
-
-    private String payingWallet;
-    private String receivingWallet;
+    private Wallet payingWallet;
+    private Wallet receivingWallet;
     private double amount;
     private String PIN;
 
-    public Transaction(String payingWallet, String receivingWallet, double amount, String PIN) {
-        if(payingWallet==null||receivingWallet==null||amount<0||PIN==null){
-            throw new IllegalArgumentException();
-        }else{
-            this.payingWallet = payingWallet;
-            this.receivingWallet = receivingWallet;
-            this.amount = amount;
-            this.PIN = PIN;
-        }
+//    public Transaction(String type, double amount, String fromId, String toId, String pin) {
+//        this.type = type;
+//        this.amount = amount;
+//        this.fromId = fromId;
+//        this.toId = toId;
+//        this.pin = pin;
+//    }
+
+    public Transaction(Wallet payingWallet, Wallet receivingWallet, double amount, String PIN) {
+        if (payingWallet == null)
+            throw new IllegalArgumentException("Payer cannot be null");
+        if (receivingWallet == null)
+            throw new IllegalArgumentException("Receiver cannot be null");
+        if (amount <= 0.0)
+            throw new IllegalArgumentException("Amount cannot be negative or zero");
+        if (PIN == null || PIN.isBlank() || PIN.isEmpty())
+            throw new IllegalArgumentException("PIN cannot be null, empty or blank");
+        //InvalidPinException
+        if (!payingWallet.verifyPin(PIN))
+            throw new IllegalArgumentException("Invalid PIN");
+        this.payingWallet = payingWallet;
+        this.receivingWallet = receivingWallet;
+        this.amount = amount;
+        this.PIN = PIN;
     }
 
+    public abstract double fee();
 
-
+    public abstract void settle();
 }
