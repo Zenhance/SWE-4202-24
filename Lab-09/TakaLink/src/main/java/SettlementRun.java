95345cb6c882b35cac17816bfe0ea6146a5e13d6
wasif-22 <twasif68@gmail.com>
2026-07-14 12:45:11 +0600

    added settlement run class

---
+++ Lab-09/TakaLink/src/main/java/SettlementRun.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class SettlementRun {
+
+    private final ArrayList<Transaction> queue = new ArrayList<>();
+
+    public void submit(Transaction transaction) {
+    }
+
+    public void submit(Transaction transaction, int count) {
+    }
+
+    public int pending() {
+        return -1;
+    }
+
+}
