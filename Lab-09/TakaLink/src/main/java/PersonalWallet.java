e9f4fafccd29d4e6935f0970b2b139b7deaf5c27
md-musaddik <musaddikibna1@gmail.com>
2026-07-14 12:06:22 +0600

    Personal Wallet Class declared.

--- Lab-09/TakaLink/src/main/java/PersonalWallet.java
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -3,5 +3,9 @@ public class PersonalWallet extends Wallet{
     public PersonalWallet(String id, double balance, String pin){
         super(id,balance,pin);
     }
+    @Override
+    public double dailyLimit(){
+        return DAILY_LIMIT;
+    }
 
 }
