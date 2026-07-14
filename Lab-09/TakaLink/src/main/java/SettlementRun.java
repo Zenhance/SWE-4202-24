61080d673cbcc794939ee6d2db407131d11cec2d
wasifjafar <wasifjafar71@gmail.com>
2026-07-14 09:53:51 +0600

    create SettlementRun class's overloaded Submit method

--- Lab-09/TakaLink/src/main/java/SettlementRun.java
+++ Lab-09/TakaLink/src/main/java/SettlementRun.java
@@ -6,4 +6,13 @@ public class SettlementRun {
     public SettlementRun() {
         transactions = new ArrayList<>();
     }
+    public void submit(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public void submit(Transaction transaction, int amount) {
+        for (int i = 0; i < amount; i++) {
+            transactions.add(transaction);
+        }
+    }
 }
