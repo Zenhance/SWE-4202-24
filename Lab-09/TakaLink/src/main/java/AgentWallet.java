b3e210806f76c47bd96acbe5b118a5a2b093fcbe
wasifjafar <wasifjafar71@gmail.com>
2026-07-15 13:16:41 +0600

    fix AgentWallet class's debit method to handle daily limit

--- Lab-09/TakaLink/src/main/java/AgentWallet.java
+++ Lab-09/TakaLink/src/main/java/AgentWallet.java
@@ -1,6 +1,16 @@
 public class AgentWallet extends Wallet{
+    private final double DAILY_LIMIT = 500_000;
+    private double remainingLimit;
 
     public AgentWallet(String name, double balance, String PIN) {
         super(name, balance, PIN);
+        remainingLimit = DAILY_LIMIT;
+    }
+
+    @Override
+    public void debit(double amount) throws TransactionException {
+        if (amount > remainingLimit)
+            throw new DailyLimitExceededException("Daily limit exceeded");
+        super.debit(amount);
     }
 }
