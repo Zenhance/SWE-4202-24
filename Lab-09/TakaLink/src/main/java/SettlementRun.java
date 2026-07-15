6a89e44d609ae263d8fa7f5b3c78e077e26ef06b
Zunied-57 <zunied0411@gmail.com>
2026-07-16 04:11:59 +0600

    variables and constructor added

---
+++ Lab-09/TakaLink/src/main/java/SettlementRun.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class SettlementRun
+{
+    private ArrayList<Transaction> batch;
+
+    public SettlementRun()
+    {
+        batch = new ArrayList<>();
+    }
+
+
+}
