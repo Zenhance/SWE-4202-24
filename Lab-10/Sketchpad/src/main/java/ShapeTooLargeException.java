ffd066ac845261e7d1cba9418f0b00ac1ef4d60b
Rakin <rakinhasan001@gmail.com>
2026-07-21 09:09:42 +0600

    shape too small  Exception class constructed

--- Lab-10/Sketchpad/src/main/java/ShapeTooLargeException.java
+++ Lab-10/Sketchpad/src/main/java/ShapeTooLargeException.java
@@ -1,5 +1,5 @@
 public class ShapeTooLargeException extends ShapeDrawException{
     public ShapeTooLargeException(Shape shape,Canvas canvas){
-        super(String.format("%s exceeds canvas (%d cells)",shape.description(),canvas.capacity()),shape);
+        super(String.format("%s exceeds canvas (%d cells)",shape.describe(),canvas.capacity()),shape);
     }
 }
--- Lab-10/Sketchpad/src/main/java/ShapeTooSmallException.java
+++ Lab-10/Sketchpad/src/main/java/ShapeTooSmallException.java
@@ -1,2 +1,5 @@
-public class ShapeTooSmallException {
+public class ShapeTooSmallException extends ShapeDrawException{
+    public ShapeTooSmallException(Shape shape,Canvas canvas){
+        super(String.format("%s would not cover a single cell",shape.describe()),shape);
+    }
 }
