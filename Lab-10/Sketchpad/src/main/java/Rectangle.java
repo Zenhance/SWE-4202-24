65068467dd1dba060a85b9cd2fee837458642639
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-21 08:52:42 +0600

    added constructor

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -1,3 +1,12 @@
 public class Rectangle extends Shape{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height){
+        requirePositive(width,"width");
+        requirePositive(height,"height");
+        this.width = width;
+        this.height = height;
+    }
 
 }
