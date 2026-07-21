139aac1c755ace043f5a49e13fe8d41b31108d9f
wasifjafar <wasifjafar71@gmail.com>
2026-07-21 10:34:29 +0600

    fix single drawing instead of multiple drawings

--- Lab-10/Sketchpad/src/main/java/Gallery.java
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -30,7 +30,15 @@ public class Gallery {
         canvas.show();
     }
 }
-// Q1.Which class is abstract, and why nobody may construct it.
+// Q1.Which class is abstract, and why nobody may construct it?
 // Ans: for my case, Shape and ShapeException classes are abstract.
-//      a shape without any definition of how it looks should not be constructed
-//        and ShapeException is
+//      a shape without any definition of how it looks should not be
+//      constructed and ShapeException is a general exception which
+//      should not be called, instead specific exceptions should be called
+// Q2. Which parent class you left concrete, and why?
+// Ans: Rectangle class was left concrete because a rectangle shape is a
+//      valid shape which should be allowed to be created.
+// Q3. One method you overrode even though the parent implemented it, and why?
+// Ans: the setWidth method was implemented in Rectangle, but it was overridden
+//      by Square's setWidth becuase both the width and height should be
+//      changed and be equal to eact other.
