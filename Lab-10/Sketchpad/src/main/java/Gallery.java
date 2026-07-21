17344ce7c01e9193269d74261487cfe6ea25aa7d
ayiham-1012 <ayiham1012@gmail.com>
2026-07-21 14:24:45 +0600

    done :')

--- Lab-10/Sketchpad/src/main/java/Gallery.java
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -3,49 +3,70 @@ import java.util.ArrayList;
 public class Gallery {
     private Canvas c;
     private ArrayList<Shape> shapes = new ArrayList<>();
-    private ArrayList<String> skipped = new ArrayList<>();
+    private ArrayList<Shape> skipped = new ArrayList<>();
+    private ArrayList<Shape> waiting  =new ArrayList<>();
+    private ArrayList<String> reasons  =new ArrayList<>();
 
 
     Gallery(Canvas c){
         this.c=c;
     }
 
-    public void add(Shape s){
+    private void draw(Shape s){
         if(s.getWidth() > c.width() || s.getHeight() > c.height() || s.area() > c.capacity()){
-            skipped.add(s.toString()+ "was too large");
             throw new ShapeTooLarge("Shape too large!");
         }
 
 
         if(s.area() < 1) {
-            skipped.add(s.toString()+ "was too large");
-            throw new ShapeTooSmall("Shape too small");
+            throw new ShapeTooSmall("Shape too small!");
         }
 
+    }
+
+    public void add(Shape s){
+
         shapes.add(s);
+        try{
+            draw(s);
+            waiting.add(s);
+        }
+        catch (Exception e){
+            skipped.add(s);
+            reasons.add(e.getMessage());
+        }
     }
 
     public String waiting(){
         int count = 1;
-        String out = "In waiting:\n";
+        String out = "";
+
         for(Shape s: shapes){
+            out += (count+". "+s.toString()+"\n");
+            count++;
+        }
+        return out;
+    }
+
+    public void render(){
+
+        int count = 1;
+        String out = "Is drawn:\n";
+        for(Shape s: waiting){
             out += (count+". "+s.toString()+"\n");
             count++;
         }
 
         int skipCount = 1;
-        out += "\nSkipped";
-        for(String k: skipped){
-            out += (skipCount+k+"\n");
+        out += "\nSkipped:\n";
+        for(int i=0; i < skipped.size(); i++){
+            Shape k = skipped.get(i);
+            out += (skipCount+". "+k.toString());
+            out += (" ("+ reasons.get(i) +")\n");
             skipCount++;
         }
+        System.out.println(out);
 
-                return  out;}
-
-    public void render(){
-        for(Shape s: shapes){
-            System.out.println(s.toString() + " is drawn");
-        }
         shapes.clear();
     }
 }
