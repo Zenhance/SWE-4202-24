9c95e58a9ca01451b23f1c73534bccc2336a96b1
Samin-init <mhsamin10@gmail.com>
2026-07-21 10:02:22 +0600

    Main part of Rectangle completed

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -3,6 +3,9 @@ public class Rectangle extends Shape {
     private double width;
     private double height;
     public Rectangle(double width, double height) {
+
+        requirePositive(width, "Width");
+        requirePositive(height, "Height");
         this.width = width;
         this.height = height;
     }
@@ -20,17 +23,33 @@ public class Rectangle extends Shape {
         return height;
 
  }
- @Override
- public double area(){
-        return 0.0;
- }
- @Override
-    public String describe(){
-        return "Rectangle";
+    public void setWidth(double width) {
+        requirePositive(width, "Width");
+        this.width = width;
+    }
+    public void setHeight(double height) {
+        requirePositive(height, "Height");
+        this.height = height;
     }
     @Override
-    public void draw(Canvas canvas) throws ShapeException{
+    public double area() {
+        return width * height;
+    }
 
+    @Override
+    public String describe() {
+        return String.format(
+                "%s(width=%.2f, height=%.2f, area=%.2f)",
+                getClass().getSimpleName(),
+                width,
+                height,
+                area()
+        );
+    }
+
+    @Override
+    protected void paint(Canvas canvas) {
+        canvas.rectangle(width, height);
     }
 
 }
