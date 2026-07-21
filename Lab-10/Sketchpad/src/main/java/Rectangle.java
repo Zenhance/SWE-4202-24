2be38ac7fabfa23910510c517c16ff15deaef024
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-21 11:54:08 +0600

    write : create and copy contents from circle class to modify

---
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -0,0 +1,42 @@
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Width and Height must be positive");
+        }
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void draw(Canvas canvas) throws ShapeException {
+        double calArea = area();
+        if (calArea < 1.0) {
+            throw new ShapeTooSmallException("Too small");
+        }
+        if (calArea > canvas.capacity()) {
+            throw new ShapeTooLargeException("Too Large");
+        }
+        canvas.circle(radius);
+    }
+
+    @Override
+    public String describe() {
+        return "Circle [radius=" + radius + ", area=" + String.format("%.2f", area()) + "]";
+    }
+}
