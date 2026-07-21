55384eb728dd7d1a142ae0cdf313124cf73b7ee5
sabbir240042147 <eftesabbir2725@gmail.com>
2026-07-21 12:07:34 +0600

    gallery

---
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gallery {
+    private Canvas canvas;
+    private List<Shape> shapes=new ArrayList<>();
+
+    public Gallery(Canvas canvas){
+        this.canvas=canvas;
+    }
+    public void add(Shape shape){
+        shapes.add(shape);
+    }
+}
