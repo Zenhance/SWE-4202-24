ec72b4190d4f15f94353f64ae3fcc343541e1ca3
Tasauf Abdullah <tasaufalter@gmail.com>
2026-07-21 11:26:45 +0600

    change : override draw method on child class circle to abstract parent class shape

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -18,4 +18,12 @@ public class Circle extends Shape{
         return "This is a circle with" + radius + "and area" + area();
     }
 
+    @Override
+    public void draw(Canvas canvas) throws ShapeException{
+        if (area() < 1) throw new ShapeTooSmallException("area cannot be less than 1");
+        else if (area() > canvas.capacity()) throw new ShapeTooLargeException("area cannot be greater than" + canvas.capacity());
+
+        canvas.circle(radius);
+    }
+
 }
