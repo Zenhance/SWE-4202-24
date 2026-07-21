3bc875f0848cd2902f5769428a9cc96aa2baa591
sameer-abdullah-240042107 <sameerabdullah1117@gmail.com>
2026-07-21 11:09:22 +0600

    4

---
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -0,0 +1,9 @@
+package main.java;
+
+import java.awt.*;
+
+public abstract class Shape {
+    public abstract double area();
+    public abstract String describe();
+    public abstract void draw(Canvas canvas) throws ShapeException;
+}
