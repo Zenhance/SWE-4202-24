a99c32d947d6f55f3a6fde01caa8f88468d45bc7
waseefmunir-max <waseef05@gmail.com>
2026-07-21 09:11:41 +0600

    Shape.java: Add draw method to Shape class and area validation and painting logic

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,3 +1,27 @@
 public abstract class Shape {
     public abstract double area();
+
+    protected abstract void paint(Canvas canvas);
+
+    public String describe(){
+        return getClass().getSimpleName() + "(area=" + area() +")";
+    }
+
+    public void draw(Canvas canvas) throws ShapeException {
+        double shapeArea = area();
+
+        if (shapeArea < 1) {
+            throw new ShapeTooSmallException(
+                    describe() + " will not cover even one cell of canvas"
+            );
+        }
+
+        if (shapeArea > canvas.capacity()) {
+            throw new ShapeTooLargeException(
+                    describe() + " exceeds canvas capacity " + canvas.capacity() + "."
+            );
+        }
+
+        paint(canvas);
+    }
 }
