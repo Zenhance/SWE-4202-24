3d87b2f8bf4b9b4ae579703ec7ed8d75d42dab21
arnoboic <arnobarafat@iut-dhaka.edu>
2026-07-21 08:44:44 +0600

    Finished Shape and Circle classes

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -1,4 +1,28 @@
-public class Circle extends Gallery {
+public class Circle extends Shape {
+    private double radius;
+    private final double PI = 3.1416;
 
-    public Circle(double r)
+    public Circle(double radius){
+        if (radius < 0) throw new IllegalArgumentException("radius cannot be negative");
+
+        this.radius = radius;
+    }
+
+    @Override
+    public double area(){
+        return PI*radius*radius;
+    }
+
+    @Override
+    public String describe() {
+        return "This is a circle with" + radius + "and area" + area();
+    }
+
+    @Override
+    public void draw(Canvas canvas) throws ShapeException{
+        if (area() < 1) throw new ShapeTooSmallException("area cannot be less than 1");
+        else if (area() > canvas.capacity()) throw new ShapeTooLargeException("area cannot be greater than" + canvas.capacity());
+
+        canvas.circle(radius);
+    }
 }
--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,2 +1,7 @@
 public abstract class Shape {
+    public abstract double area();
+
+    public abstract String describe();
+
+    public abstract void draw(Canvas canvas) throws ShapeException;
 }
