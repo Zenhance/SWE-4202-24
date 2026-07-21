a1055e22212c947de2cf535c6c5050c5c088ca8e
md-musaddik <musaddikibna1@gmail.com>
2026-07-21 11:07:42 +0600

    ShapeException Class created and and added the constructor.

---
+++ Lab-10/Sketchpad/src/main/java/ShapeException.java
@@ -0,0 +1,7 @@
+public abstract class ShapeException extends Exception {
+    private Shape shape;
+    protected ShapeException(String message, Shape shape) {
+        super(message);
+        this.shape = shape;
+    }
+}
