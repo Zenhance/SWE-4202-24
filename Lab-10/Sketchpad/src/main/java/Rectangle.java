3488d46e1fbad49c5b5c97ef535ee5b1abfc2962
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-21 09:30:36 +0600

    adding methods

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -5,5 +5,14 @@ public class Rectangle {
 
     public Rectangle(double width, double height) {
 
+        if (width <= 0 || height <=0 ) {
+            throw new IllegalArgumentException("Width and height has to be positive");
+        }
+        this.width = width;
+        this.height = height;
+    }
+
+    public double area() {
+        return width*height;
     }
 }
