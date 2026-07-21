b9ff969904ba6d63c99ddb36e681adc697300621
Rakin <rakinhasan001@gmail.com>
2026-07-21 09:33:39 +0600

    Rectangle class completed

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -1,16 +1,32 @@
 public class Rectangle extends Shape{
-    public Rectangle(int length, int width){
+    protected double length;
+    protected double  width;
+    public Rectangle(double length, double width){
+        if(width<=0){
+            throw new IllegalArgumentException("Invalid Width");
+        }
+        if(length<=0){
+            throw new IllegalArgumentException("Invalid length");
+        }
+        this.length=length;
+        this.width=width;
 
     }
+    public double getLength(){
+        return length;
+    }
+    public double getWidth(){
+        return width;
+    }
     public double area(){
-
+    return length*width;
     }
 
     public String describe(){
-
+        return String.format("Length %.2f , Width %.2f , area= %.2f",length,width,area());
     }
 
     public void render(Canvas canvas){
-
+    canvas.rectangle(width,length);
     }
 }
