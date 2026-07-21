50732404c41c7b6dc1fcc529623a1f9c596c0fcc
waseefmunir-max <waseef05@gmail.com>
2026-07-21 09:29:18 +0600

    Circle.java: Extend Shape class and add radius validation in constructor

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -1,2 +1,11 @@
-public class Circle {
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        if (radius <= 0) {
+            throw new IllegalArgumentException("Circle must be greater than zero.");
+        }
+
+        this.radius = radius;
+    }
 }
