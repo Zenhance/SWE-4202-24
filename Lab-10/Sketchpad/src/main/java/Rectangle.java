575370995f207319222a419f88e487d96e1dc961
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-07-21 11:39:06 +0600

    Added Rectangle class

---
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -0,0 +1,34 @@
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        requirePositive(width);
+        requirePositive(height);
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setWidth(double width) {
+        requirePositive(width);
+        this.width = width;
+    }
+    public void setHeight(double height) {
+        requirePositive(height);
+        this.height = height;
+    }
+
+    @Override
+    protected void make(Canvas canvas) {
+        canvas.rectangle(width, height);
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+
+    @Override
+    public String description(){
+        return "Rectangle";}
+}
