5f83db929a1351332071b582f48c373833ed3f44
wasifjafar <wasifjafar71@gmail.com>
2026-07-21 08:45:24 +0600

    create skeleton Gallery class

---
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Gallery {
+    private Canvas canvas;
+    private ArrayList<Shape> shapes;
+
+    public Gallery(Canvas canvas) {
+        this.canvas = canvas;
+        shapes = new ArrayList<>();
+    }
+}
