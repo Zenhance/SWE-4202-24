9bcc38e275f34e5ae2f9ad16f89dc94e461253b1
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-21 08:53:22 +0600

    added getters

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -8,5 +8,11 @@ public class Rectangle extends Shape{
         this.width = width;
         this.height = height;
     }
+    public double getWidth(){
+        return width;
+    }
+    public double getHeight(){
+        return height;
+    }
 
 }
