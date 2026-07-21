c70334b6ac9987a5facd60088678962237e5cff5
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-21 09:37:16 +0600

    init commit

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,7 +1,7 @@
-public class Shape {
+public abstract class Shape {
     public abstract double area();
     public abstract void draw(Canvas canvas) throws ShapeTooLargeException,ShapeTooSmallException;
     public String describe(){
-        return String.format("%s (area=%.2f)",getClass(),getSimpleName(),area());
+        return String.format("%s (area=%.2f)",getClass().getSimpleName(),area());
     }
 }
