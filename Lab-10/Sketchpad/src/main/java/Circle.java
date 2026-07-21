d328788c2893310b84704f8b649dd391e1728bc2
Muhtasim Moin <muhtasi13@gmail.com>
2026-07-21 08:37:16 +0600

    Circle class updated

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -1,14 +1,24 @@
 public class Circle extends Shape {
+        private double radius;
+        public Circle(double radius){
+            if (radius<=0){
+                throw new  IllegalArgumentException("Radius cannot be negative");
+            }
+            this.radius=radius;
+        }
 
+    public double getRadius() {
+        return radius;
+    }
 
     @Override
     public double area() {
-        return 0;
+        return Math.PI*radius*radius;
     }
 
     @Override
     public String describe() {
-        return "";
+        return String.format("Circle(radius=%.2f,area=%.2f)",radius,area());
     }
 
     @Override
