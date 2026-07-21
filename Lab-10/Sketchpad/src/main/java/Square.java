3e38ccd72e404e825d0ba96de23a1d3e5a5c1fa2
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-21 09:29:34 +0600

    made square pass compilation

--- Lab-10/Sketchpad/src/main/java/Square.java
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -1,13 +1,6 @@
 public class Square extends Rectangle {
 
     public Square(double side) {
-        super((int) side, (int) side);
-    }
-
-    public void setWidth(int i) {
-    }
-
-    public String describe() {
-        return null;
+        super(side, side);
     }
 }
