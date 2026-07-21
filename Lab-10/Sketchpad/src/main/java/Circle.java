d348fadf84a0b1021a3f55b3090d61bfb8e9ec43
LENOVO <tanviribnezaman@iut-dhaka.edu>
2026-07-21 12:07:30 +0600

    Circle class

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,26 @@
+public class Circle extends Shape{
+    private double radius;
+
+    public Circle (double radius){
+
+        if(radius<0)
+            throw new IllegalArgumentException("Radius can't be negative");
+        this.radius=radius;
+    }
+
+    @Override
+    public double area() {
+        return 3.14 * radius * radius;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius)
+    {
+        this.radius=radius;
+    }
+
+
+}
