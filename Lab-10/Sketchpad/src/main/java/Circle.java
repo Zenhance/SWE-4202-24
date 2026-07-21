19c1276543841e228263cd0a60cf06573fad7315
Samin-init <mhsamin10@gmail.com>
2026-07-21 09:05:23 +0600

    Circle class created

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,23 @@
+public class Circle extends Shape {
+
+        private double radius;
+
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+
+        @Override
+        public double area() {
+            return 0.0;
+        }
+
+        @Override
+        public String describe() {
+            return "Circle";
+        }
+
+        @Override
+        public void draw(Canvas canvas) throws ShapeException {
+        }
+    }
+
