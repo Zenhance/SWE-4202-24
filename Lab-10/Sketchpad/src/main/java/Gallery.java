e95b3767799c75ec6290a1b0fa61e8ea06aa0bd8
arnoboic <arnobarafat@iut-dhaka.edu>
2026-07-21 09:26:59 +0600

    Finished Gallery class

--- Lab-10/Sketchpad/src/main/java/Gallery.java
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -1,7 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gallery {
     private Canvas canvas;
+    private List<Shape> shapes = new ArrayList<>();
 
     public Gallery(Canvas canvas){
         this.canvas = canvas;
+    }
+
+    public void add(Shape shape) {
+        shapes.add(shape);
+    }
+
+    public int waiting() {
+        return shapes.size();
+    }
+
+    public void render() {
+        int drawn = 0;
+        int skipped = 0;
+
+        ArrayList<String> report = new ArrayList<>();
+
+        for (Shape shape : shapes){
+            try {
+                shape.draw(canvas);
+                drawn++;
+            }
+            catch(ShapeException e){
+                skipped++;
+                report.add(e.getMessage());
+            }
+        }
+
+        System.out.println("Drawn = " + drawn);
+        System.out.println("SKipped = "+ skipped);
+
+        for (String r : report){
+            System.out.println(r);
+        }
+
+        canvas.show();
     }
 }
