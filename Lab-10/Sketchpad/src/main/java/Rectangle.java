ed525a223ff93bf23381160ce6cdc0699efcb349
wasifjafar <wasifjafar71@gmail.com>
2026-07-21 08:37:16 +0600

    create skeleton Rectangle class's width and height fields and constructor

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -1,4 +1,11 @@
 public class Rectangle extends Shape{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
 
     @Override
     public String describe() {
