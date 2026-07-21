21c62653b2d81ec7010c5d8baae862ab532c5084
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-07-21 11:45:32 +0600

    Added Square class

---
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -0,0 +1,26 @@
+public class Square extends Rectangle {
+    public Square(double width, double height) {
+        super(width, height);
+    }
+    public Square(double length) {
+        super(length, length);
+    }
+
+
+    @Override
+    public String describe(){
+        return "Square";
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+
+    @Override
+    public void setHeight(double height) {
+        super.setHeight(height);
+        super.setWidth(height);
+    }
+}
