31c0258bb68b7da83269411cdb36a68e5f17a240
anonymous <farnaz28.rahman@gmail.com>
2026-07-21 11:40:04 +0600

    shape class

---
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -0,0 +1,7 @@
+public abstract class Shape {
+    public abstract double area();
+    public abstract String getDescription();
+    public abstract void draw(Canvas canvas) throws ShapeDrawException;
+
+
+}
