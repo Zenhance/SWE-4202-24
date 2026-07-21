39d5eed1ee9abf68e3126e20b1c604110582dcc5
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-21 08:56:53 +0600

    init commit

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -1,7 +1,7 @@
 public class Circle extends Shape{
     private final double radius;
     public Circle(double radius) {
-        if (radius < 0) {
+        if (radius <= 0) {
             throw new IllegalArgumentException("radius cannot be negative");
         }
         this.radius = radius;
@@ -14,8 +14,12 @@ public class Circle extends Shape{
         return Math.PI*radius*radius;
         }
         @Override
-    public String describe(){
+        public String describe(){
         return String.format("Circle(radius=%.2f,area=%.2f)",radius,area());
+        }
+        @Override
+        public void draw(Canvas canvas) throws ShapeTooLargeException,ShapeTooSmallException{
+        double diameter = 2* radius;
         }
 
 
