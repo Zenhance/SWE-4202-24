3bcd759a0ebb96d658492c6fbbb64a40c978b562
wasifjafar <wasifjafar71@gmail.com>
2026-07-21 08:49:06 +0600

    create getter and setter for Circle class's radius field

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -9,6 +9,14 @@ public class Circle extends Shape{
         return 3.14 * radius * radius;
     }
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
     @Override
     public String describe() {
         return "";
