cadb8afe7c6eed10f1cfe56b00ad131a34031dc1
md-musaddik <musaddikibna1@gmail.com>
2026-07-21 11:57:39 +0600

    Square class completed

---
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -0,0 +1,15 @@
+public class Square extends Shape{
+    public Square(double side) {
+        super(side, side);
+    }
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+    @Override
+    public void setHeight(double height) {
+        super.setHeight(height);
+        super.setWidth(height);
+    }
+}
