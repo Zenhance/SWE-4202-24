c6d1a372b5a9b4241b17a6a2ca55288d4050582e
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-21 08:53:20 +0600

    completed Shape class

---
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -0,0 +1,5 @@
+public abstract class Shape{
+    public abstract double area();
+    public abstract String describe();
+    public abstract void draw(Canvas canvas) throws ShapeDrawingException;
+}
