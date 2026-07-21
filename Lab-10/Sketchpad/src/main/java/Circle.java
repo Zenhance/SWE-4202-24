bfb87aa099d2f187df460cd5a2aea277091941fd
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-21 09:33:53 +0600

    completed circle class

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -6,5 +6,22 @@ public class Circle extends Shape{
         }
         this.radius = radius;
     }
-
+    @Override
+    public double area(){
+        return Math.PI * radius * radius;
+    }
+    @Override
+    public String describe(){
+        return "Circle(radius=" + radius + ", area=" + area() + ")";
+    }
+    @Override
+    public void draw(Canvas canvas) throws ShapeDrawingException{
+        if(area() < 1){
+            throw new ShapeTooSmallException("Circle area is smaller than one cell");
+        }
+        if(area() > canvas.capacity()){
+            throw new ShapeTooLargeException("Circle area is larger than canvas capacity");
+        }
+        canvas.circle(radius);
+    }
 }
