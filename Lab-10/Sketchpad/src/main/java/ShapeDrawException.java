28a9b6455d0012647ee4be62bd98004627e82095
Rakin <rakinhasan001@gmail.com>
2026-07-21 09:00:03 +0600

    ShapeDraw Exception constructed

--- Lab-10/Sketchpad/src/main/java/ShapeDrawException.java
+++ Lab-10/Sketchpad/src/main/java/ShapeDrawException.java
@@ -1,2 +1,11 @@
-public class ShapeDrawException {
+public abstract class ShapeDrawException extends Exception {
+    private final Shape shape;
+
+    protected ShapeDrawException(String message,Shape shape){
+        super(message);
+        this.shape=shape;
+    }
+    public Shape getShape(){
+        return shape;
+    }
 }
