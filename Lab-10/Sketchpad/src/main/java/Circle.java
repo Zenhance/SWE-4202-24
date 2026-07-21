0a149e1a3b7abe89f745a3b385195d9eec1d0401
tryhardFuad <mubtasimsmail@gmail.com>
2026-07-21 11:27:52 +0600

    feat: created constructor and getter,setter for Circle class

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,19 @@
+public class Circle extends Shape {
+    private double radius;
+    public Circle(double radius) {
+        if(radius <= 0){
+            throw new IllegalArgumentException("radius must be positive");
+        }
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        if(radius <= 0){
+            throw new IllegalArgumentException("radius must be positive");
+        }
+        this.radius = radius;
+    }
+
+}
