54857f8651ae35f2a448632e9e30e79cb7dbaaf3
Tasauf Abdullah <tasaufalter@gmail.com>
2026-07-21 11:31:56 +0600

    added constructor to rectangle class

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -1,2 +1,13 @@
 public class Rectangle extends Shape{
+
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        if (width <  0 || height < 0) throw new IllegalArgumentException("width or height cannot be negative");
+
+        this.height = height;
+        this.width = width;
+    }
+
 }
