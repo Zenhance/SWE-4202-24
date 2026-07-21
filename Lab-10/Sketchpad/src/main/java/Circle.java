10a3707cb4fcdd8c6aef6839217ec083e0c60f12
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-21 09:04:09 +0600

    init commit

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -1,31 +1,38 @@
-public class Circle extends Shape{
+public class Circle extends Shape {
     private final double radius;
+
     public Circle(double radius) {
         if (radius <= 0) {
             throw new IllegalArgumentException("radius cannot be negative");
         }
         this.radius = radius;
     }
-        public double getRadius(){
-            return radius;
-        }
-        @Override
-        public double area(){
-        return Math.PI*radius*radius;
-        }
-        @Override
-        public String describe(){
-        return String.format("Circle(radius=%.2f,area=%.2f)",radius,area());
-        }
-        @Override
-        public void draw(Canvas canvas) throws ShapeTooLargeException,ShapeTooSmallException{
-        double diameter = 2* radius;
-        }
-        if(diameter>canvas.getWidth() || diameter > canvas.getHeight()){
-            throw new ShapeTooLargeException(describe()+ "doesn't fit on the canvas");
+
+    public double getRadius() {
+        return radius;
     }
 
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
 
+    @Override
+    public String describe() {
+        return String.format("Circle(radius=%.2f,area=%.2f)", radius, area());
+    }
 
+    @Override
+    public void draw(Canvas canvas) throws ShapeTooLargeException, ShapeTooSmallException {
+        double diameter = 2 * radius;
+
+        if (diameter > canvas.getWidth() || diameter > canvas.getHeight()) {
+            throw new ShapeTooLargeException(describe() + "doesn't fit on the canvas");
+        }
+        if (diameter < 1) {
+            throw new ShapeTooSmallException(describe() + "is too small to cover a cell");
+        }
+        canvas.paintCircle(radius);
 
     }
+}
