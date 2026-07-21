dc617744cfa0a9c32504a3fce3900d1a25dc2a9b
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-21 08:58:58 +0600

    created too methods that will also handle the exceptions of too large and too small and describe the reasoning as well

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,8 +1,24 @@
 public abstract class Shape {
 
-    public abstract double area();
-
+    public abstract double area()
     public abstract String describe();
 
-    public abstract void draw(Canvas canvas) throws ShapeException;
-}
+
+    public final void draw(Canvas canvas) throws ShapeException {
+    {
+        double shapeArea=area();
+        if (shapeArea < 1.0) {
+            throw new ShapeTooSmallException(
+                    describe() + " covers less than one cell"
+            );
+        }
+        if(shapeArea> canvas.capacity()){
+            throw new ShapeTooLargeException(
+                    describe() + " exceeds canvas capacity " + canvas.capacity()
+            );
+
+            }
+        protected abstract void paint(Canvas canvas);
+
+    }
+    }
