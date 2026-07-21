96ece2faa8213dc18b1b741f2cfc4e1df0a87374
wasifjafar <wasifjafar71@gmail.com>
2026-07-21 08:38:22 +0600

    create skeleton Square class

---
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -0,0 +1,11 @@
+public class Square extends Rectangle{
+
+    public Square(double width) {
+        super(width, width);
+    }
+
+    @Override
+    public String describe() {
+        return super.describe();
+    }
+}
