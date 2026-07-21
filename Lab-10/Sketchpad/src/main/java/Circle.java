7b1a4f5f2ca539c61453a214eb68d95f61d8bf0b
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-07-21 11:38:16 +0600

    circle class

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -1,2 +1,10 @@
 public class Circle extends Shape{
+    private double radius;
+
+    public Circle (double radius){
+
+        if(radius<0)
+            throw new IllegalArgumentException("Radius can't be negative");
+        this.radius=radius;
+    }
 }
