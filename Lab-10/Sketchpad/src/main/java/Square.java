9b6abec5a23f60354d5b0656e828130fca029764
Tasauf Abdullah <tasaufalter@gmail.com>
2026-07-21 12:04:52 +0600

    bug fix to square class

--- Lab-10/Sketchpad/src/main/java/Square.java
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -1,17 +1,18 @@
-public class Square extends Shape{
-    public class Square extends Rectangle {
-        private double length;
-        public Square(double length){
-            super(length, length);
+public class Square extends Rectangle {
+    private double length;
+
+    public Square(double length) {
+        super(length, length);
         }
 
         @Override
         public String describe() {
-            return "This is a square with length "+ length + " and area " + area();
+            return "This is a square with length " + length + " and area " + area();
         }
 
         public void setWidth(int width) {
             this.length = width;
 
         }
-}
+    }
+
