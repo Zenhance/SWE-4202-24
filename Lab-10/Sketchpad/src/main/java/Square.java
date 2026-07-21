151c55dc8596fa8586f8a39e8905ade7241a804c
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-21 09:00:07 +0600

    overridden methods

--- Lab-10/Sketchpad/src/main/java/Square.java
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -1,5 +1,17 @@
 public class Square extends Rectangle{
     public Square(double side){
-        super(side);
+        super(side,side);
+    }
+    @Override
+    public void setWidth(double side){
+        requirePositive(side,"Side");
+        super.setWidth(side);
+        super.setHeight(side);
+    }
+    @Override
+    public void setHeight(double side){
+        requirePositive(side,"Side");
+        super.setWidth(side);
+        super.setHeight(side);
     }
 }
