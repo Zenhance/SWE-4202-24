29a1a87a0cc485334c665d573d32d57ff480fd8b
04-Labiba <sabihalabiba639@gmail.com>
2026-07-21 08:53:43 +0600

    init commit

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -14,4 +14,16 @@ public class Rectangle extends Shape{
     public double getHeight(){
         return height;
     }
+    public void setWidth(double width){
+        if(width<=0){
+            throw new IllegalArgumentException("width must be greater than 0.");
+        }
+        this.width=width;
+    }
+    public void setHeight(double height){
+        if(height<=0){
+            throw new IllegalArgumentException("Height must be greater than 0.");
+        }
+        this.height=height;
+    }
 }
