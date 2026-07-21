dafffe3635c08ec9c14bf59da7db93446c0dfcfd
Tasauf Abdullah <tasaufalter@gmail.com>
2026-07-21 12:13:01 +0600

    first part of Gallery class

--- Lab-10/Sketchpad/src/main/java/Gallery.java
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -1,2 +1,15 @@
 public class Gallery {
-}
+    private Canvas canvas;
+    private List<Shape> shapes = new ArrayList<>();
+
+    public Gallery(Canvas canvas){
+        this.canvas = canvas;
+    }
+
+    public void add(Shape shape) {
+        shapes.add(shape);
+    }
+
+    public int waiting() {
+        return shapes.size();
+    }
