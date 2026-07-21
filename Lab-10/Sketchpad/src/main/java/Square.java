9922c08250bb76d6e39c20c607f0bc13d23161ad
cse <tasintajwar24@iut-dhaka.edu>
2026-07-21 11:58:46 +0600

    Added Square Class

---
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -0,0 +1,30 @@
+public class Square extends Rectangle {
+    public Square(double width) {
+        super(width, width);
+    }
+
+    @Override
+    public double area() {
+        return width * width;
+    }
+
+    @Override
+    public String describe() {
+        return "Square";
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public void draw(Canvas canvas) throws ShapeException {
+        if (area() > canvas.capacity()) {
+            throw new ShapeTooLargeException("Square is too large");
+        }
+
+        if (area() < 1) {
+            throw new ShapeTooSmallException("Square is too small");
+        }
+    }
+}
