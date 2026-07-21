4092ca0b1faffad4e2043ad1ac6ac419b298425c
wasifjafar <wasifjafar71@gmail.com>
2026-07-21 09:41:26 +0600

    fix Circle class's add method to throw

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -24,7 +24,11 @@ public class Circle extends Shape{
     }
 
     @Override
-    public void draw(Canvas canvas) {
+    public void draw(Canvas canvas) throws ShapeException {
+        if (area() > canvas.capacity())
+            throw new ShapeTooLargeException("Shape too large exception");
+        if (area() < 1.0)
+            throw new ShapeTooSmallException("Shape too small exception");
         canvas.circle(radius);
     }
 }
