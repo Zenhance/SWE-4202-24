5cf2eb4d438632656190df67b1430a368f002f2d
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-21 08:49:56 +0600

    overridden methods

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -11,5 +11,12 @@ public class Circle extends Shape{
     public double area(){
         return Math.PI*radius*radius;
     }
-
+    @Override
+    public String describe(){
+        return String.format("Circle(radius = %.2f, area = %.2f)",radius, area());
+    }
+    @Override
+    protected void paint (Canvas canvas){
+        canvas.circle(radius);
+    }
 }
