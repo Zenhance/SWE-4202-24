2d60039b056a4a23dbebad4bf55668d603728277
LENOVO <tanviribnezaman@iut-dhaka.edu>
2026-07-21 12:09:50 +0600

    rectangle class

---
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -0,0 +1,74 @@
+public class Rectangle extends Shape {
+
+    protected double width;
+    protected double height;
+
+    public Rectangle(double width, double height) {
+
+
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Height and Weight can't be negative");
+        }
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return height*width;
+    }
+
+    @Override
+    public String describe() {
+        return "Rectangle";
+    }
+
+    @Override
+    public void draw(Canvas canvas) throws ShapeException {
+        if (canvas.capacity() < area()) {
+            throw new ShapeTooLargeException("Rectangle is too large");
+        }
+
+        if (area() < 1) {
+            throw new ShapeTooSmallException("Rectangle is too small");
+        }
+
+    }
+
+
+    public class Rectangle extends Shape {
+
+        protected double width;
+        protected double height;
+
+        public Rectangle(double width, double height) {
+
+
+            if (width < 0 || height < 0) {
+                throw new IllegalArgumentException("Height and Weight can't be negative");
+            }
+            this.width = width;
+            this.height = height;
+        }
+
+        @Override
+        public double area() {
+            return height*width;
+        }
+
+        @Override
+        public String describe() {
+            return "Rectangle";
+        }
+
+        @Override
+        public void draw(Canvas canvas) throws ShapeException {
+            if (canvas.capacity() < area()) {
+                throw new ShapeTooLargeException("Rectangle is too large");
+            }
+
+            if (area() < 1) {
+                throw new ShapeTooSmallException("Rectangle is too small");
+            }
+
+        }
