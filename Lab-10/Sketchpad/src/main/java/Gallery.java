144258325a763e218f0b676bd7bf6fcd1de9a20c
tryhardFuad <mubtasimsmail@gmail.com>
2026-07-21 12:37:40 +0600

    feat: created constructor and add, waiting method for gallery class

---
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gallery {
+    private Canvas canvas;
+    List<Shape> shapes = new ArrayList<>();
+
+    public Gallery(Canvas canvas) {
+        this.canvas = canvas;
+    }
+    public void add(Shape shape){
+        shapes.add(shape);
+    }
+    public int waiting(){
+        return shapes.size();
+    }
+
+
+}
