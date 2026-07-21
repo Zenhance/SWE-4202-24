d08c2b11f448f12e54cf4841a9a28383734fec73
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-21 09:08:40 +0600

    created a requirepositive method, it requires to the inputs be positive if not it will throw illegalexception

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,11 +1,11 @@
 public abstract class Shape {
 
-    public abstract double area()
+    public abstract double area();
     public abstract String describe();
 
 
     public final void draw(Canvas canvas) throws ShapeException {
-    {
+
         double shapeArea=area();
         if (shapeArea < 1.0) {
             throw new ShapeTooSmallException(
@@ -15,10 +15,19 @@ public abstract class Shape {
         if(shapeArea> canvas.capacity()){
             throw new ShapeTooLargeException(
                     describe() + " exceeds canvas capacity " + canvas.capacity()
-            );
+            );}
 
-            }
+            paint(canvas);
+        }
+
         protected abstract void paint(Canvas canvas);
 
+        protected static void requirePositive(double value, String name) {
+        if (value <= 0) {
+            throw new IllegalArgumentException(
+                    name + " must be a positive, finite number"
+            );
+        }
     }
-    }
+
+}
