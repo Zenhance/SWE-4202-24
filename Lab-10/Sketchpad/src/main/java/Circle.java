2dc635b50d6aaf48ccf375464f119157bdc8612a
shafin-75 <arafatshafin85@gmail.com>
2026-07-21 12:07:28 +0600

    Circle.java

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,8 @@
+public class Circle extends Shape{
+    private final double radius;
+
+    public Circle(double radius){
+        if(radius<=0) throw new IllegalArgumentException("radius must be positvie");
+        this.radius=radius;
+    }
+}
