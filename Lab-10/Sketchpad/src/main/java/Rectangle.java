2c531346ce9dca37c9e99563d9ac770f18cfcbda
iam-tahmid <tahmidim99@gmail.com>
2026-07-21 12:06:56 +0600

    All classes created

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -12,4 +12,5 @@ public class Rectangle extends Shape {
     public String describe() {
         return "";
     }
+
 }
--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,5 +1,12 @@
 package main.java;
 
+import java.awt.*;
+
 public abstract class Shape {
     public abstract String describe();
+
+    public abstract double area();
+
+    public abstract void draw(Canvas canvas) throws ShapeException;
+
 }
