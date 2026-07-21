4ee00af377abdf599ad75dfb8889eea7d2e7f1ab
Munman <214246799+bollamokles@users.noreply.github.com>
2026-07-21 09:08:29 +0600

    commit Square

---
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -0,0 +1,14 @@
+public class Square extends Rectangle{
+
+    public Square(int width, int height) {
+        super(width, height);
+    }
+
+    public void setWidth(double width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+    public String describe() {
+        super.describe();
+    }
+}
