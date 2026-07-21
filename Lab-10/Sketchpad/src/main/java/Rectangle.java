3ffbcae01e4fa92aac8902a31668377d182ace65
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-21 09:44:22 +0600

    adding methods

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -11,6 +11,16 @@ public class Rectangle extends Shape {
         this.width = width;
         this.height = height;
     }
+
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
     @Override
     public double area() {
         return width*height;
--- Lab-10/Sketchpad/src/main/java/Square.java
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -3,4 +3,8 @@ public class Square extends Rectangle{
     public Square(double side) {
         super(side, side);
     }
+
+    public double getSide() {
+
+    }
 }
