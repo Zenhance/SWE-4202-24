5a858e4bf150d5d43444b323a000107e6760d7a3
tryhardFuad <mubtasimsmail@gmail.com>
2026-07-21 12:58:49 +0600

    feat: added describe method

--- Lab-10/Sketchpad/src/main/java/Square.java
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -1,9 +1,16 @@
 public class Square extends Rectangle {
+    private double side;
     public Square(double side){
         super(side, side);
+        this.side = side;
     }
     public double setSide(double side){
         super.setHeight(side);
         super.setWidth(side);
+    }
+
+    @Override
+    public String describe() {
+        return "Square " + side +"";
     }
 }
