5e73cc635fbe76d51204fdb272fb652e9df55fad
Rakib <rakibulhasan4266@gmail.com>
2026-07-21 09:15:30 +0600

    Shape Complete

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,10 +1,23 @@
 public abstract class Shape
 {
-    public abstract double area;
-    public abstract void draw(Canvas canvas) throws ShapeDwawException;
+    public abstract double area();
+    public abstract void draw(Canvas canvas) throws ShapeDrawException;
     public String describe()
     {
         return String.format("%s with area %.2f", getClass().getSimpleName(), area());
     }
-
+    protected void checkFits(Canvas canvas) throws ShapeDrawException
+    {
+        double area = area();
+        if(area < 1.0)
+        {
+            throw new ShapeTooSmallException(
+                    describe() + " would not cover even one cell");
+        }
+        if (area > canvas.capacity())
+        {
+            throw new ShapeTooLargeException(
+                    describe() + " exceeds the canvas capacity of " + canvas.capacity());
+        }
+    }
 }
