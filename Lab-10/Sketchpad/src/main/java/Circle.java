8df2487a6596395ec7ea2e6ec3c8097e75ec0a63
tasnitaraida <tasnitar@gmail.com>
2026-07-21 12:12:32 +0600

    constructor getter

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -1,2 +1,18 @@
-public class Circle {
+public class Circle extends Shape{
+    private final double radius;
+
+    public Circle(double radius){
+        if(radius<=0){
+            throw new IllegalArgumentException("Circle radius cannot be negative");
+        }
+        this.radius=radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double area(){
+        return Math.PI*radius*radius;
+    }
 }
