9f2f4c023d6a71af0af0948053775e8e638607ba
wasif-22 <twasif68@gmail.com>
2026-07-21 11:27:25 +0600

    change: complete the rectangle class (1)

---
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle extends Shape {
+    protected double width;
+    protected double height;
+
+    public Rectangle(double width, double height) {
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public void setWidth(double width) {
+        if (width <= 0) throw new IllegalArgumentException("width must be positive");
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        if (height <= 0) throw new IllegalArgumentException("height must be positive");
+        this.height = height;
+    }
