6f8e50d9f73bc87965f12903039986fb2780a220
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-21 09:00:05 +0600

    adding constructor

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -2,6 +2,11 @@ public class Circle extends Shape{
 
     private double radius;
 
+    public Circle(double radius) {
+        this.radius = radius;
+        }
+    }
+
     @Override
     public abstract double area();
 
