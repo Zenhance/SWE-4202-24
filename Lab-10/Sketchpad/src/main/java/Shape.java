8c8599a94c4335ec655c27c6e7d62d6715a361c5
shafin-75 <arafatshafin85@gmail.com>
2026-07-21 11:24:44 +0600

    Done Shape.java

---
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public abstract class Shape {
+    public abstract double area();
+    public abstract String describe();
+
+    public abstract void draw(Canvas canvas) throws ShapeTooLargeException,ShapeTooSmallException;
+}
