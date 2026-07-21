bbaf823f42eda699f65f1d0da2396ce7431e5ef5
Rakin <rakinhasan001@gmail.com>
2026-07-21 09:14:33 +0600

    remaining part of Shape class constructed

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,10 +1,17 @@
 public abstract class Shape {
     public abstract double area();
-    public abstract String description();
+    public abstract String describe();
     protected abstract void render(Canvas canvas);
 
     public final void draw(Canvas canvas) throws ShapeTooLargeException,ShapeTooSmallException{
         double area=area();
+        if(area>canvas.capacity()){
+            throw new ShapeTooLargeException(this,canvas);
 
+        }
+        if(area<1){
+            throw new ShapeTooSmallException(this,canvas);
+        }
+        render(canvas);
     }
 }
