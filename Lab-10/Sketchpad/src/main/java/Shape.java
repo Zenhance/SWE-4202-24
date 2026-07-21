8dc3a7493845bda9cdbf2a2081343104a5fba8b5
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-07-21 11:28:39 +0600

    Added Shape abstract class

---
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -0,0 +1,24 @@
+public abstract class Shape {
+    public void draw(Canvas canvas) throws ShapeException{
+        if(canvas == null){
+            throw new IllegalArgumentException("Argument 'canvas' must not be null.");
+        }
+
+        if(area() <= 0){
+            throw new ShapeTooSmallException("Shape is too small.");
+        }
+        if(area()>canvas.capacity()){
+            throw new ShapeTooLargeException("Shape is too large.");
+        }
+        make(canvas);
+    }
+    public abstract double area();
+    protected abstract void make(Canvas canvas);
+    public abstract String desciption();
+
+    protected void requirePositive(double value){
+        if(!Double.isFinite(value)||value<0){
+            throw new IllegalArgumentException("The value needs to be positive.");
+        }
+    }
+}
