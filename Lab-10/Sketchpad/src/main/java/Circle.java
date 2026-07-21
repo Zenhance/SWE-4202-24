5cacb4dbd41ec6c29f757a114ebc0be1ad4230fb
Munman <214246799+bollamokles@users.noreply.github.com>
2026-07-21 09:02:41 +0600

    commit Circle

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,16 @@
+public class Circle extends Shape {
+    private double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public String describe(){
+        return "Circle(" + radius + ")";
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
