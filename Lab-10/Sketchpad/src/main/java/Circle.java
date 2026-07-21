0beb71d40da0863ccc0ecbde8b7311e99960ae10
Tasauf Abdullah <tasaufalter@gmail.com>
2026-07-21 10:58:43 +0600

    add constructor to Circle

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,11 @@
+public class Circle extends Shape{
+    private double radius;
+    private final double PI = 3.1416;
+
+    public Circle(double radius){
+        if (radius < 0) throw new IllegalArgumentException("radius cannot be negative");
+
+        this.radius = radius;
+    }
+
+}
---
+++ Lab-10/Sketchpad/src/main/java/ShapeException.java
@@ -0,0 +1,2 @@
+public class ShapeException {
+}
