023ef5cae0cabae74b7f95026f42f1641d4208db
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-21 09:32:24 +0600

    square is completed

--- Lab-10/Sketchpad/src/main/java/Square.java
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -3,4 +3,14 @@ public class Square extends Rectangle {
     public Square(double side) {
         super(side, side);
     }
+    @Override
+    public void setWidth(double side){
+        super.setWidth(side);
+        super.setHeight(side);
+    }
+    @Override
+    public void setHeight(double side){
+        super.setWidth(side);
+        super.setHeight(side);
+    }
 }
