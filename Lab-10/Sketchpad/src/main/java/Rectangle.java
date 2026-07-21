08c43ac8ba2cf5f0a349a93e0bd201092ccc8ae7
md-musaddik <musaddikibna1@gmail.com>
2026-07-21 11:36:50 +0600

    getter functions declared in rectangle class.

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -13,4 +13,11 @@ public class Rectangle extends Shape{
         this.width=width;
         this.height=height;
     }
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight(){
+        return height;
+    }
 }
