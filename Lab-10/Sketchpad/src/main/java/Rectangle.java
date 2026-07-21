040bd129d205e1bda548782547fcb2cf72a2bbab
shafin-75 <arafatshafin85@gmail.com>
2026-07-21 11:42:00 +0600

    done rectangle.java

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+public class Rectangle extends Shape{
+    protected double width;
+    protected double height;
 
-public class Rectangle {
+    public Rectangle(double width, double height){
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public void setWidth(double width){
+        if(width<=0) throw new IllegalArgumentException("width must be +ve ");
+        this.width=width;
+    }
+
+    public void setHeight(double height){
+        if(height<=0) throw new IllegalArgumentException("height must be +ve ");
+        this.height=height;
+    }
 }
