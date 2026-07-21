f83e0f6fa3506b23380037c2976af7c75cbd93d2
tahsin1r <naylakagi@gmail.com>
2026-07-21 09:51:52 +0600

    Answered the 4 questions from the readme file at the end of the Gallery class in comments

--- Lab-10/Sketchpad/src/main/java/Gallery.java
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -40,3 +40,15 @@ public class Gallery {
    }
 
 }
+/*
+1.ans: The shape class is abstract. Because shape itself cannot be created. It cannot be constructed because a shape doesnt have any specific attributes like sides or radius.
+
+2.ans: The Rectangle class is left concrete. Even though the rectangle is a parent class and its child class is square. Rectangle itself can be constructed and is a valid shape.
+
+3.ans: I overrrode the setWidth and setHeight method in the square class. Because otherwise according to the parent class only one of the sides would change whereas in the square shape we need to have same sides.
+
+4.ans: I did not override the area method in square class. because In parent class rectangle the area is length*width. Its the same for a sqaure if its height and width are the same.
+
+
+
+ */
