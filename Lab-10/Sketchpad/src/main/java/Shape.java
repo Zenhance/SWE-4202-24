e44077ff8bc60a3034b3a9949887fcb08eba13c9
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-07-21 11:31:47 +0600

    shape.java

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,6 +1,8 @@
 public abstract class Shape {
 
-    public abstract String describe;
+    public abstract String describe();
 
-    public abstract double area;
+    public abstract void draw(Canvas canvas) throws ShapeException;
+
+    public abstract double area();
 }
