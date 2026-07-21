aa2ce7c6bdc7fe97dc467155287ccc14ba1910c5
elfie1502 <mumtahina@iut-dhaka.edu>
2026-07-21 09:32:32 +0600

    In the Rectangle class draw method,describe and area method implemented!

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -8,5 +8,30 @@ public class Rectangle extends Shape {
         this.width = width;
         this.height = height;
     }
+    @Override
+    public double area() {
+        return width * height;
+    }
+
+    @Override
+    public String describe() {
+        return String.format("Rectangale %.1f %.1f", width, height);
+    }
+
+    @Override
+    public void draw (Canvas canvas) throws ShapeTooLargeException, ShapToolSmallException {
+        double area = area();
+
+        if (area > canvas.capacity()){
+            throw new ShapeTooLargeException(String.format("Rectangle area is too large %.2f exceeds canvas capacity %d", area,
+                    canvas.capacity()));
+        }
+        if (area < 1.0){
+            throw new ShapToolSmallException(
+                    String.format("Circle area %.2f is less than canvas capacity (1 cell)", area)
+            );
+        }
+        canvas.rectangle(width,height);
+    }
 
 }
