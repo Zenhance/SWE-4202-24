fcd17480a7337b5c9612f230cfe782aec2597ee0
iam-tahmid <tahmidim99@gmail.com>
2026-07-21 10:55:48 +0600

    skeleton Circle and Rectangle class created

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,16 @@
+package main.java;
+
+public class Circle extends Shape{
+    private double radius;
+
+    public Circle (double radius){
+        this.radius = radius;
+    }
+
+    @Override
+    public String describe() {
+        return "";
+    }
+
+
+}
---
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class Rectangle extends Shape {
+    @Override
+    public String describe() {
+        return "";
+    }
+}
