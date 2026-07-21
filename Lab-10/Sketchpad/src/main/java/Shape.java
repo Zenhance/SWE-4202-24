45737d33f80ff143309a5c7045f766355dd64dd3
tryhardFuad <mubtasimsmail@gmail.com>
2026-07-21 12:52:15 +0600

    fix: fixed implementation for draw method

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -3,9 +3,9 @@ import java.awt.*;
 public abstract class Shape {
     public abstract double area();
     public abstract void paint(Canvas canvas);
-    public String draw(Canvas canvas){
+    public void draw(Canvas canvas) throws ShapeException{
         double a = area();
-        if(a > canvas.capacity(){
+        if(a > canvas.capacity()){
             throw new ShapeTooLargeException(a,  canvas.capacity());
         }
         if(a < 1.0){
