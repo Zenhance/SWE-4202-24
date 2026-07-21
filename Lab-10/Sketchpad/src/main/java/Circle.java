9dc26c863540911b05c3c4508080676ff1bcf161
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-07-21 11:42:59 +0600

    circle class

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -8,5 +8,19 @@ public class Circle extends Shape{
         this.radius=radius;
     }
 
+    @Override
+    public double area() {
+        return 3.14 * radius * radius;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius)
+    {
+        this.radius=radius;
+    }
+
 
 }
