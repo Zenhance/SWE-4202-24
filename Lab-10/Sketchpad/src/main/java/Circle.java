2385368abae0546d61d82c25e6cd7869a3259955
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-21 08:46:47 +0600

    edited methods

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,13 @@
+public class Circle extends Shape{
+
+    @Override
+    public abstract double area();
+
+    @Override
+    public abstract String describe();
+
+    @Override
+    public abstract void draw();
+
+
+}
