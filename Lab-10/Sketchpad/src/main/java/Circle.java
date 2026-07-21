d2fe7a8c35be1e8e91b2145a1225394ea16a9d25
Maomao-starry <aminarahmanshalu@gmail.com>
2026-07-21 11:33:15 +0600

    task

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,34 @@
+public class Circle extends Shape{
+    private final double radius;
+    public Circle(double radius){
+        if(radius<=0)
+            throw new IllegalArgumentException("Radius is too smoll");
+        this.radius=radius;
+    }
+    public double getRadius(){
+        return radius;
+    }
+
+    @Override
+    public double area(){
+        return Math.PI*radius*radius;
+    }
+    @Override
+    public String describe(){
+        return String.format(Circle(radius=%.2f,area=%.2f),radius,area();
+    }
+    @Override
+    public void draw(Canvas canva)throws ShapeException{
+        double diameter=radius*2;
+        if(diameter>canva.getHeight || diameter>canva.getWidth)
+            throw new ShapeTooLargeException("Invalid");
+        if(diameter<1)
+            throw new ShapeTooSmallException("Invalid");
+        canva.circle(radius);
+    }
+
+
+
+
+
+}
