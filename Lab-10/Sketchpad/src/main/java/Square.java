fbf415590746bd159ecc6bea63aab66043199385
Rakin <rakinhasan001@gmail.com>
2026-07-21 09:46:01 +0600

    Square class completed

--- Lab-10/Sketchpad/src/main/java/Square.java
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -1,8 +1,29 @@
-public class Square {
+public class Square extends Rectangle{
+    private double side;
     public Square(double side){
+        super(side,side);
 
     }
+
+    @Override
+    public void resize(double newW, double newL){
+        if(newW != newL){
+            throw new IllegalArgumentException("A square must have equal sides, got " + newW +"x"+ newL);
+        }
+        super.resize(newW,newL);
+    }
+    public void resize(double newSide) {
+        resize(newSide,newSide);
+    }
+    @Override
+    public void setLength(double newL){
+        side=newL;
+    }
+    public void setWidth(double newW){
+        side= newW;
+    }
     public String describe() {
+        return String.format("Side %.2f , area= %.2f",side,area());
     }
 
     public void setWidth(int i) {
