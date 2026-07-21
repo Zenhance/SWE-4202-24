2303aeac8aac2e13a21b6061adf5eac37408014d
Rakib <rakibulhasan4266@gmail.com>
2026-07-21 09:06:20 +0600

    file Structure

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,2 +1,10 @@
-public class Shape {
+public abstract class Shape
+{
+    public abstract double area;
+    public abstract void draw(Canvas canvas) throws ShapeDwawException;
+    public String describe()
+    {
+        return String.format("%s with area %.2f", getClass().getSimpleName(), area());
+    }
+
 }
