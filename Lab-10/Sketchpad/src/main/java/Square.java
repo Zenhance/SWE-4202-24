28d745751cb1551b6a8eecd69b6884037f8ff509
sabbir240042147 <eftesabbir2725@gmail.com>
2026-07-21 11:58:19 +0600

    square

---
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class Square extends Rectangle{
+    private double length;
+    public Square(double length){
+        super(length,length);
+    }
+    public String describe(){
+        return "This is a square with length "+length;
+    }
+
+}
