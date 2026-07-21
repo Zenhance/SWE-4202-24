37cc8de0f0cac217536f2c9086e45ee31399d32a
wasifjafar <wasifjafar71@gmail.com>
2026-07-21 08:49:36 +0600

    create getter and setter for Rectangle class's width and height fields

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -11,4 +11,20 @@ public class Rectangle extends Shape{
     public String describe() {
         return "";
     }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
