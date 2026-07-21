ed049a2959d55354071feed7e5afdf4046380320
Rakib <rakibulhasan4266@gmail.com>
2026-07-21 09:30:08 +0600

    Rectangle class created

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -1,2 +1,14 @@
-public class Rectangle {
+public class Rectangle extends Shape
+{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException(
+                    "width and height must be positive, got " + width + " x " + height);
+        }
+        this.width = width;
+        this.height = height;
+    }
 }
