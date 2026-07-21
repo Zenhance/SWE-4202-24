1348a83389ac1663fd0a0068d50fc39a1d181e5b
arnoboic <arnobarafat@iut-dhaka.edu>
2026-07-21 09:54:54 +0600

    finished Lab 10 and four questions answered

--- Lab-10/Sketchpad/src/main/java/Gallery.java
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -46,3 +46,26 @@ public class Gallery {
 
     }
 }
+
+/*
+
+1.Which class is abstract, and why nobody may construct it.
+Ans: I left Shape class abstract. It would never be constructed
+     as it cannnot be define itself how much area it should take.
+     Again in cavas there are no methods to draw "Shape".
+
+2.Which parent class you left concrete, and why.
+Ans: I left ShapeException and Rectangle concrete.
+    In Main, I need to construct Rectangle.
+    That's why it had been left concrete.
+    Again in render() method of Gallery I
+    need to call ShapeException which is better
+     than calling both its child class using pipe operator(it is a bit reduntdant).
+3. One method you overrode even though the parent implemented it, and why.
+Ans: I overrode setwidth() method in Square eventhough Rectangle implemented it.
+     Because when changing width in square I also need to change height as height = width,
+     whereas in Rectangle I need to change its width only
+4. One method you deliberately did not override, and why.
+Ans: I did not overried in area() in Square as we can already implement it correctly
+     in parent class Rectangle.
+*/
