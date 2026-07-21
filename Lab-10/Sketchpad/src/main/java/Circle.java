4695c688313e6f0ae654bd6c359e892ddefcbf48
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-21 08:51:32 +0600

    init commit

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -13,6 +13,10 @@ public class Circle extends Shape{
         public double area(){
         return Math.PI*radius*radius;
         }
+        @Override
+    public String describe(){
+        return String.format("Circle(radius=%.2f,area=%.2f)",radius,area());
+        }
 
 
 
