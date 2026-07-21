8bae27bc8ac44f29e9e59cd07e4529a0ac1e1041
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-07-21 11:49:49 +0600

    rectangle class

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -1,2 +1,12 @@
 public abstract class Rectangle extends Shape{
+    private double width;
+    private double height;
+
+    public Rectangle(double width,double height){
+
+        if(width<0) throw new IllegalArgumentException("Width is not valid");
+
+        if(height<0) throw new IllegalArgumentException("Height is not valid");
+    }
+
 }
