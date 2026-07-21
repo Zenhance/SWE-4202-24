df418a60cf24862e03265e1a4897259e262fea09
Sadman Raiyan <kamrulseed@gmail.com>
2026-07-21 09:14:16 +0600

    Created Circle class

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,35 @@
+public class Circle extends Shape {
+    private double  radius;
+    public Circle(double radius) {
+        if(radius <= 0) {
+            throw new IllegalArgumentException("radius must be positive");
+        }
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI*radius*radius;
+    }
+
+    @Override
+    public String describe() {
+        return "Circle";
+    }
+
+    @Override
+    public void draw(Canvas canvas) throws ShapeException {
+        if(canvas.capacity()<area()){
+            throw new ShapeTooLargeException("Cirle too large");
+        }
+        if(area()<1){
+            throw new ShapeTooSmallException("Circle too small");
+        }
+
+
+    }
+}
