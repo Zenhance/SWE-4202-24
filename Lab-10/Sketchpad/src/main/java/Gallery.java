0f29ab303d335499e95743e550a8c6ef84a09a37
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-21 09:03:39 +0600

    added private fields and constructor

--- Lab-10/Sketchpad/src/main/java/Gallery.java
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -1,3 +1,12 @@
 import java.util.ArrayList;
 public class Gallery {
+    private final Canvas canvas;
+    private final ArrayList<Shape> shapes;
+    public Gallery(Canvas canvas){
+        if(canvas == null){
+            throw new IllegalArgumentException("Canvas cannot be null.")
+        }
+        this.canvas = canvas;
+        this.shapes = shapes;
+    }
 }
