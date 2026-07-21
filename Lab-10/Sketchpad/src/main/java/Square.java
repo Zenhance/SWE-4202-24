520766b8428bf99a6c98fba56e453bc42fb895b1
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-21 09:51:56 +0600

    completed square class which extends from rectangle....

--- Lab-10/Sketchpad/src/main/java/Square.java
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -1,4 +1,25 @@
 public class Square extends Rectangle{
     public Square(double side) {
-        super(side, side);
+        super(side,side);
     }
+    @Override
+    public void setWidth(double width) {
+        if(width <= 0) {
+            throw new IllegalArgumentException("Side must be positive");
+        }
+        this.width = width;
+        this.height = width;
+    }
+    @Override
+    public void setHeight(double height) {
+        if(height <= 0) {
+            throw new IllegalArgumentException("Side must be positive");
+        }
+        this.width = height;
+        this.height = height;
+    }
+    @Override
+    public String describe() {
+        return "Square(side=" + width + ", area=" + area() + ")";
+    }
+}
