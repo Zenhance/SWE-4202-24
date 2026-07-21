db889eab239edae0fa25dc3884c8efe86f0cfbd0
waseefmunir-max <waseef05@gmail.com>
2026-07-21 10:06:31 +0600

    Rectangle.java: Add validation for width and height, implement area calculation and paint method

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -3,7 +3,40 @@ public class Rectangle extends Shape {
     private double height;
 
     public Rectangle(double width, double height) {
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Width and height of rectangle must be greater than zero.");
+        }
         this.width = width;
         this.height = height;
+    }
+
+    @Override
+    public double area(){
+        return height * width;
+    }
+
+    public void setWidth(double width) {
+        if (width <= 0) {
+            throw new IllegalArgumentException(
+                    "Rectangle width must be greater than zero."
+            );
+        }
+
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        if (height <= 0) {
+            throw new IllegalArgumentException(
+                    "Rectangle height must be greater than zero."
+            );
+        }
+
+        this.height = height;
+    }
+
+    @Override
+    protected void paint(Canvas canvas) {
+        canvas.rectangle(width, height);
     }
 }
