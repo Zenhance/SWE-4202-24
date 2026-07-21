bf7e9ba05632dd3e050d9fedd61f28668acad547
arnoboic <arnobarafat@iut-dhaka.edu>
2026-07-21 08:49:09 +0600

    Finished Rectangle class

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -1,5 +1,30 @@
 public class Rectangle extends Shape{
-    public Rectangle(int i, int i1) {
-        super();
+
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        if (width <  0 || height < 0) throw new IllegalArgumentException("width or height cannot be negative");
+
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return width*height;
+    }
+
+    @Override
+    public String describe() {
+        return "This is a rectangle with width "+ width + " and height " + height + " and area " + area();
+    }
+
+    @Override
+    public void draw(Canvas canvas) throws ShapeException {
+        if (area() < 1) throw new ShapeTooSmallException("area cannot be less than 1");
+        else if (area() > canvas.capacity()) throw new ShapeTooLargeException("area cannot be greater than" + canvas.capacity());
+
+        canvas.rectangle(width, height);
     }
 }
