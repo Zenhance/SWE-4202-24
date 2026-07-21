b4c4bec312dedbd0e538df1b5374ce25835f4cb2
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-21 09:28:05 +0600

    init commit

--- Lab-10/Sketchpad/src/main/java/Gallery.java
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -4,4 +4,7 @@ public class Gallery {
     private final List<Shape> shapes = new ArrayList<>();
     private final List<String> skippedReasons = new ArrayList<>();
     private int DrawnCount = 0;
+    public void add(Shape shape){
+        shapes.add(shape);
+    }
 }
