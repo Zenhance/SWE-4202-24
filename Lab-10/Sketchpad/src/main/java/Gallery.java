a617d7dd2198d4ef9c3c779c773c5525a563ae36
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-07-21 12:18:27 +0600

    questions

--- Lab-10/Sketchpad/src/main/java/Gallery.java
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -30,6 +30,19 @@ public class Gallery {
     } canvas.show();
 }  }
 
+// Q1.in my case Shape and Shape exception are abstract, because
+// shape without any definition of how it looks should not be
+//      constructed and ShapeException is a general exception which
+//      should not be called, instead specific exceptions should be called.
+
+//Q2.I left Rectangle CLass concrete because a rectangle shape is a valid shape which should
+//be allowed to be created.
+
+//Q3.the setWidth method was overridden by square's setWidth
+// though being implemented at Rectangle,because both set and width should be changed and
+//be equal to each other
+
+
 
 
 
