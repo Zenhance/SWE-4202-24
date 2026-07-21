6db7f34b5e8c07fdf93f1fcd454548e7a7dc4984
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-07-21 11:35:19 +0600

    Added Circle class

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,23 @@
+public class Circle extends Shape{
+    private final double radius;
+
+    public Circle(double radius) {
+        requirePositive(radius);
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public void make(Canvas canvas) {
+        canvas.circle(radius);
+    }
+
+    @Override
+    public String description(){
+        return "Circle";
+    }
+}
