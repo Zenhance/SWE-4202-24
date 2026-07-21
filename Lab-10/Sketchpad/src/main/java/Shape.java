bf12f32720c9c33ab76df1cf26001b2de8b832e3
Rakin <rakinhasan001@gmail.com>
2026-07-21 08:52:15 +0600

    element added and abstract methods created for Shape class

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,2 +1,10 @@
-public class Shape {
+public abstract class Shape {
+    public abstract double area();
+    public abstract String description();
+    protected abstract void render(Canvas canvas);
+
+    public final void draw(Canvas canvas) throws ShapeTooLargeException,ShapeTooSmallException{
+        double area=area();
+
+    }
 }
