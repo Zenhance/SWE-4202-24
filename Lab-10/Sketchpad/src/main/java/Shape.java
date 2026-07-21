5ae34857f104f759eea8c212200b5c8d42a70d04
md-musaddik <musaddikibna1@gmail.com>
2026-07-21 11:27:00 +0600

    Shape class completed

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -2,4 +2,19 @@ public abstract class Shape {
     public abstract double area();
 
     public abstract void paint(Canvas canvas);
+    public String describe() {
+        return String.format("%s with area %.2f", getClass().getSimpleName(), area());
+    }
+    public void draw(Canvas canvas) throws ShapeException {
+        double a = area();
+        int capacity = canvas.capacity();
+
+        if (a > capacity) {
+            throw new ShapeTooLargeException(this, a, capacity);
+        }
+        if (a < 1.0) {
+            throw new ShapeTooSmallException(this, a);
+        }
+        paint(canvas);
+    }
 }
