923dba85789d9cbc56042f0c5ee029cace7f85fe
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-21 09:07:55 +0600

    init commit

---
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle extends Shape{
+    private double width;
+    private double height;
+    public Rectangle(double width,double height){
+        if(width<= 0 || height<=0){
+            throw new IllegalArgumentException("width and height can't be negative");
+        }
+        this.width=width;
+        this.height=height;
+    }
+}
