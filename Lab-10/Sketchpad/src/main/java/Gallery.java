7068e95fb329e965e3a45649c327e87ff71edd04
shafin-75 <arafatshafin85@gmail.com>
2026-07-21 12:20:22 +0600

    Gallery.java

---
+++ Lab-10/Sketchpad/src/main/java/Gallery.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gallery {
+    private final Canvas canvas;
+    private final List<Shape> shapes = new ArrayList<>();
+    public Gallery(Canvas canvas){this.canvas = canvas; }
+    public void add(Shape shape){shapes.add(shape);}
+
+    public  int waiting(){
+        return shapes.size();
+    }
+}
