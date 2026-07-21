cd678eade6641c98067b077f7da59f9b6ec26fc3
Rakin <rakinhasan001@gmail.com>
2026-07-21 10:21:06 +0600

    CIRCLE class completed

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -1,5 +1,26 @@
-public class Circle {
-    public Circle(int radius){
+public class Circle extends Shape {
+    private double radius;
+    private final double PI=3.14;
 
+    public Circle(int radius){
+        if (radius<0) throw new IllegalArgumentException("Invalid radius");
+        this.radius=radius;
+    }
+    @Override
+    public double area(){
+        return PI*radius*radius;
+    }
+
+    @Override
+    public String describe() {
+        return "This is a circle with" + radius + "and area" + area();
+    }
+
+    @Override
+    public void render(Canvas canvas)throws ShapeTooLargeException,ShapeTooSmallException{
+
+        canvas.circle(radius);
     }
 }
+
+
