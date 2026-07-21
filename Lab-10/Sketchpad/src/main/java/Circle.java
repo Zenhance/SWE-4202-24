2a7b3aa8b76d080e26c5877edcc4a5c0193e7d54
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-21 11:38:43 +0600

    write : complete the Circle class [3]

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -11,4 +11,9 @@ public class Circle extends Shape {
     public double getRadius() {
         return radius;
     }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
 }
