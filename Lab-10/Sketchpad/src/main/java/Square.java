f8c692b09eb4d5ac0a8b67754aefe293faebc8fb
hossdia <hossainmahdia22@gmail.com>
2026-07-21 11:47:09 +0600

    sqr

---
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -0,0 +1,10 @@
+public class Square  extends Rectangle{
+    public Square(double side) {
+        super(side, side);
+    }
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+}
