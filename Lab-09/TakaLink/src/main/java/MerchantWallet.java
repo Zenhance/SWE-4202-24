cae044ff000b01eb7bfcc427c6e2917777e0c338
04-Labiba <sabihalabiba639@gmail.com>
2026-07-27 17:38:20 +0600

    init commit

--- Lab-09/TakaLink/src/main/java/MerchantWallet.java
+++ Lab-09/TakaLink/src/main/java/MerchantWallet.java
@@ -1,21 +1,25 @@
-public class MerchantWallet extends Wallet{
+public class MerchantWallet extends Wallet {
+
     private static final double DAILY_LIMIT = 0;
-    public MerchantWallet(String id,double balance,String pin) {
-        super(id, balance,pin);
+
+    public MerchantWallet(String id, double balance, String pin) {
+        super(id, balance, pin);
     }
 
     @Override
     public double getDailyLimit() {
         return DAILY_LIMIT;
     }
+
     @Override
     public boolean canPerform(Transaction transaction) {
-        if(transaction instanceof SendTransaction) {
+
+        if(transaction instanceof SendTransaction)
             return false;
-        }
-        if(transaction instanceof CashOutTransaction) {
+
+        if(transaction instanceof CashOutTransaction)
             return false;
-        }
+
         return true;
-}
+    }
 }
