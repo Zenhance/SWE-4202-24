f9f64edd808403b86ed077b2c28cb6feb60012b2
04-Labiba <sabihalabiba639@gmail.com>
2026-07-27 16:30:21 +0600

    init commit

---
+++ Lab-09/TakaLink/src/main/java/PersonalWallet.java
@@ -0,0 +1,14 @@
+public class PersonalWallet extends Wallet{
+    private static final double DAILY_LIMIT = 25000;
+    public PersonalWallet(String id,double balance,String pin) {
+        super(id,balance,pin);
+    }
+    @Override
+    public double getDailyLimit() {
+        return DAILY_LIMIT;
+    }
+    @Override
+    public boolean canPerform(Transaction transaction) {
+        return true;
+    }
+}
