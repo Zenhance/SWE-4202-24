32c70c3d78e2b4e21d43514919be80707d4f4943
04-Labiba <sabihalabiba639@gmail.com>
2026-07-27 16:15:55 +0600

    init commit

--- Lab-09/TakaLink/src/main/java/Wallet.java
+++ Lab-09/TakaLink/src/main/java/Wallet.java
@@ -4,4 +4,20 @@ public abstract class Wallet {
     private String pin;
     private boolean frozen;
     private double spentToday;
+    public Wallet(String id,double balance,String pin){
+        if(id==null || id.isBlank()){
+            throw new IllegalArgumentException("Invalid ID");
+        }
+        if(pin==null){
+            throw new IllegalArgumentException("PIN cannot be null");
+        }
+        if(balance<0){
+            throw new IllegalArgumentException("Negative balance");
+        }
+        this.id=id;
+        this.balance = balance;
+        this.pin = pin;
+        this.frozen = false;
+        this.spentToday = 0;
+    }
 }
