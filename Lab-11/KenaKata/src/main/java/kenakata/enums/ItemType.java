9efa8624d96cb0ba0d46b13f8785caa32d14775e
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-28 09:36:51 +0600

    change : write the variables and constructors in ItemType

---
+++ Lab-11/KenaKata/src/main/java/kenakata/enums/ItemType.java
@@ -0,0 +1,11 @@
+package kenakata.enums;
+
+public class ItemType {
+    private double vatRate;
+    private double commissionRate;
+
+    public ItemType(double vatRate, double commissionRate) {
+        this.vatRate = vatRate;
+        this.commissionRate = commissionRate;
+    }
+}
