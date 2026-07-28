112e5a38eb697dc3592709a212ab84f68b16f272
04-Labiba <sabihalabiba639@gmail.com>
2026-07-28 09:15:57 +0600

    init commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/Seller.java
@@ -0,0 +1,14 @@
+package kenakata.settlement;
+
+public class Seller {
+    private final String name;
+    public Seller(String name){
+        if(name==null || name.isBlank()){
+            throw new IllegalArgumentException("Seller name cannot be empty");
+        }
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+}
