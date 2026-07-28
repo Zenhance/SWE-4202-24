ce2843c5854d7e71672aa62976da909e6fb7d644
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-28 11:58:07 +0600

    Square class upgraded and finished

--- Lab-10/Sketchpad/src/main/java/Square.java
+++ Lab-10/Sketchpad/src/main/java/Square.java
@@ -3,4 +3,20 @@ public class Square extends Rectangle{
     public Square(double length) {
         super(length, length);
     }
+
+    public void setLength(double length) {
+        if(length <=0) throw new IllegalArgumentException("Length can't be negative");
+        this.length = length;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    @Override
+    public String description() {
+        return ("The length of the rectangle is: " + length + "& the area is: " + area());
+    }
+
+
 }
