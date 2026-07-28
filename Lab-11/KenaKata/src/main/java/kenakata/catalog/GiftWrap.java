76118cbd979dc0e7d2351dc88f49566b65e3d3b3
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-28 10:28:33 +0600

    overridden method

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/GiftWrap.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/GiftWrap.java
@@ -4,7 +4,11 @@ public final class GiftWrap implements Chargeable{
     private static final long CHARGE = 50;
     @Override
     public long unitCharge(){
-        return charge;
+        return CHARGE;
+    }
+    @Override
+    public long unitVat(){
+        return MoneyMath.ceilFraction(CHARGE,75,1000);
     }
 
 }
