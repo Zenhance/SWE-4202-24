68382b444ebdb69e9c4424e6b98a8a135db38007
04-Labiba <sabihalabiba639@gmail.com>
2026-07-28 09:19:33 +0600

    init commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
@@ -0,0 +1,17 @@
+package kenakata.settlement;
+
+public class SellerPayout {
+    private final Seller seller;
+    private long grossSales;
+    private long commission;
+    private long refunds;
+    public SellerPayout(Seller seller){
+        this.seller=seller;
+    }
+    public void addSales(long amount){
+        this.grossSales+=amount;
+    }
+    public void addCommission(long amount){
+        this.commission+=commission;
+    }
+}
