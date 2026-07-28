f8404a5650b3e4353908916a5b85aedd26ffa026
04-Labiba <sabihalabiba639@gmail.com>
2026-07-28 09:02:45 +0600

    init commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/ReturnNotAllowedException.java
@@ -0,0 +1,7 @@
+package kenakata.exceptions;
+
+public class ReturnNotAllowedException extends CheckoutException{
+    public ReturnNotAllowedException(String message){
+        super(message);
+    }
+}
