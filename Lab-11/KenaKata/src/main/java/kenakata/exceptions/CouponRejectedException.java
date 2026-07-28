5af7c5ef9f1caa1278a3331ede8401c76cfcdc86
wasif-22 <twasif68@gmail.com>
2026-07-28 11:42:13 +0600

    added exception class(2)

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/CouponRejectedException.java
@@ -0,0 +1,8 @@
+package kenakata.exceptions;
+
+public class CouponRejectedException extends CheckoutException {
+
+    public CouponRejectedException(String message) {
+        super(message);
+    }
+}
