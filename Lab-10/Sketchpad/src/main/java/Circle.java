3ecebead5d2d2449ded8ba8407f9e501260a2afc
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-28 11:26:11 +0600

    Circle class initiated

--- Lab-10/Sketchpad/src/main/java/Circle.java
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -1,2 +1,21 @@
-public class Circle {
+public class Circle extends Shape {
+    private double radius;
+    private final double PI = 3.1416;
+
+    public Circle(double radius) {
+        if(radius < 0) throw new IllegalArgumentException("Radius can't be negative");
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return PI*radius*radius;
+    }
+
+    @Override
+    public String description() {
+        return ("The radius of the circle is: " +radius+ "& the area is: " + area());
+    }
+
+
 }
