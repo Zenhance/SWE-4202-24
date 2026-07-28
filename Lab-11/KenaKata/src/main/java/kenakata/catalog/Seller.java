ed3246df057bb256450bbf86c80b95214d2c8555
Munman <214246799+bollamokles@users.noreply.github.com>
2026-07-28 09:48:42 +0600

    commit Seller

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -0,0 +1,11 @@
+package kenakata.catalog;
+
+public class Seller {
+    private String sellerName;
+    private String sellerEmail;
+
+    public Seller(String sellerName, String sellerEmail) {
+        this.sellerName = sellerName;
+        this.sellerEmail = sellerEmail;
+    }
+}
