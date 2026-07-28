5af57df88ea3c89ef2af55fea9ba80d4b34e7546
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-28 08:59:02 +0600

    added Seller class

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Product.java
@@ -0,0 +1,9 @@
+package kenakata.catalog;
+
+public class Product {
+    private String sku;
+    private String title;
+    private double unitPrice;
+    private int stock;
+    private Seller seller;
+}
