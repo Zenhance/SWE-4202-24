3752d4d0e29f9e20fc54ab5f16e0e298e02c6a4d
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-28 09:06:32 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/CouponRejectedException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/CouponRejectedException.java
@@ -1,4 +1,7 @@
 package kenakata.exceptions;
 
-public class CouponRejectedException {
+public class CouponRejectedException extends CheckoutException{
+    public CouponRejectedException(String message){
+        super(message);
+    }
 }
