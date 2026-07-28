79d1dd95be5b868024dff756be43d53f7ca68f23
wasifjafar <wasifjafar71@gmail.com>
2026-07-28 08:49:56 +0600

    create skeleton Good class

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Good.java
@@ -0,0 +1,18 @@
+package kenakata.catalog;
+
+public class Good {
+    private String SKU;
+    private String title;
+    private double unitPrice;
+    private int stockCount;
+    private Seller seller;
+
+
+    public Good(String SKU, String title, double unitPrice, int stockCount, Seller seller) {
+        this.SKU = SKU;
+        this.title = title;
+        this.unitPrice = unitPrice;
+        this.stockCount = stockCount;
+        this.seller = seller;
+    }
+}
