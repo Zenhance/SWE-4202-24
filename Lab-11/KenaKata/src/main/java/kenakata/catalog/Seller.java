39732080383312d0c65111ba06027669aa41f042
Zunied-57 <zunied0411@gmail.com>
2026-07-28 09:19:58 +0600

    seller class created

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -0,0 +1,24 @@
+package kenakata.catalog;
+
+public class Seller
+{
+    private String name;
+
+    public Seller(String name)
+    {
+        if(name==null || name.isBlank())
+            throw new IllegalArgumentException("Name can not be null");
+        this.name = name;
+    }
+
+    public String name()
+    {
+        return name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return name;
+    }
+}
