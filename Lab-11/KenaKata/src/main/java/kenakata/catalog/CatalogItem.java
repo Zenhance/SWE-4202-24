d820d18b709da6b8a71419a37850afc0e779ec18
ahnaf-code <ahnafcode@gmail.com>
2026-07-28 12:29:52 +0600

    catalog item

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
@@ -1,4 +1,13 @@
 package kenakata.catalog;
 
 public abstract class CatalogItem {
+
+
+    private String sku;
+    private String title;
+    private double unitPrice;
+    private int stock;
+    private Seller seller;
+
+
 }
