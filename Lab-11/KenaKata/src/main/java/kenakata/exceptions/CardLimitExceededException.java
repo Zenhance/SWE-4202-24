6583b7f2affb77e72ce8fc7ea39fcad5eb9d0f57
Inan <inanjaki184@gmail.com>
2026-07-28 11:46:29 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/CardLimitExceededException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/CardLimitExceededException.java
@@ -1,4 +1,7 @@
 package exceptions;
 
-public class CardLimitExceededException {
+public class CardLimitExceededException extends PaymentDeclineException {
+    public CardLimitExceededException(String message) {
+        super(message);
+    }
 }
