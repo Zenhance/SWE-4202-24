2b9bdeb44d0fec035c7efba82b29bbd92e103cc5
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-28 09:24:21 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
@@ -1,4 +1,22 @@
 package kenakata.settlement;
 
 public class SellerPayout {
+    private final Seller seller;
+    private long grossSales;
+    private long commission;
+    private long refunds;
+
+    public SellerPayout(Seller seller){
+        this.seller=seller;
+    }
+
+
+
+
+
+
+
+
+
+
 }
