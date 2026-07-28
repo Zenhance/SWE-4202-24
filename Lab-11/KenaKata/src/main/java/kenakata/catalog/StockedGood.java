2d6627099a7545feb26c7cc06daf5f3ddf8fb5b8
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-28 09:42:27 +0600

    new java class

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -0,0 +1,6 @@
+package kenakata.catalog;
+
+public final class StockedGood extends CatalogItem implements Weighable, Insurable, Returnable, Discountable{
+    private final int unitWeightGrams;
+
+}
