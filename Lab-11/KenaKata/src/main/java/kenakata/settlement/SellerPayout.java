aa0e13a66d207126975a632a491bc8108d2d8072
Fayaz16 <shahtanzim900@gmail.com>
2026-07-28 10:04:22 +0600

    SellerPayout created

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
@@ -1,4 +1,35 @@
 package kenakata.settlement;
 
-public class SellerPayout {
+public class SellerPayout{
+    private final seller seller;
+    private long grossSales;
+    private long commission;
+    private long refunds;
+
+    public SellerPayout(seller seller){
+        this.seller = seller;
+    }
+    public void addSales(long amount){
+        this.grossSales += amount;
+    }
+
+    public void addCommission(long amount){
+        this.commission += amount;
+    }
+
+    public void addRefund(long amount){
+        this.refunds += amount;
+    }
+    public long grossSales(){
+        return grossSales;
+    }
+    public long commission(){
+        return commission;
+    }
+    public long refunds(){
+        return refunds;
+    }
+    public long payout(){
+        return grossSales - commission - refunds;
+    }
 }
