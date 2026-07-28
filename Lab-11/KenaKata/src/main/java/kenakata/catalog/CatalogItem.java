603b21daf7cebe7f66f9bc2abe79026fed486c9c
Fayaz16 <shahtanzim900@gmail.com>
2026-07-28 10:32:05 +0600

    CatalogItem created

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
@@ -1,4 +1,27 @@
 package kenakata.catalog;
 
-public class CatalogItem {
+import kenakata.exceptions.OutOfStockException;
+import kenakata.order.Chargeable;
+import kenakata.settlement.Seller;
+
+public class CatalogItem implements Chargeable{
+    private final String sku;
+    private final String title;
+    private final long unitPrice;
+    private int stock;
+    private final Seller seller;
+
+    public CatalogItem(String sku, String title, long unitPrice, int stock, Seller seller){
+        if (sku == null || sku.isBlank() || title == null || title.isBlank() || seller == null){
+            throw new IllegalArgumentException( );
+        }
+        if (unitPrice < 0 || stock < 0){
+            throw new IllegalArgumentException( );
+        }
+        this.sku = sku;
+        this.title = title;
+        this.unitPrice = stock;
+        this.seller = seller;
+    }
+
 }
