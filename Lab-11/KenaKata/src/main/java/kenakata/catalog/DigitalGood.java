6d25b89043c897cd94063260b208956a11e56e36
nusra <nusratnishi1103@gmail.com>
2026-07-28 12:34:35 +0600

    first

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -0,0 +1,25 @@
+package kenakata.catalog;
+
+public abstract class DigitalGood extends CatalogItem{
+    private final int downloads;
+
+    public DigitalGood(String sku,String name,int price,int downloads,Seller seller){
+        super(sku,name,price,seller);
+        if(downloads<0){
+            throw new IllegalArgumentException();
+        }
+        this.downloads=downloads;
+    }
+
+    public int downloads(){
+        return downloads;
+    }
+
+    public int unitVat(){
+        return ceilPercent(unitCharge(),5);
+    }
+
+    public int commission(int amount){
+        return (int) Math.ceil(amount*20/100.0);
+    }
+}
