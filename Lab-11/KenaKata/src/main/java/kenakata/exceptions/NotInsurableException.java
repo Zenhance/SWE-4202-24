22073db5eb8642a0742a43cfd9d353da1e585dc3
Zunied-57 <zunied0411@gmail.com>
2026-07-28 09:27:17 +0600

    not insurable created

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/NotInsurableException.java
@@ -0,0 +1,9 @@
+package kenakata.exceptions;
+
+public class NotInsurableException extends CheckoutException
+{
+    public NotInsurableException(String message)
+    {
+        super(message);
+    }
+}
