790213f39a1c75f559aab20d542e41631eafd9ac
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-28 11:46:30 +0600

    Rectangle class initiated and updated

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -1,2 +1,37 @@
-public class Rectangle {
+public class Rectangle extends Shape{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        if(width <=0 && height <=0)
+            throw new IllegalArgumentException("Width and height can't be negative");
+    }
+
+    public void setWidth(double width) {
+        if(width<=0) throw new IllegalArgumentException("Width has to be positive");
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        if(height<=0) throw new IllegalArgumentException("Height has to be positive");
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double area() {
+        return width*height;
+    }
+
+
+
+
+
 }
