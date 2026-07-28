64954b17d6e2f5e4a610eeaf511a0b07cae47fed
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-07-28 11:16:33 +0600

    initial commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogueItems.java
@@ -0,0 +1,19 @@
+package kenakata.catalog;
+
+public abstract class CatalogueItems {
+    public String sku;
+    public String title;
+    public int unit_price;
+    public int stock_count;
+    public Seller seller;
+
+    public CatalogueItems(String sku, String title, int unit_price, int stock_count, Seller seller) {
+        this.sku = sku;
+        this.title = title;
+        this.unit_price = unit_price;
+        this.stock_count = stock_count;
+        this.seller = seller;
+    }
+
+    public abstract boolean isShippable();
+}
