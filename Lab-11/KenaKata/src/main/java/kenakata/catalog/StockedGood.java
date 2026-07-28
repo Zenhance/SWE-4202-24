19977420b4351a1956f0e12f8887cce90a268f58
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-28 09:29:11 +0600

    adding constructor

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -1,4 +1,9 @@
 package kenakata.catalog;
 
+import kenakata.exceptions.InsufficientStockException;
+
 public class StockedGood extends Product{
+    public StockedGood(String sku, String title, double unitPrice, int stock, Seller seller) throws InsufficientStockException {
+        super(sku, title, unitPrice, stock, seller);
+    }
 }
