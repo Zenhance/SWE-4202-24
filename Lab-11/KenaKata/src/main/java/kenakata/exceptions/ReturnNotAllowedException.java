1f7c46c329659be4e668257b76668deeb651f56a
Inan <inanjaki184@gmail.com>
2026-07-28 11:41:12 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/ReturnNotAllowedException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/ReturnNotAllowedException.java
@@ -1,4 +1,7 @@
 package exceptions;
 
-public class ReturnNotAllowedException {
+public class ReturnNotAllowedException extends CheckoutException {
+    public ReturnNotAllowedException(String message) {
+        super(message);
+    }
 }
