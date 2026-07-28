3147c15c2d1034eefdf825e5364ee769bc8766bf
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-28 09:39:10 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
@@ -5,4 +5,9 @@ import java.util.List;
 public class SettlementReport {
     private final List<SellerPayout>payouts;
     private final long platformRevenue;
+
+    public SettlementReport(List<SellerPayout>payouts,long platformRevenue){
+        this.payouts=payouts;
+        this.platformRevenue=platformRevenue;
+    }
 }
