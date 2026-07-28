f874c36c349e81dd70f06556606b759279927551
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-28 09:44:14 +0600

    change : write the exception classes [5]

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/PaymentDeclinedException.java
@@ -0,0 +1,7 @@
+package kenakata.exceptions;
+
+public class PaymentDeclinedException extends KenaKataException {
+    public PaymentDeclinedException(String message) {
+        super(message);
+    }
+}
