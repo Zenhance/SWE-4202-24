d74833299cd42e5e68d3a20628d0e9ce5eceb014
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-07-28 09:53:13 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/Marketplace.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/Marketplace.java
@@ -1,4 +1,17 @@
 package kenakata.settlement;
+import kenakata.catalog.CatalogItem;
+import kenakata.order.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Marketplace {
+    private final List<Seller> registeredSellers = new ArrayList<>();
+    private final List<Order>placeOrders= new ArrayList<>();
+
+    public void register(Seller seller){
+        if(seller != null && !registeredSellers.contains(seller)){
+            registeredSellers.add(seller);
+        }
+    }
 }
