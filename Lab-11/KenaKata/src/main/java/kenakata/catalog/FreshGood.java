60ec690d0a7c80b7439c1818d139554696282cda
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-28 09:28:20 +0600

    adding constructor

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
@@ -1,4 +1,9 @@
 package kenakata.catalog;
 
-public class FreshGood {
+import kenakata.exceptions.InsufficientStockException;
+
+public class FreshGood extends Product{
+    public FreshGood(String sku, String title, double unitPrice, int stock, Seller seller) throws InsufficientStockException {
+        super(sku, title, unitPrice, stock, seller);
+    }
 }
