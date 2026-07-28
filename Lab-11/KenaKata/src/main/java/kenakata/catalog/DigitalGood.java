8a88e713caa0886fcda9656ba0f562e919835bb5
wasifjafar <wasifjafar71@gmail.com>
2026-07-28 09:16:04 +0600

    create DigitalGood class's unitCharge, unitVat and commissionOn methods

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -4,4 +4,19 @@ public class DigitalGood extends Good{
     public DigitalGood(String SKU, String title, double unitPrice, int stockCount, Seller seller) {
         super(SKU, title, unitPrice, stockCount, seller);
     }
+
+    @Override
+    public int unitCharge() {
+        return (int) Math.ceil(getUnitPrice());
+    }
+
+    @Override
+    public int unitVat() {
+        return (int) Math.ceil(getUnitPrice() * 0.05);
+    }
+
+    @Override
+    public int commissionOn(int something) {
+        return (int) Math.ceil(something * 0.2);
+    }
 }
