970f126b0e59c1edfe348cba0961b88cf70f4e73
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-28 10:58:12 +0600

    Seller class initiated properly

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
@@ -1,4 +1,12 @@
 package kenakata.settlement;
 
+import kenakata.catalog.Seller;
+
 public class SellerPayout {
+    private final Seller seller;
+    private long grossSales;
+    private long comission;
+    private long refunds;
+
+
 }
