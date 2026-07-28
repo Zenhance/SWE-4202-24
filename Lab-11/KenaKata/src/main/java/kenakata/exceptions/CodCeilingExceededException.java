9f70b07ae11f66274fb2d9c9614ef3f42afef1ec
wasif-22 <twasif68@gmail.com>
2026-07-28 11:54:17 +0600

    added exception class(4)

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/CodCeilingExceededException.java
@@ -0,0 +1,8 @@
+package kenakata.exceptions;
+
+public class CodCeilingExceededException extends PaymentDeclinedException {
+
+    public CodCeilingExceededException(String message) {
+        super(message);
+    }
+}
