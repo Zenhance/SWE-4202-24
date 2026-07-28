969c9a2ac533ab2db221daff3b2c40b38c2ba1bc
ahnaf-code <ahnafcode@gmail.com>
2026-07-28 12:00:37 +0600

    wallet

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -1,4 +1,15 @@
 package kenakata.payment;
 
 public class Wallet {
+    double wallet;
+
+    public Wallet(double wallet){
+
+
+        this.wallet=wallet;
+    }
+
+    public double getWallet() {
+        return wallet;
+    }
 }
