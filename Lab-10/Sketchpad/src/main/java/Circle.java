18c5911f6248b63fd3bd5f0cabc419a4812aed22
Zunied-57 <zunied0411@gmail.com>
2026-07-28 11:50:13 +0600

    methods overriden in circle class

---
+++ Lab-10/Sketchpad/src/main/java/Circle.java
@@ -0,0 +1,34 @@
+public class Circle extends Shape
+{
+    private double radius;
+    private static final double PI = 3.1416;
+
+    public Circle(double radius)
+    {
+        if(radius<=0)
+            throw new IllegalArgumentException("Radius must be positive");
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea()
+    {
+        return PI*radius*radius;
+    }
+
+    @Override
+    public String describe()
+    {
+        return "Circle : "+PI+"*("+radius+")^2";
+    }
+
+    @Override
+    public void draw(Canvas canvas) throws ShapeException
+    {
+        if(calculateArea()>canvas.capacity())
+            throw new ShapeTooLargeException("Circle is too large");
+        if(calculateArea()<1)
+            throw new ShapeTooSmallException("Circle is too small");
+        canvas.circle(radius);
+    }
+}
