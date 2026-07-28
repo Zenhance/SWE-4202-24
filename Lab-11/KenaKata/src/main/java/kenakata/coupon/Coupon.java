fa4022fbc1e4a1b9b4db0144d122dfce049b83a2
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-07-28 10:10:54 +0600

    coupon

---
+++ Lab-11/KenaKata/src/main/java/kenakata/coupon/Coupon.java
@@ -0,0 +1,34 @@
+package kenakata.coupon;
+
+public class Coupon {
+    private String code;
+    private int discountAmount;
+    private int expiryDay;
+
+    public Coupon (String code,int discountAmount,int expiryDay){
+        this.code=code;
+        this.discountAmount=discountAmount;
+        this.expiryDay=expiryDay;
+    }
+
+    public String getCode(){
+        return code;
+    }
+    public int getDiscountAmount(){
+        return discountAmount;
+    }
+    public int getExpiryDay(){
+        return expiryDay;
+    }
+    public boolean isExpired(int currentDay){
+        return currentDay>expiryDay;
+    }
+    public long computeDiscount(long subtotal,int currentDay){
+        return 0;
+    }
+
+    if(discountAmount>subtotal){
+        return subtotal;
+    }
+    return discountAmount;
+}
