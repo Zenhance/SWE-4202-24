b920013305856745fd87cb298aa76d3e34b1df66
58-jahin <jahinhossain@iut-dhaka.edu>
2026-07-28 09:55:00 +0600

    Add NotInsurableException for unsupported insurance requests

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/NotInsurableException.java
@@ -0,0 +1,8 @@
+package kenakata.exceptions;
+
+public class NotInsurableException extends CheckoutException {
+
+    public NotInsurableException(String message) {
+        super(message);
+    }
+}
