6b32fc79cdd8091fb0a4ea9d3b21b51031206756
hossdia <hossainmahdia22@gmail.com>
2026-07-28 11:41:52 +0600

    catalog

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
@@ -1,6 +1,20 @@
 package kenakata.catalog;
-import kenakata.exceptions.OutOfStockException;
 
-public class CatalogItem {
+public abstract class CatalogItem implements Chargeable {
+    private final String sku;
+    private final String title;
+    private final long price;
+    private int stock;
+    private final Seller seller;
 
+    public CatalogItem(String sku, String title, long price, Seller seller) {
+        if (sku == null || sku.isBlank() || title == null || title.isBlank() || seller == null || price < 0 || stock < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.sku = sku;
+        this.title = title;
+        this.price = price;
+        this.seller = seller;
+        this.stock = stock;
+    }
 }
