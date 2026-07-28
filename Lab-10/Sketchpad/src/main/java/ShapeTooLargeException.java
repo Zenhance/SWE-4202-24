c5fd07cb7a3e57e767effb71062f7436c79c47de
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-28 11:16:04 +0600

    ShapeTooLargeException class created

--- Lab-10/Sketchpad/src/main/java/ShapeTooLargeException.java
+++ Lab-10/Sketchpad/src/main/java/ShapeTooLargeException.java
@@ -1,2 +1,5 @@
-public class ShapeTooLargeException {
+public class ShapeTooLargeException extends ShapeException {
+    public ShapeTooLargeException(String message) {
+        super(message);
+    }
 }
