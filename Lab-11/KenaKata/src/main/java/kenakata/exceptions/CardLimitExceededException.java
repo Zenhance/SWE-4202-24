017023cbba9aca28e14e45bf5795f306282d1864
04-Labiba <sabihalabiba639@gmail.com>
2026-07-28 09:10:43 +0600

    init commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/CardLimitExceededException.java
@@ -0,0 +1,7 @@
+package kenakata.exceptions;
+
+public class CardLimitExceededException extends CheckoutException{
+    public CardLimitExceededException(String message){
+        super(message);
+    }
+}
