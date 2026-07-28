ddf220d0477b0be6fd41a07bbc39bcadd76de997
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-28 10:04:27 +0600

    Seller class initiated properly

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -1,4 +1,16 @@
 package kenakata.catalog;
 
 public class Seller {
+    private final String name;
+
+    public Seller(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name can't be empty");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
