92fe68921e09082ece4d0eb9d7190b40e0887064
wasifjafar <wasifjafar71@gmail.com>
2026-07-28 10:22:19 +0600

    create Order class's addProduct method

--- Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
@@ -1,11 +1,19 @@
 package kenakata.order;
 
+import kenakata.catalog.Good;
+import java.util.ArrayList;
+
 public class Order {
     private Zone zone;
     private DeliveryCalculator calculator;
+    private ArrayList<Good> products;
 
     public Order(Zone zone, DeliveryCalculator calculator) {
         this.zone = zone;
         this.calculator = calculator;
+    }
+
+    public void addProduct(Good product) {
+        products.add(product);
     }
 }
