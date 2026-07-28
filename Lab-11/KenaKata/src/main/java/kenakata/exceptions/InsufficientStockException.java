34c6130cc99b1bef6af4658e5ba564ccffe3a724
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-07-28 09:41:06 +0600

    change : write the InsufficientStockException class

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/InsufficientStockException.java
@@ -0,0 +1,7 @@
+package kenakata.exceptions;
+
+public class InsufficientStockException extends KenaKataException {
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+}
