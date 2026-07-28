0d811aa67377e07abf093154bc33b4a67b2dff87
wasifjafar <wasifjafar71@gmail.com>
2026-07-28 08:51:52 +0600

    create skeleton StockedGood class

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -0,0 +1,10 @@
+package kenakata.catalog;
+
+public class StockedGood extends Good{
+    private double weight;
+
+    public StockedGood(String SKU, String title, double unitPrice, int stockCount, Seller seller, double weight) {
+        super(SKU, title, unitPrice, stockCount, seller);
+        this.weight = weight;
+    }
+}
