d9cc5f3933b6da457ff0d0c67c9c8e4284ba6b03
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-28 09:58:06 +0600

    new java class called DigitalGood

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -0,0 +1,7 @@
+package kenakata.catalog;
+
+public final class DigitalGood extends CatalogItem{
+    public DigitalGood(String sku, String title, long unitPrice, int stock, Seller seller){
+        super(sku,title,unitPrice,stock,seller);
+    }
+}
