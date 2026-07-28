f37f28ae1679dd2cc6ab45232eedb39b222b58e8
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-28 09:24:42 +0600

    Completed not insurable exception class...

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/NotInsurableException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/NotInsurableException.java
@@ -2,4 +2,7 @@ package kenakata.exceptions;
 
 public class NotInsurableException extends CheckoutException {
 
+    public NotInsurableException(String message) {
+        super(message);
+    }
 }
