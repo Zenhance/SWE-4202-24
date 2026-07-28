d9dbab873451a33a51be02c003d7d262e2629da5
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-07-28 09:59:06 +0600

    Completed seller class..

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -2,4 +2,20 @@ package kenakata.catalog;
 
 public class Seller {
 
+    private String name;
+
+    public Seller(String name) {
+        if(name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid seller name");}
+        this.name = name;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
