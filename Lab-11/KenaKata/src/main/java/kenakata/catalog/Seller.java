cde04a7eb89fc431eb243ce09aba8dc2e09cd862
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-28 09:48:35 +0600

    added field, constructor and method

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -1,4 +1,11 @@
 package kenakata.catalog;
 
 public class Seller {
+    private String name;
+
+    public Seller(String name) {
+        this.name = name;
+    }
+
+    public String getName() {return name;}
 }
