a4ca08261f04cfbc6a59fa81ad1b21984bcb8332
Maomao-starry <aminarahmanshalu@gmail.com>
2026-07-28 11:21:54 +0600

    Exceptions

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/StockUnavailableException.java
@@ -0,0 +1,8 @@
+package kenakata.exceptions;
+
+public class StockUnavailableException extends KenaKataException{
+
+    public StockUnavailableException(String msg) {
+        super(msg);
+    }
+}
