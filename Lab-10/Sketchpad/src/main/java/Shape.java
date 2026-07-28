a50a50c3e0159e528fe64cab344822fc5037d7a1
Tnvr09 <rahmantanvir64@gmail.com>
2026-07-28 11:13:28 +0600

    Shape class initiated

--- Lab-10/Sketchpad/src/main/java/Shape.java
+++ Lab-10/Sketchpad/src/main/java/Shape.java
@@ -1,2 +1,7 @@
-public class Shape {
+public abstract class Shape {
+    public abstract double area();
+    public abstract String description();
+    public abstract void draw(Canvas canvas) {
+        throws ShapeException;
+    }
 }
