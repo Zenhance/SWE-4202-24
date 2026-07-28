1ec6a140c990ffd95b79a2025d7376a4d7ad14d6
wasifjafar <wasifjafar71@gmail.com>
2026-07-28 10:06:52 +0600

    create WarrantyPlan class and implement Chargeable interface's methods

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
@@ -0,0 +1,24 @@
+package kenakata.catalog;
+
+public class WarrantyPlan implements Chargeable{
+    private Good item;
+
+    public WarrantyPlan(Good item) {
+        this.item = item;
+    }
+
+    @Override
+    public int unitCharge() {
+        return (int) Math.ceil(item.unitCharge() * 0.10);
+    }
+
+    @Override
+    public int unitVat() {
+        return (int) Math.ceil(unitCharge() * 0.15);
+    }
+
+    @Override
+    public int commissionOn(int something) {
+        return 0;
+    }
+}
