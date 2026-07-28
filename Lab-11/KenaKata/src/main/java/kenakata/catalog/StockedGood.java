b87bb3429a3af78408497b089ec31e82e0d2e09e
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-07-28 09:45:02 +0600

    added constructor

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -2,5 +2,11 @@ package kenakata.catalog;
 
 public final class StockedGood extends CatalogItem implements Weighable, Insurable, Returnable, Discountable{
     private final int unitWeightGrams;
-
+    private StockedGood(String sku, String title, long unitPrice, int stock, Seller seller, int unitWeightGrams){
+        super(sku,title,unitPrice,stock,seller);
+        if(unitWeightGrams<=0){
+            throw new IllegalArgumentException("Weight must be positive");
+        }
+        this.unitWeightGrams = unitWeightGrams;
+    }
 }
