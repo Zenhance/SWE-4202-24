49613a0fb452f86372c2ea97278f7e7641901b85
Munman <214246799+bollamokles@users.noreply.github.com>
2026-07-28 09:33:02 +0600

    commit FreshGood

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
@@ -4,4 +4,10 @@ public class FreshGood extends Good{
     public FreshGood(String SKU, String title, double unitPrice, int stockCount, Seller seller) {
         super(SKU, title, unitPrice, stockCount, seller);
     }
+    public int unitVat(){
+        return (int) Math.ceil(getUnitPrice()*0.00);
+    }
+    public int commissionOn(int stockCount){
+        return (int) Math.ceil(0.05*stockCount);
+    }
 }
