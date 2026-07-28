1f033a1f4d3e25a11915f75fea79c986fa86c2d6
iam-tahmid <tahmidim99@gmail.com>
2026-07-28 10:52:44 +0600

    finished

--- Lab-10/Sketchpad/src/main/java/Rectangle.java
+++ Lab-10/Sketchpad/src/main/java/Rectangle.java
@@ -1,9 +1,11 @@
 package main.java;
 
+import java.awt.*;
+
 public class Rectangle extends Shape {
     private double width;
     private double height;
-    Rectangle(double width, double height){
+    public Rectangle(double width, double height){
         this.width = width;
         this.height = height;
     }
@@ -11,6 +13,17 @@ public class Rectangle extends Shape {
     @Override
     public String describe() {
         return "";
+    }
+
+    @Override
+    public double area() {
+        return height*width;
+    }
+
+    @Override
+    public void draw(Canvas canvas) throws ShapeException {
+        canvas.rectangle(width, height);
+
     }
 
 }
