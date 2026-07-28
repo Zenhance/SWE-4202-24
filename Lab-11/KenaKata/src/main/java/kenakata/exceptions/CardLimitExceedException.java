945bd90f0caeee799ff6a181d8e72986bf6364cd
Zunied-57 <zunied0411@gmail.com>
2026-07-28 09:32:20 +0600

    empty wallet & card limit exceed created

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/CardLimitExceedException.java
@@ -0,0 +1,9 @@
+package kenakata.exceptions;
+
+public class CardLimitExceedException extends PaymentDeclinedException
+{
+    public CardLimitExceedException(String message)
+    {
+        super(message);
+    }
+}
