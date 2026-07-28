afe5641f38750b3d99c8d665f07078e47158544d
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-07-28 12:19:11 +0600

    NotInsurableException created

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/NotInsurableException.java
@@ -0,0 +1,14 @@
+package kenakata.exceptions;
+
+public class NotInsurableException extends CheckoutException{
+
+public NotInsurableException (String message)
+{
+
+    super(message);
+}
+
+
+
+
+}
