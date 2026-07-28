df9bdd3c28458e3455196f6a3303a2fecb9a458f
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-28 09:34:40 +0600

    updating constructor

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -3,7 +3,14 @@ package kenakata.catalog;
 import kenakata.exceptions.InsufficientStockException;
 
 public class StockedGood extends Product{
-    public StockedGood(String sku, String title, double unitPrice, int stock, Seller seller) throws InsufficientStockException {
+    private double weightGrams;
+    public StockedGood(String sku, String title, double unitPrice, int stock, Seller seller, double weightGrams) throws InsufficientStockException {
         super(sku, title, unitPrice, stock, seller);
+
+        if (weightGrams <= 0) {
+            throw new IllegalArgumentException("Weight must be greater than zero in grams.");
+        }
+
+        this.weightGrams = weightGrams;
     }
 }
