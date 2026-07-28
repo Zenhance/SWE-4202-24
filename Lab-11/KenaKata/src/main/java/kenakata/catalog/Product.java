e5785e558496abc8b8744cbe54883d03cd775f90
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-07-28 09:07:45 +0600

    adding exceptions

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Product.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Product.java
@@ -8,6 +8,17 @@ public abstract class Product {
     private Seller seller;
 
     public Product(String sku, String title, double unitPrice, int stock, Seller seller) {
+        if (sku == null || title == null || seller == null) {
+            throw new IllegalArgumentException("SKU, title and seller can not be blank.");
+        }
+
+        if (unitPrice < 0) {
+            throw new IllegalArgumentException("Unit price must be greater than zero.");
+        }
+
+        if (stock <= 0) {
+
+        }
 
         this.sku = sku;
         this.title = title;
