870aca677558e3fae326a8b26ac7c512d7d9b86d
Inan <inanjaki184@gmail.com>
2026-07-28 11:42:11 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/OutOfStockException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/OutOfStockException.java
@@ -1,4 +1,7 @@
 package exceptions;
 
-public class OutOfStockException {
+public class OutOfStockException extends CheckoutException {
+    public OutOfStockException(String message) {
+        super(message);
+    }
 }
