57ba4af711184b825a47dcff783ba6ce16043d5c
Tasin-Tajwar24 <tasintajwar24@iut-dhaka.edu>
2026-07-28 12:49:10 +0600

    Added essential getters and Constructors inside Catalogitem

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
@@ -0,0 +1,59 @@
+package kenakata.catalog;
+
+public abstract class CatalogItem implements Chargable {
+
+private String sku;
+private String title;
+private double unitPrice;
+private int remaining;
+private Seller seller;
+
+
+    public CatalogItem(String sku, String title, double unitPrice, int remaining, Seller seller)
+    {
+        if (sku==null || sku.isBlank())
+throw new IllegalArgumentException( "Sku can not be null");
+        if (title == null || title.isBlank())
+throw new IllegalArgumentException("Title can not be null");
+        if(unitPrice<0)
+            throw new IllegalArgumentException("Unit price must be positive");
+        if(remaining<0)
+            throw new IllegalArgumentException("Remaining items can not be negative");
+        if(seller==null)
+            throw new IllegalArgumentException("Seller can not be null");
+        this.sku = sku;
+        this.title = title;
+        this.unitPrice = unitPrice;
+        this.remaining = remaining;
+        this.seller = seller;
+    }
+
+    public String sku()
+    {
+        return sku;
+    }
+
+    public String title()
+    {
+        return title;
+    }
+
+    public Seller seller()
+    {
+        return seller;
+    }
+
+    @Override
+    public String label()
+    {
+        return title;
+    }
+
+    @Override
+    public double unitCharge()
+    {
+        return unitPrice;
+    }
+}
+
+
