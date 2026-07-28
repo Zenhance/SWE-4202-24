37709641af91af6e64c6c797b23b2b9a6cbdc6c6
ahnaf-code <ahnafcode@gmail.com>
2026-07-28 11:32:13 +0600

    seller class

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -4,10 +4,13 @@ public class Seller {
     private String name;
 
     public Seller(String name){
+        if(name==null || name.isBlank())
+            throw new IllegalArgumentException("Invalid name");
 
         this.name=name;
     }
 
-
-
+    public String getName() {
+        return name;
+    }
 }
