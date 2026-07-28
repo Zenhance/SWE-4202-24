9ce7e6039a852c6b883e24590169526c4940573d
shafin-75 <arafatshafin85@gmail.com>
2026-07-28 11:51:55 +0600

    done cardlimitexceededexception

--- Lab-11/KenaKata/src/main/java/kenakata/Exceptions/CardLimitExceededException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/Exceptions/CardLimitExceededException.java
@@ -1,4 +1,7 @@
 package Exceptions;
 
-public class CardLimitExceededException {
+public class CardLimitExceededException extends PaymentDeclinedException{
+    public CardLimitExceededException(String message){
+        super(message);
+    }
 }
