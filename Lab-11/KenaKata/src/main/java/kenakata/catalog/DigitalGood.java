4ec0af8134c02dca8ee551bea810b6e9b2968fec
wasifjafar <wasifjafar71@gmail.com>
2026-07-28 08:52:36 +0600

    create skeleton DigitalGood class

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -0,0 +1,7 @@
+package kenakata.catalog;
+
+public class DigitalGood extends Good{
+    public DigitalGood(String SKU, String title, double unitPrice, int stockCount, Seller seller) {
+        super(SKU, title, unitPrice, stockCount, seller);
+    }
+}
