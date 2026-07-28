be12db2fe5c576b0588380318e98a24a787f31f5
Inan <inanjaki184@gmail.com>
2026-07-28 11:59:46 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -1,4 +1,17 @@
 package catalog;
 
 public class Seller {
+    private String name;
+    public Seller(String name) {
+        if(name == null||name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or blank");
+        }
+        this.name = name;
+    }
+    public String name() {
+        return name;
+    }
+    public String toString() {
+        return name;
+    }
 }
