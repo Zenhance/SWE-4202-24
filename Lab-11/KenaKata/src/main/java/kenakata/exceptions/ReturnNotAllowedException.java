c085eaab5926d376b11b8d1924668aee62b269a6
Zunied-57 <zunied0411@gmail.com>
2026-07-28 09:35:49 +0600

    rest of the two  exception classes created

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/ReturnNotAllowedException.java
@@ -0,0 +1,9 @@
+package kenakata.exceptions;
+
+public class ReturnNotAllowedException extends CheckoutException
+{
+    public ReturnNotAllowedException(String message)
+    {
+        super(message);
+    }
+}
