7790d74b712f6786956022062ca1eee9dbce9aca
Zunied-57 <zunied0411@gmail.com>
2026-07-28 09:29:30 +0600

    payment declined created

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/PaymentDeclinedException.java
@@ -0,0 +1,9 @@
+package kenakata.exceptions;
+
+public class PaymentDeclinedException extends CheckoutException
+{
+    public PaymentDeclinedException(String message)
+    {
+        super(message);
+    }
+}
