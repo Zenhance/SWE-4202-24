f89f6421d305c4781c77dc6df929486ecc2b319f
hossdia <hossainmahdia22@gmail.com>
2026-07-30 21:28:47 +0600

    order

---
+++ Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
@@ -0,0 +1,6 @@
+package kenakata.order;
+
+public record PriceBreakdown(long subtotal, long discount,
+                             long delivery, long vat, long insurance,
+                             long serviceFee, long grandTotal) {
+}
