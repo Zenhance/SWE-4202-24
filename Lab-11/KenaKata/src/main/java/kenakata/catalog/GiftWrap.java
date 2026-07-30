06f5281c3136a718298563e636d047e84dad1e55
hossdia <hossainmahdia22@gmail.com>
2026-07-30 20:12:49 +0600

    catalog

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/GiftWrap.java
@@ -0,0 +1,18 @@
+package kenakata.catalog;
+
+public class GiftWrap implements Chargeable{
+    @Override
+    public long unitCharge() {
+        return 50;
+    }
+
+    @Override
+    public long unitVat() {
+        return (long)Math.ceil(50 * 7.5 / 100));
+    }
+
+    @Override
+    public String label() {
+        return "Gift wrapping";
+    }
+}
