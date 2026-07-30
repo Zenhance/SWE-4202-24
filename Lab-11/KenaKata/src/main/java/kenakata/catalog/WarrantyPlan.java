3312074f964de768fd71b5056fd8eb992d21863b
nusra <nusratnishi1103@gmail.com>
2026-07-30 23:31:47 +0600

    first

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
@@ -0,0 +1,24 @@
+package kenakata.catalog;
+
+public class WarrantyPlan implements Chargeable{
+    private final CatalogItem coveredItem;
+
+    public WarrantyPlan(CatalogItem coveredItem){
+        if(coveredItem==null){
+            throw new IllegalArgumentException();
+        }
+        this.coveredItem=coveredItem;
+    }
+
+    public String label(){
+        return "Warranty for" + coveredItem.name();
+    }
+
+    public int unitCharge(){
+        return (int) Math.ceil(coveredItem.unitCharge()*10/100.0);
+    }
+
+    public int unitVat(){
+            return (int) Math.ceil(unitCharge()*15/100.0);
+    }
+}
