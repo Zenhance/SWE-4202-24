602bee8a735b7dd4e3b0072c18ff1ea5de3cbc84
23-Fahad <ifahad2k@gmail.com>
2026-07-30 17:29:35 +0600

    all exception class done

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/CheckoutException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/CheckoutException.java
@@ -1,8 +1,6 @@
 package main.java.kenakata.exceptions;
 
-/**
- * Base checked exception for business refusals in the KenaKata engine.
- */
+
 public class CheckoutException extends Exception {
     public CheckoutException(String message) {
         super(message);
