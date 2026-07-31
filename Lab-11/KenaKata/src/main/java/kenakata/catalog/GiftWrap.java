67f645b5ed73f7f16896d9cd595f22cc2537bc76
sameer-abdullah-240042107 <sameerabdullah1117@gmail.com>
2026-08-01 03:52:21 +0600

    c12

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/GiftWrap.java
@@ -0,0 +1,16 @@
+package kenakata.catalog;
+
+public class GiftWrap implements  Chargeable{
+    @Override
+    public long unitCharge() {
+        return 50;
+    }
+
+    @Override
+    public long unitVat() {
+        return (long)Math.ceil(50*0.075);
+    }
+    public String label(){
+        return "Gift wrapping";
+    }
+}
