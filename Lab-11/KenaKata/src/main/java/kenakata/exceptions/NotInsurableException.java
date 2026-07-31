e24affcb75b22c588a97d5c6b1e8116ff482519e
sameer-abdullah-240042107 <sameerabdullah1117@gmail.com>
2026-08-01 03:52:55 +0600

    c13

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/NotInsurableException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/NotInsurableException.java
@@ -1,6 +1,6 @@
 package kenakata.exceptions;
 
-public class NotInsurableException extends CheckOutExceptions{
+public class NotInsurableException extends CheckoutException {
     public NotInsurableException() {
         super("Item cannot be insured.");
     }
--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/OutOfStockException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/OutOfStockException.java
@@ -1,6 +1,6 @@
 package kenakata.exceptions;
 
-public class OutOfStockException extends CheckOutExceptions{
+public class OutOfStockException extends CheckoutException {
     public OutOfStockException(){
         super("Out of Stock");
     }
