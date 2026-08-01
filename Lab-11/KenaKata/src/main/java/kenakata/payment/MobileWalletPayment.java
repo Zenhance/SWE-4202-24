a00f8278e9a1fc0b76701387271f5549382e0f6d
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-02 00:08:45 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
@@ -10,5 +10,12 @@ public abstract class MobileWalletPayment implements PaymentMethod {
     public MobileWalletPayment(Wallet wallet){
         this.wallet=wallet;
     }
+    @Override
+    public void authorise(long amount) throws PaymentDeclinedException{
+        if(wallet.balance()<amount){
+            throw new EmptyWalletException("Wallet balance too low");
+        }
+        wallet.debit(amount);
+    }
 
 }
