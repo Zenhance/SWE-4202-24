6b69defdf4c0054904d91c49fc17d2c95178edb5
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-02 00:28:20 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
@@ -1,4 +1,26 @@
 package kenakata.order;
 
+import java.util.List;
+
 public class DeliveryCalculator {
+    public long calculateDelivery(List<OrderLine> lines, Zone zone){}
+    int totalWeightGrams= 0;
+    long coldChainFee = 0;
+    boolean hasWeighable= false;
+
+    for(OrderLine line: lines){
+        Chargeable item = line.item();
+        if(item instanceof Weighable weighable){
+            totalWeightGrams += weighable.weightGrams() * line.quantity();
+            hasWeighable= true;
+        }
+        if(item instanceof ColdChain coldChain){
+            coldChainFee+=coldChainFee.coldChainSurcharge();
+        }
+    }
+    if(!hasWeighable){
+        return 0;
+    }
+    int billedKg= (int) Math.ceil(totalWeightGrams/1000.0);
+    long baseShipping;
 }
