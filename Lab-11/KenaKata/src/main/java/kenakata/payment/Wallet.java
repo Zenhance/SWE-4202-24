43d393c2c4e268a79facdb25a5073decc29b8d8f
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-02 00:01:43 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -2,7 +2,7 @@ package kenakata.payment;
 
 public class Wallet{
     private long balance;
-}
+
 public Wallet(long initialBalance){
     if(initialBalance < 0){
         throw new IllegalArgumentException("Initial balance cant be negative");
@@ -11,4 +11,17 @@ public Wallet(long initialBalance){
 }
 public long balance(){
     return balance;
+}
+public void credit(long amount){
+    if(amount <=0){
+        throw new IllegalArgumentException("Credit amount must be positive");
+    }
+    balance+=amount;
+}
+public void debit(long amount){
+if(amount>balance){
+throw new IllegalArgumentException("Insufficient balance");
+}
+balance-=amount;
+}
 }
