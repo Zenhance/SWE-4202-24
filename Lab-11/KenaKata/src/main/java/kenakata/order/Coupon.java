e95c01c5950ac749e8d334adc69b7fc744735404
Sadman Raiyan <kamrulseed@gmail.com>
2026-08-01 22:03:43 +0600

    Implemented attributes for Coupon and PriceBreakdown classes

--- Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -1,4 +1,40 @@
 package kenakata.order;
 
+import kenakata.exceptions.CouponRejectedException;
+
 public class Coupon {
+    private String code;
+    private long percentage;
+    private long cap;
+    private long minimumSpend;
+    private int lastValidDay;
+
+    public Coupon(String code, long percentage, long cap, long minimumSpend, int lastValidDay) {
+        this.code = code;
+        this.percentage = percentage;
+        this.cap = cap;
+        this.minimumSpend = minimumSpend;
+        this.lastValidDay = lastValidDay;
+    }
+
+    public String code() {
+        return code;
+    }
+
+    public long percentage() {
+        return percentage;
+    }
+
+    public long cap() {
+        return cap;
+    }
+
+    public long minimumSpend() {
+        return minimumSpend;
+    }
+
+    public int lastValidDay() {
+        return lastValidDay;
+    }
+
 }
--- Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
@@ -1,4 +1,43 @@
 package kenakata.order;
 
 public class PriceBreakdown {
+    private final long subtotal;
+    private final long discount;
+    private final long delivery;
+    private final long tax;
+    private final long insurance;
+    private final long serviceFee;
+    private final long grandTotal;
+
+    public PriceBreakdown(long subtotal, long discount, long delivery, long tax, long insurance, long serviceFee, long grandTotal) {
+        this.subtotal = subtotal;
+        this.discount = discount;
+        this.delivery = delivery;
+        this.tax = tax;
+        this.insurance = insurance;
+        this.serviceFee = serviceFee;
+        this.grandTotal = grandTotal;
+    }
+
+    public long subtotal() {
+        return subtotal;
+    }
+    public long discount() {
+        return discount;
+    }
+    public long delivery() {
+        return delivery;
+    }
+    public long tax() {
+        return tax;
+    }
+    public long insurance() {
+        return insurance;
+    }
+    public long serviceFee() {
+        return serviceFee;
+    }
+    public long grandTotal() {
+        return grandTotal;
+    }
 }
