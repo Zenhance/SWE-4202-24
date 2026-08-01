a0bd523d60ef75ad0c74096ae1b443b11691650d
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-01 21:42:49 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/order/GiftWrap.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/GiftWrap.java
@@ -1,8 +1,16 @@
 package kenakata.order;
 
-public class GiftWrap implements Chargeable{
+public abstract class GiftWrap implements Chargeable{
     @Override
     public long unitCharge(){
         return 50;
+    }
+    @Override
+    public long unitVat(){
+        return (long) Math.ceil(50*0.075);
+    }
+    @Override
+    public String label(){
+        return "Gift Wrapping";
     }
 }
