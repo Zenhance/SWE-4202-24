c21d82f50e1fab3dedf229bb72139add82b9ef10
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-01 23:54:22 +0600

    init commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/PaymentMethod.java
@@ -0,0 +1,7 @@
+package kenakata.payment;
+import kenakata.exceptions.PaymentDeclinedException;
+
+
+public interface PaymentMethod {
+    void authorise(long amount) throws PaymentDeclinedException;
+}
