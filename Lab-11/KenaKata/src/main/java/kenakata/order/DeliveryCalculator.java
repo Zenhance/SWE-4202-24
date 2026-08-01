2c047e9d27211d9c720c357f99378baec7ed29aa
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-02 00:34:47 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
@@ -3,32 +3,32 @@ package kenakata.order;
 import java.util.List;
 
 public class DeliveryCalculator {
-    public long calculateDelivery(List<OrderLine> lines, Zone zone){}
-    int totalWeightGrams= 0;
-    long coldChainFee = 0;
-    boolean hasWeighable= false;
+    public long calculateDelivery(List<OrderLine> lines, Zone zone) {
+        int totalWeightGrams = 0;
+        long coldChainFee = 0;
+        boolean hasWeighable = false;
 
-    for(OrderLine line: lines){
-        Chargeable item = line.item();
-        if(item instanceof Weighable weighable){
-            totalWeightGrams += weighable.weightGrams() * line.quantity();
-            hasWeighable= true;
+        for (OrderLine line : lines) {
+            Chargeable item = line.item();
+            if (item instanceof Weighable weighable) {
+                totalWeightGrams += weighable.weightGrams() * line.quantity();
+                hasWeighable = true;
+            }
+            if (item instanceof ColdChain coldChain) {
+                coldChainFee += coldChainFee.coldChainSurcharge();
+            }
         }
-        if(item instanceof ColdChain coldChain){
-            coldChainFee+=coldChainFee.coldChainSurcharge();
+        if (!hasWeighable) {
+            return 0;
         }
+        int billedKg = (int) Math.ceil(totalWeightGrams / 1000.0);
+        long baseShipping;
 
-    if(!hasWeighable){
-        return 0;
+        if (zone == Zone.DHAKA) {
+            baseShipping = 60 + (billedKg * 20L);
+        } else {
+            baseShipping = 120 + (billedKg * 35L);
+        }
+        return baseShipping + coldChainFee;
     }
-    int billedKg= (int) Math.ceil(totalWeightGrams/1000.0);
-    long baseShipping;
-
-    if(zone== Zone.DHAKA){
-        baseShipping = 60 + (billedKg * 20L);
-    } else{
-        baseShipping = 120 + (billedKg * 35L);
-    }
-    return baseShipping + coldChainFee;
-}
 }
