1cf0b0e9a89af4290a4543f4354f181ae0b48218
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-01 22:48:51 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -1,4 +1,22 @@
 package kenakata.order;
 
 public class Coupon {
+    private final String code;
+    private final double percentage;
+    private final long maxDiscount;
+    private final long minSpeed;
+    private final int lastValidDay;
+    public Coupon(String code,double percentage,long maxDiscount,long minSpeed, int lastValidDay){
+        if(percentage<0 || percentage > 100) {
+            throw new IllegalArgumentException("Invalid percentage");
+        }
+        this.code=code;
+        this.percentage = percentage;
+        this.maxDiscount= maxDiscount;
+        this.minSpeed= minSpeed;
+        this.lastValidDay = lastValidDay;
+        }
+        public String code(){
+        return code;
+    }
 }
--- Lab-11/KenaKata/src/main/java/kenakata/order/WarrantyPlan.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/WarrantyPlan.java
@@ -10,4 +10,16 @@ public abstract class WarrantyPlan implements Chargeable{
         }
         this.coveredItem=coveredItem;
     }
+    @Override
+    public long unitCharge(){
+        return (long) Math.ceil(coveredItem.unitCharge()*0.10);
+    }
+    @Override
+    public long unitVat(){
+        return (long) Math.ceil(unitCharge()*0.15);
+    }
+    @Override
+    public String label(){
+        return "Warranty for " + coveredItem.label();
+    }
 }
