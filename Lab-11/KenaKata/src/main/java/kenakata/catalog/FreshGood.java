eee4f747edd15ee4f673467f0675785a9d70380d
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-01 23:39:49 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
@@ -1,4 +1,22 @@
 package kenakata.catalog;
+import kenakata.order.ColdChain;
+import kenakata.order.Insurable;
+import kenakata.order.Returnable;
+import kenakata.order.Weighable;
+import kenakata.settlement.Seller;
 
-public class FreshGood {
+public class FreshGood extends CatalogItem Weighable,ColdChain,Insurable,Returnable{
+    private final int weightGrams;
+
+    public FreshGood(String sku, String title,long unitPrice,int stock,Seller seller,int weightGrams){
+        super(sku,title,unitPrice,stock,seller);
+        if(weightGrams<=0){
+            throw new IllegalArgumentException("Weight must be positive");
+        }
+        this.weightGrams=weightGrams;
+    }
+    @Override
+    public long unitVat(){
+        return 0;
+    }
 }
