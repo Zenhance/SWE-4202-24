f11d7d1c64e3d4aa320c62649ea831e3375f39db
Sadman Raiyan <kamrulseed@gmail.com>
2026-08-01 21:01:53 +0600

    Implemented charable in Gift and ExpressHandling

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/ExpressHandling.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/ExpressHandling.java
@@ -1,4 +1,14 @@
 package kenakata.catalog;
 
-public class ExpressHandling {
+public class ExpressHandling implements Chargeable{
+
+    @Override
+    public long unitCharge() {
+        return 120;
+    }
+
+    @Override
+    public long unitVat() {
+        return (long) Math.ceil(unitCharge() * 0.075);
+    }
 }
