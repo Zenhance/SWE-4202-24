2ab2dc5333ef4b5ef41bdc969aef7d1919c6c7ae
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-01 23:34:46 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -1,12 +1,16 @@
 package kenakata.catalog;
 import kenakata.settlement.Seller;
 
-public class DigitalGood extends CatalogItem {
+public abstract class DigitalGood extends CatalogItem {
     public DigitalGood(String sku, String title, long unitPrice,int Stock, Seller seller){
         super(sku,title,unitPrice,stock,seller);
     }
     @Override
     public long unitVat(){
         return (long) Math.ceil(unitCharge()*0.05);
+    }
+    @Override
+    public long commissionOn(long amount){
+        return (long) Math.ceil(amount*0.20);
     }
 }
