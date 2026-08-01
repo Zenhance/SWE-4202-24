599e110c1b2a913e71bacefe61aeeb5f3b59bb97
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-01 21:44:41 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/order/ExpressHandling.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/ExpressHandling.java
@@ -1,4 +1,16 @@
 package kenakata.order;
 
-public class ExpressHandling {
+public class ExpressHandling implements Chargeable{
+    @Override
+    public long unitCharge(){
+        return 120;
+    }
+    @Override
+    public long unitVat(){
+        return (long)Math.ceil(120*0.075);
+    }
+    @Override
+    public String label(){
+        return "Express Handling";
+    }
 }
