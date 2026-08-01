41324574f7f618e3d4b4abc3989d115ca7b06155
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-02 00:37:02 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -1,4 +1,20 @@
 package kenakata.order;
 
 public class OrderLine {
+    private final Chargeable item;
+    private final int quantity;
+    private boolean insured = false;
+    private boolean returned = false;
+
+    public OrderLine(Chargeable item, int quantity){
+        if(quantity <=0){
+            throw new IllegalArgumentException("Quantity must be positive");
+        }
+        this.item= item;
+        this.quantity=quantity;
+    }
+
+
+
+
 }
