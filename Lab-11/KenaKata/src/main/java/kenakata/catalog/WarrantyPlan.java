65250fc42da4fe3112f00a86a03b8b0f34856c5d
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-08-01 15:22:03 +0600

    added contsructor

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
@@ -4,5 +4,11 @@ public final class WarrantyPlan implements Chargeable{
     private final CatalogItem coveredItem;
     private final long charge;
 
-
+    public WarrantyPlan(CatalogItem covereItem){
+        if(coveredItem == null){
+            throw new IllegalArgumentException("Covered item must not be null");
+        }
+        this.coveredItem = coveredItem;
+        this.charge = MoneyMath.ceilPercent(coveredItem.unitPrice(),10);
+    }
 }
