cd8a65f1627a50f820dd1a1fb35e5336f03f7915
tasnitaraida <tasnitar@gmail.com>
2026-08-03 02:38:48 +0600

    WarrantyPlan done

--- Lab-11/KenaKata/src/main/java/kenakata/order/WarrantyPlan.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/WarrantyPlan.java
@@ -7,6 +7,22 @@ public class WarrantyPlan implements Chargeable{
     private final CatalogItem item;
 
     public WarrantyPlan(CatalogItem item) {
+        if(item == null) throw new IllegalArgumentException();
+        this.item=item;
+    }
 
+    @Override
+    public long unitCharge() {
+        return 0;
+    }
+
+    @Override
+    public long unitVat() {
+        return 0;
+    }
+
+    @Override
+    public String label() {
+        return "";
     }
 }
