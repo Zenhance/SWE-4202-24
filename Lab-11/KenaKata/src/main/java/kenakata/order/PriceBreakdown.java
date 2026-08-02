2458b1b16158d2f7907653925a0177aec9570afd
tasnitaraida <tasnitar@gmail.com>
2026-08-03 02:57:58 +0600

    PriceBreakDown class

--- Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
@@ -1,4 +1,11 @@
 package kenakata.order;
 
 public class PriceBreakdown {
+    long subtotal;
+    long discount;
+    long vat;
+    long delivery;
+    long insurance;
+    long serviceFee;
+    long grandTotal;
 }
