69a0c060d6b1eaabb79cffa861933577d3d83c3b
wasifjafar <wasifjafar71@gmail.com>
2026-08-02 23:23:45 +0600

    add limit field in CardPayment class

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
@@ -1,4 +1,9 @@
 package kenakata.payment;
 
-public class CardPayment {
+public class CardPayment extends PaymentMethod{
+    private int limit;
+
+    public CardPayment(int limit) {
+        this.limit = limit;
+    }
 }
