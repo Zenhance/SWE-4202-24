fafbf59d642d2c7752396b67dc94d12468647e5b
tasnitaraida <tasnitar@gmail.com>
2026-08-03 02:00:48 +0600

    create WarrantyPlan class

---
+++ Lab-11/KenaKata/src/main/java/kenakata/order/WarrantyPlan.java
@@ -0,0 +1,12 @@
+package kenakata.order;
+
+import kenakata.catalog.CatalogItem;
+import kenakata.catalog.Chargeable;
+
+public class WarrantyPlan implements Chargeable{
+    private final CatalogItem item;
+
+    public WarrantyPlan(CatalogItem item) {
+
+    }
+}
