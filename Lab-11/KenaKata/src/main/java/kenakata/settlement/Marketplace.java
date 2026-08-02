bd6f842dc24aea8f6df7b7141530920ee2c4c17d
wasifjafar <wasifjafar71@gmail.com>
2026-08-03 00:26:24 +0600

    create Marketplace class's record method

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/Marketplace.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/Marketplace.java
@@ -1,13 +1,19 @@
 package kenakata.settlement;
 
 import kenakata.catalog.Seller;
+import kenakata.order.Order;
 
 import java.util.ArrayList;
 
 public class Marketplace {
     private ArrayList<Seller> sellers;
+    private ArrayList<Order> orders;
 
     public void register(Seller seller) {
         sellers.add(seller);
+    }
+
+    public void record(Order order) {
+        orders.add(order);
     }
 }
