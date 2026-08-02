5f2f50ee04ab5a0ed9d4fffd094f4be217cfadc5
wasif-22 <twasif68@gmail.com>
2026-08-02 18:54:43 +0600

    fixed errors in wallet class

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -10,3 +10,24 @@ public final class Wallet {
         }
         this.balance = initialBalance;
     }
+    public long balance() {
+        return balance;
+    }
+
+    void debit(long amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("");
+        }
+        if (amount > balance) {
+            throw new IllegalStateException("");
+        }
+        balance -= amount;
+    }
+
+    public void credit(long amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("");
+        }
+        balance += amount;
+    }
+}
