4ece57ff0d6b410bf0910c51a51df4c4d3a4983d
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-03 03:21:40 +0600

    StockedGood

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -12,11 +12,11 @@ public class StockedGood extends CatalogItem{
 
     @Override
     public int unitVat() {
-        return 0;
+        return (int) Math.ceil(getUnitPrice() * 0.075);
     }
 
     @Override
     public int commissionOn(int total) {
-        return 0;
+        return (int) Math.ceil(total * 0.08);
     }
 }
