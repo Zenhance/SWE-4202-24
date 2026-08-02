00cacfb6db2bfef4f84cfae81f3bfa9f7b0227df
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-03 02:46:38 +0600

    payment classes extension

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
@@ -1,6 +1,13 @@
 package kenakata.payment;
 
 public class CardPayment extends PaymentMethod {
+    private int limit;
+
+    public CardPayment(int limit) {
+        this.limit = limit;
+    }
+
+
     @Override
     public void authorise(int amount) {
 
