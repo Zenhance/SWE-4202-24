2a049dd1564b6429d835c46c8b4cf0aac3bac718
wasifjafar <wasifjafar71@gmail.com>
2026-08-03 00:34:33 +0600

    create SettlementReport class's skeleton forSeller method

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
@@ -1,4 +1,15 @@
 package kenakata.settlement;
 
+import kenakata.catalog.Seller;
+import kenakata.order.Order;
+
+import java.util.ArrayList;
+
 public class SettlementReport {
+    private ArrayList<Order> orders;
+    private ArrayList<Seller> sellers;
+
+    public SellerPayout forSeller(Seller seller) {
+        return new SellerPayout();
+    }
 }
