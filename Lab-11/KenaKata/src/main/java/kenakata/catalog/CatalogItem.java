4800af2f5037ab7483355fd8f24cdbc9982406a0
tasnitaraida <tasnitar@gmail.com>
2026-08-03 01:38:06 +0600

    create CatalogItem class

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
@@ -1,4 +1,22 @@
 package kenakata.catalog;
 
-public class CatalogItem {
+import kenakata.exceptions.OutOfStockException;
+
+public abstract class CatalogItem implements Chargeable{
+    private final String sku;
+    private final String title;
+    private final long unitPrice;
+    private int remainingStock;
+    private final Seller seller;
+
+    public Catalogitem(String sku,String title,long unitPrice, int initialStock, Seller seller) {
+        if(sku==null || sku.trim().isEmpty()) throw new IllegalArgumentException();
+        if(title==null || title.trim().isEmpty()) throw new IllegalArgumentException();
+        if(unitPrice<0 || initialStock<0 || seller==null) throw new IllegalArgumentException();
+        this.sku=sku;
+        this.title=title;
+        this.unitPrice=unitPrice;
+        this.remainingStock=initialStock;
+        this.seller=seller;
+    }
 }
--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -4,10 +4,11 @@ public class Seller {
     private final String name;
 
     public Seller(String name) {
+        if(name==null || name.trim().isEmpty()) throw new IllegalArgumentException();
         this.name=name;
     }
 
     public String name() {
-
+        return name;
     }
 }
