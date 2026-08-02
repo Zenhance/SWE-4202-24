c68369fe4c8fa8ab810a62677086d6d7851477d2
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-02 20:31:16 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
@@ -1,4 +1,15 @@
 package kenakata.order;
 
 public class PriceBreakdown {
+    private final long subtotal;
+    private final long discount;
+    private final long vat;
+    private final long delivery;
+    private final long insurance;
+    private final long serviceFee;
+    private final long grandTotal;
+
+
+
+
 }
