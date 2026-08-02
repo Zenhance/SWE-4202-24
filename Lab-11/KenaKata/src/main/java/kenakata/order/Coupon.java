94a8b7a345deee80167a64c843839d3bee1c9835
tasnitaraida <tasnitar@gmail.com>
2026-08-03 02:42:54 +0600

    Coupon class

--- Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -7,4 +7,19 @@ public class Coupon {
     private final int pct;
     private final long cap;
     private final long minSpeed;
+    private final int expiryDay;
+
+    public Coupon(String code, int pct, long cap, long minSpeed, int expiryDay) {
+        this.code = code;
+        this.pct = pct;
+        this.cap = cap;
+        this.minSpeed = minSpeed;
+        this.expiryDay = expiryDay;
+    }
+
+    public String code() {
+        return code;
+    }
+
+    public long calculateDiscount()
 }
