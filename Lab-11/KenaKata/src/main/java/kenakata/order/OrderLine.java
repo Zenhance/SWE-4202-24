e9b57dd72eeac03167d37af0aabcf16cda64ca7b
Sadman Raiyan <kamrulseed@gmail.com>
2026-08-02 22:30:33 +0600

    I think this is what i need to do..Don't know..Brain Fried

--- Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -1,6 +1,8 @@
 package kenakata.order;
 
 import kenakata.catalog.Chargeable;
+import kenakata.catalog.FreshGood;
+import kenakata.catalog.StockedGood;
 
 public class OrderLine {
     private Chargeable item;
@@ -23,6 +25,43 @@ public class OrderLine {
     }
     public boolean returned(){
         return returned;
+    }
+
+    public long subtotal(){
+        return item.unitCharge()*quantity;
+    }
+
+    public long vat(){
+        return item.unitVat()*quantity;
+    }
+
+    public void insure(){
+        insured=true;
+    }
+
+    public void returnItem(){
+        returned=true;
+    }
+
+    public long insuranceFee(){
+        if(!insured){
+            return 0;
+        }
+        long value=0;
+        if(item instanceof StockedGood){
+            StockedGood stocked = (StockedGood) item;
+            value=stocked.insure(quantity);
+        }
+        else if(item instanceof FreshGood fresh){
+            value=fresh.insure(quantity);
+        }
+        else{
+            return 0;
+        }
+        long fee=(long)Math.ceil(value*0.01);
+
+        return Math.max(fee,20);
+
     }
 
 }
