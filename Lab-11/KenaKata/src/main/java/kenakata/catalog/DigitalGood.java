368b511f29e4f3a324e5a35bf1a37b70f5b04b29
tasnitaraida <tasnitar@gmail.com>
2026-08-03 01:48:25 +0600

    create DigitalGood class

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -1,4 +1,8 @@
 package kenakata.catalog;
 
-public class DigitalGood {
+public class DigitalGood extends CatalogItem{
+    public DigitalGood(String sku, String title,long price, int stock, Seller seller) {
+        super(sku, title, price, stock, seller);
+    }
+
 }
