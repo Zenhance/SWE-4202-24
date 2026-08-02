20d3a0ac93c1236be14d7fdc991e273ae3b06b73
tasnitaraida <tasnitar@gmail.com>
2026-08-03 02:50:04 +0600

    DeliveryCalculator class

--- Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
@@ -1,4 +1,13 @@
 package kenakata.order;
 
+import java.util.List;
+
 public class DeliveryCalculator {
+    public long  calculate(List<OrderLine> lines, Zone zone) {
+        long totalWeightGrams=0;
+        long coldChainSurcharge=0;
+        boolean hasWeighable = false;
+
+
+    }
 }
