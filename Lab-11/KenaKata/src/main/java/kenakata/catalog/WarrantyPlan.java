bc092151fc3410d7478bf35a53a94232d3f889c0
Munman <214246799+bollamokles@users.noreply.github.com>
2026-08-03 00:09:52 +0600

    commit WarrantyPlan

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
@@ -0,0 +1,29 @@
+package kenakata.catalog;
+
+public class WarrantyPlan extends AddOn{
+    private Good item;
+
+    public WarrantyPlan(Good product) {
+        this.item = product;
+        product.insure();
+    }
+
+    @Override
+    public int unitCharge() {
+        return (int) Math.ceil(item.unitCharge() * 0.10);
+    }
+
+    @Override
+    public int unitVat() {
+        return (int) Math.ceil(unitCharge() * 0.15);
+    }
+
+    @Override
+    public int commissionOn(int something) {
+        return 0;
+    }
+
+    public String label() {
+        return item.getTitle();
+    }
+}
