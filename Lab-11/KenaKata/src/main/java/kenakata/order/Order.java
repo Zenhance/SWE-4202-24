90a6f4efce1601f26bf046b51cecd58dbfc60fd3
tasnitaraida <tasnitar@gmail.com>
2026-08-03 03:07:48 +0600

    Order class

--- Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
@@ -1,4 +1,27 @@
 package kenakata.order;
 
+import kenakata.catalog.CatalogItem;
+import kenakata.catalog.Chargeable;
+import kenakata.catalog.StockedGood;
+import kenakata.exceptions.*;
+import kenakata.payment.PaymentMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private final Zone zone;
+    private final DeliveryCalculator deliveryCalculator;
+    private final List<OrderLine> lines = new ArrayList<>();
+    private Coupon coupon;
+    private boolean placed = false;
+    private PriceBreakdown finalBreakdown;
+    private int placementDay;
+
+    public Order(Zone zone, DeliveryCalculator deliveryCalculator) {
+        this.zone = zone;
+        this.deliveryCalculator = deliveryCalculator;
+    }
+
+
 }
