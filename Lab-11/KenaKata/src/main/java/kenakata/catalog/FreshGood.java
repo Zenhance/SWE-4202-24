8926115c5c958a9336a52ef6bf562e7d6105514e
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-03 03:12:06 +0600

    automated extension skeleton

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
@@ -1,4 +1,22 @@
 package kenakata.catalog;
 
-public class FreshGood {
+public class FreshGood extends CatalogItem{
+    public FreshGood(String sku, String title, double unitPrice, int stock, Seller seller) {
+        super(sku, title, unitPrice, stock, seller);
+    }
+
+    @Override
+    public int unitCharge() {
+        return 0;
+    }
+
+    @Override
+    public int unitVat() {
+        return 0;
+    }
+
+    @Override
+    public int commissionOn(int total) {
+        return 0;
+    }
 }
--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -1,4 +1,22 @@
 package kenakata.catalog;
 
-public class StockedGood {
+public class StockedGood extends CatalogItem{
+    public StockedGood(String sku, String title, double unitPrice, int stock, Seller seller) {
+        super(sku, title, unitPrice, stock, seller);
+    }
+
+    @Override
+    public int unitCharge() {
+        return 0;
+    }
+
+    @Override
+    public int unitVat() {
+        return 0;
+    }
+
+    @Override
+    public int commissionOn(int total) {
+        return 0;
+    }
 }
