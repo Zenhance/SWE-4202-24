69dd8ba0d5482c7fe5a6dbe68cd4c2e2c0b28ab1
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-08-02 11:34:00 +0600

    updating constructor

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/CheckoutException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/CheckoutException.java
@@ -1,7 +1,10 @@
 package kenakata.exceptions;
 
 public class CheckoutException extends Exception{
+    public CheckoutException(String message) {
+        super(message);
+    }
     public CheckoutException() {
-
+        super();
     }
 }
