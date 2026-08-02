3792ec3500d52a2dca3319282a5aa85db3524235
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-08-02 23:45:03 +0600

    added methods

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -1,20 +1,31 @@
 package kenakata.catalog;
 
+import kenakata.catalog.capabilities.Insurable;
+import kenakata.catalog.capabilities.Returnable;
 import kenakata.catalog.capabilities.Weighable;
-import kenakata.exceptions.InsufficientStockException;
+import kenakata.exceptions.OutOfStockException;
 
-public class StockedGood extends Product implements Weighable {
-    private double weightGrams;
-    private final double vat;
+public class StockedGood extends Product implements Weighable, Returnable, Insurable {
+    private int weightInGrams;
 
-    public StockedGood(String sku, String title, double unitPrice, int stock, Seller seller, double weightGrams, double vat) throws InsufficientStockException {
+    public StockedGood(String sku, String title, long unitPrice, int stock, Seller seller, int weightGrams)
+            throws OutOfStockException {
         super(sku, title, unitPrice, stock, seller);
 
         if (weightGrams <= 0) {
             throw new IllegalArgumentException("Weight must be greater than zero in grams.");
         }
-
         this.weightGrams = weightGrams;
-        this.vat = 0.075;
+    }
+
+    @Override
+    public int weightGrams() { return weightInGrams;}
+    @Override
+    public long unitVat() {
+        return (long) Math.ceil(unitPrice()*0.075);
+    }
+    @Override
+    public long commissionOn() {
+        return (long) Math.ceil(unitPrice()*0.08);
     }
 }
