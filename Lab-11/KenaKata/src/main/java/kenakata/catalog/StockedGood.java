3b67c7ee4c9dcc24bac468965d18ce0e299ca2e0
tasnitaraida <tasnitar@gmail.com>
2026-08-03 02:23:46 +0600

    StockedGood done

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -7,4 +7,22 @@ public class StockedGood extends CatalogItem {
         super(sku, title, unitPrice, initialStock, seller);
         this.weight = weight;
     }
+
+    public long unitVat() {
+        return (long) Math.ceil(unitPrice() * 0.075);
+    }
+
+    public long comissionOn(long val) {
+        return (long) Math.ceil(val * 0.08);
+    }
+
+    public Integer weightGrams() {
+        return weight;
+    }
+    public boolean isInsurable() {
+        return true;
+    }
+    public Integer returnWindowDays() {
+        return 7;
+    }
 }
