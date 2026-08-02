1248b7b69ddc154a558a66a325fc21dc602a9c5b
tasnitaraida <tasnitar@gmail.com>
2026-08-03 03:26:30 +0600

    MobileWalletPayment class

---
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
@@ -0,0 +1,17 @@
+package kenakata.payment;
+
+import kenakata.exceptions.EmptyWalletException;
+
+public class MobileWalletPayment implements PaymentMethod {
+    private final Wallet wallet;
+
+    public MobileWalletPayment(Wallet wallet) {
+        this.wallet=wallet;
+    }
+
+    public void authorise(long amount) throws EmptyWalletException {
+        if(wallet.balance()<amount) {
+            throw new EmptyWalletException(" ");
+        }
+    }
+}
