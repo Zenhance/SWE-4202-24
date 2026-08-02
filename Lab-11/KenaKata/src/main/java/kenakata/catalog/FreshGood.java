4fc3271482d67f03cf89d65fcdffa0484ca196f3
tasnitaraida <tasnitar@gmail.com>
2026-08-03 02:32:39 +0600

    FreshGood done

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
@@ -5,6 +5,37 @@ public class FreshGood extends CatalogItem{
 
     public FreshGood(String sku, String title, long price, int stock, Seller seller, int weight) {
         super(sku, title, price, stock, seller);
+        if(weight<=0) throw new IllegalArgumentException();
         this.weight=weight;
+    }
+
+    @Override
+    public long unitVat() {
+        return 0;
+    }
+
+    @Override
+    public long comissionOn(long val) {
+        return (long) Math.ceil(val * 0.05);
+    }
+
+    @Override
+    public Integer weightGrams() {
+        return weight;
+    }
+
+    @Override
+    public boolean isColdChain() {
+        return true;
+    }
+
+    @Override
+    public boolean isInsurable() {
+        return true;
+    }
+
+    @Override
+    public Integer returnWindowDays() {
+        return 100000;
     }
 }
