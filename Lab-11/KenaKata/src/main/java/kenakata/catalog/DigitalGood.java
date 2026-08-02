27effa540350562e1590d65dfa508145a6135e47
tasnitaraida <tasnitar@gmail.com>
2026-08-03 01:51:05 +0600

    create FreshGood class

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -4,5 +4,4 @@ public class DigitalGood extends CatalogItem{
     public DigitalGood(String sku, String title,long price, int stock, Seller seller) {
         super(sku, title, price, stock, seller);
     }
-
 }
--- Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
@@ -1,4 +1,10 @@
 package kenakata.catalog;
 
-public class FreshGood {
+public class FreshGood extends CatalogItem{
+    private final int weight;
+
+    public FreshGood(String sku, String title, long price, int stock, Seller seller, int weight) {
+        super(sku, title, price, stock, seller);
+        this.weight=weight;
+    }
 }
