39c854f36f7d6691ef79e7b393e5172a37fb79a0
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-08-02 23:46:12 +0600

    Created and completed card payment class...

---
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
@@ -0,0 +1,26 @@
+package kenakata.payment;
+
+import kenakata.exceptions.CardLimitExceededException;
+import kenakata.exceptions.PaymentDeclinedException;
+
+public class CardPayment implements PaymentMethod {
+
+    private long remainingLimit;
+
+    public CardPayment(long limit) {
+
+        this.remainingLimit = limit;
+    }
+    public long remainingLimit() {
+
+        return remainingLimit;
+    }
+    @Override
+    public void authorise(long amount)
+            throws PaymentDeclinedException {
+        if(amount > remainingLimit) {
+            throw new CardLimitExceededException("Card limit exceeded");
+        }
+        remainingLimit -= amount;
+    }
+}
