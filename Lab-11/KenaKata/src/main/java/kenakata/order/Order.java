ee86ea6d57bd8051cee30567a6b51fc0f0b0ed6f
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-02 20:52:03 +0600

    init commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
@@ -0,0 +1,19 @@
+package kenakata.order;
+import kenakata.catalog.CatalogItem;
+import kenakata.catalog.StockedGood;
+import kenakata.exceptions.*;
+import kenakata.payment.PaymentMethod;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Order {
+    private final Zone zone;
+    private final DeliveryCalculator deliveryCalculator;
+    private final List<OrderLine> line = new ArrayList<>();
+    private Coupon coupon;
+    private boolean placed = false;
+    private PriceBreakdown finalBreakdown;
+    private int placementDay;
+}
