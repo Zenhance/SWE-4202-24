40883d23c8feac6916e611787c31ef77b2c0756a
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-03 02:12:32 +0600

    wallet

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -15,6 +15,20 @@ public class Wallet {
         return balance;
     }
 
+    public void debit(double amount){
+        if(amount>balance) {
+            throw new IllegalArgumentException("Invalid Debit Amount");
+        }
+        balance-=amount;
+    }
+
+    public void credit(double amount){
+        if(amount<=0) {
+            throw new IllegalArgumentException("Invalid Credit Amount");
+        }
+        balance+=amount;
+    }
+
 
 }
 
