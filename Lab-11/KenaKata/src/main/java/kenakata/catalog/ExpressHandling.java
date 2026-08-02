0cc9c94fbaa0b40d52a753260b646f110fad042e
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-03 03:52:48 +0600

    ExpressHandling

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/ExpressHandling.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/ExpressHandling.java
@@ -1,4 +1,10 @@
 package kenakata.catalog;
 
-public class ExpressHandling {
+public class ExpressHandling extends AddOn {
+    private final int unitPrice = 120;
+
+    public double unitVat() {
+        return (int) Math.ceil(unitPrice * 0.075);
+    }
+
 }
