36d0e908bb95ab449823959fcb72e88280641cfa
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-03 02:06:31 +0600

    Revert "wallet fix"
    This reverts commit e21e410dcfc7ffc35fb3b53e55fb92c3027499c9.

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -15,12 +15,6 @@ public class Wallet {
         return balance;
     }
 
-    public void credit(long amount){
-        if(amount <=0){ throw new IllegalArgumentException("Invalid Credit Amount");
-        }
-        balance+=amount;
-    }
-
 
 }
 
