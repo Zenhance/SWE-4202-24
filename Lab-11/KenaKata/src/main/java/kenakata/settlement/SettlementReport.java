124cda359b10fbe0639c1ec916131c3ed526a0cd
tasnitaraida <tasnitar@gmail.com>
2026-08-03 03:40:06 +0600

    SettlementReport class

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
@@ -1,4 +1,22 @@
 package kenakata.settlement;
 
+import kenakata.catalog.Seller;
+import java.util.Map;
+
 public class SettlementReport {
+    private final Map<Seller, SellerPayout> sellerPayouts;
+    private final long platformRevenue;
+
+    public SettlementReport(Map<Seller, SellerPayout> sellerPayouts, long platformRevenue) {
+        this.sellerPayouts = sellerPayouts;
+        this.platformRevenue = platformRevenue;
+    }
+
+    public SellerPayout forSeller(Seller s) {
+        return sellerPayouts.getOrDefault(s, new SellerPayout(0,0,0,0));
+    }
+
+    public long platformRevenue() {
+        return platformRevenue;
+    }
 }
