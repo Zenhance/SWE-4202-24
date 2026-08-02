e3eaea1e35cf90fbe79a92be4b262475062f1a38
wasifjafar <wasifjafar71@gmail.com>
2026-08-02 23:16:32 +0600

    add balance field and method in Wallet class

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -1,4 +1,13 @@
 package kenakata.payment;
 
 public class Wallet {
+    private int balance;
+
+    public Wallet(int balance) {
+        this.balance = balance;
+    }
+
+    public int balance() {
+        return balance;
+    }
 }
