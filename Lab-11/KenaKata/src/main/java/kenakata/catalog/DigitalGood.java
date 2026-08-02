375b0fc6e7f5eaa50ca40812bc9e2f4d34aada59
tasnitaraida <tasnitar@gmail.com>
2026-08-03 02:28:16 +0600

    DigitalGood done

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -4,4 +4,11 @@ public class DigitalGood extends CatalogItem{
     public DigitalGood(String sku, String title,long price, int stock, Seller seller) {
         super(sku, title, price, stock, seller);
     }
+
+    public long unitVat() {
+        return (long) Math.ceil(unitPrice() * 0.05);
+    }
+    public long comissionOn(long val) {
+        return (long) Math.ceil(val * 0.20);
+    }
 }
