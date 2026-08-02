23de5fc18cc08a37dbd86b1dfc109d15d02e91e2
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-03 02:35:25 +0600

    payment classes extension

--- Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
@@ -1,6 +1,11 @@
 package kenakata.payment;
 
 public class MobileWalletPayment extends PaymentMethod {
+    private Wallet wallet;
+
+    public MobileWalletPayment(Wallet wallet) {
+        this.wallet = wallet;
+    }
     @Override
     public void authorise(int amount) {
 
