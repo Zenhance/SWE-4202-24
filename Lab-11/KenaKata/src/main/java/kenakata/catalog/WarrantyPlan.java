9fbe4c2e2423cfe2f4b2558ef293fb916a873a17
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-08-02 23:08:25 +0600

    Created and completed warranty plan class...

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
@@ -0,0 +1,28 @@
+package kenakata.catalog;
+
+public class WarrantyPlan implements Chargeable {
+    private CatalogItem item;
+    public WarrantyPlan(CatalogItem item) {
+        if(item == null) {
+            throw new IllegalArgumentException("Invalid item");
+        }
+
+        this.item = item;
+    }
+    @Override
+    public long unitCharge() {
+        return (item.unitCharge() * 10) / 100;
+    }
+
+
+    @Override
+    public long unitVat() {
+        return (unitCharge() * 15) / 100;
+    }
+
+
+    @Override
+    public String label() {
+        return "Warranty for " + item.name();
+    }
+}
