5128c4d04c2cc686fc5ef076c11769233d18e666
tasnitaraida <tasnitar@gmail.com>
2026-08-03 02:36:42 +0600

    ExpressHandling done

--- Lab-11/KenaKata/src/main/java/kenakata/order/ExpressHandling.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/ExpressHandling.java
@@ -4,4 +4,18 @@ import kenakata.catalog.Chargeable;
 
 public class ExpressHandling implements Chargeable{
 
+    @Override
+    public long unitCharge() {
+        return 0;
+    }
+
+    @Override
+    public long unitVat() {
+        return 0;
+    }
+
+    @Override
+    public String label() {
+        return "";
+    }
 }
