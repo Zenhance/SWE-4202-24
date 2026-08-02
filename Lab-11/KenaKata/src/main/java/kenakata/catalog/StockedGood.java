73e9c27b05aeddc5463192879fdbd099b2779d87
tasnitaraida <tasnitar@gmail.com>
2026-08-03 02:19:17 +0600

    StockedGood

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -3,5 +3,8 @@ package kenakata.catalog;
 public class StockedGood extends CatalogItem {
     private final int weight;
 
-
+    public StockedGood(String sku, String title, long unitPrice, int initialStock, Seller seller, int weight) {
+        super(sku, title, unitPrice, initialStock, seller);
+        this.weight = weight;
+    }
 }
