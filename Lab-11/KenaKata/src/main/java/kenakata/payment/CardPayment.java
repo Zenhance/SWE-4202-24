1d7b19a180ddd17c9eded08145166d3e792f8899
04-Labiba <sabihalabiba639@gmail.com>
2026-08-03 21:44:00 +0600

    init commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
@@ -0,0 +1,22 @@
+package kenakata.payment;
+import kenakata.exceptions.CardLimitExceededException;
+import kenakata.exceptions.PaymentDeclinedException;
+public class CardPayment implements PaymentMethod {
+    private long limit;
+
+    public CardPayment(long limit) {
+        this.limit = limit;
+    }
+
+    public long remainingLimit() {
+        return limit;
+    }
+
+    @Override
+    public void authorise(long amount) throws PaymentDeclinedException {
+        if (amount > limit) {
+            throw new CardLimitExceededException("Card limit exceeded");
+        }
+        limit -= amount;
+    }
+}
