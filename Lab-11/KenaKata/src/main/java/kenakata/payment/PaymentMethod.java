bbfb6dfb562710c1b788d796964e362223601dde
Zunied-57 <zunied0411@gmail.com>
2026-08-03 21:23:50 +0600

    paymentmethod added

---
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/PaymentMethod.java
@@ -0,0 +1,8 @@
+package kenakata.payment;
+
+import kenakata.exceptions.PaymentDeclinedException;
+
+public interface PaymentMethod
+{
+    void authorise(long amount) throws PaymentDeclinedException;
+}
