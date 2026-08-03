dea9a3b1f7b01153a158f25744bc20adff600a31
58-jahin <jahinhossain@iut-dhaka.edu>
2026-08-03 20:47:42 +0600

    created PaymentMethod is an interface that gives all payment types one common method

--- Lab-11/KenaKata/src/main/java/kenakata/payment/PaymentMethod.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/PaymentMethod.java
@@ -1,6 +1,9 @@
 package kenakata.payment;
 
-public class PaymentMethod {
-    public void authorise(int i) {
-    }
+import kenakata.exceptions.PaymentDeclinedException;
+@FunctionalInterface
+public interface PaymentMethod {
+
+    void authorise(long amount)
+            throws PaymentDeclinedException;
 }
