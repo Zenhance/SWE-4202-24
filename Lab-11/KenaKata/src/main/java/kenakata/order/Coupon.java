d126fa3b1531ecf90636894dee7fc9446b2c90b5
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-04 03:45:11 +0600

    coupon.order

---
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -0,0 +1,8 @@
+package kenakata.order;
+
+public class Coupon {
+    private final String couponCode;
+    private final long percentage;
+    private final long cap;
+    private final long validDays;
+}
