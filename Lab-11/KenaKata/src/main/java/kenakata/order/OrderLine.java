50206ebada445a629b905c3b9444d107b1d5bb3b
Inan <inanjaki184@gmail.com>
2026-08-03 23:27:19 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -3,11 +3,43 @@ package kenakata.order;
 import kenakata.catalog.Chargeable;
 
 public class OrderLine {
+    private static final double INSURANCE_RATE_PERCENT=1;
+    private static final double INSURANCE_MINIMUM=20;
+
+    private Chargeable item;
+    private int quantity;
+    private boolean insured;
+    private boolean returned;
+
+    OrderLine(Chargeable item, int quantity){
+        this.item = item;
+        this.quantity = quantity;
+    }
 
     public Chargeable item(){
-        return null;
+        return item;
     }
     public int quantity(){
-        return 0;
+        return quantity;
     }
+    public boolean insured(){
+        return insured;
+    }
+    public boolean returned(){
+        return returned;
+    }
+    double charge(){
+        return item.unitVat()*quantity;
+    }
+    double vat(){
+        return item.unitVat()*quantity;
+    }
+    double insuranceFee(){
+    return 0;
+    }
+    void markReturned(){
+        returned = true;
+    }
+
 }
+
