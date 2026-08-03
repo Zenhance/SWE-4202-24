56b267ec4ed29afc1d5d9f58588eda431e93dc36
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-03 22:33:46 +0600

    change : write the FreshGood class [2]

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
@@ -1,4 +1,23 @@
 package kenakata.catalog;
 
-public class FreshGood {
+public class FreshGood extends CatalogItem implements Weighable, ColdChain, Insurable, Returnable {
+    private int weightGrams;
+
+    public FreshGood(String sku, String title, long unitPrice, int stock, Seller seller) {
+        super(sku, title, unitPrice, stock, seller);
+        if (weightGrams <= 0) {
+            throw new IllegalArgumentException("Weight must be positive");
+        }
+        this.weightGrams = weightGrams;
+    }
+
+    @Override
+    public int weightGrams() {
+        return weightGrams;
+    }
+
+    @Override
+    public long unitVat() {
+        return 0;
+    }
 }
