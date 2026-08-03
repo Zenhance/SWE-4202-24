4a3fac356638b1f7cb5a39461b7c14d0c4a3fc60
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-08-04 01:37:41 +0600

    finished class with overridden methods

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
@@ -3,5 +3,15 @@ import kenakata.exceptions.CodCeilingExceededException;
 
 public final class CashOnDeliveryPayment implements PaymentMethod{
     public static final long CEILING = 15_000;
-
+    @Override
+    public void validate(long amount) throws CodCeilingExceededException {
+        PaymentMethod.requireNonNegative(amount);
+        if(amount>CEILING){
+            throw new CodCeilingExceededException("Cash on delivery ceiling is Tk "+CEILING);
+        }
+    }
+    @Override
+    public void authorise(long amount)throws CodCeilingExceededException {
+        validate(amount);
+    }
 }
