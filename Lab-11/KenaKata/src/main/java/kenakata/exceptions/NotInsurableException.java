f190f82b40a3dd7fb17382180380e17239910283
23-Fahad <ifahad2k@gmail.com>
2026-08-03 13:49:19 +0600

    exception class done with proper hierarchy thee are two parent class 1. CheckoutException 2.PaymentDeclinedException other exceptions extends to this two.

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/NotInsurableException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/NotInsurableException.java
@@ -1,4 +1,10 @@
 package kenakata;
 
-public class NotInsurableException {
+
+import kenakata.exceptions.CheckoutException;
+
+public class NotInsurableException extends CheckoutException {
+    public NotInsurableException(String message) {
+        super(message);
+    }
 }
