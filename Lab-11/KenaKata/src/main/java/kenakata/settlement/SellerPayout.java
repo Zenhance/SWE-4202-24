be596dd0233c38f584ebd367e216944c7801f05f
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-04 05:46:20 +0600

    SellerPayout

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
@@ -1,4 +1,11 @@
 package kenakata.settlement;
 
 public class SellerPayout {
+    public String seller(){
+        return null;
+    }
+
+    public String grossSales(){
+        return null;
+    }
 }
