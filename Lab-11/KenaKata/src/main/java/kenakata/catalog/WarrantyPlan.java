8b6d0aab19e065fecb650b154217f0a37ac198ab
Maomao-starry <aminarahmanshalu@gmail.com>
2026-08-04 04:52:03 +0600

    catalog

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
@@ -0,0 +1,28 @@
+package kenakata.catalog;
+
+public class WarrantyPlan implements OrderableLine{
+    private static final double vat=0.15;
+    private static final double charge= 0.1;
+    private final CatalogueItem item;
+
+    public WarrantyPlan(CatalogueItem item) {
+        if(item==null)
+            throw new IllegalArgumentException("It is invalid");
+        this.item = item;
+    }
+
+    @Override
+    public long unitVat() {
+        return (long)Math.ceil(unitCharge()*vat);
+    }
+
+    @Override
+    public String label() {
+        return "Warranty for "+ item.label();
+    }
+
+    @Override
+    public long unitCharge() {
+        return (long)Math.ceil(item.unitCharge()*charge);
+    }
+}
