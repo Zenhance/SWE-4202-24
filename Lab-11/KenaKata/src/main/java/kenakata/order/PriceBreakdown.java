20fa1e89e170307965f27bd263228563ebd669bd
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-08-04 03:12:43 +0600

    instance variables declare

--- Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
@@ -4,4 +4,8 @@ import kenakata.catalog.*;
 import java.util.*;
 
 public class PriceBreakdown {
+    private ArrayList<Changeable> changeables;
+    private Coupon coupon;
+    private Zone zone;
+    
 }
