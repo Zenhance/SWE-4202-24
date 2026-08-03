be5140374f5a3c88397c1ea125a6055dec7a9109
sameer-abdullah-240042107 <sameerabdullah1117@gmail.com>
2026-08-03 20:58:51 +0600

    c16

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -1,7 +1,7 @@
 package kenakata.catalog;
 
 public class DigitalGood extends CatalogItem{
-    public DigitalGood(String sku,String title,long unitPrice,int stock,Seller seller,int weight) {
+    public DigitalGood(String sku,String title,long unitPrice,int stock,Seller seller) {
         super(sku, title, unitPrice, stock, seller);
     }
 
---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/CheckoutException.java
@@ -0,0 +1,10 @@
+package kenakata.exceptions;
+
+public class CheckoutException extends Exception{
+    public CheckoutException(){
+        super();
+    }
+    public CheckoutException(String message){
+        super(message);
+    }
+}
