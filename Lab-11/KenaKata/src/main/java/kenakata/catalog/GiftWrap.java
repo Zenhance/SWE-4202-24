695fa05198e158e539cc8f382690ffa7b2618859
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-08-04 01:47:05 +0600

    Trying to fix calculation issues...

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/GiftWrap.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/GiftWrap.java
@@ -1,14 +1,13 @@
 package kenakata.catalog;
 
 public class GiftWrap implements Chargeable {
-
     @Override
     public long unitCharge() {
         return 50;
     }
     @Override
     public long unitVat() {
-        return (unitCharge() * 75 + 999) / 1000;
+        return (unitCharge() * 75 + 99) / 100;
     }
     @Override
     public String label() {
