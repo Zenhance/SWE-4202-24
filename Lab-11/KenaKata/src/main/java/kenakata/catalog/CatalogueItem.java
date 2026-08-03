0ad03676b1942d2a5681aad6105f3fd2c616e848
Maomao-starry <aminarahmanshalu@gmail.com>
2026-08-04 02:49:20 +0600

    catalog

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogueItem.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogueItem.java
@@ -1,20 +1,21 @@
 package kenakata.catalog;
 
+import kenakata.exceptions.OutOfStockException;
 import kenakata.settlement.Seller;
 
 public abstract class CatalogueItem implements OrderableLine{
     private final String sku;
     private final String title;
-    private final double unitprice;
-    private int LiveStockCount;
+    private final long unitprice;
+    private int livestock;
     private final Seller seller;
 
 
-    public CatalogueItem(String sku, String title, double unitprice,int LiveStockCount,Seller seller) {
+    public CatalogueItem(String sku, String title, long unitprice,int livestock,Seller seller) {
         if(sku==null || sku.isBlank() || title==null || title.isBlank() || seller==null){
             throw new IllegalArgumentException("MEOWWWWWWW");
         }
-        if(unitprice<0 || LiveStockCount<0){
+        if(unitprice<0 || livestock<0){
             throw new IllegalArgumentException("MEOWWWWWWW");
         }
 
@@ -22,6 +23,8 @@ public abstract class CatalogueItem implements OrderableLine{
         this.title = title;
         this.unitprice = unitprice;
         this.seller = seller;
+        this.livestock=livestock;
+
     }
     public String getSku(){
         return sku;
@@ -30,23 +33,40 @@ public abstract class CatalogueItem implements OrderableLine{
         return title;
     }
 
-    public double getUnitprice() {
+    public long getUnitprice() {
         return this.unitprice;
     }
 
-    public int getLiveStockCount() {
-        return this.LiveStockCount;
+    public int getLivestock() {
+        return this.livestock;
     }
 
     public Seller getSeller() {
         return this.seller;
     }
+    public int remaining(){
+        return livestock;
+    }
+    public void reserve(int qty)throws OutOfStockException {
+            if(qty<=0){
+                throw new IllegalArgumentException("Invalid quantity is given");
+            }
+            if(qty>livestock){
+                throw new OutOfStockException("Not enough amount is available");
+            }
+            livestock=livestock-qty;
+    }
+
     @Override
-    public String getReceiptLabel(){
+    public String label(){
         return title;
     }
     @Override
-    public long getCharge(int quantity){
-        return (long) (unitprice*quantity);
+    public long unitCharge(){
+        return unitprice;
     }
+
+    @Override
+    public abstract long unitVat() ;
+    public abstract long commissionOn(long amount);
 }
