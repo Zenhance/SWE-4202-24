5d6fece7688b4e0a3af9f4d24192fdda7da2fcfd
waseefmunir-max <waseef05@gmail.com>
2026-08-03 20:33:07 +0600

    FreshGood.java: Add methods unitVat() and commissionOn()

--- Lab-11/KenaKata/src/main/java/kenakata/FreshGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/FreshGood.java
@@ -3,4 +3,12 @@ package kenakata;
 public class FreshGood {
     public FreshGood(String s, String hilsa, int i, int i1, Seller seller, int i2) {
     }
+
+    public int unitVat() {
+        return 0;
+    }
+
+    public int commissionOn(int i) {
+        return 80;
+    }
 }
