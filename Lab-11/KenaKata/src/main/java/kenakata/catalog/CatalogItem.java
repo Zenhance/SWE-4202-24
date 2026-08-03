6cacfd4c8f87599f3c09e20cace628f0adb2c1c5
wasifjafar <wasifjafar71@gmail.com>
2026-08-03 10:56:51 +0600

    add validation logic in CatalogItem class's constructor

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
@@ -11,11 +11,30 @@ public abstract class CatalogItem implements Chargeable{
     private boolean isInsured;
 
     public CatalogItem(String SKU, String title, double unitPrice, int stockCount, Seller seller) {
+        if (SKU == null)
+            throw new IllegalArgumentException("SKU cannot be null");
+        if (SKU.isBlank())
+            throw new IllegalArgumentException("SKU cannot be blank");
         this.SKU = SKU;
+
+        if (title == null)
+            throw new IllegalArgumentException("Title cannot be null");
+        if (title.isBlank())
+            throw new IllegalArgumentException("Title cannot be blank");
         this.title = title;
+
+        if (unitPrice < 0.0)
+            throw new IllegalArgumentException("Unit price cannot negative");
         this.unitPrice = unitPrice;
+
+        if (stockCount < 0)
+            throw new IllegalArgumentException("Stock count cannot be negative");
         this.stockCount = stockCount;
+
+        if (seller == null)
+            throw new IllegalArgumentException("Seller cannot be null");
         this.seller = seller;
+
         isInsured = false;
     }
 
