3fe87f73802e750eebcd7c7b8660da5e77c93005
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-04 01:35:36 +0600

    change : write CashOnDeliveryPayment class [1]

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
@@ -1,4 +1,14 @@
 package kenakata.payment;
 
-public class CashOnDeliveryPayment {
+import kenakata.exceptions.CodCeilingExceededException;
+
+public class CashOnDeliveryPayment implements PaymentMethod {
+    private static long COD_CEILING = 15000;
+
+    @Override
+    public void authorise(long amount) throws CodCeilingExceededException {
+        if (amount > COD_CEILING) {
+            throw new CodCeilingExceededException("Grand total exceeds maximum COD limit of 15000");
+        }
+    }
 }
