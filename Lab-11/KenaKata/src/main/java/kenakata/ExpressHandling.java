2f14c6ad5ae6c4215b7af7bbbe2b38fc66b19a21
waseefmunir-max <waseef05@gmail.com>
2026-08-03 20:41:25 +0600

    ExpressHandling.java: Add method unitCharge() GiftWrap.java: Add methods unitCharge() and label()

--- Lab-11/KenaKata/src/main/java/kenakata/ExpressHandling.java
+++ Lab-11/KenaKata/src/main/java/kenakata/ExpressHandling.java
@@ -4,4 +4,8 @@ public class ExpressHandling {
     public int unitVat() {
         return 9;
     }
+
+    public int unitCharge() {
+        return 120;
+    }
 }
--- Lab-11/KenaKata/src/main/java/kenakata/GiftWrap.java
+++ Lab-11/KenaKata/src/main/java/kenakata/GiftWrap.java
@@ -4,4 +4,12 @@ public class GiftWrap {
     public int unitVat() {
         return 4;
     }
+
+    public int unitCharge() {
+        return 50;
+    }
+
+    public String label() {
+        return "Gift wrapping";
+    }
 }
