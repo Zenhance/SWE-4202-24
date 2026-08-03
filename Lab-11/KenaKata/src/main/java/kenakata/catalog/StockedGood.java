4262112b30fb492399704904127036bd4ac73f1f
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-08-03 23:22:10 +0600

    fixed errors

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -6,26 +6,40 @@ import kenakata.catalog.capabilities.Weighable;
 import kenakata.exceptions.OutOfStockException;
 
 public class StockedGood extends Product implements Weighable, Returnable, Insurable {
-    private int weightInGrams;
+    private final int weightInGrams;
 
-    public StockedGood(String sku, String title, long unitPrice, int stock, Seller seller, int weightGrams)
-            throws OutOfStockException {
+    public StockedGood(String sku, String title, long unitPrice, int stock, Seller seller, int weightInGrams) throws OutOfStockException {
         super(sku, title, unitPrice, stock, seller);
 
-        if (weightGrams <= 0) {
+        if (weightInGrams <= 0) {
             throw new IllegalArgumentException("Weight must be greater than zero in grams.");
         }
         this.weightInGrams = weightInGrams;
+
     }
 
     @Override
-    public int weightInGrams() { return weightInGrams;}
-    @Override
+    public int weightInGrams() {
+        return weightInGrams;}
+
     public long unitVat() {
         return (long) Math.ceil(unitPrice()*0.075);
     }
+
+    @Override
+    public boolean isDiscountable() {
+        return true;
+    }
+
     @Override
     public long commissionOn() {
         return (long) Math.ceil(unitPrice()*0.08);
+    }
+
+    @Override
+    public int returnWindowDays() { return  7; }
+
+    public long insurableValue() {
+        return (long) unitPrice();
     }
 }
