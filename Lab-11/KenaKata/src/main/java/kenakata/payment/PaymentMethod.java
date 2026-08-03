2ed711d14a76686e4008a192e13ad909d21372cc
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-04 02:59:34 +0600

    payment method fixing

--- Lab-11/KenaKata/src/main/java/kenakata/payment/PaymentMethod.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/PaymentMethod.java
@@ -1,5 +1,5 @@
 package kenakata.payment;
 
-public abstract class PaymentMethod {
-    public abstract void authorise(int amount);
+public interface PaymentMethod {
+     void authorise(int amount);
 }
