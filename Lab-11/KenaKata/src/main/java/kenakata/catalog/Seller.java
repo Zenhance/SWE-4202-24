c612d55d2afcbe9749ea3a3e9090fa695554ffbd
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-08-03 22:12:49 +0600

    Fixed some issues and wrong spacing

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -1,19 +1,15 @@
 package kenakata.catalog;
 
 public class Seller {
-
     private String name;
-
     public Seller(String name) {
         if(name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException("Invalid seller name");}
         this.name = name;
     }
-
     public String name() {
         return name;
     }
-
     @Override
     public String toString() {
         return name;
