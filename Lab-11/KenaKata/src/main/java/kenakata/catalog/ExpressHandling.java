2ac431e9e776cb6f4b1e6326af84327b5c5cfbd2
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-03 22:59:14 +0600

    change : write the ExpressHandling class [2]

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/ExpressHandling.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/ExpressHandling.java
@@ -5,4 +5,14 @@ public class ExpressHandling implements Item {
     public long unitCharge() {
         return 120;
     }
+
+    @Override
+    public long unitVat() {
+        return (long) Math.ceil(120 * 0.075);
+    }
+
+    @Override
+    public String label() {
+        return "Express handling";
+    }
 }
