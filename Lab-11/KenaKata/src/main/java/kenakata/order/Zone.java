9f81123f9e2c57cd6553ebb226cf77d5d65752b5
Rakin <rakinhasan001@gmail.com>
2026-08-04 01:56:34 +0600

    Zone completed as enum

--- Lab-11/KenaKata/src/main/java/kenakata/order/Zone.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Zone.java
@@ -1,4 +1,21 @@
 package kenakata.order;
 
-public class Zone {
+
+    public enum Zone {
+        DHAKA(60,20),
+        OUTSIDE(120,35);
+
+        private final long baseCharge;
+        private final long perKgCharge;
+        Zone(long baseCharge,long perKgCharge){
+            this.baseCharge=baseCharge;
+            this.perKgCharge=perKgCharge;
+        }
+        public long baseCharge(){
+            return baseCharge;
+        }
+        public long perKgCharge(){
+            return perKgCharge;
+        }
+
 }
