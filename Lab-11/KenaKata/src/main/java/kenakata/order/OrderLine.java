dcd568a8566f47e01602a78b6b2201f1a8164ee8
wasif-22 <twasif68@gmail.com>
2026-08-04 00:30:57 +0600

    completed orderline class

--- Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -14,4 +14,45 @@ public final class OrderLine {
         this.item = item;
         this.quantity = quantity;
     }
+    public Chargeable item() {
+        return item;
+    }
+
+    public int quantity() {
+        return quantity;
+    }
+
+    public boolean insured() {
+        return insured;
+    }
+
+    public boolean returned() {
+        return returned;
+    }
+
+    public int placedDay() {
+        return placedDay;
+    }
+
+    public long lineCharge() {
+        return 0;
+    }
+
+    public long lineVat() {
+        return 0;
+    }
+
+    void markInsured() {
+        this.insured = true;
+    }
+
+    void markReturned() {
+        this.returned = true;
+    }
+
+    void recordPlacementDay(int today) {
+        this.placedDay = today;
+    }
+
+}
 
