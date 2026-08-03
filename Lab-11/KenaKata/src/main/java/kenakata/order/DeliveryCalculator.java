e9667730a3b77901db18db1b70dc4dc368d5e583
Inan <inanjaki184@gmail.com>
2026-08-03 20:07:41 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
@@ -1,4 +1,14 @@
 package kenakata.order;
 
+import java.util.List;
+
 public class DeliveryCalculator {
+    public static final long DHAKA_BASE=60;
+    public static final long DHAKA_PER_KG=20;
+    public static final long OUTSIDE_BASE=120;
+    public static final long OUTSIDE_PER_KG=35;
+
+    public long deliveryFor(List<Order> lines, Zone zone){
+        return (long) 0.00;
+    }
 }
