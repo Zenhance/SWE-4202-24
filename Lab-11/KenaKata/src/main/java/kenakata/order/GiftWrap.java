2b7ce665a885907ea8f9f34336d03bbe4fa34b8f
Inan <inanjaki184@gmail.com>
2026-08-03 20:30:10 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/order/GiftWrap.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/GiftWrap.java
@@ -1,4 +1,18 @@
 package kenakata.order;
 
-public class GiftWrap {
+import kenakata.catalog.Chargeable;
+
+public class GiftWrap implements Chargeable {
+    private static final double CHARGE=50;
+    private static final double VAT_RATE_PERCENT=7.5;
+
+    public double unitCharge(){
+        return CHARGE;
+    }
+    public double unitVat(){
+        return (double) Math.ceil(CHARGE*VAT_RATE_PERCENT/100.0);
+    }
+    public String label(){
+        return "Gift wrapping";
+    }
 }
