0222c71eebe546ac58554c87259d64319c715ac8
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-03 21:51:58 +0600

    change : write the AbstractItem class [1]

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/AbstractItem.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/AbstractItem.java
@@ -1,58 +1,11 @@
 package kenakata.catalog;
 
-import kenakata.enums.ItemType;
+import kenakata.exceptions.OutOfStockException;
 
 public abstract class AbstractItem implements Item {
     private String sku;
     private String title;
-    private double unitPrice;
+    private long unitPrice;
     private int stock;
     private Seller seller;
-    private ItemType type;
-
-    public AbstractItem(String sku, String title, double unitPrice, int stock, Seller seller, ItemType type) {
-        if (sku == null || title == null || seller == null) {
-            throw new IllegalArgumentException("Fields cannot be null.");
-        }
-        if (unitPrice < 0 || stock < 0) {
-            throw new IllegalArgumentException("Non-negative value needed.");
-        }
-
-        this.sku = sku;
-        this.title = title;
-        this.unitPrice = unitPrice;
-        this.stock = stock;
-        this.seller = seller;
-        this.type = type;
-    }
-
-    @Override
-    public String getSku() {
-        return sku;
-    }
-
-    @Override
-    public String getTitle() {
-        return title;
-    }
-
-    @Override
-    public double getUnitPrice() {
-        return unitPrice;
-    }
-
-    @Override
-    public int getStockRemaining() {
-        return stock;
-    }
-
-    @Override
-    public Seller getSeller() {
-        return seller;
-    }
-
-    @Override
-    public ItemType getType() {
-        return type;
-    }
 }
