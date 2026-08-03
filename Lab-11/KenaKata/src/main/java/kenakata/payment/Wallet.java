fe54471e0e230ce35398fd409999c9c02bae3aef
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-04 01:17:28 +0600

    change : write Wallet class [1]

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -1,4 +1,18 @@
 package kenakata.payment;
 
+import kenakata.exceptions.EmptyWalletException;
+
 public class Wallet {
+    private long balance;
+
+    public Wallet(long balance) {
+        if (balance < 0) {
+            throw new IllegalArgumentException("Initial balance cannot be negative");
+        }
+        this.balance = balance;
+    }
+
+    public long balance() {
+        return balance;
+    }
 }
