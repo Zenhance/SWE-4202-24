fc8859239691ce2f6463dd8a8dac94d71d5a5e5f
wasifjafar <wasifjafar71@gmail.com>
2026-08-03 12:37:53 +0600

    add validation logic in CardPayment class's constructor

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
@@ -4,6 +4,8 @@ public class CardPayment extends PaymentMethod{
     private int limit;
 
     public CardPayment(int limit) {
+        if (limit <= 0)
+            throw new IllegalArgumentException("Card limit cannot be negative or null");
         this.limit = limit;
     }
 
