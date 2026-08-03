f2db5c2e402951ae03de21cc49587134195ce7b2
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-08-04 01:08:21 +0600

    added constructor

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -3,5 +3,10 @@ import kenakata.exceptions.EmptyWalletException;
 
 public final class Wallet {
     private long balance;
-
+    public Wallet(long openingBalance){
+        if(openingBalance<0){
+            throw new IllegalArgumentException("Wallet balance cannot be negative");
+        }
+        this.balance = openingBalance;
+    }
 }
