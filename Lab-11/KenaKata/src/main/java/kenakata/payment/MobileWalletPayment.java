764531911ad1098773b74b6930b897ea040206d6
58-jahin <jahinhossain@iut-dhaka.edu>
2026-08-03 21:54:13 +0600

    MobileWalletPayment is a payment class that uses a Wallet to pay for an order.

--- Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
@@ -1,6 +1,30 @@
 package kenakata.payment;
+import kenakata.exceptions.EmptyWalletException;
+public final class MobileWalletPayment
+        implements PaymentMethod {
 
-public class MobileWalletPayment {
+    private final Wallet wallet;
+
     public MobileWalletPayment(Wallet wallet) {
+        if (wallet == null) {
+            throw new IllegalArgumentException(
+                    "Wallet cannot be null"
+            );
+        }
+
+        this.wallet = wallet;
+    }
+
+    @Override
+    public void authorise(long amount)
+            throws EmptyWalletException {
+
+        if (amount < 0) {
+            throw new IllegalArgumentException(
+                    "Payment amount cannot be negative"
+            );
+        }
+
+        wallet.debit(amount);
     }
 }
