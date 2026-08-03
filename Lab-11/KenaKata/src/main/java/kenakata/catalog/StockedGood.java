a1f5839326b9e1f47e538d956d2936d46268eaa7
jasin <anishaajasin.13.04.2005@gmail.com>
2026-08-04 02:57:17 +0600

    commit

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -2,21 +2,28 @@ package kenakata.catalog;
 
 
 public class StockedGood extends Item{
+    private final int weight;
 
-    public StockedGood(String sku, String title, long unitPrice, int stock, Seller seller){
+    public StockedGood(String sku, String title, long unitPrice, int stock, Seller seller,int weight){
         super(sku, title, unitPrice, stock, seller);
+
+        if (weight <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.weight = weight;
     }
 
-    public int unitCharge(){
+    public long unitCharge(){
         return unitPrice;
     }
 
-    public int unitVat(){
-        return ceil(unitPrice*7.5%);
+    public long unitVat(){
+        return (long)Math.ceil(unitPrice*0.075);
     }
 
-    public int commissionOn(int n){
-        return ceil(n*8%);
+    public long commissionOn(int n){
+        return (long)Math.ceil(n*0.008);
     }
 
 }
