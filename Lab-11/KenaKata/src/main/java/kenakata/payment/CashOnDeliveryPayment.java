aceb65d970d4e50dca0dcef7d7ddbeaa35111ef8
04-Labiba <sabihalabiba639@gmail.com>
2026-08-03 21:45:50 +0600

    init commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
@@ -0,0 +1,13 @@
+package kenakata.payment;
+import kenakata.exceptions.CodCeilingExceededException;
+import kenakata.exceptions.PaymentDeclinedException;
+public class CashOnDeliveryPayment implements PaymentMethod {
+    private static final long COD_CEILING = 15000;
+
+    @Override
+    public void authorise(long amount) throws PaymentDeclinedException {
+        if (amount > COD_CEILING) {
+            throw new CodCeilingExceededException("COD ceiling exceeded");
+        }
+    }
+}
