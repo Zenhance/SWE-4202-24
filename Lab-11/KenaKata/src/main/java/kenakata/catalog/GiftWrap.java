3cacda92c278cf01e39cd3e108c7197a7b7d69a6
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-08-03 23:48:00 +0600

    added constructor and methods

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/GiftWrap.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/GiftWrap.java
@@ -1,4 +1,17 @@
 package kenakata.catalog;
 
-public class GiftWrap {
+public class GiftWrap extends Product{
+
+    public GiftWrap() {
+        super(50);
+    }
+
+    public String label() {
+        return "Gift wrapping";
+    }
+
+    @Override
+    public long unitVat() {
+        return 0;
+    }
 }
