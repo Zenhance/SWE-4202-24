7cfb846c299e75dbc40ab8b8c3ce2f91d512ec13
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-08-03 23:03:25 +0600

    new SettlementReport class

---
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
@@ -0,0 +1,13 @@
+package kenakata.settlement;
+import java.util.Collections;
+import java.util.List;
+import kenakata.catalog.Seller;
+
+public final class SettlementReport {
+    private final List<SellerPayout> payouts;
+    private final long platformRevenue;
+
+    public SettlementReport(List<SellerPayout> payouts, long platformRevenue){
+
+    }
+}
