f1593b9c7a74c204f8a61e1c197ebdc0f29d65ca
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-04 01:24:22 +0600

    change : write MobileWalletPayment class [1]

--- Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
@@ -1,4 +1,14 @@
 package kenakata.payment;
 
-public class MobileWalletPayment {
+import kenakata.exceptions.EmptyWalletException;
+
+public class MobileWalletPayment implements PaymentMethod {
+    private final Wallet wallet;
+
+    public MobileWalletPayment(Wallet wallet) {
+        if (wallet == null) {
+            throw new IllegalArgumentException("Wallet cannot be null");
+        }
+        this.wallet = wallet;
+    }
 }
