dd371388b5661472b272d514ac0aadcda28a197c
Maomao-starry <aminarahmanshalu@gmail.com>
2026-08-04 04:14:47 +0600

    catalog

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/GiftWrap.java
@@ -0,0 +1,21 @@
+package kenakata.catalog;
+
+public class GiftWrap implements OrderableLine{
+    private static final double vat=0.075;
+    private static final int wrapping_charge=50;
+
+    @Override
+    public long unitVat() {
+        return (long)Math.ceil(unitCharge()*0.075);
+    }
+
+    @Override
+    public String label() {
+        return "Gift wrapping";
+    }
+
+    @Override
+    public long unitCharge() {
+        return wrapping_charge;
+    }
+}
