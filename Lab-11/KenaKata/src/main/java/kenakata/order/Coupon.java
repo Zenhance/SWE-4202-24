f1c5ab4a1fef2064aa1ab0f39b1276014cd3190e
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-03 23:52:05 +0600

    change : write Coupon class [1]

--- Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -1,4 +1,12 @@
 package kenakata.order;
 
+import kenakata.exceptions.InvalidCouponException;
+
 public class Coupon {
+    private String code;
+    private int percentage;
+    private long cap;
+    private long minSpend;
+    private int lastValidDay;
+}
 }
