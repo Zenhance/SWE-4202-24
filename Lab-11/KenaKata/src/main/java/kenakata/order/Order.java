9ecdc95851e9daeaa6c1c389f6824309c427c082
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-04 00:11:31 +0600

    change : write Order class [2]

--- Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
@@ -9,4 +9,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class Order {
+    private Zone zone;
+    private DeliveryCalculator deliveryCalculator;
+    private List<OrderLine> lines = new ArrayList<>();
+    private Coupon coupon;
+    private boolean isPlaced = false;
+    private PriceBreakdown finalBreakdown;
+    private int placementDay;
 }
