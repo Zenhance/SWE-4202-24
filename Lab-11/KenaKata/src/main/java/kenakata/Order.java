98dbb8b6238ad594cb3a8f05c063e836c45c6aed
waseefmunir-max <waseef05@gmail.com>
2026-08-03 21:57:29 +0600

    Order.java: Add addProduct() method

--- Lab-11/KenaKata/src/main/java/kenakata/Order.java
+++ Lab-11/KenaKata/src/main/java/kenakata/Order.java
@@ -3,4 +3,7 @@ package kenakata;
 public class Order {
     public Order(Object dhaka, DeliveryCalculator deliveryCalculator) {
     }
+
+        public void addProduct(StockedGood stockedGood, int i) {
+    }
 }
