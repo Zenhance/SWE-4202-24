3c64f730092f51fdcf79d2b66569689ec6e007b5
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-03 21:52:39 +0600

    change : write the AbstractItem class [2]

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/AbstractItem.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/AbstractItem.java
@@ -8,4 +8,12 @@ public abstract class AbstractItem implements Item {
     private long unitPrice;
     private int stock;
     private Seller seller;
+
+    public AbstractItem(String sku, String title, long unitPrice, int stock, Seller seller) {
+        this.sku = sku;
+        this.title = title;
+        this.unitPrice = unitPrice;
+        this.stock = stock;
+        this.seller = seller;
+    }
 }
