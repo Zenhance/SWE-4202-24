98bb92f4c8f2355d27b39cd5fc9852c7e84212b6
Inan <inanjaki184@gmail.com>
2026-08-03 21:10:34 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
@@ -1,4 +1,47 @@
 package kenakata.order;
 
+import kenakata.catalog.CatalogItem;
+import kenakata.catalog.Chargeable;
+import kenakata.payment.PaymentMethod;
+
+import java.util.Collections;
+import java.util.List;
+
 public class Order {
+
+    public void addProduct(CatalogItem item, int quantity){
+
+    }
+    public void addAddOn(Chargeable addOn){
+
+    }
+    public void applyCoupon(Coupon coupon){
+
+    }
+    public void insure(int lineIndex){
+
+    }
+    public List<Order> lines(){
+        return null;
+    }
+    public boolean placced(){
+        return false;
+    }
+    public PriceBreakdown finalBreakdown(){
+        return null;
+    }
+    public PriceBreakdown quote(int today){
+        return null;
+    }
+    public void place(PaymentMethod payment,int today){
+
+    }
+    public void acceptReturn(int lineIndex, int today){
+
+    }
+    public PriceBreakdown priceOrder(int today){
+        return null;
+    }
+
+
 }
