98cf55df4ae74c661f1b474fecb4b6556f5e80c5
04-Labiba <sabihalabiba639@gmail.com>
2026-08-03 20:58:00 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -1,6 +1,6 @@
 package kenakata.payment;
 
-public class Wallet {
+public class Wallet{
     private long balance;
 
     public Wallet(long initialBalance){
@@ -8,5 +8,21 @@ public class Wallet {
             throw new IllegalArgumentException("Initial balance cannot be negative");
         }
         this.balance=initialBalance;
+    }
+    public long balance(){
+        return balance;
+    }
+    public void credit(long amount){
+        if (amount<=0) {
+            throw new IllegalArgumentException("Credit amount must be positive");
+        }
+        balance+=amount;
+    }
+
+    public void debit(long amount){
+        if (amount>balance){
+            throw new IllegalArgumentException("Insufficient balance");
+        }
+        balance-=amount;
     }
 }
