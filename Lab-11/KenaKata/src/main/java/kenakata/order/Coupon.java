5d55e6ff51c709fb6eecddf9b906daefcb2ed881
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-04 03:45:52 +0600

    coupon.order added constructor

--- Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -5,4 +5,11 @@ public class Coupon {
     private final long percentage;
     private final long cap;
     private final long validDays;
+
+    public Coupon(String couponCode, long percentage, long cap, long validDays) {
+        this.couponCode = couponCode;
+        this.percentage = percentage;
+        this.cap = cap;
+        this.validDays = validDays;
+    }
 }
