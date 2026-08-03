1dcaad21068dc57b856a0b426f9db37189574866
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-08-03 22:29:43 +0600

    new final class

---
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -0,0 +1,17 @@
+package kenakata.order;
+
+import kenakata.catalog.CatalogItem;
+import kenakata.catalog.Chargeable;
+
+public final class OrderLine {
+    private final Chargeable unit;
+    private final int quantity;
+    private boolean insured;
+    private boolean returned;
+
+    OrderLine(Chargeable unit, int quantity){
+        if(unit == null){
+            throw new IllegalArgumentException("Order line unit must not be null");
+        }
+    }
+}
