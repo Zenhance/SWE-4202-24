74a468ccabba478195b3908f64830fdd1dcded2d
Rakin <rakinhasan001@gmail.com>
2026-08-04 01:12:17 +0600

    CodCeiling exception

--- Lab-11/KenaKata/src/main/java/kenakata/exception/CodCeilingExceededException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exception/CodCeilingExceededException.java
@@ -1,4 +1,7 @@
 package kenakata.exception;
 
-public class CodCeilingExceededException {
+public class CodCeilingExceededException extends PaymentDeclinedException{
+    public CodCeilingExceededException(String message) {
+        super(message);
+    }
 }
