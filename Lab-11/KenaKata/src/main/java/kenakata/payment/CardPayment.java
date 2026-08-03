4ae24a904a7db46456d77b1544a4a98cf719f702
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-08-04 01:34:06 +0600

    finished class with overridden methods

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
@@ -1,6 +1,8 @@
 package kenakata.payment;
 import kenakata.exceptions.CardLimitExceededException;
 
+import javax.smartcardio.Card;
+
 public final class CardPayment implements PaymentMethod{
     private long remainingLimit;
     public CardPayment(long limit){
@@ -9,4 +11,20 @@ public final class CardPayment implements PaymentMethod{
         }
         this.remainingLimit= limit;
     }
+    public long remainingLimit(){
+        return remainingLimit;
+    }
+    @Override
+    public void validate(long amount)throws CardLimitExceededException{
+        PaymentMethod.requireNonNegative(amount);
+        if(amount>remainingLimit){
+            throw new CardLimitExceededException("Card limit exceeded");
+    }
+    }
+    @Override
+    public void authorise(long amount) throws CardLimitExceededException{
+        validate(amount);
+        remainingLimit-=amount;
+    }
 }
+
