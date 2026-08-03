5a29accdc4c9149da99410f717a8d9e864cf73f5
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-03 22:43:11 +0600

    change : write the GiftWrap class [2]

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/GiftWrap.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/GiftWrap.java
@@ -5,4 +5,14 @@ public class GiftWrap implements Item {
     public long unitCharge() {
         return 50;
     }
+
+    @Override
+    public long unitVat() {
+        return (long) Math.ceil(50 * 0.075);
+    }
+
+    @Override
+    public String label() {
+        return "Gift wrapped";
+    }
 }
