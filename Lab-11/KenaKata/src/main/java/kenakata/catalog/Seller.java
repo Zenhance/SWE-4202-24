5c05e4df98b7e75d2af59c9b9fc41f18222e9000
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-08-04 02:53:28 +0600

    Seller class modified

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -3,6 +3,9 @@ package kenakata.catalog;
 public class Seller {
     private final String name;
     
+    public Seller(){
+        this.name = "Seller";
+    }
     public Seller(String name){
         this.name = name;
     }
