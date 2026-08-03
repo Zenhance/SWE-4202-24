861a87acb546ed1798541bb67450d8d686847d67
Maomao-starry <aminarahmanshalu@gmail.com>
2026-08-04 05:22:35 +0600

    task

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/IneligibleLineException.java
+++
@@ -1,7 +0,0 @@
-package kenakata.exceptions;
-
-public class IneligibleLineException extends KenaKataException{
-    public IneligibleLineException(String msg) {
-        super(msg);
-    }
-}
---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/ReturnNotAllowedException.java
@@ -0,0 +1,7 @@
+package kenakata.exceptions;
+
+public class ReturnNotAllowedException extends KenaKataException{
+    public ReturnNotAllowedException(String msg) {
+        super(msg);
+    }
+}
