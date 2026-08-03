91c1ce45970b53f6d509c1cd3eedcf97dff7348b
04-Labiba <sabihalabiba639@gmail.com>
2026-08-03 20:48:55 +0600

    init commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
@@ -0,0 +1,16 @@
+package kenakata.catalog;
+import kenakata.order.ColdChain;
+import kenakata.order.Insurable;
+import kenakata.order.Returnable;
+import kenakata.order.Weighable;
+import kenakata.settlement.Seller;
+public class FreshGood extends CatalogItem implements Weighable,ColdChain,Insurable,Returnable{
+    private final int weightGrams;
+    public FreshGood(String sku,String title,long unitPrice,int stock, Seller seller, int weightGrams) {
+        super(sku, title, unitPrice, stock, seller);
+        if (weightGrams<=0){
+            throw new IllegalArgumentException("Weight must be positive");
+        }
+        this.weightGrams=weightGrams;
+    }
+}
