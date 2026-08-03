3d0aed675096b613baf3a03771497acf7a31e2b1
Maomao-starry <aminarahmanshalu@gmail.com>
2026-08-04 05:53:06 +0600

    task

---
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -0,0 +1,52 @@
+package kenakata.order;
+
+
+import kenakata.catalog.OrderableLine;
+
+public class OrderLine {
+    private final OrderableLine item;
+    private final int quantity;
+    private boolean insured;
+    private boolean returned;
+
+
+    public OrderLine(OrderableLine item, int quantity) {
+        if(item==null)
+            throw new IllegalArgumentException("Invalid");
+        if(quantity<=0)
+            throw new IllegalArgumentException("Invalid");
+        this.item = item;
+        this.quantity = quantity;
+    }
+    public OrderableLine getItem(){
+        return item;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public boolean isInsured() {
+        return this.insured;
+    }
+
+    public void insure() {
+        insured = true;
+    }
+    public boolean returned() {
+        return returned;
+    }
+
+     public void markreturned(){
+        returned=true;
+     }
+
+
+    public long vat(){
+        return item.unitVat()*quantity;
+    }
+
+
+   public  long Charge(){
+        return  item.unitCharge()*quantity;
+   }
+}
