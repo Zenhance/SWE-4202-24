b825ddd586189d4a1025bea211b8aa6e2676fa75
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-08-03 23:37:10 +0600

    added overridden methods

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -1,9 +1,19 @@
 package kenakata.catalog;
 
-import kenakata.exceptions.InsufficientStockException;
+import kenakata.exceptions.OutOfStockException;
 
 public class DigitalGood extends Product{
-    public DigitalGood(String sku, String title, double unitPrice, int stock, Seller seller) throws InsufficientStockException {
+    public DigitalGood(String sku, String title, long unitPrice, int stock, Seller seller) throws OutOfStockException {
         super(sku, title, unitPrice, stock, seller);
+    }
+
+    @Override
+    public long unitVat() {
+        return (long) Math.ceil(unitPrice()*0.05);
+    }
+
+    @Override
+    public long commissionOn() {
+        return (long) Math.ceil(unitPrice()*0.2);
     }
 }
