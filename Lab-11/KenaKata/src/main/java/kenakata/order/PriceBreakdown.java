81f8d6d24395f99e803fa7ff7d22b45b436fd4a7
04-Labiba <sabihalabiba639@gmail.com>
2026-08-03 22:20:46 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
@@ -1,6 +1,6 @@
 package kenakata.order;
 
-public class PriceBreakdown{
+public class PriceBreakdown {
     private final long subtotal;
     private final long discount;
     private final long vat;
@@ -8,13 +8,35 @@ public class PriceBreakdown{
     private final long insurance;
     private final long serviceFee;
     private final long grandTotal;
-    public PriceBreakdown(long subtotal,long discount,long vat,long delivery,long insurance,long serviceFee,long grandTotal){
-        this.subtotal=subtotal;
-        this.discount=discount;
-        this.vat=vat;
-        this.delivery=delivery;
-        this.insurance=insurance;
-        this.serviceFee=serviceFee;
-        this.grandTotal=grandTotal;
+
+    public PriceBreakdown(long subtotal, long discount, long vat, long delivery, long insurance, long serviceFee, long grandTotal) {
+        this.subtotal = subtotal;
+        this.discount = discount;
+        this.vat = vat;
+        this.delivery = delivery;
+        this.insurance = insurance;
+        this.serviceFee = serviceFee;
+        this.grandTotal = grandTotal;
     }
-}
+        public long subtotal () {
+            return subtotal;
+        }
+        public long discount () {
+            return discount;
+        }
+        public long vat () {
+            return vat;
+        }
+        public long delivery () {
+            return delivery;
+        }
+        public long insurance () {
+            return insurance;
+        }
+        public long serviceFee () {
+            return serviceFee;
+        }
+        public long grandTotal () {
+            return grandTotal;
+        }
+    }
