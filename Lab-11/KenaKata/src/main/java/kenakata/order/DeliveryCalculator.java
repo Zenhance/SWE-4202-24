16a2018b4ce4b2f142269987e61139460f609aa1
04-Labiba <sabihalabiba639@gmail.com>
2026-08-03 21:50:08 +0600

    init commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
@@ -0,0 +1,19 @@
+package kenakata.order;
+import java.util.List;
+public class DeliveryCalculator{
+    public long calculateDelivery(List<OrderLine> lines,Zone zone) {
+        int totalWeightGrams = 0;
+        long coldChainFee = 0;
+        boolean hasWeighable = false;
+
+        for (OrderLine line : lines) {
+            Chargeable item = line.item();
+            if (item instanceof Weighable weighable) {
+                totalWeightGrams += weighable.weightGrams() * line.quantity();
+                hasWeighable = true;
+            }
+            if (item instanceof ColdChain coldChain) {
+                coldChainFee += coldChain.coldChainSurcharge();
+            }
+        }
+    }
