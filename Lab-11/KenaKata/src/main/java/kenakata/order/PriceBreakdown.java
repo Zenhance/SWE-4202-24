6b359061fa0312879b41a440be7370bef943f81f
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-08-04 03:20:41 +0600

    Initialize and create constructor with validation of PriceBreakdown class

--- Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
@@ -7,5 +7,17 @@ public class PriceBreakdown {
     private ArrayList<Chargeable> chargeables;
     private Coupon coupon;
     private Zone zone;
-    
+
+    public PriceBreakdown(ArrayList<Chargeable> chargeables, Zone zone){
+        if(chargeables == null){
+            throw new IllegalArgumentException("ArrayList of chargeables cannot be null");
+    }
+        this.chargeables = chargeables;
+
+        if(zone == null){
+            throw new IllegalArgumentException("Delivery zone cannot be null.");
+    }
+        this.zone = zone;
+    }
+
 }
