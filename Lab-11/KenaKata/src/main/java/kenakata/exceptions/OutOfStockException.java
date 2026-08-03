93df3ac042a34c7045e9b195a3439eae51db6dc3
Maomao-starry <aminarahmanshalu@gmail.com>
2026-08-04 02:24:02 +0600

    catalog

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/OutOfStockException.java
@@ -0,0 +1,8 @@
+package kenakata.exceptions;
+
+public class OutOfStockException extends KenaKataException{
+
+    public OutOfStockException(String msg) {
+        super(msg);
+    }
+}
--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/StockUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package kenakata.exceptions;
-
-public class StockUnavailableException extends KenaKataException{
-
-    public StockUnavailableException(String msg) {
-        super(msg);
-    }
-}
