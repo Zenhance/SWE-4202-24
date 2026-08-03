133cfb0f5a00c9b03b7ff506bd4cf8439b09b3ab
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-03 23:30:15 +0600

    change : write OrderLine class [1]

--- Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -1,4 +1,17 @@
 package kenakata.order;
 
+import kenakata.catalog.Chargeable;
+
 public class OrderLine {
+    private Chargeable item;
+    private int quantity;
+    private boolean insured;
+    private boolean returned;
+
+    public OrderLine(Chargeable item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+        this.insured = false;
+        this.returned = false;
+    }
 }
