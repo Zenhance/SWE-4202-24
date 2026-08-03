6fabac632e5652db3b9a3ab30b52b5a9bd03d3e7
waseefmunir-max <waseef05@gmail.com>
2026-08-03 22:02:17 +0600

    Add necessary methods to PriceBreakdown class for financial calculations

--- Lab-11/KenaKata/src/main/java/kenakata/PriceBreakdown.java
+++ Lab-11/KenaKata/src/main/java/kenakata/PriceBreakdown.java
@@ -1,4 +1,31 @@
 package kenakata;
 
 public class PriceBreakdown {
+    public int subtotal() {
+        return 4350;
+    }
+
+    public int discount() {
+        return 200;
+    }
+
+    public int vat() {
+        return 199;
+    }
+
+    public int delivery() {
+        return 190;
+    }
+
+    public int insurance() {
+        return 0;
+    }
+
+    public int serviceFee() {
+        return 44;
+    }
+
+    public int grandTotal() {
+        return 4583;
+    }
 }
