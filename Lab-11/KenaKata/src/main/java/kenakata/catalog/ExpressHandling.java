83ea0a20b09cd699070c395fd103919840fdb90f
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-08-03 23:51:40 +0600

    added constructor & method

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/ExpressHandling.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/ExpressHandling.java
@@ -1,4 +1,12 @@
 package kenakata.catalog;
 
-public class ExpressHandling {
+public class ExpressHandling extends Product{
+    public ExpressHandling() {
+        super(120);
+    }
+
+    @Override
+    public long unitVat() {
+        return (long) Math.ceil(unitPrice()*0.075);
+    }
 }
