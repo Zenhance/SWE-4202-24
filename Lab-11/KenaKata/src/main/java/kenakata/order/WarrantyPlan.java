41fe2f817c1bf0cc90fc121989d612294dd7be5b
Inan <inanjaki184@gmail.com>
2026-08-04 00:08:20 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/order/WarrantyPlan.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/WarrantyPlan.java
@@ -1,4 +1,21 @@
 package kenakata.order;
 
-public class WarrantyPlan {
+import kenakata.catalog.Chargeable;
+
+public class WarrantyPlan implements Chargeable {
+    private static final double CHARGE_RATE_PERCENT = 10;
+    private static final double VAT_RATE_PERCENT = 15;
+
+    private Chargeable coveredItem;
+
+    public double unitCharge(){
+        return (double)Math.ceil(coveredItem.unitCharge()*CHARGE_RATE_PERCENT/100.0);
+    }
+    public double unitVat(){
+        return (double) Math.ceil(unitCharge() *VAT_RATE_PERCENT/100.0);
+    }
+    public String label(){
+        return "Extended warranty for"+coveredItem.label();
+    }
+
 }
