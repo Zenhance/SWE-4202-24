1a8429f98f020445ee0bdff4231de7a39c084269
Fayaz16 <shahtanzim900@gmail.com>
2026-08-04 02:17:58 +0600

    zone created

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/ReturnNotAllowedException.java
@@ -0,0 +1,8 @@
+package kenakata.exceptions;
+
+public class ReturnNotAllowedException extends CheckoutException {
+
+    public ReturnNotAllowedException (String message){
+        super(message);
+    }
+}
