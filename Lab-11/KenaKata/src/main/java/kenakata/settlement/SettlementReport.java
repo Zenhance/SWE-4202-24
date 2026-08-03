765c31d092efc866c1a58f7e9eeb25771ba86fb1
sameer-abdullah-240042107 <sameerabdullah1117@gmail.com>
2026-08-04 01:42:45 +0600

    c25

---
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
@@ -0,0 +1,25 @@
+package kenakata.settlement;
+
+import kenakata.catalog.Seller;
+import java.util.Collection;
+import java.util.Map;
+
+public class SettlementReport {
+    private final Map<Seller, SellerPayout> payouts;
+    private final long platformRevenue;
+
+    public SettlementReport(Map<Seller, SellerPayout> payouts, long platformRevenue) {
+        this.payouts = payouts;
+        this.platformRevenue = platformRevenue;
+    }
+    public SellerPayout forSeller(Seller seller){
+        return payouts.getOrDefault(seller,new SellerPayout(seller));
+    }
+    public Collection<SellerPayout> payouts() {
+        return payouts.values();
+    }
+
+    public long platformRevenue() {
+        return platformRevenue;
+    }
+}
