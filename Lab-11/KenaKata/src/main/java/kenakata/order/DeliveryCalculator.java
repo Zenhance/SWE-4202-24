b9cbed5a62b9dc00080fab20afc4999f546caaed
wasif-22 <twasif68@gmail.com>
2026-08-04 00:39:21 +0600

    added deliverycalculator class

---
+++ Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
@@ -0,0 +1,10 @@
+package kenakata.order;
+
+import java.util.List;
+
+public final class DeliveryCalculator {
+
+    public long deliveryFee(Zone zone, List<OrderLine> lines) {
+        return 0;
+    }
+}
