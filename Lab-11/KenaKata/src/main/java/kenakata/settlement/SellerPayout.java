b0e2e3593dab59f369f4f31c140349dbc438a00b
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-08-03 23:00:14 +0600

    record SellerPayout

---
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
@@ -0,0 +1,11 @@
+package kenakata.settlement;
+import kenakata.catalog.Seller;
+
+public record SellerPayout(
+  Seller seller,
+  long grossSales,
+  long commission,
+  long refunds,
+  long payout
+){
+}
