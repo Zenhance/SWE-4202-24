3e49c96f4a0ba2c8a4a52edba2b3c37fe7e40f4b
Zunied-57 <zunied0411@gmail.com>
2026-08-03 21:13:02 +0600

    warrantyplan created

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
@@ -0,0 +1,24 @@
+package kenakata.catalog;
+
+public class WarrantyPlan implements Chargeable
+{
+    private CatalogItem catalogItem;
+
+    @Override
+    public long unitCharge()
+    {
+        return (long)Math.ceil(catalogItem.unitCharge()*0.01);
+    }
+
+    @Override
+    public long unitVat()
+    {
+        return (long)Math.ceil(unitCharge()*0.15);
+    }
+
+    @Override
+    public String label()
+    {
+        return catalogItem.title();
+    }
+}
