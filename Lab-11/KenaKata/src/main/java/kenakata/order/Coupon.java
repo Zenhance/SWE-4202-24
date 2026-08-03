b6cbb79501126e9ec665f55ea3a48c8eed755c27
wasifjafar <wasifjafar71@gmail.com>
2026-08-03 12:21:17 +0600

    add validation logic in Coupon class's constructor

--- Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -1,5 +1,7 @@
 package kenakata.order;
 
+import java.util.IllformedLocaleException;
+
 public class Coupon {
     private String code;
     private int percentage;
@@ -8,10 +10,26 @@ public class Coupon {
     private int lastValidDay;
 
     public Coupon(String code, int percentage, int cap, int minimumSpend, int lastValidDay) {
+        if (code == null)
+            throw new IllegalArgumentException("Coupon code cannot be null");
+        if (code.isBlank())
+            throw new IllegalArgumentException("Coupon code cannot be blank");
         this.code = code;
+
+        if (percentage <= 0)
+            throw new IllegalArgumentException("Discount percentage cannot be negative or equal to zero");
         this.percentage = percentage;
+
+        if (cap <= 0)
+            throw new IllegalArgumentException("Cap cannot be negative or zero");
         this.cap = cap;
+
+        if (minimumSpend < 0)
+            throw new IllegalArgumentException("Minimum expenditure cannot be negative");
         this.minimumSpend = minimumSpend;
+
+        if (lastValidDay <= 0)
+            throw new IllegalArgumentException("Last valid day cannot be negative or zero");
         this.lastValidDay = lastValidDay;
     }
 
