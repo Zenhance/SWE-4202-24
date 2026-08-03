c9b8a7d617ae7a41d9c814dfd8d7be42453a07fb
Maomao-starry <aminarahmanshalu@gmail.com>
2026-08-04 02:49:40 +0600

    catalog

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/OrderableLine.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/OrderableLine.java
@@ -1,7 +1,10 @@
 package kenakata.catalog;
 
 public interface OrderableLine {
-    String getReceiptLabel();
-    long getCharge(int quantity);
-    long getVat(int quantity);
+
+   long unitVat();
+
+    String label();
+
+    long unitCharge();
 }
