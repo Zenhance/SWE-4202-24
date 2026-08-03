fce13e81c2c29666496b7d43e6b89b60dd510f8e
Inan <inanjaki184@gmail.com>
2026-08-03 23:47:58 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
@@ -1,4 +1,28 @@
 package kenakata.order;
 
 public class PriceBreakdown {
+    public double subtotal() {
+        return 0;
+    }
+
+    public double discount() {
+        return 0;
+    }
+
+    public double delivery() {
+        return 0;
+    }
+
+    public double vat() {
+        return 0;
+    }
+    public double insurance() {
+        return 0;
+    }
+    public double serviceFee() {
+        return 0;
+    }
+    public double grandTotal() {
+        return 0;
+    }
 }
