309ff3287e51525561bde6f01c31902c918d4438
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-03 23:22:19 +0600

    change : write PriceBreakdown class [1]

--- Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
@@ -1,4 +1,21 @@
 package kenakata.order;
 
 public class PriceBreakdown {
+    private final long subtotal;
+    private final long discount;
+    private final long delivery;
+    private final long vat;
+    private final long insurance;
+    private final long serviceFee;
+    private final long grandTotal;
+
+    public PriceBreakdown(long subtotal, long discount, long delivery, long vat, long insurance, long serviceFee, long grandTotal) {
+        this.subtotal = subtotal;
+        this.discount = discount;
+        this.delivery = delivery;
+        this.vat = vat;
+        this.insurance = insurance;
+        this.serviceFee = serviceFee;
+        this.grandTotal = grandTotal;
+    }
 }
