bacd6afcd2c98f6f37a219a28f1cd2f454c689ac
04-Labiba <sabihalabiba639@gmail.com>
2026-08-03 20:27:51 +0600

    init commit

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
@@ -0,0 +1,24 @@
+package kenakata.catalog;
+import kenakata.exceptions.OutOfStockException;
+import kenakata.order.Chargeable;
+import kenakata.settlement.Seller;
+public abstract class CatalogItem implements Chargeable{
+    private final String sku;
+    private final String title;
+    private final long unitPrice;
+    private int stock;
+    private final Seller seller;
+    public CatalogItem(String sku, String title, long unitPrice, int stock, Seller seller){
+        if(sku==null || sku.isBlank() || title==null || title.isBlank() || seller==null) {
+            throw new IllegalArgumentException("Invalid catalog item properties");
+        }
+        if(unitPrice<0 || stock<0) {
+            throw new IllegalArgumentException("Price and stock cannot be negative");
+        }
+        this.sku=sku;
+        this.title=title;
+        this.unitPrice=unitPrice;
+        this.stock=stock;
+        this.seller=seller;
+    }
+}
