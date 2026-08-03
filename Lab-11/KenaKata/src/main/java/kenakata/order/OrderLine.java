bbc3ba95d2263521383a96e52c4c8a82e5ad7ccc
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-04 04:47:10 +0600

    orderline

--- Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -5,4 +5,7 @@ import kenakata.catalog.Chargeable;
 public class OrderLine {
     private final Chargeable unit;
     private final int quantity;
+    private boolean insured;
+    private boolean returned;
+
 }
