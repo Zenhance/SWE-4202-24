5951cd83cc37413160a21b79b200855b00d4bca1
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-03 23:38:38 +0600

    change : write DeliveryCalculator class [1]

--- Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/DeliveryCalculator.java
@@ -1,4 +1,13 @@
 package kenakata.order;
 
+import kenakata.catalog.ColdChain;
+import kenakata.catalog.Weighable;
+
+import java.util.List;
+
 public class DeliveryCalculator {
+    public long calculate(List<OrderLine> lines, Zone zone) {
+        long totalWeightGrams = 0;
+        int freshLineCount = 0;
+    }
 }
