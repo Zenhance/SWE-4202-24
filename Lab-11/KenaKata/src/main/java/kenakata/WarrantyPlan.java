91ebbede642880fe9dfe7facd6bcb2513d5561ec
waseefmunir-max <waseef05@gmail.com>
2026-08-03 20:43:29 +0600

    WarrantyPlan.java: Add unitCharge, unitVat, and label methods

--- Lab-11/KenaKata/src/main/java/kenakata/WarrantyPlan.java
+++ Lab-11/KenaKata/src/main/java/kenakata/WarrantyPlan.java
@@ -1,6 +1,20 @@
 package kenakata;
 
+import java.nio.charset.Charset;
+
 public class WarrantyPlan {
     public WarrantyPlan(StockedGood lamp) {
+    }
+
+    public int unitCharge() {
+        return 120;
+    }
+
+    public int unitVat() {
+        return 18;
+    }
+
+    public Charset label() {
+        return null;
     }
 }
