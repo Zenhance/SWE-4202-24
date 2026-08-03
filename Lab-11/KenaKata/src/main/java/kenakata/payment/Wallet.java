006c0ea6e8ad1f010f99d6c9cb2e6b9f769daa28
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-08-04 01:10:14 +0600

    added methods

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -9,4 +9,15 @@ public final class Wallet {
         }
         this.balance = openingBalance;
     }
+    public long balance(){
+        return balance;
+    }
+    public void ensureCanDebit(long amount)
+        throws EmptyWalletException{
+        validatePositiveOrZero(amount);
+        if(amount>balance){
+            throw new EmptyWalletException("Wallet balance is insufficient");
+        }
+    }
+
 }
