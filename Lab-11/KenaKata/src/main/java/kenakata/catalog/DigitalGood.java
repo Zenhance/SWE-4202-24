b985a59219a173e0951f3c729f4192ad7bf39be7
md-musaddik <musaddikibna1@gmail.com>
2026-08-04 03:00:26 +0600

    Created Constructors and Method from the test file(2)

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -1,4 +1,19 @@
 package kenakata.catalog;
 
 public class DigitalGood {
+    public DigitalGood(String s, String s1, int i, int i1, Seller seller) {
+
+    }
+
+    public int unitCharge() {
+        return 1;
+    }
+
+    public int unitVat() {
+        return 1;
+    }
+
+    public int commissionOn(int i) {
+        return 1;
+    }
 }
--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -3,4 +3,16 @@ package kenakata.catalog;
 public class StockedGood {
     public StockedGood(String s, String tableLamp, int i, int i1, Seller s1, int i2) {
     }
+
+    public int unitCharge() {
+        return 1;
+    }
+
+    public int unitVat() {
+        return 1;
+    }
+
+    public int commissionOn(int i) {
+        return 1;
+    }
 }
