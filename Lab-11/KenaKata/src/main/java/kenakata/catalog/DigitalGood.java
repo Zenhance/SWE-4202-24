4c9cbf99daebd1b92bada811d5f57293121ccec2
Maomao-starry <aminarahmanshalu@gmail.com>
2026-08-04 03:40:34 +0600

    catalog

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -0,0 +1,24 @@
+package kenakata.catalog;
+
+import kenakata.settlement.Seller;
+
+public class DigitalGood extends CatalogueItem {
+    private static final double comission_rate=0.2;
+    private static final double vat=0.05;
+
+    public StockedGood(String sku, String title, long unitprice, int livestock, Seller seller ) {
+        super(sku, title, unitprice, livestock, seller);
+
+    }
+
+
+    @Override
+    public long unitVat() {
+        return (long)Math.ceil(unitCharge()*vat);
+    }
+
+    @Override
+    public long commissionOn(long amount) {
+        return (long)Math.ceil(amount*comission_rate);
+    }
+}
