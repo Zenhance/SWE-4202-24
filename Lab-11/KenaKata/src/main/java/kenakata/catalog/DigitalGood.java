59edf19c8e04af76af2f60af8cd0bf5a9b678bec
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-03 18:17:07 +0600

    fixed constructor

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -2,11 +2,12 @@ package kenakata.catalog;
 import kenakata.settlement.Seller;
 
 public abstract class DigitalGood extends CatalogItem {
-    public DigitalGood(String sku, String title, long unitPrice,int Stock, Seller seller){
-        super(sku,title,unitPrice,stock,seller);
+    public DigitalGood(String sku, String title, long unitPrice,int stock, Seller seller){
+        super(sku,title,unitPrice,stock,seller,title,(double)unitPrice);
     }
     @Override
     public long unitVat(){
+
         return (long) Math.ceil(unitCharge()*0.05);
     }
     @Override
