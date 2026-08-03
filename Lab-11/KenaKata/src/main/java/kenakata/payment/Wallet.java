5bf0b0e42a3a7d16e6eb2f61e42b9c595db820fe
Inan <inanjaki184@gmail.com>
2026-08-04 00:53:44 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -1,4 +1,15 @@
 package kenakata.payment;
 
 public class Wallet {
+    private double balance;
+
+    public double balance() {
+        return balance;
+    }
+    public void credit(double amount) {
+        balance = balance + amount;
+    }
+    public void debit(double amount) {
+        balance = balance - amount;
+    }
 }
