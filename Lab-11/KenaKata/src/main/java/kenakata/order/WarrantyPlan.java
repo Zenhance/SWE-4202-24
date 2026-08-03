33aca4914aa9f8f1092d5e8503b979108723b8a7
04-Labiba <sabihalabiba639@gmail.com>
2026-08-03 20:09:22 +0600

    init commit

--- Lab-11/KenaKata/src/main/java/kenakata/order/WarrantyPlan.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/WarrantyPlan.java
@@ -4,10 +4,24 @@ import kenakata.catalog.CatalogItem;
 public class WarrantyPlan implements Chargeable{
     private final CatalogItem coveredItem;
 
-    public WarrantyPlan(CatalogItem coveredItem) {
-        if (coveredItem == null) {
+    public WarrantyPlan(CatalogItem coveredItem){
+        if (coveredItem == null){
             throw new IllegalArgumentException("Covered item cannot be null");
         }
         this.coveredItem = coveredItem;
+    }
+    @Override
+    public long unitCharge(){
+        return (long) Math.ceil(coveredItem.unitCharge() * 0.10);
+    }
+
+    @Override
+    public long unitVat(){
+        return (long) Math.ceil(unitCharge() * 0.15);
+    }
+
+    @Override
+    public String label(){
+        return "Warranty for "+coveredItem.label();
     }
 }
