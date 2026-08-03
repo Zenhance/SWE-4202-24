c1f1baa90c067c858d59dcea696566a422775703
wasif-22 <twasif68@gmail.com>
2026-08-03 22:38:43 +0600

    completed stockedgood class

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -11,3 +11,28 @@ public final class StockedGood extends CatalogItem implements Weighable, Insurab
 
         this.weightGrams = weightGrams;
     }
+    @Override
+    public long unitVat() {
+        return 0;
+    }
+
+    @Override
+    public long commissionOn(long lineValue) {
+        return 0;
+    }
+
+    @Override
+    public long weightGrams() {
+        return weightGrams;
+    }
+
+    @Override
+    public long insurableValue() {
+        return unitPrice();
+    }
+
+    @Override
+    public int returnWindowDays() {
+        return 0;
+    }
+}
