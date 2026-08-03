7cd85dba3c70fd3f41808b2a4bb93a86ff70dfa6
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-04 02:48:38 +0600

    change : SettlementReport class

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
@@ -1,4 +1,24 @@
 package kenakata.settlement;
 
+import kenakata.catalog.Seller;
+import java.util.List;
+
 public class SettlementReport {
+    private final List<SellerPayout> payouts;
+    private final long platformRevenue;
+
+    public SettlementReport(List<SellerPayout> payouts, long platformRevenue) {
+        this.payouts = payouts;
+        this.platformRevenue = platformRevenue;
+    }
+
+    public List<SellerPayout> payouts() { return payouts; }
+    public long platformRevenue() { return platformRevenue; }
+
+    public SellerPayout forSeller(Seller seller) {
+        return payouts.stream()
+                .filter(p -> p.seller().equals(seller))
+                .findFirst()
+                .orElse(new SellerPayout(seller, 0, 0, 0, 0));
+    }
 }
