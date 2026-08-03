f5b8df3dca1dbc6f078032f3f1fba66bcb87f74e
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-03 22:37:12 +0600

    change : write the DigitalGood class [1]

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -1,4 +1,8 @@
 package kenakata.catalog;
 
-public class DigitalGood {
+public class DigitalGood extends CatalogItem {
+
+    public DigitalGood(String sku, String title, long unitPrice, int stock, Seller seller) {
+        super(sku, title, unitPrice, stock, seller);
+    }
 }
