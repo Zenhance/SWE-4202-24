03a9279fa8db0b469a7978a45bd06ce8fc333876
jasin <anishaajasin.13.04.2005@gmail.com>
2026-08-04 02:25:23 +0600

    commit

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -1,7 +1,20 @@
 package kenakata.catalog;
 
 public class Seller {
-    public Seller(Item item) {
-
+    private final String name;
+    public Seller(String name) {
+       if(name==null){
+           throw new IllegalArgumentException("name is null");
+       }
+       this.name = name;
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public String toString(){
+        return name;
+    }
+
 }
