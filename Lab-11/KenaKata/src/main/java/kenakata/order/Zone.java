20bd25230c26dedc2239e5a09f76a30fa4083ea0
58-jahin <jahinhossain@iut-dhaka.edu>
2026-08-03 22:21:59 +0600

    Zone is an enum that tells the system where the order will be delivered.

--- Lab-11/KenaKata/src/main/java/kenakata/order/Zone.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Zone.java
@@ -1,7 +1,5 @@
 package kenakata.order;
 
-public class Zone {
-    public static final Object DHAKA = null
-            ;
-    public static final Object OUTSIDE = null;
+public enum Zone {
+    DHAKA, OUTSIDE
 }
