8eba56adad9839d1c261eae0f217ff2f3172f573
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-08-03 22:53:27 +0600

    updated constructor

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -4,8 +4,13 @@ public class Seller {
     private String name;
 
     public Seller(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Seller name can not be blank.");
+        }
+
         this.name = name;
     }
 
-    public String getName() {return name;}
+    public String name() {
+        return name;}
 }
