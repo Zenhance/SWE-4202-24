8f7389febe3d84cd44e7c18df53bef142e44720e
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-08-04 02:00:16 +0600

    fixed typing and spacing issues...

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
@@ -6,21 +6,16 @@ public class WarrantyPlan implements Chargeable {
         if(item == null) {
             throw new IllegalArgumentException("Invalid item");
         }
-
         this.item = item;
     }
     @Override
     public long unitCharge() {
         return (item.unitCharge() * 10) / 100;
     }
-
-
     @Override
     public long unitVat() {
         return (unitCharge() * 15) / 100;
     }
-
-
     @Override
     public String label() {
         return "Warranty for " + item.name();
