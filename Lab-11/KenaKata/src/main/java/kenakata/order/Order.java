b7d79c68e8b5cb6b2f804527b41308d4612634ff
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-04 05:58:44 +0600

    order class

--- Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
@@ -3,5 +3,9 @@ import java.util.ArrayList;
 
 public class Order {
     ArrayList<Line> Lines = new ArrayList<>();
+    public Coupon coupon;
+    public Order(Zone zone,DeliveryCalculator deliveryCalculator) {}
+
+
 
 }
