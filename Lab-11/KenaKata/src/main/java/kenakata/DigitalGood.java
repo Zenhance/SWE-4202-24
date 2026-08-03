8158490d0d177f5068c59fbcbfebcced038b78bc
waseefmunir-max <waseef05@gmail.com>
2026-08-03 20:31:12 +0600

    DigitalGood.java: - Add method unitCharge(), unitVat() and commissionOn()

--- Lab-11/KenaKata/src/main/java/kenakata/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/DigitalGood.java
@@ -3,4 +3,16 @@ package kenakata;
 public class DigitalGood {
     public DigitalGood(String s, String s1, int i, int i1, Seller seller) {
     }
+
+    public int unitCharge() {
+        return 300;
+    }
+
+    public int unitVat() {
+        return 15;
+    }
+
+    public int commissionOn(int i) {
+        return 60;
+    }
 }
