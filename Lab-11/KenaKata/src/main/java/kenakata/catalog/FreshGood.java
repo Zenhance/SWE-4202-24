6bf527b581ca72a384cdf9767cb4f28e8de6cfd6
Fayaz16 <shahtanzim900@gmail.com>
2026-08-04 02:18:32 +0600

    zone created

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
@@ -1,4 +1,7 @@
 package kenakata.catalog;
 
-public class FreshGood {
+public class FreshGood extends CatalogItem implements Weighable,ColdChain,Insurable,Returnable{
+    private final int weightGrams;
+
+    public FreashGood(String sku,String title,long unitPrice,int )
 }
--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -1,4 +1,38 @@
 package kenakata.catalog;
 
 public class Seller {
-}
+    package kenakata.settlement;
+
+
+        private final Seller seller;
+        private long grossSales;
+        private long commission;
+        private long refunds;
+
+        public SellerPayout(seller seller){
+            this.seller = seller;
+        }
+        public void addSales(long amount){
+            this.grossSales += amount;
+        }
+
+        public void addCommission(long amount){
+            this.commission += amount;
+        }
+
+        public void addRefund(long amount){
+            this.refunds += amount;
+        }
+        public long grossSales(){
+            return grossSales;
+        }
+        public long commission(){
+            return commission;
+        }
+        public long refunds(){
+            return refunds;
+        }
+        public long payout(){
+            return grossSales - commission - refunds;
+        }
+    }
