245f2c9a14dd7a906ad060da32757cf45e356bad
Maomao-starry <aminarahmanshalu@gmail.com>
2026-08-04 04:35:50 +0600

    catalog

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -0,0 +1,12 @@
+package kenakata.catalog;
+
+public class Seller {
+    private final String name;
+
+    public Seller(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
