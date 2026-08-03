67e258803cd17e07b8d46bb3516e587598d2e2ce
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-04 05:52:16 +0600

    SettlementReport

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
@@ -1,7 +1,16 @@
 package kenakata.settlement;
 
+
 public class SettlementReport {
     public SellerPayout[] payouts(){
+        return null;
+    }
+
+    public int platformRevenue(){
+        return 0;
+    }
+
+    public SellerPayout forSeller(Seller x){
         return null;
     }
 
