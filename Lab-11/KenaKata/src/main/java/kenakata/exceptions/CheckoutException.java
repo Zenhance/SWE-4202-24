867439b056e4408ee294710872f12bf1d1fa4b35
wasi <waseesomrat8@gmail.com>
2026-08-04 00:50:29 +0600

    CheckoutException class is created

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/CheckoutException.java
@@ -0,0 +1,12 @@
+package kenakata.exceptions;
+
+public class CheckoutException extends Exception{
+
+    public CheckoutException(){
+        super();
+    }
+
+    public CheckoutException(String message){
+        super(message);
+    }
+}
