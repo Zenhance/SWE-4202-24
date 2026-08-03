e24ce2646cdf286d9de376351c20b46a7c461070
Mohammad Safwan Bhuiyan <safwanbhuiyan@iut-dhaka.edu>
2026-08-03 23:08:06 +0600

    adding methods

--- Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -1,7 +1,25 @@
 package kenakata.order;
 
+import kenakata.catalog.*;
+
 public class OrderLine {
+
+    private Chargeable item;
+    private int quantity;
+    private boolean insured;
+    private boolean returned;
+
+    public OrderLine(Chargeable item , int quantity) {
+        this.quantity=quantity;
+        this.item=item;
+        returned=false;
+    }
+
     public boolean returned(){
-        return true;
+        returned=true;
+        return returned;
+    }
+    public boolean isReturned(){
+        return returned;
     }
 }
