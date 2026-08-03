92dc15ec14983326b9ff78cbedaea5f416d0e5a3
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-03 18:06:52 +0600

    fixed constructor

--- Lab-11/KenaKata/src/main/java/kenakata/order/WarrantyPlan.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/WarrantyPlan.java
@@ -1,16 +1,24 @@
-import kenakata.catalog.CatalogItem;
 package kenakata.order;
+import kenakata.catalog.CatalogItem;
 
-public abstract class WarrantyPlan implements CatalogItem{
+public abstract class WarrantyPlan extends CatalogItem{
     private String label;
     private double unitCharge;
     private int durationMonths;
 
     public WarrantyPlan(String label,double unitCharge,int durationMonths){
+        if(unitCharge<0){
+            throw new IllegalArgumentException("Unit charge cant be negative");
+
+        }
+        if(durationMonths <=0){
+            throw new IllegalArgumentException("Duration months must be positive");
+        }
         this.label=label;
         this.unitCharge=unitCharge;
         this.durationMonths=durationMonths;
     }
+
     @Override
     public String label(){
         return this.label;
