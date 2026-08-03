2c0b1b2e8ff05801895cd2f5c5a413dcfe63727e
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-08-03 23:37:37 +0600

    added constructor and methods

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
@@ -1,9 +1,24 @@
 package kenakata.catalog;
 
-import kenakata.exceptions.InsufficientStockException;
+import kenakata.catalog.capabilities.Coldchainable;
+import kenakata.catalog.capabilities.Insurable;
+import kenakata.catalog.capabilities.Returnable;
+import kenakata.catalog.capabilities.Weighable;
+import kenakata.exceptions.OutOfStockException;
 
-public class FreshGood extends Product{
-    public FreshGood(String sku, String title, double unitPrice, int stock, Seller seller) throws InsufficientStockException {
+public class FreshGood extends Product implements Weighable, Coldchainable, Insurable, Returnable {
+
+    private final int weightInGrams;
+    public FreshGood(String sku, String title, long unitPrice, int stock, Seller seller, int weightInGrams) throws OutOfStockException {
         super(sku, title, unitPrice, stock, seller);
+        this.weightInGrams = weightInGrams;
+    }
+
+    @Override
+    public int weightInGrams() { return weightInGrams;}
+
+    @Override
+    public long unitVat() {
+        return 0;
     }
 }
