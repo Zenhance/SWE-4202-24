7412d9f3a6ed8efd3dd456508ed4091699e8e7f6
Inan <inanjaki184@gmail.com>
2026-08-03 21:16:07 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -1,4 +1,13 @@
 package kenakata.order;
 
+import kenakata.catalog.Chargeable;
+
 public class OrderLine {
+
+    public Chargeable item(){
+        return null;
+    }
+    public int quantity(){
+        return 0;
+    }
 }
