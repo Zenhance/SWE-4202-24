c5ff7902bc7c163d8fb1dc52478f18db55ee256d
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-03 21:37:04 +0600

    change : write the Item class

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Item.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Item.java
@@ -1,18 +1,4 @@
 package kenakata.catalog;
 
-import kenakata.enums.ItemType;
-import kenakata.exceptions.InsufficientStockException;
-
-public interface Item {
-    String getSku();
-    String getTitle();
-    double getUnitPrice();
-    int getStockRemaining();
-    Seller getSeller();
-    ItemType getType();
-
-    void restoreStock(int quantity);
-    long getCommission(long lineTotal);
-    void reserve(int quantity) throws InsufficientStockException;
-    long getPerUnitVat();
+public interface Item extends Chargeable {
 }
