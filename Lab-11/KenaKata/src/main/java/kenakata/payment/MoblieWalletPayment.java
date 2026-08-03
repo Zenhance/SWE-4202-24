0722f967a6fba99d621d0da01ae271f227b56bcd
wasi <waseesomrat8@gmail.com>
2026-08-03 17:54:46 +0600

    MobileWalletPayment class created

---
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/MoblieWalletPayment.java
@@ -0,0 +1,19 @@
+public class MobileWalletPayment implements PaymentMethod {
+
+    private final Wallet wallet;
+
+    public MobileWalletPayment(Wallet wallet){
+        if(wallet == null){
+            throw new IllegalArgumentException();
+        }
+        this.wallet = wallet;
+    }
+    @Override
+    public void authorise(long amount)
+            throws EmptyWalletException{
+        if(wallet.balance() < amount){
+            throw new EmptyWalletException();
+        }
+        wallet.debit(amount);
+    }
+}
