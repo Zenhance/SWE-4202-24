a8fd66b0716a571890a95043d7829a4843ac25b5
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2026-08-04 03:05:25 +0600

    payment method fixing

--- Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
@@ -4,6 +4,8 @@ public class MobileWalletPayment implements PaymentMethod {
     private final Wallet wallet;
 
     public MobileWalletPayment(Wallet wallet) {
+
+        if(wallet==null) {throw new IllegalArgumentException("Wallet cannot be null");}
         this.wallet = wallet;
     }
     @Override
