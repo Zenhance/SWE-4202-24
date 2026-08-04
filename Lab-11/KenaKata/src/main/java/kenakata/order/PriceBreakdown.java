5874ade632734a3eed2d9bca278447a55a9bcc86
anonymous <farnaz28.rahman@gmail.com>
2026-08-04 21:39:21 +0600

    fixed error

--- Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
@@ -1,11 +1,11 @@
 package kenakata.order;
 
-public record PriceBreakdown() {
-    long subtotal;
-    long discount;
-    long vat;
-    long delivery;
-    long insurance;
-    long serviceFee;
-    long grandTotal;
-}
+public record PriceBreakdown(
+        long subtotal,
+        long discount,
+        long vat,
+        long delivery,
+        long insurance,
+        long serviceFee,
+        long grandTotal
+) {}
