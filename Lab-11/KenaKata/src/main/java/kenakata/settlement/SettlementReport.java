78a782c41f55502aa5894eb321f2cea231a0a716
Inan <inanjaki184@gmail.com>
2026-08-04 10:58:44 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SettlementReport.java
@@ -1,4 +1,17 @@
 package kenakata.settlement;
 
+import kenakata.catalog.Seller;
+
+import java.util.Map;
+
 public class SettlementReport {
+    private Map<Seller, SellerPayout> payouts;
+    private double platformRevenue;
+
+    public SellerPayout forSeller(Seller seller) {
+        return null;
+    }
+    public long platformRevenue(){
+        return platformRevenue;
+    }
 }
