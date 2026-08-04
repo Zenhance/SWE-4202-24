e48316cff15811c6882084b46c8f57e4a3b404f7
ummulkhaer-max <ummulkhaer@iut-dhaka.edu>
2026-08-04 09:51:05 +0600

    initial commit

--- Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
@@ -1,4 +1,17 @@
 package kenakata.payment;
 
-public class MobileWalletPayment {
+import kenakata.exceptions.*;
+
+public class MobileWalletPayment implements PaymentMethod {
+    private final Wallet wallet;
+
+    public MobileWalletPayment(Wallet wallet) {
+        this.wallet = wallet;
+    }
+
+    @Override
+    public void authorise(long amount) throws CheckoutException, EmptyWalletException{
+        wallet.deduct(amount);
+    }
 }
+
--- Lab-11/KenaKata/src/main/java/kenakata/payment/Payment.java
+++
@@ -1,5 +0,0 @@
-package kenakata.payment;
-
-public interface Payment {
-    boolean authorize(int amount);
-}
