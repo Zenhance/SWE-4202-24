1d2baa3a2b3cca90a83b2106fc64dbdb5238e8b3
tryhardFuad <mubtasimsmail@gmail.com>
2026-08-04 11:35:45 +0600

    fix: fixed constructor

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/FreshGood.java
@@ -2,6 +2,7 @@ package kenakata.catalog;
 
 public class FreshGood extends CatalogItem implements Chargeable {
     public FreshGood(String s, String hilsa, int i, int i1, Seller seller, int i2) {
+        super(s, hilsa, i, i1, seller);
     }
 
     public int unitVat() {
