09283ff8e1cf50d47976d0a38176d1692e560eaa
Inan <inanjaki184@gmail.com>
2026-08-04 10:51:50 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
@@ -1,4 +1,16 @@
 package kenakata.settlement;
 
 public class SellerPayout {
+    public double grossSales(){
+        return 0;
+    }
+    public double commision(){
+        return 0;
+    }
+    public double refunds(){
+        return 0;
+    }
+    public double payout(){
+        return 0;
+    }
 }
