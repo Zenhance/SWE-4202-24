66f76550371ef84797ab4853ae71929d3ddf049c
tryhardFuad <mubtasimsmail@gmail.com>
2026-08-04 11:35:00 +0600

    fix: fixed constructor

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/DigitalGood.java
@@ -2,6 +2,7 @@ package kenakata.catalog;
 
 public class DigitalGood extends CatalogItem implements Chargeable {
     public DigitalGood(String s, String s1, int i, int i1, Seller seller) {
+        super(s, s1, i, i1, seller);
     }
 
     public int unitCharge() {
