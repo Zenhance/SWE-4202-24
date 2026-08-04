7c2d7cefa46c5f2b55ae124bd22bc5dc506d5f18
Inan <inanjaki184@gmail.com>
2026-08-04 10:49:07 +0600

    1

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/MarketPlace.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/MarketPlace.java
@@ -1,4 +1,15 @@
 package kenakata.settlement;
 
+import kenakata.catalog.Seller;
+import kenakata.order.Order;
+
 public class MarketPlace {
+
+    public void register(Seller seller) {}
+
+    public void record(Order order) {}
+
+    public SettlementReport settle(){
+        return null;
+    }
 }
