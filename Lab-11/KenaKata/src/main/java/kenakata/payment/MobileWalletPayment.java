cebadf98ec1c288a243666138efe46a875f3053b
anonymous <farnaz28.rahman@gmail.com>
2026-08-04 17:33:30 +0600

    MobileWallet payment class

--- Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
@@ -1,4 +1,19 @@
 package kenakata.payment;
+import kenakata.exceptions.CheckoutException;
+import kenakata.exceptions.EmptyWalletException;
 
-public class MobileWalletPayment {
+public class MobileWalletPayment implements PaymentMethod{
+    private final Wallet wallet;
+    public MobileWalletPayment(Wallet wallet){
+        this.wallet=wallet;
+    }
+
+    @Override
+    public void authorise(long amount) throws EmptyWalletException {
+        if(wallet.balance()<0){
+            throw new EmptyWalletException("Wallet balance insufficient");
+        }
+        wallet.deduct(amount);
+
+    }
 }
