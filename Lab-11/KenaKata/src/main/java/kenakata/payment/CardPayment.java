3c21c669fca0c7de7489d239915a1a55115f278e
Mohammad Safwan Bhuiyan <safwanbhuiyan@iut-dhaka.edu>
2026-08-04 09:21:27 +0600

    method body changing

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
@@ -2,23 +2,27 @@ package kenakata.payment;
 
 import kenakata.exceptions.CardLimitExceededException;
 
-public class CardPayment implements PaymentMethod{
-    public long limit;
-    public CardPayment(long limit) {
-        if(limit<0)
-            throw new IllegalArgumentException();
-        this.limit=limit;
-    }
 
-    @Override
-    public long remainingLimit() {
-        return limit;
+public class CardPayment implements PaymentMethod {
+    public long limit, amount;
+
+    public CardPayment(long limit) {
+        if (limit < 0)
+            throw new IllegalArgumentException();
+        this.limit = limit;
     }
 
     @Override
     public void authorise(long amount) throws CardLimitExceededException {
 
         if (amount>limit)
-            throw new CardLimitExceededException("no limit");
+            throw new CardLimitExceededException("limit exceeded");
+        this.amount=amount;
+    }
+
+
+    @Override
+    public long remainingLimit() {
+        return limit-amount;
     }
 }
