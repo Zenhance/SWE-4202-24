a0b2c42a4cf4a9244a8dadf83782198ff3e08bb5
Maomao-starry <aminarahmanshalu@gmail.com>
2026-08-04 06:19:58 +0600

    task

--- Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -1,5 +1,7 @@
 package kenakata.order;
 
+import kenakata.exceptions.CouponRejectedException;
+
 public class Coupon {
 private final String code;
 private final int discount_per;
@@ -11,7 +13,7 @@ private final int expirydate;
     public Coupon(String code, int discountPer, long maxDiscount, long minimumspend, int expirydate) {
         if(code==null || code.isBlank())
             throw new IllegalArgumentException("Invalid");
-        if(discountPer<0 || maxDiscount<0 || minimumspend<0 || expirydate<0)
+        if(discountPer<0 ||discountPer>100 || maxDiscount<0 || minimumspend<0 || expirydate<0)
             throw new IllegalArgumentException("Invalid");
         this.code = code;
         this.discount_per = discountPer;
@@ -38,5 +40,10 @@ private final int expirydate;
     public long getMinimumspend() {
         return this.minimumspend;
     }
-
+  public long calculatediscount(long amount){
+      long discount = (long) Math.ceil(amount * (discount_per / 100.0));
+      if(discount >maxDiscount)
+          return maxDiscount;
+      else return discount;
+  }
 }
