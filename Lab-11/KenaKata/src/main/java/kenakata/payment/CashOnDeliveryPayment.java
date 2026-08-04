c0bea479ec9e3ae576d267a9fd7e941c9caee5b8
anonymous <farnaz28.rahman@gmail.com>
2026-08-04 17:44:56 +0600

    Cash on delivery Payment class

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
@@ -1,6 +1,14 @@
 package kenakata.payment;
+import kenakata.exceptions.CheckoutException;
+import kenakata.exceptions.CodCeilingExceededException
+public class CashOnDeliveryPayment implements PaymentMethod{
+    private static final long CEILING=15000;
 
-public class CashOnDeliveryPayment {
-    public void authorise(int i) {
+    @Override
+    public void authorise(long amount) throws  CodCeilingExceededException{
+        if(amount>CEILING){
+            throw new CodCeilingExceededException("COD ceiling exceeded");
+        }
+
     }
 }
