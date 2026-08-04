66c4a8f8f44cfd0d9f6b34c8308039ced6bd354a
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-08-04 13:08:27 +0600

    fixed superclass relation

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/PaymentDeclinedException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/PaymentDeclinedException.java
@@ -1,6 +1,6 @@
 package kenakata.exceptions;
 
-public class PaymentDeclinedException extends EmptyWalletException {
+public class PaymentDeclinedException extends CheckoutException {
     public PaymentDeclinedException(String message){
         super(message);
     }
