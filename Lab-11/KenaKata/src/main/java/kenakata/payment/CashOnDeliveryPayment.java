a2612034aa0300608783040368ec858926501688
wasifjafar <wasifjafar71@gmail.com>
2026-08-05 03:32:55 +0600

    make CashOnDeliveryPayment class's authorise method to throw CodCeilingExceededException

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
@@ -1,9 +1,13 @@
 package kenakata.payment;
 
+import kenakata.exceptions.CodCeilingExceededException;
+
 public class CashOnDeliveryPayment implements PaymentMethod{
+    private final int ceiling = 15_000;
 
     @Override
-    public void authorise(int amount) {
-
+    public void authorise(int amount) throws CodCeilingExceededException {
+        if (amount > ceiling)
+            throw new CodCeilingExceededException("Cash on delivery ceiling exceeded");
     }
 }
