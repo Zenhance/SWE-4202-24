93537eb48dc0f49f457fada9910fad4db064a21e
tryhardFuad <mubtasimsmail@gmail.com>
2026-08-04 11:34:20 +0600

    feat: added fields, constructor and getters for CatalogItem

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
@@ -1,8 +1,49 @@
 package kenakata.catalog;
 
-public class CatalogItem implements Chargeable {
-    private String itemName;
-    private int quantity;
-    private int price;
+public abstract class CatalogItem implements Chargeable {
+
+    private String sku;
+    private String title;
+    private long unitPrice;
+    private int stock;
+    private Seller seller;
+
+    public CatalogItem(String sku, String title, long unitPrice,int stock, Seller seller) {
+        if (sku == null || sku.isEmpty()) {
+            throw new IllegalArgumentException("sku is required");
+        }
+        if (title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("title is required");
+        }
+        if (unitPrice < 0) {
+            throw new IllegalArgumentException("unitPrice cant't be negative");
+        }
+        if (stock < 0) {
+            throw new IllegalArgumentException("stock cant't be negative");
+        }
+        this.sku = sku;
+        this.title = title;
+        this.unitPrice = unitPrice;
+        this.seller = seller;
+        this.stock = stock;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public long getUnitPrice() {
+        return unitPrice;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+
 
 }
