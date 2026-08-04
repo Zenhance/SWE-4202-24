8706639c78ec0816ecea7e0d67f9b70e077a3fe2
waseefmunir-max <waseef05@gmail.com>
2026-08-04 09:02:02 +0600

    Add OrderLine class to handle Chargeable units and track quantity

--- Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -1,4 +1,19 @@
 package kenakata.order;
 
+import kenakata.catalog.Chargeable;
+
 public class OrderLine {
+    private final Chargeable unit;
+    private final int quantity;
+    private boolean returned;
+
+    public OrderLine(Chargeable unit, int quantity) {
+        this.unit = unit;
+        this.quantity = quantity;
+        this.returned = false;
+    }
+
+    public boolean isReturned() {
+        return false;
+    }
 }
