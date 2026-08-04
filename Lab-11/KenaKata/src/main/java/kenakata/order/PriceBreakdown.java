91132166c04c0a672da4442b8b25cec807f3064f
anonymous <farnaz28.rahman@gmail.com>
2026-08-04 18:27:31 +0600

    record

--- Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/PriceBreakdown.java
@@ -1,24 +1,11 @@
 package kenakata.order;
 
-public class PriceBreakdown {
-    public int subtotal() {
-    }
-
-    public int discount() {
-    }
-
-    public int vat() {
-    }
-
-    public int delivery() {
-    }
-
-    public int insurance() {
-    }
-
-    public int serviceFee() {
-    }
-
-    public int grandTotal() {
-    }
+public record PriceBreakdown() {
+    long subtotal;
+    long discount;
+    long vat;
+    long delivery;
+    long insurance;
+    long serviceFee;
+    long grandTotal;
 }
