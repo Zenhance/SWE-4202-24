052e8b5382b741b229139c0868df62f9390bc2f2
jasin <anishaajasin.13.04.2005@gmail.com>
2026-08-10 01:05:51 +0600

    exception

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/ReturnNotAllowedException.java
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/ReturnNotAllowedException.java
@@ -1,4 +1,8 @@
 package kenakata.exceptions;
 
-public class ReturnNotAllowedException {
+public class ReturnNotAllowedException extends CheckoutException {
+
+    public ReturnNotAllowedException() {
+        super("Return not allowed");
+    }
 }
