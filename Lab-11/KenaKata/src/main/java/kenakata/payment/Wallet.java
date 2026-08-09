38bbb98d71c022c18a32d0b752739a16d008dffb
sabbir240042147 <eftesabbir2725@gmail.com>
2026-08-09 11:03:32 +0600

    wallet

---
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -0,0 +1,22 @@
+package kenakata.payment;
+
+public class Wallet {
+    private int balance;
+    public Wallet(int balance) {
+        if(balance<0) throw new IllegalArgumentException("balance cannot be negative");
+        this.balance=balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void credit(int amount){
+        if(amount<0) throw new IllegalArgumentException("amount cannot be negative");
+        balance+=amount;
+    }
+    public void debit(int amount){
+        if(amount>balance) throw new IllegalArgumentException("not enough balance");
+        balance-=amount;
+    }
+}
