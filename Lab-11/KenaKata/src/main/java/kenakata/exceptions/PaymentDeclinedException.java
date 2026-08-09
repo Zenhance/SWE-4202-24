55565a64b19641ff4b125190605d2acf7343cd42
jasin <anishaajasin.13.04.2005@gmail.com>
2026-08-09 22:44:44 +0600

    wallet exceptions

---
+++ Lab-11/KenaKata/src/main/java/kenakata/exceptions/PaymentDeclinedException.java
@@ -0,0 +1,12 @@
+package kenakata.exceptions;
+
+public class PaymentDeclinedException extends CheckoutException {
+
+    public PaymentDeclinedException() {
+        super();
+    }
+
+    public PaymentDeclinedException(String message) {
+        super(message);
+    }
+}
