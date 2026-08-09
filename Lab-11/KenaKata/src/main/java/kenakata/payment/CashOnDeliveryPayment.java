5cb5f85eb2dd6488cf891b38b9ece6782bb154aa
jasin <anishaajasin.13.04.2005@gmail.com>
2026-08-09 23:24:13 +0600

    cash on delivery payment

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
@@ -7,4 +7,16 @@ public class CashOnDeliveryPayment implements PaymentMethod {
 
     private static final long COD_CEILING = 15000;
 
+    @Override
+    public void authorise(long amount)
+            throws PaymentDeclinedException {
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (amount > COD_CEILING) {
+            throw new CodCeilingExceededException();
+        }
+    }
 }
