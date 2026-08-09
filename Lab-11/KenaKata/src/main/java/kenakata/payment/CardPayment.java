bd011348ffe7f249e5e1e3710eaaf577c95e6f47
jasin <anishaajasin.13.04.2005@gmail.com>
2026-08-09 23:14:36 +0600

    card payment

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
@@ -5,12 +5,17 @@ import kenakata.exceptions.PaymentDeclinedException;
 
 
 public class CardPayment implements PaymentMethod {
-    private long amount;
-    public CardPayment(long n) {
-        if (n < 0) {
+    private long remainingLimit;
+    public CardPayment(long limit) {
+        if (limit < 0) {
             throw new IllegalArgumentException();
         }
 
-        this.amount = n;
+        this.remainingLimit = limit;
+    }
+
+
+    public long remainingLimit() {
+        return remainingLimit;
     }
 }
