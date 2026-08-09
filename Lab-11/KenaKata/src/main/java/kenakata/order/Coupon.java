97db6cade467138785f8d34e808cf467b27e4e09
jasin <anishaajasin.13.04.2005@gmail.com>
2026-08-10 01:23:12 +0600

    coupon

--- Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -7,12 +7,30 @@ public class Coupon {
     private final int percent;
 
     public Coupon(String code, int percent) {
+        if (code == null || code.isBlank() || percent < 0 || percent > 100) {
+            throw new IllegalArgumentException();
+        }
+
         this.code = code;
         this.percent = percent;
     }
 
-    public long discount(long amount, int day){
-        return 0;
+    public long discount(long amount, int day)throws CouponRejectedException {
+        if (amount < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (percent == 0) {
+            throw new CouponRejectedException();
+        }
+
+        long discount = (amount * percent + 99) / 100;
+
+        if (discount > 200) {
+            discount = 200;
+        }
+
+        return discount;
     }
 
     public String code() {
