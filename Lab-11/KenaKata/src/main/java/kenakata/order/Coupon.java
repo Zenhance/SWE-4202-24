a9ee0b1e5e2ad8e9f68effa730a235c58a236eea
jasin <anishaajasin.13.04.2005@gmail.com>
2026-08-10 02:00:43 +0600

    forgot to commit and push

--- Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -5,14 +5,20 @@ import kenakata.exceptions.CouponRejectedException;
 public class Coupon {
     private final String code;
     private final int percent;
+    private final long maxDiscount;
+    private final long minimumSubtotal;
+    private final int expiryDay;
 
-    public Coupon(String code, int percent) {
-        if (code == null || code.isBlank() || percent < 0 || percent > 100) {
+    public Coupon(String code, int percent, long maxDiscount, long minimumSubtotal, int expiryDay) {
+        if (code == null || code.isBlank() || percent < 0 || percent > 100 || maxDiscount<0 || minimumSubtotal<0 || expiryDay<0) {
             throw new IllegalArgumentException();
         }
 
         this.code = code;
         this.percent = percent;
+        this.maxDiscount = maxDiscount;
+        this.minimumSubtotal = minimumSubtotal;
+        this.expiryDay = expiryDay;
     }
 
     public long discount(long amount, int day)throws CouponRejectedException {
@@ -24,10 +30,14 @@ public class Coupon {
             throw new CouponRejectedException();
         }
 
+        if (amount < minimumSubtotal || day > expiryDay) {
+            throw new CouponRejectedException();
+        }
+
         long discount = (amount * percent + 99) / 100;
 
-        if (discount > 200) {
-            discount = 200;
+        if (discount > maxDiscount) {
+            discount = maxDiscount;
         }
 
         return discount;
