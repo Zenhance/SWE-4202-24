ae435ceaa5324d428c6486054672cef9faa3c11e
jasin <anishaajasin.13.04.2005@gmail.com>
2026-08-10 01:10:10 +0600

    coupon(updating)

--- Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -3,10 +3,24 @@ package kenakata.order;
 import kenakata.exceptions.CouponRejectedException;
 
 public class Coupon {
-
+    private final String code;
+    private final int percent;
 
     public Coupon(String code, int percent) {
+        this.code = code;
+        this.percent = percent;
+    }
 
+    public long discount(long amount, int day){
+        return 0;
+    }
+
+    public String code() {
+        return code;
+    }
+
+    public int percent() {
+        return percent;
     }
 
 
