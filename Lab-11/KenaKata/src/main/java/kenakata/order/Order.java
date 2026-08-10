3f3e9eeccea8cee6237bbd56e51cba0a8054ce8c
iam-tahmid <tahmidim99@gmail.com>
2026-08-11 02:01:57 +0600

    Method added in order class

--- Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Order.java
@@ -2,17 +2,24 @@ package kenakata.order;
 
 import kenakata.catalog.GiftWrap;
 import kenakata.catalog.StockedGood;
+import kenakata.exceptions.EmptyWalletException;
+import kenakata.exceptions.NotInsurableException;
+import kenakata.exceptions.CouponRejectedException;
+import kenakata.payment.MobileWalletPayment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class Order {
-    public Order(Zone zone, DeliveryCalculator delivery) {
+    private final Zone zone;
+    private final DeliveryCalculator deliveryCalculator;
+
+    public Order(Zone zone, DeliveryCalculator deliveryCalculator){
+        this.zone = zone;
+        this.deliveryCalculator= deliveryCalculator;
     }
 
-    public void addProduct(StockedGood lamp, int i) {
-    }
 
-    public void addAddOn(GiftWrap giftWrap) {
-    }
-
-    public void applyCoupon(Coupon eid10) {
-    }
 }
