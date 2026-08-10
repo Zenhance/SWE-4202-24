16e9c6b6342140b728647ccfadc5f755f4da4fba
23-Fahad <ifahad2k@gmail.com>
2026-08-10 20:53:13 +0600

    initial orederline

--- Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/OrderLine.java
@@ -1,4 +1,39 @@
 package kenakata.order;
 
+import kenakata.catalog.Chargeable;
+
 public class OrderLine {
+    private final Chargeable item;
+    private final int unit;
+    private  boolean insured;
+    private  boolean returned;
+
+    public OrderLine(Chargeable item, int unit) {
+        this.item = item;
+        this.unit = unit;
+        this.insured = false;
+        this.returned = false;
+    }
+
+    public Chargeable getItem() {
+        return item;
+    }
+
+    public int getUnit() {
+        return unit;
+    }
+
+    public boolean isInsured() {
+        return insured;
+    }
+
+    public boolean isReturned() {
+        return returned;
+    }
+    public boolean makeIsured(){
+        return true;
+    }
+    private boolean makeReturned(){
+        return true;
+    }
 }
