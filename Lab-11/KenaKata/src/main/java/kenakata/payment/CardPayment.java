d97ee53fe588e7213fe8d884a24e7b356c9ba5fa
wasi <waseesomrat8@gmail.com>
2026-08-10 23:00:52 +0600

    CardPayment exception error fixed

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CardPayment.java
@@ -1,7 +1,5 @@
 package kenakata.payment;
-
 import kenakata.exceptions.CardLimitExceededException;
-
 public class CardPayment implements PaymentMethod{
     private long remainingLimit;
     public CardPayment(long remainingLimit){
@@ -10,22 +8,17 @@ public class CardPayment implements PaymentMethod{
         }
         this.remainingLimit = remainingLimit;
     }
-
     public long remainingLimit(){
         return remainingLimit;
     }
-
     @Override
     public void authorise(long amount) throws CardLimitExceededException{
-
         if (amount <= 0){
             throw new IllegalArgumentException("Invalid amount.");
         }
-
         if (amount > remainingLimit){
-            throw new CardLimitExceededException();
+            throw new CardLimitExceededException("Card limit exceeded");
         }
-
         remainingLimit -= amount;
     }
 }
