0e72522cbfb2e8a60d4c5f6f872916a99832bd49
tahsin1r <naylakagi@gmail.com>
2026-08-11 04:00:32 +0600

    Mistakenly Changed the value of charge in ExpressHandling which has now been fixed

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/ExpressHandling.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/ExpressHandling.java
@@ -3,7 +3,7 @@ package kenakata.catalog;
 
 public final class ExpressHandling implements Chargeable, Labeled {
 
-    private static final long CHARGE = 120/100;
+    private static final long CHARGE = 120;
     private static final double VAT = 7.5/100.0;
 
     @Override
