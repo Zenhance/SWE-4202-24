f6c96f355bd4063e71e92bdf2221caf737f611ee
wasi <waseesomrat8@gmail.com>
2026-08-11 01:39:33 +0600

    ExpressHandling is created

---
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/ExpressHandling.java
@@ -0,0 +1,15 @@
+package kenakata.catalog;
+public class ExpressHandling implements Chargeable {
+    @Override
+    public long unitCharge(){
+        return 120;
+    }
+    @Override
+    public long unitVat(){
+        return (long) Math.ceil(unitCharge() * 0.075);
+    }
+    @Override
+    public String label(){
+        return "Express handling";
+    }
+}
