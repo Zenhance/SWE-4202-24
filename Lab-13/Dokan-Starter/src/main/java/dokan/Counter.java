063cc5f1d4d5f295fe0c63f48c124a8b6b9664bb
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-11 09:53:39 +0600

    init commit

--- Lab-13/Dokan-Starter/src/main/java/dokan/Counter.java
+++ Lab-13/Dokan-Starter/src/main/java/dokan/Counter.java
@@ -3,17 +3,21 @@ package dokan;
 import java.util.HashMap;
 
 
-public final class Counter<T>{
-    private final Map<T,Integer> counts = new HashMap<>();
-    public void add(T value){
-        counts.put(value,count(value)+1);
+public final class Counter<T> {
+    private final Map<T, Integer> counts = new HashMap<>();
+
+    public void add(T value) {
+        counts.put(value, count(value) + 1);
     }
-    public int count(T value){
+
+    public int count(T value) {
         return counts.getOrDefault(value, 0);
     }
+
     public int distinct() {
         return counts.size();
     }
+
     public Box<T> mostCommon() {
         if (counts.isEmpty()) {
             return Box.empty();
@@ -21,4 +25,12 @@ public final class Counter<T>{
 
         T mostCommon = null;
         int highestCount = -1;
+        for (Map.Entry<T, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() > highestCount) {
+                highestCount = entry.getValue();
+                mostCommon = entry.getKey();
+            }
+        }
+        return Box.of(mostCommon);
+    }
 }
