95bd646575675080bd36ffa0fb30dae1ac5aaf38
Zunied-57 <zunied0411@gmail.com>
2026-08-11 10:21:11 +0600

    counter class updated

--- Lab-13/Dokan-Starter/src/main/java/dokan/Counter.java
+++ Lab-13/Dokan-Starter/src/main/java/dokan/Counter.java
@@ -71,8 +71,11 @@ public final class Counter<T>
         T mostCommon=values.get(0);
         for(int i=1;i< values.size();i++)
         {
-            if(count.get(i)>highest) highest=count.get(i);
-
+            if(count.get(i)>highest)
+            {
+                highest=count.get(i);
+                mostCommon=values.get(i);
+            }
         }
         return Box.of(mostCommon);
     }
