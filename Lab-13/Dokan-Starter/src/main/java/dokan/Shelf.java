1441bf5e63965c2788e1b4002f912bbcde88c671
wasi <waseesomrat8@gmail.com>
2026-08-11 12:02:42 +0600

    Shelf class capacity method is made

--- Lab-13/Dokan-Starter/src/main/java/dokan/Shelf.java
+++ Lab-13/Dokan-Starter/src/main/java/dokan/Shelf.java
@@ -2,15 +2,15 @@ package dokan;
 
 import java.util.ArrayList;
 import java.util.List;
-public final class Shelf<T extends Item> {
+public final class Shelf<T extends Item>{
     private final List<T> items;
     private final int capacity;
     public Shelf(int capacity) {
         this.capacity = capacity;
         this.items = new ArrayList<>();
     }
-    public boolean add(T item) {
-        if (isFull()) {
+    public boolean add(T item){
+        if (isFull()){
             return false;
         }
         items.add(item);
@@ -28,6 +28,9 @@ public final class Shelf<T extends Item> {
     public boolean isFull() {
         return items.size() == capacity;
     }
+    public int capacity(){
+        return capacity;
+    }
     public boolean contains(String name) {
         for (T item : items) {
             if (item.name().equals(name)) {
@@ -36,25 +39,23 @@ public final class Shelf<T extends Item> {
         }
         return false;
     }
-    public boolean remove(String name) {
-        for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).name().equals(name)) {
+    public boolean remove(String name){
+        for (int i = 0; i < items.size(); i++){
+            if (items.get(i).name().equals(name)){
                 items.remove(i);
                 return true;
             }
         }
-
         return false;
     }
-
-    public int totalPriceTaka() {
+    public int totalPriceTaka(){
         int total = 0;
-        for (T item : items) {
+        for (T item : items){
             total += item.priceTaka();
         }
         return total;
     }
-    public List<T> items() {
+    public List<T> items(){
         return new ArrayList<>(items);
     }
 }
