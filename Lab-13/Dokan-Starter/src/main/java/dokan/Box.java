092a7fcc1f43cec76e4ca3864f31b9803d91848e
Tnvr09 <rahmantanvir64@gmail.com>
2026-08-11 09:40:43 +0600

    Box class initiated

--- Lab-13/Dokan-Starter/src/main/java/dokan/Box.java
+++ Lab-13/Dokan-Starter/src/main/java/dokan/Box.java
@@ -23,6 +23,11 @@ package dokan;
  */
 public final class Box<T> {
 
+    private T value;
+    private Box(T value){
+        this.value=value;
+    }
+
     /** A box holding {@code value}. Refuse null: a full box holding nothing is a lie. */
     public static <T> Box<T> of(T value) {
         throw new UnsupportedOperationException("TODO: Box.of");
