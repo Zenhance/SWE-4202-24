dcb29fd1f0fedb9a001a1ef040f7e8fa8c441110
tasnitaraida <tasnitar@gmail.com>
2026-08-11 11:31:44 +0600

    box class fixed

--- Lab-13/Dokan-Starter/src/main/java/dokan/Box.java
+++ Lab-13/Dokan-Starter/src/main/java/dokan/Box.java
@@ -22,10 +22,18 @@ package dokan;
  * </ul>
  */
 public final class Box<T> {
+    private final T value;
+
+    private Box(T value) {
+        this.value=value;
+    }
 
     /** A box holding {@code value}. Refuse null: a full box holding nothing is a lie. */
     public static <T> Box<T> of(T value) {
-        throw new UnsupportedOperationException("TODO: Box.of");
+        if(value==null) {
+            throw new UnsupportedOperationException("Value cannot be null");
+        }
+        return new Box<>(value);
     }
 
     /** An empty box. */
