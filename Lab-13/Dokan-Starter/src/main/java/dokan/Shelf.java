ab4284b80dcfd09c2a0b5987f011dc0f9a06c210
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-11 09:34:44 +0600

    init commit

--- Lab-13/Dokan-Starter/src/main/java/dokan/Shelf.java
+++ Lab-13/Dokan-Starter/src/main/java/dokan/Shelf.java
@@ -1,5 +1,6 @@
 package dokan;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,58 +30,12 @@ import java.util.List;
  * caller change the shelf.
  */
 public final class Shelf<T extends Item> {
+     private final int capacity;
+     private final List<T> items = new ArrayList<>();
 
-    public Shelf(int capacity) {
-        throw new UnsupportedOperationException("TODO: Shelf constructor");
-    }
-
-    /** Puts an item on the shelf. Returns false, without adding, if the shelf is full. */
-    public boolean add(T item) {
-        throw new UnsupportedOperationException("TODO: Shelf.add");
-    }
-
-    public T get(int index) {
-        throw new UnsupportedOperationException("TODO: Shelf.get");
-    }
-
-    public int size() {
-        throw new UnsupportedOperationException("TODO: Shelf.size");
-    }
-
-    public int capacity() {
-        throw new UnsupportedOperationException("TODO: Shelf.capacity");
-    }
-
-    public boolean isFull() {
-        throw new UnsupportedOperationException("TODO: Shelf.isFull");
-    }
-
-    public boolean isEmpty() {
-        throw new UnsupportedOperationException("TODO: Shelf.isEmpty");
-    }
-
-    public boolean contains(String name) {
-        throw new UnsupportedOperationException("TODO: Shelf.contains");
-    }
-
-    /** Takes the first item with this name off the shelf. True if one was there. */
-    public boolean remove(String name) {
-        throw new UnsupportedOperationException("TODO: Shelf.remove");
-    }
-
-    /** What everything on the shelf is worth. This is the method the bound pays for. */
-    public int totalPriceTaka() {
-        throw new UnsupportedOperationException("TODO: Shelf.totalPriceTaka");
-    }
-
-    /** The items, as a list the caller cannot use to change the shelf. */
-    public List<T> items() {
-        throw new UnsupportedOperationException("TODO: Shelf.items");
-    }
-
-    @Override
-    public String toString() {
-        // The test cases print shelves, so this should look like a list.
-        throw new UnsupportedOperationException("TODO: Shelf.toString");
-    }
-}
+  public Shelf(int capacity){
+      if(capacity<0){
+          throw new IllegalArgumentException("Capacity cant be negative");
+      }
+      this.capacity=capacity;
+  }
