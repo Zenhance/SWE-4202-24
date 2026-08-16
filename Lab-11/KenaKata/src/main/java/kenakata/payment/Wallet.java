2b860868ae347e07da6ce52f96af92e2d504b3a4
tryhardFuad <mubtasimsmail@gmail.com>
2026-08-17 01:39:48 +0600

    feat: added check for argument in constructor

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -3,6 +3,9 @@ package kenakata.payment;
 public class Wallet {
     private int balance;
     public Wallet(int balance) {
+        if(balance < 0){
+            throw new IllegalArgumentException("balance must be greater than 0");
+        }
         this.balance = balance;
     }
     public int balance() {
