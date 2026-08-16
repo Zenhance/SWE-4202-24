7b194cc118c19b31c30d7b771fdf68295b08edcf
tryhardFuad <mubtasimsmail@gmail.com>
2026-08-17 01:36:44 +0600

    fixed coupon class

--- Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
+++ Lab-11/KenaKata/src/main/java/kenakata/order/Coupon.java
@@ -1,6 +1,16 @@
 package kenakata.order;
 
 public class Coupon {
-    public Coupon(String c, int i, int i1, int i2, int i3) {
+    String code;
+    int percentage, cap, minSpend, validDate;
+    public Coupon(String code, int percentage, int cap, int minSpend, int validDate) {
+        if(percentage < 0 || percentage > 100) {
+            throw new IllegalArgumentException("percentage must be between 0 and 100");
+        }
+        this.code = code;
+        this.percentage = percentage;
+        this.cap = cap;
+        this.minSpend = minSpend;
+        this.validDate = validDate;
     }
 }
