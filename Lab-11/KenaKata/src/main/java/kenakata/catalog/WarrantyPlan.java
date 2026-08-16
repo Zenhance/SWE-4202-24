bff09e7ffa42554fb436b7ce714b4fd2f1f1e3b7
tryhardFuad <mubtasimsmail@gmail.com>
2026-08-16 18:25:24 +0600

    fix: throw exception in constructor

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/WarrantyPlan.java
@@ -5,18 +5,21 @@ import java.awt.*;
 public class WarrantyPlan implements Chargeable {
     private StockedGood stockedGood;
     public WarrantyPlan(StockedGood good) {
+        if(stockedGood == null) {
+            throw new IllegalArgumentException("stockedGood cannot be null");
+        }
         this.stockedGood = good;
     }
 
-    public int unitCharge() {
-        return 0;
+    public long unitCharge() {
+        return (long) Math.ceil(stockedGood.unitCharge() * 0.1);
     }
 
-    public int unitVat() {
-        return 0;
+    public long unitVat() {
+        return (long) Math.ceil(unitCharge() * 0.15);
     }
 
     public String label() {
-        return "Table";
+        return "Table Lamp";
     }
 }
