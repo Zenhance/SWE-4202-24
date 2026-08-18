0311385c853ddc39b7dd9408d6d30d4e91ad9a1a
arnoboic <arnobarafat@iut-dhaka.edu>
2026-08-18 09:58:53 +0600

    made Large Class

--- Lab-Final/Car-Parking/src/main/java/Large.java
+++ Lab-Final/Car-Parking/src/main/java/Large.java
@@ -1,10 +1,36 @@
 public class Large extends Slot{
+
+    private static int total;
+    private static int count;
+
     public Large(String license, String Scheme) {
         super(license, Scheme);
+        count++;
     }
 
     @Override
-    public int fee() {
-        return 0;
+    public int calculatefee() {
+        int fee = 30 + hours*20;
+
+        if(surchargeApplicable){
+            fee += 15;
+        }
+        fee = feeAfterScheme(fee);
+
+        earning(fee);
+
+        return fee;
+    }
+
+    public int getLargeCount(){
+        return count;
+    }
+
+    public int getAvailableLarge(){
+        return total-count;
+    }
+
+    public static void setSlotforLarge(int total){
+        Large.total = total;
     }
 }
