c2cd67fd95a52b255365e9392c4ac95cd60cea6a
tasnitaraida <tasnitar@gmail.com>
2026-08-18 11:08:31 +0600

    fixed child classes

--- Lab-Final/Car-Parking/src/main/java/Car.java
+++ Lab-Final/Car-Parking/src/main/java/Car.java
@@ -1,5 +1,15 @@
 public abstract class Car extends Vehicle {
-    public Car(String plate, int time, slotType slot, schemeType scheme) {
+    public Car(String plate, int time, SlotType slot, schemeType scheme) {
         super(plate, time, slot, scheme);
+    }
+
+    @Override
+    public SlotType[] occupiedSlot() {
+        return new SlotType[0];
+    }
+
+    @Override
+    public int rate() {
+        return 20;
     }
 }
--- Lab-Final/Car-Parking/src/main/java/MotorCycle.java
+++ Lab-Final/Car-Parking/src/main/java/MotorCycle.java
@@ -1,5 +1,15 @@
 public abstract class MotorCycle extends Vehicle {
-    public MotorCycle(String plate, int time, slotType slot, schemeType scheme) {
+    public MotorCycle(String plate, int time, SlotType slot, schemeType scheme) {
         super(plate, time, slot, scheme);
+    }
+
+    @Override
+    public SlotType[] occupiedSlot() {
+        return new SlotType[0];
+    }
+
+    @Override
+    public int rate() {
+        return 5;
     }
 }
