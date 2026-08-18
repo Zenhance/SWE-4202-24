988b44b4e06432590578a5f4b4309747c90ab088
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-08-18 09:42:28 +0600

    input command ready

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+        while (input.hasNextLine()) {
+            String line = input.nextLine().trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            String[] field = line.split(" ");
 
+        }
     }
 }
