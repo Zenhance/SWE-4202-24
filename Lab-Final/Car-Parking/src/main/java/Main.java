07d8471597c046f3b38ae3afd12649aa57a8d952
Rakin <rakinhasan001@gmail.com>
2026-08-18 10:13:20 +0600

    Probable cases pathway added

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,7 +1,57 @@
+import Records.HoursPassed;
+
 import java.util.Scanner;
+import Records.*;
 
 public class Main {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        while (input.hasNextLine()) {
+            String line = input.nextLine().trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            String[] field = line.split(" ");
 
+            if (field[0].equals("END")) {
+                break;
+            } else if (field[0].equals("MAXSTAY")) {
+
+            } else if (field[0].equals("COUNT")) {
+
+            }
+            else if (field[0].equals("SLOT")) {
+                Bike.set(field[1]);
+                Regular.set(field[2]);
+                Large.set(field[3]);
+
+            }
+            else if (field[0].equals("BILL")) {
+
+            }else if (field[0].equals("BIKE")) {
+
+            }else if (field[0].equals("CAR")) {
+
+            }else if (field[0].equals("TRUCK")) {
+
+            }else if (field[0].equals("PASSTIME")) {
+                int hours = Integer.parseInt(field[1]);
+                HoursPassed.set(hours);
+
+            }else if (field[0].equals("LEAVE")) {
+
+            }else if (field[0].equals("FREE")) {
+
+            }
+            else if (field[0].equals("COUNT")) {
+
+            }else if (field[0].equals("EARNED")) {
+
+            }else if (field[0].equals("REFUSED")) {
+
+            }
+
+            // ... one branch per keyword ...
+        }
     }
 }
