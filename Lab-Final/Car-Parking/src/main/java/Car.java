31d950d0e95b32a245850b880d7ba62de37aab0d
waseefmunir-max <waseef05@gmail.com>
2026-08-18 12:27:10 +0600

    Add plate field and constructor to Vehicle and Car classes for vehicle identification

--- Lab-Final/Car-Parking/src/main/java/Car.java
+++ Lab-Final/Car-Parking/src/main/java/Car.java
@@ -1,6 +1,10 @@
 import java.util.List;
 
 public class Car extends Vehicle {
+    protected Car(String plate) {
+        super(plate);
+    }
+
     @Override
     public List<SlotType> getAcceptedSlotTypes() {
         return List.of(
--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -1,6 +1,16 @@
 import java.util.List;
 
 public abstract class Vehicle {
+    private final String plate;
+
+    protected Vehicle(String plate) {
+        this.plate = plate;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
     public abstract List<SlotType> getAcceptedSlotTypes();
 
     public SlotType getHomeSlotType() {
