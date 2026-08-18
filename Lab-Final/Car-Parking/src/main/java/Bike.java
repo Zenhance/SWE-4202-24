ad75fbb6832a91ac021740aa3cf051b4911f8aa8
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-08-18 12:38:03 +0600

    completed the bike class maybe

--- Lab-Final/Car-Parking/src/main/java/Bike.java
+++ Lab-Final/Car-Parking/src/main/java/Bike.java
@@ -1,2 +1,9 @@
-public class Bike {
+public class Bike extends Vehicle{
+    public Bike(String registration,String pass){
+        super(registration,pass);
+    }
+    @Override
+    public String[] getSlotPreferences(){
+        return new String[]{"BIKE", "REGULAR", "LARGE"};
+    }
 }
