ee0f2968f74957825236f2f7cdde4bde797ff153
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-08-18 12:31:48 +0600

    applying the inputs methods

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,7 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ParkingLot parkingLot = new ParkingLot();
+        while (input.hasNextLine()){
+            String line = input.nextLine().trim();
+            if (line.isEmpty()){
+                continue;
+            }
+        }
 
     }
 }
