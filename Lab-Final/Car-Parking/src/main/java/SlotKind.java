221ac9b839c762a1e8468ba78262011b105ef808
wasif-22 <twasif68@gmail.com>
2026-08-18 09:24:18 +0600

    created Slotkind enum

---
+++ Lab-Final/Car-Parking/src/main/java/SlotKind.java
@@ -0,0 +1,15 @@
+public enum SlotKind {
+    BIKE (10,5,0);
+    REGULAR(30,20,15);
+    LARGE(50,40,25);
+
+    final int firstHour;
+    final int furtherHour;
+    final int surcharge;
+
+    SlotKind(int surcharge, int furtherHour, int firstHour) {
+        this.surcharge = surcharge;
+        this.furtherHour = furtherHour;
+        this.firstHour = firstHour;
+    }
+}
