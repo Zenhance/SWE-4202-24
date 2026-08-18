5d95a86e2beb5f2768adaf0d9723aba75a378bd8
ASUS <tasintajwar991@gmail.com>
2026-08-18 13:04:01 +0600

    created main class and fixed ParkingException

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,8 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args){
 
+        Scanner input = new Scanner(System.in);
+        List<Slot> vehicles = new ArrayList<>();
+        while (input.hasNextLine())
+        {
+            String line = input.nextLine().trim();
+            if (line.isEmpty())
+            {
+                continue;
+            }
+            String[] word = line.split(" ");
+            switch(word[0])
+            {
+                case "END" ->
+                {
+                    break;
+                }
 
+            }
+        }
     }
 }
