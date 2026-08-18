cf8073bada93eacae373728159c3b74044651728
58-jahin <jahinhossain@iut-dhaka.edu>
2026-08-18 11:13:34 +0600

    created student DISCOUNT CLASS

--- Lab-Final/Car-Parking/src/main/java/StudentDiscount.java
+++ Lab-Final/Car-Parking/src/main/java/StudentDiscount.java
@@ -1,2 +1,7 @@
-public class StudentDiscount {
+public final class StudentDiscount implements DiscountScheme {
+
+    @Override
+    public int apply(int bill) {
+        return bill - (20 * bill / 100);
+    }
 }
