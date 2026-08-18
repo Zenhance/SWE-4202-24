8d93caceb7726d22d9ae95a391457a3f78340ebf
04-Labiba <sabihalabiba639@gmail.com>
2026-08-18 12:20:33 +0600

    init commit

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner input=new Scanner(System.in);
-        ParkingLot lot=new ParkingLot();
-    while(input.hasNextLine()) {
-        String line = input.nextLine().trim();
-        if (line.isEmpty()) {
-            continue;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ParkingLot lot = new ParkingLot();
+        while(input.hasNextLine()){
+            String line=input.nextLine().trim();
+            if(line.isEmpty()){
+                continue;
+            }
+            String[] field=line.split(" ");
+            String command=field[0];
+            if(command.equals("END")) {
+                break;
+            }else if(command.equals("SLOTS")) {
+                int bike=Integer.parseInt(field[1]);
+                int regular=Integer.parseInt(field[2]);
+                int large=Integer.parseInt(field[3]);
+                lot.setSlots(bike,regular,large);
+            }else if(command.equals("MAXSTAY")) {
+                int hours=Integer.parseInt(field[1]);
+                lot.setMaxStay(hours);
+            }
         }
-        String[] field = line.split(" ");
-        String command = field[0];
-        if (command.equals("END")) {
-            break;
-        }
-    }
     }
 }
