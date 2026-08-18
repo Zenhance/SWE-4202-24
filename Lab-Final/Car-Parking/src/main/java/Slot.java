f07c510751b79e622ba14f14e8b2209a9dd5aa25
iam-tahmid <tahmidim99@gmail.com>
2026-08-18 12:32:19 +0600

    getters added in Slot class

--- Lab-Final/Car-Parking/src/main/java/Slot.java
+++ Lab-Final/Car-Parking/src/main/java/Slot.java
@@ -1,15 +1,42 @@
 package src.main.java;
 
 public class Slot {
-    Vehicle vehicle;
-    SlotType type;
+    private boolean isFree;
+    private int firstHourFee;
+    private int laterHourFee;
+    private int surcharge;
 
-    public Slot(SlotType type){
-        this.type=type;
-         vehicle = null;
-    }
-    public boolean isFree(){
-        return vehicle == null;
+    public Slot(){
+        this.isFree = true;
     }
 
+    public void setFirstHourFee(int firstHourFee) {
+        if(firstHourFee<=0){
+            throw new IllegalArgumentException("First hour fee can't be zero or negative");
+        }
+
+        this.firstHourFee = firstHourFee;
+    }
+
+    public void setLaterHourFee(int laterHourFee){
+        if (laterHourFee<=0){
+            throw new IllegalArgumentException("LaterhourFee can't be zero or negative");
+        }
+        this.laterHourFee = laterHourFee;
+    }
+
+    public void setSurcharge(int surcharge) {
+        if(surcharge<=0)
+        throw new IllegalArgumentException("Surcharge can't be zero or negative");
+        this.surcharge = surcharge;
+    }
+    public int getFirstHourFee(){
+        return firstHourFee;
+    }
+    public int getLaterHourFee(){
+        return laterHourFee;
+    }
+    public int getSurcharge(){
+        return surcharge;
+    }
 }
