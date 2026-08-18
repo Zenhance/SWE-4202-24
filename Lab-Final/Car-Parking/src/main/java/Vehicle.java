a2b77b938dafb53c1a9cee82a95cd566a31fc7a9
M-mahfuj321 <mustakimmahfuj@gmail.com>
2026-08-18 10:37:28 +0600

    4

--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -13,4 +13,30 @@ public class Vehicle {
         this.plate=plate;
     }
 
+    public Type getType(){
+        return type;
+    }
+    public Scheme getScheme(){
+        return scheme;
+    }
+    public int getEntry(){
+        return entry;
+    }
+    public String getPlate(){
+        return plate;
+    }
+
+    public int getParkedHours(int current){
+        return current-entry;
+    }
+
+    private Slot slot;
+
+    public void setSlot(Slot slot){
+        this.slot=slot;
+    }
+
+    public Slot getSlot(){
+        return slot;
+    }
 }
