fde1e95edb52e08d80344d0a1143032e9c231a7d
04-Labiba <sabihalabiba639@gmail.com>
2026-08-18 09:05:06 +0600

    init commit

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
+            Scanner input = new Scanner(System.in);
+            ParkingLot lot = new ParkingLot();
+
+            while (input.hasNextLine()) {
+                String line = input.nextLine().trim();
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                String[] field = line.split(" ");
+                String command = field[0];
     }
 }
---
+++ Lab-Final/Car-Parking/src/main/java/SlotType.java
@@ -0,0 +1,3 @@
+public enum SlotType {
+    BIKE, REGULAR,LARGE;
+}
