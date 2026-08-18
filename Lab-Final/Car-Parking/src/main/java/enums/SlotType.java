31733fa2af731b9006176cc0445d92d9af2cee2a
mubashirulhasan-netizen <mubashirulhasan@iut-dhaka.edu>
2026-08-18 09:41:49 +0600

    created two enums for different slot types and different vehicle types

---
+++ Lab-Final/Car-Parking/src/main/java/enums/SlotType.java
@@ -0,0 +1,30 @@
+package enums;
+
+public enum SlotType {
+
+    BIKE(10, 5, 0),
+    REGULAR(30, 20, 15),
+    LARGE(50, 40, 25);
+
+    private final int firstHourFee;
+    private final int furtherHourFee;
+    private final int surcharge;
+
+    SlotType(int firstHourFee, int furtherHourFee, int surcharge) {
+        this.firstHourFee = firstHourFee;
+        this.furtherHourFee = furtherHourFee;
+        this.surcharge = surcharge;
+    }
+
+    public int getFirstHourFee() {
+        return firstHourFee;
+    }
+
+    public int getFurtherHourFee() {
+        return furtherHourFee;
+    }
+
+    public int getSurcharge() {
+        return surcharge;
+    }
+}
