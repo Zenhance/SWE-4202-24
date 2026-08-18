abcf341113a28a0bdb93fbf9a772d4178129cb10
04-Labiba <sabihalabiba639@gmail.com>
2026-08-18 10:26:18 +0600

    init commit

---
+++ Lab-Final/Car-Parking/src/main/java/ParkingLot.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+public class ParkingLot {
+    private int bikeTotal,regularTotal,LargeTotal;
+    private int bikeFree,regularFree,largeFree;
+    private HashMap<String,Vehicle> parkedVehicles=new HashMap<>();
+    private int maxStayhrs;
+    private int time=0;
+    private int refusedCount=0;
+    private int totalEarned=0;
+
+}
