7828bf1d8443ef7a30c4141bba569428f3ab259c
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-08-18 12:12:26 +0600

    Added methods

--- Lab-Final/Car-Parking/src/main/java/ParkingLot.java
+++ Lab-Final/Car-Parking/src/main/java/ParkingLot.java
@@ -1,4 +1,4 @@
-import exceptions.ParkingRefusedException;
+import exceptions.ParkingException;
 import exceptions.SlotFullException;
 import model.Slot;
 import model.Vehicle;
@@ -42,13 +42,78 @@ public class ParkingLot {
         return null;
     }
 
-    public void arrive(Vehicle vehicle) {
+    public void arrive(Vehicle vehicle) throws ParkingException {
+        if(findVehicle(vehicle.getLicencePlate()) != null) {
+            throw new ParkingException();
+        }
         Slot slot = findFreeSlot(vehicle);
         if(slot == null){
-            throw new SlotFullException("");
+            throw new SlotFullException();
         }
         slot.park(vehicle);
         vehicles.add(vehicle);
         vehicle.setEntryTime(time);
+    }
+
+    public Vehicle findVehicle(String plate){
+        for(Vehicle v: vehicles){
+            if(v.getLicencePlate().equals(plate)){
+                return v;
+            }
+        }
+        return null;
+    }
+    public Slot findSlot(String plate){
+        for(Slot s: slots){
+            if(!s.free() && s.getVehicle().getLicencePlate().equals(plate)){
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public int calculate(Vehicle v, Slot s, int hours){
+        int total = s.getFirstHourCHarge() + (hours-1)* s.getExtraHourCHarge();
+        if(!s.getType().equals(v.bestSlot())){
+            total += s.getSurcharge();
+        }
+
+        if(v.getPermit().equals("STUDENT")){
+            total -= 20/total*100;
+        }
+        if(v.getPermit().equals("WEEKEND")){
+            total = Math.max(0, total-10);
+        }
+        return total;
+    }
+
+    public int bill(String plate){
+        Vehicle v = findVehicle(plate);
+        if(v == null){
+            return -1;
+        }
+        Slot s = findSlot(plate);
+
+        int hours = time - v.getEntryTime();
+
+        return calculate(v, s, hours);
+    }
+
+    public void leave(String plate) throws ParkingException {
+        Vehicle v = findVehicle(plate);
+        if(v == null){
+            throw new ParkingException();
+        }
+
+        Slot s = findSlot(plate);
+
+        earned += bill(plate);
+
+        s.remove();
+        vehicles.remove(v);
+    }
+
+    public int free(String type){
+
     }
 }
