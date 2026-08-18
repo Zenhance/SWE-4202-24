a2acb360df47dde6b1fba9ce965f36f9da18b597
md-musaddik <musaddikibna1@gmail.com>
2026-08-18 13:01:48 +0600

    leaveVehicle function declared.

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 
 public class Main {
-    static int bikeSlots,regularSlots,largeSlots;
+    static int bikeSlots, regularSlots, largeSlots;
     static int freeBike, freeRegular, freeLarge;
 
     static ArrayList<String> plates = new ArrayList<>();
@@ -17,73 +17,99 @@ public class Main {
     static int refusedCount = 0;
     static int totalEarned = 0;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        while(input.hasNextLine()){
+        while (input.hasNextLine()) {
             String line = input.nextLine().trim();
             String[] field = line.split(" ");
-            String firstWord=field[0];
+            String firstWord = field[0];
 
             if (firstWord.equals("END")) {
                 break;
-            }else if(firstWord.equals("SLOTS")){
+            } else if (firstWord.equals("SLOTS")) {
                 bikeSlots = parseInt(field[1]);
                 regularSlots = parseInt(field[2]);
                 largeSlots = parseInt(field[3]);
                 freeBike = bikeSlots;
                 freeRegular = regularSlots;
                 freeLarge = largeSlots;
-            }else if (firstWord.equals("MAXSTAY")) {
+            } else if (firstWord.equals("MAXSTAY")) {
                 maxStay = Integer.parseInt(field[1]);
-            }else if (firstWord.equals("BIKE") || firstWord.equals("CAR") || firstWord.equals("TRUCK")) {
+            } else if (firstWord.equals("BIKE") || firstWord.equals("CAR") || firstWord.equals("TRUCK")) {
                 plates.add(field[1]);
                 types.add(firstWord);
                 discounts.add(field[2]);
                 parkedIn.add("");
                 hoursParked.add(0);
-            }else if (firstWord.equals("SLOT")) {
+            } else if (firstWord.equals("SLOT")) {
                 System.out.println(parkVehicle(field[1]));
             } else if (firstWord.equals("FREE")) {
                 System.out.println(freeSlots(field[1]));
-            }else if (firstWord.equals("COUNT")) {
+            } else if (firstWord.equals("COUNT")) {
                 System.out.println(countParked());
-            }else if(firstWord.equals("REFUSED")){
+            } else if (firstWord.equals("REFUSED")) {
                 System.out.println(refusedCount);
-            }else if (firstWord.equals("BILL")) {
+            } else if (firstWord.equals("BILL")) {
 
-            }else if (firstWord.equals("PASSTIME")) {
+            } else if (firstWord.equals("PASSTIME")) {
                 int hrs = Integer.parseInt(field[1]);
 
-            }else if(firstWord.equals("LEAVE")){
+            } else if (firstWord.equals("LEAVE")) {
 
-            }else if (firstWord.equals("EARNED")) {
+            } else if (firstWord.equals("EARNED")) {
                 System.out.println(totalEarned);
             }
 
 
-            }
+        }
 
     }
+
     static int indexOf(String plate) {
         for (int i = 0; i < plates.size(); i++) {
             if (plates.get(i).equals(plate)) return i;
         }
         return -1;
     }
-    static String parkVehicle(String plate){
+
+    static String parkVehicle(String plate) {
         int i = indexOf(plate);
         String type = types.get(i);
-        if(type.equals("BIKE")){
-            if (freeBike > 0) { freeBike--; parkedIn.set(i, "BIKE"); return "BIKE"; }
-            if (freeRegular > 0) { freeRegular--; parkedIn.set(i, "REGULAR"); return "REGULAR"; }
-            if (freeLarge > 0) { freeLarge--; parkedIn.set(i, "LARGE"); return "LARGE"; }
-        }else if (type.equals("CAR")) {
-            if (freeRegular > 0) { freeRegular--; parkedIn.set(i, "REGULAR"); return "REGULAR"; }
-            if (freeLarge > 0) { freeLarge--; parkedIn.set(i, "LARGE"); return "LARGE"; }
+        if (type.equals("BIKE")) {
+            if (freeBike > 0) {
+                freeBike--;
+                parkedIn.set(i, "BIKE");
+                return "BIKE";
+            }
+            if (freeRegular > 0) {
+                freeRegular--;
+                parkedIn.set(i, "REGULAR");
+                return "REGULAR";
+            }
+            if (freeLarge > 0) {
+                freeLarge--;
+                parkedIn.set(i, "LARGE");
+                return "LARGE";
+            }
+        } else if (type.equals("CAR")) {
+            if (freeRegular > 0) {
+                freeRegular--;
+                parkedIn.set(i, "REGULAR");
+                return "REGULAR";
+            }
+            if (freeLarge > 0) {
+                freeLarge--;
+                parkedIn.set(i, "LARGE");
+                return "LARGE";
+            }
 
-        }else if (type.equals("TRUCK")) {
-            if (freeLarge > 0) { freeLarge--; parkedIn.set(i, "LARGE"); return "LARGE"; }
+        } else if (type.equals("TRUCK")) {
+            if (freeLarge > 0) {
+                freeLarge--;
+                parkedIn.set(i, "LARGE");
+                return "LARGE";
+            }
         }
         refusedCount++;
         return "REFUSED";
@@ -103,6 +129,7 @@ public class Main {
         }
         return count;
     }
+
     static int billFor(String plate) {
         int i = indexOf(plate);
         if (i == -1 || parkedIn.get(i).equals("")) return 0;
@@ -146,5 +173,10 @@ public class Main {
         }
 
         return bill;
+    }
+    static void leaveVehicle(String plate) {
+        int i = indexOf(plate);
+        if (i == -1 || parkedIn.get(i).equals("")) return;
+        totalEarned += billFor(plate);
     }
 }
