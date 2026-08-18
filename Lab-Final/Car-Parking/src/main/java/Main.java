bca23c441290281a0627053fa18b86cc006c0016
sabbir240042147 <eftesabbir2725@gmail.com>
2026-08-18 13:35:11 +0600

    final commit done

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        String line=input.nextLine();
+        String[] parts=line.split(" ");
+         int bike=Integer.parseInt(parts[1]);
+        int regular=Integer.parseInt(parts[2]);
+        int large=Integer.parseInt(parts[3]);
+        ParkingSlot parkingSlot=new ParkingSlot(bike,regular,large);
+        line = input.nextLine();
+        parts = line.split(" ");
+        int maxStay = Integer.parseInt(parts[1]);
 
+        input.close();
     }
 }
