afc730fc5631c6003aedac61f63aa7a0608f4ee8
mubashirulhasan-netizen <mubashirulhasan@iut-dhaka.edu>
2026-08-18 12:49:23 +0600

    Completed after fixing all the bugs

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,7 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args){
 
+    public static void main(String[] args)
+            throws Exception {
+
+        CarPark carPark = new CarPark();
+
+        CommandProcessor processor =
+                new CommandProcessor(carPark);
+
+        BufferedReader reader =
+                new BufferedReader(
+                        new InputStreamReader(System.in)
+                );
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            line = line.trim();
+
+            if (line.equals("END")) {
+                break;
+            }
+
+            processor.process(line);
+        }
     }
 }
