38344212c94e9e51c0c517c12f1ff372743ea98d
Tasauf Abdullah <tasaufalter@gmail.com>
2026-08-18 13:04:49 +0600

    bug fixes to class Truck

--- Lab-Final/Car-Parking/src/main/java/Truck.java
+++ Lab-Final/Car-Parking/src/main/java/Truck.java
@@ -1,5 +1,17 @@
-public class Truck extends Vehicle{
-    public Truck (String plate, DiscountScheme discount) {
-        super(plate,discount);
+import java.util.List;
+
+public class Truck extends Vehicle {
+    public Truck(String plate, DiscountScheme discount) {
+        super(plate, discount);
+    }
+
+    @Override
+    public SlotKind getHomeSlotKind() {
+        return SlotKind.LARGE;
+    }
+
+    @Override
+    public List<SlotKind> getPreferredSlots() {
+        return List.of(SlotKind.LARGE);
     }
 }
