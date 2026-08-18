c9dd5982df25e46a418a789881c768f04fd3d021
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-08-18 11:10:35 +0600

    Added Vehicle abstract class

--- Lab-Final/Car-Parking/src/main/java/model/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/model/Vehicle.java
@@ -3,42 +3,32 @@ package model;
 import exceptions.InvalidLicensePlateException;
 
 public abstract class Vehicle {
-    private final String type;
     private final String licencePlate;
     private final String permit;
-    private boolean isParked;
-    private int parkedTime;
+    private int entryTime;
 
-    public Vehicle(String type, String licencePlate, String permit) {
-        if(type==null || permit==null){
+
+    public Vehicle(String licencePlate, String permit) {
+        if(permit==null){
             throw new IllegalArgumentException();
         }
         if(licencePlate==null){
-            throw new InvalidLicensePlateException("licencePlate is null");
+            throw new InvalidLicensePlateException("");
         }
-
-        this.type = type;
         this.licencePlate = licencePlate;
         this.permit = permit;
-        this.parkedTime = 0;
-        isParked = false;
     }
 
-    public String getType() {
-        return type;
-    }
     public String getLicencePlate() {
         return licencePlate;
     }
     public String getPermit() {
         return permit;
     }
-    public int getParkedTime() {
-        return parkedTime;
+    public void setEntryTime(int currentTime){
+        entryTime = currentTime;
     }
 
-    public void park(){
-        isParked = true;
-        parkedTime++;
-    }
+    public abstract String[] slotOrder();
+
 }
