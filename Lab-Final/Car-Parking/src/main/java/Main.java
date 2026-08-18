61493fb73fd127cb348b27b7c63722582d55ac4a
04-Labiba <sabihalabiba639@gmail.com>
2026-08-18 12:29:40 +0600

    init commit

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -40,6 +40,25 @@ public class Main {
                     System.out.println(slot.name());
                 }
             }
+            else if(command.equals("FREE")) {
+                SlotType type = SlotType.valueOf(field[1]);
+                System.out.println(lot.freeCount(type));
+            }
+            else if(command.equals("COUNT")) {
+                System.out.println(lot.parkedCount());
+            }
+            else if(command.equals("REFUSED")) {
+                System.out.println(lot.refusedCount());
+            }
+            else if(command.equals("BILL")) {
+                Integer bill = lot.billFor(field[1]);
+                if(bill==null) {
+                    System.out.println("NONE");
+                }
+                else{
+                    System.out.println(bill);
+                }
+            }
         }
     }
 }
