8d1406e8c7e541652c2c017a3688151655a372a3
Tasnova Tahia <tasnovatahia2014@gmail.com>
2026-08-18 09:26:29 +0600

    preparing to take nouts in a way so that last line is always end first line is slot

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,7 +1,33 @@
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
+
+Scanner input=new Scanner(System.in);
+CarPark park=null;
+
+while(input.hasNextLine()){
+    String line= input.nextLine().trim();
+
+    if(line.isEmpty()){
+        continue;
+    }
+
+    String[] field=line.split("\\s+");
+    String command=field[0];
+
+    if(command.equals("END")){
+        break;
+    }
+    try{
+        if(command.equals("SLOTS")){
+
+            int bike=Integer.parseInt()
+        }
+    }
+}
 
     }
 }
