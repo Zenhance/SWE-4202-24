c69e107b1ca95fc1277c5dc55f4ba5f35edb1f9b
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-08-18 11:46:45 +0600

    added method for creating vehicle

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -12,6 +12,18 @@ public class Main {
             return new NoDiscount();
         }
     }
+    public static Vehicle makeVehicle(String type, String plate, String d){
+        Discount discount = makeDiscount(d);
+        if(type.equals("BIKE")){
+            return new Bike(plate,discount);
+        }
+        else if(type.equals("CAR")){
+            return new Car(plate,discount);
+        }
+        else{
+            return new Truck(plate,discount);
+        }
+    }
     public static void main(String[] args){
 
     }
