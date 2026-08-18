905f0c4937eaf63ea82ca295c392eb5b07ecd147
Fayaz16 <shahtanzim900@gmail.com>
2026-08-18 09:41:36 +0600

    commited

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,7 +1,42 @@
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Main {
     public static void main(String[] args){
 
-    }
+     Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNextLine()) {
+            String line = input.nextLine().trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            String[] field = line.split(" ");
+
+            if (field[0].equals("END")) {
+                break;
+            } else if (field[0].equals("MAXSTAY")) {
+                int hours = parseInt(field[1]);
+                // ... do something with hours ...
+            } else if (field[0].equals("COUNT")) {
+                System.out.println(/* the number you worked out */);
+            } else if (field[0].equals("LEAVE")){
+
+            // ... one branch per keyword ...
+        }
+        else if (field[0].equals("BILLS")) {}
+        else if (field[0].equals("SLOT")){
 }
+        else if (field[0].equals("EARNED")){}
+        else if (field[0].equals("FREE")){
+     if(field[1].equals("BIKE"))System.out.println(slot.bike);
+            }
+}       else if (field[0].equals("PASSTIME")){
+            int hours = Integer.parseInt(field[1]);
+            time = hours;
+            }
+}
+        else if (field[0].equals("SLOTS")){}
+        else if (field[0].equals("BIKE"))
---
+++ Lab-Final/Car-Parking/src/main/java/Slot.java
@@ -0,0 +1,9 @@
+public class Slot {
+    public int bike;
+    public int regular;
+    public int truck;
+
+}
+enum slot{
+    "BIKE","TRUCK","REGULAR";
+}
