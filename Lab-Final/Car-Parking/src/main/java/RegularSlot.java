83f9b288d3c933f22b8edc566ec8303c564829f9
iam-tahmid <tahmidim99@gmail.com>
2026-08-18 12:42:37 +0600

    RegularSlot class added and the values are set

--- Lab-Final/Car-Parking/src/main/java/RegularSlot.java
+++ Lab-Final/Car-Parking/src/main/java/RegularSlot.java
@@ -1,5 +1,11 @@
 package src.main.java;
 
 public class RegularSlot extends Slot{
+    public RegularSlot(){
+        super();
+        setFirstHourFee(30);
+        setLaterHourFee(20);
+        setSurcharge(15);
+    }
 
 }
