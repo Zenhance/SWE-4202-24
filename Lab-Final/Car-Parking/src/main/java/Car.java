5243f692f382d0d81ba43fa59cb538bddde41cd8
mubashirulhasan-netizen <mubashirulhasan@iut-dhaka.edu>
2026-08-18 11:30:22 +0600

    completed all the individual vehicles

---
+++ Lab-Final/Car-Parking/src/main/java/Car.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Car extends Vehicle {
+
+    public Car(String plate, DiscountScheme discountScheme) {
+        super(plate, discountScheme);
+    }
+
+    @Override
+    public VehicleType getVehicleType() {
+        return VehicleType.CAR;
+    }
+
+    @Override
+    public List<SlotType> getPreferredSlotTypes() {
+        return List.of(
+                SlotType.REGULAR,
+                SlotType.LARGE
+        );
+    }
+}
---
+++ Lab-Final/Car-Parking/src/main/java/Motorcycle.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Motorcycle extends Vehicle {
+
+    public Motorcycle(String plate, DiscountScheme discountScheme) {
+        super(plate, discountScheme);
+    }
+
+    @Override
+    public VehicleType getVehicleType() {
+        return VehicleType.BIKE;
+    }
+
+    @Override
+    public List<SlotType> getPreferredSlotTypes() {
+        return List.of(
+                SlotType.BIKE,
+                SlotType.REGULAR,
+                SlotType.LARGE
+        );
+    }
+}
