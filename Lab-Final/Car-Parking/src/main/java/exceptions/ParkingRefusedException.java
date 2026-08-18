722762edfee0e537f4c5ff1e4d64e1bcdc5bdae1
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-08-18 12:12:35 +0600

    Added methods

--- Lab-Final/Car-Parking/src/main/java/exceptions/ParkingRefusedException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public abstract class ParkingRefusedException extends RuntimeException {
-    public ParkingRefusedException(String message) {
-        super(message);
-    }
-}
