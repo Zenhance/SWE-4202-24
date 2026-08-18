2164e98a7c28e97059c91812cab207b9b0362f23
shafin-75 <arafatshafin85@gmail.com>
2026-08-18 09:41:55 +0600

    ...

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        ParkingLot lot = null;
+        While(input.hasNextLine()){
+            String Line = input.nextLine().trim();
+            if(line.isEmpty()){
+                continue;
+            }
+            String[] field = line.split(" ");
+            if(field[0].equals("END")){
+                break;
+            }
+            else if(field[0].equals("MAXSTAY")){
+                int hours = Integer.parseInt(field[1]);
 
+            }
+            else if(field[0].equals("COUNT")){
+                System.out.println("   ");
+            }
+        }
     }
 }
