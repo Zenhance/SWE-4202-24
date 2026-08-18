56e22fb8906926b6683c5f7c55b8822ae68a1673
Mahdi Nur Samin <mahdinursamin@iut-dhaka.edu>
2026-08-18 13:00:02 +0600

    BikeSlot class

---
+++ Lab-Final/Car-Parking/src/main/java/BikeSlot.java
@@ -0,0 +1,8 @@
+public class BikeSlot extends Slot{
+        public BikeSlot(){
+            super();
+            setSurcharge(0);
+            setFirstaHourFee(10);
+            setLaterFee(5);
+        }
+}
