881a5674d151b9d6f6f0b6ec184a5bd78d3fd8d4
wasifjafar <wasifjafar71@gmail.com>
2026-08-18 10:01:55 +0600

    add Vehicle class's timeStayed field and it's getter

--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -1,14 +1,20 @@
 public abstract class Vehicle {
     private final String plate;
     private Slot preferredType;
+    private int timeStayed;
 
     protected Vehicle(String plate) {
         this.plate = plate;
+        this.timeStayed = 0;
     }
 
     public void setPreferredType(Slot preferredType) {
         if (preferredType == null)
             throw new IllegalArgumentException("Preferred slot type cannot be null");
         this.preferredType = preferredType;
+    }
+
+    public int getTimeStayed() {
+        return timeStayed;
     }
 }
