0bfb5d0e0eb79da1c15b3d442cee20bb9153dfae
58-jahin <jahinhossain@iut-dhaka.edu>
2026-08-18 09:30:17 +0600

    1

---
+++ Lab-Final/Car-Parking/src/main/java/CarPark.java
@@ -0,0 +1,7 @@
+public class CarPark {
+    public CarPark(int i, int i1, int i2) {
+    }
+
+    public void setMaximumStay(int i) {
+    }
+}
--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        CarPark park = null;
 
+        while (input.hasNextLine()) {
+            String line = input.nextLine().trim();
+
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            String[] field = line.split("\\s+");
+
+            if (field[0].equals("END")) {
+                break;
+            }
+
+        }
     }
 }
