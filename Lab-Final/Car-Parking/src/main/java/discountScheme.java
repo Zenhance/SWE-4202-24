ba9e2cfcc701f40ce8424d7d76bb0ba628ca0aef
tuireuit <jamilhasan@iut-dhaka.edu>
2026-08-18 12:04:15 +0600

    discount scheme fix

--- Lab-Final/Car-Parking/src/main/java/discountScheme.java
+++ Lab-Final/Car-Parking/src/main/java/discountScheme.java
@@ -1,3 +1,9 @@
-public enum discountScheme {
-    NONE,STUDENT,WEEKEND;
+public class discountScheme {
+    private slotType slot;
+
+    public discountScheme(slotType slot) {
+        if (slot == null)
+            throw new IllegalArgumentException("Slot cannot be null");
+        this.slot = slot;
+    }
 }
