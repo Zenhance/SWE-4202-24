3f36e64b847de0c5e150ce8b5bf23b4c10fad8dc
58-jahin <jahinhossain@iut-dhaka.edu>
2026-08-18 10:53:00 +0600

    vehicle class 1

--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -1,2 +1,35 @@
-public class Vehicle {
+import java.util.List;
+
+public abstract class Vehicle {
+    private final String plate;
+    private final DiscountScheme discount;
+    private int hours;
+
+    protected Vehicle(String plate,DiscountScheme discount) {
+        this.plate = plate;
+        this.discount = discount;
+        this.hours = 0;
+    }
+
+    public String plate() {
+        return plate;
+    }
+
+    public DiscountScheme discount() {
+        return discount;
+    }
+
+    public int hours() {
+        return hours;
+    }
+
+    public void addHours(int addedHours) {
+        hours += addedHours;
+    }
+
+    public SlotKind ownSlotKind() {
+        return acceptedSlots().get(0);
+    }
+
+    public abstract List<SlotKind> acceptedSlots();
 }
