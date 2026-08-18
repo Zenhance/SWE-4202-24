82486b394f328eaf0be186ba2ee4f79a0c6013d7
sabbir240042147 <eftesabbir2725@gmail.com>
2026-08-18 12:07:13 +0600

    slot car

--- Lab-Final/Car-Parking/src/main/java/Car.java
+++ Lab-Final/Car-Parking/src/main/java/Car.java
@@ -1,2 +1,10 @@
-public class Car {
+import java.util.List;
+
+public class Car extends Vehicle{
+    Car(String numberPlate,Discount discount){
+        super(numberPlate,discount);
+    }
+    public List<Slot> acceptanceOrder(){
+        return List.of(Slot.REGULAR,Slot.LARGE);
+    }
 }
