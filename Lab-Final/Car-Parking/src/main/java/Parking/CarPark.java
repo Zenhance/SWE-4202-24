88aa9ca91a44c9a975dd38e4b9eb04b80989bd8a
mahibrahaman175-stack <mahibrahaman175@gmail.com>
2026-08-18 12:53:40 +0600

    commit changes again

--- Lab-Final/Car-Parking/src/main/java/Parking/CarPark.java
+++ Lab-Final/Car-Parking/src/main/java/Parking/CarPark.java
@@ -49,4 +49,77 @@ public class CarPark {
         return null;
     }
 
+    public void parkVehicle(Vehicle vehicle) throws NoPlateException, NoSlotException {
+        if(vehicle==null || vehicle.getNumberPlate()==null || vehicle.getNumberPlate().isBlank()){
+            throw new Exception.NoPlateException("Vehicle has no plate");
+        }
+
+        Slot slot= findAvailableSlot(vehicle);
+        if(slot==null){
+            throw new NoSlotException("No suitable slot found");
+        }
+        vehicle.setEntryTime(currentTime);
+
+        slot.park(vehicle);
+        vehicle.add(vehicles);
+
+    }
+
+    private Vehicle findVehicle(String plate){
+        for(Vehicle vehicle: vehicles ){
+            if(vehicle.getNumberPlate().equals(plate)){
+                return vehicle;
+            }
+        }
+        return null;
+    }
+
+    private Slot findSlot(Vehicle vehicle){
+        for(Slot slot: slots){
+            if(slot.getVehicle()==vehicle){
+                return slot;
+            }
+        }
+        return null;
+    }
+
+    private int calculateBaseBill(Slot slot, int hours){
+        if(hours<1){
+            hours=1;
+        }
+        int firsthour;
+        int furtherhour;
+        int surcharge=0;
+
+        if(slot.getType()==SlotType.BIKE){
+            firsthour=10;
+            furtherhour=5;
+        }else if(slot.getType()==SlotType.REGULAR){
+            firsthour=30;
+            furtherhour=20;
+            surcharge=15;
+        }else{
+            firsthour=50;
+            furtherhour=40;
+            surcharge=25;
+        }
+        return firsthour+(hours-1)*furtherhour+surcharge;
+    }
+
+
+    private int applyDiscount(int bill,  DiscountScheme discountScheme){
+        if(discountScheme==DiscountScheme.NONE){
+            return bill;
+        }
+
+        if(discountScheme== Slot.DiscountScheme.STUDENT){
+            return bill-(bill*20/100);
+        }
+        if(discountScheme== Slot.DiscountScheme.WEEKEND){
+            return Math.max(0, bill-10);
+        }
+
+        return bill;
+    }
+
 }
