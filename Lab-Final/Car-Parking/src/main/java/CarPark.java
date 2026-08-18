9948ab82318abf8a8f9da153d8ad3623ed9bf156
waseefmunir-max <waseef05@gmail.com>
2026-08-18 10:42:43 +0600

    Initialize slots in CarPark constructor for bike, regular, and large types

--- Lab-Final/Car-Parking/src/main/java/CarPark.java
+++ Lab-Final/Car-Parking/src/main/java/CarPark.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarPark {
     private final int bikeSlots;
     private final int regularSlots;
     private final int largeSlots;
+    private final List<Slot> slots = new ArrayList<>();
 
     public CarPark(int bikeSlots, int regularSlots, int largeSlots) {
         this.bikeSlots = bikeSlots;
         this.regularSlots = regularSlots;
         this.largeSlots = largeSlots;
+
+        for (int i = 0; i < bikeSlots; i++) {
+            slots.add(new Slot(SlotType.BIKE));
+        }
+
+        for (int i = 0; i < regularSlots; i++) {
+            slots.add(new Slot(SlotType.REGULAR));
+        }
+
+        for (int i = 0; i < largeSlots; i++) {
+            slots.add(new Slot(SlotType.LARGE));
+        }
     }
 }
