b6e8d50748f7673339bd1abf0b5b2fde6c5e118a
Zunied-57 <zunied0411@gmail.com>
2026-08-18 09:58:48 +0600

    Vehicle Class created

---
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -0,0 +1,42 @@
+public class Vehicle
+{
+    private String numberPlate;
+    private int entranceTime;
+    private Category category;
+    private DiscountScheme scheme;
+    private Slot slot;
+
+    public Vehicle(String numberPlate, int entranceTime, Category category, DiscountScheme scheme, Slot slot)
+    {
+        this.numberPlate = numberPlate;
+        this.entranceTime = entranceTime;
+        this.category = category;
+        this.scheme = scheme;
+        this.slot = slot;
+    }
+
+    public String getNumberPlate()
+    {
+        return numberPlate;
+    }
+
+    public int getEntranceTime()
+    {
+        return entranceTime;
+    }
+
+    public Slot getSlot()
+    {
+        return slot;
+    }
+
+    public void setSlot(Slot slot)
+    {
+        this.slot = slot;
+    }
+
+    public int stayHours(int presentTime)
+    {
+        return presentTime-entranceTime;
+    }
+}
