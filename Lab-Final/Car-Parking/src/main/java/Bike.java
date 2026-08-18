c5b8ce96425d531daf0318bb720fb58cf80fc29f
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-18 09:52:12 +0600

    init commit

--- Lab-Final/Car-Parking/src/main/java/Bike.java
+++ Lab-Final/Car-Parking/src/main/java/Bike.java
@@ -2,4 +2,13 @@ public class Bike extends Vehicle{
     public Bike(String plate,Category category){
         super(plate,category);
     }
+    @Override
+    public SlotType[] prefferedSlot(){
+        return new SlotType[]{
+                SlotType.BIKE,
+                SlotType.REGULAR,
+                SlotType.LARGE
+        };
+    }
+
 }
