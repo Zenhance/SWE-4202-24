e4506629568e6de6870b3a04f2beb8d44089bb15
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-18 11:38:23 +0600

    init commit

--- Lab-Final/Car-Parking/src/main/java/Parkinglot.java
+++ Lab-Final/Car-Parking/src/main/java/Parkinglot.java
@@ -10,54 +10,58 @@ public class Parkinglot {
     private int regularFree;
     private int largeFree;
 
-    private HashMap<String,Vehicle>parkedVehicles = new HashMap<>();
+    private HashMap<String, Vehicle> parkedVehicles = new HashMap<>();
     private int maxStayhrs;
-    private int time=0;
-    private int refusedCount=0;
-    private int totalEarned=0;
+    private int time = 0;
+    private int refusedCount = 0;
+    private int totalEarned = 0;
 
-public void setSlots(int bike,int regular,int large){
-    bikeTotal=bike;
-    regularTotal=regular;
-    largeTotal=large;
-    regularFree=regular;
-    largeFree=large;
-}
-public void setMaxStay(int hrs) {
-    maxStayhrs = hrs;
-}
-public void advanceTime(int hrs){
-    currentTime+=hrs;
-}
-private int getFreeSlots(SlotType type){
-    if(type==SlotType.BIKE){
-        return bikeFree;
+    public void setSlots(int bike, int regular, int large) {
+        bikeTotal = bike;
+        regularTotal = regular;
+        largeTotal = large;
+        regularFree = regular;
+        largeFree = large;
     }
-    else if(type==SlotType.REGULAR){
-        return regularFree;
+
+    public void setMaxStay(int hrs) {
+        maxStayhrs = hrs;
     }
-    else{
-        return largeFree;
+
+    public void advanceTime(int hrs) {
+        time += hrs;
     }
-}
-private void takeOneSlot(SlotType type) {
-    if (type == SlotType.BIKE) {
-        bikeFree = bikeFree - 1;
-    } else if (type == SlotType.REGULAR) {
-        regularFree = regularFree - 1;
-    } else {
-        largeFree = largeFree - 1;
+
+    private int getFreeSlots(SlotType type) {
+        if (type == SlotType.BIKE) {
+            return bikeFree;
+        } else if (type == SlotType.REGULAR) {
+            return regularFree;
+        } else {
+            return largeFree;
+        }
     }
-}
-private void returnOneSlot(SlotType type){
-    if (type == SlotType.BIKE) {
-        bikeFree = bikeFree + 1;
-    } else if (type == SlotType.REGULAR) {
-        regularFree = regularFree + 1;
-    } else {
-        largeFree = largeFree + 1;
+
+    private void takeOneSlot(SlotType type) {
+        if (type == SlotType.BIKE) {
+            bikeFree = bikeFree - 1;
+        } else if (type == SlotType.REGULAR) {
+            regularFree = regularFree - 1;
+        } else {
+            largeFree = largeFree - 1;
+        }
     }
-}
+
+    private void returnOneSlot(SlotType type) {
+        if (type == SlotType.BIKE) {
+            bikeFree = bikeFree + 1;
+        } else if (type == SlotType.REGULAR) {
+            regularFree = regularFree + 1;
+        } else {
+            largeFree = largeFree + 1;
+        }
+    }
+
     public void register(Vehicle vehicle) {
         SlotType[] choices = vehicle.prefferedSlot();
 
@@ -65,12 +69,27 @@ private void returnOneSlot(SlotType type){
             SlotType type = choices[i];
             if (getFreeSlots(type) > 0) {
                 takeOneSlot(type);
-                vehicle.park(type, currentTime);
+                vehicle.park(type, time);
                 parkedVehicles.put(vehicle.getPlate(), vehicle);
                 return;
             }
         }
-refusedCount=refusedCount+1;
+        refusedCount = refusedCount + 1;
+    }
+
+    public SlotType slotOf(String plate) {
+        Vehicle v = parkedVehicles.get(plate);
+        if (v == null) {
+            return null;
+        }
+        return v.getAssignedSlot();
+    }
+    public int freeCount(SlotType type) {
+        return getFreeSlots(type);
+    }
+    public int parkedCount() {
+        return parkedVehicles.size();
+    }
 }
 
 
