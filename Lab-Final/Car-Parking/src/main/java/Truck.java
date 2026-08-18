bff1d1bb26aa4dd912b94fdb318b23cd24b11add
nusra <nusratnishi1103@gmail.com>
2026-08-18 10:07:24 +0600

    first

---
+++ Lab-Final/Car-Parking/src/main/java/Truck.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public abstract class Truck extends Vehicle{
+    public Truck(String plate,DiscountScheme scheme){
+        super(plate, scheme);
+    }
+
+    @Override
+    public List<SlotType> acceptanceOrder(){
+        return List.of(SlotType.LARGE);
+    }
+}
