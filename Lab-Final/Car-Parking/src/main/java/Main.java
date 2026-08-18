4573efd19f62fc46b37f563b03cdec35ad85b286
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-08-18 09:52:14 +0600

    updating main

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -36,8 +39,78 @@ public class Main {
             this.discountType = discountType;
             this.entryHour = entryHour;
         }
+
+        int calculateBill(int currentHour, int maxStay) {
+            int totalHour = currentHour - entryHour;
+
+            int firstHourFee = switch (slotType) {
+                case BIKE -> 10;
+                case REGULAR -> 30;
+                case LARGE -> 50;
+            };
+
+            int furtherHourFee = switch (slotType) {
+                case BIKE -> 5;
+                case REGULAR -> 20;
+                case LARGE -> 40;
+            };
+
+            int surcharge = switch (slotType) {
+                case BIKE -> 0;
+                case REGULAR -> 5;
+                case LARGE -> 15;
+            };
+
+            int totalFee = firstHourFee + (totalHour - 1)*furtherHourFee + surcharge;
+
+            double discount = switch (discountType) {
+                case NONE -> 0;
+                case STUDENT -> 0.2 * totalFee;
+                case WEEKEND -> 10;
+            };
+
+            int bill = (int) Math.round(totalFee - discount);
+
+            if (bill < 0) {
+                throw new IllegalArgumentException("Bill can not be less than zero.");
+            }
+
+            return bill;
+        }
     }
 
     public static void main(String[] args){
+
+        List<Vehicle> parkedvehicles = new ArrayList<>();
+
+        int bikeCap = 0, regCap = 0, largeCap = 0;
+        int bikeOcc = 0, regOcc = 0, largeOcc = 0;
+
+        SlotType assigned = null;
+
+
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNextLine()) {
+            String line = input.nextLine().trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            String[] field = line.split(" ");
+
+            if (field[0].equals("END")) {
+                break;
+
+            } else if (field[0].equals("MAXSTAY")) {
+                int hours = Integer.parseInt(field[1]);
+
+
+
+            } else if (field[0].equals("COUNT")) {
+                System.out.println(/* the number you worked out */);
+
+            }
+
+        }
     }
 }
