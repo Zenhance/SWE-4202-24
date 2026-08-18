b08f49bacab66d9faa96813daed0da1fb6c93913
wasif-22 <twasif68@gmail.com>
2026-08-18 11:16:36 +0600

    added constructors and setter  in carpark method

--- Lab-Final/Car-Parking/src/main/java/CarPark.java
+++ Lab-Final/Car-Parking/src/main/java/CarPark.java
@@ -9,4 +9,16 @@ public class CarPark {
     private int refused;
     private final Map<String, Vehicle> vehicles = new HashMap<>();
 
+    public CarPark(int bikeCapacity, int regularCapacity, int largeCapacity, int bikeOccupied, int regularOccipied, int largeOccupied) {
+        this.bikeCapacity = bikeCapacity;
+        this.regularCapacity = regularCapacity;
+        this.largeCapacity = largeCapacity;
+        this.bikeOccupied = bikeOccupied;
+        this.regularOccipied = regularOccipied;
+        this.largeOccupied = largeOccupied;
+    }
+
+    public void setMaxStay(int hours){
+        this.maxStay=maxStay;
+    }
 }
