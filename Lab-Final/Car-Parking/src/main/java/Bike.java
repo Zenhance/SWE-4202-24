13b71de86ba726c6baa3bc17f79e9193b5c14952
Fayaz16 <shahtanzim900@gmail.com>
2026-08-18 11:27:12 +0600

    Bike committed

--- Lab-Final/Car-Parking/src/main/java/Bike.java
+++ Lab-Final/Car-Parking/src/main/java/Bike.java
@@ -1,2 +1,16 @@
-public class Bike {
+package parking;
+public class Bike extends Vehicle{
+    public Bike(String registration,String permit){
+        super(registration,permit);
+    }
+ @Override
+ public VehicleType getType() {
+        return VehicleType.BIKE;
+ }
+ @Override
+    public boolean canPark(SlotType slotType){
+        return slotType == SlotType.BIKE
+                ||slotType == SlotType.Regular;
+ }
+
 }
