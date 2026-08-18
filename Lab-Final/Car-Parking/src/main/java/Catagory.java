c3ac50c5a9d994baa17b09e32a5b0c912dbe0938
Sadman Raiyan <kamrulseed@gmail.com>
2026-08-18 10:26:36 +0600

    Slot class end for now and made Catagory enum for slot

---
+++ Lab-Final/Car-Parking/src/main/java/Catagory.java
@@ -0,0 +1,3 @@
+public enum Catagory {
+    BIKE,REGULAR,LARGE
+}
--- Lab-Final/Car-Parking/src/main/java/Slot.java
+++ Lab-Final/Car-Parking/src/main/java/Slot.java
@@ -23,7 +23,28 @@ public class Slot {
         vehicle.setSlot(this);
     }
 
+    public void free(){
+        if(vehicle != null){
+            vehicle.setSlot(null);
+        }
+        vehicle = null;
+    }
+    public Catagory catagory;
+    public int firstHourRate(){
+        return switch(catagory){
+            case BIKE -> 10;
+            case REGULAR -> 30;
+            case LARGE -> 50;
+        };
+    }
 
+    public int surchargeRate(){
+        return switch(catagory){
+            case BIKE -> 0;
+            case REGULAR -> 15;
+            case LARGE -> 25;
+        };
+    }
 
 
 }
