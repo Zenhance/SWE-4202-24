e8c579d4c7f481daa69e886e1996e557a3ad9ecf
Munman <214246799+bollamokles@users.noreply.github.com>
2026-08-18 11:09:14 +0600

    commit Vehicle again

--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -1,14 +1,31 @@
 public abstract class Vehicle {
     private final String plate;
     private Slot preferredType;
+    private int timeStayed;
+    private int maxTimeStay;
 
     protected Vehicle(String plate) {
         this.plate = plate;
+        this.timeStayed = 0;
     }
 
     public void setPreferredType(Slot preferredType) {
         if (preferredType == null)
             throw new IllegalArgumentException("Preferred slot type cannot be null");
         this.preferredType = preferredType;
+    }
+
+    public int getTimeStayed() {
+        return timeStayed;
+    }
+
+    public void setMaxTimeStay(int maxTimeStay) {
+        if (maxTimeStay <= 0)
+            throw new IllegalArgumentException("Max time stay cannot be neagtive or zero");
+        this.maxTimeStay = maxTimeStay;
+    }
+
+    public int getMaxTimeStay() {
+        return maxTimeStay;
     }
 }
