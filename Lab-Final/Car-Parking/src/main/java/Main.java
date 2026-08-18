5736ff2d74ee0a803837e2ebb7b6f2639acbf5e7
shafin-75 <arafatshafin85@gmail.com>
2026-08-18 11:50:58 +0600

    ..

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -11,39 +11,6 @@ public class Main {
             }
             String[] field = line.split(" ");
             String word = field[0];
-
-            if(word.equals("END")){
-                break;
-            } else if (word.equals("SLOTS")) {
-                int bike = Integer.parseInt(field[1]);
-                int regular = Integer.parseInt(field[2]);
-                int large = Integer.parseInt(field[3]);
-                lot = new ParkingLot(bike,regular,large);
-            }else if(word.equals("MAXSTAY")){
-                lot.setMaxStay(Integer.parseInt(field[1]));
-            }else if(word.equals("BIKE")){
-                lot.arrive(field[1],  ); ///////////////
-            }else if(word.equals("CAR")){
-                lot.arrive(field[1],  ); ////////////
-            }else if(word.equals("TRUCK")){
-                lot.arrive(field[1,  ]); //////////////
-            }else if(word.equals("PASSTIME")){
-                lot.passTime(Integer.parseInt(field[1]));
-            }else if(word.equals("LEAVE")){
-                lot.leave(field[1]);
-            }else if(word.equals("BILL")){
-                System.out.println(lot.bill(field[1]));
-            }else if(word.equals("SLOT")){
-                System.out.println(lot.slotOf(field[1]));
-            }else if(word.equals("FREE")){
-                System.out.println("lot.   ");  ///////////////
-            }else if(word.equals("COUNT")){
-                System.out.println(lot.count());
-            }else if(word.equals("EARNED")){
-                System.out.println(lot.earned());
-            }else if(word.equals("REFUSED")){
-                System.out.println(lot.refused());
-            }
         }
     }
 }
