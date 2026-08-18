bb802be98c49ec330fc8bbc81e48b7c79a24993f
58-jahin <jahinhossain@iut-dhaka.edu>
2026-08-18 11:13:11 +0600

    created WEEKEND DISCOUNT CLASS

--- Lab-Final/Car-Parking/src/main/java/WeekendDiscount.java
+++ Lab-Final/Car-Parking/src/main/java/WeekendDiscount.java
@@ -1,2 +1,7 @@
-public class WeekendDiscount {
+public final class WeekendDiscount implements DiscountScheme {
+
+    @Override
+    public int apply(int bill) {
+        return Math.max(0, bill - 10);
+    }
 }
