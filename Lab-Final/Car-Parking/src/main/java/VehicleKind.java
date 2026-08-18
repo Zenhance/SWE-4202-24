2a8e39738d83c05c93c80251edbe9a6af57091e3
shafin-75 <arafatshafin85@gmail.com>
2026-08-18 10:24:16 +0600

    VehicleKind

--- Lab-Final/Car-Parking/src/main/java/VehicleKind.java
+++ Lab-Final/Car-Parking/src/main/java/VehicleKind.java
@@ -1,2 +1,13 @@
 public class VehicleKind {
+    BIKE(SlotKind.BIKE, new SlotKind[]{SlotKind.BIKE,SlotKind.REGULAR,Slotkind.LARGE}),
+    CAR(SlotKind.REGULAR, new SlotKind[]{SlotKind.REGULAR,Slotkind.LARGE}),
+    TRUCK(SlotKind.LARGE, new SlotKind[]{Slotkind.LARGE});
+
+    public final SlotKind naturalSlot;
+    public final SlotKind[] acceptableSlots;
+
+    VehicleKind(Slotkind naturalSlot, SlotKind[] acceptableSlots){
+        this.naturalSlot=naturalSlot;
+        this.acceptableSlots=acceptableSlots;
+    }
 }
