109576dead06809feab59ad684de4c9e5cff5c15
tuireuit <jamilhasan@iut-dhaka.edu>
2026-08-18 11:32:26 +0600

    parking lot

---
+++ Lab-Final/Car-Parking/src/main/java/Parkinglot.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Parkinglot {
+
+    private int bikeTotal;
+    private int regularTotal;
+    private int largeTotal;
+    private int bikeFree;
+    private int regularFree;
+    private int largeFree;
+
+
+
+}
