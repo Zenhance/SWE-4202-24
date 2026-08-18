e70e7b084c92431f2263760fec832711b9e15be7
Fayaz16 <shahtanzim900@gmail.com>
2026-08-18 11:50:23 +0600

    car and truck committed

--- Lab-Final/Car-Parking/src/main/java/Car.java
+++ Lab-Final/Car-Parking/src/main/java/Car.java
@@ -1,2 +1,15 @@
-public class Car {
+package parking;
+public class Car extends Vehicle{
+    public Car(String registration,String permit) {
+        super(registration, permit);
+    }
+ @Override
+ public VehicleType getType(){
+        return VehicleType.CAR;
+ }
+ @Override
+    public boolean canPark(SlotType slotType){
+        return slotType == SlotType.REGULAR
+                ||slotTYpe == SlotType.LARGE;
+ }
 }
--- Lab-Final/Car-Parking/src/main/java/Truck.java
+++ Lab-Final/Car-Parking/src/main/java/Truck.java
@@ -1,2 +1,17 @@
-public class Truck {
-}
+package parking;
+
+public class Truck extends Vehicle {
+
+    public Truck(String registration, String permit) {
+        super(registration, permit);
+    }
+
+    @Override
+    public VehicleType getType() {
+        return VehicleType.TRUCK;
+    }
+
+    @Override
+    public boolean canPark(SlotType slotType) {
+        return slotType == SlotType.LARGE;
+    }
