b19190392b84cac5096faf1ff7989bf9c85beece
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-08-18 11:41:37 +0600

    added method for discount

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+    public static Discount makeDiscount(String x){
+        if(x.equals("STUDENT")){
+            return new StudentDiscount();
+        }
+        else if(x.equals("WEEKEND")){
+            return new WeekendDiscount();
+        }
+        else{
+            return new NoDiscount();
+        }
+    }
     public static void main(String[] args){
 
     }
