bd1ff6686e40d6107afca966af313da1c876d718
tasnitaraida <tasnitar@gmail.com>
2026-08-18 10:09:18 +0600

    vehicle class

--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -4,7 +4,7 @@ public class Vehicle {
     private slotType slot;
     private schemeType scheme;
 
-    public vehicle(String plate, int time,slotType slot, schemeType scheme) {
+    public Vehicle(String plate, int time,slotType slot, schemeType scheme) {
         this.plate=plate;
         this.time=0;
         this.slot=slot;
