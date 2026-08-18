4d2ce8b671bbfb129feb7bda3e22677518868d42
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-18 12:15:46 +0600

    init commit

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-                Scanner input = new Scanner(System.in);
-                Parkinglot lot = new Parkinglot();
-    }
-    while(input.hasNextLine()){
-        String line = input.nextLine().trim();
-        if (line.isEmpty()) {
-            continue;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Parkinglot lot = new Parkinglot();
+
+        while (input.hasNextLine()) {
+            String line = input.nextLine().trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            String[] field = line.split(" ");
+            String command = field[0];
         }
-        String[] field = line.split(" ");
-        String command = field[0];
+        if (command.equals("END")) {
+            break;
+        } else if (command.equals("SLOTS")) {
+            int bike = Integer.parseInt(field[1]);
+            int regular = Integer.parseInt(field[2]);
+            int large = Integer.parseInt(field[3]);
+            lot.setSlots(bike, regular, large);
+        }
     }
 }
