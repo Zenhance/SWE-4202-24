b7ac9bf77c0e2d8950263ae8e228987dbe886d29
Rakib <rakibulhasan4266@gmail.com>
2026-08-18 09:15:41 +0600

    custom exception for refuses car parking

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
+class BlankPlateException extends Exception
+{
+
+}
+class NoSlotAvailableException extends Exception
+{
+
+}
+class VehicleNotFoundException extends Exception
+{
+
+}
+
+
+
 public class Main {
     public static void main(String[] args)
     {
