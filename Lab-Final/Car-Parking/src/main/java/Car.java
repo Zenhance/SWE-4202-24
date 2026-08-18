f3d6e4f28319f71ab8e6223b0f731c63d8740304
shafin-75 <arafatshafin85@gmail.com>
2026-08-18 11:27:19 +0600

    Car

---
+++ Lab-Final/Car-Parking/src/main/java/Car.java
@@ -0,0 +1,8 @@
+public class Car extends Vehicle{
+    public Car(String plate, Scheme scheme){
+        super(plate,scheme);
+    }
+    SlotKind[] acceptOrder(){
+        return new SlotKind[]{SlotKind.LARGE,SlotKind.REGULAR};
+    }
+}
