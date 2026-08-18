4b74c447ff711fc5fc52985f19ff4e4dc4724c1f
tryhardFuad <mubtasimsmail@gmail.com>
2026-08-18 10:43:31 +0600

    implemented how Main will handle SLOTS

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -14,9 +14,11 @@ public class Main {
 
             if (field[0].equals("END")) {
                 break;
+            } else if (field[0].equals("SLOTS")) {
+                ParkingLot parkingLot = new ParkingLot(Integer.parseInt(field[1]), Integer.parseInt(field[2]), Integer.parseInt(field[3]));
             } else if (field[0].equals("MAXSTAY")) {
                 int hours = Integer.parseInt(field[1]);
-                // ... do something with hours ...
+                
             } else if (field[0].equals("COUNT")) {
                 System.out.println(/* the number you worked out */);
             }
