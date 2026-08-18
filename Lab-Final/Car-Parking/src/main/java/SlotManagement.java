a8fe33064dc4689dd4170234a377db098eda63d1
Sadman Raiyan <kamrulseed@gmail.com>
2026-08-18 11:39:13 +0600

    SlotManagement class added

---
+++ Lab-Final/Car-Parking/src/main/java/SlotManagement.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlotManagement {
+    private List<Slot> bikeSlots;
+    private List<Slot> regularSlots;
+    private List<Slot> largeSlots;
+
+    public SlotManagement(int bike,int regular,int large){
+        bikeSlots=new ArrayList<>();
+        regularSlots=new ArrayList<>();
+        largeSlots=new ArrayList<>();
+    }
+
+
+}
