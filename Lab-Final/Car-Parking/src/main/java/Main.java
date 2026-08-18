f35e2967e8db2acedf1d583cc1d779901abee268
hossdia <hossainmahdia22@gmail.com>
2026-08-18 13:18:19 +0600

    solution done

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -26,7 +26,64 @@ public class Main {
                 maxStay = Integer.parseInt(field[1]);
                 parkingLot = new ParkingLot(bikeSlots, regularSlots, largeSlots, maxStay);
             }
+            else {
+                try {
+                    switch (field[0]) {
+                        case "BIKE":
+                            Vehicle bike = new Motorcycle(field[1], field[2]);
+                            parkingLot.arrive(bike);
+                            break;
 
+                        case "CAR":
+                            Vehicle car = new Car(field[1], field[2]);
+                            parkingLot.arrive(car);
+                            break;
+
+                        case "TRUCK":
+                            Vehicle truck = new Truck(field[1], field[2]);
+                            parkingLot.arrive(truck);
+                            break;
+
+                        case "PASSTIME":
+                            int hours = Integer.parseInt(field[1]);
+                            parkingLot.passTime(hours);
+                            break;
+
+                        case "LEAVE":
+                            parkingLot.leave(field[1]);
+                            break;
+
+                        case "BILL":
+                            System.out.println(parkingLot.bill(field[1]));
+                            break;
+
+                        case "SLOT":
+                            System.out.println(parkingLot.slot(field[1]));
+                            break;
+
+                        case "FREE":
+                            System.out.println(parkingLot.free(field[1]));
+                            break;
+
+                        case "COUNT":
+                            System.out.println(parkingLot.count());
+                            break;
+
+                        case "EARNED":
+                            System.out.println(parkingLot.getEarned());
+                            break;
+
+                        case "REFUSED":
+                            System.out.println(parkingLot.getRefused());
+                            break;
+                    }
+                } catch (ParkingException e) {
+                    parkingLot.addRefusal();
+                    if (field[0].equals("BILL") || field[0].equals("SLOT")) {
+                        System.out.println("NONE");
+                    }
+                }
+            }
         }
         }
     }
