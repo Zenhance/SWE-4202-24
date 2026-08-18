930fe6ee5304186f6b9b99cedecfafd9ea934001
ahnaf-code <ahnafcode@gmail.com>
2026-08-18 12:25:35 +0600

    Write SlotKind

--- Lab-Final/Car-Parking/src/main/java/SlotKind.java
+++ Lab-Final/Car-Parking/src/main/java/SlotKind.java
@@ -1,5 +1,27 @@
 public enum SlotKind {
-    BIKE,
-    REGULAR,
-    LARGE
+    BIKE(10, 5, 0),
+    REGULAR(30, 20, 15),
+    LARGE(50, 40, 25);
+
+    private final int firstHourFee;
+    private final int furtherHourFee;
+    private final int surcharge;
+
+    SlotKind(int firstHourFee, int furtherHourFee, int surcharge) {
+        this.firstHourFee = firstHourFee;
+        this.furtherHourFee = furtherHourFee;
+        this.surcharge = surcharge;
+    }
+
+    public int firstHourFee() {
+        return firstHourFee;
+    }
+
+    public int furtherHourFee() {
+        return furtherHourFee;
+    }
+
+    public int surcharge() {
+        return surcharge;
+    }
 }
