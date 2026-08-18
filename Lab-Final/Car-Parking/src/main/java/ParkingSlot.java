31b7486915e1ab0592066653319b0f15a2b43524
sabbir240042147 <eftesabbir2725@gmail.com>
2026-08-18 12:31:49 +0600

    parking slote

---
+++ Lab-Final/Car-Parking/src/main/java/ParkingSlot.java
@@ -0,0 +1,10 @@
+public class ParkingSlot {
+    private int bikeslots;
+    private int regularSlots;
+    private int largeSlots;
+    public ParkingSlot(int bikeslots,int regularSlots,int largeSlots){
+        this.bikeslots=bikeslots;
+        this.regularSlots=regularSlots;
+        this.largeSlots=largeSlots;
+    }
+}
