18815d64b6b9f70bfa97f022dba7e34cbd80d36b
Fayaz16 <shahtanzim900@gmail.com>
2026-08-18 11:16:16 +0600

    Vehicles committed

--- Lab-Final/Car-Parking/src/main/java/Vehicles.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicles.java
@@ -1,7 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
+package parking;
+public abstract class Vehicle {
+    private String registration;
+    private String permit;
 
-public class Vehicles {
-    String plate;
-    String slot;
+
+    public Vehicle(String registration, String permit) {
+        this.registration = registration;
+        this.permit = permit;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public String getPermit() {
+        return permit;
+    }
+
+    public abstract VehicleType getType();
+
+    public abstract boolean canPark(SlotType slotType);
 }
