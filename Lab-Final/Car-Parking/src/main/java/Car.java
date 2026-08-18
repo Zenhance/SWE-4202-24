95ce5e0c97117472f726c9784a32072995f25ebb
58-jahin <jahinhossain@iut-dhaka.edu>
2026-08-18 10:59:55 +0600

    CREATED CAR WITH NECESSARY CONTRUCTORS AND METHODS

--- Lab-Final/Car-Parking/src/main/java/Car.java
+++ Lab-Final/Car-Parking/src/main/java/Car.java
@@ -1,4 +1,18 @@
-public class Car {
-    public Car(String plate, DiscountScheme scheme) {
+import java.util.List;
+
+public final class Car extends Vehicle {
+    public Car(
+            String plate,
+            DiscountScheme discount
+    ) {
+        super(plate, discount);
+    }
+
+    @Override
+    public List<SlotKind> acceptedSlots() {
+        return List.of(
+                SlotKind.REGULAR,
+                SlotKind.LARGE
+        );
     }
 }
