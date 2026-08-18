df93b4a54f71c77e7beb54b994a8d91b9af8e920
md-musaddik <musaddikibna1@gmail.com>
2026-08-18 10:35:20 +0600

    Scanner Declared.

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -2,18 +2,21 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    int bikeSlots,regularSlots,largeSlots;
+    int freeBike, freeRegular, freeLarge;
+
+    ArrayList<String> plates = new ArrayList<>();
+    ArrayList<String> types = new ArrayList<>();
+    ArrayList<String> discounts = new ArrayList<>();
+    ArrayList<String> parkedIn = new ArrayList<>();
+    ArrayList<Integer> hoursParked = new ArrayList<>();
+
+    int maxStay;
+    int totalEarned = 0;
+
     public static void main(String[] args){
-        int bikeSlots,regularSlots,largeSlots;
-        int freeBike, freeRegular, freeLarge;
 
-        ArrayList<String> plates = new ArrayList<>();
-        ArrayList<String> types = new ArrayList<>();
-        ArrayList<String> discounts = new ArrayList<>();
-        ArrayList<String> parkedIn = new ArrayList<>();
-        ArrayList<Integer> hoursParked = new ArrayList<>();
-
-        int maxStay;
-        int totalEarned = 0;
+        Scanner input = new Scanner(System.in);
 
     }
 }
