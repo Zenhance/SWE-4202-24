7aef54c683741715856b039a33eddc25a188c5a9
tryhardFuad <mubtasimsmail@gmail.com>
2026-08-18 12:21:58 +0600

    Add addVehicle method for ParkingLot class

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         ParkingLot parkingLot = null;
-
+        Bike bike = null;
+        Car car = null;
+        Truck truck = null;
         while (input.hasNextLine()) {
             String line = input.nextLine().trim();
             if (line.isEmpty()) {
@@ -19,19 +21,22 @@ public class Main {
                 parkingLot = new ParkingLot(Integer.parseInt(field[1]), Integer.parseInt(field[2]), Integer.parseInt(field[3]));
             } else if (field[0].equals("MAXSTAY")) {
                 int hours = Integer.parseInt(field[1]);
-                
+                parkingLot.setMaxHours(hours);
             } else if (field[0].equals("BIKE")) {
                 String plate = field[1];
                 String discount =  field[2];
-                Bike bike = new Bike(plate, discount);
+                bike = new Bike(plate, discount);
+                parkingLot.addVehicle(bike);
             }else if (field[0].equals("CAR")) {
                 String plate = field[1];
                 String discount =  field[2];
-                Car car = new Car(plate, discount);
+                car = new Car(plate, discount);
+                parkingLot.addVehicle(car);
             }else if (field[0].equals("TRUCK")) {
                 String plate = field[1];
                 String discount =  field[2];
-                Truck truck = new Truck(plate, discount);
+                truck = new Truck(plate, discount);
+                parkingLot.addVehicle(truck);
             }else if (field[0].equals("PASSTIME")) {
                 int hours = Integer.parseInt(field[1]);
             }else if (field[0].equals("LEAVE")) {
