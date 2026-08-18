002357c8f8c0ebbf285c58435f35583db3abc75d
58-jahin <jahinhossain@iut-dhaka.edu>
2026-08-18 11:03:51 +0600

    CREATED Motorcycle WITH NECESSARY CONTRUCTORS AND METHODS

--- Lab-Final/Car-Parking/src/main/java/Motorcycle.java
+++ Lab-Final/Car-Parking/src/main/java/Motorcycle.java
@@ -1,4 +1,16 @@
-public class Motorcycle {
-    public Motorcycle(String plate, DiscountScheme scheme) {
+import java.util.List;
+
+public final class Motorcycle extends Vehicle {
+    public Motorcycle(String plate, DiscountScheme discount
+    ) {super(plate, discount);
+    }
+
+    @Override
+    public List<SlotKind> acceptedSlots() {
+        return List.of(
+                SlotKind.BIKE,
+                SlotKind.REGULAR,
+                SlotKind.LARGE
+        );
     }
 }
