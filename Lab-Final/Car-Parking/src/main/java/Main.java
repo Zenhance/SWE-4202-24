ad2af0afc306a902d252fb003e71a0c8f2f0a253
md-musaddik <musaddikibna1@gmail.com>
2026-08-18 11:00:58 +0600

    Refused Count attributes declared.

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -14,6 +14,7 @@ public class Main {
     static ArrayList<Integer> hoursParked = new ArrayList<>();
 
     static int maxStay;
+    static int refusedCount = 0;
     static int totalEarned = 0;
 
     public static void main(String[] args){
