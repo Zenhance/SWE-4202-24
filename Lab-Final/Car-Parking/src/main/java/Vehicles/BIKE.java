4c41160e3bb11f3a749791a534f6c3544021733d
Rakin <rakinhasan001@gmail.com>
2026-08-18 11:34:28 +0600

    vehicle classes designed

--- Lab-Final/Car-Parking/src/main/java/Vehicles/BIKE.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicles/BIKE.java
@@ -1,13 +1,25 @@
 package Vehicles;
-import Slots.*;
+import Refusals.NoPlateException;
 import Refusals.NoSpaceAvailableException;
+import Slots.*;
+import Records.*;
+import Slots.slotCount;
+
 import static Records.slotCount.Count;
 import static Records.slotCount.RefuseCount;
 
-public class TRUCK implements Discountable{
+public class BIKE implements Discountable{
     String numPlate;
     String scheme;
-    if(){
+    if(bikeCount()>0){
+        Bike slot = new Bike();
+        Count();
+    }
+    else if(regularCount()>0){
+        Regular slot=new Regular();
+        Count();
+    }
+    else if(largeCount()>0){
         Large slot=new Large();
         Count();
     }
@@ -16,11 +28,9 @@ public class TRUCK implements Discountable{
         throw new NoSpaceAvailableException("No Space is Availabe");
 
     }
-
     public void setNumPlate(String numPlate) {
         this.numPlate = numPlate;
     }
-
     public void setScheme(String scheme) {
         this.scheme = scheme;
     }
--- Lab-Final/Car-Parking/src/main/java/Vehicles/CAR.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicles/CAR.java
@@ -1,13 +1,18 @@
 package Vehicles;
-import Slots.*;
 import Refusals.NoSpaceAvailableException;
+import Slots.*;
+
 import static Records.slotCount.Count;
 import static Records.slotCount.RefuseCount;
 
-public class TRUCK implements Discountable{
+public class CAR implements Discountable{
     String numPlate;
     String scheme;
     if(){
+        Regular slot=new Regular();
+        Count();
+    }
+    else if(){
         Large slot=new Large();
         Count();
     }
@@ -16,11 +21,9 @@ public class TRUCK implements Discountable{
         throw new NoSpaceAvailableException("No Space is Availabe");
 
     }
-
     public void setNumPlate(String numPlate) {
         this.numPlate = numPlate;
     }
-
     public void setScheme(String scheme) {
         this.scheme = scheme;
     }
