59b26f1da24bd694480dd451e8e3101cb2889217
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-18 10:42:40 +0600

    init commit

--- Lab-Final/Car-Parking/src/main/java/Parkinglot.java
+++ Lab-Final/Car-Parking/src/main/java/Parkinglot.java
@@ -1,7 +1,18 @@
+import java.util.HashMap;
+
 public class Parkinglot {
     //total slots
     private int bikeTotal;
     private int regularTotal;
     private int largeTotal;
+    //slots still free
+    private int bikeFree;
+    private int regularFree;
+    private int largeFree;
 
+    private HashMap<String,Vehicle>parkedVehicles = new HashMap<>();
+    private int maxStayhrs;
+    private int time=0;
+    private int refusedCount=0;
+    private int totalEarned=0;
 }
