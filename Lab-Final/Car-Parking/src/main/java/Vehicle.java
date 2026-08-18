9b6d187a80bb8e6964e3ff3284cf593f1fe44227
wasi <waseesomrat8@gmail.com>
2026-08-18 09:44:03 +0600

    constructor made for vehicle

--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -1,7 +1,13 @@
 public abstract class Vehicle{
     private final String Plate;
-    private final String scheme;
+    private final String Scheme;
     private int hours;
     private ParkingSlot slot;
+}
+
+public Vehicle(String Plate,String Scheme){
+     this.Plate = PLate;
+     this.Scheme = Plate;
+     this.hour = hour;
 }
 
