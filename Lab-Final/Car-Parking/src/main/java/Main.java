1081402d66a538f1d252d272aeb93093c2b70f1e
md-musaddik <musaddikibna1@gmail.com>
2026-08-18 10:23:32 +0600

    Some Array list's declared for types and storing informations like plates, hoursparked etc

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         int bikeSlots,regularSlots,largeSlots;
         int freeBike, freeRegular, freeLarge;
+
+        ArrayList<String> plates = new ArrayList<>();
+        ArrayList<String> types = new ArrayList<>();
+        ArrayList<String> discounts = new ArrayList<>();
+        ArrayList<String> parkedIn = new ArrayList<>();
+        ArrayList<Integer> hoursParked = new ArrayList<>();
+
     }
 }
