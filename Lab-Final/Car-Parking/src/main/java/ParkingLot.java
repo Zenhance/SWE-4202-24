2169ac8e13108f239bdd5c085152f33dee31cad4
ayiham-1012 <ayiham1012@gmail.com>
2026-08-18 10:52:14 +0600

    maybe "slot" is just a method of ParkingLot

--- Lab-Final/Car-Parking/src/main/java/ParkingLot.java
+++ Lab-Final/Car-Parking/src/main/java/ParkingLot.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class ParkingLot {
     int bikeSlot;
     int regularSlot;
     int largeSlot;
+    static ArrayList<Vehicle> vehicles = new ArrayList<>();
 
     public ParkingLot(int bikeSlot, int regularSlot, int largeSlot) {
         this.bikeSlot = bikeSlot;
@@ -9,5 +12,19 @@ public class ParkingLot {
         this.largeSlot = largeSlot;
     }
 
+    public void enterVehicle(Vehicle v){
+        vehicles.add(v);
+    }
+
+    public static Vehicle search(String number){
+        for(Vehicle v: vehicles){
+            if(number.equals(v.number)) return v;
+        }
+        return null;
+    }
+
+    public void slotVehicle (String number){
+
+    }
 
 }
--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -5,7 +5,7 @@ public abstract class Vehicle {
     protected String number;
     protected Scheme scheme;
     public static int maxHours;
-    static ArrayList<Vehicle> vehicles = new ArrayList<>();
+
 
     Vehicle(String number, String schemeS) {
         if (number.isBlank()) throw new IllegalArgumentException("No number-plate");
@@ -16,19 +16,14 @@ public abstract class Vehicle {
             case "WEEKEND" -> this.scheme = Scheme.WEEKEND;
             default -> this.scheme = Scheme.NONE;
         }
-        vehicles.add(this);
+
     }
 
     Vehicle(String number){
         this(number, "NONE");
     }
 
-    public static Vehicle search(String number){
-        for(Vehicle v: vehicles){
-            if(number.equals(v.number)) return v;
-        }
-        return null;
-    }
+
 
 
 
