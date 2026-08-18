8fd8ac1bddb9f823f9c8d206ece5d933e2468539
shafin-75 <arafatshafin85@gmail.com>
2026-08-18 13:11:30 +0600

    SOLUTION DONE

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -3,12 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        While(input.hasNextLine()){
-            String Line = input.nextLine().trim();
-            if(line.isEmpty()){
-                continue;
-            }
-            String[] field = line.split(" ");
-        }
+        
     }
 }
