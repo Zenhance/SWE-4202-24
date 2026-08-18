04db5c544c9882d7daf8e6e1f8956168c23669d9
Tasauf Abdullah <tasaufalter@gmail.com>
2026-08-18 10:27:27 +0600

    add body to StudentDiscount class

--- Lab-Final/Car-Parking/src/main/java/StudentDiscount.java
+++ Lab-Final/Car-Parking/src/main/java/StudentDiscount.java
@@ -1,2 +1,6 @@
-public class StudentDiscount implements DiscountScheme{
+public  class StudentDiscount implements DiscountScheme {
+    @Override
+    public int applyDiscount(int bill) {
+        return bill - (20 * bill / 100);
+    }
 }
