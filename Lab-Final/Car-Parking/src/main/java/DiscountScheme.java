d5cdda493c823d4e06557e7231c440ecbcc64da2
Inan <inanjaki184@gmail.com>
2026-08-18 10:09:56 +0600

    2

---
+++ Lab-Final/Car-Parking/src/main/java/DiscountScheme.java
@@ -0,0 +1,21 @@
+public enum DiscountScheme {
+    NONE{
+        public int apply(int bill){
+            return bill;
+        }
+    },
+    STUDENT{
+        public int apply(int bill){
+            int discount=(20*bill)/100;
+            return bill-discount;
+        }
+    },
+    WEEKEND{
+        public int apply(int bill){
+            int discounted=bill-10;
+            return Math.max(0,discounted);
+        }
+    };
+    public abstract int apply(int bill);
+}
+
