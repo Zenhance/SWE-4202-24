d5f5caff67daa251ed3b0b3868e2e72189573bd7
md-musaddik <musaddikibna1@gmail.com>
2026-08-18 11:02:56 +0600

    Condition for refused count declared.

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -48,6 +48,8 @@ public class Main {
 
             }else if (firstWord.equals("COUNT")) {
 
+            }else if(firstWord.equals("REFUSED")){
+                System.out.println(refusedCount);
             }
 
 
