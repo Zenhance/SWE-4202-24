a632e03c59f8f397833a9994e36d1ac84fee0f42
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-08-18 12:42:21 +0600

    in search of correct outputs

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -156,6 +156,10 @@ public class Main {
                     refused++;
                 }
 
+                if (numPlate==null) {
+                    refused++;
+                }
+
             }
 
 
