67fee8d5b9900eed5b577e7d9559fa08bb6c19af
Rakin <rakinhasan001@gmail.com>
2026-08-18 12:53:39 +0600

    slotcount class added in record package

--- Lab-Final/Car-Parking/src/main/java/Records/slotCount.java
+++ Lab-Final/Car-Parking/src/main/java/Records/slotCount.java
@@ -1,23 +1,60 @@
 package Records;
 
 public class slotCount {
-    private int bike;
-    private int regular;
-    private int large;
-    public static int vehicleCount;
-    public int bikeCount(){
+    private static  int bike;
+    private static int regular;
+    private static int large;
+    public static int vehicleCount=0;
+    public static int refuseCount=0;
+    public static int getBikeCount(){
         return bike;
     }
-    public int regularCount(){
+    public static int getRegularCount(){
         return regular;
     }
-    public int largeCount(){
+    public static int getLargeCount(){
         return large;
     }
     public static void Count(){
         vehicleCount++;
     }
+    public static void RefuseCount(){
+        refuseCount++;
+    }
     public static int getCount(){
         return vehicleCount;
+    }
+    public static int getRefuseCount(){
+        return refuseCount;
+    }
+
+    public static void setBike(int bike) {
+        slotCount.bike = bike;
+    }
+
+    public static void setRegular(int regular) {
+        slotCount.regular = regular;
+    }
+
+    public static void setLarge(int large) {
+        slotCount.large = large;
+    }
+    public static void addBike(){
+        bike++;
+    }
+    public static void addRegular(){
+        regular++;
+    }
+    public static void addLarge(){
+        large++;
+    }
+    public static void removeBike(){
+        bike-- ;
+    }
+    public static void removeRegular(){
+        regular-- ;
+    }
+    public static void removeLarge(){
+        large-- ;
     }
 }
