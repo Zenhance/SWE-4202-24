01670d8de15ad32e5cb8da77c5867f8e220b1fe8
ayeshaa-mahjabin <ayeshamahjabin27@gmail.com>
2026-08-18 12:36:46 +0600

    fixed error

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -12,68 +12,68 @@ public class Main {
             }
             String[] field = line.split(" ");
             String command = field[0];
-        }
-        if (command.equals("END")) {
-            break;
-        } else if (command.equals("SLOTS")) {
-            int bike = Integer.parseInt(field[1]);
-            int regular = Integer.parseInt(field[2]);
-            int large = Integer.parseInt(field[3]);
-            lot.setSlots(bike, regular, large);
-        } else if (command.equals("MAXSTAY")) {
-            int hours = Integer.parseInt(field[1]);
-            lot.setMaxStay(hours);
 
-        } else if (command.equals("BIKE")) {
-            String plate = field[1];
-            Category category = Category.valueOf(field[2]);
-            Vehicle vehicle = new Bike(plate, category);
-            lot.register(vehicle);
-        } else if (command.equals("CAR")) {
-            String plate = field[1];
-            Category category = Category.valueOf(field[2]);
-            Vehicle vehicle = new Car(plate, category);
-            lot.register(vehicle);
+            if (command.equals("END")) {
+                break;
+            } else if (command.equals("SLOTS")) {
+                int bike = Integer.parseInt(field[1]);
+                int regular = Integer.parseInt(field[2]);
+                int large = Integer.parseInt(field[3]);
+                lot.setSlots(bike, regular, large);
+            } else if (command.equals("MAXSTAY")) {
+                int hours = Integer.parseInt(field[1]);
+                lot.setMaxStay(hours);
 
-        } else if (command.equals("TRUCK")) {
-            String plate = field[1];
-            Category category = Category.valueOf(field[2]);
-            Vehicle vehicle = new Truck(plate, category);
-            lot.register(vehicle);
-        } else if (command.equals("SLOT")) {
-            SlotType slot = lot.slotOf(field[1]);
-            if (slot == null) {
-                System.out.println("NONE");
-            } else {
-                System.out.println(slot.name());
-            }
-         else if (command.equals("FREE")) {
-                SlotType type = SlotType.valueOf(field[1]);
-                System.out.println(lot.freeCount(type));
+            } else if (command.equals("BIKE")) {
+                String plate = field[1];
+                Category category = Category.valueOf(field[2]);
+                Vehicle vehicle = new Bike(plate, category);
+                lot.register(vehicle);
+            } else if (command.equals("CAR")) {
+                String plate = field[1];
+                Category category = Category.valueOf(field[2]);
+                Vehicle vehicle = new Car(plate, category);
+                lot.register(vehicle);
 
-            } else if (command.equals("COUNT")) {
-                System.out.println(lot.parkedCount());
-
-            } else if (command.equals("REFUSED")) {
-                System.out.println(lot.refusedCount());
-
-            } else if (command.equals("BILL")) {
-                Integer bill = lot.billFor(field[1]);
-                if (bill == null) {
+            } else if (command.equals("TRUCK")) {
+                String plate = field[1];
+                Category category = Category.valueOf(field[2]);
+                Vehicle vehicle = new Truck(plate, category);
+                lot.register(vehicle);
+            } else if (command.equals("SLOT")) {
+                SlotType slot = lot.slotOf(field[1]);
+                if (slot == null) {
                     System.out.println("NONE");
                 } else {
-                    System.out.println(bill);
+                    System.out.println(slot.name());
                 }
+                if (command.equals("FREE")) {
+                    SlotType type = SlotType.valueOf(field[1]);
+                    System.out.println(lot.freeCount(type));
 
-            } else if (command.equals("PASSTIME")) {
-                int hours = Integer.parseInt(field[1]);
-                lot.advanceTime(hours);
-            }
-            else if (command.equals("LEAVE")) {
-                lot.leave(field[1]);
+                } else if (command.equals("COUNT")) {
+                    System.out.println(lot.parkedCount());
 
-            } else if (command.equals("EARNED")) {
-                System.out.println(lot.totalEarned());
+                } else if (command.equals("REFUSED")) {
+                    System.out.println(lot.refusedCount());
+
+                } else if (command.equals("BILL")) {
+                    Integer bill = lot.billFor(field[1]);
+                    if (bill == null) {
+                        System.out.println("NONE");
+                    } else {
+                        System.out.println(bill);
+                    }
+
+                } else if (command.equals("PASSTIME")) {
+                    int hours = Integer.parseInt(field[1]);
+                    lot.advanceTime(hours);
+                } else if (command.equals("LEAVE")) {
+                    lot.leave(field[1]);
+
+                } else if (command.equals("EARNED")) {
+                    System.out.println(lot.totalEarned());
+                }
             }
         }
     }
