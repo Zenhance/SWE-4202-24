91500f11469c7936b589bd1c0cc4555211107eea
wasif-22 <twasif68@gmail.com>
2026-08-18 11:11:27 +0600

    added fields in carpark class

---
+++ Lab-Final/Car-Parking/src/main/java/CarPark.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CarPark {
+    private int bikeCapacity, regularCapacity, largeCapacity;
+    private int bikeOccupied, regularOccipied, largeOccupied;
+    private int maxStay;
+    private int earned;
+    private int refused;
+    private final Map<String, Vehicle> vehicles = new HashMap<>();
+
+}
