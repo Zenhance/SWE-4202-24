1e93017c4ba8f99a18123e8bf02226635a882071
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-08-18 10:27:56 +0600

    created a constructor

--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -1,2 +1,12 @@
-public class Vehicle {
+public abstract class Vehicle {
+    private final String registration;
+    private final String pass;
+    private String assignedSlot;
+    private int entryTime;
+    private boolean oversized;
+    public Vehicle(String registration, String pass) {
+        this.registration = registration;
+        this.pass = pass;
+    }
+
 }
