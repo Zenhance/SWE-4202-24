4077c59ef48a4711c29669578af8ccc5ebacc250
Your Name <you@example.com>
2026-08-18 10:27:05 +0600

    vehicle

--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -2,13 +2,13 @@ import java.util.ServiceLoader;
 
 public class Vehicle {
     private final String Vehicleplate;
-    private final int hourEntered;
+    private int time;
     private final Category category;
-    private final slotType keptslot;
+    private slotType keptslot;
 
-    public Vehicle(String vehicleplate, int hourEntered, Category category, slotType keptslot) {
+    public Vehicle(String vehicleplate, int time, Category category, slotType keptslot) {
         this.Vehicleplate = vehicleplate;
-        this.hourEntered = hourEntered;
+        this.time = time;
         this.category = category;
         this.keptslot = keptslot;
     }
@@ -17,8 +17,8 @@ public class Vehicle {
     return Vehicleplate;
     }
 
-    public int getHourEntered(){
-        return hourEntered;
+    public int getTime(){
+        return time;
     }
 
     public slotType getKeptslot() {
@@ -28,4 +28,12 @@ public class Vehicle {
     public Category getCategory(){
         return category;
     }
+
+    public void park(slotType keptslot,int time){
+        this.keptslot=keptslot;
+        this.time=time;
+    }
+    public abstract slotType[] slotpreference();
+    public abstract int hourlyRate();
 }
+
