807a932a075d16536333b3399d4a539958d6a3b6
tanbirulislam-hash <tanbirulislam@iut-dhaka.edu>
2026-08-18 11:10:57 +0600

    Completed SlotType

---
+++ Lab-Final/Car-Parking/src/main/java/SlotType.java
@@ -0,0 +1,29 @@
+public enum SlotType {
+
+    BIKE(10, 5, 0),
+    REGULAR(30, 20, 15),
+    LARGE(50, 40, 25);
+
+
+
+    private final int surcharge;
+    private final int firstHourFee;
+    private final int furtherHourFee;
+
+
+    SlotType(int firstHourFee,int furtherHourFee,int surcharge) {
+        this.firstHourFee=firstHourFee;
+        this.furtherHourFee=furtherHourFee;
+        this.surcharge=surcharge;
+    }
+
+    public int getFirstHourFee() {
+        return firstHourFee;
+    }
+    public int getFurtherHourFee() {
+        return furtherHourFee;
+    }
+    public int getSurcharge() {
+        return surcharge;
+    }
+}
