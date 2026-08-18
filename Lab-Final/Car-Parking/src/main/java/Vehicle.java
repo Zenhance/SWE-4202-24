fe419120eef4ebfc44421423356ce11d3ca75efe
wasifjafar <wasifjafar71@gmail.com>
2026-08-18 09:53:14 +0600

    create Vehicle class's preferredType field's setter

--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -5,4 +5,10 @@ public abstract class Vehicle {
     protected Vehicle(String plate) {
         this.plate = plate;
     }
+
+    public void setPreferredType(Slot preferredType) {
+        if (preferredType == null)
+            throw new IllegalArgumentException("Preferred slot type cannot be null");
+        this.preferredType = preferredType;
+    }
 }
