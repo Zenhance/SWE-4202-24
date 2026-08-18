6b34242bcc259387d262969c87aca9e38a73150e
wasifjafar <wasifjafar71@gmail.com>
2026-08-18 10:09:46 +0600

    add Slot class's fee fields and their setters

--- Lab-Final/Car-Parking/src/main/java/Slot.java
+++ Lab-Final/Car-Parking/src/main/java/Slot.java
@@ -1,7 +1,29 @@
 public abstract class Slot {
     private boolean isFree;
+    private int firstHourFee;
+    private int laterFee;
+    private int surcharge;
+
 
     public Slot() {
         this.isFree = true;
+    }
+
+    public void setFirstHourFee(int firstHourFee) {
+        if (firstHourFee <= 0)
+            throw new IllegalArgumentException("First hour fee cannot negative or zero");
+        this.firstHourFee = firstHourFee;
+    }
+
+    public void setLaterFee(int laterFee) {
+        if (laterFee <= 0)
+            throw new IllegalArgumentException("Later fee cannot be negative or zero");
+        this.laterFee = laterFee;
+    }
+
+    public void setSurcharge(int surcharge) {
+        if (surcharge < 0)
+            throw new IllegalArgumentException("Surcharge cannot be negative but can be zero");
+        this.surcharge = surcharge;
     }
 }
