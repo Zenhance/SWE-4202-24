20f96cc0bad8225d421601c184dd3c8d250ea85c
Md. Tanvir Rahman Hridoy <tanvirrahman36@iut-dhaka.edu>
2026-08-18 12:38:07 +0600

    build : complete the Main class [1]

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,7 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    private static int maxBike = 0;
+    private static int maxRegular = 0;
+    private static int maxLarge = 0;
+
+    private static List<Slot> activeSlots = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String opt = scanner.next();
+
+            switch (opt) {
+                case "SLOTS" -> {
+
+                    maxBike = scanner.nextInt();
+
+                    maxRegular = scanner.nextInt();
+
+                    maxLarge = scanner.nextInt();
+
+                }
+                case "MAXSTAY" -> {
+                    int hours = scanner.nextInt();
+                    Slot.setMaxStay(hours);
+                }
+                case "BIKE" ->
+            }
+        }
 
     }
 }
