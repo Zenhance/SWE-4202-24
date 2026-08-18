9c6e7ddca56051938211fb377058c6c30f4e3d17
wasif-22 <twasif68@gmail.com>
2026-08-18 09:51:07 +0600

    fixed vehicle class

--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -3,9 +3,16 @@ public abstract class Vehicle {
     int hoursStood;
     SlotKind slot;
     final Scheme scheme;
-    Vehicle(String platte){
-        this.plate=plate;
-        this.hoursStood=0;
-        this.scheme=scheme;
+
+    Vehicle(String platte) {
+        this.plate = plate;
+        this.hoursStood = 0;
+        this.scheme = scheme;
+    }
+
+    abstract SlotKind[] acceptOrder();
+
+    SlotKind ownKind() {
+        return acceptOrder()[0];
     }
 }
