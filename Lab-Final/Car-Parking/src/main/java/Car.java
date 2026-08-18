082ef8aea09a3eb2bed5ebb1e6d44865d6914079
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-08-18 12:39:48 +0600

    completed the car class by extending vehicle abstract class

--- Lab-Final/Car-Parking/src/main/java/Car.java
+++ Lab-Final/Car-Parking/src/main/java/Car.java
@@ -1,2 +1,9 @@
-public class Car {
+public class Car extends Vehicle{
+    public Car(String registration,String pass) {
+        super(registration,pass);
+    }
+    @Override
+    public String[] getSlotPreferences(){
+        return new String[]{"REGULAR", "LARGE"};
+    }
 }
