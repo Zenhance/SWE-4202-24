eb815ee800ef0007b93a291015fe96291bad1f73
Samin-init <mhsamin10@gmail.com>
2026-08-18 11:27:13 +0600

    Slot class and Slot Kind enum created

---
+++ Lab-Final/Car-Parking/src/main/java/SlotKind.java
@@ -0,0 +1,34 @@
+public enum SlotKind {
+    BIKE(10, 5, 0),
+    REGULAR(30, 20, 15),
+    LARGE(50, 40, 25);
+
+    private final int firstHour;
+    private final int furtherHour;
+    private final int surcharge;
+
+    SlotKind(
+            int firstHour,
+            int furtherHour,
+            int surcharge
+    ) {
+        this.firstHour = firstHour;
+        this.furtherHour = furtherHour;
+        this.surcharge = surcharge;
+    }
+
+    public int price(
+            int hours,
+            SlotKind vehicleKind
+    ) {
+        int amount =
+                firstHour
+                        + (hours - 1) * furtherHour;
+
+        if (this != vehicleKind) {
+            amount += surcharge;
+        }
+
+        return amount;
+    }
+}
