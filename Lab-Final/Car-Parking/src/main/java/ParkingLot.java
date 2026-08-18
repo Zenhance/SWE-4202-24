1e6cda1ebd5bb1161dfe64c6c58666a1e0e6fe54
Mugdho54 <mugdhorrmahee1525@gmail.com>
2026-08-18 11:18:10 +0600

    created a method

--- Lab-Final/Car-Parking/src/main/java/ParkingLot.java
+++ Lab-Final/Car-Parking/src/main/java/ParkingLot.java
@@ -1,2 +1,19 @@
-public class ParkingLot {
+import java.util.ArrayList;
+public class ParkingLot{
+    private int freeBike;
+    private int freeRegular;
+    private int freeLarge;
+    private int maxStay = Integer.MAX_VALUE;
+    private int currentTime = 0;
+    private int refused = 0;
+    private int earned = 0;
+    private final ArrayList<Vehicle> parkedVehicles = new ArrayList<>();
+
+    public void setSlots(int bikeSlots, int regularSlots, int largeSlots) {
+
+        freeBike = bikeSlots;
+        freeRegular = regularSlots;
+        freeLarge = largeSlots;
+    }
+
 }
