96f02ccb358eedcf8521a3d765d0b38915e155e5
shafin-75 <arafatshafin85@gmail.com>
2026-08-18 10:14:11 +0600

    Slotkind

--- Lab-Final/Car-Parking/src/main/java/SlotKind.java
+++ Lab-Final/Car-Parking/src/main/java/SlotKind.java
@@ -1,9 +1,17 @@
 import java.security.interfaces.DSAPublicKey;
 
-public class SlotKind {
+public class SlotKind {///
     BIKE(10,5,0);
     REGULAR(30,20,15);
     LARGE(50,40,25);
 
-    public final int FirstHour;
+    public final int firstHour;
+    public final int furtherHour;
+    public final int surcharge;
+
+    Slotkind(int firstHour, int furtherHour, int surcharge){
+        this.firstHour=firstHour;
+        this.furtherHour=furtherHour;
+        this.surcharge=surcharge;
+    }
 }
