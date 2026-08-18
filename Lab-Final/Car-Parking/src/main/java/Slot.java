e8822ea9906a4aa35c9e4a0d7366c40fb9210015
Areeba Zahra Kabir <areebazahra@iut-dhaka.edu>
2026-08-18 09:14:20 +0600

    created class slot

---
+++ Lab-Final/Car-Parking/src/main/java/Slot.java
@@ -0,0 +1,12 @@
+public class Slot {
+    SlotType type;
+    Vehicle vehicle;
+
+    public Slot(SlotType type){
+        this.type = type;
+        vehicle==null;
+    }
+    public boolean isFree(){
+        return vehicle=null;
+    }
+}
