296ed3a32039ae582106d4aaecad948d4b78da6c
Rakib <rakibulhasan4266@gmail.com>
2026-08-18 09:38:51 +0600

    copy README.md file

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -18,11 +18,33 @@ enum SlotType
     BIKE, REGULAR, LARGE
 }
 
-
-public class Main
+interface DiscountScheme
 {
-    public static void main(String[] args)
-    {
+    int applyDiscount(int billAmount);
+}
 
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNextLine()) {
+            String line = input.nextLine().trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            String[] field = line.split(" ");
+
+            if (field[0].equals("END")) {
+                break;
+            } else if (field[0].equals("MAXSTAY")) {
+                int hours = Integer.parseInt(field[1]);
+                // ... do something with hours ...
+            } else if (field[0].equals("COUNT")) {
+                System.out.println(/* the number you worked out */);
+            }
+            // ... one branch per keyword ...
+        }
     }
 }
