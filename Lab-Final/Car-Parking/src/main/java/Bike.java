ad534674189353404246f005b8a709d1c4844e64
Your Name <you@example.com>
2026-08-18 10:31:11 +0600

    vehicle

--- Lab-Final/Car-Parking/src/main/java/Bike.java
+++ Lab-Final/Car-Parking/src/main/java/Bike.java
@@ -1,2 +1,18 @@
-public class Bike {
+public class Bike extends Vehicle{
+    public Bike(String plate,Category category){
+        super(plate,category);
+    }
+    @Override
+    public SlotType[] prefferedSlot(){
+        return new SlotType[]{
+                SlotType.BIKE,
+                SlotType.REGULAR,
+                SlotType.LARGE
+        };
+    }
+    @Override
+    public int hourlyRate(){
+        return 10;
+    }
+
 }
