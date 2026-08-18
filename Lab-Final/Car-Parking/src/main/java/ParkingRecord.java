a58e0c11397f270979f7c4337a58e8684186884f
Fayaz16 <shahtanzim900@gmail.com>
2026-08-18 12:14:38 +0600

    parkingslot and parkingrecord committed

--- Lab-Final/Car-Parking/src/main/java/ParkingRecord.java
+++ Lab-Final/Car-Parking/src/main/java/ParkingRecord.java
@@ -1,2 +1,21 @@
-public class ParkingRecord {
+package parking;
+public class ParkingRecord{
+    private Vehicle vehicle;
+    private ParkingSlot slot;
+    private int startTime;
+
+    public ParkingRecord(Vehicle vehicle, ParkingSlot slot,int startTime){
+        this.vehicle = vehicle;
+        this.slot = slot;
+        this.startTime = startTime;
+    }
+    public Vehicle getVehicle(){
+        return vehicle;
+    }
+    public ParkingSlot getSlot(){
+        return slot;
+    }
+    public int getStartTime(){
+        return startTime;
+    }
 }
--- Lab-Final/Car-Parking/src/main/java/ParkingSlot.java
+++ Lab-Final/Car-Parking/src/main/java/ParkingSlot.java
@@ -1,2 +1,24 @@
-public class ParkingSlot {
+package parking ;
+public class ParkingSlot{
+    private SlotType type;
+    private Vehicle vehicle;
+    public ParkingSlot(SlotType type){
+        this.type = type;
+    }
+    public SlotType getType(){
+        return type;
+    }
+    public Vehicle getVehicle(){
+        return vehicle;
+    }
+    public boolean isFree(){
+        return vehicle == null;
+    }
+    public void park(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+        public void leave(){
+            this.vehicle = null;
+    }
 }
+
