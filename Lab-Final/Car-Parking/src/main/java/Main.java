987e66f70d317a527baf03e30b256acde064ee05
1mohsina <mohsinaislam@iut-dhaka.edu>
2026-08-18 09:03:39 +0600

    adding fields, class and constructor

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,7 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
 
+    enum VehicleType {
+        BIKE,
+        CAR,
+        TRUCK
+    }
+
+    enum SlotType {
+        BIKE,
+        REGULAR,
+        LARGE
+    }
+
+    enum DiscountType {
+        NONE,
+        STUDENT,
+        WEEKEND
+    }
+
+
+    public static class Vehicle {
+        String numPlate;
+        VehicleType type;
+        SlotType slotType;
+        DiscountType discountType;
+
+        int entryHour;
+
+        Vehicle(String numPlate, VehicleType type, SlotType slotType, DiscountType discountType, int entryHour) {
+            this.numPlate = numPlate;
+            this.type = type;
+            this.slotType = slotType;
+            this.discountType = discountType;
+            this.entryHour = entryHour;
+        }
+    }
+
+    public static void main(String[] args){
     }
 }
