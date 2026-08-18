9e807eb4490bcf8d40e441ee7c074fd7dc00535a
sabbir240042147 <eftesabbir2725@gmail.com>
2026-08-18 13:21:57 +0600

    parking slote update

--- Lab-Final/Car-Parking/src/main/java/ParkingSlot.java
+++ Lab-Final/Car-Parking/src/main/java/ParkingSlot.java
@@ -8,15 +8,43 @@ public class ParkingSlot {
     private int earned;
     private int refused;
     private ArrayList<Vehicle> vehicles;
-    public ParkingSlot(int bikeslots,int regularSlots,int largeSlots){
-        this.bikeslots=bikeslots;
-        this.regularSlots=regularSlots;
-        this.largeSlots=largeSlots;
-        this.earned=0;
-        this.refused=0;
-        vehicles=new ArrayList<>();
+    public ParkingSlot(int bikeslots,int regularSlots,int largeSlots) {
+        this.bikeslots = bikeslots;
+        this.regularSlots = regularSlots;
+        this.largeSlots = largeSlots;
+        this.earned = 0;
+        this.refused = 0;
+        vehicles = new ArrayList<>();
     }
+
+    public int getBikeslots() {
+        return bikeslots;
+    }
+
+    public int getRegularSlots() {
+        return regularSlots;
+    }
+
+    public int getLargeSlots() {
+        return largeSlots;
+    }
+
+    public int getMaxStay() {
+        return maxStay;
+    }
+
+    public int getEarned() {
+        return earned;
+    }
+
+    public int getRefused() {
+        return refused;
+    }
+
     public void setMaxStay(int maxStay){
         this.maxStay=maxStay;
+    }
+    public int getVehicleCount() {
+        return vehicles.size();
     }
 }
