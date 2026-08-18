e95777d377c1880fbc889e2fd2526a761f4d4129
waseefmunir-max <waseef05@gmail.com>
2026-08-18 11:56:56 +0600

    Add getHomeSlotType method to Vehicle for default slot type retrieval

--- Lab-Final/Car-Parking/src/main/java/Vehicle.java
+++ Lab-Final/Car-Parking/src/main/java/Vehicle.java
@@ -2,4 +2,8 @@ import java.util.List;
 
 public abstract class Vehicle {
     public abstract List<SlotType> getAcceptedSlotTypes();
+
+    public SlotType getHomeSlotType() {
+        return getAcceptedSlotTypes().get(0);
+    }
 }
