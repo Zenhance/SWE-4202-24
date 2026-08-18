f4d4d655e5ba7586e6d6f0347c4d48687380fe95
sabbir240042147 <eftesabbir2725@gmail.com>
2026-08-18 11:45:44 +0600

    slot part

--- Lab-Final/Car-Parking/src/main/java/Slot.java
+++ Lab-Final/Car-Parking/src/main/java/Slot.java
@@ -1,12 +1,23 @@
-public abstract  class Slot {
+import javax.swing.tree.VariableHeightLayoutCache;
+import java.util.concurrent.TimeUnit;
+
+public enum Slot {
+    BIKE(10,5,0),
+
+    REGULAR(30 ,20, 15),
+    LARGE(50, 40, 25);
+
     private final int firstHour;
     private final int furtherHour;
     private final int subCharge;
+
     Slot(int firstHour,int furtherHour,int subCharge){
         this.firstHour =firstHour;
         this.furtherHour=furtherHour;
         this.subCharge=subCharge;
+
     }
+
 
     public int getFirstHour() {
         return firstHour;
@@ -19,4 +30,5 @@ public abstract  class Slot {
     public int getSubCharge() {
         return subCharge;
     }
+
 }
