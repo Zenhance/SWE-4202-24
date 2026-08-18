1b19eb61d7f6331857309eac7a4af3302d1161a1
Tnvr09 <rahmantanvir64@gmail.com>
2026-08-18 10:49:55 +0600

    Slot class updated

--- Lab-Final/Car-Parking/src/main/java/Slot.java
+++ Lab-Final/Car-Parking/src/main/java/Slot.java
@@ -1,3 +1,29 @@
 public abstract class Slot {
+    private Vehicle currentVehicle = null;
     public abstract String getKindName();
+    public abstract int getFirstHourRate();
+    public abstract int getFurtherHourRate();
+    public abstract int getSurcharge();
+
+    public boolean isFree() {
+        return currentVehicle == null;
+    }
+
+    public Vehicle getCurrentVehicle() {
+        return currentVehicle;
+    }
+
+    public void park(Vehicle vehicle) {
+        this.currentVehicle = vehicle;
+    }
+
+    public void vacate() {
+        this.currentVehicle = null;
+    }
+
+
+
+
+
+
 }
