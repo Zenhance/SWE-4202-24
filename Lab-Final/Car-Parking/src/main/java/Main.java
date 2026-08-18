fe9159e9a07449ab9a615dfd6f4fa26fbd644579
hossdia <hossainmahdia22@gmail.com>
2026-08-18 13:11:52 +0600

    commit

--- Lab-Final/Car-Parking/src/main/java/Main.java
+++ Lab-Final/Car-Parking/src/main/java/Main.java
@@ -1,7 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int bikeSlots = 0;
+        int regularSlots = 0;
+        int largeSlots = 0;
+        int maxStay = 0;
+        ParkingLot parkingLot = null;
+        while (input.hasNextLine()) {
+            String line = input.nextLine().trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            String[] field = line.split(" ");
+            if (field[0].equals("END")) {
+                break;
+            }
+            if (field[0].equals("SLOTS")) {
+                bikeSlots = Integer.parseInt(field[1]);
+                regularSlots = Integer.parseInt(field[2]);
+                largeSlots = Integer.parseInt(field[3]);
+            }
+            else if (field[0].equals("MAXSTAY")) {
+                maxStay = Integer.parseInt(field[1]);
+                parkingLot = new ParkingLot(bikeSlots, regularSlots, largeSlots, maxStay);
+            }
 
+        }
+        }
     }
-}
