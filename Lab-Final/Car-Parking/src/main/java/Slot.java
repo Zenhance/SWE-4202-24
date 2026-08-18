98b79f0acac769fdeabfdc3c427970a1530b89ac
58-jahin <jahinhossain@iut-dhaka.edu>
2026-08-18 11:51:13 +0600

    2

--- Lab-Final/Car-Parking/src/main/java/Slot.java
+++ Lab-Final/Car-Parking/src/main/java/Slot.java
@@ -1,2 +1,37 @@
-public class Slot {
+public final class Slot {
+    private final SlotKind kind;
+    private Vehicle vehicle;
+
+    public Slot(SlotKind kind) {
+        this.kind = kind;
+    }
+
+    public SlotKind kind() {
+        return kind;
+    }
+
+    public Vehicle vehicle() {
+        return vehicle;
+    }
+
+    public boolean isFree() {
+        return vehicle == null;
+    }
+
+    public void park(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public int bill(int hours) {
+        int originalBill = kind.price(
+                hours,
+                vehicle.ownSlotKind()
+        );
+
+        return vehicle.discount().apply(originalBill);
+    }
+
+    public void removeVehicle() {
+        vehicle = null;
+    }
 }
