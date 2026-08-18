25f73c8e108fd431890af4d85f87d9185cd50de3
mahibrahaman175-stack <mahibrahaman175@gmail.com>
2026-08-18 10:16:04 +0600

    commit changes again

--- Lab-Final/Car-Parking/src/main/java/Slot/Slot.java
+++ Lab-Final/Car-Parking/src/main/java/Slot/Slot.java
@@ -1,4 +1,22 @@
 package Slot;
 
+import Vehicle.Vehicle;
+
 public class Slot {
+    private SlotType slotType;
+    private Vehicle vehicle;
+    private int hours;
+
+    public void isFree(){
+
+    }
+
+    public void accepts(Vehicle vehicle){
+
+    }
+
+    public double calculateBill(Vehicle vehicle, int hours){
+        return SlotType.
+    }
+
 }
