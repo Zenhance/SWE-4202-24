88ccb56727e20a3d59f8da943b51159d60603242
nusra <nusratnishi1103@gmail.com>
2026-08-18 10:29:15 +0600

    first

--- Lab-Final/Car-Parking/src/main/java/NoplateException.java
+++ Lab-Final/Car-Parking/src/main/java/NoplateException.java
@@ -1,4 +1,7 @@
 public class NoplateException extends Exception{
+    public NoplateException(){
+        super("There is no Vehicle with that plate Number");
+    }
     public NoplateException(String message){
         super(message);
     }
