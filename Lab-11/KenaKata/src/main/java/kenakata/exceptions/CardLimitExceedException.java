c238a7bc36a8f89f972067cd69fc23457484a364
iam-tahmid <tahmidim99@gmail.com>
2026-09-24 04:30:05 +0600

    duplicate CardLimitExceedException removed

--- Lab-11/KenaKata/src/main/java/kenakata/exceptions/CardLimitExceedException.java
+++
@@ -1,7 +0,0 @@
-package kenakata.exceptions;
-
-public class CardLimitExceedException extends CheckoutException {
-    public CardLimitExceedException(String message) {
-        super(message);
-    }
-}
