fc682e75c452948bd94113829a99223197a1b8c0
iam-tahmid <tahmidim99@gmail.com>
2026-09-25 02:24:31 +0600

    constructor fixed in Wallet class

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -4,6 +4,9 @@ public class Wallet {
     private int balance;
 
     public Wallet(int balance){
+        if(balance<0){
+            throw new IllegalArgumentException("Initial balance can't be negative");
+        }
         this.balance=balance;
     }
     public int getBalance(){
