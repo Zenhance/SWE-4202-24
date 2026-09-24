0e0b75066c0bad531c5bd75e98e13f2ae0222721
iam-tahmid <tahmidim99@gmail.com>
2026-09-25 04:19:41 +0600

    constructor added and pay method overriden in MobileWalletPayment

--- Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
@@ -1,4 +1,14 @@
 package kenakata.payment;
 
-public class MobileWalletPayment {
+public class MobileWalletPayment implements PaymentMethod{
+    private final Wallet wallet;
+
+    public MobileWalletPayment(Wallet wallet) {
+        this.wallet = wallet;
+    }
+
+    @Override
+    public void pay() {
+
+    }
 }
