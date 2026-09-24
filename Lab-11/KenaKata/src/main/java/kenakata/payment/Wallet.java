a522bb4502fca4a0c56503e8a4406bd819ab4d2a
iam-tahmid <tahmidim99@gmail.com>
2026-09-25 02:21:53 +0600

    constructor declared in Wallet class

--- Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/Wallet.java
@@ -1,4 +1,12 @@
 package kenakata.payment;
 
 public class Wallet {
+    private int balance;
+
+    public Wallet(int balance){
+        this.balance=balance;
+    }
+    public int getBalance(){
+        return balance;
+    }
 }
