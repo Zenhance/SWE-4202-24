b943d77105c340ab92bde41477bfb238ad31b32c
iam-tahmid <tahmidim99@gmail.com>
2026-09-24 06:06:42 +0600

    Seller class completed

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/Seller.java
@@ -1,6 +1,30 @@
 package kenakata.catalog;
 
+
+import java.util.Objects;
+
 public class Seller {
-    public Seller(String seller) {
+    private String name;
+    public Seller(String name) {
+        if(name == null || name.isBlank()){
+            throw new IllegalArgumentException("Seller can't be empty");
+        }
+
+        this.name=name;
+    }
+    public String name(){
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Seller seller = (Seller) o;
+        return Objects.equals(name, seller.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
