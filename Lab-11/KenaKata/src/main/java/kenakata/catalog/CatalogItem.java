db8424ab6f96113f6b8dcc41584857ea5e49f635
iam-tahmid <tahmidim99@gmail.com>
2026-09-24 22:56:56 +0600

    constructor and method added in StockedGood

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
@@ -49,5 +49,4 @@ public abstract class CatalogItem implements Chargeable {
         return title;
     }
 
-
 }
--- Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/StockedGood.java
@@ -1,6 +1,17 @@
 package kenakata.catalog;
 
-public class StockedGood  {
+public class StockedGood extends CatalogItem  {
+
+    private final int weightGrams;
+
+    public StockedGood(String sku, String title, long price, int stock, Seller seller, int weightGrams){
+        super(sku, title, price, stock, seller);
+        this.weightGrams=weightGrams;
+    }
+    public int WeightGrams(){
+        return weightGrams;
+    }
+
 
 
 
