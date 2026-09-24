5dea0c0dc104abc4627ab2b050b58e29c027fa00
iam-tahmid <tahmidim99@gmail.com>
2026-09-24 22:28:03 +0600

    constructor and variable declared in CatalogItem interface

--- Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
+++ Lab-11/KenaKata/src/main/java/kenakata/catalog/CatalogItem.java
@@ -3,7 +3,21 @@ package kenakata.catalog;
 
 public abstract class CatalogItem implements Chargeable {
 
+    protected final String sku;
+    protected final String title;
+    protected final long price;
+    protected final int stock;
+    protected final Seller seller;
+
+
+
     protected CatalogItem(String sku, String title, long price, int stock, Seller seller){
+        this.sku = sku;
+        this.title = title;
+        this.price = price;
+        this.stock=stock;
+        this.seller = seller;
+
     }
 
     @Override
