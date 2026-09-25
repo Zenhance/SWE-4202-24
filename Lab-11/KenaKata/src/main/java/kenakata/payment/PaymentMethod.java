fbc1bf910dd61d1de37204053232282d7ff0bc20
iam-tahmid <tahmidim99@gmail.com>
2026-09-25 16:02:02 +0600

    PaymentMethod interface fixed

--- Lab-11/KenaKata/src/main/java/kenakata/payment/PaymentMethod.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/PaymentMethod.java
@@ -1,5 +1,8 @@
 package kenakata.payment;
 
+import kenakata.exceptions.PaymentDeclinedException;
+
 public interface PaymentMethod {
     public void pay();
+    public void authorise(int amount) throws PaymentDeclinedException;
 }
