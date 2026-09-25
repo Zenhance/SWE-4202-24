b239ad2bf8283119807d00ba4392707c7f2c1c88
iam-tahmid <tahmidim99@gmail.com>
2026-09-25 18:02:47 +0600

    CashOnDeliveryPayment and MobileWalletPayment fixed

--- Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/CashOnDeliveryPayment.java
@@ -1,8 +1,14 @@
 package kenakata.payment;
 
+import kenakata.exceptions.PaymentDeclinedException;
+
 public class CashOnDeliveryPayment implements PaymentMethod{
     @Override
     public void pay() {
+    }
+
+    @Override
+    public void authorise(int amount) throws PaymentDeclinedException {
 
     }
 }
--- Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
+++ Lab-11/KenaKata/src/main/java/kenakata/payment/MobileWalletPayment.java
@@ -1,5 +1,10 @@
 package kenakata.payment;
 
+import kenakata.exceptions.EmptyWalletException;
+import kenakata.exceptions.PaymentDeclinedException;
+
+import java.util.EmptyStackException;
+
 public class MobileWalletPayment implements PaymentMethod{
     private final Wallet wallet;
 
@@ -9,6 +14,14 @@ public class MobileWalletPayment implements PaymentMethod{
 
     @Override
     public void pay() {
+
+    }
+
+    @Override
+    public void authorise(int amount) throws EmptyWalletException{
+        if(wallet !=null && wallet.balance()<amount){
+            throw new EmptyWalletException("Insufficient Balance");
+        }
 
     }
 }
