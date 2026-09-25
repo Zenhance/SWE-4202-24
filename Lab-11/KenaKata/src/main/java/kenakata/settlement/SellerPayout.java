ae156ffa9d2f0b242fc4f1b74f9fa3346acb7b9c
iam-tahmid <tahmidim99@gmail.com>
2026-09-25 06:22:53 +0600

    necessary methods added in SellerPayout

--- Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
+++ Lab-11/KenaKata/src/main/java/kenakata/settlement/SellerPayout.java
@@ -1,4 +1,21 @@
 package kenakata.settlement;
 
+import kenakata.catalog.Seller;
+
 public class SellerPayout {
+    public Seller seller(){
+        return null;
+    }
+    public long grossSales(){
+        return 0;
+    }
+    public long commission(){
+        return 0;
+    }
+    public long refunds(){
+        return 0;
+    }
+    public long payout(){
+        return 0;
+    }
 }
